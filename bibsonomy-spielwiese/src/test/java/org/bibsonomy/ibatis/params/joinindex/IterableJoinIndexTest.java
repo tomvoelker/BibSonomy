a9b669245a1de11d42a52708f4b7381f24922cd2
cschenk <devnull@localhost>
2006-10-19 00:46:43 +0200

    Implemented a JoinIndex as an Iterable which doesn't work with iBATIS

---
+++ bibsonomy-spielwiese/src/test/java/org/bibsonomy/ibatis/params/joinindex/IterableJoinIndexTest.java
@@ -0,0 +1,29 @@
+package org.bibsonomy.ibatis.params.joinindex;
+
+import java.util.Iterator;
+
+import junit.framework.TestCase;
+
+import org.bibsonomy.ibatis.JoinTest;
+import org.bibsonomy.ibatis.params.ByTagNames;
+import org.bibsonomy.ibatis.params.joinindex.IterableJoinIndex;
+import org.bibsonomy.ibatis.params.joinindex.JoinIndex;
+
+public class IterableJoinIndexTest extends TestCase {
+
+	/**
+	 * Tests the index-pairs.
+	 */
+	public void testIt() {
+		final ByTagNames btn = (new JoinTest()).getDefaultByTagNames();
+		// btn.addTagName("test");
+
+		final IterableJoinIndex iji = new IterableJoinIndex(btn.getTagIndex());
+		int i = 1;
+		for (final Iterator<JoinIndex> iter = iji.iterator(); iter.hasNext(); ) {
+			final JoinIndex index = iter.next();
+			assertEquals(i, index.getIndex1());
+			assertEquals(++i, index.getIndex2());
+		}
+	}
+}
