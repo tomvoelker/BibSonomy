38058135b6e7ebd8ad0f775a3125089fdaa31c46
cschenk <devnull@localhost>
2006-12-10 12:42:06 +0100

    Sanity check

--- misc/eclipse/project-descriptors/copy.sh
+++ misc/eclipse/project-descriptors/copy.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+test "`pwd | sed 's/.*\/\(.*\)/\1/'`" != "project-descriptors" && echo "Change to misc/eclipse/project-descriptors" && exit 1
 find . -iname .classpath -or -iname .project | sed 's/^.\/\(.*$\)/\1 ..\/..\/..\/\1/' | xargs -L1 cp
