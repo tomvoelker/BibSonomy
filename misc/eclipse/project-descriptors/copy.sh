acad41f2ad0ca9d2adcc0397271b8b373b731a34
cschenk <devnull@localhost>
2007-02-05 11:47:37 +0100

    Minor cleanup

--- misc/eclipse/project-descriptors/copy.sh
+++ misc/eclipse/project-descriptors/copy.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -x
+#!/bin/sh
 case $1 in
 	save)	test "`pwd | sed 's/.*\/\(.*\)/\1/'`" != "bibsonomy2" && echo "Change to the bibsonomy2 directory" && exit 1
 		find . -mindepth 2 -maxdepth 2 -iname .classpath -or -iname .project | sed 's/^\.\/\(.*\)/\1 misc\/eclipse\/project-descriptors\/\1/' | xargs -L1 cp
