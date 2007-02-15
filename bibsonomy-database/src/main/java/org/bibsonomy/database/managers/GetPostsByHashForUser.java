b9b87037cd5be157b3f278a1e744f0d68c57bd89
mgrahl <devnull@localhost>
2007-02-15 09:56:09 +0100

    handle object to check if request is applicable (given user and hash)

---
+++ bibsonomy-database/src/main/java/org/bibsonomy/database/managers/GetPostsByHashForUser.java
@@ -0,0 +1,39 @@
+package org.bibsonomy.database.managers;
+
+import java.util.List;
+import java.util.Set;
+
+import org.bibsonomy.database.params.BookmarkParam;
+import org.bibsonomy.model.Post;
+import org.bibsonomy.model.Resource;
+import org.bibsonomy.rest.enums.GroupingEntity;
+import org.bibsonomy.rest.enums.ResourceType;
+
+public class GetPostsByHashForUser extends RequestHandlerForGetPosts{
+
+	
+
+	@Override
+	protected List<Post<Resource>> handleRequestForGetPosts(String authUser, ResourceType resourceType, GroupingEntity grouping, String groupingName, Set<String> tags, String hash, boolean popular, boolean added, int start, int end) {
+		final BookmarkParam param = new BookmarkParam();
+		param.setRequestedUserName(groupingName);
+		param.setUserName(authUser);
+		param.setHash(hash);
+		param.setOffset(start);
+		int limit=end-start;
+		param.setLimit(limit);
+		
+		List<Post<Resource>> posts = db.bookmarkDatabaseManager.bookmarkList("getPostsByHashForUser", param, true);
+		return null;
+	}
+
+	@Override
+	protected boolean canHandle(String authUser, ResourceType resourceType, GroupingEntity grouping, String groupingName, Set<String> tags, String hash, boolean popular, boolean added, int start, int end) {
+		
+		if (hash != null && hash.length() > 0 && authUser!=null && grouping==GroupingEntity.USER && groupingName!=null) {
+			return true;
+		}
+		return false;
+
+}
+}
