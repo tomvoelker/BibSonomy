0aca31aab6ae8d6b4d097c87e795a94fa9bb0229
Manuel Bork <mbork@cs.uni-kassel.de>
2007-02-15 11:35:16 +0100

    cleanup

--- bibsonomy-model/src/main/java/org/bibsonomy/model/util/ModelUtils.java
+++ bibsonomy-model/src/main/java/org/bibsonomy/model/util/ModelUtils.java
@@ -1,12 +1,5 @@
 package org.bibsonomy.model.util;
 
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
-
-import org.bibsonomy.model.Post;
-import org.bibsonomy.model.Resource;
-import org.bibsonomy.model.User;
 
 /**
  * We are juggling with the model.
@@ -15,21 +8,5 @@ import org.bibsonomy.model.User;
  */
 public class ModelUtils {
 
-	/**
-	 * Creates a set of posts from a list of resources.
-	 */
-	// FIXME we don't need this anymore...
-//	public static Set<Post<Resource>> putResourcesIntoPosts(final List<? extends Resource> resources) {
-//		final Set<Post<Resource>> rVal = new LinkedHashSet<Post<Resource>>();
-//		for (final Resource resource : resources) {
-//			final Post<Resource> post = new Post<Resource>();
-////			post.setResource(resource);
-//			post.setTags(resource.getTags());
-//			final User user = new User();
-//			user.setName(resource.getUserName());
-//			post.setUser(user);
-//			rVal.add(post);
-//		}
-//		return rVal;
-//	}
+
 }
