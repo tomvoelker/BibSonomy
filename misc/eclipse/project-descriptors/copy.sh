8af45171ad592b17ee5b6dce459bb4ec29bcbdba
Robert Jäschke <jaeschke@l3s.de>
2007-02-20 13:21:56 +0100

    fixed to work with older xargs version

--- misc/eclipse/project-descriptors/copy.sh
+++ misc/eclipse/project-descriptors/copy.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 case $1 in
 	save)	test "`pwd | sed 's/.*\/\(.*\)/\1/'`" != "bibsonomy2" && echo "Change to the bibsonomy2 directory" && exit 1
-		find . -mindepth 2 -maxdepth 2 -iname .classpath -or -iname .project | sed 's/^\.\/\(.*\)/\1 misc\/eclipse\/project-descriptors\/\1/' | xargs -L1 cp
+		find . -mindepth 2 -maxdepth 2 -iname .classpath -or -iname .project | sed 's/^\.\/\(.*\)/\1 misc\/eclipse\/project-descriptors\/\1/' | xargs -l1 cp
 		;;
 	*)	test "`pwd | sed 's/.*\/\(.*\)/\1/'`" != "project-descriptors" && echo "Change to misc/eclipse/project-descriptors" && exit 1
-		find . -iname .classpath -or -iname .project | sed 's/^.\/\(.*$\)/\1 ..\/..\/..\/\1/' | xargs -L1 cp
+		find . -iname .classpath -or -iname .project | sed 's/^.\/\(.*$\)/\1 ..\/..\/..\/\1/' | xargs -l1 cp
 		;;
 esac
