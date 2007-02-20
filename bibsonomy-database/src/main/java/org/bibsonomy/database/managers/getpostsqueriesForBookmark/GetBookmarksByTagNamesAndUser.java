53e3104cd61d5a9c460e00e46509e5989a819c15
mgrahl <devnull@localhost>
2007-02-20 14:48:08 +0100

    return a list of bookmark by given user and tags

---
+++ bibsonomy-database/src/main/java/org/bibsonomy/database/managers/getpostsqueriesForBookmark/GetBookmarksByTagNamesAndUser.java
@@ -0,0 +1,74 @@
+package org.bibsonomy.database.managers.getpostsqueriesForBookmark;
+
+import java.util.List;
+
+import org.bibsonomy.database.params.BookmarkParam;
+import org.bibsonomy.model.Post;
+import org.bibsonomy.model.Resource;
+import org.bibsonomy.model.Tag;
+import org.bibsonomy.rest.enums.GroupingEntity;
+
+public class GetBookmarksByTagNamesAndUser extends RequestHandlerForGetPosts{
+	/**
+	 * 
+	 * @author mgr
+	 *
+	 */
+
+	/*
+	 * return a list of bookmark by given tag/tags and User.
+	 * Following arguments have to be given:
+	 * 
+	 * grouping:User
+	 * name:given
+	 * tags:given
+	 * hash:null
+	 * popular:false
+	 * added:false
+	 *   
+	 */
+	@Override
+	protected List<Post<? extends Resource>> handleRequestForGetPosts(String authUser,  GroupingEntity grouping, String groupingName, List<String> tags, String hash, boolean popular, boolean added, int start, int end) {
+		
+		final BookmarkParam param = new BookmarkParam();
+		param.setRequestedUserName(groupingName);
+		param.setUserName(authUser);
+		
+		param.setOffset(start);
+		int limit=end-start;
+		param.setLimit(limit);
+		param.setGroups(db.generalDatabaseManager.getGroupsForUser(param));
+		
+		for (String tag : tags){
+			
+			param.addTagName(tag);
+			
+			}
+		
+		
+		List<Post<? extends Resource>> posts = db.bookmarkDatabaseManager.bookmarkList("getBookmarkByTagNamesForUser", param, true);
+		return posts;
+
+	}
+	
+
+	@Override
+	protected boolean canHandle(String authUser, GroupingEntity grouping, String groupingName, List<String> tags, String hash, boolean popular, boolean added, int start, int end) {
+		
+		return
+		authUser != null && 
+		grouping==GroupingEntity.USER &&
+		tags!=null && 
+		hash==null &&
+		popular==false &&
+		added==false;
+		
+		
+		
+	}	
+
+
+}
+
+
+
