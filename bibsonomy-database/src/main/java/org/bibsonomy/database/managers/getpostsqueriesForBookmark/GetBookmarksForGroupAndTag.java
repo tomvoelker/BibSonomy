45f6668135daff8510e5e5f75b3676da1d444fc6
mgrahl <devnull@localhost>
2007-02-20 14:57:53 +0100

    return a list of bookmark by given tags and group

---
+++ bibsonomy-database/src/main/java/org/bibsonomy/database/managers/getpostsqueriesForBookmark/GetBookmarksForGroupAndTag.java
@@ -0,0 +1,64 @@
+package org.bibsonomy.database.managers.getpostsqueriesForBookmark;
+
+import java.util.List;
+
+import org.bibsonomy.database.params.BookmarkParam;
+import org.bibsonomy.model.Post;
+import org.bibsonomy.model.Resource;
+import org.bibsonomy.rest.enums.GroupingEntity;
+
+public class GetBookmarksForGroupAndTag extends RequestHandlerForGetPosts{
+
+	/**
+	 * 
+	 * @author mgr
+	 *
+	 */
+
+	/*
+	 * return a list of bookmark by a given group and common tags of a group.
+	 * Following arguments have to be given:
+	 * 
+	 * grouping:group
+	 * name:given
+	 * tags:given
+	 * hash:null
+	 * popular:false
+	 * added:false
+	 *   
+	 */
+	@Override
+	protected List<Post<? extends Resource>> handleRequestForGetPosts(String authUser, GroupingEntity grouping, String groupingName, List<String> tags, String hash, boolean popular, boolean added, int start, int end) {
+		final BookmarkParam param = new BookmarkParam();
+		
+		param.setRequestedGroupName(groupingName);
+		param.setUserName(authUser);
+		param.setOffset(start);
+		int limit=end-start;
+		param.setLimit(limit);
+		
+			for (String tag : tags){
+			
+			param.addTagName(tag);
+			
+			}
+		
+	    param.setGroupId(db.generalDatabaseManager.getGroupIdByGroupName(param));
+		param.setGroups(db.generalDatabaseManager.getGroupsForUser(param));
+		
+		List<Post<? extends Resource>> posts = db.bookmarkDatabaseManager.bookmarkList("getBookmarkForGroupByTag", param, true);
+		return posts;
+	}
+
+	@Override
+	protected boolean canHandle(String authUser,GroupingEntity grouping, String groupingName, List<String> tags, String hash, boolean popular, boolean added, int start, int end) {
+		return authUser != null && 
+			grouping == GroupingEntity.GROUP && groupingName != null && 
+			tags!=null && 
+			hash==null     &&
+			popular == false && 
+			added == false;
+	}
+
+
+}
