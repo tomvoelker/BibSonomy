b05fe8b46b317bcefaa7427477ccd13251c8c03d
mgrahl <devnull@localhost>
2007-04-04 10:56:10 +0200

    Tests are working work

--- bibsonomy-database/src/test/java/org/bibsonomy/database/managers/GenericChainHandlerTest.java
+++ bibsonomy-database/src/test/java/org/bibsonomy/database/managers/GenericChainHandlerTest.java
@@ -1,4 +1,4 @@
-package org.bibsonomy.database.managers;
+                package org.bibsonomy.database.managers;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -13,10 +13,36 @@ public class GenericChainHandlerTest extends AbstractDatabaseTest {
 
 	@Test
 	public void perform() {
-		// TODO implement tests
 		final List<String> taglist = new LinkedList<String>();
+		final List<String> taglistfriend = new LinkedList<String>();
 		taglist.add("semantic");
-		final List<Post<? extends Resource>> test = this.chainHandler.perform("jaeschke", GroupingEntity.USER, "jaeschke", taglist, null, false, false, 0, 19);
-		System.out.println(test.size() + " in my FirstTest");
+		taglistfriend.add("DVD");
+		// ByTagName
+	   // final List<Post<? extends Resource>> test = this.chainHandler.perform("jaeschke", GroupingEntity.ALL, "jaeschke", taglist, null, false, false, 0, 10);
+		 //ByTagNameForUser
+		//final List<Post<? extends Resource>> test = this.chainHandler.perform("jaeschke", GroupingEntity.USER, "jaeschke", taglist, null, false, false, 0, 19);
+		//ByConceptForUser 
+		//final List<Post<? extends Resource>> test = this.chainHandler.perform("jaeschke", GroupingEntity.USER, "jaeschke", taglist, null, false,true, 0, 19);
+		//ForUser
+		//final List<Post<? extends Resource>> test = this.chainHandler.perform("jaeschke", GroupingEntity.USER, "jaeschke", null, null, false, false, 0, 19);
+		//ByHash
+		//final List<Post<? extends Resource>> test = this.chainHandler.perform("jaeschke", GroupingEntity.ALL, "jaeschke", null,"de85f4da2df7bc83640a960cfd367497", false, false, 0, 19);
+		//ByHashForUser
+		//final List<Post<? extends Resource>> test = this.chainHandler.perform("jaeschke", GroupingEntity.USER, "jaeschke", null,"de85f4da2df7bc83640a960cfd367497", false, false, 0, 19);
+		//ByViewable
+		//final List<Post<? extends Resource>> test = this.chainHandler.perform("jaeschke", GroupingEntity.VIEWABLE, "jaeschke", null, null, false, false, 0, 19);
+		//ForGroup
+		//final List<Post<? extends Resource>> test = this.chainHandler.perform("jaeschke", GroupingEntity.GROUP, "kde", null, null, false, false, 0, 19);
+		//ForGroupByTag
+		//final List<Post<? extends Resource>> test = this.chainHandler.perform("jaeschke", GroupingEntity.GROUP, "kde", taglist, null, false, false, 0, 19);
+		//ByFriendName
+		//final List<Post<? extends Resource>> test = this.chainHandler.perform("jaeschke", GroupingEntity.FRIEND, "ralfm", null, null, false, false, 0, 19);
+		//ByFriendNameAndTag
+		//final List<Post<? extends Resource>> test = this.chainHandler.perform("jaeschke", GroupingEntity.FRIEND, "ralfm", taglistfriend, null, false, false, 0, 19);
+        //Popular
+		//final List<Post<? extends Resource>> test = this.chainHandler.perform("jaeschke", GroupingEntity.ALL, "jaeschke", taglist, null, true, false, 0, 19);
+		//Home
+		//final List<Post<? extends Resource>> test = this.chainHandler.perform("jaeschke", null, "jaeschke", taglist, null, false, false ,0, 19);
+		//System.out.println(test.size() + " in my FirstTest");
 	}
 }
