18aa708afc6be3efc165c380a6c246d94129323c
mgrahl <devnull@localhost>
2007-04-20 18:36:18 +0200

    first approach of postUser (logic is the same as for post a group)

--- bibsonomy-database/src/main/java/org/bibsonomy/database/managers/UserDatabaseManager.java
+++ bibsonomy-database/src/main/java/org/bibsonomy/database/managers/UserDatabaseManager.java
@@ -1,8 +1,13 @@
 package org.bibsonomy.database.managers;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import org.bibsonomy.database.AbstractDatabaseManager;
+import org.bibsonomy.database.params.UserParam;
+import org.bibsonomy.model.Bookmark;
+import org.bibsonomy.model.Post;
+import org.bibsonomy.model.Resource;
 import org.bibsonomy.model.User;
 /**
  * Used to retrieve users from the database.
@@ -43,6 +48,19 @@ public class UserDatabaseManager extends AbstractDatabaseManager  {
 		return (User) this.queryForObject("getUserDetails", user);
 	}
 	
+	/*
+	 * insert attributes for new user account
+	 */
+	public void insertUser(final User user) {
+		this.insert("insertUser", user);
+	}
+	
+	public void deleteUser(final User user) {
+		this.delete("deleteUser", user);
+	}
+	
+	
+	
 	
 	public List<User> getUsers(String authUser, int start, int end) {
 		return null;
@@ -57,17 +75,130 @@ public class UserDatabaseManager extends AbstractDatabaseManager  {
 	}
 	
 	
+	
 	public User getUserDetails(String authUserName, String userName) {
 		return null;
 	}
+	
+	/*
+	 * TODO delete should also include delete of tas beside personla information
+	 */
 	
 	public void deleteUser(String userName) {
 
 	}
 	
     public void storeUser(User user, boolean update) {
+    	/*
+    	 * TODO sql-statements are not implemented 
+    	 */
+    	/*
+    	 * user would like to update his/her personal information like password or email address
+    	 *************************************UPDATE********************************************
+    	 */
+    	
+    	if(update==true ){
+    		
+    	/*
+    	 * test if user already exist
+    	 */
+    	
+    		
+    		
+    	
+         List<User> userTemp=new  LinkedList<User>();
+    	 userTemp= getUsers(user.getName(),1,1);
+    		
+    		
+        /*
+         * if the user already exists, it must be exist an user account in database according give name
+         */	
+    		
+    		if(userTemp.size()==0){
+        		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        		System.out.println("++++++++++++++++no user for given name in database++++++++++++++++++++++");
+        		System.out.println("++++++++++++++++++++EXCEPTION++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        		throw new RuntimeException("No user for given name in database");
+        	}
+    		
+    		
+    		else{	
+    			/*
+    			 * userProve is the objec, which is already written in the database 
+    			 */
+    			
+    	        User proveUser =userTemp.get(0);
+   		 		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+   		 		System.out.println("+++++++an user object is returned for given user name++++++++++++++++++++++++++++++");     
+   		 		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+   		 		System.out.println("user_name = " + proveUser.getName());
+   		 		System.out.println(" email_address= " + proveUser.getEmail());
+   		 		System.out.println("password " + proveUser.getPassword());
+   		 		System.out.println("homepage  " + proveUser.getHomepage());
+   		 		System.out.println("registry Date " + proveUser.getRegistrationDate());
+   		 		
+   		 		/*
+   		 		 * comparison of user object in database and current handled user object 
+   		 		 * each attribute two is compared
+   		 		 * 
+   		 		 */
+
+   		 		if(proveUser.getName()!= user.getName()||proveUser.getEmail()!=user.getEmail()||proveUser.getHomepage()!=user.getHomepage()||proveUser.getPassword()!=user.getPassword()||proveUser.getRealname()!=user.getRealname()){
+   		 			
+   		 		/*
+   		 		 * loggen löschen einfügen
+   		 		 */
+   		 			
+   		 			
+   		 		}
+   		 		
+   		 		/*
+   		 		 * no changes in user settings 
+   		 		 */
+   		 		
+   		 		else{
+   		 			
+   		 		System.out.println("user make no changes for his settings");
+   		 			
+   		 			
+   		 		}
+   		 		
+    		}
+    		
+    		
+    		
+    		
+    	
+    	}
+    	
+    	
+    	else{
+    		
+    		/*
+    		 * new user does not exist and would like get an account
+    		 * **********************INSERT************************* 
+    		 */
+    		
+    	 if(update==false){
+    		 
+    		 this.insert("insertUser",user);
+    		 
+    		
+    		
+    	}
+    		
+    		
+   }
+    	
+    
+    
+    }
+    	
+    	
+    	
+    	
 		
-	}
 	
 	public boolean validateUserAccess(String username, String password) {
 		return true;
