ba818a2f9654d5a969e2963b7182fd1d79962495
Jens Illig <jil@cs.uni-kassel.de>
2007-05-02 00:18:01 +0200

    ->moved old test into web-project ->moved testing of chain-requests here

--- bibsonomy-database/src/test/java/org/bibsonomy/database/managers/RestDatabaseManagerTest.java
+++ bibsonomy-database/src/test/java/org/bibsonomy/database/managers/RestDatabaseManagerTest.java
@@ -1,110 +1,136 @@
+/*
+ * Created on 01.05.2007
+ */
 package org.bibsonomy.database.managers;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
-import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.commons.configuration.Configuration;
-import org.apache.commons.configuration.ConfigurationException;
-import org.apache.commons.configuration.PropertiesConfiguration;
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpException;
-import org.apache.commons.httpclient.UsernamePasswordCredentials;
-import org.apache.commons.httpclient.auth.AuthScope;
-import org.apache.commons.httpclient.methods.GetMethod;
-import org.dom4j.Document;
-import org.dom4j.DocumentException;
-import org.dom4j.DocumentHelper;
-import org.dom4j.Node;
+import org.bibsonomy.common.enums.GroupingEntity;
+import org.bibsonomy.database.LogicInterface;
+import org.bibsonomy.model.Post;
+import org.bibsonomy.model.Resource;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class RestDatabaseManagerTest {
-
-	private HttpClient client;
-	private String apiUrl;
+public class RestDatabaseManagerTest extends AbstractDatabaseManagerTest {
+	
+	protected LogicInterface restDb;
+	private List<Post<Resource>> postsList;
+	private List<String> taglist;
+	private List<String> taglistfriend;
 
 	@Before
-	public void setUp() throws ConfigurationException {
-		// Load configuration
-		final Configuration config = new PropertiesConfiguration("rest-api.properties");
-		final String host = config.getString("host");
-		final Integer port = config.getInt("port");
-		final String realm = config.getString("realm");
-		final String user = config.getString("user");
-		final String pass = config.getString("pass");
-		this.apiUrl = config.getString("url");
-
-		// configure the HTTP client
-		this.client = new HttpClient();
-		final AuthScope authScope = new AuthScope(host, port, realm);
-		final UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(user, pass);
-		this.client.getState().setCredentials(authScope, credentials);
+	public void setUp() {
+		super.setUp();
+		this.restDb = RestDatabaseManager.getInstance();
+		this.postsList = null;
+		this.taglist = new LinkedList<String>();
+		this.taglistfriend = new LinkedList<String>();
+		this.taglist.add("semantic");
+		this.taglistfriend.add("DVD");
 	}
 
 	@After
 	public void tearDown() {
-		this.client = null;
-		this.apiUrl = null;
-	}
-
-	/**
-	 * Starts a GET request with authentication on the REST-API
-	 */
-	private GetMethod getWebServiceAction(final String path) {
-		GetMethod get = null;
-		try {
-			get = new GetMethod(this.apiUrl + path);
-			get.setDoAuthentication(true);
-			this.client.executeMethod(get);
-		} catch (final Exception ex) {
-			fail("Exception");
-		}
-		return get;
-	}
-
-	/**
-	 * Converts the XML response from the REST-API into a DOM
-	 */
-	private Document getResponseBodyAsDocument(final GetMethod get) {
-		Document doc = null;
-		try {
-			doc = DocumentHelper.parseText(get.getResponseBodyAsString());
-		} catch (final DocumentException ex) {
-			fail("DocumentException");
-		} catch (final IOException ex) {
-			fail("IOException");
-		}
-		return doc;
+		super.tearDown();
+		this.postsList = null;
+		this.taglist = null;
+		this.taglistfriend = null;
 	}
 
 	@Test
-	public void aGetRequestWithoutAuthentication() throws HttpException, IOException {
-		final GetMethod get = new GetMethod(this.apiUrl);
-		this.client.executeMethod(get);
-		assertEquals(HttpServletResponse.SC_FORBIDDEN, get.getStatusCode());
+	public void performByTagName() {
+		// ByTagName
+		this.postsList = restDb.getPosts("jaeschke", Resource.class, GroupingEntity.ALL, "jaeschke", taglist, null, false, false, 0, 10);
+		assertEquals(10, this.postsList.size());
 	}
 
 	@Test
-	public void requestWithoutAction() throws HttpException, IOException {
-		final GetMethod get = this.getWebServiceAction("");
-		assertEquals(HttpServletResponse.SC_FORBIDDEN, get.getStatusCode());
+	public void performByTagNameForUser() {
+		// ByTagNameForUser
+		this.postsList = this.restDb.getPosts("jaeschke", Resource.class, GroupingEntity.USER, "jaeschke", taglist, null, false, false, 0, 19);
+		assertEquals(19, this.postsList.size());
 	}
 
 	@Test
-	public void getPosts() throws IOException, DocumentException {
-		final GetMethod get = this.getWebServiceAction("posts");
-		assertEquals(HttpServletResponse.SC_OK, get.getStatusCode());
+	public void performByConceptForUser() {
+		// ByConceptForUser
+		this.postsList = this.restDb.getPosts("jaeschke", Resource.class, GroupingEntity.USER, "jaeschke", taglist, null, false, true, 0, 19);
+		assertEquals(19, this.postsList.size());
+	}
 
-		final Document doc = this.getResponseBodyAsDocument(get);
+	@Test
+	public void performForUser() {
+		// ForUser
+		this.postsList = this.restDb.getPosts("jaeschke", Resource.class, GroupingEntity.USER, "jaeschke", null, null, false, false, 0, 19);
+		assertEquals(19, this.postsList.size());
+	}
 
-		// Check posts count
-		final Node posts = doc.selectSingleNode("//posts");
-		assertEquals(0, Integer.parseInt(posts.valueOf("@start")));
-		assertEquals(18, Integer.parseInt(posts.valueOf("@end")));
+	@Test
+	public void performByHash() {
+		// ByHash
+		this.postsList = this.restDb.getPosts("jaeschke", Resource.class, GroupingEntity.ALL, "jaeschke", null, "7d85e1092613fd7c91d6ba5dfcf4a044", false, false, 0, 19);
+		assertEquals(6, this.postsList.size());
+	}
+
+	@Test
+	public void performByHashForUser() {
+		// ByHashForUser
+		// FIXME geht noch nicht
+		this.postsList = this.restDb.getPosts("jaeschke", Resource.class, GroupingEntity.USER, "jaeschke", null, "7d85e1092613fd7c91d6ba5dfcf4a044", false, false, 0, 19);
+		assertEquals(1, this.postsList.size());
+	}
+
+	@Test
+	public void performByViewable() {
+		// ByViewable
+		this.postsList = this.restDb.getPosts("jaeschke", Resource.class, GroupingEntity.VIEWABLE, "jaeschke", null, null, false, false, 0, 19);
+		assertEquals(19, this.postsList.size());
+	}
+
+	@Test
+	public void performForGroup() {
+		// ForGroup
+		this.postsList = this.restDb.getPosts("jaeschke", Resource.class, GroupingEntity.GROUP, "kde", null, null, false, false, 0, 19);
+		assertEquals(19, this.postsList.size());
+	}
+
+	@Test
+	public void performForGroupByTag() {
+		// ForGroupByTag
+		this.postsList = this.restDb.getPosts("jaeschke", Resource.class, GroupingEntity.GROUP, "kde", taglist, null, false, false, 0, 19);
+		assertEquals(19, this.postsList.size());
+	}
+
+	@Test
+	public void performByFriendName() {
+		// ByFriendName
+		this.postsList = this.restDb.getPosts("jaeschke", Resource.class, GroupingEntity.FRIEND, "ralfm", null, null, false, false, 0, 19);
+		assertEquals(0, this.postsList.size());
+	}
+
+	@Test
+	public void performByFriendNameAndTag() {
+		// ByFriendNameAndTag
+		this.postsList = this.restDb.getPosts("jaeschke", Resource.class, GroupingEntity.FRIEND, "ralfm", taglistfriend, null, false, false, 0, 19);
+		assertEquals(0, this.postsList.size());
+	}
+
+	@Test
+	public void performPopular() {
+		// Popular
+		this.postsList = this.restDb.getPosts("jaeschke", Resource.class, GroupingEntity.ALL, "jaeschke", taglist, null, true, false, 0, 19);
+		assertEquals(91, this.postsList.size());
+	}
+
+	@Test
+	public void performHome() {
+		// Home
+		this.postsList = this.restDb.getPosts("jaeschke", Resource.class, null, "jaeschke", taglist, null, false, false, 0, 19);
+		assertEquals(15, this.postsList.size());
 	}
 }
