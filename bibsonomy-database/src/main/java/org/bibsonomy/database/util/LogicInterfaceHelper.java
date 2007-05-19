928e328ef8da798cccf3608da5cf05dd9c4c321a
Jens Illig <jil@cs.uni-kassel.de>
2007-05-20 01:40:48 +0200

    ->group enum

--- bibsonomy-database/src/main/java/org/bibsonomy/database/util/LogicInterfaceHelper.java
+++ bibsonomy-database/src/main/java/org/bibsonomy/database/util/LogicInterfaceHelper.java
@@ -3,6 +3,7 @@ package org.bibsonomy.database.util;
 import java.util.List;
 
 import org.bibsonomy.common.enums.GroupingEntity;
+import org.bibsonomy.database.Order;
 import org.bibsonomy.database.params.BibTexParam;
 import org.bibsonomy.database.params.BookmarkParam;
 import org.bibsonomy.database.params.GenericParam;
@@ -19,15 +20,14 @@ public class LogicInterfaceHelper {
 	/**
 	 * Builds a param object for the given parameters from the LogicInterface.
 	 */
-	public static <T extends GenericParam> T buildParam(final Class<T> type, final String authUser, final GroupingEntity grouping, final String groupingName, final List<String> tags, final String hash, final boolean popular, final boolean added, final int start, final int end) {
+	public static <T extends GenericParam> T buildParam(final Class<T> type, final String authUser, final GroupingEntity grouping, final String groupingName, final List<String> tags, final String hash, final Order order, final int start, final int end) {
 		final T param = getParam(type);
 		param.setUserName(authUser);
 		param.setGrouping(grouping);
 		param.setRequestedUserName(groupingName);
 		param.setRequestedGroupName(groupingName);
 		param.setHash(hash);
-		param.setPopular(popular);
-		param.setAdded(added);
+		param.setOrder(order);
 		param.setOffset(start);
 		param.setLimit(end - start);
 		if (tags != null) {
