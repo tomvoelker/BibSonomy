43cce07a757fa467a3a709ef5fc7793c004e8d72
Dominik Benz <benz@cs.uni-kassel.de>
2007-05-22 10:59:11 +0200

    inserted dummy test method

--- bibsonomy-database/src/test/java/org/bibsonomy/database/managers/chain/BookmarkChainTest.java
+++ bibsonomy-database/src/test/java/org/bibsonomy/database/managers/chain/BookmarkChainTest.java
@@ -1,5 +1,8 @@
 package org.bibsonomy.database.managers.chain;
 
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 /**
  * Test related to the Bookmark Chain
  * 
@@ -7,5 +10,11 @@ package org.bibsonomy.database.managers.chain;
  *
  */
 public class BookmarkChainTest extends AbstractChainTest {
-
+	/**
+	 * FIXME dummy method
+	 */
+	@Test
+	public void GetBookmarksForUser() {
+		
+	}
 }
