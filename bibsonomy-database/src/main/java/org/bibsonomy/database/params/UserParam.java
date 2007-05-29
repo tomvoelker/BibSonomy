6d156a7647220d5284d55b7b3ccffa23e8cbd3a4
mgrahl <devnull@localhost>
2007-05-29 17:02:37 +0200

    addition changes

--- bibsonomy-database/src/main/java/org/bibsonomy/database/params/UserParam.java
+++ bibsonomy-database/src/main/java/org/bibsonomy/database/params/UserParam.java
@@ -1,5 +1,7 @@
 package org.bibsonomy.database.params;
 
+import java.net.URL;
+
 /**
  * Parameters that are specific to users.
  *
@@ -9,6 +11,43 @@ package org.bibsonomy.database.params;
 public class UserParam extends GenericParam {
 
 	private String groupingName;
+	private String apiKey;
+	private String email;
+	private String password;
+	private String realname;
+	private URL homepage;
+	
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public URL getHomepage() {
+		return this.homepage;
+	}
+
+	public void setHomepage(URL url) {
+		this.homepage = url;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRealname() {
+		return this.realname;
+	}
+
+	public void setRealname(String realname) {
+		this.realname = realname;
+	}
 
 	public String getGroupingName() {
 		return this.groupingName;
@@ -16,5 +55,13 @@ public class UserParam extends GenericParam {
 
 	public void setGroupingName(String groupingName) {
 		this.groupingName = groupingName;
+	}
+
+	public String getApiKey() {
+		return this.apiKey;
+	}
+
+	public void setApiKey(String apiKey) {
+		this.apiKey = apiKey;
 	}
 }
