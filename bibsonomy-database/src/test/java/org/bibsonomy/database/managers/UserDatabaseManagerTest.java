6587176869aaffd3921b010ac044f75d38889205
Dominik Benz <benz@cs.uni-kassel.de>
2007-05-30 14:13:46 +0200

    added comments & created some new tests

--- bibsonomy-database/src/test/java/org/bibsonomy/database/managers/UserDatabaseManagerTest.java
+++ bibsonomy-database/src/test/java/org/bibsonomy/database/managers/UserDatabaseManagerTest.java
@@ -23,22 +23,35 @@ import org.junit.Test;
  */
 public class UserDatabaseManagerTest extends AbstractDatabaseManagerTest {
 
+	/**
+	 * Retrieve all users
+	 */
 	@Test
 	public void getAllUsers() {
 		final List<User> users = this.userDb.getAllUsers(this.userParam, this.dbSession);
 		assertEquals(1568, users.size());
 	}
 
+	/**
+	 * Details of a given user
+	 */
 	@Test
 	public void getUserDetails() {
 		this.userDb.getUserDetails(this.userParam, this.dbSession);
 	}
 
+	/**
+	 * Privacy level of a user
+	 */
 	@Test
 	public void getPrivlevelOfUser() {
 		assertEquals(GroupID.GROUP_PUBLIC.getId(), this.userDb.getPrivlevelOfUser(this.userParam, this.dbSession));
 	}
 
+	/**
+	 * Retrieve the names of users present in a group with given group ID
+	 */
+	@Test
 	public void getUserNamesOfGroupId() {
 		final List<String> users = this.userDb.getUserNamesByGroupId(GroupID.GROUP_KDE.getId(), dbSession);
 		final String[] kdeUsers = new String[] { "kde", "schmitz", "chs", "jaeschke", "stumme", "gst", "sfi", "finis", "rja", "aho", "hotho", "grahl", "beate" };
@@ -46,113 +59,93 @@ public class UserDatabaseManagerTest extends AbstractDatabaseManagerTest {
 		Assert.assertEquals(kdeUsers.length, users.size());
 	}
 
-	// TODO: implement and @Test
+	/**
+	 * Retrieve users who belong to the group "public" 
+	 * 
+	 * TODO: implement and @Test
+	 */
 	public void getUsersOfGroupPublic() {
 		final List<User> users = this.userDb.getUsersOfGroupPublic(this.userParam, this.dbSession);
 		assertEquals(13, users.size());
 	}
 
+	/**
+	 * Retrieve users who belong to the group "hidden"
+	 */
 	@Test
 	public void getUsersOfGroupHidden() {
 		final List<User> users = this.userDb.getUsersOfGroupPrivate(this.userParam, this.dbSession);
 		assertEquals(1, users.size());
 	}
 
-	// @SuppressWarnings("unchecked")
-	// public void testGetUsersByDate() {
-	// try {
-	// final ByUser bu = this.getDefaultByUser();
-	// /*
-	// * bekomme Liste von Usern zurück
-	// */
-	// /*
-	// * in XML ist id eingebettet
-	// */
-	// final List<User> users = this.sqlMap.queryForList("getByUser", bu);
-	//
-	// for (final User user : users) {
-	// System.out.println("name = " + user.getName());
-	// System.out.println("real name = " + user.getRealname());
-	// System.out.println("email = " + user.getEmail());
-	// System.out.println("homepage = " + user.getHomepage());
-	// System.out.println("registration date = " + user.getRegistrationDate());
-	// System.out.println("-------------------");
-	// }
-	// } catch (final SQLException ex) {
-	// ex.printStackTrace();
-	// }
-	// }
-
+	/**
+	 * Insert a new user
+	 */
 	@Test
 	public void testInsertUser() {
-		
-		UserParam param=new UserParam();
-		URL url;
+		UserParam param = new UserParam();
 		try {
-			url = new URL("http://www.db.de");
+			URL url = new URL("http://www.db.de");
 			param.setUserName("neuerUser");
 			param.setEmail("mgr@cs.uni-kassel.de");
 			param.setHomepage(url);
 			param.setPassword("dhdhd");
 			param.setRealname("mira");
-			this.userDb.insertUser(param,this.dbSession);
-			
-			User user =this.userDb.getUserDetails(param,this.dbSession);
-			
+			this.userDb.insertUser(param, this.dbSession);
+
+			User user = this.userDb.getUserDetails(param, this.dbSession);
+
 			assertEquals(user.getName(), "neuerUser");
 			assertEquals(user.getEmail(), "mgr@cs.uni-kassel.de");
 			assertEquals(user.getHomepage(), url);
 			assertEquals(user.getRealname(), "mira");
 			assertEquals(user.getApiKey(), param.getApiKey());
-			
-			
+
 		} catch (MalformedURLException ex) {
-			// TODO Auto-generated catch block
+			System.out.println("Malformed URL: ");
 			ex.printStackTrace();
 		}
-		
-		
-		
-		
-		
-		/*
-		 * try { final User user = this.getDefaultUser();
-		 * 
-		 * this.sqlMap.insert("insertUser", user);
-		 *  } catch (final SQLException ex) { ex.printStackTrace(); }
-		 */
 	}
 
-	/*
-	 * @Test public void getApiKeyForUser() {
-	 * this.userDb.getApiKeyForUser(this.userParam, this.dbSession); }
+	/**
+	 * Generate 10 API Keys and print them to stdout
 	 */
-
 	@Test
-	public void getApiKeyForUser() {
-
+	public void generateApiKeys() {
 		for (int i = 0; i < 10; i++) {
 			String apiKey = UserUtils.generateApiKey();
-
-			//System.out.println("ApiKey: " + apiKey);
-
+			System.out.println("Generated Api Key: " + apiKey);
 		}
 	}
 
+	/**
+	 *  Update an API key for a given user
+	 */
 	@Test
 	public void updateApiKeyForUser() {
-		
-		this.resetParameters();		
+		this.resetParameters(); // load default parameters
 		this.userDb.updateApiKeyForUser(this.userParam, this.dbSession);
-				
 		assertEquals(this.userParam.getApiKey(), this.userDb.getApiKeyForUser(this.userParam, this.dbSession));
-				
 	}
-	
+
+	/**
+	 * Re-Generate API keys for all users - handle with care! 
+	 */
 	@Test
-	
 	public void generateApiKeysForAllUsers() {
-		this.userDb.generateApiKeysForAllUsers(this.dbSession);
+		// this.userDb.generateApiKeysForAllUsers(this.dbSession);
+	}
+
+	/**
+	 * Test the user authentication via API key
+	 */
+	@Test
+	public void validateUserAccess() {
+		String username = "dbenz";
+		String apiKey = "ThisIsJustAFakeAPIKey";
+		Assert.assertFalse(this.userDb.validateUserAccess(username, apiKey, this.dbSession));
+		apiKey = "a9999a44a48879d28bd34fd32bdfa0c1"; // the correct key
+		Assert.assertTrue(this.userDb.validateUserAccess(username, apiKey, this.dbSession));				
 	}
 
 }
