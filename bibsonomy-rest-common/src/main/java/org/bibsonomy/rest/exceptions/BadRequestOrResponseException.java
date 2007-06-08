232214345e052da8213e115cb2ebf4127387b8b6
cschenk <devnull@localhost>
2007-06-09 01:06:35 +0200

    Removed unused code

--- bibsonomy-rest-common/src/main/java/org/bibsonomy/rest/exceptions/BadRequestOrResponseException.java
+++ bibsonomy-rest-common/src/main/java/org/bibsonomy/rest/exceptions/BadRequestOrResponseException.java
@@ -8,14 +8,6 @@ public class BadRequestOrResponseException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 
-	public BadRequestOrResponseException() {
-		super();
-	}
-
-	public BadRequestOrResponseException(final String message, final Throwable cause) {
-		super(message, cause);
-	}
-
 	public BadRequestOrResponseException(final String message) {
 		super(message);
 	}
