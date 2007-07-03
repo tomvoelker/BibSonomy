ce29403f951ec534d71bba38f326d080beada80e
mgrahl <devnull@localhost>
2007-07-03 15:14:58 +0200

    Extension of different simhashes in bookmarks

--- bibsonomy-database/src/main/java/org/bibsonomy/database/params/BookmarkParam.java
+++ bibsonomy-database/src/main/java/org/bibsonomy/database/params/BookmarkParam.java
@@ -1,6 +1,7 @@
 package org.bibsonomy.database.params;
 
 import org.bibsonomy.common.enums.ConstantID;
+import org.bibsonomy.common.enums.HashID;
 import org.bibsonomy.model.Bookmark;
 
 /**
@@ -13,6 +14,27 @@ public class BookmarkParam extends ResourcesParam<Bookmark> {
 
 	/** A single resource */
 	private Bookmark resource;
+	
+	/**
+	 * This is used to restrict simHashes, i.e. which limit the overall
+	 * resultset. By default simhash1 is used.
+	 */
+	private HashID simHash;
+	
+	/**
+	 * A user can search for hashes and this defines which simHash should be
+	 * used, e.g. either a restrictive or non-restrictive one. By default
+	 * simhash1 is used.
+	 */
+	
+	private HashID requestedSimHash;
+
+	public BookmarkParam() {
+		this.simHash = HashID.SIM_HASH;
+		this.requestedSimHash = HashID.SIM_HASH;
+	}
+
+	
 
 	@Override
 	public int getContentType() {
@@ -26,4 +48,22 @@ public class BookmarkParam extends ResourcesParam<Bookmark> {
 	public void setResource(Bookmark resource) {
 		this.resource = resource;
 	}
+	
+	public int getSimHash() {
+		return this.simHash.getId();
+	}
+
+	public void setSimHash(HashID simHash) {
+		this.simHash = simHash;
+	}
+
+	public int getRequestedSimHash() {
+		return this.requestedSimHash.getId();
+	}
+
+	public void setRequestedSimHash(HashID requSim) {
+		this.requestedSimHash = requSim;
+	}
+	
+	
 }
