15077e5118cf4a2298c760c1801a838361c23057
Jens Illig <jil@cs.uni-kassel.de>
2007-09-06 18:53:02 +0200

    ->moved method into bibsonomy-scraper project

--- bibsonomy-webapp/src/main/java/servlets/BibtexHandler.java
+++ bibsonomy-webapp/src/main/java/servlets/BibtexHandler.java
@@ -6,6 +6,7 @@ import helpers.database.DBBibtexManager;
 import helpers.database.DBContentManager;
 import helpers.database.DBGroupCopyManager;
 import helpers.database.DBScraperMetadataManager;
+import ie.ie.BibExtraction;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -925,12 +926,12 @@ public class BibtexHandler extends HttpServlet {
 		tagAbstractValue = entry.getFieldValue("tags");
 		if (tagAbstractValue != null) {
 			// clean tags
-			allTags.append(cleanTags(((BibtexString) tagAbstractValue).getContent(), substitute, delimiter, whitespace) + " ");
+			allTags.append(BibExtraction.cleanTags(((BibtexString) tagAbstractValue).getContent(), substitute, delimiter, whitespace) + " ");
 		}
 		tagAbstractValue = entry.getFieldValue("keywords");
 		if (tagAbstractValue != null) {
 			// clean tags
-			allTags.append(cleanTags(((BibtexString) tagAbstractValue).getContent(), substitute, delimiter, whitespace));
+			allTags.append(BibExtraction.cleanTags(((BibtexString) tagAbstractValue).getContent(), substitute, delimiter, whitespace));
 		}
 
 		
@@ -942,47 +943,6 @@ public class BibtexHandler extends HttpServlet {
 			bib.addTag(Tag.IMPORTED_TAG);
 		} else {
 			bib.setTags(allTags.toString());
-		}
-	}
-
-	/** Allows in a string of tags to change the delimiter to space. Additionally, tags consisting of
-	 * more than one word (separated by whitespace) can be joined with whitespaceSub.
-	 * 
-	 *  Example:
-	 *  
-	 *  with the delimiter = "," and whitespace = "_"
-	 *  the string 
-	 *    computer algebra, maple, math
-	 *  would be changed to
-	 *    computer_algebra maple math
-	 *  
-	 * @param tagstring - a string of tags.
-	 * @param substitute - <code>true</code>, if the tag string should be modified.
-	 * @param delimiter - the character which separates the tags in the tag string.
-	 * @param whitespaceSub - the character with which whitespace should be separated. 
-	 * This allows to merge tags with more than one word into a tag with one word.
-	 *   
-	 * @return The cleaned string of tags.
-	 */
-	public static String cleanTags(String tagstring, boolean substitute, String delimiter, String whitespaceSub) {
-		if (tagstring != null) {
-			if (substitute && delimiter != null && delimiter.length() == 1 && !delimiter.trim().equals("")) {
-				String tmpTags = tagstring.trim();
-
-				// remove whitespace around delimiter
-				tmpTags = tmpTags.replaceAll("\\s*" + Pattern.quote(delimiter) + "\\s*", delimiter);
-
-				// substitute whitespace inside tags
-				if (whitespaceSub != null && whitespaceSub.length() <= 1) {
-					tmpTags = tmpTags.replaceAll("\\s+", whitespaceSub);
-				}
-
-				// user wants to have another delimiter than whitespace
-				return tmpTags.replaceAll(Pattern.quote(delimiter), " ");
-			}
-			return tagstring;
-		} else {
-			return "";
 		}
 	}
 	
