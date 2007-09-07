900c888bffb0416069d7ecbaadf47d47bce81bf4
Robert Jäschke <jaeschke@l3s.de>
2007-09-07 08:35:21 +0200

    reverted

---
+++ bibsonomy-webapp/src/main/java/resources/SplittedAuthors.java
@@ -0,0 +1,65 @@
+package resources;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+/**
+ * Builds from a string containing white space separated author names
+ * a subquery which selects all publications which are published by the given authors.
+ *
+ */
+public class SplittedAuthors extends LinkedList<String> {
+
+	/**
+	 * serial version UID
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * The query string 
+	 */
+	String query = null;
+		
+	/**
+	 * Constructor
+	 * @param authors: String of authors and editor names
+	 */
+	public SplittedAuthors(String authors) {		
+		this.query = null;
+		
+		Scanner s = new Scanner(authors);
+		s.useDelimiter(" ");
+		while(s.hasNext()) {
+			String author = s.next();
+			this.add(author);
+		}		
+	}
+	
+	/**
+	 * generates the substring of the database query which mathes the author names
+	 * against author attribute
+	 *
+	 */
+	private void generateQuery() {
+		StringBuffer buf = new StringBuffer();
+		for (int i=0; i<this.size(); i++) {
+			if (this.get(i).startsWith("!"))
+				buf.append(" -" + this.get(i).substring(1, this.get(i).length()));
+			else
+				buf.append(" +" + this.get(i));
+		}
+		query = buf.toString();
+	}
+	
+	/**
+	 * 
+	 * @return the generated sub-querystring
+	 */
+	public String getQuery() {
+		if (query == null) {
+			generateQuery();
+		}
+		return query;
+	}
+}
