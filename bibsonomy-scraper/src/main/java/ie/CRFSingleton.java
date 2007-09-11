437b6b9c5d32fbd5c72b54263e75909d6be3f442
Robert Jäschke <jaeschke@l3s.de>
2007-09-11 09:22:37 +0200

    switched IEscraper to read crf.dat via class.getResourceAsStream

--- bibsonomy-scraper/src/main/java/ie/CRFSingleton.java
+++ bibsonomy-scraper/src/main/java/ie/CRFSingleton.java
@@ -1,12 +1,9 @@
 package ie;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 import edu.umass.cs.mallet.base.fst.CRF4;
@@ -16,12 +13,13 @@ import edu.umass.cs.mallet.base.fst.CRF4;
  */
 public class CRFSingleton {
 		
+	private static final String CRF_DAT = "crf.dat";
 	private static CRF4 crf = null;
 	
 	public CRFSingleton() throws FileNotFoundException, IOException, NamingException, ClassNotFoundException {
 		if(crf == null){
 			// get path to crf file and read crf
-			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(((String) ((Context) new InitialContext().lookup("java:/comp/env")).lookup("ieScraperCrfPath"))));
+			ObjectInputStream ois = new ObjectInputStream(this.getClass().getResourceAsStream(CRF_DAT));
 			crf = (CRF4) ois.readObject();
 			ois.close();
 		}		
