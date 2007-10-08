ff0d93e2e7f2a98bd61e374f266cb30be95a6b08
Dominik Benz <benz@cs.uni-kassel.de>
2007-10-08 16:11:59 +0200

    -> added support for fulltext search via the api (available under /api/posts?resourcetype=[TYPE]&search=[SEARCH])

--- bibsonomy-spielwiese/src/main/java/org/bibsonomy/spielwiese/springmvc/controller/HomepageController.java
+++ bibsonomy-spielwiese/src/main/java/org/bibsonomy/spielwiese/springmvc/controller/HomepageController.java
@@ -26,7 +26,7 @@ public class HomepageController implements Controller {
 		final Map<String, Object> model = new HashMap<String, Object>();
 		model.put("projectName", "BibSonomy");
 
-		final List<Post<Bookmark>> bookmarks = RestDatabaseManager.getInstance().getPosts("cschenk", Bookmark.class, null, null, null, null, null, 0, 10);
+		final List<Post<Bookmark>> bookmarks = RestDatabaseManager.getInstance().getPosts("cschenk", Bookmark.class, null, null, null, null, null, 0, 10, null);
 		model.put("bookmarks", bookmarks);
 
 		return new ModelAndView("home", model);
