5ba08078273c45c1e40b3b8f1e48005b6728c8e9
Robert Jäschke <jaeschke@l3s.de>
2007-10-15 16:54:04 +0200

    fixed myBibTeX link and re-ordered comparison operators

--- bibsonomy-webapp/src/main/java/servlets/MyBibSonomyHandler.java
+++ bibsonomy-webapp/src/main/java/servlets/MyBibSonomyHandler.java
@@ -18,37 +18,33 @@ import filters.SessionSettingsFilter;
  *
  */
 public class MyBibSonomyHandler extends HttpServlet {
-	
+
 	private static final long serialVersionUID = 3691036578076309554L;
-	
+
 	public void init(ServletConfig config) throws ServletException{	
 		super.init(config); 
 	}	
-	
+
 	public void doPost (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
 	}
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		 
+
 		UserBean user = SessionSettingsFilter.getUser(request);
-		
+
 		if (user.getName() != null) {
-				if (request.getRequestURI().equals("/myBibSonomy")){
-					response.sendRedirect("/user/" + URLEncoder.encode(user.getName(), "UTF-8"));
-				}
-				if (request.getRequestURI().equals("/myBibTeX")){
-					response.sendRedirect("/user/" + URLEncoder.encode(user.getName(), "UTF-8") + "?items=1000");
-				}
-				if (request.getRequestURI().equals("/myRelations")){
-					response.sendRedirect("/relations/" + URLEncoder.encode(user.getName(), "UTF-8"));
-				}
-				if (request.getRequestURI().equals("/myPDF")){
-					response.sendRedirect("/user/" + URLEncoder.encode(user.getName(), "UTF-8") + "?filter=myPDF");
-				}
-				if (request.getRequestURI().equals("/myDuplicates")){
-					response.sendRedirect("/user/" + URLEncoder.encode(user.getName(), "UTF-8") + "?filter=myDuplicates");
-				}
+			if ("/myBibSonomy".equals(request.getRequestURI())) {
+				response.sendRedirect("/user/" + URLEncoder.encode(user.getName(), "UTF-8"));
+			} else if ("/myBibTeX".equals(request.getRequestURI())) {
+				response.sendRedirect("/bib/user/" + URLEncoder.encode(user.getName(), "UTF-8") + "?items=1000");
+			} else if ("/myRelations".equals(request.getRequestURI())) {
+				response.sendRedirect("/relations/" + URLEncoder.encode(user.getName(), "UTF-8"));
+			} else if ("/myPDF".equals(request.getRequestURI())) {
+				response.sendRedirect("/user/" + URLEncoder.encode(user.getName(), "UTF-8") + "?filter=myPDF");
+			} else if ("/myDuplicates".equals(request.getRequestURI())) {
+				response.sendRedirect("/user/" + URLEncoder.encode(user.getName(), "UTF-8") + "?filter=myDuplicates");
+			}
 		} else {
 			response.sendRedirect("/login");
 		}
