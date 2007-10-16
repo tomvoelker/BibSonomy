38a326617fc281a6caf261c3c211046cc57dcfcc
Jens Illig <jil@cs.uni-kassel.de>
2007-10-16 10:38:15 +0200

    +version with only style CONTENT output

---
+++ bibsonomy-webapp/src/main/java/tags/XotherPersonsStyle.java
@@ -0,0 +1,40 @@
+package tags;
+
+import java.io.*;
+import javax.servlet.jsp.*;
+import javax.servlet.jsp.tagext.*;
+
+/**
+ * @author mgr
+ * Anzahl der USer, die auch diese Ressource getaggt haben: Einfärbung des Hintergrundes (v)
+ */
+
+
+public class XotherPersonsStyle extends TagSupport {
+	private static final long serialVersionUID = -5215482499853870382L;
+	private String value;
+	
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+	
+	public int doStartTag() throws JspException {
+		try {
+			// get integer from string
+			int v = Integer.parseInt(value) + 4;
+			// set maximum
+			if (v > 1024) {
+				v = 1024;
+			}
+			// 1024 posts = 100% (=50% of brightness)
+			v = (int) (100.0 - Math.log((double) v) / Math.log(2) * 2.0);
+			pageContext.getOut().print("background-color: rgb("+v+"%, "+v+"%, "+v+"%);");
+		} catch (IOException ioe) {
+			throw new JspException("Error: IOException while writing to client" + ioe.getMessage());
+		} catch (NumberFormatException e) {
+			throw new JspException("Error: NumberFormatException while writing to client" + e.getMessage());
+		}
+		return SKIP_BODY;
+	}
+}
