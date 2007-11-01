a0fbf9dbea20b86c7b3f9d2f7d98363ce144d176
Jens Illig <jil@cs.uni-kassel.de>
2007-11-01 22:33:47 +0100

    +comments

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/util/ErrorAware.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/util/ErrorAware.java
@@ -3,11 +3,23 @@
  */
 package org.bibsonomy.webapp.util;
 
-import org.bibsonomy.webapp.util.MinimalisticController;
-import org.bibsonomy.webapp.util.View;
+import org.bibsonomy.webapp.util.spring.controller.MinimalisticControllerSpringWrapper;
 import org.springframework.validation.Errors;
 
+/**
+ * optional interface MinimalisticControllers can implement to receive info
+ * about validation errors.
+ * 
+ * @author Jens Illig
+ */
 public interface ErrorAware {
+	/**
+	 * @return the validationerrors that occured in binding this request
+	 *         to the command
+	 */
 	public Errors getErrors();
+	/**
+	 * @param errors setter used by the framework (namely {@link MinimalisticControllerSpringWrapper}) to inject possible validationerrors
+	 */
 	public void setErrors(Errors errors);
 }
--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/util/View.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/util/View.java
@@ -3,6 +3,17 @@
  */
 package org.bibsonomy.webapp.util;
 
+/**
+ * interface of symbols describing an representational applicationstate.
+ * This is the communication protocoll between a controller and a viewresolver,
+ * which maps view symbols to renderers  
+ * 
+ * @author Jens Illig
+ */
 public interface View {
+	/**
+	 * @return the name of the view. may be used by a viewresolver to resolve
+	 *         the renderer to be used
+	 */
 	public String getName();
 }
