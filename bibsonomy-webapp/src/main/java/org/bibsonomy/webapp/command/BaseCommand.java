12a8f1ad8cb959212ae95f78cb1c40e923c4a306
Jens Illig <jil@cs.uni-kassel.de>
2007-11-01 22:29:50 +0100

    -unnecessary fields +comments

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/BaseCommand.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/BaseCommand.java
@@ -5,34 +5,57 @@ package org.bibsonomy.webapp.command;
 
 import org.bibsonomy.model.User;
 
+/**
+ * Base class for command objects. Contains request and response fields
+ * that are commonly used across a lot of controllers.
+ * 
+ * Command objects normally contain 
+ * all request arguments that a controller needs and all response values,
+ * which it creates. Views use the information in commands for rendering.
+ * 
+ * @author Jens Illig
+ */
 public class BaseCommand {
-	public static final String PROJECT_NAME = "bibsonomy";
 	private String ckey;
 	private User loginUser;
 	private String pageTitle;
 	
+	/**
+	 * @return the page title
+	 */
 	public String getPageTitle() {
 		return this.pageTitle;
 	}
 
+	/**
+	 * @param pageTitle the page title
+	 */
 	public void setPageTitle(String pageTitle) {
 		this.pageTitle = pageTitle;
 	}
 
-	public String getProjectName() {
-		return PROJECT_NAME;
-	}
-
+	/**
+	 * @return communication key used to save us from request forgery attacks
+	 */
 	public String getCkey() {
 		return this.ckey;
 	}
+	/**
+	 * @param ckey communication key used to save us from request forgery attacks
+	 */
 	public void setCkey(String ckey) {
 		this.ckey = ckey;
 	}
 	
+	/**
+	 * @return the user, who is currently logged in
+	 */
 	public User getLoginUser() {
 		return this.loginUser;
 	}
+	/**
+	 * @param loginUser the user, who is currently logged in
+	 */
 	public void setLoginUser(User loginUser) {
 		this.loginUser = loginUser;
 	}
