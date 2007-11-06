49307e25e840ca5561a17899c49a7ee4a80e995a
BibSonomy <webmaster@bibsonomy.org>
2007-11-06 14:34:01 +0100

    added URL to doc

--- bibsonomy-webapp/src/build/batch_scripts/purge_master_logs.sh
+++ bibsonomy-webapp/src/build/batch_scripts/purge_master_logs.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # purges the binary logs (on the master) which are older than two weeks
+# see also http://dev.mysql.com/doc/refman/5.0/en/purge-master-logs.html
 
 # which day was it two weeks ago?
 TWO_WEEKS_BEFORE=`date --date='-2 weeks' +'%Y-%m-%d'`
