04192a65d1a8eddbf83203b0e2727cbfc66420d2
Robert Jäschke <jaeschke@l3s.de>
2007-11-07 18:08:21 +0100

    builder to generate a nice tag cloud

--- bibsonomy-dl-importer/src/main/java/org/bibsonomy/importer/event/iswc/TagCloudBuilder.java
+++ bibsonomy-dl-importer/src/main/java/org/bibsonomy/importer/event/iswc/TagCloudBuilder.java
@@ -1,84 +1,158 @@
 package org.bibsonomy.importer.event.iswc;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.HashMap;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
-import java.util.StringTokenizer;
+import java.util.SortedSet;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
-import org.bibsonomy.importer.event.iswc.exceptions.RepositoryException;
-import org.bibsonomy.importer.event.iswc.rdf.RDFRepository;
+import org.apache.log4j.Logger;
 
-/**
- * With this class you can build an tag cloud, which counts every tag
- * and prints the results to a file.
- * @author tst
- *
- */
+
+
 public class TagCloudBuilder {
 
-	/**
-	 * Build the tag cloud and print it into file tag_cloud.txt
-	 * @param args first param must be the path to the rdf file
-	 */
+	private static final Logger log = Logger.getLogger(TagCloudBuilder.class);
+	
 	public static void main(String[] args) {
+		
+		final String[] tracks = new String[]{"fca", "todo", "text"};
+		final String requUser = "jaeschke";
+		final String username = args[0];
+		final String password = args[1];
+		final String dbUrl = "jdbc:mysql://gromit:3306/bibsonomy_clean?useUnicode=true&characterEncoding=UTF-8&characterSetResults=UTF-8";
+		
+		/*
+		 * build hashset for tracks
+		 */
+		final HashSet<String> trackSet = new HashSet<String>();
+		for (String track:tracks) {
+			trackSet.add(track);
+		}
+		
+		
+		
+			
 		try {
+			Connection conn = null;
+			PreparedStatement stmtP = null;
+			ResultSet rst = null;
+			/*
+			 * connect to DB
+			 */
+			Class.forName ("com.mysql.jdbc.Driver").newInstance ();
+			conn = DriverManager.getConnection (dbUrl, username, password);
+			log.info("Database connection established");
+			
+			if (conn != null){
+				try {
+					/*
+					 * query db
+					 * layout: data, date, host
+					 */
+					stmtP = conn.prepareStatement("SELECT t1.tag_name AS tag1, t2.tag_name AS tag2, COUNT(*) AS abcount FROM tas t1 JOIN tas t2 USING (content_id) " +
+							                      "  WHERE t1.user_name = ? " +
+							                      "    AND t1.content_type = 2 GROUP BY tag1, tag2");
+					stmtP.setString(1, requUser);
+					rst = stmtP.executeQuery();
+					
+					
+					Map<String, SortedSet<TagWithCount>> matrix = new TreeMap<String, SortedSet<TagWithCount>>();
+					/*
+					 * output result
+					 */
+					while (rst.next()) {
+						
+						String tag1 = rst.getString("tag1");
+						String tag2 = rst.getString("tag2");
+						int count = rst.getInt("abcount");
+						
+						if (trackSet.contains(tag1)) {
+							/*
+							 * tag2 co-occured with track!
+							 */
+							if (!matrix.containsKey(tag2)) {
+								/*
+								 * first occurence of tag2
+								 */
+								matrix.put(tag2, new TreeSet<TagWithCount>(new TagWithCountComparator()));
+							}
+							/*
+							 * remember count
+							 */
+							matrix.get(tag2).add(new TagWithCount(tag1, count, 0));
+						}
+					}
+					conn.close ();
+					
+					/*
+					 * find track with most occurences for each tag
+					 */
+					for (String tag: matrix.keySet()) {
+						/*
+						 * get maximum
+						 */
+						TagWithCount trackTag = matrix.get(tag).first();
+						
+						int fontSize = trackTag.count + 100; 
+						
+						String href  = " href=\"/user/" + requUser + "/" + tag + "?items=50\""; 
+						String title = " title=\"" + trackTag.globalCount + " posts\"";
+						String clazz = " class=\"tag_" + trackTag.tag + "\"";
+						String style = " style=\"font-size:" + fontSize + "%;\"";
+						
+						System.out.println("<a " + href + title + clazz + style + ">" + tag + "</a>&nbsp;");
+					}
+					
+				} catch (SQLException e) {
+					log.fatal(e);
+				}
+			}
+			
+		} catch (SQLException e) {
+			log.fatal(e);
+		} catch (InstantiationException e) {
+			log.fatal(e);
+		} catch (IllegalAccessException e) {
+			log.fatal(e);
+		} catch (ClassNotFoundException e) {
+			log.fatal(e);
+		} finally {
+			
+		}
+		
+		
+	}
+	
+	private static class TagWithCountComparator implements Comparator<TagWithCount> {
 
-			// init parameters
-			String rdfPath = null;
-			rdfPath = args[0];
-			
-			// map which use the tagname as key and counts the Frequency of the tag as its value
-			HashMap<String, Integer> tagMap = new HashMap<String, Integer>();
-			// read rdf file and build the repository
-			RDFRepository repository = new RDFRepository(rdfPath);
-			
-			// extract topics from RDF and put it into the tagMap
-			Map<String, String> keywords = repository.getKeywords();
-			for(String keywordString: keywords.values()){
-				StringTokenizer tokenizer = new StringTokenizer(keywordString);
-				while(tokenizer.hasMoreTokens()){
-					String tag = tokenizer.nextToken();
-					
-					// put new tag or incerement old tag
-					if(tagMap.containsKey(tag))
-						tagMap.put(tag, tagMap.get(tag).intValue()+1);
-					else
-						tagMap.put(tag, 1);
-				}
-			}
-			
-			// extract sessions from RDF and put it into tagMap
-			Map<String, String> sessions = repository.getSessions();
-			for(String sessionString: sessions.values()){
-				StringTokenizer tokenizer = new StringTokenizer(sessionString);
-				while(tokenizer.hasMoreTokens()){
-					String tag = tokenizer.nextToken();
-					
-					// put new tag or incerement old tag
-					if(tagMap.containsKey(tag))
-						tagMap.put(tag, tagMap.get(tag).intValue()+1);
-					else
-						tagMap.put(tag, 1);
-				}
-			}
-			
-			// write file tag_cloud.txt
-			FileWriter writer = new FileWriter("tag_cloud.txt");
-			for(String tag: tagMap.keySet()){
-				writer.write(tag + " " + tagMap.get(tag) + "\n");
-			}
-			
-			// cleanup
-			writer.flush();
-			writer.close();
-			
-		} catch (RepositoryException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
+		public int compare(TagWithCount o1, TagWithCount o2) {
+			return o2.count - o1.count;
 		}
 		
 	}
+	
+	private static class TagWithCount {
+		public String tag;
+		public int count;
+		public int globalCount;
+		
+		public TagWithCount(String tag, int count, int globalCount) {
+			super();
+			this.tag = tag;
+			this.count = count;
+			this.globalCount = globalCount;
+		}
+		
+	}
+	
 
 }
