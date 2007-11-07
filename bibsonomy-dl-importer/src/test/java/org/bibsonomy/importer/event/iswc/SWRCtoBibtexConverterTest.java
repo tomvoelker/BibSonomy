3a152a6404a2e70d4c9852a6e78efda62af6e92a
Thomas Steuber <steuber@cs.uni-kassel.de>
2007-11-07 11:22:33 +0100

    tests for iswc converter

---
+++ bibsonomy-dl-importer/src/test/java/org/bibsonomy/importer/event/iswc/SWRCtoBibtexConverterTest.java
@@ -0,0 +1,37 @@
+package org.bibsonomy.importer.event.iswc;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.bibsonomy.importer.event.iswc.exceptions.RepositoryException;
+import org.bibsonomy.importer.event.iswc.rdf.RDFRepositoryTest;
+
+/**
+ * test of the methods from {@link SWRCtoBibtexConverter}.
+ * @author tst
+ *
+ */
+public class SWRCtoBibtexConverterTest {
+
+	/**
+	 * checks if the convertion works through completely. 
+	 */@Test
+	public void convertToBibtexTest(){
+		
+		// init converter
+		SWRCtoBibtexConverter converter = new SWRCtoBibtexConverter();
+		
+		try {
+			// run convertion with test files
+			converter.convertToBibtex(RDFRepositoryTest.TEST_RDF_FILE);
+			// it runs through all, assert as true
+			assertTrue(true);
+		} catch (RepositoryException e) {
+			fail(e.getMessage());
+		} catch (IOException e) {
+			fail(e.getMessage());
+		}
+	}
+}
---
+++ bibsonomy-dl-importer/src/test/java/org/bibsonomy/importer/event/iswc/rdf/RDFRepositoryTest.java
@@ -0,0 +1,82 @@
+package org.bibsonomy.importer.event.iswc.rdf;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+import org.bibsonomy.importer.event.iswc.exceptions.RepositoryException;
+import org.bibsonomy.importer.event.iswc.model.Publication;
+
+import static org.junit.Assert.*;
+
+/**
+ * Test class for {@link RDFRepository}.
+ * @author tst
+ */
+public class RDFRepositoryTest {
+
+	/**
+	 * standard test RDF file
+	 */
+	public static final String TEST_RDF_FILE = "src/test/resources/conferenceTest.rdf";
+
+	/**
+	 * checks if the RDF file is successfull loaded and checks if inproceedings can be extracted. 
+	 */@Test
+	public void readInproceedingsFromRDFTest(){
+		try {
+			
+			// first check repository
+			RDFRepository repository = new RDFRepository(TEST_RDF_FILE);
+			assertNotNull(repository);
+
+			// build and check inproceedings result
+			List<Publication> publications = repository.getInproceedings();
+			assertNotNull(publications);
+			assertTrue(publications.size() > 0);
+			
+		} catch (RepositoryException e) {
+			fail(e.getMessage());
+		}
+	}
+
+	/**
+	 * checks if the RDF file is successfull loaded and checks if proceedings can be extracted. 
+	 */@Test
+	public void readProceedingsFromRDFTest(){
+		try {
+			
+			// first check repository
+			RDFRepository repository = new RDFRepository(TEST_RDF_FILE);
+			assertNotNull(repository);
+
+			// build and check proceedings result
+			List<Publication> publications = repository.getProceedings();
+			assertNotNull(publications);
+			assertTrue(publications.size() > 0);
+				
+		} catch (RepositoryException e) {
+			fail(e.getMessage());
+		}
+	}
+			
+	/**
+	 * checks if the RDF file is successfull loaded and checks if sessions can be extracted. 
+	 */@Test
+	public void readSessionsFromRDFTest(){
+		try {
+			
+			// first check repository
+			RDFRepository repository = new RDFRepository(TEST_RDF_FILE);
+			assertNotNull(repository);
+
+			// build and check session result
+			Map<String, String> sessions = repository.getSessions();
+			assertNotNull(sessions);
+			assertTrue(sessions.size() > 0);
+			
+		} catch (RepositoryException e) {
+			fail(e.getMessage());
+		}
+	}		 
+}
