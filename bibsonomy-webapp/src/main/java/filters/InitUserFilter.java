c27b30cc4abfc2f625c743ecdcc6a73b2e9fec8b
Dominik Benz <benz@cs.uni-kassel.de>
2007-12-05 21:05:33 +0100

    transferred user authentication to new dblogic backend

--- bibsonomy-webapp/src/main/java/filters/InitUserFilter.java
+++ bibsonomy-webapp/src/main/java/filters/InitUserFilter.java
@@ -1,7 +1,5 @@
 package filters;
 
-import helpers.database.DBUserManager;
-
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.security.cert.CertificateException;
@@ -20,7 +18,15 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.codec.binary.Base64;
 import org.apache.log4j.Logger;
+import org.bibsonomy.common.exceptions.ValidationException;
+import org.bibsonomy.database.DBLogicUserInterfaceFactory;
+import org.bibsonomy.database.util.IbatisDBSessionFactory;
 
+import org.bibsonomy.model.User;
+import org.bibsonomy.model.Group;
+import org.bibsonomy.model.logic.LogicInterface;
+
+import beans.SettingsBean;
 import beans.UserBean;
 
 /**
@@ -43,9 +49,10 @@ public class InitUserFilter implements Filter {
 	/**
 	 * Constants to describe Cookie and Bean informations
 	 */
-	public static final String USER_COOKIE_NAME     = "_currUser";
-	public static final String SETTINGS_COOKIE_NAME	= "_styleSettings";
-	public static final String REQ_ATTRIB_USER      = "user";
+	public static final String USER_COOKIE_NAME     	= "_currUser";
+	public static final String SETTINGS_COOKIE_NAME		= "_styleSettings";
+	public static final String REQ_ATTRIB_USER      	= "user";
+	public static final String REQ_ATTRIB_LOGIN_USER    = "loginUser";
 	public static boolean useX509forAuth = false;
 
 	/**
@@ -78,7 +85,6 @@ public class InitUserFilter implements Filter {
 	/** 
 	 * This method does the main work
 	 * 
-	 * 
 	 * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest, javax.servlet.ServletResponse, javax.servlet.FilterChain)
 	 */
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
@@ -96,83 +102,96 @@ public class InitUserFilter implements Filter {
 
 
 		/*
-		 * check user and get user information
+		 * try to authenticate with the given username & password;
+		 * if successful, get user details
 		 */
-		UserBean user = null;
 
+		DBLogicUserInterfaceFactory dbLogicFactory = new DBLogicUserInterfaceFactory();
+		dbLogicFactory.setDbSessionFactory(new IbatisDBSessionFactory());
+		User loginUser = null;
+				
 		String userCookie = getCookie(httpServletRequest,USER_COOKIE_NAME);
-
-		if (userCookie != null) {
-			log.info("found user cookie");
-			/* 
-			 * user has Cookie set: try to authenticate 
-			 */
-			String userCookieParts[] = userCookie.split("%20");
-			if (userCookieParts.length >= 2) {
-				/*
-				 * all two parts of cookie available
+		
+		// check user authentication
+		try {			
+			if (userCookie != null) {
+				log.info("found user cookie");
+				/* 
+				 * user has Cookie set: try to authenticate 
 				 */
-				user = DBUserManager.getSettingsForUser(userCookieParts[0], userCookieParts[1]);
-			} else {
+				String userCookieParts[] = userCookie.split("%20");
+				if (userCookieParts.length >= 2) {
+					/*
+					 * all two parts of cookie available
+					 */
+					LogicInterface logic = dbLogicFactory.getLogicAccess(userCookieParts[0], userCookieParts[1]);
+					loginUser = logic.getUserDetails(userCookieParts[0]);								
+				} else {
+					/*
+					 * something is wrong with the cookie: log!
+					 */
+					String ip_address = ((HttpServletRequest)request).getHeader("x-forwarded-for");
+					log.warn("Someone manipulated the user cookie (IP: " + ip_address + ") : " + userCookie);
+				}			
+			} else if (useX509forAuth) {
 				/*
-				 * something is wrong with the cookie: log!
+				 * special handling for SAP X.509 certificates
 				 */
-				String ip_address = ((HttpServletRequest)request).getHeader("x-forwarded-for");
-				log.warn("Someone manipulated the user cookie (IP: " + ip_address + ") : " + userCookie);
+				try {
+					log.info("no user cookie found, trying X.509");
+					/*
+					 * get user name from client certificate
+					 */
+					String uname = getUserName(httpServletRequest);
+					/*
+					 *  FIXME: here we should put user into DB, if not already contained, i.e., 
+					 *  INSERT IGNORE INTO user VALUES ...  
+					 */
+					LogicInterface logic = dbLogicFactory.getLogicAccess(uname, "*");
+					loginUser = logic.getUserDetails(uname);					
+					/*
+					 * this should not be neccessary, if we got the user from the database ...
+					 */
+					if (loginUser == null) {
+						loginUser = new User();
+						loginUser.setName(uname);
+					}
+				} catch (Exception e)  {
+					log.info("certificate authentication failed");
+				}				
+			} else if (HttpServletRequest.BASIC_AUTH.equals(httpServletRequest.getAuthType())) {
+				/*
+				 * HTTP BASIC AUTHENTICATION
+				 */
+				log.info("auth via http basic auth");
+				
+				// get password sent by client in HTTP Authentication Header
+				String userpassDecoded = getUserPassFromHTTPBasicAuthHeader(httpServletRequest);
+				    
+				/*
+				 * FIXME: The password is expected to be already MD5-encoded (as in the cookie).
+				 * This is typically not the case (i.e., user enters password in browser), but 
+				 * we decided to do it this way because we implemented this mechanism exclusively
+				 * for integration of publication lists into CMS. There cookie handling is often
+				 * difficult and one does not want the cleartext password to be written into the 
+				 * CMS (at least in our (eecs) scenario).
+				 */
+				
+				// try to authenticate user
+				LogicInterface logic = dbLogicFactory.getLogicAccess(httpServletRequest.getRemoteUser(), userpassDecoded);
+				loginUser = logic.getUserDetails(httpServletRequest.getRemoteUser());
 			}
-		} else if (useX509forAuth) {
-			/*
-			 * special handling for SAP X.509 certificates
-			 */
-			try {
-				log.info("no user cookie found, trying X.509");
-				/*
-				 * get user name from client certificate
-				 */
-				String uname = getUserName(httpServletRequest);
-				/*
-				 *  FIXME: here we should put user into DB, if not already contained, i.e., 
-				 *  INSERT IGNORE INTO user VALUES ...  
-				 */
-				user =  DBUserManager.getSettingsForUser(uname,"*");
-				/*
-				 * this should not be neccessary, if we got the user from the database ...
-				 */
-				if (user == null) {
-					user = new UserBean();
-					user.setName(uname);
-				}
-			} catch (Exception e)  {
-				log.info("certificate authentication failed");
-			}
-		} else if (HttpServletRequest.BASIC_AUTH.equals(httpServletRequest.getAuthType())) {
-			/*
-			 * HTTP BASIC AUTHENTICATION
-			 */
-			log.info("auth via http basic auth");
-			
-			// get password sent by client in HTTP Authentication Header
-			String userpassDecoded = getUserPassFromHTTPBasicAuthHeader(httpServletRequest);
-			    
-			/*
-			 * FIXME: The password is expected to be already MD5-encoded (as in the cookie).
-			 * This is typically not the case (i.e., user enters password in browser), but 
-			 * we decided to do it this way because we implemented this mechanism exclusively
-			 * for integration of publication lists into CMS. There cookie handling is often
-			 * difficult and one does not want the cleartext password to be written into the 
-			 * CMS (at least in our (eecs) scenario).
-			 */
-			
-			// try to authenticate user
-			user = DBUserManager.getSettingsForUser(httpServletRequest.getRemoteUser(), userpassDecoded);
 		}
-
-		if (user == null) {
+		catch (ValidationException valEx) {
+			log.info(valEx.getMessage());
+		}
+		
+		if (loginUser == null) {
 			log.info("user not found in DB or user has no cookie set");
 			/*
 			 * user is not in DB/authenticated properly: get/set values from Cookie 
 			 */
-			user = new UserBean();
+			loginUser = new User();
 
 			String settingsCookie = getCookie(httpServletRequest, SETTINGS_COOKIE_NAME);
 			if (settingsCookie != null) {
@@ -185,11 +204,11 @@ public class InitUserFilter implements Filter {
 				// check for manipulated cookie
 				if (settingsCookieParts.length == 5) {
 					log.info("settings cookie is valid, using it");
-					user.setTagboxStyle		(Integer.parseInt(settingsCookieParts[0]));
-					user.setTagboxSort		(Integer.parseInt(settingsCookieParts[1]));
-					user.setTagboxMinfreq	(Integer.parseInt(settingsCookieParts[2]));
-					user.setTagboxTooltip	(Integer.parseInt(settingsCookieParts[3]));
-					user.setItemcount		(Integer.parseInt(settingsCookieParts[4]));
+					loginUser.getSettings().setTagboxStyle		(Integer.parseInt(settingsCookieParts[0]));
+					loginUser.getSettings().setTagboxSort		(Integer.parseInt(settingsCookieParts[1]));
+					loginUser.getSettings().setTagboxMinfreq	(Integer.parseInt(settingsCookieParts[2]));
+					loginUser.getSettings().setTagboxTooltip	(Integer.parseInt(settingsCookieParts[3]));
+					loginUser.getSettings().setListItemcount	(Integer.parseInt(settingsCookieParts[4]));
 				}
 			}
 		}
@@ -197,8 +216,16 @@ public class InitUserFilter implements Filter {
 		/*
 		 * put bean into request for following Servlets/JSPs
 		 */
-		httpServletRequest.setAttribute(REQ_ATTRIB_USER, user);
-		log.info("finished: " + user);
+		httpServletRequest.setAttribute(REQ_ATTRIB_LOGIN_USER, loginUser);
+		
+		/*
+		 * for backwards compatibility, we copy here the data from the user object into a
+		 * "BibSonomy 1" UserBean Object
+		 */				
+		// TODO copy contents loginUser -> user
+		httpServletRequest.setAttribute(REQ_ATTRIB_USER, createUserBean(loginUser));
+		
+		log.info("finished: " + loginUser);
 
 		// Pass control on to the next filter
 		chain.doFilter(request, response);		
@@ -306,5 +333,31 @@ public class InitUserFilter implements Filter {
 		return uid.trim();
 	}
 
-
+	private static UserBean createUserBean(User loginUser) {
+		
+		// general info
+		UserBean userBean = new UserBean();
+		userBean.setEmail(loginUser.getEmail());
+		userBean.setHomepage(loginUser.getHomepage() == null? null : loginUser.getHomepage().toString());
+		userBean.setName(loginUser.getName());
+		userBean.setOpenurl(loginUser.getOpenURL());
+		userBean.setRealname(loginUser.getRealname());
+		
+		//settings
+		userBean.setTagboxMinfreq(loginUser.getSettings().getTagboxMinfreq());
+		userBean.setTagboxSort(loginUser.getSettings().getTagboxSort());
+		userBean.setTagboxStyle(loginUser.getSettings().getTagboxStyle());
+		userBean.setTagboxTooltip(loginUser.getSettings().getTagboxTooltip());
+		userBean.setItemcount(loginUser.getSettings().getListItemcount());
+		
+		//basket size
+		userBean.setPostsInBasket(loginUser.getBasket().getNumPosts());
+				
+		//groups
+		for(Group g : loginUser.getGroups()) {
+			userBean.addGroup(g.getName());
+		}
+		return userBean;
+	}
+	
 }
