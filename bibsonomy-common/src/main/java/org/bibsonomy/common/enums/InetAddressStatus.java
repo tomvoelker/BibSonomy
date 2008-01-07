8f80ecaa277a633b871f6402a31bfc520a13b2f8
Robert Jäschke <jaeschke@l3s.de>
2008-01-07 15:29:25 +0100

    added Enum to represent states of internet addresses

---
+++ bibsonomy-common/src/main/java/org/bibsonomy/common/enums/InetAddressStatus.java
@@ -0,0 +1,12 @@
+package org.bibsonomy.common.enums;
+
+/**
+ * @author rja
+ * @version $Id$
+ */
+public enum InetAddressStatus {
+	
+	/** The IP is blocked - write access is not allowed at all (neither registration nor posting). */
+	WRITEBLOCKED;
+
+}
