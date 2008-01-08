0570daa0d2ce251738467a7c65a310db84ea2cf6
Robert Jäschke <jaeschke@l3s.de>
2008-01-08 17:55:59 +0100

    implemented neccessary methods

--- bibsonomy-database/src/main/java/org/bibsonomy/database/managers/AdminDatabaseManager.java
+++ bibsonomy-database/src/main/java/org/bibsonomy/database/managers/AdminDatabaseManager.java
@@ -1,7 +1,12 @@
 package org.bibsonomy.database.managers;
 
+import java.net.InetAddress;
+
 import org.apache.log4j.Logger;
+import org.bibsonomy.common.enums.InetAddressStatus;
 import org.bibsonomy.database.AbstractDatabaseManager;
+import org.bibsonomy.database.params.AdminParam;
+import org.bibsonomy.database.util.DBSession;
 
 /** Provides functionalities which are typically only available to admins.
  * This might include flagging a user as spammer, setting the status of an 
@@ -24,6 +29,23 @@ public class AdminDatabaseManager extends AbstractDatabaseManager {
 	 */
 	public static AdminDatabaseManager getInstance() {
 		return singleton;
+	}
+	
+	public void addInetAddressStatus(InetAddress address, InetAddressStatus status, DBSession session) {
+		final AdminParam param = new AdminParam();
+		param.setInetAddress(address);
+		param.setInetAddressStatus(status);
+		this.insert("addInetAddressStatus", param, session);
+	}
+
+	public void deleteInetAdressStatus(InetAddress address, DBSession session) {
+		this.delete("deleteInetAddressStatus", address, session);
+	}
+
+	public InetAddressStatus getInetAddressStatus(InetAddress address, DBSession session) {
+		InetAddressStatus status = (InetAddressStatus) this.queryForObject("getInetAddressStatus", address, session);
+		if (status == null) return InetAddressStatus.UNKNOWN;
+		return status;
 	}
 	
 }
