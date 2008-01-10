f0b224626e7ed49dd875ac6536690e295da63eb8
Dominik Benz <benz@cs.uni-kassel.de>
2008-01-10 10:11:02 +0100

    added StatisticDatabaseManager and made it available via the LogicInterface

---
+++ bibsonomy-common/src/main/java/org/bibsonomy/common/enums/StatisticsConstraint.java
@@ -0,0 +1,12 @@
+package org.bibsonomy.common.enums;
+
+/**
+ * defines possible constraints on statistical queries
+ * 
+ * TODO: to be filled when necessary
+ * 
+ * @author Dominik Benz
+ * @version $Id$
+ */
+public enum StatisticsConstraint {
+}
