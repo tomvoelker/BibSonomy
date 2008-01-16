fffd65734539173029eb6c391cb349cc9d906b3d
Dominik Benz <benz@cs.uni-kassel.de>
2008-01-16 13:15:29 +0100

    implemented tag relation / concept retrieval

---
+++ bibsonomy-common/src/main/java/org/bibsonomy/common/enums/ConceptStatus.java
@@ -0,0 +1,13 @@
+package org.bibsonomy.common.enums;
+
+/**
+ * defines possible statusses of tag relations
+ * 
+ * @author Dominik Benz
+ * @version $Id$
+ */
+public enum ConceptStatus {
+	PICKED,
+	UNPICKED,
+	ALL;
+}
