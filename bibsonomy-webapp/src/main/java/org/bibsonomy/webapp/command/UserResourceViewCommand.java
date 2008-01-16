21df0bc6264c0f20c22cbeb7c269bb1c161923bb
Dominik Benz <benz@cs.uni-kassel.de>
2008-01-16 11:24:52 +0100

    added concepts to user page

---
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/UserResourceViewCommand.java
@@ -0,0 +1,27 @@
+package org.bibsonomy.webapp.command;
+
+/**
+ * Bean for User-Sites
+ *
+ * @author  Dominik Benz
+ * @version $Id$
+ */
+public class UserResourceViewCommand extends ResourceViewCommand {
+
+	/** the group whode resources are requested*/
+	private ConceptsCommand concepts = new ConceptsCommand();
+
+	/**
+	 * @return
+	 */
+	public ConceptsCommand getConcepts() {
+		return this.concepts;
+	}
+
+	/**
+	 * @param concepts
+	 */
+	public void setConcepts(ConceptsCommand concepts) {
+		this.concepts = concepts;
+	}	
+}
