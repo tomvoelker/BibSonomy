772cbd832e5e3fe3f889909d0b8cbfddde3f5bb5
daill <devnull@localhost>
2008-02-19 17:40:07 +0100

    added structure to handle FOLKRANK order

--- bibsonomy-common/src/main/java/org/bibsonomy/util/ValidationUtils.java
+++ bibsonomy-common/src/main/java/org/bibsonomy/util/ValidationUtils.java
@@ -53,9 +53,22 @@ public class ValidationUtils {
 	/**
 	 * @param requested argument to check
 	 * @param supported reference argument for comparison
-	 * @return true iff the first argument requested is null or equals the second argument
+	 * @return true if the first argument requested is null or equals the second argument
 	 */
 	public static boolean nullOrEqual(final Object requested, final Object supported) {
 		return ((requested == null) || (requested == supported));
+	}
+	
+	
+	/**
+	 * Build this method to make it possible to have i.e. two different order values ADDED & FOLKRANK
+	 * 
+	 * @param requested
+	 * @param supported1
+	 * @param supported2
+	 * @return true if the first argument requested is null or equals the second or third argument
+	 */
+	public static boolean nullOrEqual(final Object requested, final Object supported1, final Object supported2) {
+		return ((requested == null) || (requested == supported1) || (requested == supported2));
 	}
 }
