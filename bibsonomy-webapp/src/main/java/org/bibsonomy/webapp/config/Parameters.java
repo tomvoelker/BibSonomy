a274c4a03bf18ff0c96ca85ba30fcf54a78b8f11
Dominik Benz <benz@cs.uni-kassel.de>
2008-03-19 16:27:37 +0100

    added configuration parameters

---
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/config/Parameters.java
@@ -0,0 +1,20 @@
+package org.bibsonomy.webapp.config;
+
+/**
+ * Some configuration parameters for the webapp
+ * 
+ * @author dbenz
+ * @version $Id$
+ */
+public class Parameters {
+	
+	public static final Integer NUM_RELATED_USERS = 20;
+	
+	public static final Integer NUM_RELATED_TAGS = 20;
+	
+	public static final Integer NUM_BOOKMARKS_ON_HOMEPAGE = 20;
+	
+	public static final Integer NUM_PUBLICATIONS_ON_HOMEPAGE = 20;
+	
+
+}
