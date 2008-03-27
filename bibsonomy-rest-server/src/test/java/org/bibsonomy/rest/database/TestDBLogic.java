b706edcd7dbe91800d337fbc669e50a438d43e6e
Dominik Benz <benz@cs.uni-kassel.de>
2008-03-27 19:59:24 +0100

    - added statistics query to retrieve nr. of resources for a list of tags - unified query to fetch the groups a user is member of: this now done directly after authentication against the logicInterface, instead of several times later

--- bibsonomy-rest-server/src/test/java/org/bibsonomy/rest/database/TestDBLogic.java
+++ bibsonomy-rest-server/src/test/java/org/bibsonomy/rest/database/TestDBLogic.java
@@ -746,7 +746,7 @@ public class TestDBLogic implements LogicInterface {
 		return null;
 	}
 	
-	public int getStatistics(Class<? extends Resource> resourceType, GroupingEntity grouping, String groupingName, StatisticsConstraint constraint, String search) {
+	public int getStatistics(Class<? extends Resource> resourceType, GroupingEntity grouping, String groupingName, StatisticsConstraint constraint, String search, List<String> tags) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
