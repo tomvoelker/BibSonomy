d338ff17ac35157115a047d3570e9b3ff917f753
cschenk <devnull@localhost>
2008-04-09 21:38:19 +0200

    Fixed warnings

--- bibsonomy-database/src/main/java/org/bibsonomy/database/DBLogicUserInterfaceFactory.java
+++ bibsonomy-database/src/main/java/org/bibsonomy/database/DBLogicUserInterfaceFactory.java
@@ -14,49 +14,58 @@ import org.bibsonomy.model.util.UserUtils;
  * This class produces DBLogic instances with user authentication
  * 
  * @author Jens Illig
+ * @version $Id$
  */
 public class DBLogicUserInterfaceFactory implements LogicInterfaceFactory {
+
 	protected final UserDatabaseManager userDBManager = UserDatabaseManager.getInstance();
 	protected final GeneralDatabaseManager generalDB = GeneralDatabaseManager.getInstance();
-	
+
 	private DBSessionFactory dbSessionFactory;
-	
-	/* (non-Javadoc)
-	 * @see org.bibsonomy.model.logic.LogicInterfaceFactory#getLogicAccess(java.lang.String, java.lang.String)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.bibsonomy.model.logic.LogicInterfaceFactory#getLogicAccess(java.lang.String,
+	 *      java.lang.String)
 	 */
 	public LogicInterface getLogicAccess(final String loginName, final String password) {
 		if (loginName != null) {
-			final User loggedInUser = getLoggedInUser(loginName, password); 
+			final User loggedInUser = getLoggedInUser(loginName, password);
 			if (loggedInUser.getName() != null) {
 				return new DBLogic(loggedInUser, dbSessionFactory);
 			}
 			throw new ValidationException("Wrong Authentication.");
-		}		
-		return new DBLogic(new User(), dbSessionFactory);  // guest access
+		}
+		// guest access
+		return new DBLogic(new User(), dbSessionFactory);
 	}
 
-	/** Returns a user object containing the details of the user, if he is logged in
-	 * correctly. If not, the returned user object is empty and it's user name NULL. 
-	 *  
+	/**
+	 * Returns a user object containing the details of the user, if he is logged
+	 * in correctly. If not, the returned user object is empty and it's user
+	 * name NULL.
+	 * 
 	 * @param loginName
 	 * @param password
-	 * @return
+	 * @return user object with details of the logged in user
 	 */
-	protected User getLoggedInUser(String loginName, String password) {
+	protected User getLoggedInUser(final String loginName, final String password) {
 		final DBSession session = openSession();
 		try {
-			User loggedInUser = userDBManager.validateUserUserAccess(loginName, password, session);
+			final User loggedInUser = this.userDBManager.validateUserUserAccess(loginName, password, session);
 			if (loggedInUser.getName() != null) {
-				UserUtils.setGroupsByGroupIDs(loggedInUser, generalDB.getGroupIdsForUser(loggedInUser.getName(), session));
+				UserUtils.setGroupsByGroupIDs(loggedInUser, this.generalDB.getGroupIdsForUser(loggedInUser.getName(), session));
 			}
 			return loggedInUser;
 		} finally {
 			session.close();
 		}
 	}
-	
+
 	/**
-	 * @param dbSessionFactory the {@link DBSessionFactory} to use
+	 * @param dbSessionFactory
+	 *            the {@link DBSessionFactory} to use
 	 */
 	public void setDbSessionFactory(DBSessionFactory dbSessionFactory) {
 		this.dbSessionFactory = dbSessionFactory;
--- bibsonomy-database/src/main/java/org/bibsonomy/database/managers/PermissionDatabaseManager.java
+++ bibsonomy-database/src/main/java/org/bibsonomy/database/managers/PermissionDatabaseManager.java
@@ -12,6 +12,7 @@ import org.bibsonomy.model.Group;
 import org.bibsonomy.model.Post;
 import org.bibsonomy.model.Resource;
 import org.bibsonomy.model.User;
+import static org.bibsonomy.util.ValidationUtils.present;
 
 /**
  * Database Manager for permissions
@@ -23,7 +24,7 @@ public class PermissionDatabaseManager extends AbstractDatabaseManager {
 
 	private final static PermissionDatabaseManager singleton = new PermissionDatabaseManager();
 	private final GroupDatabaseManager groupDb;
-	
+
 	private PermissionDatabaseManager() {
 		this.groupDb = GroupDatabaseManager.getInstance();
 	}
@@ -55,20 +56,20 @@ public class PermissionDatabaseManager extends AbstractDatabaseManager {
 	 * @param loginUser
 	 */
 	public void ensureWriteAccess(final Post<?> post, final User loginUser) {
-		// delegate write access check		
+		// delegate write access check
 		ensureWriteAccess(loginUser, post.getUser().getName());
 	}
-	
+
 	/**
 	 * @param loginUser
-	 * @param userName 
+	 * @param userName
 	 */
-	public void ensureWriteAccess(final User loginUser, final String userName){
-		if (loginUser.getName() == null || !loginUser.getName().toLowerCase().equals(userName.toLowerCase())){
+	public void ensureWriteAccess(final User loginUser, final String userName) {
+		if (loginUser.getName() == null || !loginUser.getName().toLowerCase().equals(userName.toLowerCase())) {
 			throw new ValidationException("You are not authorized to perform the requested operation.");
 		}
 	}
-	
+
 	/**
 	 * @param document
 	 * @param loginUser
@@ -77,45 +78,48 @@ public class PermissionDatabaseManager extends AbstractDatabaseManager {
 		// delegate write access check
 		ensureWriteAccess(loginUser, document.getUserName());
 	}
-	
-	/** This method checks, whether the user is allowed to access the posts documents.
-	 * The user is allowed to access the documents, 
+
+	/**
+	 * This method checks, whether the user is allowed to access the posts
+	 * documents. The user is allowed to access the documents,
 	 * 
 	 * <ul>
-	 * <li>if the post is public and the posts 
-	 * user is together with the user in a group, which allows to share documents, or 
-	 * <li>if the post is viewable for a specific group, in which both users are and 
-	 * which allows to share documents.
-	 * </ul>  
+	 * <li>if the post is public and the posts user is together with the user
+	 * in a group, which allows to share documents, or
+	 * <li>if the post is viewable for a specific group, in which both users
+	 * are and which allows to share documents.
+	 * </ul>
 	 * 
 	 * TODO: eventually, we don't want to have the post as parameter, but only
 	 * its groups?
 	 * 
-	 * @param userName - the name of the user which wants to access the posts documents.
-	 * @param post - the post which contains the documents the user wants to access.
-	 * @param session - a DBSession.
-	 * @return <code>true</code> if the user is allowed to access the documents of the post.
+	 * @param userName -
+	 *            the name of the user which wants to access the posts
+	 *            documents.
+	 * @param post -
+	 *            the post which contains the documents the user wants to
+	 *            access.
+	 * @param session -
+	 *            a DBSession.
+	 * @return <code>true</code> if the user is allowed to access the
+	 *         documents of the post.
 	 */
 	public boolean isAllowedToAccessPostsDocuments(final String userName, final Post<? extends Resource> post, final DBSession session) {
 		final String postUserName = post.getUser().getName();
 		final List<Group> postGroups = post.getGroups();
-		/*
-		 * Get the groups in which both users are.
-		 */
-		final List<Group> commonGroups = groupDb.getCommonGroups(userName, postUserName, session);
-		/*
-		 * Construct the public group. TODO: this should better be done in a GroupFactory!
-		 */
+
+		// Get the groups in which both users are.
+		final List<Group> commonGroups = this.groupDb.getCommonGroups(userName, postUserName, session);
+
+		// Construct the public group.
+		// TODO: this should better be done in a GroupFactory!
 		final Group publicGroup = new Group();
 		publicGroup.setGroupId(GroupID.PUBLIC.getId());
-		/*
-		 * Find a common group of both users, which allows to share documents. 
-		 */
-		for (final Group group: commonGroups) {
+
+		// Find a common group of both users, which allows to share documents.
+		for (final Group group : commonGroups) {
 			if (group.isSharedDocuments()) {
-				/*
-				 * both users are in a group which allows to share documents
-				 */
+				// both users are in a group which allows to share documents
 				if (postGroups.contains(publicGroup) || postGroups.contains(group)) {
 					return true;
 				}
@@ -123,24 +127,25 @@ public class PermissionDatabaseManager extends AbstractDatabaseManager {
 		}
 		return false;
 	}
-	
-	/** Ensures that the user is an admin.
+
+	/**
+	 * Ensures that the user is an admin.
+	 * 
 	 * @param loginUser
 	 */
 	public void ensureAdminAccess(final User loginUser) {
-		if (loginUser.getName() == null || !loginUser.getRole().equals(Role.ADMIN)) { 
+		if (present(loginUser.getName()) == false || loginUser.getRole().equals(Role.ADMIN) == false) {
 			throw new ValidationException("You are not authorized to perform the requested operation.");
 		}
 	}
-	
+
 	/**
 	 * Check maximum number of allowed tags per request
 	 * 
 	 * @param tags
 	 * @return true if maximum size is exceeded, false otherwise
 	 */
-	public Boolean exceedsMaxmimumSize (List<String> tags) {
+	public boolean exceedsMaxmimumSize(final List<String> tags) {
 		return tags != null && tags.size() >= 10;
 	}
-
 }
