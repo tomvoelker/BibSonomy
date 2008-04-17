da7b53b1c877f953960a641077d1f79fbcc7e2e4
ss05sstuetzer <devnull@localhost>
2008-04-17 18:00:16 +0200

    add sharing of (PDF) documents within groups

---
+++ bibsonomy-common/src/main/java/org/bibsonomy/common/enums/FilterEntity.java
@@ -0,0 +1,22 @@
+package org.bibsonomy.common.enums;
+
+/**
+ * Defines possible filter entities
+ * 
+ * @author Stefan Stützer
+ * @version $Id$
+ */
+public enum FilterEntity {
+
+	/**
+	 * Use this when you want to retrieve also PDF documents for
+	 * your BibTeX resources   
+	 */
+	PDF,
+	
+	/**
+	 * Use this when you ONLY want to retrieve resources with a PDF
+	 * file attached
+	 */
+	JUST_PDF;	
+}
