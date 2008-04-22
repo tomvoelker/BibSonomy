d4e37a56cf7327883db61f786beb8f8e44224f71
BibSonomy <webmaster@bibsonomy.org>
2008-04-22 10:56:14 +0200

    fixed conflict

--- bibsonomy-webapp/src/build/batch_scripts/check_long_queries.pl
+++ bibsonomy-webapp/src/build/batch_scripts/check_long_queries.pl
@@ -9,29 +9,31 @@ my $MAXTIME = 30;
 
 my $msg = "";
 
-my $user = "bibsonomy";
+my $user = "bibsonomy_rdonly";
 my $password = $ENV{'DB_PASS'}; 
 my $database = shift @ARGV;
 
-my $dbh = DBI->connect("DBI:mysql:database=$database;host=localhost:6033;mysql_socket=/home/bibsonomy/mysql-var/mysql.sock", $user, $password, {RaiseError => 1, AutoCommit => 0, "mysql_enable_utf8" => 1});
+my $dbh = DBI->connect("DBI:mysql:database=$database;host=localhost:6033;mysql_socket=/var/mysql/run/mysqld.sock", $user, $password, {RaiseError => 1, AutoCommit => 0, "mysql_enable_utf8" => 1});
 
-my $stm = $dbh->prepare("show processlist");
+my $stm = $dbh->prepare("show full processlist");
 
 $stm->execute();
 
 
-my @tobekilled = ();
+my %tobekilled = ();
 while(my $row=$stm->fetchrow_hashref) {
 	if (defined $row->{"State"} && ("statistics" eq $row->{"State"}) && ($row->{"Time"} > $MAXTIME)) {
-		push @tobekilled, $row->{"Id"};
+		# push @tobekilled, $row->{"Id"};
+		$tobekilled{$row->{"Id"}} = $row->{"Info"};
 	}
 }
 
-if (@tobekilled) {
-	$msg .= "Versuche Jobs zu killen: " . (join ",", @tobekilled) . "\n";
-	for my $id (@tobekilled) {
+if (%tobekilled) {
+	$msg .= "Versuche Jobs zu killen: " . (join ",", keys(%tobekilled)) . "\n";
+	while ((my $id, my $query) = each(%tobekilled)) {
 		$stm = $dbh->prepare("kill $id");
 		$msg .= "kill $id\n";
+	 	$msg .= "query: $query\n\n";
 		$stm->execute;
 	}
 }
