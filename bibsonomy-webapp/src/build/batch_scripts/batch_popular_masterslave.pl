865d0a06ccbd33a2b194471c1b319243b7ba7f29
Robert Jäschke <jaeschke@l3s.de>
2008-04-22 17:49:54 +0200

    - several content_types for tags - several days for each table

--- bibsonomy-webapp/src/build/batch_scripts/batch_popular_masterslave.pl
+++ bibsonomy-webapp/src/build/batch_scripts/batch_popular_masterslave.pl
@@ -5,12 +5,16 @@ use strict;
 use English;
 
 ##################################################################
-#
+# This script fills the popular tables for bibtexs, bookmarks, and
+# tags. For each of the given number of days the top items are
+# extracted.
 #
 # Changes:
 #   2008-04-22 (rja)
 #   - created master/slave version
 #   - changed from "last N rows" to "last N days"
+#   - added possibility to compute popularity for different
+#     number of days
 #
 ##################################################################
 
@@ -29,12 +33,16 @@ if (am_i_running($ENV{'TMP'}."/$PROGRAM_NAME.pid")) {
 ##################################################################
 # configuration
 ##################################################################
-my $last_bookmark_days = 20;
-my $last_bibtex_days   = 20;
-my $last_tag_days      = 20;
+# these are arrays - enter as many days as blocks you like
+my @last_bookmark_days = (10); 
+my @last_bibtex_days   = (10);
+my @last_tag_days      = (10);
+# max number of posts for each day block
 my $max_bookmarks      = 100;
 my $max_bibtexs        = 100;
 my $max_tags           = 100;
+
+my $DEBUG = 1; # 1 = on, 0 = off
 
 # run configuration
 # master
@@ -88,7 +96,7 @@ SELECT book_url_hash,count(book_url_hash) AS ctr
   FROM (
     SELECT book_url_hash, user_name FROM bookmark 
       WHERE bookmark.group = 0
-        AND bookmark.date > SUBDATE(CURRENT_TIMESTAMP, INTERVAL $last_bookmark_days DAY)
+        AND bookmark.date > SUBDATE(CURRENT_TIMESTAMP, INTERVAL ? DAY)
       GROUP BY book_url_hash, user_name
     ) AS b 
   GROUP BY book_url_hash
@@ -104,7 +112,7 @@ my $fz_bookmark = ""; foreach (split (",", $rows_bookmark)) {$fz_bookmark .= "?,
 my $stm_get_first_bookmark = $slave->prepare("SELECT $rows_bookmark FROM bookmark WHERE book_url_hash = ? AND bookmark.group = 0 ORDER BY date LIMIT 1");
 
 # insert posts in popular table
-my $stm_insert_bookmark = $master->prepare("INSERT INTO temp_bookmark ($rows_bookmark,book_url_ctr,rank) VALUES ($fz_bookmark?,?)");
+my $stm_insert_bookmark = $master->prepare("INSERT INTO temp_bookmark ($rows_bookmark,book_url_ctr,rank,popular_days) VALUES ($fz_bookmark?,?,?)");
 
 # delete old values from temp table
 my $stm_delete_bookmark = $master->prepare("DELETE FROM temp_bookmark");
@@ -113,24 +121,28 @@ my $stm_delete_bookmark = $master->prepare("DELETE FROM temp_bookmark");
 # execute statements (bookmark)
 ##########################################
 
-# get TOP bookmarks
-$stm_select_bookmark->execute();
-$slave->commit;
-
 # clean table
 $stm_delete_bookmark->execute();
 
-# get first occurence of every bookmark
-my $rank = 0;
-while (my @id = $stm_select_bookmark->fetchrow_array ) {
-  $rank++;
-  #print "U: $rank ";
-  $stm_get_first_bookmark->execute($id[0]);
-  #print "e ";
-  while (my @row = $stm_get_first_bookmark->fetchrow_array) {
-     $stm_insert_bookmark->execute($row[ 0],$row[ 1],$row[ 2],$row[ 3],$row[ 4],$row[5],$row[6],$id[1],$rank);
-     #print "i\n";
-  }
+foreach my $days (@last_bookmark_days) {
+
+    debug("bookmark, $days days: ");
+
+    # get TOP bookmarks
+    $stm_select_bookmark->execute($days);
+    $slave->commit;
+
+    # get first occurence of every bookmark
+    my $rank = 0;
+    while (my @id = $stm_select_bookmark->fetchrow_array ) {
+	$rank++;
+	debug(".");
+	$stm_get_first_bookmark->execute($id[0]);
+	while (my @row = $stm_get_first_bookmark->fetchrow_array) {
+	    $stm_insert_bookmark->execute($row[ 0],$row[ 1],$row[ 2],$row[ 3],$row[ 4],$row[5],$row[6],$id[1],$rank, $days);
+	}
+    }
+    debug("\n");
 }
 $master->commit;
 $stm_select_bookmark->finish();
@@ -148,7 +160,7 @@ SELECT simhash1,count(simhash1) AS ctr
   FROM (
     SELECT simhash1, user_name FROM bibtex 
       WHERE bibtex.group = 0 
-        AND bibtex.date > SUBDATE(CURRENT_TIMESTAMP, INTERVAL $last_bibtex_days DAY)
+        AND bibtex.date > SUBDATE(CURRENT_TIMESTAMP, INTERVAL ? DAY)
       GROUP BY simhash1, user_name
     ) AS b 
     GROUP BY simhash1 
@@ -161,10 +173,10 @@ my $rows_bibtex   = "content_id,journal,volume,chapter,edition,month,day,bookTit
 my $fz_bibtex   = ""; foreach (split (",", $rows_bibtex))   {$fz_bibtex   .= "?,";}
 
 # get the first post of each resource
-my $stm_get_first_bibtex   = $slave->prepare("SELECT $rows_bibtex   FROM bibtex   WHERE simhash1 = ?      AND bibtex.group = 0   ORDER BY date LIMIT 1");
+my $stm_get_first_bibtex   = $slave->prepare("SELECT $rows_bibtex FROM bibtex   WHERE simhash1 = ? AND bibtex.group = 0 ORDER BY date LIMIT 1");
 
 # statements to insert posts in popular table
-my $stm_insert_bibtex   = $master->prepare("INSERT INTO temp_bibtex   ($rows_bibtex,ctr,rank)            VALUES ($fz_bibtex?,?)");
+my $stm_insert_bibtex   = $master->prepare("INSERT INTO temp_bibtex ($rows_bibtex,ctr,rank,popular_days) VALUES ($fz_bibtex?,?,?)");
 
 # delete old values from temp tables
 my $stm_delete_bibtex   = $master->prepare("DELETE FROM temp_bibtex");
@@ -174,33 +186,37 @@ my $stm_delete_bibtex   = $master->prepare("DELETE FROM temp_bibtex");
 # execute statements (BibTeX)
 ##########################################
 
-# get TOP posts
-$stm_select_bibtex->execute();
-$slave->commit;
 
 # clean table
 $stm_delete_bibtex->execute();
 
-# get first occurence of every bookmark
-$rank = 0;
-while (my @id = $stm_select_bibtex->fetchrow_array ) {
-  $rank++;
-  #print "B: $rank ";
-  $stm_get_first_bibtex->execute($id[0]);
-  #print "e ";
-  while (my @row = $stm_get_first_bibtex->fetchrow_array) {
-     $stm_insert_bibtex->execute($row[ 0],$row[ 1],$row[ 2],$row[ 3],$row[ 4],
-                                 $row[ 5],$row[ 6],$row[ 7],$row[ 8],$row[ 9],
-                                 $row[10],$row[11],$row[12],$row[13],$row[14],
-                                 $row[15],$row[16],$row[17],$row[18],$row[19],
-                                 $row[20],$row[21],$row[22],$row[23],$row[24],
-                                 $row[25],$row[26],$row[27],$row[28],$row[29],
-                                 $row[30],$row[31],$row[32],$row[33],$row[34],
-                                 $row[35],$row[36],$row[37],$row[38],
-                                 int($id[1]),int($rank));
-     #print "i\n";
-     #print "i\n";
-  }
+foreach my $days (@last_bibtex_days) {
+
+    debug("bibtex, $days days: ");
+
+    # get TOP posts
+    $stm_select_bibtex->execute($days);
+    $slave->commit;
+
+    # get first occurence of every bookmark
+    my $rank = 0;
+    while (my @id = $stm_select_bibtex->fetchrow_array ) {
+	$rank++;
+	debug(".");
+	$stm_get_first_bibtex->execute($id[0]);
+	while (my @row = $stm_get_first_bibtex->fetchrow_array) {
+	    $stm_insert_bibtex->execute($row[ 0],$row[ 1],$row[ 2],$row[ 3],$row[ 4],
+					$row[ 5],$row[ 6],$row[ 7],$row[ 8],$row[ 9],
+					$row[10],$row[11],$row[12],$row[13],$row[14],
+					$row[15],$row[16],$row[17],$row[18],$row[19],
+					$row[20],$row[21],$row[22],$row[23],$row[24],
+					$row[25],$row[26],$row[27],$row[28],$row[29],
+					$row[30],$row[31],$row[32],$row[33],$row[34],
+					$row[35],$row[36],$row[37],$row[38],
+					int($id[1]),int($rank), $days);
+	}
+    }
+    debug ("\n");
 }
 $master->commit;
 $stm_select_bibtex->finish();
@@ -209,47 +225,74 @@ $stm_get_first_bibtex->finish();
 
 
 
-
 ##################################################################
 # prepare statements (tags)
 ##################################################################
 
-# get last tag rows, order them by ctr
-my $stm_select_tags = $slave->prepare("
-SELECT tag_lower, COUNT(tag_lower) AS ctr 
-  FROM (
-    SELECT tag_lower, user_name FROM tas 
-      WHERE tas.group = 0
-        AND tas.date > SUBDATE(CURRENT_TIMESTAMP, INTERVAL $last_tag_days DAY)
-      GROUP BY tag_lower, user_name
-    ) AS b
-  GROUP BY tag_lower
-  ORDER BY ctr DESC
-  LIMIT $max_tags");
-
 # insert tags
-my $stm_insert_tags     = $master->prepare("INSERT INTO popular_tags  (tag_lower, tag_ctr, content_type) VALUES (?,?,?)");
+my $stm_insert_tags     = $master->prepare("INSERT INTO popular_tags  (tag_lower, tag_ctr, content_type, popular_days) VALUES (?,?,?,?)");
 
 # delete old values from temp tables
 my $stm_delete_tags     = $master->prepare("DELETE FROM popular_tags");
 
 ##########################################
 # execute statements (tags)
+# for each content type we calculate the
+# most popular tags
 ##########################################
-
-# get TOP bookmarks
-$stm_select_tags->execute();
-$slave->commit;
 
 # clean table
 $stm_delete_tags->execute();
 
-# get tags
-while (my @row = $stm_select_tags->fetchrow_array ) {
-    $stm_insert_tags->execute($row[0],$row[1], 0);
+# all, bookmark, bibtex
+my @content_types = (0,1,2);
+
+# loop over different content types
+foreach my $content_type (@content_types) {
+    debug("tags, content_type = $content_type\n");
+    my $condition = "";
+    if ($content_type == 0) {
+	# all content types
+	$condition = "";
+    } else {
+	# bookmark, bibtex, ...
+	$condition = " AND content_type = $content_type ";
+    }
+
+    # get last tag rows, order them by ctr
+    my $stm_select_tags = $slave->prepare("
+    SELECT tag_lower, COUNT(tag_lower) AS ctr 
+      FROM (
+        SELECT tag_lower, user_name FROM tas 
+          WHERE tas.group = 0 " 
+          . $condition .
+          " AND tas.date > SUBDATE(CURRENT_TIMESTAMP, INTERVAL ? DAY)
+          GROUP BY tag_lower, user_name
+        ) AS b
+      GROUP BY tag_lower
+      ORDER BY ctr DESC
+      LIMIT $max_tags");
+
+    # insert rows
+    foreach my $days (@last_tag_days) {
+	debug ("$days days: ");
+	
+	# get TOP bookmarks
+	$stm_select_tags->execute($days);
+	$slave->commit;
+	
+	# get tags
+	while (my @row = $stm_select_tags->fetchrow_array ) {
+	    debug (".");
+	    $stm_insert_tags->execute($row[0],$row[1], $content_type, $days);
+	}
+    }
+    
+    debug ("\n");
+
+    $master->commit;
+    $stm_select_tags->finish();
 }
-$master->commit;
-$stm_select_tags->finish();
 
 
 
@@ -261,7 +304,6 @@ $stm_select_tags->finish();
 # disconnect database
 $slave->disconnect();
 $master->disconnect();
-
 
 
 
@@ -293,4 +335,12 @@ sub am_i_running {
   print FILE $$;
   close (FILE);
   return 0;
+}
+
+# logs statements if debugging is enabled
+sub debug {
+    my $msg = shift;
+    if ($DEBUG) {
+	print STDERR $msg;
+    }
 }
