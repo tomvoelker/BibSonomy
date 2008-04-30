95934309e850fa13b16bb0dadfc5e22ca73e51b9
Dominik Benz <benz@cs.uni-kassel.de>
2008-04-30 18:34:59 +0200

    added infrastructure for similar tags

--- bibsonomy-rest-client/src/main/java/org/bibsonomy/rest/client/RestLogic.java
+++ bibsonomy-rest-client/src/main/java/org/bibsonomy/rest/client/RestLogic.java
@@ -12,6 +12,7 @@ import org.bibsonomy.common.enums.GroupingEntity;
 import org.bibsonomy.common.enums.InetAddressStatus;
 import org.bibsonomy.common.enums.SpamStatus;
 import org.bibsonomy.common.enums.StatisticsConstraint;
+import org.bibsonomy.common.enums.TagRelationType;
 import org.bibsonomy.model.Document;
 import org.bibsonomy.model.Group;
 import org.bibsonomy.model.Post;
@@ -118,7 +119,7 @@ public class RestLogic implements LogicInterface {
 		return execute(new GetTagDetailsQuery(tagName));
 	}
 
-	public List<Tag> getTags(Class<? extends Resource> resourceType, GroupingEntity grouping, String groupingName, String regex, List<String> tags, String hash, Order order, int start, int end, String search) {
+	public List<Tag> getTags(Class<? extends Resource> resourceType, GroupingEntity grouping, String groupingName, String regex, List<String> tags, String hash, Order order, int start, int end, String search, TagRelationType relation) {
 		GetTagsQuery query = new GetTagsQuery(start, end);
 		query.setGrouping(grouping, groupingName);
 		query.setFilter(regex);
