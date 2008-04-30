305dab4c567d6c53754ac8a27d1fbb47836fd7f4
Dominik Benz <benz@cs.uni-kassel.de>
2008-04-30 18:34:13 +0200

    added infrastructure for similar tags

---
+++ bibsonomy-common/src/main/java/org/bibsonomy/common/enums/TagRelationType.java
@@ -0,0 +1,15 @@
+package org.bibsonomy.common.enums;
+
+/**
+ * Defines types of relatedness between tags
+ * 
+ * @author Dominik Benz
+
+ * @version $Id$
+ */
+public enum TagRelationType {
+	/** tag co-occurrence */
+	COOC,
+	/** cosine similarity*/
+	COSINE;
+}
