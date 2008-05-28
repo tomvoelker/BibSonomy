c7132c26b28e07352e07f06cca2a915ec0a130db
Robert Jäschke <jaeschke@l3s.de>
2008-05-28 12:06:27 +0200

    added EasyChair importer

---
+++ bibsonomy-dl-importer/src/main/java/org/bibsonomy/importer/easychair/Importer.java
@@ -0,0 +1,114 @@
+package org.bibsonomy.importer.easychair;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+import java.util.Properties;
+
+import org.bibsonomy.importer.filter.PostFilterChain;
+import org.bibsonomy.model.BibTex;
+import org.bibsonomy.model.Post;
+import org.bibsonomy.model.Tag;
+import org.xml.sax.SAXException;
+
+/** 
+ * Converts an EasyChair XML file into BibTeX.
+ * 
+ * @author rja
+ * @version $Id$
+ */
+public class Importer {
+
+	/**
+	 * @param args
+	 * @throws SAXException 
+	 * @throws IOException 
+	 * @throws ClassNotFoundException 
+	 * @throws NoSuchMethodException 
+	 * @throws InvocationTargetException 
+	 * @throws IllegalAccessException 
+	 * @throws InstantiationException 
+	 * @throws SecurityException 
+	 * @throws IllegalArgumentException 
+	 */
+	public static void main(String[] args) throws SAXException, IOException, IllegalArgumentException, SecurityException, InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException, ClassNotFoundException {
+
+		/*
+		 * Load properties for configuring the filters. 
+		 */
+		final Properties prop = new Properties();
+		prop.load(new InputStreamReader(Importer.class.getClassLoader().getResourceAsStream("easychair.properties"), "UTF-8"));
+
+		/*
+		 * The reader which will read and parse the XML.
+		 */
+		final XMLPostListReader reader = new XMLPostListReader(new BufferedReader(new InputStreamReader(new FileInputStream(args[0]), "UTF-8")));
+
+		/*
+		 * The filter chain to preprocess the posts.
+		 */
+		final PostFilterChain filter = new PostFilterChain(prop);
+		
+		/*
+		 * Get list of BibTex posts.
+		 */
+		final List<Post<BibTex>> bibTeXListFromXML = reader.readPostList();
+		
+		/*
+		 * filter posts and write them to into a file.
+		 */
+		
+		final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(args[1]), "UTF-8"));
+		
+		for (final Post<BibTex> post:bibTeXListFromXML) {
+			filter.filterPost(post);
+			writer.write(toString(post) + "\n");
+		}
+		
+		writer.close();
+		
+
+	}
+
+	
+	/** Converts a BibTeX posts into BibTeX. Does not include all BibTeX fields! 
+	 * 
+	 * 
+	 * @param post
+	 * @return
+	 */
+	public static String toString(final Post<BibTex> post) {
+		final BibTex resource = post.getResource();
+		
+		return "@inproceedings{" + resource.getBibtexKey() + ",\n" + 
+		   	   "  title     = \"" + resource.getTitle() + "\",\n" +
+		       "  authors   = \"" + resource.getAuthor() + "\",\n" +
+		       "  url       = \"" + resource.getUrl() + "\",\n" +
+		       "  year      = \"" + resource.getYear() + "\",\n" +
+		       "  month     = \"" + resource.getMonth() + "\",\n" +
+		       "  booktitle = \"" + resource.getBooktitle() + "\",\n" +
+		       "  publisher = \"" + resource.getPublisher() + "\",\n" +
+		       "  address   = \"" + resource.getAddress() + "\",\n" +
+		       "  series    = \"" + resource.getSeries() + "\",\n" +
+		       "  editor    = \"" + resource.getEditor() + "\",\n" +
+		       "  keywords  = \"" + toString(post.getTags()) + "\"\n" +
+		       "}\n";
+		
+	}
+	
+	public static String toString(final List<Tag> tags) {
+		final StringBuffer buf = new StringBuffer();
+		for (final Tag tag: tags) {
+			buf.append(tag.getName() + " ");
+		}
+		
+		return buf.toString().trim();
+	}
+
+}
---
+++ bibsonomy-dl-importer/src/main/java/org/bibsonomy/importer/easychair/XMLHandler.java
@@ -0,0 +1,84 @@
+package org.bibsonomy.importer.easychair;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.bibsonomy.model.BibTex;
+import org.bibsonomy.model.Post;
+import org.xml.sax.Attributes;
+import org.xml.sax.helpers.DefaultHandler;
+
+
+/**
+ * Callback handler for the SAX parser.
+ * 
+ * @author rja
+ * @version $Id$
+ */
+public class XMLHandler extends DefaultHandler {
+	private StringBuffer buf = new StringBuffer();
+	private List<Post<BibTex>> list;
+
+	private Post<BibTex> post;
+	private StringBuffer authors;
+
+	private String authorFirstName;
+	private String authorLastName;
+
+	public void startDocument() {
+		list = new LinkedList<Post<BibTex>>();
+	}
+
+	public void endDocument() {
+	}
+
+	public void startElement (final String uri, final String name, final String qName, final Attributes atts) {
+		if ("submission".equals(qName)) {
+			post = new Post<BibTex>();
+			post.setResource(new BibTex());
+			/*
+			 * set the EasyChair submission number as content id
+			 */
+			post.setContentId(Integer.parseInt(atts.getValue("number")));
+		} else if ("authors".equals(qName)) {
+			authors = new StringBuffer();
+		}
+		buf = new StringBuffer();
+	}
+
+	/** Collect characters.
+	 * 
+	 * @see org.xml.sax.helpers.DefaultHandler#characters(char[], int, int)
+	 */
+	public void characters (final char ch[], final int start, final int length) {
+		buf.append(ch, start, length);
+	}
+
+	public void endElement (final String uri, final String name, final String qName) {
+		if ("submission".equals(qName)) {
+			list.add(post);
+		} else if ("authors".equals(qName)) {
+			post.getResource().setAuthor(authors.toString());
+		} else if ("author".equals(qName)) {
+			if (authors.toString().trim().equals("")) {
+				authors.append(authorFirstName + " " + authorLastName);
+			} else {
+				authors.append(" and " + authorFirstName + " " + authorLastName);
+			}
+		} else if ("first_name".equals(qName)) {
+			authorFirstName = buf.toString();
+		} else if ("last_name".equals(qName)) {
+			authorLastName = buf.toString();
+		} else if ("title".equals(qName)) {
+			post.getResource().setTitle(buf.toString());
+		} else if ("abstract".equals(qName)) {
+			post.getResource().setBibtexAbstract(buf.toString());
+		} else if ("keyword".equals(qName)) {
+			post.addTag(buf.toString());
+		}
+	}
+
+	public List<Post<BibTex>> getList() {
+		return this.list;
+	}
+}
