9f20e6c070f55087cd1b4d983f49e6d9e6190be9
BibSonomy <webmaster@bibsonomy.org>
2008-06-03 15:33:45 +0200

    removed unnecessary log output

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/util/spring/controller/MinimalisticControllerSpringWrapper.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/util/spring/controller/MinimalisticControllerSpringWrapper.java
@@ -108,7 +108,7 @@ public class MinimalisticControllerSpringWrapper<T extends BaseCommand> extends 
 		catch (MalformedURLSchemeException malformed) {
 			response.setStatus(HttpStatus.SC_NOT_FOUND);
 			command.setError(malformed.getMessage());
-			LOGGER.error(malformed);
+			// LOGGER.error(malformed);
 			view = Views.ERROR;
 		}
 		catch (ValidationException notValid) {
