9109b4d35a889c7ecb9e37b1197a344c8d9fa173
Robert Jäschke <jaeschke@l3s.de>
2008-06-09 13:54:39 +0200

    - introduced RequestWrapperContext which replaces attributes automatically set from the request attributes   access to loginUser, isLoggedIn, ckey, etc. is now possible via   command.context.*

---
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/util/RequestWrapperContext.java
@@ -0,0 +1,95 @@
+package org.bibsonomy.webapp.util;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.bibsonomy.model.User;
+import org.tuckey.web.filters.urlrewrite.UrlRewriter;
+
+import filters.ActionValidationFilter;
+import filters.InitUserFilter;
+
+/**
+ * <p>
+ * Provides access to loginUser, format, ckey, validCkey, etc. by extracting
+ * this attributes from the request.
+ * <br/>
+ * Basically, a wrapper/proxy for the request. 
+ * </p>
+ * 
+ * <p>Using this class is only a <em>workaround</em> to transfer request attributes
+ * from the filters {@link UrlRewriter}, {@link ActionValidationFilter}, and
+ * {@link InitUserFilter} to the command and later to the Views.
+ * </p>
+ * 
+ * @author rja
+ * @version $Id$
+ */
+public class RequestWrapperContext {
+
+	private HttpServletRequest request;
+
+	public HttpServletRequest getRequest() {
+		return this.request;
+	}
+
+	public void setRequest(HttpServletRequest request) {
+		this.request = request;
+	}
+	
+	/** Extract format from request. If not given, defaults to <code>html</code>.
+	 * 
+	 * @return The requested format.
+	 */
+	public String getFormat() {
+		final String format = (String) getRequestAttribute("format");
+		if (format != null && !format.trim().equals("")) return format;
+		return "html";
+	}
+	
+	/** Returns the logged in user.
+	 *  
+	 * @return An instance of the logged in user.
+	 */
+	public User getLoginUser() {
+		return (User) getRequestAttribute(InitUserFilter.REQ_ATTRIB_LOGIN_USER); 
+	}
+	
+	/**
+	 * helper function to ease checking if a logged in user exists
+	 * @return true if a user is logged in, false otherwise
+	 */
+	public boolean isUserLoggedIn() {
+		return this.getLoginUser().getName() != null;
+	}
+	
+	/**
+	 * wrapper for function userLoggedIn to enable access from JSPs
+	 * @see #isUserLoggedIn()
+	 * @return true if a user is logged in, false otherwise 
+	 */
+	public boolean getUserLoggedIn() {
+		return this.isUserLoggedIn();
+	}
+	
+	/** Delivers a new ckey to be used in forms.
+	 * 
+	 * @return A ckey. 
+	 */
+	public String getCkey() {
+		return (String) getRequestAttribute(ActionValidationFilter.REQUEST_ATTRIB_CREDENTIAL);
+	}
+	
+	/** Returns <code>true</code> only, when the user request contained a valid ckey.
+	 * 
+	 * @return <code>true</code>, when the request contained a valid ckey.
+	 */
+	public boolean isValidCkey() {
+		return (Boolean) getRequestAttribute(ActionValidationFilter.REQUEST_ATTRIB_VALID_CREDENTIAL);
+	}
+	
+	private Object getRequestAttribute(final String name) {
+		return request.getAttribute(name);
+	}
+	
+	
+}
