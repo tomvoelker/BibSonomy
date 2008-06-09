c10a043d0197025f702bda12a01779752de61384
Dominik Benz <benz@cs.uni-kassel.de>
2008-06-09 11:49:11 +0200

    restricted display to document links on group pages to groups with sharedDocuments=ENABLED

--- bibsonomy-database/src/main/java/org/bibsonomy/database/managers/chain/bibtex/get/GetBibtexForGroupAndTag.java
+++ bibsonomy-database/src/main/java/org/bibsonomy/database/managers/chain/bibtex/get/GetBibtexForGroupAndTag.java
@@ -5,12 +5,14 @@ import static org.bibsonomy.util.ValidationUtils.present;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bibsonomy.common.enums.FilterEntity;
 import org.bibsonomy.common.enums.GroupID;
 import org.bibsonomy.common.enums.GroupingEntity;
 import org.bibsonomy.database.managers.chain.bibtex.BibTexChainElement;
 import org.bibsonomy.database.params.BibTexParam;
 import org.bibsonomy.database.util.DBSession;
 import org.bibsonomy.model.BibTex;
+import org.bibsonomy.model.Group;
 import org.bibsonomy.model.Post;
 
 /**
@@ -24,12 +26,17 @@ public class GetBibtexForGroupAndTag extends BibTexChainElement {
 	 */
 	@Override
 	protected List<Post<BibTex>> handle(final BibTexParam param, final DBSession session) {
-		final Integer groupId = this.groupDb.getGroupIdByGroupName(param.getRequestedGroupName(), session);
-		if (groupId == GroupID.INVALID.getId()  || GroupID.isSpecialGroupId(groupId)) {
+		// final Integer groupId = this.groupDb.getGroupIdByGroupName(param.getRequestedGroupName(), session);
+		final Group group = this.groupDb.getGroupByName(param.getRequestedGroupName(), session);
+		if (group == null || group.getGroupId() == GroupID.INVALID.getId()  || GroupID.isSpecialGroupId(group.getGroupId())) {
 			log.debug("groupId " +  param.getRequestedGroupName() + " not found or special group" );
 			return new ArrayList<Post<BibTex>>(0);			
 		}
-		param.setGroupId(groupId);	
+		// remove pdf filter if sharing documents for this group is disabled
+		if (!group.isSharedDocuments() && ( param.getFilter().equals(FilterEntity.PDF) || param.getFilter().equals(FilterEntity.JUST_PDF))) {
+			param.setFilter(null);
+		}		
+		param.setGroupId(group.getGroupId());	
 		return this.db.getBibTexForGroupByTag(param, session);
 	}
 
