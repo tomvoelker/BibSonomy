36b0e0f5cc9f621a1cabfe88036a85ccb6bf6768
Robert Jäschke <jaeschke@l3s.de>
2008-06-12 16:52:42 +0200

    controller + validator for user login

---
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/actions/UserLoginCommand.java
@@ -0,0 +1,67 @@
+package org.bibsonomy.webapp.command.actions;
+
+import java.io.Serializable;
+
+import org.bibsonomy.webapp.command.BaseCommand;
+
+/** 
+ * This command encapsulates the user's name and his password for the login page. 
+ * 
+ * @author rja
+ * @version $Id$
+ */
+public class UserLoginCommand extends BaseCommand implements Serializable {
+
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -115425449425900599L;
+	/**
+	 * The name of the user which wants to login.
+	 */
+	private String username;
+	/**
+	 * The users password
+	 */
+	private String password;	
+
+	/**
+	 * URL to which the user wants to jump back after successful login.
+	 */
+	private String referer;
+
+	/**
+	 * Some pages need the user to login first, before they can be used.
+	 * They can give the user a notice using this param. 
+	 */
+	private String notice;
+	
+	
+	public String getNotice() {
+		return this.notice;
+	}
+	public void setNotice(String notice) {
+		this.notice = notice;
+	}
+	public String getUsername() {
+		return this.username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return this.password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getReferer() {
+		return this.referer;
+	}
+	public void setReferer(String referer) {
+		this.referer = referer;
+	}
+	
+	
+}
---
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/validation/UserLoginValidator.java
@@ -0,0 +1,38 @@
+package org.bibsonomy.webapp.validation;
+
+import org.bibsonomy.webapp.command.actions.UserLoginCommand;
+import org.bibsonomy.webapp.util.Validator;
+import org.springframework.util.Assert;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+
+/**
+ * @author rja
+ * @version $Id$
+ */
+public class UserLoginValidator implements Validator<UserLoginCommand> {
+
+	@SuppressWarnings("unchecked")
+	public boolean supports(final Class clazz) {
+		return UserLoginCommand.class.equals(clazz);
+	}
+
+	/**
+	 * Validates the given loginObj.
+	 * 
+	 * @see org.springframework.validation.Validator#validate(java.lang.Object, org.springframework.validation.Errors)
+	 */
+	public void validate(final Object loginObj, final Errors errors) {
+		/*
+		 * To ensure that the received command is not null, we throw an
+		 * exception, if this assertion fails.
+		 */
+		Assert.notNull(loginObj);
+		
+
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "error.field.required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "error.field.required");
+	}
+
+}
+
