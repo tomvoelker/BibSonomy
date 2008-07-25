018f261771cc38b3e641024adf2584c2131020dc
Robert Jäschke <jaeschke@l3s.de>
2008-07-25 13:51:58 +0200

    comment

--- bibsonomy-common/src/main/java/org/bibsonomy/common/enums/TagRelationType.java
+++ bibsonomy-common/src/main/java/org/bibsonomy/common/enums/TagRelationType.java
@@ -3,6 +3,8 @@ package org.bibsonomy.common.enums;
 /**
  * Defines types of relatedness between tags
  * 
+ * FIXME: very bad wording - tag relations are something different (see "relations" and "concepts")
+ * 
  * @author Dominik Benz
 
  * @version $Id$
