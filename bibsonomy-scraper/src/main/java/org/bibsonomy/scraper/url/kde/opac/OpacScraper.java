d79ef2eede87a645ee30543476118f0f37b73543
Robert Jäschke <jaeschke@l3s.de>
2008-11-19 16:06:23 +0100

    fixed but tests broken

--- bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/opac/OpacScraper.java
+++ bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/opac/OpacScraper.java
@@ -1,15 +1,9 @@
 package org.bibsonomy.scraper.url.kde.opac;
 
-import java.net.URL;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.log4j.Logger;
-import org.bibsonomy.scraper.Scraper;
 import org.bibsonomy.scraper.ScrapingContext;
 import org.bibsonomy.scraper.Tuple;
 import org.bibsonomy.scraper.UrlScraper;
@@ -17,70 +11,47 @@ import org.bibsonomy.scraper.converter.PicaToBibtexConverter;
 import org.bibsonomy.scraper.exceptions.InternalFailureException;
 import org.bibsonomy.scraper.exceptions.ScrapingException;
 import org.bibsonomy.scraper.exceptions.ScrapingFailureException;
-import org.bibsonomy.scraper.url.UrlMatchingHelper;
 
 /**
  * @author C. Kramer
  * @version $Id$
  */
-public class OpacScraper implements Scraper, UrlScraper {
-	private static final String info = "OPAC Scraper: This scraper parses a publication page from <a href=\"http://opac.bibliothek.uni-kassel.de/\">Bibliothek Kassel</a>  " +
-	"and other OPAC sites. It extracts the adequate BibTeX entry from the OPAC. Author: KDE";
+public class OpacScraper extends UrlScraper {
+	private static final String info = "OPAC Scraper: This scraper parses a publication page from " + href("http://opac.bibliothek.uni-kassel.de/" , "Bibliothek Kassel");
+
+	private static final List<Tuple<Pattern, Pattern>> patterns = Collections.singletonList(new Tuple<Pattern, Pattern>(Pattern.compile("^http.*"), Pattern.compile(".*?/CHARSET=UTF-8/PRS=PP/PPN\\?PPN=[0-9X]+$")));
 	
-	private static final Logger log = Logger.getLogger(OpacScraper.class);
-	private static final String OPAC_URL ="http://opac.???/";
-	
-	
-	
-	
-	public boolean scrape(ScrapingContext sc) throws ScrapingException {
+	protected boolean scrapeInternal(ScrapingContext sc) throws ScrapingException {
 		//log.fatal("Opac");
-        //log.fatal(sc.getUrl().toString());
+		//log.fatal(sc.getUrl().toString());
 		//Pattern.matches("^http.*?+/CHARSET=UTF-8/PRS=PP/PPN\\?PPN=[0-9X]+$", sc.getUrl().toString())
 		//sc.getUrl().toString().startsWith(OPAC_URL)
-		if (sc.getUrl() != null && (Pattern.matches("^http.*?/CHARSET=UTF-8/PRS=PP/PPN\\?PPN=[0-9X]+$", sc.getUrl().toString()))){
-			sc.setScraper(this);
-			
-			String bibResult = null;
-			PicaToBibtexConverter converter = null;
-			
-			try {
-				// create a converter and start converting :)
-				converter = new PicaToBibtexConverter(sc.getPageContent(), "xml", sc.getUrl().toString());
+		sc.setScraper(this);
 
-				bibResult = converter.getBibResult();
+		try {
+			// create a converter and start converting :)
+			final PicaToBibtexConverter converter = new PicaToBibtexConverter(sc.getPageContent(), "xml", sc.getUrl().toString());
 
-				if(bibResult != null){
-					sc.setBibtexResult(bibResult);
-					return true;
-				}else
-					throw new ScrapingFailureException("getting bibtex failed");
+			final String bibResult = converter.getBibResult();
 
-			} catch (Exception e){
-				throw new InternalFailureException(e);
-			}
-		 }
-	
-		return false;
+			if(bibResult != null){
+				sc.setBibtexResult(bibResult);
+				return true;
+			}else
+				throw new ScrapingFailureException("getting bibtex failed");
+
+		} catch (Exception e){
+			throw new InternalFailureException(e);
+		}
 	}
 
 	public String getInfo() {
 		return info;
 	}
 
-	public Collection<Scraper> getScraper() {
-		return Collections.singletonList((Scraper) this);
-	}
-		
 	public List<Tuple<Pattern, Pattern>> getUrlPatterns() {
-		List<Tuple<Pattern,Pattern>> list = new LinkedList<Tuple<Pattern,Pattern>>();
-		list.add(new Tuple<Pattern, Pattern>(UrlScraper.EMPTY_PATTERN, UrlScraper.EMPTY_PATTERN));
-		return list;
+		return patterns;
 	}
 
-	public boolean supportsUrl(URL url) {
-		// match every url
-		return true;
-	}
-	
+
 }
