426cd660cdb6d206215c017bf784ebfbec79ca5c
Robert Jäschke <jaeschke@l3s.de>
2008-11-19 16:36:09 +0100

    fixed but test broken

--- bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/l3s/L3SScraper.java
+++ bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/l3s/L3SScraper.java
@@ -1,38 +1,36 @@
 package org.bibsonomy.scraper.url.kde.l3s;
 
-import java.net.URL;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.bibsonomy.scraper.Scraper;
 import org.bibsonomy.scraper.ScrapingContext;
 import org.bibsonomy.scraper.Tuple;
 import org.bibsonomy.scraper.UrlScraper;
 import org.bibsonomy.scraper.exceptions.ScrapingException;
 import org.bibsonomy.scraper.exceptions.ScrapingFailureException;
-import org.bibsonomy.scraper.url.UrlMatchingHelper;
 
-public class L3SScraper implements Scraper, UrlScraper {
-	private static final String info 	= "L3S Scraper: Scrapes publications from <a href=\"http://www.l3s.de\">L3S</a> and returns it as bibtex. Author: KDE";
+/** Scrapes publications from L3S.
+ * 
+ * @author rja
+ *
+ */
+public class L3SScraper extends UrlScraper {
+	private static final String info 	= "L3S Scraper: Scrapes publications from " + href("http://www.l3s.de", "L3S");
 	
 	private static final String L3S_URL = "l3s.de";
+	private static Pattern patternTd = Pattern.compile("<td class=\" value text\">([^<]*)</td>", Pattern.MULTILINE | Pattern.DOTALL);
 	
-	private static final String PATTERN_HTML_TD = "<td class=\" value text\">([^<]*)</td>";
-
-	public boolean scrape(ScrapingContext sc) throws ScrapingException {
-		
-		//-- url shouldn't be null
-		if (sc != null && sc.getUrl() != null && supportsUrl(sc.getUrl())) {
+	private static final List<Tuple<Pattern, Pattern>> pattern = Collections.singletonList(new Tuple<Pattern, Pattern>(Pattern.compile(".*" + L3S_URL), UrlScraper.EMPTY_PATTERN));
+	
+	protected boolean scrapeInternal(ScrapingContext sc) throws ScrapingException {
 				
 				sc.setScraper(this);
 				String bibtexresult = null;
 				
-				Pattern patternTd = Pattern.compile(PATTERN_HTML_TD, Pattern.MULTILINE | Pattern.DOTALL);
-				Matcher matcherTd = patternTd.matcher(sc.getPageContent());
+				
+				final Matcher matcherTd = patternTd.matcher(sc.getPageContent());
 				while(matcherTd.find()){
 					
 					String td = matcherTd.group();
@@ -57,29 +55,14 @@ public class L3SScraper implements Scraper, UrlScraper {
 					return true;
 				}else
 					throw new ScrapingFailureException("getting bibtex failed");
-
-		}
 		
-
-		return false;
 	}
 
 	public String getInfo() {
 		return info;
 	}
-			
-	public Collection<Scraper> getScraper() {
-		return Collections.singletonList((Scraper) this);
-	}
 	
 	public List<Tuple<Pattern, Pattern>> getUrlPatterns() {
-		List<Tuple<Pattern,Pattern>> list = new LinkedList<Tuple<Pattern,Pattern>>();
-		list.add(new Tuple<Pattern, Pattern>(Pattern.compile(".*" + L3S_URL), UrlScraper.EMPTY_PATTERN));
-		return list;
+		return pattern;
 	}
-
-	public boolean supportsUrl(URL url) {
-		return UrlMatchingHelper.isUrlMatch(url, this);
-	}
-	
 }
