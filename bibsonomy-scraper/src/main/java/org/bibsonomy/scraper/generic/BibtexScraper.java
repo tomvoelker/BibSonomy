3bb0dd069d42d563d523390bc24f7ebec5ca2664
Robert Jäschke <jaeschke@l3s.de>
2008-11-19 16:59:47 +0100

    moved here because they're generic!

---
+++ bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/generic/BibtexScraper.java
@@ -0,0 +1,91 @@
+package org.bibsonomy.scraper.generic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.net.URL;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang.StringEscapeUtils;
+import org.bibsonomy.scraper.Scraper;
+import org.bibsonomy.scraper.ScrapingContext;
+import org.bibsonomy.scraper.Tuple;
+import org.bibsonomy.scraper.UrlScraper;
+import org.bibsonomy.scraper.exceptions.InternalFailureException;
+import org.bibsonomy.scraper.exceptions.ScrapingException;
+
+import bibtex.dom.BibtexEntry;
+import bibtex.dom.BibtexFile;
+import bibtex.parser.BibtexParser;
+import bibtex.parser.ParseException;
+
+/**
+ * Search in sourcecode from the given page for bibtex and scrape it.
+ * @author tst
+ * @version $Id$
+ */
+public class BibtexScraper implements Scraper {
+	
+	private static final String INFO = "Scraper for bibtex, independent from URL";
+
+	public String getInfo() {
+		return INFO;
+	}
+
+	public Collection<Scraper> getScraper() {
+		return Collections.singletonList((Scraper) this);
+	}
+
+	/*
+	 * FIXME: This is not a URL scraper!
+	 * 
+	 * @see org.bibsonomy.scraper.Scraper#scrape(org.bibsonomy.scraper.ScrapingContext)
+	 */
+	public boolean scrape(ScrapingContext sc)throws ScrapingException {
+		if(sc != null && sc.getUrl() != null && supportsUrl(sc.getUrl())){
+			sc.setScraper(this);
+			
+			String source = sc.getPageContent();
+			
+			// html clean up
+			source = StringEscapeUtils.unescapeHtml(source);
+			source = source.replace("<br/>", "\n");
+			// TODO: may be some other format elements like <i>, <p> etc. are still in code
+			
+			try {
+				
+				/* 
+				 * copy from SnippetScraper
+				 */
+				BibtexParser parser = new BibtexParser(true);
+				BibtexFile bibtexFile = new BibtexFile();
+				BufferedReader sr = new BufferedReader(new StringReader(source));
+				// parse source
+				parser.parse(bibtexFile, sr);
+
+				for (Object potentialEntry:bibtexFile.getEntries()) {
+					if ((potentialEntry instanceof BibtexEntry)) {
+						sc.setBibtexResult(potentialEntry.toString());
+						return true; 
+					}
+				}
+			} catch (ParseException ex) {
+				throw new InternalFailureException(ex);
+			} catch (IOException ex) {
+				throw new InternalFailureException(ex);
+			}
+
+		}
+		return false;
+	}
+
+	public boolean supportsUrl(URL url) {
+		// match against every url
+		return true;
+	}
+
+}
