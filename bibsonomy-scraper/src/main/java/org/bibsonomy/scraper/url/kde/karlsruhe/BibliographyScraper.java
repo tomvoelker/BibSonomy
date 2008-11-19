f95a9f6d1978388db1f65dc393aeae9906f3f3c2
Robert Jäschke <jaeschke@l3s.de>
2008-11-19 18:12:42 +0100

    fixed but tests broken

--- bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/karlsruhe/BibliographyScraper.java
+++ bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/karlsruhe/BibliographyScraper.java
@@ -1,27 +1,22 @@
 package org.bibsonomy.scraper.url.kde.karlsruhe;
 
-import java.net.URL;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.regex.Pattern;
 
-import org.bibsonomy.scraper.Scraper;
 import org.bibsonomy.scraper.ScrapingContext;
 import org.bibsonomy.scraper.Tuple;
 import org.bibsonomy.scraper.UrlScraper;
 import org.bibsonomy.scraper.exceptions.ScrapingException;
-import org.bibsonomy.scraper.url.UrlMatchingHelper;
 
 /**
  * Scraper for liinwww.ira.uka.de/bibliography
  * @author tst
  * @version $Id$
  */
-public class BibliographyScraper implements Scraper, UrlScraper {
+public class BibliographyScraper extends UrlScraper {
 
-	private static final String INFO = "LIIN Scraper: Scrapes BibTeX refrences from <a href=\"http://liinwww.ira.uka.de/\">LIIN</a>. Author: KDE";
+	private static final String INFO = "LIIN Scraper: Scrapes BibTeX refrences from " + href("http://liinwww.ira.uka.de/", "LIIN");
 	
 	private static final String HOST = "liinwww.ira.uka.de";
 	private static final String PATH = "/cgi-bin/bibshow";
@@ -29,16 +24,14 @@ public class BibliographyScraper implements Scraper, UrlScraper {
 	private static final String BIBTEX_START_BLOCK = "<pre class=\"bibtex\">";
 	private static final String BIBTEX_END_BLOCK = "</pre>";
 	
+	private static final List<Tuple<Pattern, Pattern>> patterns = Collections.singletonList(new Tuple<Pattern, Pattern>(Pattern.compile(".*" + HOST), Pattern.compile(PATH + ".*")));
+
+	
 	public String getInfo() {
 		return INFO;
 	}
 
-	public Collection<Scraper> getScraper() {
-		return Collections.singletonList((Scraper) this);
-	}
-
-	public boolean scrape(ScrapingContext sc)throws ScrapingException {
-		if(sc != null && sc.getUrl() != null && supportsUrl(sc.getUrl())){
+	protected boolean scrapeInternal(ScrapingContext sc)throws ScrapingException {
 			sc.setScraper(this);
 			
 			String page = sc.getPageContent();
@@ -58,18 +51,9 @@ public class BibliographyScraper implements Scraper, UrlScraper {
 			}else
 				throw new ScrapingException("Can't find bibtex in scraped page.");			
 
-		}
-		return false;
 	}
 
 	public List<Tuple<Pattern, Pattern>> getUrlPatterns() {
-		List<Tuple<Pattern,Pattern>> list = new LinkedList<Tuple<Pattern,Pattern>>();
-		list.add(new Tuple<Pattern, Pattern>(Pattern.compile(".*" + HOST), Pattern.compile(PATH + ".*")));
-		return list;
+		return patterns;
 	}
-
-	public boolean supportsUrl(URL url) {
-		return UrlMatchingHelper.isUrlMatch(url, this);
-	}
-	
 }
