3e18aee743f85a883a8ecb5c975ef24383edb80b
Robert Jäschke <jaeschke@l3s.de>
2008-11-19 16:18:03 +0100

    moved

---
+++ bibsonomy-scraper/src/test/java/org/bibsonomy/scraper/url/kde/nasaads/NasaAdsScraperTest.java
@@ -0,0 +1,34 @@
+package org.bibsonomy.scraper.url.kde.nasaads;
+
+import org.bibsonomy.scraper.UnitTestRunner;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Scraper URL tests #22 #23 for NasaAdsScraper
+ * @author tst
+ *
+ */
+public class NasaAdsScraperTest {
+	
+	/**
+	 * starts URL test with id url_22
+	 */
+	@Test
+	public void url1TestRun(){
+		UnitTestRunner runner = new UnitTestRunner();
+		assertTrue(runner.runSingleTest("url_22"));
+	}
+
+	/**
+	 * starts URL test with id url_23
+	 */
+	@Test
+	public void url2TestRun(){
+		UnitTestRunner runner = new UnitTestRunner();
+		assertTrue(runner.runSingleTest("url_23"));
+	}
+	
+}
