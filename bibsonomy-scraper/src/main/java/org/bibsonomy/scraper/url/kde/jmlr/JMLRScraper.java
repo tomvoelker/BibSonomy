8138087c021aae977e9d3f0fe4740bbf341a996b
Robert Jäschke <jaeschke@l3s.de>
2008-11-19 16:39:25 +0100

    fixed but test broken

--- bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/jmlr/JMLRScraper.java
+++ bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/jmlr/JMLRScraper.java
@@ -1,37 +1,34 @@
 package org.bibsonomy.scraper.url.kde.jmlr;
 
-import java.net.URL;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.bibsonomy.scraper.Scraper;
 import org.bibsonomy.scraper.ScrapingContext;
 import org.bibsonomy.scraper.Tuple;
 import org.bibsonomy.scraper.UrlScraper;
 import org.bibsonomy.scraper.exceptions.PageNotSupportedException;
 import org.bibsonomy.scraper.exceptions.ScrapingException;
-import org.bibsonomy.scraper.url.UrlMatchingHelper;
 
 /**
  * Scraper for papers from http://jmlr.csail.mit.edu/
  * @author tst
  * @version $Id$
  */
-public class JMLRScraper implements Scraper, UrlScraper {
-	
-	private static final String INFO = "JMLR Scraper: Scraper for papers from <a href=\"http://jmlr.csail.mit.edu/\">Journal of Machine Learning Research</a>. Author: KDE";
-	
+public class JMLRScraper extends UrlScraper {
+
+	private static final String INFO = "JMLR Scraper: Scraper for papers from " + href("http://jmlr.csail.mit.edu/", "Journal of Machine Learning Research");
+
 	private static final String HOST = "jmlr.csail.mit.edu";
-	
+
 	private static final String PATH = "/papers/";
-	
-	
+
+	private static final List<Tuple<Pattern, Pattern>> patterns = Collections.singletonList(new Tuple<Pattern, Pattern>(Pattern.compile(".*" + HOST), UrlScraper.EMPTY_PATTERN));
+
 	/*
 	 * pattern
+	 * FIXME: refactor into static Patterns
 	 */
 	private static final String PATTERN_TITLE = "<h2>([^<]*)</h2>";
 	private static final String PATTERN_AUTHOR = "<i>([^<]*)</i></b>";
@@ -45,147 +42,133 @@ public class JMLRScraper implements Scraper, UrlScraper {
 		return INFO;
 	}
 
-	public Collection<Scraper> getScraper() {
-		return Collections.singletonList((Scraper) this);
-	}
+	protected boolean scrapeInternal(ScrapingContext sc)throws ScrapingException {
+		sc.setScraper(this);
 
-	public boolean scrape(ScrapingContext sc)throws ScrapingException {
-		if(sc != null && sc.getUrl() != null && supportsUrl(sc.getUrl())){
-			sc.setScraper(this);
-			
-			if(sc.getUrl().getPath().startsWith(PATH) && sc.getUrl().getPath().endsWith(".html")){
-				String pageContent = sc.getPageContent();
-				
-				// get title (directly)
-				String title = null;
-				Pattern titlePattern = Pattern.compile(PATTERN_TITLE);
-				Matcher titleMatcher = titlePattern.matcher(pageContent);
-				if(titleMatcher.find())
-					title = titleMatcher.group(1);
+		if(sc.getUrl().getPath().startsWith(PATH) && sc.getUrl().getPath().endsWith(".html")){
+			String pageContent = sc.getPageContent();
 
-				// get author (directly)
-				String author = null;
-				Pattern authorPattern = Pattern.compile(PATTERN_AUTHOR);
-				Matcher authorMatcher = authorPattern.matcher(pageContent);
-				if(authorMatcher.find())
-					author = authorMatcher.group(1);
-				// clean up author string
-				if(author != null)
-					author = author.replace(",", " and");
-				
-				// get volume (from url)
-				String volume = null;
-				Pattern volumePattern = Pattern.compile(PATTERN_VOLUME);
-				Matcher volumeMatcher = volumePattern.matcher(sc.getUrl().getPath());
-				if(volumeMatcher.find())
-					volume = volumeMatcher.group(1);
+			// get title (directly)
+			String title = null;
+			Pattern titlePattern = Pattern.compile(PATTERN_TITLE);
+			Matcher titleMatcher = titlePattern.matcher(pageContent);
+			if(titleMatcher.find())
+				title = titleMatcher.group(1);
 
-				// get pageYear (directly)
-				String pageYear = null;
-				Pattern pageYearPattern = Pattern.compile(PATTERN_YEAR_PAGE);
-				Matcher pageYearMatcher = pageYearPattern.matcher(pageContent);
-				if(pageYearMatcher.find())
-					pageYear = pageYearMatcher.group(1);
+			// get author (directly)
+			String author = null;
+			Pattern authorPattern = Pattern.compile(PATTERN_AUTHOR);
+			Matcher authorMatcher = authorPattern.matcher(pageContent);
+			if(authorMatcher.find())
+				author = authorMatcher.group(1);
+			// clean up author string
+			if(author != null)
+				author = author.replace(",", " and");
 
-				// extract year from pageYear string
-				String year = null;
-				Pattern yearPattern = Pattern.compile(PATTERN_YEAR);
-				Matcher yearMatcher = yearPattern.matcher(pageYear);
-				if(yearMatcher.find())
-					year = yearMatcher.group(1);
+			// get volume (from url)
+			String volume = null;
+			Pattern volumePattern = Pattern.compile(PATTERN_VOLUME);
+			Matcher volumeMatcher = volumePattern.matcher(sc.getUrl().getPath());
+			if(volumeMatcher.find())
+				volume = volumeMatcher.group(1);
 
-				// extarct page from pageYear string
-				String page = null;
-				Pattern pagePattern = Pattern.compile(PATTERN_PAGE);
-				Matcher pageMatcher = pagePattern.matcher(pageYear);
-				if(pageMatcher.find())
-					page = pageMatcher.group(1);
+			// get pageYear (directly)
+			String pageYear = null;
+			Pattern pageYearPattern = Pattern.compile(PATTERN_YEAR_PAGE);
+			Matcher pageYearMatcher = pageYearPattern.matcher(pageContent);
+			if(pageYearMatcher.find())
+				pageYear = pageYearMatcher.group(1);
 
-				/*
-				 * build bibtex
-				 */
-				StringBuffer bibtex = new StringBuffer();
-				bibtex.append("@proceedings{");
-				
-				// build bibtex key
-				if(year != null && author != null){
-					Pattern lastnamePattern = Pattern.compile(PATTERN_FIRST_LASTNAME);
-					Matcher lastnameMatcher = lastnamePattern.matcher(author);
-					if(lastnameMatcher.find()) // combination lastname and year
-						bibtex.append(lastnameMatcher.group(1)).append(year);
-					else // only year
-						bibtex.append(year);
-				}else // default value
-					bibtex.append("jmlrKey");
-				bibtex.append(",\n");
-				
-				// add title
-				if(title != null){
-					bibtex.append("title = {");
-					bibtex.append(title);
-					bibtex.append("},\n");
-				}
-				
-				// add author
-				if(author != null){
-					bibtex.append("author = {");
-					bibtex.append(author);
-					bibtex.append("},\n");
-				}
-				
-				// add year
-				if(year != null){
-					bibtex.append("year = {");
+			// extract year from pageYear string
+			String year = null;
+			Pattern yearPattern = Pattern.compile(PATTERN_YEAR);
+			Matcher yearMatcher = yearPattern.matcher(pageYear);
+			if(yearMatcher.find())
+				year = yearMatcher.group(1);
+
+			// extarct page from pageYear string
+			String page = null;
+			Pattern pagePattern = Pattern.compile(PATTERN_PAGE);
+			Matcher pageMatcher = pagePattern.matcher(pageYear);
+			if(pageMatcher.find())
+				page = pageMatcher.group(1);
+
+			/*
+			 * build bibtex
+			 */
+			StringBuffer bibtex = new StringBuffer();
+			bibtex.append("@proceedings{");
+
+			// build bibtex key
+			if(year != null && author != null){
+				Pattern lastnamePattern = Pattern.compile(PATTERN_FIRST_LASTNAME);
+				Matcher lastnameMatcher = lastnamePattern.matcher(author);
+				if(lastnameMatcher.find()) // combination lastname and year
+					bibtex.append(lastnameMatcher.group(1)).append(year);
+				else // only year
 					bibtex.append(year);
-					bibtex.append("},\n");
-				}
+			}else // default value
+				bibtex.append("jmlrKey");
+			bibtex.append(",\n");
 
-				// add page
-				if(page != null){
-					bibtex.append("page = {");
-					bibtex.append(page);
-					bibtex.append("},\n");
-				}
+			// add title
+			if(title != null){
+				bibtex.append("title = {");
+				bibtex.append(title);
+				bibtex.append("},\n");
+			}
 
-				// add page
-				if(page != null){
-					bibtex.append("page = {");
-					bibtex.append(page);
-					bibtex.append("},\n");
-				}
+			// add author
+			if(author != null){
+				bibtex.append("author = {");
+				bibtex.append(author);
+				bibtex.append("},\n");
+			}
 
-				// add volume
-				if(volume != null){
-					bibtex.append("volume = {");
-					bibtex.append(volume);
-					bibtex.append("},\n");
-				}
-				
-				// remove last ","
-				bibtex.deleteCharAt(bibtex.lastIndexOf(","));
-				
-				// last part
-				bibtex.append("}");
-				
-				// finish
-				sc.setBibtexResult(bibtex.toString());
-				return true;
+			// add year
+			if(year != null){
+				bibtex.append("year = {");
+				bibtex.append(year);
+				bibtex.append("},\n");
+			}
 
-			}else
-				throw new PageNotSupportedException("Select a page with the abtract view from a jmlr paper.");
-		}
-		
-		return false;
+			// add page
+			if(page != null){
+				bibtex.append("page = {");
+				bibtex.append(page);
+				bibtex.append("},\n");
+			}
+
+			// add page
+			if(page != null){
+				bibtex.append("page = {");
+				bibtex.append(page);
+				bibtex.append("},\n");
+			}
+
+			// add volume
+			if(volume != null){
+				bibtex.append("volume = {");
+				bibtex.append(volume);
+				bibtex.append("},\n");
+			}
+
+			// remove last ","
+			bibtex.deleteCharAt(bibtex.lastIndexOf(","));
+
+			// last part
+			bibtex.append("}");
+
+			// finish
+			sc.setBibtexResult(bibtex.toString());
+			return true;
+
+		}else
+			throw new PageNotSupportedException("Select a page with the abtract view from a jmlr paper.");
 	}
 
 	public List<Tuple<Pattern, Pattern>> getUrlPatterns() {
-		List<Tuple<Pattern,Pattern>> list = new LinkedList<Tuple<Pattern,Pattern>>();
-		list.add(new Tuple<Pattern, Pattern>(Pattern.compile(".*" + HOST), UrlScraper.EMPTY_PATTERN));
-		return list;
+		return patterns;
 	}
 
-	public boolean supportsUrl(URL url) {
-		return UrlMatchingHelper.isUrlMatch(url, this);
-	}
-	
 }
