c1f36042d4e5f89b6ef51fe873bbad7212026fb7
Robert Jäschke <jaeschke@l3s.de>
2008-11-19 16:51:21 +0100

    fixed but test broken

--- bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/eric/EricScraper.java
+++ bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/eric/EricScraper.java
@@ -2,14 +2,11 @@ package org.bibsonomy.scraper.url.kde.eric;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.bibsonomy.scraper.Scraper;
 import org.bibsonomy.scraper.ScrapingContext;
 import org.bibsonomy.scraper.Tuple;
 import org.bibsonomy.scraper.UrlScraper;
@@ -18,36 +15,32 @@ import org.bibsonomy.scraper.exceptions.InternalFailureException;
 import org.bibsonomy.scraper.exceptions.PageNotSupportedException;
 import org.bibsonomy.scraper.exceptions.ScrapingException;
 import org.bibsonomy.scraper.exceptions.ScrapingFailureException;
-import org.bibsonomy.scraper.url.UrlMatchingHelper;
 
 /**
  * SCraper for papers from http://www.eric.ed.gov/
  * @author tst
  * @version $Id$
  */
-public class EricScraper implements Scraper, UrlScraper {
+public class EricScraper extends UrlScraper {
 	
-	private static final String INFO = "ERIC Scraper: Scraper for publications from the <a href=\"http://www.eric.ed.gov/\">Education Resources Information Center</a>. Author: KDE";
+	private static final String INFO = "ERIC Scraper: Scraper for publications from the " + href("http://www.eric.ed.gov/", "Education Resources Information Center");
 	
 	private static final String ERIC_HOST = "eric.ed.gov";
-	
-	private static final String ERIC_URL = "http://www.eric.ed.gov/";
 	
 	private static final String EXPORT_BASE_URL = "http://www.eric.ed.gov/ERICWebPortal/custom/portlets/clipboard/performExport.jsp?texttype=endnote&accno=";
 	
 	private static final String PATTERN_ACCNO = "accno=([^&]*)";
 
+	private static final Pattern accnoPattern = Pattern.compile("accno=([^&]*)");
+
+	
+	private static final List<Tuple<Pattern, Pattern>> patterns = Collections.singletonList(new Tuple<Pattern, Pattern>(Pattern.compile(".*" + ERIC_HOST), UrlScraper.EMPTY_PATTERN));
+	
 	public String getInfo() {
 		return INFO;
 	}
 
-	public Collection<Scraper> getScraper() {
-		return Collections.singletonList((Scraper) this);
-	}
-
-	public boolean scrape(ScrapingContext sc)throws ScrapingException {
-		// TODO Auto-generated method stub
-		
+	protected boolean scrapeInternal (ScrapingContext sc)throws ScrapingException {
 		/*
 		 * example:
 		 * http://www.eric.ed.gov/ERICWebPortal/Home.portal?_nfpb=true&ERICExtSearch_SearchValue_0=star&searchtype=keyword&ERICExtSearch_SearchType_0=kw&_pageLabel=RecordDetails&objectId=0900019b802f2e44&accno=EJ786532&_nfls=false
@@ -57,14 +50,12 @@ public class EricScraper implements Scraper, UrlScraper {
 		 * 
 		 */
 		
-		if(sc != null && sc.getUrl() != null && supportsUrl(sc.getUrl())){
 			sc.setScraper(this);
 			
 			//extract accno from url query
 			String accno = null;
 			
-			Pattern accnoPattern = Pattern.compile(PATTERN_ACCNO);
-			Matcher accnoMatcher = accnoPattern.matcher(sc.getUrl().getQuery());
+			final Matcher accnoMatcher = accnoPattern.matcher(sc.getUrl().getQuery());
 			if(accnoMatcher.find())
 				accno = accnoMatcher.group(1);
 			
@@ -97,19 +88,10 @@ public class EricScraper implements Scraper, UrlScraper {
 			} catch (MalformedURLException ex) {
 				throw new InternalFailureException(ex);
 			}
-		}
-		
-		return false;
 	}
 	
 	public List<Tuple<Pattern, Pattern>> getUrlPatterns() {
-		List<Tuple<Pattern,Pattern>> list = new LinkedList<Tuple<Pattern,Pattern>>();
-		list.add(new Tuple<Pattern, Pattern>(Pattern.compile(".*" + ERIC_HOST), UrlScraper.EMPTY_PATTERN));
-		return list;
-	}
-
-	public boolean supportsUrl(URL url) {
-		return UrlMatchingHelper.isUrlMatch(url, this);
+		return patterns;
 	}
 
 }
