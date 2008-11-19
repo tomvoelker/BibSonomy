3aca2af479a18845d8d6b5ccf33bd0c3626e0189
Robert Jäschke <jaeschke@l3s.de>
2008-11-19 17:58:11 +0100

    fixed but tests broken

--- bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/ssrn/SSRNScraper.java
+++ bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/ssrn/SSRNScraper.java
@@ -1,64 +1,48 @@
 package org.bibsonomy.scraper.url.kde.ssrn;
 
-import java.net.URL;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.regex.Pattern;
 
 import org.apache.log4j.Logger;
-import org.bibsonomy.scraper.Scraper;
 import org.bibsonomy.scraper.ScrapingContext;
 import org.bibsonomy.scraper.Tuple;
 import org.bibsonomy.scraper.UrlScraper;
-import org.bibsonomy.scraper.exceptions.PageNotSupportedException;
 import org.bibsonomy.scraper.exceptions.ScrapingException;
-import org.bibsonomy.scraper.url.UrlMatchingHelper;
 
 /**
  * Scraping Logger for access on http://www.ssrn.com/
  * @author tst
  * @version $Id$
  */
-public class SSRNScraper implements Scraper, UrlScraper {
-	
+public class SSRNScraper extends UrlScraper {
+
 	private static final Logger log = Logger.getLogger(SSRNScraper.class);
 
 	private static final String INFO = "currently not available";
-	
+
 	private static final String HOST = "ssrn.com";
+	
+	private static final List<Tuple<Pattern, Pattern>> patterns = Collections.singletonList(new Tuple<Pattern, Pattern>(Pattern.compile(".*" + HOST), UrlScraper.EMPTY_PATTERN));
 	
 	public String getInfo() {
 		return INFO;
 	}
 
-	public Collection<Scraper> getScraper() {
-		return Collections.singletonList((Scraper) this);
-	}
-
-	public boolean scrape(ScrapingContext sc)throws ScrapingException {
+	protected boolean scrapeInternal(ScrapingContext sc)throws ScrapingException {
 		/*
 		 * See alternative code in _SSRNScraper.java
 		 */
-		if(sc != null && sc.getUrl() != null && supportsUrl(sc.getUrl())){
-			// log message
-			log.debug("Observed Scraper called: SSRNScraper is called with " + sc.getUrl().toString());
-			
-			// TODO: throw exception or not?
-			// throw new PageNotSupportedException("This Page is currently not supported");
-		}
+		// log message
+		log.debug("Observed Scraper called: SSRNScraper is called with " + sc.getUrl().toString());
+
+		// TODO: throw exception or not?
+		// throw new PageNotSupportedException("This Page is currently not supported");
 		return false;
 	}
-	
-	public List<Tuple<Pattern, Pattern>> getUrlPatterns() {
-		List<Tuple<Pattern,Pattern>> list = new LinkedList<Tuple<Pattern,Pattern>>();
-		list.add(new Tuple<Pattern, Pattern>(Pattern.compile(".*" + HOST), UrlScraper.EMPTY_PATTERN));
-		return list;
-	}
 
-	public boolean supportsUrl(URL url) {
-		return UrlMatchingHelper.isUrlMatch(url, this);
+	public List<Tuple<Pattern, Pattern>> getUrlPatterns() {
+		return patterns;
 	}
 
 }
