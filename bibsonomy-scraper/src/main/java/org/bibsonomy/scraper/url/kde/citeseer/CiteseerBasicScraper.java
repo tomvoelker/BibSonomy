8cabe732f3d9b379c128e50b7a66aa8a30ddb708
Robert Jäschke <jaeschke@l3s.de>
2008-11-19 17:06:24 +0100

    fixed but test broken

--- bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/citeseer/CiteseerBasicScraper.java
+++ bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/citeseer/CiteseerBasicScraper.java
@@ -1,64 +1,47 @@
 package org.bibsonomy.scraper.url.kde.citeseer;
 
-import java.net.URL;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.bibsonomy.scraper.Scraper;
 import org.bibsonomy.scraper.ScrapingContext;
 import org.bibsonomy.scraper.Tuple;
 import org.bibsonomy.scraper.UrlScraper;
 import org.bibsonomy.scraper.exceptions.PageNotSupportedException;
 import org.bibsonomy.scraper.exceptions.ScrapingException;
-import org.bibsonomy.scraper.exceptions.ScrapingFailureException;
-import org.bibsonomy.scraper.url.UrlMatchingHelper;
 
 
-public class CiteseerBasicScraper implements Scraper, UrlScraper {
-	
+/** Scraper for CiteSeer
+ * @author rja
+ *
+ */
+public class CiteseerBasicScraper extends UrlScraper {
+
 	private static final String info = "CiteSeer Scraper: This scraper parses a publication page from the " +
-									   "Scientific Literature Digital Library (<a href=\"http://citeseer.ist.psu.edu/\">CiteSeer</a>) " +
-	   								   "and extracts the adequate BibTeX entry. Author: KDE";
-	
+	"Scientific Literature Digital Library " + href("http://citeseer.ist.psu.edu/", "CiteSeer");
+
 	private static final String  CS_HOST_NAME   = "citeseer.ist.psu.edu";
-	private static final String  CS_BIB_PATTERN = ".*<pre>\\s*(@[A-Za-z]+\\s*\\{.+?\\})\\s*</pre>.*";
-
-	public boolean scrape(ScrapingContext sc) throws ScrapingException{
-		if (sc != null && sc.getUrl() != null && supportsUrl(sc.getUrl())){
-			sc.setScraper(this);
-			
-			Pattern p = Pattern.compile(CS_BIB_PATTERN, Pattern.MULTILINE | Pattern.DOTALL);
-			Matcher m = p.matcher(sc.getPageContent());	
-			if (m.matches()) {
-				sc.setBibtexResult(m.group(1));
-				return true;
-			}else
-				throw new PageNotSupportedException("no bibtex snippet available");
-
-		}
-		return false;
-	}
+	private static final Pattern bibPattern = Pattern.compile(".*<pre>\\s*(@[A-Za-z]+\\s*\\{.+?\\})\\s*</pre>.*", Pattern.MULTILINE | Pattern.DOTALL);
 	
+	private static final List<Tuple<Pattern, Pattern>> patterns = Collections.singletonList(new Tuple<Pattern, Pattern>(Pattern.compile(".*" + CS_HOST_NAME), UrlScraper.EMPTY_PATTERN));
+	
+	protected boolean scrapeInternal(ScrapingContext sc) throws ScrapingException{
+		sc.setScraper(this);
+		
+		final Matcher m = bibPattern.matcher(sc.getPageContent());	
+		if (m.matches()) {
+			sc.setBibtexResult(m.group(1));
+			return true;
+		}else
+			throw new PageNotSupportedException("no bibtex snippet available");
+	}
+
 	public String getInfo() {
 		return info;
 	}
-	
-	public Collection<Scraper> getScraper() {
-		return Collections.singletonList((Scraper) this);
-	}
 
 	public List<Tuple<Pattern, Pattern>> getUrlPatterns() {
-		List<Tuple<Pattern,Pattern>> list = new LinkedList<Tuple<Pattern,Pattern>>();
-		list.add(new Tuple<Pattern, Pattern>(Pattern.compile(".*" + CS_HOST_NAME), UrlScraper.EMPTY_PATTERN));
-		return list;
+		return patterns;
 	}
-
-	public boolean supportsUrl(URL url) {
-		return UrlMatchingHelper.isUrlMatch(url, this);
-	}
-	
 }
--- bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/citeseer/CiteseerxScraper.java
+++ bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/citeseer/CiteseerxScraper.java
@@ -1,45 +1,35 @@
 package org.bibsonomy.scraper.url.kde.citeseer;
 
-import java.net.URL;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.bibsonomy.scraper.Scraper;
 import org.bibsonomy.scraper.ScrapingContext;
 import org.bibsonomy.scraper.Tuple;
 import org.bibsonomy.scraper.UrlScraper;
 import org.bibsonomy.scraper.exceptions.PageNotSupportedException;
 import org.bibsonomy.scraper.exceptions.ScrapingException;
-import org.bibsonomy.scraper.url.UrlMatchingHelper;
 
 /**
  * @author tst
  * @version $Id$
  */
-public class CiteseerxScraper implements Scraper, UrlScraper {
+public class CiteseerxScraper extends UrlScraper {
 	
 	private static final String INFO = "CiteSeerX beta Scraper: This scraper parses a publication page from the " +
-									   "Scientific Literature Digital Library and Search Engine (<a href=\"http://citeseerx.ist.psu.edu/\">CiteSeerX</a>) " +
-	   								   "and extracts the adequate BibTeX entry. Author: KDE";
+									   "Scientific Literature Digital Library and Search Engine " + href("http://citeseerx.ist.psu.edu/", "CiteSeerX");
 	
 	private static final String HOST = "citeseerx.ist.psu.edu";
-	
-	private static final String REGEX = "</h2>\\s*(@.*)\\s*</div>";
+	private static final Pattern pattern = Pattern.compile("</h2>\\s*(@.*)\\s*</div>");
 
+	private static final List<Tuple<Pattern, Pattern>> patterns = Collections.singletonList(new Tuple<Pattern, Pattern>(Pattern.compile(".*" + HOST), UrlScraper.EMPTY_PATTERN));
+	
 	public String getInfo() {
 		return INFO;
 	}
-	
-	public Collection<Scraper> getScraper() {
-		return Collections.singletonList((Scraper) this);
-	}
 
-	public boolean scrape(ScrapingContext sc)throws ScrapingException {
-		if(sc != null && sc.getUrl() != null && supportsUrl(sc.getUrl())){
+	protected boolean scrapeInternal(ScrapingContext sc)throws ScrapingException {
 			sc.setScraper(this);
 			
 			// check for selected bibtex snippet
@@ -53,8 +43,7 @@ public class CiteseerxScraper implements Scraper, UrlScraper {
 			String page = sc.getPageContent();
 			
 			// search snippet in html
-			Pattern pattern = Pattern.compile(REGEX);
-			Matcher matcher = pattern.matcher(page);
+			final Matcher matcher = pattern.matcher(page);
 			
 			if(matcher.find()){
 				String bibtex = matcher.group(1);
@@ -68,18 +57,10 @@ public class CiteseerxScraper implements Scraper, UrlScraper {
 
 			}else
 				throw new PageNotSupportedException("no bibtex snippet available");
-		}
-		return false;
 	}
 	
 	public List<Tuple<Pattern, Pattern>> getUrlPatterns() {
-		List<Tuple<Pattern,Pattern>> list = new LinkedList<Tuple<Pattern,Pattern>>();
-		list.add(new Tuple<Pattern, Pattern>(Pattern.compile(".*" + HOST), UrlScraper.EMPTY_PATTERN));
-		return list;
-	}
-
-	public boolean supportsUrl(URL url) {
-		return UrlMatchingHelper.isUrlMatch(url, this);
+		return patterns;
 	}
 
 }
