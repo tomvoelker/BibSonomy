318f6d613687378332eedf91644b25bbc706f642
Robert Jäschke <jaeschke@l3s.de>
2008-11-19 16:32:43 +0100

    fixed but test broken

--- bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/librarything/LibrarythingScraper.java
+++ bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/librarything/LibrarythingScraper.java
@@ -1,29 +1,17 @@
 package org.bibsonomy.scraper.url.kde.librarything;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.math.MathContext;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.bibsonomy.scraper.Scraper;
 import org.bibsonomy.scraper.ScrapingContext;
 import org.bibsonomy.scraper.Tuple;
 import org.bibsonomy.scraper.UrlScraper;
 import org.bibsonomy.scraper.exceptions.InternalFailureException;
 import org.bibsonomy.scraper.exceptions.ScrapingException;
-import org.bibsonomy.scraper.url.UrlMatchingHelper;
-import org.w3c.dom.Attr;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-import org.w3c.tidy.Tidy;
 
 
 /**
@@ -33,38 +21,43 @@ import org.w3c.tidy.Tidy;
  * http://www.librarything.com/work/
  * @author tst
  */
-public class LibrarythingScraper implements Scraper, UrlScraper {
-	
-	private static final String INFO = "LibrarythingScraper: extracts publication from <a herf=\"http://www.librarything.com/work-info\">librarything</a> and convert it to bibtex. If a http://www.librarything.com/work page is selectd, then the scraper trys to download the according work-info page. Author: KDE";
-	
-	
+public class LibrarythingScraper extends UrlScraper {
+
+	private static final String INFO = "LibrarythingScraper: extracts publication from " + href("http://www.librarything.com/work-info", "librarything") + 
+	". If a http://www.librarything.com/work page is selectd, then the scraper trys to download the according work-info page.";
+
+
 	private static final String URL_LIBRARYTHING_PAGE = "http://www.librarything.com";
-	
+
 	private static final String URL_LIBRARYTHING_PAGE_HOST = "librarything.com";
 
-
+	/*
+	 * FIXME: make all patterns static Patterns!
+	 */
 	private static String LIBRARYTHING_PATTERN_BIBLIOGRAPHIC_INFOS = "<td class=\"bookeditfield\" id=\"bookedit_publication\">([^<]*)</td>";
-	
+
 	private static String LIBRARYTHING_PATTERN_OTHER_AUTHORS = "<td class=\"bookeditfield\" id=\"bookedit_otherauthors\">([^<]*)</td>";
-	
+
 	private static String LIBRARYTHING_PATTERN_TITLE = "<td class=\"bookeditfield\" id=\"bookedit_title\"><b>([^<]*)</b></td>";
-	
+
 	private static String LIBRARYTHING_PATTERN_WORK_TITLE = "<span class=\"bookeditfield\" id=\"bookedit_title\"><b>([^<]*)</b></span>";
-	
+
 	private static String LIBRARYTHING_PATTERN_AUTHOR_LINK = "<td class=\"bookeditfield\" id=\"bookedit_authorunflip\">(.*)</td>";
 
 	private static String LIBRARYTHING_PATTERN_AUTHOR = "<h2>by <a href=\"/author/[^>]*>([^<]*)</a></h2>";
-	
+
 	private static String LIBRARYTHING_PATTERN_DATE = "<td class=\"bookeditfield\" id=\"bookedit_date\">([^<]*)</td>";
-	
+
 	private static String LIBRARYTHING_PATTERN_ISBN = "<td class=\"bookeditfield\" id=\"bookedit_ISBN\">([^<]*)</td>";
 
 	private static String LIBRARYTHING_PATTERN_WORK_AUTHOR_LINK = "<td class=\"left\">Author</td><td class=\"bookNonEditField\">(.*)</td>";
-	
+
 	private static String LIBRARYTHING_PATTERN_WORK_ISBN_10 = "<td class=\"left\">ISBN-10</td><td class=\"bookNonEditField\">([^<]*)</td>";
-	
+
 	private static String LIBRARYTHING_PATTERN_WORK_ISBN_13 = "<td class=\"left\">ISBN-13</td><td class=\"bookNonEditField\">([^<]*)</td>";
-	
+
+	final List<Tuple<Pattern, Pattern>> patterns = Collections.singletonList(new Tuple<Pattern, Pattern>(Pattern.compile(".*librarything\\..*"), UrlScraper.EMPTY_PATTERN));
+
 	private static final String PATTERN_LINK = "<a\\b[^>]*>([^<]*)</a>";
 
 	private String author = null;
@@ -72,52 +65,60 @@ public class LibrarythingScraper implements Scraper, UrlScraper {
 	private String year = null;
 	private String misc = null;
 	private String key = "librarything";
-	
+
 	/**
 	 * This Scraper works only with the following URL-prefixes and no selected text.
 	 * http://www.librarything.com/work-info/
 	 * http://www.librarything.com/work/
 	 */
-	public boolean scrape(ScrapingContext sc) throws ScrapingException {
-		if(sc != null && sc.getUrl() != null && supportsUrl(sc.getUrl())){
-			URL url = null;
-			
-			sc.setScraper(this);
-			
-			// build .com url			
-			if(!sc.getUrl().getHost().contains("librarything.com")){
-				String urlString = sc.getUrl().toString();
-				
-				// extract part bevor tld
-				int indexLibrarything = urlString.indexOf("librarything.");
-				String bevorTLD = urlString.substring(0, indexLibrarything + 13);
-				
-				//extract part after tld
-				urlString = urlString.substring(indexLibrarything+12);
-				int indexFirstSlash = urlString.indexOf("/");
-				String afterTLD = urlString.substring(indexFirstSlash);
-				
-				// build new .com url
-				try {
-					url = new URL(bevorTLD + "com" + afterTLD);
-				} catch (MalformedURLException e) {
-					throw new InternalFailureException(e);
-				}
-				
-			// is already a .com url
-			}else{
-				url = sc.getUrl();
+	protected boolean scrapeInternal(ScrapingContext sc) throws ScrapingException {
+		URL url = null;
+
+		sc.setScraper(this);
+
+		// build .com url			
+		if(!sc.getUrl().getHost().contains("librarything.com")){
+			String urlString = sc.getUrl().toString();
+
+			// extract part bevor tld
+			int indexLibrarything = urlString.indexOf("librarything.");
+			String bevorTLD = urlString.substring(0, indexLibrarything + 13);
+
+			//extract part after tld
+			urlString = urlString.substring(indexLibrarything+12);
+			int indexFirstSlash = urlString.indexOf("/");
+			String afterTLD = urlString.substring(indexFirstSlash);
+
+			// build new .com url
+			try {
+				url = new URL(bevorTLD + "com" + afterTLD);
+			} catch (MalformedURLException e) {
+				throw new InternalFailureException(e);
 			}
-			
-			String content = sc.getContentAsString(url);
-			
-			// extract data
-			Pattern authorPattern = Pattern.compile(LIBRARYTHING_PATTERN_AUTHOR);
-			Matcher authorMatcher = authorPattern.matcher(content);
+
+			// is already a .com url
+		}else{
+			url = sc.getUrl();
+		}
+
+		String content = sc.getContentAsString(url);
+
+		// extract data
+		Pattern authorPattern = Pattern.compile(LIBRARYTHING_PATTERN_AUTHOR);
+		Matcher authorMatcher = authorPattern.matcher(content);
+		if(authorMatcher.find()){
+			author = authorMatcher.group(1);
+		}else{
+			authorPattern = Pattern.compile(LIBRARYTHING_PATTERN_WORK_AUTHOR_LINK);
+			authorMatcher = authorPattern.matcher(content);
 			if(authorMatcher.find()){
-				author = authorMatcher.group(1);
+				author = authorMatcher.group();
+				Pattern linkPattern = Pattern.compile(PATTERN_LINK);
+				Matcher linkMatcher = linkPattern.matcher(author);
+				if(linkMatcher.find())
+					author = linkMatcher.group(1);
 			}else{
-				authorPattern = Pattern.compile(LIBRARYTHING_PATTERN_WORK_AUTHOR_LINK);
+				authorPattern = Pattern.compile(LIBRARYTHING_PATTERN_AUTHOR_LINK);
 				authorMatcher = authorPattern.matcher(content);
 				if(authorMatcher.find()){
 					author = authorMatcher.group();
@@ -125,112 +126,91 @@ public class LibrarythingScraper implements Scraper, UrlScraper {
 					Matcher linkMatcher = linkPattern.matcher(author);
 					if(linkMatcher.find())
 						author = linkMatcher.group(1);
-				}else{
-					authorPattern = Pattern.compile(LIBRARYTHING_PATTERN_AUTHOR_LINK);
-					authorMatcher = authorPattern.matcher(content);
-					if(authorMatcher.find()){
-						author = authorMatcher.group();
-						Pattern linkPattern = Pattern.compile(PATTERN_LINK);
-						Matcher linkMatcher = linkPattern.matcher(author);
-						if(linkMatcher.find())
-							author = linkMatcher.group(1);
-					}					
-				}
+				}					
 			}
-			
-			authorPattern = Pattern.compile(LIBRARYTHING_PATTERN_OTHER_AUTHORS);
-			authorMatcher = authorPattern.matcher(content);
-			if(authorMatcher.find()){
-				String otherAuthors = authorMatcher.group(1);
-				if(author == null && !otherAuthors.equals(""))
-					author = otherAuthors;
-				else if(!otherAuthors.equals(""))
-					author = author + " and " + authorMatcher.group(1);
-			}
+		}
 
-			
-			Pattern titlePattern = Pattern.compile(LIBRARYTHING_PATTERN_TITLE);
-			Matcher titleMatcher = titlePattern.matcher(content);
+		authorPattern = Pattern.compile(LIBRARYTHING_PATTERN_OTHER_AUTHORS);
+		authorMatcher = authorPattern.matcher(content);
+		if(authorMatcher.find()){
+			String otherAuthors = authorMatcher.group(1);
+			if(author == null && !otherAuthors.equals(""))
+				author = otherAuthors;
+			else if(!otherAuthors.equals(""))
+				author = author + " and " + authorMatcher.group(1);
+		}
+
+
+		Pattern titlePattern = Pattern.compile(LIBRARYTHING_PATTERN_TITLE);
+		Matcher titleMatcher = titlePattern.matcher(content);
+		if(titleMatcher.find()){
+			title = titleMatcher.group(1);
+		}else{
+			titlePattern = Pattern.compile(LIBRARYTHING_PATTERN_WORK_TITLE);
+			titleMatcher = titlePattern.matcher(content);
 			if(titleMatcher.find()){
 				title = titleMatcher.group(1);
-			}else{
-				titlePattern = Pattern.compile(LIBRARYTHING_PATTERN_WORK_TITLE);
-				titleMatcher = titlePattern.matcher(content);
-				if(titleMatcher.find()){
-					title = titleMatcher.group(1);
-				}
 			}
-			
-			Pattern yearPattern = Pattern.compile(LIBRARYTHING_PATTERN_DATE);
-			Matcher yearMatcher = yearPattern.matcher(content);
-			if(yearMatcher.find()){
-				year = yearMatcher.group(1);
-				key = key + year;
-			}
-
-			Pattern isbnPattern = Pattern.compile(LIBRARYTHING_PATTERN_ISBN);
-			Matcher isbnMatcher = isbnPattern.matcher(content);
-			if(isbnMatcher.find()){
-				misc = "isbn={" + isbnMatcher.group(1) + "}";
-			}else{
-				isbnPattern = Pattern.compile(LIBRARYTHING_PATTERN_WORK_ISBN_10);
-				isbnMatcher = isbnPattern.matcher(content);
-				if(isbnMatcher.find()){
-					if(misc == null)
-						misc = "isbn={" + isbnMatcher.group(1) + "}";
-					else
-						misc = misc + ", " + "isbn={" + isbnMatcher.group(1) + "}";
-				}
-				
-				isbnPattern = Pattern.compile(LIBRARYTHING_PATTERN_WORK_ISBN_13);
-				isbnMatcher = isbnPattern.matcher(content);
-				if(isbnMatcher.find()){
-					if(misc == null)
-						misc = "isbn={" + isbnMatcher.group(1) + "}";
-					else
-						misc = misc + ", " + "isbn={" + isbnMatcher.group(1) + "}";
-				}
-			}
-			
-			StringBuffer resultBibtex = new StringBuffer();
-			resultBibtex.append("@book{" + key + ",\n");			
-			
-			if(author != null)
-				resultBibtex.append("\tauthor = {" + author + "},\n");
-			if(title != null)
-				resultBibtex.append("\ttitle = {" + title + "},\n");
-			if(year != null)
-				resultBibtex.append("\tyear = {" + year + "},\n");
-			if(misc != null)
-				resultBibtex.append("\t" + misc + ",\n");
-			if(url != null)
-				resultBibtex.append("\turl = {" + url + "},\n");
-				
-			String bibResult = resultBibtex.toString();
-			bibResult = bibResult.substring(0, bibResult.length()-2) + "\n}\n";
-
-			sc.setBibtexResult(bibResult);
-			return true;
 		}
-		return false;
+
+		Pattern yearPattern = Pattern.compile(LIBRARYTHING_PATTERN_DATE);
+		Matcher yearMatcher = yearPattern.matcher(content);
+		if(yearMatcher.find()){
+			year = yearMatcher.group(1);
+			key = key + year;
+		}
+
+		Pattern isbnPattern = Pattern.compile(LIBRARYTHING_PATTERN_ISBN);
+		Matcher isbnMatcher = isbnPattern.matcher(content);
+		if(isbnMatcher.find()){
+			misc = "isbn={" + isbnMatcher.group(1) + "}";
+		}else{
+			isbnPattern = Pattern.compile(LIBRARYTHING_PATTERN_WORK_ISBN_10);
+			isbnMatcher = isbnPattern.matcher(content);
+			if(isbnMatcher.find()){
+				if(misc == null)
+					misc = "isbn={" + isbnMatcher.group(1) + "}";
+				else
+					misc = misc + ", " + "isbn={" + isbnMatcher.group(1) + "}";
+			}
+
+			isbnPattern = Pattern.compile(LIBRARYTHING_PATTERN_WORK_ISBN_13);
+			isbnMatcher = isbnPattern.matcher(content);
+			if(isbnMatcher.find()){
+				if(misc == null)
+					misc = "isbn={" + isbnMatcher.group(1) + "}";
+				else
+					misc = misc + ", " + "isbn={" + isbnMatcher.group(1) + "}";
+			}
+		}
+
+		StringBuffer resultBibtex = new StringBuffer();
+		resultBibtex.append("@book{" + key + ",\n");			
+
+		if(author != null)
+			resultBibtex.append("\tauthor = {" + author + "},\n");
+		if(title != null)
+			resultBibtex.append("\ttitle = {" + title + "},\n");
+		if(year != null)
+			resultBibtex.append("\tyear = {" + year + "},\n");
+		if(misc != null)
+			resultBibtex.append("\t" + misc + ",\n");
+		if(url != null)
+			resultBibtex.append("\turl = {" + url + "},\n");
+
+		String bibResult = resultBibtex.toString();
+		bibResult = bibResult.substring(0, bibResult.length()-2) + "\n}\n";
+
+		sc.setBibtexResult(bibResult);
+		return true;
 	}
-	
+
 	public String getInfo() {
 		return INFO;
 	}
 
-	public Collection<Scraper> getScraper() {
-		return Collections.singletonList((Scraper) this);
-	}
-	
 	public List<Tuple<Pattern, Pattern>> getUrlPatterns() {
-		List<Tuple<Pattern,Pattern>> list = new LinkedList<Tuple<Pattern,Pattern>>();
-		list.add(new Tuple<Pattern, Pattern>(Pattern.compile(".*librarything\\..*"), UrlScraper.EMPTY_PATTERN));
-		return list;
-	}
-
-	public boolean supportsUrl(URL url) {
-		return UrlMatchingHelper.isUrlMatch(url, this);
+		return patterns;
 	}
 
 }
