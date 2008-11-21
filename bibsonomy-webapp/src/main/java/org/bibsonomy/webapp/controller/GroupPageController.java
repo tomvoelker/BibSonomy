4660d4837ec92a73475848a61591b2734da2bf26
Robert Jäschke <jaeschke@l3s.de>
2008-11-21 11:21:56 +0100

    implemented group members in the proper way

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/GroupPageController.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/GroupPageController.java
@@ -2,15 +2,15 @@ package org.bibsonomy.webapp.controller;
 
 import java.util.List;
 
-import org.apache.log4j.Logger;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.bibsonomy.common.enums.FilterEntity;
 import org.bibsonomy.common.enums.GroupID;
 import org.bibsonomy.common.enums.GroupingEntity;
 import org.bibsonomy.model.Bookmark;
+import org.bibsonomy.model.Group;
 import org.bibsonomy.model.Resource;
-import org.bibsonomy.model.User;
 import org.bibsonomy.model.enums.Order;
-import org.bibsonomy.webapp.command.GroupMemberCommand;
 import org.bibsonomy.webapp.command.GroupResourceViewCommand;
 import org.bibsonomy.webapp.exceptions.MalformedURLSchemeException;
 import org.bibsonomy.webapp.util.MinimalisticController;
@@ -26,15 +26,15 @@ import org.bibsonomy.webapp.view.Views;
  * @version $Id$
  */
 public class GroupPageController extends SingleResourceListControllerWithTags implements MinimalisticController<GroupResourceViewCommand> {
-	private static final Logger LOGGER = Logger.getLogger(GroupPageController.class);
+	private static final Log log = LogFactory.getLog(GroupPageController.class);
 
 	public View workOn(GroupResourceViewCommand command) {
-		LOGGER.debug(this.getClass().getSimpleName());
+		log.debug(this.getClass().getSimpleName());
 		this.startTiming(this.getClass(), command.getFormat());
 
 		// if no group given -> error
 		if (command.getRequestedGroup() == null) {
-			LOGGER.error("Invalid query /group without roup name");
+			log.error("Invalid query /group without roup name");
 			throw new MalformedURLSchemeException("error.group_page_without_groupname");
 		}				
 
@@ -103,7 +103,7 @@ public class GroupPageController extends SingleResourceListControllerWithTags im
 			command.setPageTitle("group :: " + groupingName);	
 
 			this.setTags(command, Resource.class, groupingEntity, groupingName, null, null, null, null, 0, 1000, null);
-			this.setGroupMembers(command, groupingName);
+			this.setGroupDetails(command, groupingName);
 
 			if (requTags.size() > 0) {
 				this.setRelatedTags(command, Resource.class, groupingEntity, groupingName, null, requTags, Order.ADDED, 0, 20, null);
@@ -117,7 +117,7 @@ public class GroupPageController extends SingleResourceListControllerWithTags im
 			} else if(requTags.size() > 0){
 				return Views.GROUPTAGPAGE;	
 			} 
-			
+
 			return Views.GROUPPAGE;		
 		}		
 		this.endTiming();
@@ -131,18 +131,17 @@ public class GroupPageController extends SingleResourceListControllerWithTags im
 
 
 	/**
-	 * Retrieve all members of the given group in dependece of the group privacy level
+	 * Retrieve all members of the given group in dependence of the group privacy level
 	 * @param <V> extends ResourceViewCommand, the command
 	 * @param cmd the command
 	 * @param groupName the name of the group
 	 */
-	protected <V extends GroupResourceViewCommand> void setGroupMembers(V cmd, String groupName) {
-		GroupMemberCommand memberCommand = cmd.getMemberCommand();
-		List<User> members = this.logic.getUsers(groupName, 0, 100);
-		memberCommand.setGroup(groupName);
-		for (User u: members) {
-			memberCommand.addMember(u.getName());
+	protected <V extends GroupResourceViewCommand> void setGroupDetails(V cmd, String groupName) {
+		final Group group = this.logic.getGroupDetails(groupName);
+		if (group != null) {
+			group.setUsers(this.logic.getUsers(groupName, 0, 100));
 		}
+		cmd.setGroup(group);
 	}
 
 	/**
@@ -151,7 +150,7 @@ public class GroupPageController extends SingleResourceListControllerWithTags im
 	private boolean checkRelevantFor(List<String> tags){
 		for(String tag: tags){
 			if(tag.startsWith("sys:relevantFor:")){
-				LOGGER.debug("SYSTEMTAG 'sys:relevantFor:' found --> forward to the relevant-for View");
+				log.debug("SYSTEMTAG 'sys:relevantFor:' found --> forward to the relevant-for View");
 				return true;
 			}
 		}
