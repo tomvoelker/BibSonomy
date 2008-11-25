f3efde77929e2b0a8e51531645bfbe2f21e47655
BibSonomy <webmaster@bibsonomy.org>
2008-11-25 09:45:36 +0100

    added restarting of slave

--- bibsonomy-batch/src/build/batch_scripts/check_slave_status.pl
+++ bibsonomy-batch/src/build/batch_scripts/check_slave_status.pl
@@ -11,15 +11,20 @@ use DBI();
 # 2008-11-20 (rja)
 # - adapted to work with nagios instead of cron
 # - moved password from environment to ~/etc/check_slave_status.pass
+# 2008-11-25 (rja)
+# - script now tries to restart the slave, when it detect an unknown error
+#   for this, the SUPER privilege is necessary!
+#
 
 # maximal time the slave may be behind the master (in seconds)
-my $WARNING = 3;   # time in seconds 
-my $CRITICAL = 30; # time in seconds
+my $WARNING = 300;   # time in seconds 
+my $CRITICAL = 3600; # time in seconds
 
 # a user which has "REPLICATION CLIENT" privilege on the slave:
 # GRANT REPLICATION CLIENT ON *.* TO bibsonomy_status@'localhost' identified by '';
+# for restarting the slave, the SUPER privilege is also necessary
 my $user = "bibsonomy_status";
-my $password = `cat ~/etc/check_slave_status.pass`;
+my $password = `cat /home/kde/bibbackup/etc/check_slave_status.pass`;
 chomp ($password);
 
 my $seconds = -1;
@@ -51,7 +56,12 @@ if ($seconds > $CRITICAL) {
 }
 
 print "CRITICAL: unknown error occured - could not get slave status$perfdata";
+
+# try to restart slave
+$dbh = DBI->connect("DBI:mysql:;host=localhost:6033;mysql_socket=/var/mysql/run/mysqld.sock", $user, $password, {RaiseError => 1, AutoCommit => 0, "mysql_enable_utf8" => 1});
+$stm = $dbh->prepare("start slave");
+$stm->execute();
+$dbh->disconnect();
+
 exit 2;
-
-
 
