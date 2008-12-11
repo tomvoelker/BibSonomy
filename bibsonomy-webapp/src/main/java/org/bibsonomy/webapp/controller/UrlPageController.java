44c7004ca6b3a9cf5b6d357096e673dbdcba2108
Robert Jäschke <jaeschke@l3s.de>
2008-12-11 08:49:13 +0100

    added redirect handling

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/UrlPageController.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/UrlPageController.java
@@ -8,6 +8,7 @@ import org.bibsonomy.webapp.command.UrlCommand;
 import org.bibsonomy.webapp.exceptions.MalformedURLSchemeException;
 import org.bibsonomy.webapp.util.MinimalisticController;
 import org.bibsonomy.webapp.util.View;
+import org.bibsonomy.webapp.view.ExtendedRedirectView;
 import org.bibsonomy.webapp.view.Views;
 
 /**
@@ -19,7 +20,7 @@ import org.bibsonomy.webapp.view.Views;
  * 		<li>^/+url/*$ 
  *          --> error, need at least the url hash
  * 		<li>^/+url\?requestUrl=(.*)$
- *          --> should work as expected, maps to requestUrl
+ *          --> send redirect
  * 		<li>^/+url/+([0-9a-fA-F]+)(\?(.*))?$
  *          --> should work as expected, maps to requestUrlHash
  * </ul>
@@ -43,35 +44,35 @@ public class UrlPageController extends SingleResourceListController implements M
 			LOGGER.error("Invalid query /url without URL hash");
 			throw new MalformedURLSchemeException("error.url_no_hash");
 		}
-		
+
+		// send redirect to /url/HASH
+		if (ValidationUtils.present(command.getRequUrl())) {
+			// TODO: add format in front of /url/? (probably not, this URL should only be called by input form)
+			return new ExtendedRedirectView("/url/" + resources.Resource.hash(command.getRequUrl()));
+		}
+
 		// retrieve and set the requested resource lists
 		for (final Class<? extends Resource> resourceType : listsToInitialise) {
 			// disable manual setting of start value for homepage
 			command.getListCommand(resourceType).setStart(0);
 			
-			if (ValidationUtils.present(command.getRequUrl())) {
-				command.setRequUrlHash(resources.Resource.hash(command.getRequUrl()));
-			}
-			
 			setList(command, resourceType, GroupingEntity.ALL, null, null, command.getRequUrlHash(), null, null, null, 20);
+			// TODO: tags only for HTML output
 			setTags(command, resourceType, GroupingEntity.ALL, null, null, null, command.getRequUrlHash(), null, 0, 20, null);
 			postProcessAndSortList(command, resourceType);
 		}
 
 		if (ValidationUtils.present(command.getBookmark().getList())) {	
 			command.setPageTitle("url :: " + command.getBookmark().getList().get(0).getResource().getUrl() );
-		}
-		else {
+		} else {
 			command.setPageTitle("url ::");
 		}
-		
+
+		this.endTiming();
 		// html format - retrieve tags and return HTML view
 		if (command.getFormat().equals("html")) {
-			this.endTiming();
 			return Views.URLPAGE;	
 		}
-		
-		this.endTiming();
 		
 		// export - return the appropriate view
 		return Views.getViewByFormat(command.getFormat());				
