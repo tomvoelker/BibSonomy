5b5ff1d0c215c64e4dbce02231256745fad16ca8
Robert Jäschke <jaeschke@l3s.de>
2008-12-11 10:41:26 +0100

    +javadoc

--- bibsonomy-database/src/main/java/org/bibsonomy/database/params/TagSetParam.java
+++ bibsonomy-database/src/main/java/org/bibsonomy/database/params/TagSetParam.java
@@ -5,25 +5,49 @@ package org.bibsonomy.database.params;
  * @version $Id$
  */
 public class TagSetParam {
-	String setName;
-	String tagName;
-	int groupId;
+	private String setName;
+	private String tagName;
+	private int groupId;
 	
+	/** Get the name of the tag set. 
+	 * Each tag set has a name which uniquely identifies it among the groups tagsets.
+	 * 
+	 * @return The name of this tag set.
+	 */
 	public String getSetName() {
 		return this.setName;
 	}
+	
+	/** Set the name of the tag set.
+	 * Each tag set has a name which uniquely identifies it among the groups tagsets.
+	 * @param setName
+	 */
 	public void setSetName(String setName) {
 		this.setName = setName;
 	}
+	
+	/** Get the name of the tag associated with this tag set.
+	 * @return The name of the tag.
+	 */
 	public String getTagName() {
 		return this.tagName;
 	}
+	
+	/** Set the name of the tag associated with this tag set. 
+	 * @param tagName
+	 */
 	public void setTagName(String tagName) {
 		this.tagName = tagName;
 	}
+	/**
+	 * @return The id of the group this tag set belongs to.
+	 */
 	public int getGroupId() {
 		return this.groupId;
 	}
+	/**
+	 * @param groupId
+	 */
 	public void setGroupId(int groupId) {
 		this.groupId = groupId;
 	}
