2e0193423d01ef8d99bdbb95023919fcd4898aca
Thomas Steuber <steuber@cs.uni-kassel.de>
2009-01-12 14:22:54 +0100

    *** empty log message ***

--- bibsonomy-scraper/src/test/java/org/bibsonomy/scraper/UnitTestRunner.java
+++ bibsonomy-scraper/src/test/java/org/bibsonomy/scraper/UnitTestRunner.java
@@ -6,6 +6,7 @@ import java.util.List;
 import junit.framework.TestResult;
 
 import org.apache.log4j.PropertyConfigurator;
+import org.bibsonomy.scraper.URLTest.URLScraperUnitTest;
 import org.bibsonomy.scraper.importer.IUnitTestImporter;
 import org.bibsonomy.scraper.importer.xml.XMLUnitTestImporter;
 import org.junit.Ignore;
@@ -82,6 +83,35 @@ public class UnitTestRunner {
 		return false;
 	}
 	
+	/**
+	 * Runs a single URLSCraperUnitTest, which ist referenced by its ID and returns test and its results.
+	 * @param testId ID from URL test
+	 * @return scraped bibtex, null if scraping failed
+	 */
+	public URLScraperUnitTest getUrlUnitTest(String testId){
+		URL log4j = new UnitTestRunner().getClass().getResource("log4j.properties");
+		PropertyConfigurator.configure(log4j);
+		try {
+			if(importer == null)
+				throw new Exception("no UnitTestImporter available");
+			
+			List<ScraperUnitTest> unitTests = importer.getUnitTests();
+			
+			for(ScraperUnitTest test : unitTests){
+				if(test.getScraperTestId().equals(testId)){
+					TestResult result = test.run();
+					if(result.errorCount() > 0 || result.failureCount() > 0)
+						return (URLScraperUnitTest)test;
+					else
+						return (URLScraperUnitTest)test;
+				}
+			}
+			
+		} catch (Exception e) {
+			ParseFailureMessage.printParseFailureMessage(e, "main class");
+		}
+		return null;
+	}
 	
 	/**
 	 * starts the whole party
