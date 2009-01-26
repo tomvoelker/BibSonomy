afd57f203d802e58611e9c6ac0469239474145a7
Dominik Benz <benz@cs.uni-kassel.de>
2009-01-26 16:48:17 +0100

    added EscapingPrintWriter which ensures that output is free of control characters

---
+++ bibsonomy-rest-common/src/main/java/org/bibsonomy/rest/renderer/xml/tools/EscapingPrintWriter.java
@@ -0,0 +1,60 @@
+package org.bibsonomy.rest.renderer.xml.tools;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.Writer;
+
+import org.bibsonomy.util.StringUtils;
+
+/**
+ * PrintWriter which preprocesses all content to be printed/written 
+ * by replacing control characters (e.g., in order to yield valid
+ * XML). It uses hereby the method {@link StringUtils#escapeControlCharacters}}.
+ * 
+ * @see org.bibsonomy.util.StringUtils
+ * @see java.io.PrintWriter
+ * @author Dominik Benz
+ * @version $Id$
+ */
+public class EscapingPrintWriter extends Writer {
+		
+	/** PrintWriter */
+	private PrintWriter pw;
+	
+	/**
+	 * Create a new instance of an EscapingPrintWriter which is 
+	 * backed by a PrintWriter 
+	 * 
+	 * @param out an OutputStream
+	 */
+	public EscapingPrintWriter(OutputStream out) {
+		this.pw = new PrintWriter(out);
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.io.Writer#close()
+	 */
+	@Override
+	public void close() throws IOException {
+		this.pw.close();
+
+	}
+
+	/* (non-Javadoc)
+	 * @see java.io.Writer#flush()
+	 */
+	@Override
+	public void flush() throws IOException {
+		this.pw.flush();
+
+	}
+
+	/* (non-Javadoc)
+	 * @see java.io.Writer#write(char[], int, int)
+	 */
+	@Override
+	public void write(char[] cbuf, int off, int len) throws IOException {
+		this.pw.write(StringUtils.escapeControlCharacters(cbuf), off, len);
+	}
+}
