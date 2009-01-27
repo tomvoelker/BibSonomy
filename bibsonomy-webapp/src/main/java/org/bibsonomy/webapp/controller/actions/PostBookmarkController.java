8f064d664783b1b09afe2cbdffc69c9b99fe7de3
Robert Jäschke <jaeschke@l3s.de>
2009-01-27 15:57:40 +0100

    showing "all" tags of the user (i.e., 20k)

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/actions/PostBookmarkController.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/actions/PostBookmarkController.java
@@ -58,7 +58,7 @@ public class PostBookmarkController extends SingleResourceListController impleme
 	 */
 	private TagRecommender tagRecommender = null;
 
-	/** 
+	/**
 	 * Returns an instance of the command the controller handles.
 	 * 
 	 * @see org.bibsonomy.webapp.util.MinimalisticController#instantiateCommand()
@@ -70,14 +70,14 @@ public class PostBookmarkController extends SingleResourceListController impleme
 		 */
 		command.setGroups(new ArrayList<String>());
 		command.setRelevantGroups(new ArrayList<Tag>());
-		command.setRelevantTagSets(new HashMap<String, Map<String,List<String>>>());
+		command.setRelevantTagSets(new HashMap<String, Map<String, List<String>>>());
 		command.setRecommendedTags(new TreeSet<RecommendedTag>());
 		command.setCopytags(new ArrayList<Tag>());
 		/*
 		 * initialize post & resource
 		 */
 		command.setPost(new Post<Bookmark>());
-		command.getPost().setResource(new Bookmark());		
+		command.getPost().setResource(new Bookmark());
 		command.setAbstractGrouping("public");
 
 		/*
@@ -87,12 +87,13 @@ public class PostBookmarkController extends SingleResourceListController impleme
 		return command;
 	}
 
-	/** Main method which does the postBookmark-procedure.
-	 *
-	 * FIXME: for:group is missing
-	 * FIXME: if the bookmark is scrapable the bibtex-string will be put into the request (or use scraping service)
-	 * FIXME: check for other features from BookmarkShowHandler 
-	 *
+	/**
+	 * Main method which does the postBookmark-procedure.
+	 * 
+	 * FIXME: for:group is missing FIXME: if the bookmark is scrapable the
+	 * bibtex-string will be put into the request (or use scraping service)
+	 * FIXME: check for other features from BookmarkShowHandler
+	 * 
 	 * @see org.bibsonomy.webapp.util.MinimalisticController#workOn(java.lang.Object)
 	 */
 	public View workOn(EditBookmarkCommand command) {
@@ -103,30 +104,30 @@ public class PostBookmarkController extends SingleResourceListController impleme
 		command.setPageTitle("post a new bookmark");
 
 		/*
-		 * only users which are logged in might post bookmarks 
-		 * -> send them to login page
+		 * only users which are logged in might post bookmarks -> send them to
+		 * login page
 		 */
 		if (!context.isUserLoggedIn()) {
 			/*
 			 * FIXME: We need to add the ?referer= parameter such that the user
-			 * is send back to this controller after login.
-			 * This is not so simple, because we cannot access the query path
-			 * and for POST requests we would need to build the parameters by
-			 * ourselves. 
+			 * is send back to this controller after login. This is not so
+			 * simple, because we cannot access the query path and for POST
+			 * requests we would need to build the parameters by ourselves.
 			 */
 			return new ExtendedRedirectView("/login");
 		}
 
 		final User loginUser = context.getLoginUser();
 		final Post<Bookmark> post = command.getPost();
-		
+
 		/*
-		 * set the user of the post to the loginUser (the recommender might need the user name)
+		 * set the user of the post to the loginUser (the recommender might need
+		 * the user name)
 		 */
 		post.setUser(loginUser);
 
 		/*
-		 * initialize groups 
+		 * initialize groups
 		 */
 		initPostGroups(command, post);
 
@@ -135,10 +136,10 @@ public class PostBookmarkController extends SingleResourceListController impleme
 		 */
 		final String intraHashToUpdate = command.getIntraHashToUpdate();
 		if (ValidationUtils.present(intraHashToUpdate)) {
-		
+
 			log.debug("intra hash to update found -> handling update of existing post");
 			return handleUpdatePost(command, context, loginUser, post, intraHashToUpdate);
-		
+
 		}
 
 		log.debug("no intra hash given -> new post");
@@ -147,19 +148,22 @@ public class PostBookmarkController extends SingleResourceListController impleme
 	}
 
 	/**
-	 * This methods does everything which needs to be done before proceeding to the 
-	 * {@link Views#POST_BOOKMARK} view. This includes:
+	 * This methods does everything which needs to be done before proceeding to
+	 * the {@link Views#POST_BOOKMARK} view. This includes:
 	 * <ul>
 	 * <li>initializing the group tag sets</li>
 	 * <li>getting the recommended tags</li>
 	 * <li>getting the tag cloud of the user</li>
 	 * </ul>
-	 * Thus, never return the view directly, but use this method! 
+	 * Thus, never return the view directly, but use this method!
 	 * 
 	 * TODO: candidate for refactoring (e.g., abstract class PostController)
 	 * 
-	 * @param command - the command the controller is working on (and which is also handed over to the view).
-	 * @param loginUser - the login user.
+	 * @param command -
+	 *            the command the controller is working on (and which is also
+	 *            handed over to the view).
+	 * @param loginUser -
+	 *            the login user.
 	 * @return The {@link Views#POST_BOOKMARK} view.
 	 */
 	private View getPostBookmarkView(final EditBookmarkCommand command, final User loginUser) {
@@ -171,21 +175,22 @@ public class PostBookmarkController extends SingleResourceListController impleme
 		/*
 		 * get the recommended tags for the post from the command
 		 */
-		if (tagRecommender != null)	command.setRecommendedTags(tagRecommender.getRecommendedTags(command.getPost()));
+		if (tagRecommender != null) command.setRecommendedTags(tagRecommender.getRecommendedTags(command.getPost()));
 
 		/*
-		 * get the tag cloud of the user
-		 * (this must be done before any error checking, because the user must have this)
-		 * FIXME: get ALL tags of the user!
+		 * get the tag cloud of the user (this must be done before any error
+		 * checking, because the user must have this) FIXME: get ALL tags of the
+		 * user!
 		 */
-		this.setTags(command, Resource.class, GroupingEntity.USER, loginUser.getName(), null, null, null, null, 0, 100, null);
+		this.setTags(command, Resource.class, GroupingEntity.USER, loginUser.getName(), null, null, null, null, 0, 20000, null);
 		/*
 		 * return the view
 		 */
 		return Views.POST_BOOKMARK;
 	}
-	
-	/** Handles the update of an existing post with the given intra hash. 
+
+	/**
+	 * Handles the update of an existing post with the given intra hash.
 	 * 
 	 * @param command
 	 * @param context
@@ -201,7 +206,8 @@ public class PostBookmarkController extends SingleResourceListController impleme
 		if (!context.isValidCkey()) {
 			log.debug("no valid ckey found -> assuming first call, populating form");
 			/*
-			 * ckey is invalid, so this is probably the first call --> get post from DB
+			 * ckey is invalid, so this is probably the first call --> get post
+			 * from DB
 			 */
 			final Post<Bookmark> dbPost = (Post<Bookmark>) logic.getPostDetails(intraHashToUpdate, loginUser.getName());
 			if (dbPost == null) {
@@ -210,7 +216,7 @@ public class PostBookmarkController extends SingleResourceListController impleme
 				 */
 				errors.reject("errors.post.notfound");
 				return Views.ERROR;
-			} 
+			}
 			/*
 			 * put post into command
 			 */
@@ -222,7 +228,7 @@ public class PostBookmarkController extends SingleResourceListController impleme
 		}
 		log.debug("ckey given, so parse tags, validate post, update post");
 		/*
-		 * ckey is given, so user is already editing the post  -> parse tags
+		 * ckey is given, so user is already editing the post -> parse tags
 		 */
 		try {
 			post.setTags(parse(command.getTags()));
@@ -240,7 +246,7 @@ public class PostBookmarkController extends SingleResourceListController impleme
 		post.getResource().recalculateHashes();
 		if (!intraHashToUpdate.equals(post.getResource().getIntraHash())) {
 			/*
-			 * URL has changed -> check, if new URL already bookmarked 
+			 * URL has changed -> check, if new URL already bookmarked
 			 */
 			final Post<Bookmark> dbPost = (Post<Bookmark>) logic.getPostDetails(post.getResource().getIntraHash(), loginUser.getName());
 			if (dbPost != null) {
@@ -260,7 +266,7 @@ public class PostBookmarkController extends SingleResourceListController impleme
 			return getPostBookmarkView(command, loginUser);
 		}
 		/*
-		 * the post to update has the given intra hash 
+		 * the post to update has the given intra hash
 		 */
 		post.getResource().setIntraHash(command.getIntraHashToUpdate());
 		/*
@@ -269,10 +275,10 @@ public class PostBookmarkController extends SingleResourceListController impleme
 		final List<Post<?>> posts = new LinkedList<Post<?>>();
 		posts.add(post);
 		/*
-		 * update date 
-		 * TODO: don't we want to keep the posting date unchanged and only update the date?
-		 * --> actually, this does currently not work, since the DBLogic doesn't set the date
-		 * and thus we get a NPE from the database
+		 * update date TODO: don't we want to keep the posting date unchanged
+		 * and only update the date? --> actually, this does currently not work,
+		 * since the DBLogic doesn't set the date and thus we get a NPE from the
+		 * database
 		 */
 		post.setDate(new Date());
 		/*
@@ -293,18 +299,24 @@ public class PostBookmarkController extends SingleResourceListController impleme
 		return finalRedirect(command.isJump(), loginUser.getName(), post.getResource().getUrl());
 	}
 
-	/** Create the final redirect after successful creating / updating a post.
-	 * if jump is <code>true</code>, we redirect to the given postUrl. Otherwise
+	/**
+	 * Create the final redirect after successful creating / updating a post. if
+	 * jump is <code>true</code>, we redirect to the given postUrl. Otherwise
 	 * we redirect to the user's page.
 	 * 
-	 * @param jump - indicates whether to jump to postUrl (<code>true</code>) or to the user's page.
-	 * @param userName - the name of the loginUser
-	 * @param postUrl - the URL of the post 
+	 * @param jump -
+	 *            indicates whether to jump to postUrl (<code>true</code>)
+	 *            or to the user's page.
+	 * @param userName -
+	 *            the name of the loginUser
+	 * @param postUrl -
+	 *            the URL of the post
 	 * @return
 	 */
 	private View finalRedirect(final boolean jump, final String userName, final String postUrl) {
 		/*
-		 * if bookmark was posted by bookmarklet (jump = true) -> redirect to URL user is coming from 
+		 * if bookmark was posted by bookmarklet (jump = true) -> redirect to
+		 * URL user is coming from
 		 */
 		if (!jump) {
 			try {
@@ -313,14 +325,15 @@ public class PostBookmarkController extends SingleResourceListController impleme
 				log.error("Could not encode redirect URL.", ex);
 				errors.reject("error encoding URL");
 				return Views.ERROR;
-			} 
+			}
 		}
 		return new ExtendedRedirectView(postUrl);
 	}
 
 	private View handleCreatePost(EditBookmarkCommand command, final RequestWrapperContext context, final User loginUser, final Post<Bookmark> post) {
 		/*
-		 * no intra hash given --> user posts a new entry (which might already exist!)
+		 * no intra hash given --> user posts a new entry (which might already
+		 * exist!)
 		 */
 
 		/*
@@ -331,17 +344,18 @@ public class PostBookmarkController extends SingleResourceListController impleme
 		if (dbPost != null) {
 			log.debug("user already owns this URL ... handling update");
 			/*
-			 * TODO: here we could check, if the user has marked a checkbox which says "I want to update this post"
-			 * and only then set the intraHashToUpdate
+			 * TODO: here we could check, if the user has marked a checkbox
+			 * which says "I want to update this post" and only then set the
+			 * intraHashToUpdate
 			 */
 			/*
 			 * post exists -> warn user
 			 */
 			errors.rejectValue("post.resource.url", "error.field.valid.url.alreadybookmarked");
 			/*
-			 * the next time this will be handled as an update
-			 * TODO: this does not make sense, since we don't show the user the existing post
-			 * (actually: again, we can't exchange the post ... :-( ) 
+			 * the next time this will be handled as an update TODO: this does
+			 * not make sense, since we don't show the user the existing post
+			 * (actually: again, we can't exchange the post ... :-( )
 			 */
 			command.setIntraHashToUpdate(post.getResource().getIntraHash());
 			/*
@@ -368,7 +382,7 @@ public class PostBookmarkController extends SingleResourceListController impleme
 			log.warn("error parsing tags", e);
 			errors.rejectValue("tags", "error.field.valid.tags.parseerror");
 		}
-		
+
 		/*
 		 * return to form until validation passes
 		 */
@@ -379,10 +393,10 @@ public class PostBookmarkController extends SingleResourceListController impleme
 		}
 
 		/*
-		 * check credentials to fight CSRF attacks
-		 * We do this that late to not cause the error message pop up 
-		 * on the first call to the controller. Otherwise, the form would
-		 * be empty and the hidden ckey field not sent.
+		 * check credentials to fight CSRF attacks We do this that late to not
+		 * cause the error message pop up on the first call to the controller.
+		 * Otherwise, the form would be empty and the hidden ckey field not
+		 * sent.
 		 */
 		if (!context.isValidCkey()) {
 			errors.reject("error.field.valid.ckey");
@@ -392,7 +406,7 @@ public class PostBookmarkController extends SingleResourceListController impleme
 		/*
 		 * TODO: why is the content id set?
 		 */
-//			post.setContentId(null);
+		// post.setContentId(null);
 		post.setDate(new Date());
 
 		/*
@@ -406,12 +420,15 @@ public class PostBookmarkController extends SingleResourceListController impleme
 		log.debug("finally: creating a new post in the DB");
 		final String createPosts = logic.createPosts(posts).get(0);
 		log.debug("created post: " + createPosts);
-		
+
 		return finalRedirect(command.isJump(), loginUser.getName(), post.getResource().getUrl());
 	}
 
-	/** Populates the command with the given post. Ensures, that fields which depend on the post
-	 * (like the tag string, or the groups) in the command are correctly filled.
+	/**
+	 * Populates the command with the given post. Ensures, that fields which
+	 * depend on the post (like the tag string, or the groups) in the command
+	 * are correctly filled.
+	 * 
 	 * @param command
 	 * @param dbPost
 	 */
@@ -431,44 +448,52 @@ public class PostBookmarkController extends SingleResourceListController impleme
 	}
 
 	/**
-	 * Copy the groups from the command into the post (make proper groups from them)
+	 * Copy the groups from the command into the post (make proper groups from
+	 * them)
 	 * 
 	 * TODO: candidate for refactoring (e.g., abstract class PostController)
 	 * 
-	 * @param command - contains the groups as represented by the form fields.
-	 * @param post - the post whose groups should be populated from the command.
+	 * @param command -
+	 *            contains the groups as represented by the form fields.
+	 * @param post -
+	 *            the post whose groups should be populated from the command.
 	 * @see #initCommandGroups(EditBookmarkCommand, Post)
 	 */
 	private void initPostGroups(final EditBookmarkCommand command, final Post<Bookmark> post) {
 		/*
-		 * we can avoid some checks here, because they're done in the validator ...
+		 * we can avoid some checks here, because they're done in the validator
+		 * ...
 		 */
 		final Set<Group> postGroups = post.getGroups();
 		final String abstractGrouping = command.getAbstractGrouping();
-		if ("other".equals(abstractGrouping)){
+		if ("other".equals(abstractGrouping)) {
 			/*
 			 * copy groups into post
 			 */
 			final List<String> groups = command.getGroups();
-			for (final String groupname: groups){
+			for (final String groupname : groups) {
 				postGroups.add(new Group(groupname));
 			}
 		} else {
 			/*
-			 * if the post is private or public --> remove all groups and add one (private or public)
+			 * if the post is private or public --> remove all groups and add
+			 * one (private or public)
 			 */
 			postGroups.clear();
 			postGroups.add(new Group(abstractGrouping));
 		}
 	}
 
-
-	/** Populates the helper attributes of the command with the groups from the post.
+	/**
+	 * Populates the helper attributes of the command with the groups from the
+	 * post.
 	 * 
 	 * TODO: candidate for refactoring (e.g., abstract class PostController)
 	 * 
-	 * @param command - the command whose groups should be populated from the post.
-	 * @param post - the post which contains the groups.
+	 * @param command -
+	 *            the command whose groups should be populated from the post.
+	 * @param post -
+	 *            the post which contains the groups.
 	 * @see #initPostGroups(EditBookmarkCommand, Post)
 	 */
 	private void initCommandGroups(final EditBookmarkCommand command, Set<Group> groups) {
@@ -493,7 +518,7 @@ public class PostBookmarkController extends SingleResourceListController impleme
 			/*
 			 * copy groups into command
 			 */
-			for (final Group group: groups) {
+			for (final Group group : groups) {
 				commandGroups.add(group.getName());
 			}
 		}
@@ -501,25 +526,24 @@ public class PostBookmarkController extends SingleResourceListController impleme
 		log.debug("commandGroups: " + command.getGroups());
 	}
 
-
-
-	/** Gets the tagsets for each group from the DB and stores them in the 
-	 * users group list.
+	/**
+	 * Gets the tagsets for each group from the DB and stores them in the users
+	 * group list.
 	 * 
 	 * TODO: candidate for refactoring (e.g., abstract class PostController)
 	 * 
 	 * @param loginUser
 	 */
 	private void initGroupTagSets(final User loginUser) {
-		/* 
-		 * Get tagsets for each group and add them to the loginUser object.
-		 * Why into the loginUser? Because there we already have the groups 
-		 * the user is member of.
+		/*
+		 * Get tagsets for each group and add them to the loginUser object. Why
+		 * into the loginUser? Because there we already have the groups the user
+		 * is member of.
 		 */
 		final List<Group> usersGroups = loginUser.getGroups();
 		final ArrayList<Group> groupsWithTagSets = new ArrayList<Group>();
-		for(final Group group: usersGroups){
-			if(group.getName() != null){
+		for (final Group group : usersGroups) {
+			if (group.getName() != null) {
 				groupsWithTagSets.add(this.logic.getGroupDetails(group.getName()));
 			}
 		}
@@ -527,8 +551,8 @@ public class PostBookmarkController extends SingleResourceListController impleme
 
 	}
 
-
-	/** Concatenates the tags into one tag string to show them to the user.
+	/**
+	 * Concatenates the tags into one tag string to show them to the user.
 	 * 
 	 * TODO: candidate for refactoring (e.g., abstract class PostController)
 	 * 
@@ -537,20 +561,20 @@ public class PostBookmarkController extends SingleResourceListController impleme
 	 */
 	private String getSimpleTagString(final Set<Tag> tags) {
 		final StringBuffer s = new StringBuffer();
-		for (final Tag tt: tags) {
+		for (final Tag tt : tags) {
 			s.append(tt.getName() + " ");
 		}
 		return s.toString();
 	}
 
-
-	/** Parses the incoming tag string into a set of tags.
+	/**
+	 * Parses the incoming tag string into a set of tags.
 	 * 
 	 * TODO: candidate for refactoring (e.g., abstract class PostController)
 	 * 
 	 * @param tagString
 	 * @return
-	 * @throws RecognitionException 
+	 * @throws RecognitionException
 	 */
 	private Set<Tag> parse(final String tagString) throws RecognitionException {
 		final Set<Tag> tags = new TreeSet<Tag>();
@@ -566,7 +590,7 @@ public class PostBookmarkController extends SingleResourceListController impleme
 			 * parse
 			 */
 			parser.tagstring();
-		}		
+		}
 		return tags;
 	}
 
@@ -582,7 +606,10 @@ public class PostBookmarkController extends SingleResourceListController impleme
 		this.errors = errors;
 	}
 
-	/** Sets the tag recommender this controller uses to provide tag recommendations during posting.
+	/**
+	 * Sets the tag recommender this controller uses to provide tag
+	 * recommendations during posting.
+	 * 
 	 * @param tagRecommender
 	 */
 	public void setTagRecommender(TagRecommender tagRecommender) {
