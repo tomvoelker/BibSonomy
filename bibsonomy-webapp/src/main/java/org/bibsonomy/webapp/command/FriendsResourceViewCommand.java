70016bab0c51d6f4a4a99bfabe3abffd349132e2
steffen <devnull@localhost>
2009-02-07 11:34:12 +0100

    update for friends page

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/FriendsResourceViewCommand.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/FriendsResourceViewCommand.java
@@ -1,5 +1,9 @@
 package org.bibsonomy.webapp.command;
 
+import java.util.List;
+
+import org.bibsonomy.model.User;
+
 /**
  * @author Steffen
  * @version $Id$
@@ -7,6 +11,8 @@ package org.bibsonomy.webapp.command;
 public class FriendsResourceViewCommand extends TagResourceViewCommand {
 	/** the group whode resources are requested*/
 	private ConceptsCommand concepts = new ConceptsCommand();
+	private List<User> userFriends;
+	private List<User> friendsOfUser;
 	
 	/**
 	 * @return
@@ -20,6 +26,35 @@ public class FriendsResourceViewCommand extends TagResourceViewCommand {
 	 */
 	public void setConcepts(ConceptsCommand concepts) {
 		this.concepts = concepts;
+	}
+
+	/**
+	 * @param userFriends
+	 */
+	public void setUserFriends(List<User> userFriends) {
+		this.userFriends = userFriends;
+	}
+
+	/**
+	 * @param friendsOfUser
+	 */
+	public void setFriendsOfUser(List<User> friendsOfUser) {
+		this.friendsOfUser = friendsOfUser;
+		
+	}
+
+	/**
+	 * @return friends of the user
+	 */
+	public List<User> getFriendsOfUser() {
+		return friendsOfUser;
+	}
+
+	/**
+	 * @return the users friends
+	 */
+	public List<User> getUserFriends() {
+		return userFriends;
 	}
 	
 }
