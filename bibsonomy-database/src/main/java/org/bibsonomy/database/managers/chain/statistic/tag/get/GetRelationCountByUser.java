7acffe4f0a75aa1fe7d197e0ae65032feb837a41
voigtmannc <devnull@localhost>
2009-02-10 12:43:50 +0100

    Moved class name from GetRelationByUserCount to GetRelationCountByUser

---
+++ bibsonomy-database/src/main/java/org/bibsonomy/database/managers/chain/statistic/tag/get/GetRelationCountByUser.java
@@ -0,0 +1,32 @@
+package org.bibsonomy.database.managers.chain.statistic.tag.get;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bibsonomy.database.managers.chain.statistic.StatisticChainElement;
+import org.bibsonomy.database.params.StatisticsParam;
+import org.bibsonomy.database.util.DBSession;
+
+/**
+ * Returns the number of relations for the current user.
+ * 
+ * @author Christian
+ * @version $Id$
+ */
+public class GetRelationCountByUser extends StatisticChainElement{
+
+	@Override
+	protected boolean canHandle(StatisticsParam param) {
+		
+		return true;
+	}
+
+	@Override
+	protected List<Integer> handle(StatisticsParam param, DBSession session) {
+		List<Integer> counts = new ArrayList<Integer>();
+		counts.add(this.db.getNumberOfRelationsForUser(param, session));
+		
+		return counts;
+	}
+
+}
