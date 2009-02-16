1aba3828fabfcafdf548d0520cd78e62b5732e4b
Robert Jäschke <jaeschke@l3s.de>
2009-02-16 11:57:03 +0100

    new, working code

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/view/LayoutView.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/view/LayoutView.java
@@ -1,5 +1,6 @@
 package org.bibsonomy.webapp.view;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
@@ -8,24 +9,29 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-//import org.bibsonomy.layout.AbstractLayoutRenderer;
+import org.bibsonomy.layout.Layout;
+import org.bibsonomy.layout.LayoutRenderer;
 import org.bibsonomy.model.BibTex;
 import org.bibsonomy.model.Bookmark;
 import org.bibsonomy.model.Post;
+import org.bibsonomy.model.Resource;
 import org.bibsonomy.webapp.command.SimpleResourceViewCommand;
 import org.springframework.web.servlet.view.AbstractView;
 
+import tags.Functions;
+
 /**
- * View which uses an {@link AbstractLayoutRenderer} to render the output.
+ * View which uses an {@link LayoutRenderer} to render the output.
  * 
  * @author rja
  * @version $Id$
+ * @param <LAYOUT> 
  */
-public class LayoutView extends AbstractView {
+public class LayoutView<LAYOUT extends Layout> extends AbstractView {
 	private static final Log log = LogFactory.getLog(LayoutView.class);
-	
-//	private AbstractLayoutRenderer layoutRenderer;
-	
+
+	private LayoutRenderer<LAYOUT> layoutRenderer;
+
 	@Override
 	protected void renderMergedOutputModel(final Map model, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
 		/*
@@ -33,11 +39,20 @@ public class LayoutView extends AbstractView {
 		 * FIXME: we transfer this by the UrlRewriteFilter using request attributes 
 		 * ... probably this is not a good idea 
 		 */
-		final String layout = (String) request.getAttribute("layout");
-		log.debug("rendering layout " + layout);
+		final String layout   = (String) request.getAttribute("layout");
+		/*
+		 * get the requested path
+		 * we need it to generate the file names for inline content-disposition
+		 * FIXME: The path is written into the request by the UrlRewriteFilter 
+		 * ... probably this is not a good idea
+		 */
+		final String requPath = (String) request.getAttribute("requPath");
+		
+		log.info("rendering layout " + layout + " for path " + requPath);
 
 		/*
 		 * get the data
+		 * FIXME: constant for "command" - where to get?
 		 */
 		final Object object = model.get("command");
 		if (object instanceof SimpleResourceViewCommand) {
@@ -46,21 +61,28 @@ public class LayoutView extends AbstractView {
 			/*
 			 * check which list (resource type) needs to be rendered
 			 */
-			final List<Post<Bookmark>> bookmarkPosts = command.getBookmark().getList();
+			final List<Post<Bookmark>> bookmarkPosts  = command.getBookmark().getList();
 			final List<Post<BibTex>> publicationPosts = command.getBibtex().getList();
-			if (bookmarkPosts != null && bookmarkPosts.size() > 0) {
+
+			try {
+				if (bookmarkPosts != null && bookmarkPosts.size() > 0 && layoutRenderer.supportsResourceType(Bookmark.class)) {
+					/*
+					 * render bookmarks posts
+					 */
+					renderResponse(layout, requPath, bookmarkPosts, loginUserName, response);
+				} else if (publicationPosts != null && publicationPosts.size() > 0 && layoutRenderer.supportsResourceType(BibTex.class)) {
+					/*
+					 * render publication posts
+					 */
+					renderResponse(layout, requPath, publicationPosts, loginUserName, response);
+				} else {
+					/*
+					 * FIXME: what to do here?
+					 */
+				}
+			} catch (final Exception e) {
 				/*
-				 * render bookmarks posts
-				 */
-//				getLayoutRenderer().renderResponse(layout, bookmarkPosts, loginUserName, response);
-			} else if (publicationPosts != null && publicationPosts.size() > 0) {
-				/*
-				 * render publication posts
-				 */
-//				getLayoutRenderer().renderResponse(layout, publicationPosts, loginUserName, response);
-			} else {
-				/*
-				 * FIXME: what to do here?
+				 * give user useful feedback, why rendering failed
 				 */
 			}
 		}
@@ -69,21 +91,47 @@ public class LayoutView extends AbstractView {
 		 */
 
 	}
-//
-//	/**
-//	 * @return The layout renderer for this view.
-//	 */
-//	public AbstractLayoutRenderer getLayoutRenderer() {
-//		return this.layoutRenderer;
-//	}
-//
-//	/** Set the layout renderer to be used by this view.
-//	 * 
-//	 * @param layoutRenderer
-//	 */
-//	public void setLayoutRenderer(AbstractLayoutRenderer layoutRenderer) {
-//		log.debug("using layout renderer " + layoutRenderer);
-//		this.layoutRenderer = layoutRenderer;
-//	}
+	
+	private <T extends Resource> void renderResponse(final String layoutName, final String requPath, final List<Post<T>> posts, final String loginUserName, final HttpServletResponse response) throws IOException {
+		
+		final LAYOUT layout = layoutRenderer.getLayout(layoutName, loginUserName);
+		
+		log.info("got layout " + layout);
+		
+		/*
+		 * set the content type headers
+		 */				
+		response.setContentType(layout.getMimeType());
+		response.setCharacterEncoding("UTF-8");
+		final String extension = layout.getExtension();
+		/*
+		 * If an extension is given, which is differrent from ".html", this suggests to the 
+		 * browser to show a file dialog.
+		 * 
+		 * FIXME: add this ".html" check
+		 */
+		if (extension != null && !extension.trim().equals("") && !extension.equals(".html")) {
+			response.setHeader("Content-Disposition", "attachement; filename=" + Functions.makeCleanFileName(requPath) + extension);
+		}
+		/*
+		 * do the real rendering
+		 */
+		layoutRenderer.renderLayout(layout, posts, response.getOutputStream());
+	}
+
+	/**
+	 * @return The layout renderer for this view.
+	 */
+	public LayoutRenderer<LAYOUT> getLayoutRenderer() {
+		return this.layoutRenderer;
+	}
+
+	/** Set the layout renderer to be used by this view.
+	 * 
+	 * @param layoutRenderer
+	 */
+	public void setLayoutRenderer(final LayoutRenderer<LAYOUT> layoutRenderer) {
+		this.layoutRenderer = layoutRenderer;
+	}
 
 }
