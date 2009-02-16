511cc9a3a0947935469b1ba002313cd188461944
Robert Jäschke <jaeschke@l3s.de>
2009-02-16 11:56:50 +0100

    removed old codee

--- bibsonomy-webapp/src/main/java/servlets/LayoutHandler.java
+++ bibsonomy-webapp/src/main/java/servlets/LayoutHandler.java
@@ -1,15 +1,10 @@
 package servlets;
 
-import filters.SessionSettingsFilter;
 import helpers.MultiPartRequestParser;
 import helpers.database.DBLayoutManager;
-import helpers.export.bibtex.ExportBibtex;
-import helpers.export.bibtex.ExportBibtex.LayoutType;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Collection;
-import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.ServletConfig;
@@ -21,14 +16,14 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileUploadException;
 import org.apache.log4j.Logger;
+import org.bibsonomy.layout.jabref.JabrefLayoutRenderer;
+import org.bibsonomy.layout.jabref.JabrefLayoutUtils;
+import org.bibsonomy.layout.jabref.LayoutPart;
 import org.bibsonomy.util.StringUtils;
 
-import resources.Bibtex;
 import servlets.listeners.InitialConfigListener;
-import tags.Functions;
-import beans.ResourceBean;
 import beans.UploadBean;
-import beans.UserBean;
+import filters.SessionSettingsFilter;
 
 public class LayoutHandler extends HttpServlet { 
 
@@ -37,26 +32,34 @@ public class LayoutHandler extends HttpServlet {
 
 	private static String documentPath = null;
 	private static String rootPath = null;
-	private static final ExportBibtex export = ExportBibtex.getInstance();
-	private static HashMap<String,OutputType> typemap = new HashMap<String,OutputType>();
 
+	/**
+	 * An instance of the (new!) layout renderer. We need it here to unload custom
+	 * user layouts.
+	 */
+	private JabrefLayoutRenderer jabrefLayoutRenderer = JabrefLayoutRenderer.getInstance(); 
 
 	public void init(ServletConfig config) throws ServletException {	
 		super.init(config); 
 		rootPath = InitialConfigListener.getInitParam("rootPath");
 		documentPath = rootPath + "bibsonomy_docs/";
-		initTypeMap(); // initialize mapping of type names to content-type/file-ending 
 	}
 
+	/** 
+	 * Deletes the specified layout.
+	 * 
+	 * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
 	public void doGet(HttpServletRequest request, HttpServletResponse response)	throws ServletException, IOException {
-		/* Get the session attribute of current user  */
-		UserBean user = SessionSettingsFilter.getUser(request);
-		String currUser = user.getName(); 
-
-		String action = request.getParameter("action");
+		final String currUser = SessionSettingsFilter.getUser(request).getName(); 
+		final String action = request.getParameter("action");
 
 		if ("delete".equals(action) && currUser != null) {
-			String hash = request.getParameter("hash");
+			/*
+			 * FIXME: not needed any longer (at least for the renderer, because
+			 * it unloads ALL layouts of the user).
+			 */
+			final String hash = request.getParameter("hash");
 			/* 
 			 * delete entry in database
 			 */
@@ -65,69 +68,24 @@ public class LayoutHandler extends HttpServlet {
 				 * delete file from filesystem
 				 */
 				new File(documentPath + hash.substring(0,2) + "/" + hash).delete();
-				//delete layout object from exporter, if necessary
-				export.unloadCustomFilter(hash);
+				/*
+				 * delete layout object from exporter
+				 */
+				jabrefLayoutRenderer.unloadUserLayout(currUser);
 			}
 			response.sendRedirect("/settings#layout");
-		} else {
-			/*
-			 * format bibtex list for chosen layout
-			 */
-
-			String layout = (String) request.getAttribute("layout");
-			OutputType t = typemap.get(layout.toLowerCase());
-			if (t != null) {
-				StringBuffer exportStringBuffer = null;
-				try {
-					/*
-					 * get bibtex list - either with or without duplicates, depending on users choice
-					 */
-					final ResourceBean resourceBean = ((ResourceBean) request.getAttribute("ResourceBean"));
-					final Collection<Bibtex> bibtexList;
-					if ("no".equals(request.getParameter("duplicates"))) {
-						bibtexList = resourceBean.getBibtexSortedByYear();
-					} else {
-						bibtexList = resourceBean.getBibtex();
-					}
-					exportStringBuffer = export.exportBibtex(bibtexList, currUser, layout);
-				} catch (Exception e) {
-					/*
-					 * loading of layout failed: send error message to user
-					 */
-					log.warn("could not load layout for user " + currUser + ": " + e);
-					request.setAttribute("error", "Sorry, I was not able to load the layout: " + e.getMessage());
-					getServletConfig().getServletContext().getRequestDispatcher("/errors/error.jsp").forward(request, response);
-					return;
-				}
-				/*
-				 * set header
-				 */
-				response.setContentType(t.contentType);
-				response.setCharacterEncoding("UTF-8");
-				if (t.ending != null) {
-					response.setHeader("Content-Disposition", "attachement; filename=" + Functions.makeCleanFileName((String)request.getAttribute("requPath")) + "." + t.ending);
-				}
-				/*
-				 * send output to user
-				 */
-				response.getOutputStream().write(exportStringBuffer.toString().getBytes("UTF-8"));
-				response.getOutputStream().close();
-
-			} else {
-				/*
-				 * could not find layout in map for content types
-				 */
-				request.setAttribute("error", "Sorry, I was not able to load the layout " + layout + ".");
-				getServletConfig().getServletContext().getRequestDispatcher("/errors/error.jsp").forward(request, response);
-			}
 		}
 	}
 
 
+	/**
+	 * Handles upload of custom layout filters.
+	 * 
+	 * @see javax.servlet.http.HttpServlet#doPost(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		UserBean user = SessionSettingsFilter.getUser(request);
-		String currUser = user.getName(); 
+		final String currUser = SessionSettingsFilter.getUser(request).getName(); 
 
 		if (currUser == null) {
 			response.sendRedirect("/login");
@@ -142,10 +100,10 @@ public class LayoutHandler extends HttpServlet {
 		 * Map<String, FileItem> fieldMap = MultiPartRequestParser.getFields(request,rootPath);
 		 */
 		try {
-			MultiPartRequestParser parser = new MultiPartRequestParser(); 
-			Map<String, FileItem> fieldMap = parser.getFields(request, rootPath);
+			final MultiPartRequestParser parser = new MultiPartRequestParser(); 
+			final Map<String, FileItem> fieldMap = parser.getFields(request, rootPath);
 
-			for (String layoutType:LayoutType.getLayoutTypes()) {
+			for (String layoutType:LayoutPart.getLayoutTypes()) {
 
 				// retrieve form field "file"
 				FileItem file = (FileItem) fieldMap.get("file." + layoutType);
@@ -163,7 +121,7 @@ public class LayoutHandler extends HttpServlet {
 					}
 
 					// get hashed name
-					String hashedName = ExportBibtex.userLayoutHash(currUser, LayoutType.getLayoutType(layoutType));
+					String hashedName = JabrefLayoutUtils.userLayoutHash(currUser, LayoutPart.getLayoutType(layoutType));
 
 					// build path from first two letters of file name hash
 					String docPath = documentPath + hashedName.substring(0, 2).toLowerCase();
@@ -171,7 +129,8 @@ public class LayoutHandler extends HttpServlet {
 					/* *************************************************
 					 * save file and insert data into database
 					 * *************************************************/
-					File inputFile = new File(docPath, hashedName);			
+					final File inputFile = new File(docPath, hashedName);
+					log.debug("writing user layout file to " + inputFile);
 
 					if (DBLayoutManager.insertLayout(currUser, hashedName, fileName)) {
 						file.write(inputFile);	
@@ -189,54 +148,6 @@ public class LayoutHandler extends HttpServlet {
 			bean.setErrors("file", "Your upload failed: " + e);
 			request.setAttribute("upBean", bean);
 			getServletConfig().getServletContext().getRequestDispatcher("/upload_error").forward(request, response);		
-		}
-	}
-
-	private void initTypeMap() {
-		/*
-		 * add default layout types (from JabRef 2.2) TODO: this needs to be updated,
-		 * when layout directory is updated! 
-		 * 
-		 * TODO: put this into a properties file or something like that
-		 * 
-		 * TODO: if you add/change something here, you have to also add/change this on exports.jsp! 
-		 * 
-		 * TODO: please rewrite this using a configuration file! changing sourcecode is awkward
-		 * 
-		 *  NOTE: case is ignored
-		 */
-		typemap.put("html",                new OutputType(null, "text/html"));
-		typemap.put("tablerefs",           new OutputType(null, "text/html"));
-		typemap.put("tablerefsabsbib",     new OutputType(null, "text/html"));
-		typemap.put("tablerefsabsbibsort", new OutputType(null, "text/html"));
-		typemap.put("simplehtml",          new OutputType(null, "text/html"));
-		typemap.put("custom",              new OutputType(null, "text/html"));
-
-		typemap.put("bibtexml",            new OutputType(null, "text/xml"));
-		typemap.put("docbook",             new OutputType(null, "text/xml"));
-		typemap.put("dblp",                new OutputType(null, "text/xml"));
-
-		typemap.put("harvard",             new OutputType("rtf", "text/rtf"));
-
-		typemap.put("endnote",             new OutputType(null, "text/plain"));
-		typemap.put("text",                new OutputType(null, "text/plain"));
-		
-		typemap.put("openoffice-csv",      new OutputType("csv", "text/comma-separated-values"));
-		
-		// additional layouts, installed by us
-		typemap.put("se",          	       new OutputType(null, "text/html"));
-		typemap.put("jucs", 		       new OutputType("rtf", "text/rtf"));
-		typemap.put("imtm", 		       new OutputType(null, "text/html"));
-		typemap.put("din1505",             new OutputType(null, "text/html"));
-	}
-
-	private static class OutputType {
-		public String ending;
-		public String contentType;
-		public OutputType(String ending, String contentType) {
-			super();
-			this.ending = ending;
-			this.contentType = contentType;
 		}
 	}
 
