570866dab3ff5b0fcb81933d556a3c4eda54a792
Robert Jäschke <jaeschke@l3s.de>
2009-02-19 13:51:15 +0100

    new test class for post statistics chain

---
+++ bibsonomy-database/src/test/java/org/bibsonomy/database/managers/chain/statistic/post/PostStatisticChainTest.java
@@ -0,0 +1,60 @@
+package org.bibsonomy.database.managers.chain.statistic.post;
+
+import static org.bibsonomy.util.ValidationUtils.present;
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.bibsonomy.common.enums.ConstantID;
+import org.bibsonomy.common.enums.GroupID;
+import org.bibsonomy.common.enums.GroupingEntity;
+import org.bibsonomy.database.managers.chain.AbstractChainTest;
+import org.bibsonomy.database.managers.chain.statistic.post.get.GetResourcesForHashCount;
+import org.bibsonomy.model.Bookmark;
+import org.bibsonomy.model.Post;
+import org.bibsonomy.model.enums.Order;
+import org.junit.Test;
+
+/**
+ * Tests related to the bookmark chain.
+ * 
+ * @author Miranda Grahl
+ * @version $Id$
+ */
+public class PostStatisticChainTest extends AbstractChainTest {
+
+	/**
+	 * tests getBookmarkByConceptForUser
+	 */
+//	@Test
+//	public void getBookmarkByConceptForUser() {
+//		this.bookmarkParam.setGrouping(GroupingEntity.USER);
+//		this.bookmarkParam.setHash(null);
+//		this.bookmarkParam.setOrder(null);
+//		this.bookmarkParam.setRequestedUserName("hotho");
+//		this.bookmarkParam.setNumSimpleConcepts(3);
+//		this.bookmarkParam.setNumSimpleTags(0);
+//		this.bookmarkParam.setNumTransitiveConcepts(0);
+//		this.bookmarkChain.getFirstElement().perform(this.bookmarkParam, this.dbSession, this.chainStatus);
+//		assertEquals(GetBookmarksByConceptForUser.class, this.chainStatus.getChainElement().getClass());
+//	}
+
+	/**
+	 * tests getBibtexByHash
+	 */
+	@Test
+	public void getResourcesForHashCount() {
+		this.statisticsParam.setContentType(ConstantID.BIBTEX_CONTENT_TYPE);
+		this.statisticsParam.setHash("d9eea4aa159d70ecfabafa0c91bbc9f0");
+		this.statisticsParam.setGrouping(GroupingEntity.ALL);
+		this.statisticsParam.setRequestedUserName(null);
+		this.statisticsParam.setTagIndex(null);
+		this.statisticsParam.setOrder(null);
+		this.statisticsParam.setSearch(null);
+		
+		final List<Integer> counts = this.postStatisticsChain.getFirstElement().perform(this.statisticsParam, this.dbSession, this.chainStatus);
+		assertEquals(2, counts.get(0));
+		assertEquals(GetResourcesForHashCount.class, this.chainStatus.getChainElement().getClass());
+	}
+
+}
