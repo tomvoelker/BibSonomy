b3356f6030bbdefe590377ded64c4743a66e80fe
steffen <devnull@localhost>
2009-02-25 16:05:00 +0100

    update for friend page

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/FriendPageController.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/FriendPageController.java
@@ -3,14 +3,10 @@ package org.bibsonomy.webapp.controller;
 import java.util.List;
 
 import org.apache.log4j.Logger;
-import org.bibsonomy.common.enums.ConceptStatus;
 import org.bibsonomy.common.enums.FilterEntity;
 import org.bibsonomy.common.enums.GroupingEntity;
 import org.bibsonomy.common.enums.Role;
-import org.bibsonomy.model.Bookmark;
 import org.bibsonomy.model.Resource;
-import org.bibsonomy.model.Tag;
-import org.bibsonomy.model.enums.Order;
 import org.bibsonomy.webapp.command.FriendResourceViewCommand;
 import org.bibsonomy.webapp.exceptions.MalformedURLSchemeException;
 import org.bibsonomy.webapp.util.MinimalisticController;
@@ -41,41 +37,16 @@ public class FriendPageController extends SingleResourceListControllerWithTags i
 
 		FilterEntity filter = null;
 
-		// display only posts which have a document attached
-//		if (command.getFilter().equals("myPDF")) {
-//			filter = FilterEntity.JUST_PDF;
-//		}
-
-		// display duplicate entries
-//		if (command.getFilter().equals("myDuplicates")) {
-//			filter = FilterEntity.DUPLICATES;
-//		}
-
 		// determine which lists to initalize depending on the output format
 		// and the requested resourcetype
 		this.chooseListsToInitialize(command.getFormat(), command.getResourcetype());
 
-//		if (filter == FilterEntity.JUST_PDF || filter == FilterEntity.DUPLICATES) {
-//			this.listsToInitialise.remove(Bookmark.class);
-//		}
-
-//		Integer totalNumPosts = 1;
 
 		// retrieve and set the requested resource lists, along with total
 		// counts
 		for (final Class<? extends Resource> resourceType : listsToInitialise) {
 			this.setList(command, resourceType, groupingEntity, groupingName, requTags, null, null, filter, null, command.getListCommand(resourceType).getEntriesPerPage());
 			this.postProcessAndSortList(command, resourceType);
-
-//			if (filter != FilterEntity.JUST_PDF && filter != FilterEntity.DUPLICATES) {
-//				// int totalCount = this.logic.getStatistics(resourceType,
-//				// groupingEntity, groupingName, null, null, requTags);
-//				int start = command.getListCommand(resourceType).getStart();
-//				int totalCount = this.logic.getPostStatistics(resourceType, groupingEntity, groupingName, requTags, null, null, filter, start, start + command.getListCommand(resourceType).getEntriesPerPage(), null, null);
-//
-//				command.getListCommand(resourceType).setTotalCount(totalCount);
-//				totalNumPosts += totalCount;
-//			}
 		}
 		// set page title
 		command.setPageTitle("friend :: " + groupingName);
@@ -88,19 +59,7 @@ public class FriendPageController extends SingleResourceListControllerWithTags i
 			if (command.getTagcloud().getTags().size() > 19999) {
 				LOGGER.error("User " + groupingName + " has reached threshold of 20000 tags on friend page");
 			}
-
-//			if (requTags.size() > 0) {
-//				this.setRelatedTags(command, Resource.class, groupingEntity, groupingName, null, requTags, Order.ADDED, 0, 20, null);
-//				command.getRelatedTagCommand().setTagGlobalCount(totalNumPosts);
-//				this.endTiming();
-//
-//				// forward to bibtex page if filter is set
-//				if (filter == FilterEntity.JUST_PDF || filter == FilterEntity.DUPLICATES) {
-//					return Views.USERDOCUMENTPAGE;
-//				}
-				
-				//return Views.FRIENDPAGE;
-//			}
+			
 			/*
 			 * add user details to command, if loginUser is admin
 			 */
@@ -109,11 +68,6 @@ public class FriendPageController extends SingleResourceListControllerWithTags i
 			}
 
 			this.endTiming();
-			/*
-			// forward to bibtex page if filter is set
-			if (filter == FilterEntity.JUST_PDF || filter == FilterEntity.DUPLICATES) {
-				return Views.USERDOCUMENTPAGE;
-			} */
 			return Views.FRIENDPAGE; 
 		}
 		this.endTiming();
