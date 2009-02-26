36f841b004c73e1d462e40931014e66370d39acd
steffen <devnull@localhost>
2009-02-26 15:08:42 +0100

    update friends page

---
+++ bibsonomy-database/src/main/java/org/bibsonomy/database/managers/chain/tag/get/GetTagsByFriendOfUser.java
@@ -0,0 +1,34 @@
+package org.bibsonomy.database.managers.chain.tag.get;
+
+import static org.bibsonomy.util.ValidationUtils.present;
+
+import java.util.List;
+
+import org.bibsonomy.common.enums.GroupingEntity;
+import org.bibsonomy.database.managers.chain.tag.TagChainElement;
+import org.bibsonomy.database.params.TagParam;
+import org.bibsonomy.database.util.DBSession;
+import org.bibsonomy.model.Tag;
+
+/**
+ * Returns a list of tags for a given friend of a given user.
+ * 
+ * @author Steffen Kress
+ * @version $Id$
+ */
+public class GetTagsByFriendOfUser extends TagChainElement {
+	@Override
+	protected List<Tag> handle(final TagParam param, final DBSession session) {
+		if (param.getTagIndex() != null && param.getTagIndex().size() > 0) {
+			// retrieve related tags
+			return this.db.getRelatedTagsForUser(param.getUserName(), param.getRequestedUserName(), param.getTagIndex(), param.getGroups(), session);
+		}
+		// retrieve all tags from friend that are visible to his friends
+		return this.db.getTagsByFriendOfUser(param, session);
+	}
+
+	@Override
+	protected boolean canHandle(final TagParam param) {
+		return (param.getGrouping() == GroupingEntity.FRIEND && present(param.getRequestedUserName()) && !present(param.getRegex()) && !present(param.getHash()));
+	}
+}
