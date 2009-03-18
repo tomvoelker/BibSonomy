f6a15090827a450de1dba1ba115fef6280a2d738
Robert Jäschke <jaeschke@l3s.de>
2009-03-18 10:47:53 +0100

    new script which uses separate JAR files instead of the FAT JAR

--- bibsonomy-scraper/src/main/resources/org/bibsonomy/scraper/ie/training/training.sh
+++ bibsonomy-scraper/src/main/resources/org/bibsonomy/scraper/ie/training/training.sh
@@ -26,4 +26,34 @@ echo
 echo "If a bad version error oucccurs during starting the training, the used java ist not version 5. Change path to the correct java version."
 echo
 
-java -Xmx1400m -jar mallet.jar --head-or-ref 1 --input-file testdata/tagged_ie_references_newreference_start_newline_meta_reference_count_cluster_count.txt --use-cluster-features false --training-pct 0.7 --testing-pct 0.3 --output-prefix crfBibsonomy --num-reps 100 1> training_log.txt 2> training_err_log.txt
+
+#
+# location of local Maven repository
+#
+MV=/home/rja/.m2/repository
+
+# 
+# paths of JARs of libraries relative to Maven repository 
+# 
+SCRAPER=org/bibsonomy/bibsonomy-scraper/2.0.1/bibsonomy-scraper-2.0.1.jar
+MALLET=edu/umass/cs/mallet/mallet/0.4-jaeschke/mallet-0.4-jaeschke.jar
+BSH=bsh/bsh/1.3.0/bsh-1.3.0.jar
+
+JARS=$MV/$SCRAPER:$MV/$MALLET:$MV/$BSH
+
+
+MAIN=edu.umass.cs.mallet.util.bibsonomy.TUI_IE_CRF
+
+JAVA=$JAVA_HOME/bin/java
+
+$JAVA -Xmx1400m \
+    -cp $JARS \
+    $MAIN \
+    --head-or-ref 1 \
+    --input-file testdata/tagged_ie_references_newreference_start_newline_meta_reference_count_cluster_count.txt \
+    --use-cluster-features false \
+    --training-pct 0.7 \
+    --testing-pct 0.3 \
+    --output-prefix crfBibsonomy \
+    --num-reps 100 \
+    1> training_log.txt 2> training_err_log.txt
