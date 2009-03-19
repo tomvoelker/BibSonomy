1416c0ca4429577398d08df4027f0624618f8e0e
Dominik Benz <benz@cs.uni-kassel.de>
2009-03-19 14:26:34 +0100

    corrected display of counts for bibtex entries

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/BibtexPageController.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/BibtexPageController.java
@@ -4,14 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
-import org.bibsonomy.common.enums.ConceptStatus;
 import org.bibsonomy.common.enums.GroupingEntity;
-import org.bibsonomy.common.enums.ResourceType;
 import org.bibsonomy.model.BibTex;
 import org.bibsonomy.model.Post;
-import org.bibsonomy.model.Resource;
-import org.bibsonomy.model.Tag;
-import org.bibsonomy.model.User;
 import org.bibsonomy.webapp.command.BibtexResourceViewCommand;
 import org.bibsonomy.webapp.exceptions.MalformedURLSchemeException;
 import org.bibsonomy.webapp.util.MinimalisticController;
@@ -19,7 +14,12 @@ import org.bibsonomy.webapp.util.View;
 import org.bibsonomy.webapp.view.Views;
 
 /**
- * @author mwa
+ * Controller for bibtex pages:
+ *   - /bibtex/HASH
+ *   - /bibtex/HASH/USERNAME
+ * 
+ * @author Michael Wagner
+ * @author Dominik Benz, benz@cs.uni-kassel.de 
  * @version $Id$
  */
 public class BibtexPageController extends SingleResourceListControllerWithTags implements MinimalisticController<BibtexResourceViewCommand>{
@@ -31,75 +31,104 @@ public class BibtexPageController extends SingleResourceListControllerWithTags i
 		LOGGER.debug(this.getClass().getSigners());
 		this.startTiming(this.getClass(), command.getFormat());
 		
-		//if no hash given -> error
-		if(command.getRequBibtex().length() == 0){
+		/*
+		 * if no hash given -> error
+		 */
+		if(command.getRequBibtex() == null || command.getRequBibtex().length() == 0){
 			LOGGER.error("Invalid query /bibtex without hashvalue");
 			throw new MalformedURLSchemeException("error.bibtex_no_hash");
 		}
-
+		
+		/*
+		 * Set hash, username, grouping entity
+		 */
 		final String hash     = command.getRequBibtex();
 		final String requUser = command.getRequestedUser();
 		final GroupingEntity groupingEntity = (requUser != null ? GroupingEntity.USER : GroupingEntity.ALL);
 
-		// handle case when only tags are requested
-		// retrieve only 1000 tags for this resource
-		// FIXME: hardcoded end value
+		/* 
+		 * handle case when only tags are requested
+		 * retrieve only 1000 tags for this resource
+		 * FIXME: hardcoded end value
+		 */
 		command.setResourcetype("bibtex");
 		this.handleTagsOnly(command, groupingEntity, requUser, null, null, hash, null, 0, 1000, null);
 		
-		// determine which lists to initalize depending on the output format 
-		// and the requested resourcetype
-		this.chooseListsToInitialize(command.getFormat(), command.getResourcetype());
-		
-		// retrieve and set the requested bibtex(s)
-		final int entriesPerPage = command.getListCommand(BibTex.class).getEntriesPerPage();
-		
+		/*
+		 * retrieve and set the requested bibtex(s)
+		 */
+		final int entriesPerPage = command.getListCommand(BibTex.class).getEntriesPerPage();		
 		this.setList(command, BibTex.class, groupingEntity, requUser, null, hash, null, null, null, entriesPerPage);
+				
+		/* 
+		 * retrieve total count with given hash 
+		 * (only for /bibtex/HASH)
+		 */
+		if (GroupingEntity.ALL.equals(groupingEntity)) {
+			this.setTotalCount(command, BibTex.class, groupingEntity, requUser, null, hash, null, null, null, entriesPerPage, null);
+		}
+		
+		/*
+		 * complete post details for a single post of a given user 
+		 * (only for /bibtex/HASH/USER)
+		 */
+		if (GroupingEntity.USER.equals(groupingEntity)) {
+			final ArrayList<Post<BibTex>> bibtex = new ArrayList<Post<BibTex>>();
+			for (Post<BibTex> b : command.getBibtex().getList()){
+				bibtex.add((Post<BibTex>) this.logic.getPostDetails(b.getResource().getIntraHash(), b.getUser().getName()));
+			}			
+			command.getBibtex().setList(bibtex);			
+		}
+		
+		/*
+		 * post process and sort list (e.g., insert open URL)
+		 */
 		this.postProcessAndSortList(command, BibTex.class);
 		
-		// only set total count if no user is given (is always 1 otherwise)
-		if (GroupingEntity.ALL.equals(groupingEntity)) {
-			this.setTotalCount(command, BibTex.class, groupingEntity, requUser, null, hash, null, null, null, command.getListCommand(BibTex.class).getEntriesPerPage(), null);
-		}		
+		/*
+		 * extract first bibtex; if list is empty, return blank page
+		 */
+		final BibTex firstBibtex;
+		if (command.getBibtex().getList() != null && command.getBibtex().getList().size() > 0){
+			firstBibtex = command.getBibtex().getList().get(0).getResource();			
+		}
 		else {
-			/*
-			 * complete post details for a single post of a given user (only for
-			 * /bibtex/HASH/USER
-			 */
-			final ArrayList<Post<BibTex>> bibtex = new ArrayList<Post<BibTex>>();
-			for (Post<BibTex> b: command.getBibtex().getList()){
-				bibtex.add((Post<BibTex>) this.logic.getPostDetails(b.getResource().getIntraHash(), b.getUser().getName()));
-			}
-			command.getBibtex().setList(bibtex);
-
+			return (GroupingEntity.USER.equals(groupingEntity) ? Views.BIBTEXDETAILS : Views.BIBTEXPAGE);
 		}
 		
-		// get the title of the publication with the requested hash
-		final List<Post<BibTex>> bibtexList = command.getBibtex().getList();
-		if (bibtexList != null && bibtexList.size() > 0){
-			command.setTitle(bibtexList.get(0).getResource().getTitle());
-		}
+		/*
+		 * Set page title to title of first publication 
+		 */
+		command.setTitle(firstBibtex.getTitle());
 		
 		if (command.getFormat().equals("html")) {
 			this.endTiming();
 			
-			command.setPageTitle("bibtex :: " + hash );
+			command.setPageTitle("bibtex :: " + command.getTitle() );
 			
-			if (GroupingEntity.USER.equals(groupingEntity)) {
-				//bibtex/HASH/USER
-				
-				// fetch other users who have tagged this publication				
-				List<Post<BibTex>> allPosts = this.logic.getPosts(BibTex.class, GroupingEntity.ALL, null, null, bibtexList.get(0).getResource().getInterHash(), null, null, 0, 1000, null);
+			if (GroupingEntity.USER.equals(groupingEntity)) { //bibtex/HASH/USER
+				/*
+				 * fetch posts of all users with the given hash, add users to related
+				 * users list				
+				 */
+				List<Post<BibTex>> allPosts = this.logic.getPosts(BibTex.class, GroupingEntity.ALL, null, null, firstBibtex.getInterHash(), null, null, 0, 1000, null);
 				for (Post<BibTex> post : allPosts) {
 					command.getRelatedUserCommand().getRelatedUsers().add(post.getUser());
 				}
-								
+				
+				/* 
+				 * set "correct" count .This is the number of ALL users having the publication
+				 * with the interHash of firstBibtex in their collection. In allPosts, only public posts
+				 * are contained, hence it can be smaller.
+				 */
+				this.setTotalCount(command, BibTex.class, GroupingEntity.ALL, null, null, firstBibtex.getInterHash(), null, null, null, entriesPerPage, null);
+				firstBibtex.setCount(command.getBibtex().getTotalCount());
+												
 				/*
 				 * show tags by all users for this resource; the ones by the given user
 				 * will be highlighted later
 				 * FIXME: hardcoded end value
 				 */
-				//this.setTags(command, Resource.class, groupingEntity, requUser, null, tagsList, null, null, 0, Integer.MAX_VALUE, null);
 				this.setTags(command, BibTex.class, GroupingEntity.ALL, null, null, null, hash, null, 0, 1000, null);
 				return Views.BIBTEXDETAILS;
 			}
