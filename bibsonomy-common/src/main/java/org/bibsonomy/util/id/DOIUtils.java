c0bfe0be13d29802062a0eacd7767827540da7c6
Robert Jäschke <jaeschke@l3s.de>
2009-03-26 17:59:40 +0100

    added DOI utils together with extensive tests (which need to be extended!)

---
+++ bibsonomy-common/src/main/java/org/bibsonomy/util/id/DOIUtils.java
@@ -0,0 +1,152 @@
+package org.bibsonomy.util.id;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.bibsonomy.util.WebUtils;
+
+/**
+ * Helper methods to simplify <a href="http://www.doi.org/">DOI</a>s (digital object identifiers).
+ *  
+ * @author rja
+ * @version $Id$
+ */
+public class DOIUtils {
+
+	/**
+	 * Host name of the DOI resolver. 
+	 */
+	public static final String DX_DOI_ORG = "dx.doi.org";
+	/**
+	 * URL of the DOI resolver.
+	 */
+	public static final String DX_DOI_ORG_URL = "http://" + DX_DOI_ORG + "/";
+
+	private static final Pattern HOST_PATTERN = Pattern.compile(".*" + DX_DOI_ORG);
+	
+	private static final String NON_NUMBERS_OR_LETTERS = "[^0-9\\p{L}]*?";
+	
+	
+	/**
+	 * The first variant is too strict, according to 
+	 *  
+	 * http://stackoverflow.com/questions/27910/finding-a-doi-in-a-document-or-page 
+	 * 
+	 * Changes:
+	 * <ul>
+	 * <li>added "doi:" as possible prefix</li>
+	 * <li>replaced the last \d by [^\s"'], thus allowing everything but whitespace and some
+	 * "closing" characters (which are in principle allowed but makes detection of the end of a 
+	 * DOI almost impossible). We in particular disallow "}", because it is at the end of BibTeX 
+	 * DOIs (which we otherwise would extract wrong).</li>
+	 * </ul>
+	 * 
+	 * 
+	 */
+//	private static final String DOI = "(10\\.\\d+\\/\\d+?)";
+	private static final String DOI = "(doi:)?(10\\.\\d+\\/[^\\s\"'}]+)";
+	private static final String DOI_END = "[\\s\"'}]*";
+	
+	/**
+	 * Matches a pure DOI. Disregards case. 
+	 */
+	private static final Pattern DOI_PATTERN = Pattern.compile("^" + DOI + "$", Pattern.CASE_INSENSITIVE);
+
+	/**
+	 * Before the DOI we allow everything but numbers or letters (i.e., whitespace, symbols, etc.)
+	 * Since the DOI might include almost every character (in principle) we stop when we find the
+	 * first non-matching symbol.
+	 */
+	private static final Pattern STRICT_DOI_PATTERN = Pattern.compile("^" + NON_NUMBERS_OR_LETTERS + DOI + DOI_END + NON_NUMBERS_OR_LETTERS + "$", Pattern.CASE_INSENSITIVE);
+	private static final Pattern SLOPPY_DOI_PATTERN = Pattern.compile(".*?" + DOI + DOI_END + ".*", Pattern.CASE_INSENSITIVE);
+
+	/** Checks, if the given URL is a DOI URL (i.e., points to dx.doi.org) 
+	 * 
+	 * @param url
+	 * @return <code>true</code> if the URL points to the DOI resolver.
+	 */
+	public static boolean isDOIURL(final URL url) {
+		return url != null && HOST_PATTERN.matcher(url.getHost()).find();
+	}
+	
+	
+	/**
+	 * Resolves DOI to a URL using the official DOI resolver {@value #DX_DOI_ORG}.
+	 * 
+	 * @param doi DOI as String
+	 * @return URL from the referenced DOI resource, null if resolve failed
+	 */
+	public static URL getUrlForDoi(final String doi) {
+		try {
+			return WebUtils.getRedirectUrl(getURL(doi));
+		} catch (MalformedURLException ex) {
+			return null;
+		}
+	}
+
+
+	/**
+	 * Creates a URL for the given DOI which points to the DOI resolver.
+	 * 
+	 * @param doi - the DOI which the URL should resolve
+	 * @return A URL pointing to the DOI resolver.
+	 * 
+	 * @throws MalformedURLException
+	 */
+	public static URL getURL(final String doi) throws MalformedURLException {
+		return new URL(DOIUtils.DX_DOI_ORG_URL + doi);
+	}
+
+	/**
+	 * Extracts a DOI from the given string.
+	 * 
+	 * @param string
+	 * @return The extracted DOI.
+	 */
+	public static String extractDOI(final String string) {
+		if (string != null) {
+			final Matcher matcher = SLOPPY_DOI_PATTERN.matcher(string);
+			if (matcher.find()) {
+				return matcher.group(2);
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * Checks, if the given string contains a DOI.
+	 * This method checks, if <strong>somewhere</strong> in the string something
+	 * like a DOI can be found. If you want to check, if the string contains almost
+	 * only a DOI, use {@link #containsOnlyDOI(String)}.
+	 * 
+	 * @param string
+	 * @return <code>true</code>, if the given string contains a DOI.
+	 */
+	public static boolean containsDOI(final String string) {
+		return string != null && SLOPPY_DOI_PATTERN.matcher(string).find();
+	}
+	
+	/**
+	 * Checks, if the given string contains (almost only) a DOI.
+	 * 
+	 * @param string
+	 * @return <code>true</code>, if the given string contains almost only the DOI, i.e.,
+	 * no surrounding text (only whitespace or symbols like ",", "'", ")", etc.)
+	 */
+	public static boolean containsOnlyDOI(final String string) {
+		return string != null && STRICT_DOI_PATTERN.matcher(string).find();
+	}
+	
+	/**
+	 * Checks, if the given string represents a DOI.
+	 * 
+	 * @param doi
+	 * @return <code>true</code>, if the given string is a DOI
+	 */
+	public static boolean isDOI(final String doi) {
+		return doi != null && DOI_PATTERN.matcher(doi).find();
+	}
+ 
+}
---
+++ bibsonomy-common/src/test/java/org/bibsonomy/util/id/DOIUtilsTest.java
@@ -0,0 +1,209 @@
+package org.bibsonomy.util.id;
+
+import static org.junit.Assert.fail;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Random;
+
+import junit.framework.Assert;
+
+import org.bibsonomy.util.WebUtils;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * @author rja
+ * @version $Id$
+ */
+public class DOIUtilsTest {
+
+	private static final Random rand = new Random();
+
+	private static final String bibtexWithDoi = "@article{Pevzner02,\n" + 
+	"address = {Cambridge, MA, USA},\n" +
+	"author = {Lev Pevzner and Marti A. Hearst},\n" +
+	"interHash = {707f8d31137f6d39edd1d54664351d0d},\n" +
+	"intraHash = {330a11348556280054ec5fceeb23649c},\n" +
+	"journal = {Comput. Linguist.},\n" +
+	"number = {1},\n" +
+	"pages = {19--36},\n" +
+	"publisher = {MIT Press},\n" +
+	"title = {A critique and improvement of an evaluation metric for text segmentation},\n" +
+	"url = {http://portal.acm.org/citation.cfm?id=636737&dl=GUIDE&coll=GUIDE&CFID=27698866&CFTOKEN=51312152},\n" +
+	"volume = {28},\n" +
+	"year = {2002},\n" +
+	"issn = {0891-2017},\n" +
+	"doi = {http://dx.doi.org/10.1162/089120102317341756},\n" + 
+	"}";
+	
+	private static final String bibtexDoi = "10.1162/089120102317341756";
+
+	private static final String[] dois = new String[] 
+	                                                {
+		"10.1016/j.cemconres.2003.10.011",
+		"doi:10.1016/j.cemconres.2003.10.011",
+		"10.1023/A:1009769707641",
+		"10.1016/S0169-7552(98)00110-X",
+		"10.1145/860451",
+		"10.1002/cpe.607",
+		"doi:10.1109/ISSTA.2002.1048560"
+	                                                };
+
+	private static final String[] nonDois = new String[] 
+	                                                   {
+		" 10.1016/j.cemconres.2003.10.011 ",
+		"10.1016/j.cemconres.2003.10.011\"",
+		"10.1016/j.cemconres.2003.10.011'",
+		null,
+		"",
+		"ysdfklaskld",
+		"10.1016/j.cemconres.2003.10.011 asef",
+		"\n10.1016/j.cemconres.2003.10.}011"
+	                                                   };
+
+	private static final String[] fuzzyStarts = new String[] {
+		"  ",
+		":",
+		"\"",
+		"'",
+		"("
+	};
+
+	private static final String[] fuzzyEndings = new String[] {
+		" ",
+		"\"sadfasdf",
+		"'"
+	};
+
+
+
+	private static final String[] fuzzyDoiOnlyStarts = new String[] {
+		"  ",
+		":",
+		"\"",
+		"'",
+		"("
+	};
+
+	private static final String[] fuzzyDoiOnlyEndings = new String[] {
+		" ",
+		"\"",
+		"'"
+	};
+
+
+	@Test
+	public void testIsDOIURL() {
+		for(final String doi: dois) {
+			try {
+				Assert.assertTrue(DOIUtils.isDOIURL(DOIUtils.getURL(doi)));
+			} catch (MalformedURLException ex) {
+				fail(ex.getMessage());
+			}
+		}
+	}
+
+
+	private String fuzzifyDoiOnlyDOI(final String doi) {
+		return 
+		fuzzyDoiOnlyStarts[rand.nextInt(fuzzyDoiOnlyStarts.length)] + 
+		doi + 
+		fuzzyDoiOnlyEndings[rand.nextInt(fuzzyDoiOnlyEndings.length)];
+	}
+
+	private String fuzzifyDOI(final String doi) {
+		return 
+		fuzzyStarts[rand.nextInt(fuzzyStarts.length)] + 
+		doi + 
+		fuzzyEndings[rand.nextInt(fuzzyEndings.length)];
+	}
+
+	private String stripDOI(final String doi) {
+		if (doi.toLowerCase().startsWith("doi:")) {
+			return doi.substring(4);
+		}
+		return doi;
+	}
+
+	@Test
+	public void testExtractDOI() {
+		for(final String doi: dois) {
+			final String fuzzyDoi = fuzzifyDOI(doi);
+			Assert.assertEquals(stripDOI(doi), DOIUtils.extractDOI(fuzzyDoi));
+		}
+		Assert.assertEquals(bibtexDoi, DOIUtils.extractDOI(bibtexWithDoi));
+	}
+
+	@Test
+	public void testContainsOnlyDOI() {
+		for(final String doi: dois) {
+			final String fuzzyDoi = fuzzifyDoiOnlyDOI(doi);
+			System.out.println(doi + " --> " + fuzzyDoi);
+			Assert.assertTrue(DOIUtils.containsOnlyDOI(doi));
+			Assert.assertTrue(DOIUtils.containsOnlyDOI(fuzzyDoi));
+		}
+		Assert.assertFalse(DOIUtils.containsOnlyDOI(bibtexWithDoi));
+	}
+
+	@Test
+	public void testContainsDOI() {
+		for(final String doi: dois) {
+			Assert.assertTrue(DOIUtils.containsDOI(doi));
+			Assert.assertTrue(DOIUtils.containsDOI(fuzzifyDOI(doi)));
+		}
+		Assert.assertTrue(DOIUtils.containsDOI(bibtexWithDoi));
+	}
+
+
+	@Test
+	public void testIsDOI() {
+		for(final String doi: dois) {
+			Assert.assertTrue(DOIUtils.isDOI(doi));
+		}
+		Assert.assertFalse(DOIUtils.isDOI(bibtexWithDoi));
+	}
+
+	@Test
+	public void testIsNonDOI() {
+		for(final String doi: nonDois) {
+			Assert.assertFalse(DOIUtils.isDOI(doi));
+		}
+		for(final String doi: dois) {
+			Assert.assertFalse(DOIUtils.isDOI(fuzzifyDOI(doi)));
+		}
+	}
+
+
+	/**
+	 * test getting URL
+	 * FIXME: Thomas, kannst Du bitte mal schauen, was hier schiefläuft? Ist ja total seltsam ...
+	 * 
+	 * Im Browser komme ich bei Aufruf von
+	 * 
+	 * http://dx.doi.org/10.1007/11922162
+	 * 
+	 * auf 
+	 * 
+	 * http://www.springerlink.com/content/w425794t7433/
+	 * 
+	 * raus. Aber {@link WebUtils#getRedirectUrl(URL)} kommt auf 
+	 * 
+	 * http://www.springerlink.com/link.asp?id=w425794t7433
+	 * 
+	 * raus. Auch wenn ich 
+	 * 
+	 * http://www.springerlink.com/index/10.1007/11922162
+	 * 
+	 * (das ist die alte URL aus dem Test hier) eingebe, komme ich auf 
+	 * der ersten URL raus. D.h., irgendwie scheint Springer da je nach
+	 * Cookie-Handling, Referer, oder nach Browser woanders hinzuleiten. :-(
+	 * 
+	 *   
+	 */
+	@Test
+	@Ignore
+	public void getUrlForDoiTest(){
+		Assert.assertEquals("http://www.springerlink.com/link.asp?id=w425794t7433", DOIUtils.getUrlForDoi("10.1007/11922162").toString());
+	}
+}
