2a7d62e61b1f448c2495d60c686a119dd411cedf
Robert Jäschke <jaeschke@l3s.de>
2009-03-27 16:33:15 +0100

    first refactoring for psot bibtex

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/actions/EditBookmarkCommand.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/actions/EditBookmarkCommand.java
@@ -1,114 +1,12 @@
 package org.bibsonomy.webapp.command.actions;
 
-import java.util.List;
-import java.util.Map;
-import java.util.SortedSet;
-
 import org.bibsonomy.model.Bookmark;
-import org.bibsonomy.model.Post;
-import org.bibsonomy.model.Tag;
-import org.bibsonomy.model.RecommendedTag;
-import org.bibsonomy.webapp.command.PostCommand;
 
 /**
  * @author fba
  * @version $Id$
  */
-public class EditBookmarkCommand extends PostCommand {
-	/**
-	 * The tags of the copied post.
-	 */
-	private List<Tag> copytags;
-	
-	private Post<Bookmark> post;
-	private String tags ;
-	private boolean jump = false;
-	
-	private Post<Bookmark> diffPost;
-	
-	private String intraHashToUpdate;
-	
-	/**
-	 * When the tag field contains commas, it is only accepted, if this boolean is set to <code>true</code> 
-	 */
-	private boolean acceptComma = false;
-	private boolean containsComma = false;
-	
-	
-	/**
-	 * The abstract (or general) group of the post:
-	 * public, private, or other 
-	 */
-	private String abstractGrouping;
-	
-	private List<String> groups;
-	
-	private List<String> relevantGroups;
-	private SortedSet<RecommendedTag> recommendedTags;
-
-	private Map<String,Map<String,List<String>>> relevantTagSets;
-	
-	/**
-	 * stores an id, e.g. for mapping recommendations to posts
-	 */
-	private int postID;
-	
-	public Post<Bookmark> getPost() {
-		return this.post;
-	}
-
-	public void setPost(Post<Bookmark> post) {
-		this.post = post;
-	}
-
-	public void setTags(String tags) {
-		this.tags = tags;
-	}
-
-	public String getTags() {
-		return tags;
-	}
-
-	public List<String> getGroups() {
-		return this.groups;
-	}
-
-	public void setGroups(List<String> groups) {
-		this.groups = groups;
-	}
-
-	public List<String> getRelevantGroups() {
-		return this.relevantGroups;
-	}
-
-	public void setRelevantGroups(List<String> relevantGroups) {
-		this.relevantGroups = relevantGroups;
-	}
-	
-	public SortedSet<RecommendedTag> getRecommendedTags() {
-		return this.recommendedTags;
-	}
-
-	public void setRecommendedTags(SortedSet<RecommendedTag> recommendedTags) {
-		this.recommendedTags = recommendedTags;
-	}
-
-	public Map<String, Map<String, List<String>>> getRelevantTagSets() {
-		return this.relevantTagSets;
-	}
-
-	public void setRelevantTagSets(Map<String, Map<String, List<String>>> relevantTagSets) {
-		this.relevantTagSets = relevantTagSets;
-	}
-
-	public void setJump(boolean jump) {
-		this.jump = jump;
-	}
-
-	public boolean isJump() {
-		return jump;
-	}
-		
+public class EditBookmarkCommand extends EditPostCommand<Bookmark> {
 	/**
 	 * Sets the URL of the post. 
 	 * Needed for the (old) postBookmark button and "copy" links. 
@@ -116,7 +14,7 @@ public class EditBookmarkCommand extends PostCommand {
 	 * @param url 
 	 */
 	public void setUrl(final String url){
-		this.post.getResource().setUrl(url);
+		getPost().getResource().setUrl(url);
 	}
 	
 	/**
@@ -126,7 +24,7 @@ public class EditBookmarkCommand extends PostCommand {
 	 * @param title
 	 */
 	public void setDescription(final String title){
-		this.post.getResource().setTitle(title);
+		getPost().getResource().setTitle(title);
 	}
 	
 	/**
@@ -136,74 +34,6 @@ public class EditBookmarkCommand extends PostCommand {
 	 * @param description
 	 */
 	public void setExtended(final String description){
-		this.post.setDescription(description);
-	}
-	
-	/** Sets the tags from the copied post.
-	 * Needed for the (old) "copy" links.
-	 * 
-	 * @param tags
-	 */
-	public void setCopytag(final String tags){
-		final String[] splittedTags = tags.split("\\s");
-		for (final String tagname: splittedTags){
-			this.copytags.add(new Tag(tagname));
-		}
-	}
-	
-	public List<Tag> getCopytags(){
-		return this.copytags;
-	}
-	
-	public void setCopytags(final List<Tag> tags){
-		this.copytags = tags;
-	}
-
-	public String getAbstractGrouping() {
-		return this.abstractGrouping;
-	}
-
-	public void setAbstractGrouping(String abstractGrouping) {
-		this.abstractGrouping = abstractGrouping;
-	}
-
-	public Post<Bookmark> getDiffPost() {
-		return this.diffPost;
-	}
-
-	public void setDiffPost(Post<Bookmark> diffPost) {
-		this.diffPost = diffPost;
-	}
-
-	public String getIntraHashToUpdate() {
-		return this.intraHashToUpdate;
-	}
-
-	public void setIntraHashToUpdate(String intraHashToUpdate) {
-		this.intraHashToUpdate = intraHashToUpdate;
-	}
-
-	public boolean isAcceptComma() {
-		return this.acceptComma;
-	}
-
-	public void setAcceptComma(boolean acceptComma) {
-		this.acceptComma = acceptComma;
-	}
-
-	public boolean getContainsComma() {
-		return this.containsComma;
-	}
-
-	public void setContainsComma(boolean containsComma) {
-		this.containsComma = containsComma;
-	}
-
-	public void setPostID(int postID) {
-		this.postID = postID;
-	}
-
-	public int getPostID() {
-		return postID;
+		getPost().setDescription(description);
 	}
 }
