5f6957a8842c29bb6d085efc197ef1d55c5b4e8a
Robert Jäschke <jaeschke@l3s.de>
2009-04-01 12:16:04 +0200

    rendering to a stringbuffer now

--- bibsonomy-model/src/main/java/org/bibsonomy/services/renderer/LayoutRenderer.java
+++ bibsonomy-model/src/main/java/org/bibsonomy/services/renderer/LayoutRenderer.java
@@ -24,7 +24,6 @@
 package org.bibsonomy.services.renderer;
 
 import java.io.IOException;
-import java.io.OutputStream;
 import java.util.List;
 
 import org.bibsonomy.common.exceptions.LayoutRenderingException;
@@ -60,14 +59,14 @@ public interface LayoutRenderer<LAYOUT extends Layout> {
 	 * @param <T>
 	 * @param layout
 	 * @param posts
-	 * @param outputStream
 	 * @param embeddedLayout - if possible, the rendering result should be embeddable into
 	 * a page (i.e., for HTML based layouts: don't render &lt;html&gt; tags) 
+	 * @return The buffer the layout has been rendered to.
 	 * 
 	 * @throws IOException - if there was an internal problem rendering the layout
 	 * @throws LayoutRenderingException - if the layout contains errors
 	 */
-	public <T extends Resource> void renderLayout(final LAYOUT layout, final List<Post<T>> posts, final OutputStream outputStream, final boolean embeddedLayout) throws LayoutRenderingException, IOException;
+	public <T extends Resource> StringBuffer renderLayout(final LAYOUT layout, final List<Post<T>> posts, final boolean embeddedLayout) throws LayoutRenderingException, IOException;
 
 	/** Checks, if the renderer supports the given resource type.
 	 * 
