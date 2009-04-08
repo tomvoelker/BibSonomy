6ff61f5739f945d5a5f7d937383be90cda6cac23
Beate Krause <beatekr@cs.uni-kassel.de>
2009-04-08 14:53:59 +0200

    User with Evaluation properties

---
+++ bibsonomy-model/src/main/java/org/bibsonomy/model/EvaluatorUser.java
@@ -0,0 +1,82 @@
+/**
+ *  
+ *  BibSonomy-Model - Java- and JAXB-Model.
+ *   
+ *  Copyright (C) 2006 - 2008 Knowledge & Data Engineering Group, 
+ *                            University of Kassel, Germany
+ *                            http://www.kde.cs.uni-kassel.de/
+ *  
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package org.bibsonomy.model;
+
+import java.net.URL;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.bibsonomy.common.enums.Role;
+import org.bibsonomy.model.util.UserUtils;
+
+/**
+ * This class defines a user. An unknown user has an empty (<code>null</code>) name.
+ * 
+ * @version $Id$
+ */
+public class EvaluatorUser extends User{
+
+	/**
+	 * The (nick-)name of this user. Is <code>null</code> if the user is not logged in (unknown). 
+	 */
+	private String evaluator;
+	
+	private String evalDate;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param name
+	 */
+	public EvaluatorUser(final String name) {
+		super.setName(name); 
+		super.setBasket(new Basket());
+		super.setSettings(new UserSettings());
+		super.setRole(Role.ADMIN); // TODO: check, if this has any bad implications!
+	}
+
+	/**
+	 * Constructor
+	 */
+	public EvaluatorUser() {
+		this(null);
+	}
+	
+	public String getEvaluator() {
+		return this.evaluator;
+	}
+
+	public void setEvaluator(String evaluator) {
+		this.evaluator = evaluator;
+	}
+	
+	public String getEvalDate() {
+		return this.evalDate;
+	}
+
+	public void setEvalDate(String evalDate) {
+		this.evalDate = evalDate;
+	}
+}
