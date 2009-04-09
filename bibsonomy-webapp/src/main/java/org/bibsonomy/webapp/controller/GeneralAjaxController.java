66ea99762e71e64b73c3e3849066e78b468ef35a
Dominik Benz <benz@cs.uni-kassel.de>
2009-04-09 13:26:48 +0200

    renamed getUrlAjaxController to GeneralAjaxController, renamed beans accordingly

---
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/GeneralAjaxController.java
@@ -0,0 +1,120 @@
+package org.bibsonomy.webapp.controller;
+
+import java.net.URL;
+import java.util.ArrayList;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.bibsonomy.common.enums.GroupingEntity;
+import org.bibsonomy.database.systemstags.SystemTags;
+import org.bibsonomy.database.systemstags.SystemTagsUtil;
+import org.bibsonomy.model.BibTex;
+import org.bibsonomy.util.XmlUtils;
+import org.bibsonomy.webapp.command.GeneralAjaxCommand;
+import org.bibsonomy.webapp.util.MinimalisticController;
+import org.bibsonomy.webapp.util.View;
+import org.bibsonomy.webapp.view.Views;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import static org.bibsonomy.util.ValidationUtils.present;
+
+
+/** Returns information about the given URL.
+ * 
+ * @author fba
+ * @version $Id$
+ */
+public class GeneralAjaxController extends AjaxController implements MinimalisticController<GeneralAjaxCommand> {
+
+	private static final Log log = LogFactory.getLog(GeneralAjaxController.class);
+
+	//nur ausführen, wenn isJump() == false
+	public View workOn(GeneralAjaxCommand command) {
+
+		final String action = command.getAction();
+
+		if ("getTitleForUrl".equals(action)) {
+			this.getDetailsForUrl(command);
+			return Views.AJAX_GET_TITLE_FOR_URL;
+		}
+		else if ("getBibtexKeysForUser".equals(action)) {
+			this.getBibtexKeysForUser(command);
+			return Views.AJAX_GET_BIBTEXKEYS_FOR_USER;
+		}		
+		return Views.AJAX;
+	}
+
+	
+	/**
+	 * Retrieve bibtex keys of a given user
+	 * 
+	 * @param command - the command bean for this controller
+	 */
+	private void getBibtexKeysForUser(GeneralAjaxCommand command) {
+		/*
+		 * check input params, make sure user is logged in
+		 */
+		if (!present(command.getQ()) || !command.getContext().isUserLoggedIn() || !present(command.getRequestedUser())) {
+			return;
+		}
+		/*
+		 * append percent sign to bibkey for fuzzy matching
+		 */
+		final String requestedBibTexKey = command.getQ().trim() + "%";
+		final String requestedUserName   = command.getRequestedUser();
+		/*
+		 * fetch posts
+		 */		
+		final ArrayList<String> tags = new ArrayList<String>();
+		tags.add(SystemTagsUtil.buildSystemTagString(SystemTags.BIBTEXKEY, requestedBibTexKey));
+		command.setBibtexPosts(this.logic.getPosts(BibTex.class, GroupingEntity.USER, requestedUserName, tags, null, null, null, 0, 20, null));
+	}
+	
+	/**
+	 * Retrieve details for a given URL
+	 * 
+	 * @param command - the command containing the page URL
+	 */
+	private void getDetailsForUrl(final GeneralAjaxCommand command) {
+
+		if ((command.getPageURL() == null) || (command.getPageURL().length() == 0)) return;
+
+		try {
+
+			final Document document = XmlUtils.getDOM(new URL(command.getPageURL()));
+			
+
+			final NodeList title = document.getElementsByTagName("title");
+			command.setPageTitle(title.item(0).getChildNodes().item(0).getNodeValue());
+
+			final NodeList metaList = document.getElementsByTagName("meta");
+			for (int i = 0; i < metaList.getLength(); i++) {
+				final Element metaElement = (Element) metaList.item(i);
+
+				Attr nameAttr = metaElement.getAttributeNode("name");
+				if (nameAttr == null) continue; 
+
+				if (nameAttr.getNodeValue().equalsIgnoreCase("description")) {
+					command.setPageDescription(metaElement.getAttribute("content"));
+				}
+				if (nameAttr.getNodeValue().equalsIgnoreCase("keywords")) {
+					command.setPageKeywords(metaElement.getAttribute("content"));
+					log.info("KEYWORDS:" + metaElement.getAttribute("content"));
+				}
+			}
+
+		} catch (final Exception ex) {
+			// ignore exceptions silently
+		}
+	}
+
+
+
+	public GeneralAjaxCommand instantiateCommand() {
+		return new GeneralAjaxCommand();
+	}
+
+}
