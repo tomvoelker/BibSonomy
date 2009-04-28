9fd7907c54fc27f2b664df1e5e85e09848d0afb3
Beate Krause <beatekr@cs.uni-kassel.de>
2009-04-28 14:20:30 +0200

    moved options settings from jsp

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/AdminSettingsCommand.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/AdminSettingsCommand.java
@@ -1,26 +1,73 @@
 package org.bibsonomy.webapp.command;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.bibsonomy.common.enums.ClassifierSettings;
 
 /**
- * Bean for classifier settings 
+ * Bean for classifier settings
  * 
  * @author Stefan Stützer
- * @version $Id$
+ * @version $Id: AdminSettingsCommand.java,v 1.2 2008-04-07 13:25:48
+ *          ss05sstuetzer Exp $
  */
 public class AdminSettingsCommand {
 
 	private String mode;
-	
+
 	private String algorithm;
-	
+
 	private String trainingPeriod;
-	
+
+	public void setClassifyPeriod(String classifyPeriod) {
+		this.classifyPeriod = classifyPeriod;
+	}
+
+	public void setProbabilityLimit(String probabilityLimit) {
+		this.probabilityLimit = probabilityLimit;
+	}
+
 	private String classifyPeriod;
-	
+
 	private String probabilityLimit;
-	
+
 	private String testing;
+
+	private Map<String, String> algorithmOptions;
+	
+	private Map<String, String> testingOptions;
+	
+	private Map<String, String> modeOptions;
+
+	public AdminSettingsCommand() {
+		
+		/**
+		 * initialize options
+		 */
+		
+		//algorithm options
+		algorithmOptions = new HashMap<String, String>();
+		algorithmOptions.put("IBk", "weka.classifiers.lazy.IBk");
+		algorithmOptions.put("NaiveBayes", "weka.classifiers.bayes.NaiveBayes");
+		algorithmOptions.put("J48", "weka.classifiers.trees.J48");
+		algorithmOptions.put("OneR", "weka.classifiers.rules.OneR");
+		algorithmOptions.put("Logistic", "weka.classifiers.functions.Logistic");
+		algorithmOptions.put("LibSVM", "weka.classifiers.functions.LibSVM");
+		algorithmOptions.put("SMO", "weka.classifiers.functions.SMO");
+		
+		//testing options
+		testingOptions = new HashMap<String, String>();
+		testingOptions.put("on", "on");
+		testingOptions.put("off", "off");
+		
+		//mode options
+		modeOptions = new HashMap<String, String>();
+		modeOptions.put("Day", "d");
+		modeOptions.put("Night", "n");
+		
+	}
 
 	public void setAdminSetting(final ClassifierSettings setting, final String value) {
 		if (setting.equals(ClassifierSettings.ALGORITHM)) {
@@ -41,13 +88,17 @@ public class AdminSettingsCommand {
 	public String getMode() {
 		return this.mode;
 	}
-	
+
 	public String getAlgorithm() {
 		return this.algorithm;
 	}
 
 	public String getTrainingPeriod() {
 		return this.trainingPeriod;
+	}
+
+	public void setTrainingPeriod(String trainingPeriod) {
+		this.trainingPeriod = trainingPeriod;
 	}
 
 	public String getClassifyPeriod() {
@@ -60,5 +111,29 @@ public class AdminSettingsCommand {
 
 	public String getTesting() {
 		return this.testing;
-	}	
+	}
+
+	public Map<String,String> getAlgorithmOptions() {
+		return this.algorithmOptions;
+	}
+
+	public void setAlgorithmOptions(Map<String,String> algorithmOptions) {
+		this.algorithmOptions = algorithmOptions;
+	}
+
+	public Map<String, String> getTestingOptions() {
+		return this.testingOptions;
+	}
+
+	public void setTestingOptions(Map<String, String> testingOptions) {
+		this.testingOptions = testingOptions;
+	}
+
+	public Map<String, String> getModeOptions() {
+		return this.modeOptions;
+	}
+
+	public void setModeOptions(Map<String, String> modeOptions) {
+		this.modeOptions = modeOptions;
+	}
 }
