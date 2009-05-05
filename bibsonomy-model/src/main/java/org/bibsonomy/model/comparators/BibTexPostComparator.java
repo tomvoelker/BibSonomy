893f00f51031f9fcda4fdbaae8b5e87add07fdbe
Dominik Benz <benz@cs.uni-kassel.de>
2009-05-05 17:22:42 +0200

    added ranking as sort criterion for bibtex posts posts

--- bibsonomy-model/src/main/java/org/bibsonomy/model/comparators/BibTexPostComparator.java
+++ bibsonomy-model/src/main/java/org/bibsonomy/model/comparators/BibTexPostComparator.java
@@ -23,21 +23,17 @@
 
 package org.bibsonomy.model.comparators;
 
+import static org.bibsonomy.util.ValidationUtils.present;
+
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Date;
 
-import org.bibsonomy.common.enums.SortOrder;
 import org.bibsonomy.common.enums.SortKey;
+import org.bibsonomy.common.enums.SortOrder;
 import org.bibsonomy.model.BibTex;
 import org.bibsonomy.model.Post;
 import org.bibsonomy.model.util.BibTexUtils;
-import org.bibsonomy.util.DateUtils;
-import org.bibsonomy.util.StringUtils;
-
-import static org.bibsonomy.util.ValidationUtils.present;
 
 /**
  * Comparator used to sort bibtex posts
@@ -45,49 +41,18 @@ import static org.bibsonomy.util.ValidationUtils.present;
  * @author Dominik Benz
  * @version $Id$
  */
-public class BibTexPostComparator implements Comparator<Post<BibTex>>, Serializable {
-
-	private static final long serialVersionUID = 1L;
-	private List<SortCriterium> sortCriteria = new ArrayList<SortCriterium>();
-
-	/** Helper structure to bind a sort key to a sort order */
-	private class SortCriterium {
-		/** sort key */
-		public final SortKey sortKey;
-		/** sort order */
-		public final SortOrder sortOrder;
-		/**
-		 * Constructor
-		 * @param key 
-		 * @param order
-		 */
-		public SortCriterium(final SortKey key, final SortOrder order) {
-			this.sortKey = key;
-			this.sortOrder = order;
-		}
-	}	
-
-	/** helper exception */
-	private class SortKeyIsEqualException extends Exception {
-		private static final long serialVersionUID = 1L;		
-	}
+public class BibTexPostComparator extends PostComparator implements Comparator<Post<BibTex>>, Serializable {
 
 	/**
-	 * instantiate comparator
+	 * Constructor
 	 * 
 	 * @param sortKeys
 	 * @param sortOrders
 	 */
 	public BibTexPostComparator(final List<SortKey> sortKeys, final List<SortOrder> sortOrders) {
-		for (int i = 0; i <= sortKeys.size() - 1; i++) {
-			try {
-				this.sortCriteria.add(new SortCriterium(sortKeys.get(i), sortOrders.get(i)));
-			} catch (IndexOutOfBoundsException ignore) {
-				// fill up with default ascending order
-				this.sortCriteria.add(new SortCriterium(sortKeys.get(i), SortOrder.ASC));
-			}
-		}
+		super(sortKeys, sortOrders);
 	}
+
 
 	/*
 	 * (non-Javadoc)
@@ -134,6 +99,10 @@ public class BibTexPostComparator implements Comparator<Post<BibTex>>, Serializa
 				else if (SortKey.DATE.equals(crit.sortKey)) {
 					return this.compare(post1.getDate(), post2.getDate(), crit.sortOrder);
 				}
+				// ranking
+				else if (SortKey.RANKING.equals(crit.sortKey)) {
+					return this.compare(post1.getRanking(), post2.getRanking(), crit.sortOrder);
+				}
 				else {
 					return 0;
 				}
@@ -145,68 +114,4 @@ public class BibTexPostComparator implements Comparator<Post<BibTex>>, Serializa
 		return 0;
 	}
 
-	/**
-	 * Compare two strings following a specified order
-	 * 
-	 * @param s1 first string
-	 * @param s2 second string
-	 * @param order sort order
-	 * @return an int comparison value
-	 * @throws SortKeyIsEqualException 
-	 */
-	private int nomalizeAndCompare(String s1, String s2, final SortOrder order) throws SortKeyIsEqualException {
-		// normalization
-		if (present(s1)) s1 = BibTexUtils.cleanBibTex(s1).trim();
-		if (present(s2)) s2 = BibTexUtils.cleanBibTex(s2).trim();
-		// comparison
-		int comp = 0;
-		if (SortOrder.ASC.equals(order)) {
-			comp = StringUtils.secureCompareTo(s1, s2);
-		} else {
-			comp = StringUtils.secureCompareTo(s2, s1);
-		}
-		if (comp == 0) throw new SortKeyIsEqualException();
-		return comp;
-	}
-
-	/**
-	 * Compare two integers following a specified order
-	 * 
-	 * @param i1 first integer
-	 * @param i2 second integer
-	 * @param order sort order
-	 * @return an int comparison value
-	 * @throws SortKeyIsEqualException 
-	 */
-	private int compare (final int i1, final int i2, final SortOrder order) throws SortKeyIsEqualException {
-		int comp = 0;
-		if (order.equals(SortOrder.ASC)) {
-			comp = i1 - i2;
-		} else {
-			comp = i2 - i1;
-		}
-		if (comp == 0) throw new SortKeyIsEqualException();
-		return comp;
-	}	
-	
-	/**
-	 * Compare two dates following a specified order
-	 * 
-	 * @param d1 first date
-	 * @param d2 second date
-	 * @param order sort order
-	 * @return an int comparison value
-	 * @throws SortKeyIsEqualException
-	 */
-	private int compare (final Date d1, final Date d2, final SortOrder order) throws SortKeyIsEqualException {
-		int comp = 0;
-		if (order.equals(SortOrder.ASC)) {
-			comp = DateUtils.secureCompareTo(d1, d2);
-		}
-		else {
-			comp = DateUtils.secureCompareTo(d2, d1);
-		}
-		if (comp == 0) throw new SortKeyIsEqualException();
-		return comp;
-	}
 }
