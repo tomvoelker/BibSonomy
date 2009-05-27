588c8624e3f9053f2c403487d3c0186f4c5d3307
Robert Jäschke <jaeschke@l3s.de>
2009-05-27 18:13:57 +0200

    added first publication controller

--- bibsonomy-recommender-test/src/main/java/org/bibsonomy/recommender/webapp/command/EditPublicationCommand.java
+++ bibsonomy-recommender-test/src/main/java/org/bibsonomy/recommender/webapp/command/EditPublicationCommand.java
@@ -9,6 +9,10 @@ import org.bibsonomy.model.BibTex;
  * @version $Id$
  */
 public class EditPublicationCommand extends EditPostCommand<BibTex> {
+	
+	private static final String[] entryTypes = new String[]{"article", "book", "booklet", "inbook", "incollection", "inproceedings",
+		"manual", "mastersthesis", "misc", "phdthesis", "proceedings", "techreport", "unpublished"};
+	
 	/**
 	 * Sets the URL of the post. 
 	 * Needed for the (old) postBookmark button and "copy" links. 
@@ -38,4 +42,9 @@ public class EditPublicationCommand extends EditPostCommand<BibTex> {
 	public void setExtended(final String description){
 		getPost().setDescription(description);
 	}
+
+	public String[] getEntryTypes() {
+		return entryTypes;
+	}
+	
 }
