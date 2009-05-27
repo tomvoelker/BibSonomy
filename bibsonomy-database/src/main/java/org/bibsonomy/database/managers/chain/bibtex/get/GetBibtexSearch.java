108f70d2192589fb1498b850f333403b957891f3
Sven Stefani <sst@cs.uni-kassel.de>
2009-05-27 18:43:57 +0200

    added switch to switch between the two fulltext searchmodes lucene and standard mysql search. add following code to tomcat's context.xml:
    <!-- searchMode can have values: "database", "lucene"  -->
      <Environment
        name="searchMode"
        type="java.lang.String"
        value="lucene"
        />

--- bibsonomy-database/src/main/java/org/bibsonomy/database/managers/chain/bibtex/get/GetBibtexSearch.java
+++ bibsonomy-database/src/main/java/org/bibsonomy/database/managers/chain/bibtex/get/GetBibtexSearch.java
@@ -4,6 +4,10 @@ import static org.bibsonomy.util.ValidationUtils.present;
 
 import java.util.List;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
 import org.bibsonomy.common.enums.GroupingEntity;
 import org.bibsonomy.common.enums.SearchEntity;
 import org.bibsonomy.database.managers.chain.bibtex.BibTexChainElement;
@@ -24,13 +28,35 @@ public class GetBibtexSearch extends BibTexChainElement {
 	protected List<Post<BibTex>> handle(final BibTexParam param, DBSession session) {
 		// uncomment following for a quick hack to access secondary datasource
 		// session = this.dbSessionFactory.getDatabaseSession(DatabaseType.SLAVE);
+
+		Context initContext = null;
+		Context envContext = null;
+		String searchMode = "";
+		try {
+			initContext = new InitialContext();
+			envContext = (Context) initContext.lookup("java:/comp/env");
+			searchMode = (String) envContext.lookup("searchMode");
+		} catch (NamingException ex) {
+			// TODO Auto-generated catch block
+			ex.printStackTrace();
+		}
+		
+		if ("lucene".equals(searchMode)) {
+			return this.db.getBibTexSearchLucene(param.getGroupId(), param.getSearch(), param.getRequestedUserName(), param.getUserName(), param.getGroupNames(), param.getLimit(), param.getOffset(), session);
+		}
+
+		// default = database
+		return this.db.getBibTexSearch(param.getGroupType(), param.getSearch(), param.getRequestedUserName(), param.getLimit(), param.getOffset(), session);}
+
+
+/*
 		if (SearchEntity.LUCENE.equals(param.getSearchEntity())) {
 			return this.db.getBibTexSearchLucene(param.getGroupId(), param.getSearch(), param.getRequestedUserName(), param.getUserName(), param.getGroupNames(), param.getLimit(), param.getOffset(), session);
 		}
 		
 		return this.db.getBibTexSearch(param.getGroupType(), param.getSearch(), param.getRequestedUserName(), param.getLimit(), param.getOffset(), session);
 	}
-
+*/
 	@Override
 	protected boolean canHandle(final BibTexParam param) {
 		return ((param.getGrouping() == GroupingEntity.ALL || param.getGrouping() == GroupingEntity.USER) &&
--- bibsonomy-database/src/main/java/org/bibsonomy/database/managers/chain/bookmark/get/GetBookmarksSearch.java
+++ bibsonomy-database/src/main/java/org/bibsonomy/database/managers/chain/bookmark/get/GetBookmarksSearch.java
@@ -4,8 +4,11 @@ import static org.bibsonomy.util.ValidationUtils.present;
 
 import java.util.List;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
 import org.bibsonomy.common.enums.GroupingEntity;
-import org.bibsonomy.common.enums.SearchEntity;
 import org.bibsonomy.database.managers.chain.bookmark.BookmarkChainElement;
 import org.bibsonomy.database.params.BookmarkParam;
 import org.bibsonomy.database.util.DBSession;
@@ -24,6 +27,27 @@ public class GetBookmarksSearch extends BookmarkChainElement {
 	protected List<Post<Bookmark>> handle(final BookmarkParam param, DBSession session) {
 		// uncomment following for a quick hack to access secondary datasource
 		// session = this.dbSessionFactory.getDatabaseSession(DatabaseType.SLAVE);
+
+		Context initContext = null;
+		Context envContext = null;
+		String searchMode = "";
+		try {
+			initContext = new InitialContext();
+			envContext = (Context) initContext.lookup("java:/comp/env");
+			searchMode = (String) envContext.lookup("searchMode");
+		} catch (NamingException ex) {
+			// TODO Auto-generated catch block
+			ex.printStackTrace();
+		}
+		
+		if ("lucene".equals(searchMode)) {
+			return this.db.getBookmarkSearchLucene(param.getGroupId(), param.getSearch(), param.getRequestedUserName(), param.getUserName(), param.getGroupNames(), param.getLimit(), param.getOffset(), session);
+		}
+	
+		return this.db.getBookmarkSearch(param.getGroupType(), param.getSearch(), param.getRequestedUserName(), param.getLimit(), param.getOffset(), session);
+		
+		
+/*		
 		if (SearchEntity.LUCENE.equals(param.getSearchEntity())) {
 			//param.getGroups();   // gruppen, die der eingeloggte user sehen darf
 			//param.getUserName(); // eingeloggter user
@@ -31,6 +55,7 @@ public class GetBookmarksSearch extends BookmarkChainElement {
 			return this.db.getBookmarkSearchLucene(param.getGroupId(), param.getSearch(), param.getRequestedUserName(), param.getUserName(), param.getGroupNames(), param.getLimit(), param.getOffset(), session);
 		}
 		return this.db.getBookmarkSearch(param.getGroupType(), param.getSearch(), param.getRequestedUserName(), param.getLimit(), param.getOffset(), session);
+*/
 	}
 	
 	@Override
