7230d6e3b0daa921ee4a50718280c4b96027ed65
Robert Jäschke <jaeschke@l3s.de>
2009-06-19 18:14:01 +0200

    added BNF and some interfaces

---
+++ bibsonomy-post-filter/src/main/java/org/bibsonomy/util/filter/posts/matcher/UnaryMatcher.java
@@ -0,0 +1,16 @@
+package org.bibsonomy.util.filter.posts.matcher;
+
+
+/**
+ * 
+ * @author:  rja
+ * @version: $Id$
+ * $Author$
+ * 
+ */
+public interface UnaryMatcher extends Matcher {
+
+	public void setMatcher(Matcher matcher);
+	
+}
+
