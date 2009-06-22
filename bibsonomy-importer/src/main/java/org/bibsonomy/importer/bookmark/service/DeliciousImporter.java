6bd033c882d44bc02603723e49b41e4363733722
ss05mwagner <devnull@localhost>
2009-06-22 14:03:16 +0200

    new importcontroller, first checkin

--- bibsonomy-importer/src/main/java/org/bibsonomy/importer/bookmark/service/DeliciousImporter.java
+++ bibsonomy-importer/src/main/java/org/bibsonomy/importer/bookmark/service/DeliciousImporter.java
@@ -1,16 +1,37 @@
 package org.bibsonomy.importer.bookmark.service;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.net.URLConnection;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.LinkedList;
 import java.util.List;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.antlr.runtime.RecognitionException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.bibsonomy.common.enums.GroupID;
 import org.bibsonomy.model.Bookmark;
+import org.bibsonomy.model.Group;
 import org.bibsonomy.model.Post;
 import org.bibsonomy.model.Tag;
+import org.bibsonomy.model.util.TagUtils;
 import org.bibsonomy.services.importer.RelationImporter;
 import org.bibsonomy.services.importer.RemoteServiceBookmarkImporter;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+import com.sun.mail.util.BASE64EncoderStream;
 
 /**
  * 
@@ -48,21 +69,82 @@ public class DeliciousImporter implements RemoteServiceBookmarkImporter, Relatio
 		this.userAgent = userAgent;
 	}
 
-	@Override
-	public List<Post<Bookmark>> getPosts() {
+	/**
+	 * This Method retrieves a list of Posts for a given user.
+	 */
+	public List<Post<Bookmark>> getPosts() throws IOException{
+		
 		final List<Post<Bookmark>> posts = new LinkedList<Post<Bookmark>>();
-		/*
-		 * TODO: get posts
-		 */
+				
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+				
+		//open a connection to delicious and retrieve a document
+		Document document = getDocument();
+		
+		// traverse document and put everything into Post<Bookmark> Objects
+		NodeList postList = document.getElementsByTagName("post");
+		for (int i = 0; i < postList.getLength(); i++) {
+			Element resource = (Element)postList.item(i);
+								
+			Post<Bookmark> post = new Post<Bookmark>();
+			
+			post.getResource().setTitle(resource.getAttribute("description"));
+			post.getResource().setUrl(resource.getAttribute("href"));
+			try {
+				post.getTags().addAll(TagUtils.parse(resource.getAttribute("tag")));
+			} catch (RecognitionException e) {
+				throw new IOException(e);
+			}
+			
+			//no tags available? -> add one tag to the resource and mark it as "imported"
+			if(post.getTags().isEmpty()){
+				post.getTags().add(new Tag("imported"));
+			}
+			
+			post.setDescription(resource.getAttribute("extended"));
+			try {
+				post.setDate(df.parse(resource.getAttribute("time")));
+			} catch (ParseException e) {
+				throw new IOException(e);
+			}
+			
+			//set the visibility of the imported resource
+			if(resource.hasAttribute("shared")){
+				if(resource.getAttribute("shared").equals("no")){
+					post.getGroups().add(new Group(GroupID.PRIVATE.getId()));
+				}else{
+					post.getGroups().add(new Group(GroupID.PUBLIC.getId()));
+				}
+			}
+			
+			posts.add(post);
+			
+		}
+		
 		return posts;
 	}
 
-	@Override
-	public List<Tag> getRelations() {
+	/**
+	 * This method retrieves a list of tags with subTags from Delicious.
+	 */
+	public List<Tag> getRelations() throws IOException {
 		final List<Tag> relations = new LinkedList<Tag>();
-		/*
-		 * TODO: get relations
-		 */
+		
+		//open a connection to delicious and retrieve a document
+		Document document = getDocument();
+		
+		NodeList bundles = document.getElementsByTagName("bundles");
+	
+		for(int i = 0; i < bundles.getLength(); i++){
+			Element resource = (Element)bundles.item(i);
+			try {
+				Tag tag = new Tag(resource.getAttribute("name"));
+				tag.getSubTags().addAll(TagUtils.parse(resource.getAttribute("tags")));
+			} catch (RecognitionException e) {
+				throw new IOException(e);
+			}
+		}
+		
 		return relations;
 	}
 
@@ -71,5 +153,66 @@ public class DeliciousImporter implements RemoteServiceBookmarkImporter, Relatio
 		this.userName = userName;
 		this.password = password;
 	}
+	
+	/**
+	 * Method opens a connection and parses the retrieved InputStream with a JAXP parser.
+	 * @return The from the parse call returned Document Object
+	 * @throws IOException
+	 */
+	private Document getDocument() throws IOException{
+		
+		String userNameAndPassword = userName+":"+password;
+		
+		URLConnection connection = apiURL.openConnection();
+		connection.setRequestProperty("User-Agent", userAgent);
+		connection.setRequestProperty("Authorization", "Basic "+BASE64EncoderStream.encode(userNameAndPassword.getBytes()));
+		
+		InputStream inputStream = connection.getInputStream();
+		
+		// Get a JAXP parser factory object
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		// Tell the factory what kind of parser we want 
+		dbf.setValidating(false);
+		// Use the factory to get a JAXP parser object
+		
+		DocumentBuilder parser;
+		try {
+			parser = dbf.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			throw new IOException(e);
+		}
+		
+		// Tell the parser how to handle errors.  Note that in the JAXP API,
+		// DOM parsers rely on the SAX API for error handling
+		parser.setErrorHandler(new ErrorHandler() {
+			public void warning(SAXParseException e) {
+				log.warn(e);
+			}
+			public void error(SAXParseException e) {
+				log.error(e);
+			}
+			public void fatalError(SAXParseException e)
+			throws SAXException {
+				log.fatal(e);
+				throw e;   // re-throw the error
+			}
+		});
+		
+		// Finally, use the JAXP parser to parse the file.  
+		// This call returns a Document object. 
+		
+		Document document;
+		try {
+			document = parser.parse(inputStream);
+		} catch (SAXException e) {
+			throw new IOException(e);
+		}
+		
+		inputStream.close();
+		
+		return document;
+			
+	}	
+	
 }
 
