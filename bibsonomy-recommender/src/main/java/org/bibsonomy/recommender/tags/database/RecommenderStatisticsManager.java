cf49987b4a670db21186ba574cb5547ae0f32d84
Robert Jäschke <jaeschke@l3s.de>
2009-06-22 14:55:04 +0200

    moved needed code into static methods

--- bibsonomy-recommender/src/main/java/org/bibsonomy/recommender/tags/database/RecommenderStatisticsManager.java
+++ bibsonomy-recommender/src/main/java/org/bibsonomy/recommender/tags/database/RecommenderStatisticsManager.java
@@ -1,22 +1,31 @@
 package org.bibsonomy.recommender.tags.database;
 
+import java.util.Random;
 
 /**
- * Interface for gathering usage statistics for (tag) recommendations.
+ * Implements a service for logging recommendation statistics.
  * 
  * @author fei
  * @version $Id$
  */
-public interface RecommenderStatisticsManager {
-
+public class RecommenderStatisticsManager {
+	/** indicates that post identifier was not given */
+	public static int UNKNOWN_POSTID = -1;
+	private static final Random rand = new Random();
+	
 	/**
 	 * Get id which indicates that a recommendation query was not associated with a post.
 	 * @return UNKNOWN_POSTID
-	 */
-	public int getUnknownPID();
+	 */	
+	public static int getUnknownPID() {
+		return  UNKNOWN_POSTID;
+	}
 	
 	/**
 	 * @return A new post ID.
 	 */
-	public int getNewPID();
+	public static int getNewPID() {
+		return rand.nextInt(Integer.MAX_VALUE);
+	}
+
 }
