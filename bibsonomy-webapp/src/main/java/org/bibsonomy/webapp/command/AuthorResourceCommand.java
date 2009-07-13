558b983b3e99b83ff6f4ce3b39709b615c8abc97
ss05mwagner <devnull@localhost>
2009-07-13 16:54:15 +0200

    created authortag view

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/AuthorResourceCommand.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/AuthorResourceCommand.java
@@ -1,24 +1,14 @@
 package org.bibsonomy.webapp.command;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
-
 /**
  * @author daill
  * @version $Id$
  */
-public class AuthorResourceCommand extends SimpleResourceViewCommand {
+public class AuthorResourceCommand extends TagResourceViewCommand {
 	
 	// the requested Author
 	private String requestedAuthor = "";
-	
-	// the requested tags
-	private String requestedTags = "";
-	
-	// the bean for the related tags
-	private RelatedTagCommand relatedTagCommand = new RelatedTagCommand();
-
+		
 	/**
 	 * @return string with the requested author
 	 */
@@ -31,49 +21,5 @@ public class AuthorResourceCommand extends SimpleResourceViewCommand {
 	 */
 	public void setRequestedAuthor(String requestedAuthor) {
 		this.requestedAuthor = requestedAuthor;
-	}
-
-	/**
-	 * @return string of requested tags
-	 */
-	public String getRequestedTags() {
-		return this.requestedTags;
-	}
-
-	/**
-	 * @param requestedTags
-	 */
-	public void setRequestedTags(String requestedTags) {
-		relatedTagCommand.setRequestedTags(requestedTags);
-		this.requestedTags = requestedTags;
-	}
-	
-	/**
-	 * @return the requested tagstring as a list
-	 */
-	public List<String> getRequestedTagsList() {
-		List<String> tags = new ArrayList<String>();
-		
-		StringTokenizer st = new StringTokenizer(requestedTags);
-		while (st.hasMoreTokens()) {			
-			String tagname = st.nextToken();			
-			tags.add(tagname);			
-		}
-		
-		return tags;
-	}
-
-	/**
-	 * @return RelatedTagCommand with the related teags
-	 */
-	public RelatedTagCommand getRelatedTagCommand() {
-		return this.relatedTagCommand;
-	}
-
-	/**
-	 * @param relatedTagCommand
-	 */
-	public void setRelatedTagCommand(RelatedTagCommand relatedTagCommand) {
-		this.relatedTagCommand = relatedTagCommand;
 	}
 }
