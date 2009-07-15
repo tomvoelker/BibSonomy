637d90d7aeb54f696e737aa824762c28c6ac4c4b
voigtmannc <devnull@localhost>
2009-07-15 10:26:19 +0200

    added methods for getting and deleting documents without a post

--- bibsonomy-database/src/main/java/org/bibsonomy/database/managers/DocumentDatabaseManager.java
+++ bibsonomy-database/src/main/java/org/bibsonomy/database/managers/DocumentDatabaseManager.java
@@ -13,7 +13,7 @@ import org.bibsonomy.model.Document;
  * @author Christian Kramer
  * @version $Id$
  */
-public class DocumentDatabaseManager extends AbstractDatabaseManager{
+public class DocumentDatabaseManager extends AbstractDatabaseManager {
 
 	private final static DocumentDatabaseManager singleton = new DocumentDatabaseManager();
 
@@ -21,7 +21,7 @@ public class DocumentDatabaseManager extends AbstractDatabaseManager{
 	 * Documents not attached to posts get this value as content_id.
 	 */
 	public static final int DEFAULT_CONTENT_ID = 0;
-	
+
 	private DocumentDatabaseManager() {
 	}
 
@@ -35,17 +35,18 @@ public class DocumentDatabaseManager extends AbstractDatabaseManager{
 	/**
 	 * Checks, if the post has already a document with that name attached.
 	 * 
-	 * One post might have several documents attached. The documents are 
-	 * identified by their file name. Only one document per filename/post is 
+	 * One post might have several documents attached. The documents are
+	 * identified by their file name. Only one document per filename/post is
 	 * allowed
 	 * 
-	 * @param userName 
-	 * @param resourceHash 
-	 * @param fileName 
+	 * @param userName
+	 * @param resourceHash
+	 * @param fileName
 	 * @param session
-	 * @return <code>true</code> if a document is attached to that hash, <code>false</code> otherwise 
+	 * @return <code>true</code> if a document is attached to that hash,
+	 *         <code>false</code> otherwise
 	 */
-	public boolean checkForExistingDocuments(final String userName, final String resourceHash, final String fileName, final DBSession session){
+	public boolean checkForExistingDocuments(final String userName, final String resourceHash, final String fileName, final DBSession session) {
 		if (present(resourceHash) == false) {
 			return false;
 		}
@@ -64,14 +65,14 @@ public class DocumentDatabaseManager extends AbstractDatabaseManager{
 	/**
 	 * Inserts a new document to the db
 	 * 
-	 * @param userName 
-	 * @param contentId 
-	 * @param fileHash 
-	 * @param fileName 
-	 * @param md5hash 
+	 * @param userName
+	 * @param contentId
+	 * @param fileHash
+	 * @param fileName
+	 * @param md5hash
 	 * @param session
 	 */
-	public void addDocument(final String userName, final int contentId, final String fileHash, final String fileName, final String md5hash, final DBSession session){
+	public void addDocument(final String userName, final int contentId, final String fileHash, final String fileName, final String md5hash, final DBSession session) {
 		final DocumentParam docParam = new DocumentParam();
 		docParam.setUserName(userName);
 		docParam.setFileHash(fileHash);
@@ -84,13 +85,13 @@ public class DocumentDatabaseManager extends AbstractDatabaseManager{
 	/**
 	 * Updates an existing document with the new hash and filename
 	 * 
-	 * @param contentId 
-	 * @param fileHash 
-	 * @param fileName 
-	 * @param md5hash 
+	 * @param contentId
+	 * @param fileHash
+	 * @param fileName
+	 * @param md5hash
 	 * @param session
 	 */
-	public void updateDocument(final int contentId, final String fileHash, final String fileName, final String md5hash, final DBSession session){
+	public void updateDocument(final int contentId, final String fileHash, final String fileName, final String md5hash, final DBSession session) {
 		final DocumentParam docParam = new DocumentParam();
 		docParam.setFileHash(fileHash);
 		docParam.setFileName(fileName);
@@ -100,25 +101,60 @@ public class DocumentDatabaseManager extends AbstractDatabaseManager{
 	}
 
 	/**
+	 * retrieves a (layout) document
+	 * 
+	 * @param docParam
+	 * @param session
+	 * @return document
+	 */
+	private Document getDocumentForLayout(final DocumentParam docParam, final DBSession session) {
+
+		return this.queryForObject("getDocumentForLayout", docParam, Document.class, session);
+	}
+
+	/**
+	 * retrieves a (layout) document
+	 * 
+	 * @param userName
+	 * @param fileHash
+	 * @param session
+	 * @return document
+	 */
+	public Document getDocument(final String userName, final String fileHash, DBSession session) {
+		// create the docParam object
+		final DocumentParam docParam = new DocumentParam();
+
+		// fill the docParam object
+		docParam.setFileHash(fileHash);
+		docParam.setUserName(userName);
+		docParam.setContentId(0);
+
+		// get the requested document
+		final Document doc = getDocumentForLayout(docParam, session);
+
+		return doc;
+	}
+
+	/**
 	 * This method gets documents object with the given name and hash.
 	 * 
 	 * @param docParam
 	 * @param session
 	 * @return document
 	 */
-	private List<Document> getDocumentsForPost(final DocumentParam docParam, final DBSession session){
+	private List<Document> getDocumentsForPost(final DocumentParam docParam, final DBSession session) {
 		return this.queryForList("getDocumentsForPost", docParam, Document.class, session);
 	}
 
-
-	/** Returns the named document for the given user name and hash.
+	/**
+	 * Returns the named document for the given user name and hash.
 	 * 
 	 * @param userName
 	 * @param resourceHash
 	 * @param session
 	 * @return A document.
 	 */
-	public List<Document> getDocumentsForPost(final String userName, final String resourceHash, final DBSession session) {
+	public List<Document> getDocuments(final String userName, final String resourceHash, final DBSession session) {
 		// create the docParam object
 		final DocumentParam docParam = new DocumentParam();
 
@@ -128,27 +164,54 @@ public class DocumentDatabaseManager extends AbstractDatabaseManager{
 
 		// get the requested document
 		final List<Document> doc = getDocumentsForPost(docParam, session);
-		if (doc == null){
+		if (doc == null) {
 			throw new IllegalStateException("No documents for this BibTeX entry");
 		}
 		return doc;
 	}
 
+	private void deleteDocumentLayout(final DocumentParam docParam, final DBSession session) {
+
+		this.delete("deleteDocWithNoPost", docParam, session);
+	}
+
+	/**
+	 * deletes a document which is not connected to a post
+	 * @param contentId
+	 * @param userName
+	 * @param fileHash
+	 * @param session
+	 */
+	public void deleteDocumentWithNoPost(final int contentId, final String userName, final String fileHash, final DBSession session) {
+		// create a DocumentParam object
+		final DocumentParam docParam = new DocumentParam();
+		docParam.setFileHash(fileHash);
+		docParam.setUserName(userName);
+		docParam.setContentId(contentId);
+		// finally delete the document
+		deleteDocumentLayout(docParam, session);
+	}
+
+	private void deleteDocumentForPost(final DocumentParam docParam, final DBSession session) {
+		
+		this.delete("deleteDoc", docParam, session);
+	}
+	
 	/**
 	 * This method deletes an existing document
 	 * 
-	 * @param contentId 
-	 * @param userName 
-	 * @param fileName 
+	 * @param contentId
+	 * @param userName
+	 * @param fileName
 	 * @param session
 	 */
-	public void deleteDocument(final int contentId, final String userName, final String fileName, final DBSession session){
+	public void deleteDocument(final int contentId, final String userName, final String fileName, final DBSession session) {
 		// create a DocumentParam object
 		final DocumentParam docParam = new DocumentParam();
 		docParam.setFileName(fileName);
 		docParam.setUserName(userName);
 		docParam.setContentId(contentId);
 		// finally delete the document
-		this.delete("deleteDoc", docParam, session);
+		deleteDocumentForPost(docParam, session);
 	}
 }
