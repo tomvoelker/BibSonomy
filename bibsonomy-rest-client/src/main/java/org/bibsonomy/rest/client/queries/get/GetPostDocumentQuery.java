11e0eaf69f35e707de387a3062f05dceba1495f8
boogie <devnull@localhost>
2009-07-23 14:54:56 +0200

    cleaned up code. sorry

--- bibsonomy-rest-client/src/main/java/org/bibsonomy/rest/client/queries/get/GetPostDocumentQuery.java
+++ bibsonomy-rest-client/src/main/java/org/bibsonomy/rest/client/queries/get/GetPostDocumentQuery.java
@@ -1,15 +1,10 @@
 package org.bibsonomy.rest.client.queries.get;
 
-import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
-import org.bibsonomy.rest.RestProperties;
 import org.bibsonomy.rest.client.AbstractQuery;
 import org.bibsonomy.rest.client.exception.ErrorPerformingRequestException;
-import org.bibsonomy.rest.exceptions.BadRequestOrResponseException;
 
 /**
  * Downloads a document for a specific post.
@@ -19,39 +14,38 @@ import org.bibsonomy.rest.exceptions.BadRequestOrResponseException;
 public class GetPostDocumentQuery extends AbstractQuery<File> {
 
 	private File document;
-	private String url;
-	private String username;
-	private String resourceHash;
+	private final String username;
+	private final String resourceHash;
 	private boolean fileExists;
-	
+
 	/**
 	 * @param username
 	 * @param resourceHash the resource hash of a specific post
 	 * @param fileName the file name of the document
 	 */
-	public GetPostDocumentQuery(String username, String resourceHash, String fileName) {
-		
-		if (username == null || username.length() == 0) throw new IllegalArgumentException("no username given");
-		if (resourceHash == null || resourceHash.length() == 0) throw new IllegalArgumentException("no resourceHash given");
-		if (fileName == null || fileName.length() == 0) throw new IllegalArgumentException("no file name given");
-		
+	public GetPostDocumentQuery(final String username, final String resourceHash, final String fileName) {
+
+		if ((username == null) || (username.length() == 0)) throw new IllegalArgumentException("no username given");
+		if ((resourceHash == null) || (resourceHash.length() == 0)) throw new IllegalArgumentException("no resourceHash given");
+		if ((fileName == null) || (fileName.length() == 0)) throw new IllegalArgumentException("no file name given");
+
 		this.username = username;
 		this.resourceHash = resourceHash;
-		
+
 		// create the file
 		try {
-			
-			document = new File("bibsonomy_docs/" + fileName);
-			this.fileExists = !document.createNewFile();
-		} catch (IOException ex) {
-			throw new IllegalArgumentException("could not create new file " + document.getAbsolutePath());
+
+			this.document = new File("bibsonomy_docs/" + fileName);
+			this.fileExists = !this.document.createNewFile();
+		} catch (final IOException ex) {
+			throw new IllegalArgumentException("could not create new file " + this.document.getAbsolutePath());
 		}
 	}
-	
+
 	@Override
 	protected File doExecute() throws ErrorPerformingRequestException {
-		if(!fileExists)
-			performFileDownload(URL_USERS + "/" + this.username + "/posts/" + this.resourceHash + "/documents/" + document.getName(), document);
+		if(!this.fileExists)
+			this.performFileDownload(URL_USERS + "/" + this.username + "/posts/" + this.resourceHash + "/documents/" + this.document.getName(), this.document);
 		else {
 			this.setExecuted(true);
 			this.setStatusCode(200);
