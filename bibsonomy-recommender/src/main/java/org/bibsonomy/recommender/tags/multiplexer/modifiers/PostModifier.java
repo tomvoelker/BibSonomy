cadb746a087ebc1a88083153f6c856a60fd66dbf
Folke Mitzlaff <mitzlaff@cs.uni-kassel.de>
2009-07-27 11:08:21 +0200

    * Added timeouts to http-requests * reduced log traffic * added filter for removing invalid scoring values

---
+++ bibsonomy-recommender/src/main/java/org/bibsonomy/recommender/tags/multiplexer/modifiers/PostModifier.java
@@ -0,0 +1,21 @@
+package org.bibsonomy.recommender.tags.multiplexer.modifiers;
+
+import org.bibsonomy.model.Post;
+import org.bibsonomy.model.Resource;
+
+/**
+ * Post modifiers arbitrarily change a post's content.
+ * 
+ * @author fei
+ * @version $Id$
+ */
+public interface PostModifier {
+
+	/**
+	 * post modifiers arbitrarily change a post's content
+	 * 
+	 * @param post the post to filter
+	 */
+	public void alterPost(Post<? extends Resource> post);
+
+}
