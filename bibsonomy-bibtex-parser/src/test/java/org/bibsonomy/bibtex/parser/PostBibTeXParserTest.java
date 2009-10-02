0ac8f60291d66786da27d269973ea55744b78645
Robert Jäschke <jaeschke@l3s.de>
2009-10-02 10:03:05 +0200

    added test class (TODO: implement tests!)

---
+++ bibsonomy-bibtex-parser/src/test/java/org/bibsonomy/bibtex/parser/PostBibTeXParserTest.java
@@ -0,0 +1,28 @@
+package org.bibsonomy.bibtex.parser;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author rja
+ * @version $Id$
+ */
+public class PostBibTeXParserTest {
+
+	@Test
+	public void testParseBibTeXPost() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateWithParsedBibTeX() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetParsedCopy() {
+//		fail("Not yet implemented");
+	}
+
+}
