52a177eef227ae3fd74c3a55a585256c55fb8fcf
Christian Claus <claus@cs.uni-kassel.de>
2009-10-24 12:46:48 +0200

    fixed casesjournal scraper

--- bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/casesjournal/CasesJournalScraper.java
+++ bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/casesjournal/CasesJournalScraper.java
@@ -23,23 +23,19 @@
 
 package org.bibsonomy.scraper.url.kde.casesjournal;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Collections;
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.bibsonomy.model.util.BibTexUtils;
 import org.bibsonomy.scraper.AbstractUrlScraper;
 import org.bibsonomy.scraper.ScrapingContext;
 import org.bibsonomy.scraper.Tuple;
-import org.bibsonomy.scraper.exceptions.InternalFailureException;
 import org.bibsonomy.scraper.exceptions.ScrapingException;
 import org.bibsonomy.scraper.exceptions.ScrapingFailureException;
-import org.bibsonomy.util.XmlUtils;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.bibsonomy.util.WebUtils;
 
 /**
  * @author claus
@@ -56,14 +52,7 @@ public class CasesJournalScraper extends AbstractUrlScraper {
 	 * needed URLs and components
 	 */
 	private static final String CASES_JOURNAL_HOST = "casesjournal.com";
-	private static final String CASES_JOURNAL_PATH  = "/casesjournal";
-	
-	private static final String CASES_JOURNAL_ID_PREFIX = "view/";
-	private static final String CASES_JOURNAL_BIBTEX_PATH = "/rt/captureCite/{id}/0/BibtexCitationPlugin";
-	private static final String CASES_JOURNAL_TAG_NAME = "pre";
-	private static final String CASES_JOURNAL_ID_PATTERN = "{id}";
-	private static final String CASES_JOURNAL_BIBTEX_KEY_PATTERN = "@article\\s*\\{(\\s*\\w+\\s*\\w*),.*";
-	private static final String SPACE_PATTERN = "\\s+";
+	private static final String CASES_JOURNAL_PATH  = "/content";
 	
 	private static final List<Tuple<Pattern, Pattern>> patterns = Collections.singletonList(new Tuple<Pattern, Pattern>(Pattern.compile(".*" + CASES_JOURNAL_HOST), Pattern.compile(CASES_JOURNAL_PATH + ".*")));
 
@@ -78,29 +67,17 @@ public class CasesJournalScraper extends AbstractUrlScraper {
 		
 		final String url = scrapingContext.getUrl().toString();
 		String bibtex = null;
-		String id = null;
+		String _url = url + "/citation";
 		
 		scrapingContext.setScraper(this);
 		
-		if (url.startsWith(CASES_JOURNAL_URL_BASE + CASES_JOURNAL_PATH)) {
-			id = url.substring(url.lastIndexOf(CASES_JOURNAL_ID_PREFIX) + CASES_JOURNAL_ID_PREFIX.length());
-		}
-		
-		URL downloadUrl;
 		try {
-			downloadUrl = new URL(CASES_JOURNAL_URL_BASE + CASES_JOURNAL_PATH
-					+ CASES_JOURNAL_BIBTEX_PATH.replace(CASES_JOURNAL_ID_PATTERN, id));
-			final Document doc = XmlUtils.getDOM(downloadUrl);
-			bibtex = extractBibtex(doc);
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new InternalFailureException(e);
+			bibtex = WebUtils.getPostContentAsString(new URL(_url), "action=submit&format=bibtex");
+		} catch (MalformedURLException ex) {
+			ex.printStackTrace();
+		} catch (IOException ex) {
+			ex.printStackTrace();
 		}
-		
-		/*
-		 * append URL
-		 */
-		BibTexUtils.addFieldIfNotContained(bibtex, "url", url);
 		
 		if(bibtex != null) {
 			scrapingContext.setBibtexResult(bibtex);
@@ -108,33 +85,6 @@ public class CasesJournalScraper extends AbstractUrlScraper {
 		}else
 			throw new ScrapingFailureException("getting bibtex failed");
 		
-	}
-
-	/**
-	 * Extracts the BibTeX Snippet, included in <pre>...bibtex...</pre> Tag
-	 * 
-	 * @param doc
-	 * @return 
-	 */
-	private String extractBibtex(final Document doc) {
-		String bibtex = null;
-		
-		final NodeList list = doc.getElementsByTagName(CASES_JOURNAL_TAG_NAME);
-		
-		if (list.getLength() > 0) {
-			Node node = list.item(0);
-			bibtex = XmlUtils.getText(node);
-			
-			// remove spaces in bibtex key
-			Pattern p = Pattern.compile(CASES_JOURNAL_BIBTEX_KEY_PATTERN, Pattern.DOTALL);
-			Matcher m = p.matcher(bibtex);
-
-			if (m.matches()) {
-				bibtex = bibtex.replace(m.group(1), m.group(1).replaceAll(SPACE_PATTERN, ""));
-			}
-		}
-		
-		return bibtex;
 	}
 
 	public String getInfo() {
@@ -148,6 +98,5 @@ public class CasesJournalScraper extends AbstractUrlScraper {
 	public String getSupportedSiteURL() {
 		return SITE_URL;
 	}
-
 	
 }
