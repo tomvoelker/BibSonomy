f6401e046840805ac657c05f1bfdf5a906ec6ed6
Björn Elmar Macek <ema@cs.uni-kassel.de>
2009-11-02 16:18:38 +0100

    changed location of view

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/view/Views.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/view/Views.java
@@ -190,7 +190,7 @@ public enum Views implements View {
 	SCRAPER_INFO("scraperList"),
 	
 	/** scraper info list **/
-	POST_BIBTEX("actions/post/postBibtex"),
+	POST_BIBTEX("actions/post/postTabPublication"),
 	/* *****************************************************
 	 * AJAX views
 	 * *****************************************************/
