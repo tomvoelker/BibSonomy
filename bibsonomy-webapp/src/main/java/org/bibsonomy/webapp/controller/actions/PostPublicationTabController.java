85c098b7656677d3353c3fb0a1ef633cb5fe9a1e
Björn Elmar Macek <ema@cs.uni-kassel.de>
2009-11-02 16:15:08 +0100

    renamed PostBibtexController

---
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/actions/PostPublicationTabController.java
@@ -0,0 +1,32 @@
+package org.bibsonomy.webapp.controller.actions;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.bibsonomy.webapp.command.PostBibtexCommand;
+import org.bibsonomy.webapp.controller.MultiResourceListController;
+import org.bibsonomy.webapp.controller.PopularPageController;
+import org.bibsonomy.webapp.util.MinimalisticController;
+import org.bibsonomy.webapp.util.View;
+import org.bibsonomy.webapp.view.Views;
+/**
+ * @author ema
+ * @version $Id$
+ */
+public class PostPublicationTabController extends MultiResourceListController implements MinimalisticController<PostBibtexCommand>{
+	private static final Log log = LogFactory.getLog(PopularPageController.class);
+	
+	
+	public View workOn(final PostBibtexCommand command) {
+		
+		log.debug(this.getClass().getSimpleName());
+		this.startTiming(this.getClass(), command.getFormat());
+		
+		this.endTiming();
+		return Views.POST_BIBTEX;			
+	}
+	
+	public PostBibtexCommand instantiateCommand() {
+		return new PostBibtexCommand();
+	}
+
+}
