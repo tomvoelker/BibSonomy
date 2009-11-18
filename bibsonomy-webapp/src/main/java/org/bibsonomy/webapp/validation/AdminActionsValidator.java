19a78b162fe87e986f8547eec877248dc7bf043b
Beate Krause <beatekr@cs.uni-kassel.de>
2009-11-18 15:31:53 +0100

    added validator to check input of regular expression field

---
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/validation/AdminActionsValidator.java
@@ -0,0 +1,76 @@
+package org.bibsonomy.webapp.validation;
+
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.bibsonomy.common.enums.ClassifierSettings;
+import org.bibsonomy.webapp.command.ajax.AdminAjaxCommand;
+import org.bibsonomy.webapp.util.Validator;
+import org.springframework.util.Assert;
+import org.springframework.validation.Errors;
+
+/**
+ * Validates fields related to admin tasks
+ * @author bkr
+ * @version $Id$
+ */
+public class AdminActionsValidator implements Validator<AdminAjaxCommand> {
+	
+	private static final Log log = LogFactory.getLog(AdminActionsValidator.class);
+
+	@SuppressWarnings("unchecked")
+	public boolean supports(final Class clazz) {
+		return AdminAjaxCommand.class.equals(clazz);
+	}
+
+	@Override
+	public void validate(Object settingsObj, Errors errors) {
+
+		/*
+		 * To ensure that the received command is not null, we throw an
+		 * exception, if this assertion fails.
+		 */
+		Assert.notNull(settingsObj);
+		final AdminAjaxCommand command = (AdminAjaxCommand) settingsObj;
+		
+		/*
+		 * Validate regular expression
+		 */
+		if (command.getKey() != null) {
+			if (ClassifierSettings.WHITELIST_EXP.equals(ClassifierSettings
+					.getClassifierSettings(command.getKey()))) {
+				if (!errors.hasFieldErrors("value"))
+					validateRegExForWhitelist(command.getValue(), errors);
+			}
+		}
+	}
+
+	/**
+	 * Validates the correctness of the regular expression for the whitelist The
+	 * regular expression is compiled - if an exception is thrown, the pattern
+	 * is not valid
+	 * 
+	 * @param regular
+	 *            expression
+	 * @param errors
+	 */
+	private void validateRegExForWhitelist(final String regex,
+			final Errors errors) {
+		if (regex == null || "".equals(regex.trim())) {
+			errors.rejectValue("value", "error.field.valid.admin.whitelist");
+		} else {
+
+			try {
+				Pattern pattern = Pattern.compile(regex);
+			} catch (PatternSyntaxException e) {
+				errors
+						.rejectValue("value",
+								"error.field.valid.admin.whitelist");
+			}
+		}
+
+	}
+
+}
