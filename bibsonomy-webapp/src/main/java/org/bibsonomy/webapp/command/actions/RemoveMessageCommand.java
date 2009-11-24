68e2c967a6aedd316602f316b9808938f6428587
Stephan Doerfel <doerfel@cs.uni-kassel.de>
2009-11-24 14:44:08 +0100

    deleteMessages in webapp

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/actions/RemoveMessageCommand.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/actions/RemoveMessageCommand.java
@@ -11,20 +11,35 @@ import org.bibsonomy.webapp.command.BaseCommand;
  */
 public class RemoveMessageCommand extends BaseCommand implements Serializable {
 	private static final long serialVersionUID = -6623936347565283765L;
-	private int contentId; //the post to which the inbox-message is to be removed
+	private String requestedResourceHash;
+	private String user;
 
 	/**
-	 * @return contentId
+	 * @return String
 	 */
-	public int getContentId() {
-		return this.contentId;
+	public String getUser() {
+		return this.user;
 	}
 
 	/**
-	 * @param contentId
+	 * @param user
 	 */
-	public void setContentId(final int contentId) {
-		this.contentId = contentId;
+	public void setUser(final String user) {
+		this.user = user;
+	}
+
+	/**
+	 * @return String
+	 */
+	public String getRequestedResourceHash() {
+		return this.requestedResourceHash;
+	}
+
+	/**
+	 * @param requestedResourceHash
+	 */
+	public void setRequestedResourceHash(final String requestedResourceHash) {
+		this.requestedResourceHash = requestedResourceHash;
 	}
 
 	
--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/actions/RemoveMessageController.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/actions/RemoveMessageController.java
@@ -1,9 +1,14 @@
 package org.bibsonomy.webapp.controller.actions;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.bibsonomy.model.BibTex;
+import org.bibsonomy.model.Post;
+import org.bibsonomy.model.Resource;
+import org.bibsonomy.model.User;
 import org.bibsonomy.model.logic.LogicInterface;
 import org.bibsonomy.webapp.command.actions.RemoveMessageCommand;
 import org.bibsonomy.webapp.util.ErrorAware;
@@ -35,38 +40,29 @@ public class RemoveMessageController implements MinimalisticController<RemoveMes
 	
 	public View workOn(RemoveMessageCommand command){
 		RequestWrapperContext context = command.getContext();
-		System.out.println("youve reached the deleteMessagecontroller");
 		/*
-		 * user has to be logged in to delete himself
+		 * user has to be logged in to delete
 		 */
 		if (!context.isUserLoggedIn()){
 			errors.reject("error.general.login");
 		}
-		
 		/*
 		 * check the ckey
 		 */
 		if (context.isValidCkey() && !errors.hasErrors()){
 			log.debug("User is logged in, ckey is valid");
-			
-			// delete the message with the given contentId
-			ArrayList<Integer> contentIds=new ArrayList<Integer>();
-			contentIds.add(command.getContentId());
-			
-			logic.deleteMessages(context.getLoginUser().getName(), contentIds);
-			
+			// delete the message
+			final List<Post<? extends Resource>> posts = createObjects(command);
+			logic.deleteInboxMessages(posts, false);
 		} else {
 			errors.reject("error.field.valid.ckey");
 		}
-		
-
 		/*
 		 * if there are errors, show them
 		 */
 		if (errors.hasErrors()){
 			return Views.ERROR;
 		}
-		
 		// go back where you've come from
 		return new ExtendedRedirectView(requestLogic.getReferer());
 	}
@@ -88,17 +84,32 @@ public class RemoveMessageController implements MinimalisticController<RemoveMes
 		this.logic = logic;
 	}
 	
-	/**
-	 * @return errors
-	 */
-	
-	
+
 	/**
 	 * @param requestLogic
 	 */
 	public void setRequestLogic(final RequestLogic requestLogic) {
 		this.requestLogic = requestLogic;
 	}	
-
-
+	
+	private List<Post<? extends Resource>> createObjects(RemoveMessageCommand command){
+		// create new list and necessary variables
+		final List<Post<? extends Resource>> posts = new ArrayList<Post<? extends Resource>>();
+		
+		// get the has string
+		final String hash = command.getRequestedResourceHash();
+		/*
+		 * add one post - "pick one"
+		 */
+		final Post<BibTex> post = new Post<BibTex>();
+		final User user = new User();
+		//we could any Resource but since we don't need this information in the inbox...
+		final BibTex bib = new BibTex();
+		bib.setIntraHash(hash);
+		post.setResource(bib);
+		user.setName(command.getUser());
+		post.setUser(user);
+		posts.add(post);
+		return posts;
+	}
 }
