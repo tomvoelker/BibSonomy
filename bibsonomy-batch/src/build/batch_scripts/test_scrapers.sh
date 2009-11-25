21dd3ae5b3fd0fb24096fbfc65b42b718caa78a1
BibSonomy <webmaster@bibsonomy.org>
2009-11-25 13:39:15 +0100

    added scraper test script to CVS

---
+++ bibsonomy-batch/src/build/batch_scripts/test_scrapers.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+#
+# Runs the scraper tests by using the latest builds from continuum
+# 
+# 
+# Input:
+# - Environment variables AmazonAccessKey and AmazonSecretKey must
+#   be set!
+# 
+# Output:
+# - test runs results on STDOUT/STDERR
+# 
+# Changes:
+# rja, 2009-10-16
+# - added head comments
+# - removed amazon access keys
+# - documented additional xerces JAR
+#
+
+##########################################################################
+# config variables
+# 
+# directory on this machine, where the BibSonomy (and other needed) JAR's
+# are (except bibsonomy-scraper-test*.jar)
+TOMCAT="/home/bibsonomy/tomcat/"
+JARDIR=$TOMCAT"/webapps/bibsonomy-webapp/WEB-INF/lib"
+
+#
+# directory on the remote continuum machine, where the 
+# bibsonomy-scraper-test*.jar is 
+REPOSI="shaun:/home/stud/continuum/.m2/repository/org/bibsonomy/bibsonomy-scraper"
+##########################################################################
+# first: copy latest scraper-test JAR to this machine ...
+# 
+# get version number of current scraper JAR in tomcat dir
+VERSION=$(ls $JARDIR/bibsonomy-scraper*.jar | sed -e 's/.*-//' -e 's/.jar//')
+#
+# copy corresponding test JAR from continuum to /tmp
+#
+TESTJAR=bibsonomy-scraper-${VERSION}-tests.jar
+FROM=$REPOSI/$VERSION/$TESTJAR
+TO=/tmp/$TESTJAR
+echo "copying version $VERSION of scraper test JAR from $FROM to $TO"
+scp $FROM $TO
+
+
+##########################################################################
+# set class path
+#
+# initial:
+#  - scraper test JAR in /tmp
+#  - log4j JAR in tomcat/common/lib
+#  - xerces in tomcat/common/lib
+CLASSPATH=$TO:$(ls $TOMCAT/common/lib/log4j*.jar):$(ls $TOMCAT/common/lib/xerces*.jar)
+for jar in $JARDIR/*.jar; do
+  CLASSPATH=$CLASSPATH:$jar
+done
+
+
+##########################################################################
+# run tests
+java -cp $CLASSPATH org.bibsonomy.scraper.UnitTestRunner
+
