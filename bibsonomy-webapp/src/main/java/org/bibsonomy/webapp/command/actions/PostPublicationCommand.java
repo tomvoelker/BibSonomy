b7859ec3e184fcfac9c3996690aade4c6da8945b
Björn Elmar Macek <ema@cs.uni-kassel.de>
2009-11-30 09:53:35 +0100

    commit current version. not completely adapted for all 3 views yet.

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/actions/PostPublicationCommand.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/actions/PostPublicationCommand.java
@@ -22,27 +22,145 @@ import org.springframework.web.multipart.commons.CommonsMultipartFile;
  */
 public class PostPublicationCommand extends EditPublicationCommand implements TabsCommandInterface<Object> {
 	
-	protected Integer selTab = 1;	
-
-	/** list of defined tabs */
-	protected List<TabCommand> tabs = new ArrayList<TabCommand>();
 	
+	/****************************
+	 * FOR THE TAB FUNCTIONALITY
+	 ****************************/
+	
+	/*
+	 * SPECIAL FOR TABCOMMAND
+	 */
 	private final static String[] tabTitles = {
 		"post_bibtex.manual.title", 
 		"post_bibtex.pub_snippet.title", 
 		"post_bibtex.bibtex_endnote.title", 
 		"post_bibtex.doi_isbn.title"
 	};
-
-	public PostPublicationCommand(){
-		
-		// Preparation for all tabs
-		//=== make the tabtitle available
-		addTabs(tabTitles);
-
-		//=== change default tab to the manual tab
-		selTab = 0;
+	
+	
+	/*
+	 *  id of current selected tab 
+	 */
+	protected Integer selTab = 1;
+	
+	
+	private void addTab(Integer id, String title) {
+		tabs.add(new TabCommand(id, title));		
 	}
+
+	private void addTabs(final String[] titles) {
+		for(int i=0; i<titles.length; i++) {
+			addTab(i, titles[i]);
+		}
+	}
+
+	/*
+	 * dont know what this is for really...
+	 */
+	private List<TabCommand> tabs;
+	
+	@Override
+	public Integer getSelTab() {
+		return selTab;
+	}
+	
+	@Override
+	public void setSelTab(Integer selectedTab) {
+		this.selTab = selectedTab;
+	}
+
+	@Override
+	public List<TabCommand> getTabs() {
+		return tabs;
+	}
+
+	@Override
+	public void setTabs(List<TabCommand> tabs) {
+		this.tabs = tabs;
+	}
+	
+	
+	
+	/****************************
+	 * SPECIAL FOR FILE UPLOAD
+	 ****************************/
+	
+	/**
+	 * the BibTeX/Endnote file
+	 */
+	private CommonsMultipartFile file;
+	
+	
+	public CommonsMultipartFile getFile() {
+		return this.file;
+	}
+
+	public void setFile(CommonsMultipartFile file) {
+		this.file = file;
+	}
+	
+	/*
+	 * The whitespace substitute
+	 */
+	private String whitespace;
+	
+	public String getWhitespace() {
+		return this.whitespace;
+	}
+
+	public void setWhitespace(String whitespace) {
+		this.whitespace = whitespace;
+	}
+
+	/*
+	 * encoding of the file
+	 */
+	private String encoding;
+	
+	public String getEncoding() {
+		return this.encoding;
+	}
+
+	public void setEncoding(String encoding) {
+		this.encoding = encoding;
+	}
+
+	/*
+	 * the delimiter
+	 */
+	private String delimiter;
+
+	public String getDelimiter() {
+		return this.delimiter;
+	}
+
+	public void setDelimiter(String delimiter) {
+		this.delimiter = delimiter;
+	}
+
+	
+	
+	/***********************************************
+	 * FOR IMPORTS CONTAINING MULTIPLE PUBLICATIONS
+	 ***********************************************/
+	/*
+	 * The action that will be started, when hitting the submission button on the edit page
+	 */
+	private String formAction;
+	
+	public String getFormAction() {
+		return this.formAction;
+	}
+
+	public void setFormAction(String formAction) {
+		this.formAction = formAction;
+	}
+	
+	
+	
+	/****************************
+	 * FOR ALL KINDS OF IMPORTS
+	 ****************************/
 	
 	/*
 	 * Determines, if the bookmarks will be saved before being edited or afterwards
@@ -61,6 +179,7 @@ public class PostPublicationCommand extends EditPublicationCommand implements Ta
 		this.editBeforeImport = editBeforeImport;
 	}
 
+	
 	/*
 	 * Determines, if the already existing publications will be overwritten by the new ones.
 	 */
@@ -77,7 +196,8 @@ public class PostPublicationCommand extends EditPublicationCommand implements Ta
 	public void setOverwrite(boolean overwrite) {
 		this.overwrite = overwrite;
 	}
-
+	
+	
 	/*
 	 * For multiple posts
 	 */
@@ -91,60 +211,22 @@ public class PostPublicationCommand extends EditPublicationCommand implements Ta
 		this.bibtex = bibtex;
 	}
 
-	/**
-	 * the BibTeX file
-	 */
-	private CommonsMultipartFile file;
 	
-	/**
-	 * @return the file containing the BibTeX entries.
-	 */
-	public CommonsMultipartFile getFile() {
-		return this.file;
-	}
-
-	/**
-	 * Sets the file containing the BibTeX entries.
-	 * 
-	 * @param file
-	 */
-	public void setFile(CommonsMultipartFile file) {
-		this.file = file;
-	}
-
-	/**
+	/*
 	 * the description of the snippet/upload file
 	 */
 	private String description;
 	
 	
-	/**
-	 * @return the string describing the BibTeX snippet.
-	 */
 	public String getDescription() {
 		return this.description;
 	}
 	
-	/**
-	 * Sets the string describing the BibTeX snippet to import.
-	 * 
-	 * @param description
-	 */
 	@Override
 	public void setDescription(String description) {
 		this.description = description;
 	}
 
-	
-	private void addTab(Integer id, String title) {
-		tabs.add(new TabCommand(id, title));		
-	}
-
-	private void addTabs(final String[] titles) {
-		for(int i=0; i<titles.length; i++) {
-			addTab(i, titles[i]);
-		}
-	}
 
 	@Override
 	public List<Object> getContent() {
@@ -153,28 +235,20 @@ public class PostPublicationCommand extends EditPublicationCommand implements Ta
 	}
 
 	@Override
-	public Integer getSelTab() {
-		return selTab;
-	}
-
-	@Override
-	public List<TabCommand> getTabs() {
-		return tabs;
-	}
-
-	@Override
 	public void setContent(List<Object> content) {
 		// TODO Auto-generated method stub
 	}
 
-	@Override
-	public void setSelTab(Integer selectedTab) {
-		this.selTab = selectedTab;
-	}
+	
 
-	@Override
-	public void setTabs(List<TabCommand> tabs) {
-		this.tabs = tabs;
+	public PostPublicationCommand(){
+		tabs = new ArrayList<TabCommand>();
+		// Preparation for all tabs
+		//=== make the tabtitle available
+		addTabs(tabTitles);
+
+		//=== change default tab to the manual tab
+		selTab = 0;
 	}
 
 	
