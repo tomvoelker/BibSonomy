7995d1d3d7d7b7a7ef3991540711c057c3273077
Robert Jäschke <jaeschke@l3s.de>
2009-11-30 17:54:31 +0100

    updated date

--- bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/util/SignedRequestsHelper.java
+++ bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/util/SignedRequestsHelper.java
@@ -1,22 +1,24 @@
-/**********************************************************************************************
- * Copyright 2009 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file 
- * except in compliance with the License. A copy of the License is located at
- *
- *       http://aws.amazon.com/apache2.0/
- *
- * or in the "LICENSE.txt" file accompanying this file. This file is distributed on an "AS IS"
- * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under the License. 
- *
- * ********************************************************************************************
- *
- *  Amazon Product Advertising API
- *  Signed Requests Sample Code
- *
- *  API Version: 2009-03-31
- *
+/**
+ *  
+ *  BibSonomy-Scraper - Web page scrapers returning BibTeX for BibSonomy.
+ *   
+ *  Copyright (C) 2006 - 2009 Knowledge & Data Engineering Group, 
+ *                            University of Kassel, Germany
+ *                            http://www.kde.cs.uni-kassel.de/
+ *  
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
 package org.bibsonomy.scraper.util;
