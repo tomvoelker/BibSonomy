ff00ce52d688bacab89a46817b7ea81ac0d9914c
Robert Jäschke <jaeschke@l3s.de>
2009-11-30 17:55:33 +0100

    updated date

--- bibsonomy-scrapingservice/src/main/java/org/bibsonomy/scrapingservice/servlets/ScrapingServlet.java
+++ bibsonomy-scrapingservice/src/main/java/org/bibsonomy/scrapingservice/servlets/ScrapingServlet.java
@@ -3,7 +3,7 @@
  *  BibSonomy-Scrapingservice - Web application to test the BibSonomy web page scrapers (see
  * 		bibsonomy-scraper)
  *   
- *  Copyright (C) 2006 - 2008 Knowledge & Data Engineering Group, 
+ *  Copyright (C) 2006 - 2009 Knowledge & Data Engineering Group, 
  *                            University of Kassel, Germany
  *                            http://www.kde.cs.uni-kassel.de/
  *  
