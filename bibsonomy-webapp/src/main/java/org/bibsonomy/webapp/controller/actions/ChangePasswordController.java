463c51a48904b3698e9d4143070064e0d0177035
voigtmannc <devnull@localhost>
2009-12-02 17:37:14 +0100

    add some additional changes

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/actions/ChangePasswordController.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/actions/ChangePasswordController.java
@@ -15,20 +15,22 @@ import org.bibsonomy.webapp.util.ErrorAware;
 import org.bibsonomy.webapp.util.MinimalisticController;
 import org.bibsonomy.webapp.util.RequestLogic;
 import org.bibsonomy.webapp.util.RequestWrapperContext;
+import org.bibsonomy.webapp.util.ValidationAwareController;
+import org.bibsonomy.webapp.util.Validator;
 import org.bibsonomy.webapp.util.View;
+import org.bibsonomy.webapp.validation.ChangePasswordValidator;
 import org.bibsonomy.webapp.view.Views;
 import org.springframework.validation.Errors;
 
 /**
  * @author cvo
- * @version $Id$
+ * @version $Id: ChangePasswordController.java,v 1.4 2009-12-02 12:51:14
+ *          voigtmannc Exp $
  */
-public class ChangePasswordController  implements
-		MinimalisticController<SettingsViewCommand>, ErrorAware {
+public class ChangePasswordController implements MinimalisticController<SettingsViewCommand>, ErrorAware, ValidationAwareController<SettingsViewCommand> {
 
-	private static final Log log = LogFactory
-			.getLog(SearchPageController.class);
-	
+	private static final Log log = LogFactory.getLog(SearchPageController.class);
+
 	private static final String TAB_URL = "/settingsnew";
 
 	/**
@@ -40,12 +42,12 @@ public class ChangePasswordController  implements
 	 * logic interface
 	 */
 	private LogicInterface adminLogic = null;
-	
+
 	/**
 	 * cookie logic
 	 */
 	private CookieLogic cookieLogic = null;
-	
+
 	/**
 	 * request logic interface
 	 */
@@ -61,98 +63,91 @@ public class ChangePasswordController  implements
 
 	@Override
 	public View workOn(SettingsViewCommand command) {
-		
+
 		RequestWrapperContext context = command.getContext();
-		
-		Integer statusID = null;
-		
+	
 		/*
 		 * user has to be logged in to delete himself
 		 */
-		if (!context.isUserLoggedIn()){
+		if (!context.isUserLoggedIn()) {
 			errors.reject("error.general.login");
-		}else {
+			return Views.SETTINGSPAGE;
+		} else {
 			command.setUser(context.getLoginUser());
 		}
-		
+
+		/**
+		 * go back to the settings page and display errors from command field
+		 * validation
+		 */
+		if (errors.hasErrors()) {
+			return Views.SETTINGSPAGE;
+		}
+
 		/*
 		 * check the ckey
 		 */
-		if (context.isValidCkey() && !errors.hasErrors()){
+		if (context.isValidCkey()) {
 			log.debug("User is logged in, ckey is valid");
-			
-			//change password here
+
+			// change password here
 			System.out.println("password can be changed here");
-			statusID = changePassword(context.getLoginUser(), command);
-			
+			command.setStatusID(changePassword(context.getLoginUser(), command));
+
 		} else {
 			errors.reject("error.field.valid.ckey");
 		}
-		
-		
-		/*
-		 * if there are errors, show them
-		 */
-		if (errors.hasErrors()){
-			return Views.ERROR;
-		}
-		
+
+
 		return Views.SETTINGSPAGE;
 	}
-	
+
 	private int changePassword(User user, SettingsViewCommand command) {
-		
+
 		String curPassword = user.getPassword();
-		
-		Integer msgID = null;
-		
+		int msgID;
 		// create the md5 hash of the new password
 		final String hashedOldPassword = StringUtils.getMD5Hash(command.getOldPassword());
-		
-		if(curPassword.equals(hashedOldPassword)) {
-					
-			//new password was typed identical twice
-			if(command.getNewPassword().equals(command.getNewPasswordRetype())) {
+
+		if (curPassword.equals(hashedOldPassword)) {
+
+			String newPasswordHash = StringUtils.getMD5Hash(command.getNewPassword());
+
+			user.setPassword(newPasswordHash);
+
+			String updatedUser = adminLogic.updateUser(user, UserUpdateOperation.UPDATE_PASSWORD);
+
+			cookieLogic.addUserCookie(user.getName(), newPasswordHash);
+
+			requestLogic.invalidateSession();
+
+			log.info("password of " + updatedUser + " has been changed successfully");
 			
-				String newPasswordHash = StringUtils.getMD5Hash(command.getNewPassword());
-								
-				user.setPassword(newPasswordHash);
-				
-				String updatedUser = adminLogic.updateUser(user, UserUpdateOperation.UPDATE_PASSWORD);
-				
-				cookieLogic.addUserCookie(user.getName(), newPasswordHash);
-				
-				requestLogic.invalidateSession();
-				
-				msgID = SettingPageMsg.PASSWORD_CHANGED_SUCCESS.getId();
-				
-				log.info("password of " + updatedUser + " has been changed successfully");
-				
-			}else {
-				msgID = SettingPageMsg.PASSWORD_RETYPE_ERROR.getId();
-			}
-		}else {// old password is wrong
+			msgID = SettingPageMsg.PASSWORD_CHANGED_SUCCESS.getId();
 			
-			msgID = SettingPageMsg.PASSWORD_CURRENT_ERROR.getId();
-		}	
+		} else {// old password is wrong
+			msgID = SettingPageMsg.IDLE.getId();
+			errors.reject("error.settings.password.incorect");			
+		}
 		
 		return msgID;
 	}
 
 	@Override
 	public Errors getErrors() {
-		
+
 		return this.errors;
 	}
 
 	@Override
 	public void setErrors(Errors errors) {
-		
-		this.errors = errors;		
+
+		this.errors = errors;
 	}
 
 	/**
 	 * sets the adming logic interface
+	 * 
 	 * @param adminLogic
 	 */
 	public void setAdminLogic(LogicInterface adminLogic) {
@@ -161,6 +156,7 @@ public class ChangePasswordController  implements
 
 	/**
 	 * sets the cookie logic interface
+	 * 
 	 * @param cookieLogic
 	 */
 	public void setCookieLogic(CookieLogic cookieLogic) {
@@ -169,9 +165,22 @@ public class ChangePasswordController  implements
 
 	/**
 	 * sets the request logic interface
+	 * 
 	 * @param requestLogic
 	 */
 	public void setRequestLogic(RequestLogic requestLogic) {
 		this.requestLogic = requestLogic;
+	}
+
+	@Override
+	public Validator<SettingsViewCommand> getValidator() {
+
+		return new ChangePasswordValidator();
+	}
+
+	@Override
+	public boolean isValidationRequired(SettingsViewCommand command) {
+
+		return true;
 	}
 }
