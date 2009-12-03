0d48502145443e94de4682dd5e974744cbef745b
econ11 <devnull@localhost>
2009-12-03 16:56:12 +0100

    Cv-Page with list of own publications

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/CvPageController.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/CvPageController.java
@@ -1,17 +1,25 @@
 package org.bibsonomy.webapp.controller;
 
+import java.util.ArrayList;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.bibsonomy.common.enums.GroupingEntity;
+import org.bibsonomy.model.BibTex;
+import org.bibsonomy.model.Post;
+import org.bibsonomy.model.Resource;
+import org.bibsonomy.model.Tag;
+import org.bibsonomy.model.User;
 import org.bibsonomy.webapp.command.CvPageCommand;
 import org.bibsonomy.webapp.util.MinimalisticController;
 import org.bibsonomy.webapp.util.View;
 import org.bibsonomy.webapp.view.Views;
+
 /**
- * @author philipp
+ * @author Philipp Beau
  * @version $Id$
  */
-public class CvPageController extends SingleResourceListController implements MinimalisticController<CvPageCommand> {
+public class CvPageController extends ResourceListController implements MinimalisticController<CvPageCommand> {
 	private static final Log log = LogFactory.getLog(CvPageController.class);
 
 
@@ -19,26 +27,57 @@ public class CvPageController extends SingleResourceListController implements Mi
 		 * implementation of {@link MinimalisticController} interface
 		 */
 		public View workOn(CvPageCommand command) {
-			// fill out overhead
 			command.setPageTitle("Curriculum vitae");
 			
-			if(!(command.getRequestedUser() == null)) {
-			command.setUser(this.logic.getUserDetails(command.getRequestedUser()));
+			final String requUser = command.getRequestedUser();
+			User requUserDetail;
+
+			if(!(requUser == null)) {
+			requUserDetail = this.logic.getUserDetails(requUser);
+			command.setUser(requUserDetail);
 			} else {
 				
 				return Views.ERROR;
 			}
+			
+			final String groupingName = command.getRequestedUser();
+			final GroupingEntity groupingEntity = GroupingEntity.USER;
+						
 			/*
-			//user has to be logged in to see his cv
-			if(command.getContext().isUserLoggedIn()) {
-			command.setUser(command.getContext().getLoginUser());
-			} else {
+			 * retrieve and set the requested bibtex(s)
+			 */
+			for (final Class<? extends Resource> resourceType : listsToInitialise) {
 
-				log.warn("Invalid query /user without username");
-				throw new MalformedURLSchemeException("error.general.login");
-//				return Views.ERROR;
+				final int entriesPerPage = command.getListCommand(resourceType).getEntriesPerPage();		
+				this.setList(command, resourceType, groupingEntity, requUser, null, null, null, null, null, entriesPerPage);
+				
+				if (GroupingEntity.ALL.equals(groupingEntity)) {
+					/* 
+					 * retrieve total count with given hash 
+					 * (only for /bibtex/HASH)
+					 */
+					this.setTotalCount(command, resourceType, groupingEntity, requUser, null, null, null, null, null, entriesPerPage, null);
+				} else if (GroupingEntity.USER.equals(groupingEntity)) {
+					/*
+					 * complete post details for a single post of a given user 
+					 * (only for /bibtex/HASH/USER)
+					 */
+
+					final ArrayList<Post<BibTex>> bibtex = new ArrayList<Post<BibTex>>();
+					for (final Post<BibTex> b : command.getBibtex().getList()){
+						for(Tag t : b.getTags())
+							//if settings page exists 
+							//if(t.equals(requUserDetail.getMYOWNTAG)) {
+							if(t.getName().equals("myown")) {
+								bibtex.add((Post<BibTex>) this.logic.getPostDetails(b.getResource().getIntraHash(), b.getUser().getName()));
+							}
+					}			
+					
+					
+					command.getBibtex().setList(bibtex);			
+				}
 			}
-			*/
+
 			return Views.CVPAGE;
 		}
 
