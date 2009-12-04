27c8ccfdb96101bb281bb36513c9a60af047fb06
Christian Claus <claus@cs.uni-kassel.de>
2009-12-04 12:23:23 +0100

    fixed aifb scraper fixed aifb scraper tests re-enabled cases journal test

--- bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/karlsruhe/AIFBScraper.java
+++ bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/karlsruhe/AIFBScraper.java
@@ -24,248 +24,107 @@
 package org.bibsonomy.scraper.url.kde.karlsruhe;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.bibsonomy.model.util.BibTexUtils;
 import org.bibsonomy.scraper.AbstractUrlScraper;
 import org.bibsonomy.scraper.ScrapingContext;
 import org.bibsonomy.scraper.Tuple;
-import org.bibsonomy.scraper.exceptions.InternalFailureException;
 import org.bibsonomy.scraper.exceptions.ScrapingException;
 import org.bibsonomy.util.WebUtils;
 
 
 /** Scraper for AIFB.
  * 
- * @author rja
+ * @author ccl
  *
  */
 public class AIFBScraper extends AbstractUrlScraper {
 	
-	private static final String SITE_NAME = "Institut AIFB Universität Karlsruhe";
-	private static final String AIFB_HOST_NAME = "http://www.aifb.uni-karlsruhe.de";
-	private static final String SITE_URL = AIFB_HOST_NAME+"/";
-	private static final String info = "This scraper parses institute, research group and people-specific pages from the " +
-									   href("http://www.aifb.uni-karlsruhe.de/", SITE_NAME);
-
-	private static final String AIFB_HOST = "aifb.uni-karlsruhe.de";
-	private static final String AIFB_DEPT_PATH = "/Forschungsgruppen/";
-	private static final String AIFB_PERSON_PATH = "/Personen/viewPerson";	
-	private static final String AIFB_PUBL_PATH = "/Publikationen/showPublikation";
-	private static final String AIFB_PUBL_PATH_ENGLISH = "/Publikationen/showPublikation_english";
-	private static final String AIFB_PUBL_PERSON_PATH = "/Publikationen/showPublikationen";
-	private static final String AIFB_PUBL_EXPORT_PATH = "/Publikationen/exportPublikation.bib";
-	private static final String AIFB_PUBL_PERSON_EXPORT_PATH = "/Publikationen/exportPublikationenPerson.bib";
-	private static final String AIFB_PUBL_DEPT_EXPORT_PATH = "/Publikationen/exportPublikationenFG.bib";
-
-    //person id param
-	private static final String AIFB_PARAM_PERSON_ID = "person_id=";
-	//dept. id param
-	private static final String AIFB_PARAM_DEPT_ID = "fg_id=";
-    //year of publication param
-	private static final String AIFB_PARAM_YEAR = "jahr=";
+	private static final String SITE_NAME		= "Institut AIFB Universität Karlsruhe";
+	private static final String AIFB_SITE_NAME	= "AIFB";
+	private static final String AIFB_HOST_NAME	= "http://www.aifb.kit.edu";
+	private static final String AIFB_HOST		= "aifb.kit.edu";
+	private static final String AIFB_WEB		= "/web/";
+	private static final String info			= "This scraper parses institute, research group and " +
+												  "people-specific pages from the " +
+												  href("http://www.aifb.uni-karlsruhe.de/", SITE_NAME);
 	
-	//dept. code of  Betriebliche Informations- und Kommunikationssysteme
-	private static final String AIFB_DEPT_CODE_BIK = "BIK";
-    //	dept. code of  Betriebliche Informations- und Kommunikationssysteme
-	private static final int AIFB_DEPT_ID_BIK = 1;
-	
-	//dept. code of  Effiziente Algorithmen
-	private static final String AIFB_DEPT_CODE_EFFALG = "EffAlg";
-    //	dept. code of  Effiziente Algorithmen
-	private static final int AIFB_DEPT_ID_EFFALG = 2;
-	
-	//dept. code of Wissensmanagement
-	private static final String AIFB_DEPT_CODE_WBS = "WBS";
-    //dept. id of Wissensmanagement
-	private static final int AIFB_DEPT_ID_WBS = 3;
-	
-	//dept. code of Komplexitätsmanagement
-	private static final String AIFB_DEPT_CODE_COM = "CoM";
-    //dept. id of Komplexitätsmanagement
-	private static final int AIFB_DEPT_ID_COM = 4;
-
+    private static final String DOWNLOAD_HREF_STRING	= "<a href=\"(.+?;format=bibtex)\"";
+    private static final Pattern DOWNLOAD_HREF_PATTERN	= Pattern.compile(DOWNLOAD_HREF_STRING);
+    
+    private static final String AMP		 = "&amp;";
+    private static final String AMP_REPL = "&";
+    private static final String URL		 = "url";
+    
+    private static final String WC				= ".*";
+    private static final String ARTICLE			= "Article\\d+";
+    private static final String INPROCEEDINGS	= "Inproceedings\\d+";
+    private static final String BOOK			= "Book\\d+";
+    private static final String INCOLLECTION	= "Incollection\\d+";
+    private static final String PROCEEDINGS		= "Proceedings\\d+";
+    private static final String PHDTHESIS		= "Phdthesis\\d+";
+    private static final String TECHREPORT		= "Techreport\\d+";
+    private static final String DELIVERABLE		= "Deliverable\\d+";
+    private static final String UNPUBLISHED		= "Unpublished\\d+";
+    
 	private static final List<Tuple<Pattern,Pattern>> patterns = new LinkedList<Tuple<Pattern,Pattern>>();
 
     static {
-		patterns.add(new Tuple<Pattern, Pattern>(Pattern.compile(".*" + AIFB_HOST), Pattern.compile(AIFB_PERSON_PATH + ".*")));
-		patterns.add(new Tuple<Pattern, Pattern>(Pattern.compile(".*" + AIFB_HOST), Pattern.compile(AIFB_PUBL_DEPT_EXPORT_PATH + ".*")));
-		patterns.add(new Tuple<Pattern, Pattern>(Pattern.compile(".*" + AIFB_HOST), Pattern.compile(AIFB_PUBL_EXPORT_PATH + ".*")));
-		patterns.add(new Tuple<Pattern, Pattern>(Pattern.compile(".*" + AIFB_HOST), Pattern.compile(AIFB_PUBL_PATH + ".*")));
-		patterns.add(new Tuple<Pattern, Pattern>(Pattern.compile(".*" + AIFB_HOST), Pattern.compile(AIFB_PUBL_PATH_ENGLISH + ".*")));
-		patterns.add(new Tuple<Pattern, Pattern>(Pattern.compile(".*" + AIFB_HOST), Pattern.compile(AIFB_PUBL_PERSON_EXPORT_PATH + ".*")));
-		patterns.add(new Tuple<Pattern, Pattern>(Pattern.compile(".*" + AIFB_HOST), Pattern.compile(AIFB_PUBL_PERSON_PATH + ".*")));
-		patterns.add(new Tuple<Pattern, Pattern>(Pattern.compile(".*" + AIFB_HOST), Pattern.compile(AIFB_DEPT_PATH + ".*")));
+    	patterns.add(new Tuple<Pattern, Pattern>(Pattern.compile(WC + AIFB_HOST), Pattern.compile(AIFB_WEB + ARTICLE)));
+    	patterns.add(new Tuple<Pattern, Pattern>(Pattern.compile(WC + AIFB_HOST), Pattern.compile(AIFB_WEB + INPROCEEDINGS)));
+    	patterns.add(new Tuple<Pattern, Pattern>(Pattern.compile(WC + AIFB_HOST), Pattern.compile(AIFB_WEB + BOOK)));
+    	patterns.add(new Tuple<Pattern, Pattern>(Pattern.compile(WC + AIFB_HOST), Pattern.compile(AIFB_WEB + INCOLLECTION)));
+    	patterns.add(new Tuple<Pattern, Pattern>(Pattern.compile(WC + AIFB_HOST), Pattern.compile(AIFB_WEB + PROCEEDINGS)));
+    	patterns.add(new Tuple<Pattern, Pattern>(Pattern.compile(WC + AIFB_HOST), Pattern.compile(AIFB_WEB + PHDTHESIS)));
+    	patterns.add(new Tuple<Pattern, Pattern>(Pattern.compile(WC + AIFB_HOST), Pattern.compile(AIFB_WEB + TECHREPORT)));
+    	patterns.add(new Tuple<Pattern, Pattern>(Pattern.compile(WC + AIFB_HOST), Pattern.compile(AIFB_WEB + DELIVERABLE)));
+    	patterns.add(new Tuple<Pattern, Pattern>(Pattern.compile(WC + AIFB_HOST), Pattern.compile(AIFB_WEB + UNPUBLISHED)));
     }
 	
-	
-	
 	protected boolean scrapeInternal(ScrapingContext sc) throws ScrapingException {
+
+		String bibtex = null;
 		
 		if(sc.getSelectedText() == null){
 			/*
 			 * returns itself to know, which scraper scraped this
 			 */
 			sc.setScraper(this);
-
-		
-			if(sc.getUrl().getPath().contains(AIFB_PUBL_EXPORT_PATH)){
-				/* We are scraping a bibtex page!
-				 * URL must be some like this:
-				 * http://www.aifb.uni-karlsruhe.de/Publikationen/exportPublikation.bib?publ_id=867
-				 * Just set the content as the result, because it contains just a bibtex entry.
-				 */
-				sc.setBibtexResult(sc.getPageContent());	
-				return true;
-			}else if(AIFB_PUBL_PERSON_EXPORT_PATH.equals(sc.getUrl().getPath())){
-				/* We are scraping a bibtex page!
-				 * URL must be some like this:
-				 * http://www.aifb.uni-karlsruhe.de/Publikationen/exportPublikationenPerson.bib?person_id=3
-				 * Just set the content as the result, because it already contains bibtex entries.
-				 */
-				sc.setBibtexResult(sc.getPageContent());	
-				return true;
-			}else if(AIFB_PUBL_DEPT_EXPORT_PATH.equals(sc.getUrl().getPath())){
-				/* We are scraping a bibtex page!
-				 * URL must be some like this:
-				 * http://www.aifb.uni-karlsruhe.de/Publikationen/exportPublikationenFG.bib?fg_id=3&jahr=2006 (publ. of specific dept.) or
-				 * http://www.aifb.uni-karlsruhe.de/Publikationen/exportPublikationenFG.bib?jahr=2006 (publ. of all depts.)
-				 * Just set the content as the result, because it already contains bibtex entries.
-				 */
-				sc.setBibtexResult(sc.getPageContent());	
-				return true;
-			}else if(AIFB_PUBL_PATH.equals(sc.getUrl().getPath()) || AIFB_PUBL_PATH_ENGLISH.equals(sc.getUrl().getPath())){
-				/* We are scraping a publication page. Just extract the parameter publ_id
-				 * to call export publication page.
-				 * URL must be like this: 
-				 * http://www.aifb.uni-karlsruhe.de/Publikationen/showPublikation?publ_id=867
-				 * Scrape following URL to get the adequate bibtex entry:
-				 * http://www.aifb.uni-karlsruhe.de/Publikationen/exportPublikationen.bib?publ_id=867
-				 */
+			Matcher _m = DOWNLOAD_HREF_PATTERN.matcher(sc.getPageContent());
+			
+			if (_m.find()) {
 				try {
-					URL expURL = new URL(AIFB_HOST_NAME + AIFB_PUBL_EXPORT_PATH + "?" + sc.getUrl().getQuery());
-					sc.setBibtexResult(WebUtils.getContentAsString(expURL));
-					return true;
-				} catch (IOException me) {
-					throw new InternalFailureException(me);
-				}
-				
-			}else if(sc.getUrl().getPath().contains(AIFB_PERSON_PATH)){
-				/* We are scraping a person page. Just extract id_db and call the export page.
-				 * URL must be like this: 
-				 * http://www.aifb.uni-karlsruhe.de/Personen/viewPerson?id_db=29 or
-				 * http://www.aifb.uni-karlsruhe.de/Personen/viewPersonenglish?id_db=29
-				 * Scrape following URL to get the adequate bibtex entries:
-				 * http://www.aifb.uni-karlsruhe.de/Publikationen/exportPublikationenPerson.bib?person_id=29
-				 */
-				try {					
-					URL expURL = new URL(AIFB_HOST_NAME + AIFB_PUBL_PERSON_EXPORT_PATH + "?" +  
-										 AIFB_PARAM_PERSON_ID + extractParamValue(sc.getUrl().getQuery()));
-					sc.setBibtexResult(WebUtils.getContentAsString(expURL)); 
-					return true;
-				} catch (IOException me) {
-					throw new InternalFailureException(me);
-				}
-				
-			}else if(sc.getUrl().getPath().contains(AIFB_PUBL_PERSON_PATH)){
-				/*
-				 *URL must be like this: 
-				 *http://www.aifb.uni-karlsruhe.de/Publikationen/showPublikationen?id_db=2096 or
-				 *http://www.aifb.uni-karlsruhe.de/Publikationen/showPublikationen_english?id_db=2096
-				 *Scrape following URL to get the adequate bibtex entry:
-				 *http://www.aifb.uni-karlsruhe.de/Publikationen/exportPublikationenPerson.bib?person_id=2096
-				 */
-				try {					
-					URL expURL = new URL(AIFB_HOST_NAME + AIFB_PUBL_PERSON_EXPORT_PATH + "?" +  
-										 AIFB_PARAM_PERSON_ID + extractParamValue(sc.getUrl().getQuery()));
-					sc.setBibtexResult(WebUtils.getContentAsString(expURL)); 
-					return true;
-				} catch (IOException me) {
-					throw new InternalFailureException(me);
-				}
-				
-			}else if(sc.getUrl().getPath().toString().startsWith(AIFB_DEPT_PATH)){
-				Calendar cal = new GregorianCalendar(); 
-				String currYear = Integer.toString(cal.get(Calendar.YEAR)); 
-				//handle all dept. urls
-				if(sc.getUrl().getPath().toString().contains(AIFB_DEPT_CODE_WBS)){
-					try {
-						//if param year is not set, use current year
-						if(sc.getUrl().getQuery() != null && sc.getUrl().getQuery().contains(AIFB_PARAM_YEAR)){
-							currYear = extractParamValue(sc.getUrl().getQuery());
-						}
-						//http://www.aifb.uni-karlsruhe.de/Publikationen/exportPublikationenFG.bib?fg_id=3&jahr=2006
-						URL expURL = new URL(AIFB_HOST_NAME + AIFB_PUBL_DEPT_EXPORT_PATH + "?" 
-								           + AIFB_PARAM_DEPT_ID + AIFB_DEPT_ID_WBS + "&"
-								           + AIFB_PARAM_YEAR + currYear);
-						sc.setBibtexResult(WebUtils.getContentAsString(expURL));
-						return true;
-					} catch (IOException me) {
-						throw new InternalFailureException(me);
-					}	
-				}else if(sc.getUrl().getPath().toString().contains(AIFB_DEPT_CODE_BIK)){
-					try {
-						//if param year is not set, use current year
-						if(sc.getUrl().getQuery() != null && sc.getUrl().getQuery().contains(AIFB_PARAM_YEAR)){
-							currYear = extractParamValue(sc.getUrl().getQuery());
-						}
-						URL expURL = new URL(AIFB_HOST_NAME + AIFB_PUBL_DEPT_EXPORT_PATH + "?" 
-								           + AIFB_PARAM_DEPT_ID + AIFB_DEPT_ID_BIK + "&"
-								           + AIFB_PARAM_YEAR + currYear);
-						sc.setBibtexResult(WebUtils.getContentAsString(expURL));
-						return true;
-					} catch (IOException me) {
-						throw new InternalFailureException(me);
-					}
-				}else if(sc.getUrl().getPath().toString().contains(AIFB_DEPT_CODE_EFFALG)){
-					try {
-						//if param year is not set, use current year
-						if(sc.getUrl().getQuery() != null && sc.getUrl().getQuery().contains(AIFB_PARAM_YEAR)){
-							currYear = extractParamValue(sc.getUrl().getQuery());
-						}
-						URL expURL = new URL(AIFB_HOST_NAME + AIFB_PUBL_DEPT_EXPORT_PATH + "?" 
-								           + AIFB_PARAM_DEPT_ID + AIFB_DEPT_ID_EFFALG + "&"
-								           + AIFB_PARAM_YEAR + currYear);
-						sc.setBibtexResult(WebUtils.getContentAsString(expURL));
-						return true;
-					} catch (IOException me) {
-						throw new InternalFailureException(me);
-					}
-				}else if(sc.getUrl().getPath().toString().contains(AIFB_DEPT_CODE_COM)){
-					try {
-						//if param year is not set, use current year
-						if(sc.getUrl().getQuery() != null && sc.getUrl().getQuery().contains(AIFB_PARAM_YEAR)){
-							currYear = extractParamValue(sc.getUrl().getQuery());
-						}
-						URL expURL = new URL(AIFB_HOST_NAME + AIFB_PUBL_DEPT_EXPORT_PATH + "?" 
-								           + AIFB_PARAM_DEPT_ID + AIFB_DEPT_ID_COM + "&"
-								           + AIFB_PARAM_YEAR + currYear);
-						sc.setBibtexResult(WebUtils.getContentAsString(expURL));
-						return true;
-					} catch (IOException me) {
-						throw new InternalFailureException(me);
-					}
+					URL url = getDownloadUrl(_m.group(1));
+					bibtex = WebUtils.getContentAsString(url);
+				} catch (IOException ex) {
+					ex.printStackTrace();
 				}
 			}
-			
-		}		
+		}
+		
+		if (bibtex != null) {
+			bibtex = BibTexUtils.addFieldIfNotContained(bibtex, URL, sc.getUrl().toString());
+			sc.setBibtexResult(bibtex.toString());
+		}
+		
 		return false;
 	}
 	
-	/**
-	 * 
-	 * @param param String representing a parameter including the value. E.g. year=2006 or id_db=2096
-	 * @return extracted value
-	 */
-	private String extractParamValue(String param){
-		return param.trim().substring(param.indexOf("=")+1);
+	private URL getDownloadUrl(final String url) {
+		try {
+			return new URL(url.replaceAll(AMP, AMP_REPL));
+		} catch (MalformedURLException ex) {
+			ex.printStackTrace();
+		}
+		return null;
 	}
-
-
+	
 	public String getInfo() {
 		return info;
 	}
@@ -275,7 +134,7 @@ public class AIFBScraper extends AbstractUrlScraper {
 	}
 
 	public String getSupportedSiteName() {
-		return "AIFB";
+		return AIFB_SITE_NAME;
 	}
 
 	public String getSupportedSiteURL() {
