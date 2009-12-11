33e2fb0a40df9fdd530177169ae160d57d852add
Sven Stefani <sst@cs.uni-kassel.de>
2009-12-11 15:50:54 +0100

    authentication check with ldap works now with one manager account which has permission to read all data from all users.

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/util/auth/Ldap.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/util/auth/Ldap.java
@@ -1,22 +1,28 @@
 package org.bibsonomy.webapp.util.auth;
 
-import java.io.IOException;
 import java.io.Serializable;
+import java.util.Enumeration;
 import java.util.Hashtable;
-import java.util.Properties;
 
-import javax.naming.AuthenticationException;
 import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.Name;
 import javax.naming.NamingEnumeration;
 import javax.naming.NamingException;
+import javax.naming.RefAddr;
+import javax.naming.Reference;
 import javax.naming.directory.Attributes;
 import javax.naming.directory.DirContext;
 import javax.naming.directory.InitialDirContext;
 import javax.naming.directory.SearchControls;
 import javax.naming.directory.SearchResult;
+import javax.naming.spi.InitialContextFactory;
+import javax.naming.spi.ObjectFactory;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
+
 
 /**
  * Class providing features for LDAP authentication
@@ -24,7 +30,7 @@ import org.apache.commons.logging.LogFactory;
  * @author Sven Stefani
  * @version $Id$
  */
-public class Ldap implements Serializable {
+public class Ldap implements ObjectFactory, InitialContextFactory, Serializable {
 
 	/**
 	 * serial uid
@@ -38,77 +44,158 @@ public class Ldap implements Serializable {
 
 
 	/** name of the property file which configures ldap */
-	private final String LDAPPropertyFilename = "ldap.properties";
+	//private final String LDAPPropertyFilename = "ldap.properties";
+
+    public Object getObjectInstance( Object obj, Name name, Context nameCtx, Hashtable<?, ?> environment)
+    		    throws NamingException {
+
+    		        Hashtable<String, String> env = new Hashtable<String, String>();
+    		        Reference ref = (Reference) obj;
+    		        Enumeration<?> addrs = ref.getAll();
+
+    		        while (addrs.hasMoreElements()) {
+    		            RefAddr addr = (RefAddr) addrs.nextElement();
+		            	//log.info("-----------------------------------------------------------------           LDAP-JNDI: " + addr.getType().toString() + " = " + addr.getContent().toString());
+    		            if (addr.getType().equals("java.naming.factory.initial")) {
+    		                env.put(addr.getType(), addr.getContent().toString());
+    		            } else if (addr.getType().equals("java.naming.provider.url")){
+    		                env.put(addr.getType(), addr.getContent().toString());
+    		            } else if (addr.getType().equals("java.naming.security.authentication")) {
+    		                env.put(addr.getType(), addr.getContent().toString());
+    		            } else if (addr.getType().equals("java.naming.security.principal")) {
+    		                env.put(addr.getType(), addr.getContent().toString());
+    		            } else if (addr.getType().equals("java.naming.security.credentials")) {
+    		                env.put(addr.getType(), addr.getContent().toString());
+    		            } else if (addr.getType().equals("com.sun.jndi.ldap.connect.pool")) {
+    		                env.put(addr.getType(), addr.getContent().toString());
+
+    		            } else if (addr.getType().equals("userIdField")) {
+    		            	env.put(addr.getType(), addr.getContent().toString());
+    		            	//this.userIdField = addr.getContent().toString();
+    		            } else {
+    		            	//log.info(" - - - LDAP-JNDI: " + addr.getType().toString() + " = " + addr.getContent().toString());
+    		            }
+    		            	
+    		        }
+
+    		        return this.getInitialContext(env);
+    		    }
+
+
+    public Context getInitialContext(Hashtable<?, ?> environment) throws NamingException {
+    	return new InitialDirContext(environment);
+    }
 	
 	
+	
+	
+	/** check authorization of user
+	 * returns ldapuserinfo object with retireved userdata if user exists otherwise null 
+	 * @param ldapUserId
+	 * @param ldapCredentials
+	 * @return LdapUserinfo
+	 */
 	public LdapUserinfo checkauth(String ldapUserId, String ldapCredentials) {
+
+		LdapUserinfo ldapUserinfo = new	LdapUserinfo();	
 
 		// exit, if one parameter is null
 		if (null==ldapUserId || null==ldapCredentials) return null;
 		
-		log.info("check LDAP authentication");
-		Properties props = new Properties();
-
-		LdapUserinfo ldapUserinfo = new	LdapUserinfo();	
-		
+		// get some ldap parameter from environment ldap resource configuration
+		String ldapUserIdField = "";
+		String ldapSurenameField = "";
+		String ldapGivennameField = "";
+		String ldapMailField = "";
+		String ldapLocationField = "";
+		String ldapUserPasswordField = "userPassword";
 		try {
-			// read database properties
-			props.load(Ldap.class.getClassLoader().getResourceAsStream(this.LDAPPropertyFilename));
-		} catch (IOException ex) {
-			log.error("There is no LDAP properties file. It is named: " + this.LDAPPropertyFilename + " and normally located in src/main/resources");
-			throw new RuntimeException(ex);
-		}
-		
-        String query = "("+props.getProperty("ldap.principal.useridentifier")+"="+ldapUserId+")";
-        
-        String ldapPrincipal = props.getProperty("ldap.principal.useridentifier")+"="+ldapUserId;
-        if (!props.getProperty("ldap.principal.trailing").isEmpty()){
-        	ldapPrincipal = ldapPrincipal + ","+props.getProperty("ldap.principal.trailing");
-        }
-        
-        Hashtable<String, String> env = new Hashtable<String,String>();
-		env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
-		env.put(Context.PROVIDER_URL, props.getProperty("ldap.url"));
-		env.put(Context.SECURITY_AUTHENTICATION, props.getProperty("ldap.authentication"));
-		env.put(Context.SECURITY_PRINCIPAL, ldapPrincipal);
-		env.put(Context.SECURITY_CREDENTIALS, ldapCredentials);
-
-		DirContext context=null;
-		try {
-			context = new InitialDirContext(env);
-		} catch (AuthenticationException ex) {
-			// wrong user authentication
-			ldapUserinfo = null;			
+			Context initContext = new InitialContext();
+			Context envContext = (Context) initContext.lookup("java:/comp/env");
+			ldapUserIdField = (String) envContext.lookup("ldap/config/userId");
+			ldapSurenameField = (String) envContext.lookup("ldap/config/surename");
+			ldapGivennameField = (String) envContext.lookup("ldap/config/givenname");
+			ldapMailField = (String) envContext.lookup("ldap/config/mail");
+			ldapLocationField = (String) envContext.lookup("ldap/config/location");
 		} catch (NamingException ex) {
-			log.warn("NamingException in " + this.getClass().getName() + " (checkauth/1): " + ex.getMessage());
+			log.error("Error when trying to read environment variable 'searchmode' via JNDI.", ex);
 		}
 		
-		if (context != null) {
+		String query = "";
+//		query +=  "&(";
+		query +=  "("+ ldapUserIdField +"="+ldapUserId+")";
+//		query +=  "("+ ldapUserPasswordField +"="+ldapUserinfo.generatePicaHashBase64(ldapCredentials)+")";
+//		query +=  ")";
+
+		log.info("check LDAP authentication");
+		log.info("LDAP query: "+query);
+
+
+		// get ldap resource (see tomcat's context.xml)
+		Context newCtx = null;
+		Context envCtx = null;
+		DirContext ctx = null;
+		try {
+			newCtx = new InitialContext();
+			envCtx = (Context) newCtx.lookup("java:comp/env");
+			ctx = (DirContext) envCtx.lookup("ldap/DirContext");
+		} catch (NamingException ex) {
+			log.error("Error when trying to create LDAP connection with configuration provided by JNDI.", ex);
+		}
+
+		// now we have got a InitialDirContext from LDAP Resource
+		if (ctx != null) {
 			try {
 	            SearchControls ctrl = new SearchControls();
 	            ctrl.setSearchScope(SearchControls.SUBTREE_SCOPE);
 	            NamingEnumeration<SearchResult> enumeration;
-					enumeration = context.search("", query, ctrl);
-	            while (enumeration.hasMore()) {
-	                SearchResult result = enumeration.next();
-	                Attributes attribs = result.getAttributes();
-	
-                	ldapUserinfo.setUserId(attribs.get(props.getProperty("ldap.principal.useridentifier")));
-                	ldapUserinfo.setSureName(attribs.get(props.getProperty("ldap.surename")));
-                	ldapUserinfo.setFirstName(attribs.get(props.getProperty("ldap.firstname")));
-                	ldapUserinfo.seteMail(attribs.get(props.getProperty("ldap.email")));
-                	ldapUserinfo.setLocation(attribs.get(props.getProperty("ldap.location")));
-	                
-            		log.info("LDAP-Authentication successful for user " + ldapUserinfo.getUserId() + " (" + ldapUserinfo.getSureName() + ", " + ldapUserinfo.getFirstName() + ", " + ldapUserinfo.geteMail() + ", " + ldapUserinfo.getLocation() + ")");
-	            }
+	            enumeration = ctx.search("ou=pica-users,dc=ub,dc=uni-kassel,dc=de", query, ctrl);
+				if (enumeration.hasMore()) {
+		            while (enumeration.hasMore()) {
+		                SearchResult result = enumeration.next();
+		                Attributes attribs = result.getAttributes();
+		
+	                	ldapUserinfo.setUserId(attribs.get(ldapUserIdField));
+	                	ldapUserinfo.setSureName(attribs.get(ldapSurenameField));
+	                	ldapUserinfo.setFirstName(attribs.get(ldapGivennameField));
+	                	ldapUserinfo.seteMail(attribs.get(ldapMailField));
+	                	ldapUserinfo.setLocation(attribs.get(ldapLocationField));
+	                	ldapUserinfo.setPasswordPicaHash(attribs.get(ldapUserPasswordField));
+/*
+	                	log.info("LDAP-Properties: ############> " + attribs.toString());
+	                	log.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ LDAP-Password: "+ldapUserinfo.getPasswordPicaHash());
+	                	log.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ LDAP-PasswordB64: "+ldapUserinfo.getPasswordPicaHashBase64());
+	                	log.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ LDAP-PasswordMD5: "+ldapUserinfo.getPasswordPicaHashMd5());
+	                	log.info("LDAP-Authentication successful for user " + ldapUserinfo.getUserId() + " (" + ldapUserinfo.getSureName() + ", " + ldapUserinfo.getFirstName() + ", " + ldapUserinfo.geteMail() + ", " + ldapUserinfo.getLocation() + ")");
+*/
+	                }
+		            
+					// check password
+					if (ldapUserinfo.checkPasswordPicaHash(ldapCredentials)) {
+						// password is correct
+						log.info("Password is CORRECT");
+						
+					}
+					else {
+						// password is not correct
+						log.info("Password is NOT correct");
+						ldapUserinfo = null;
+					}
+		            
+				} else {
+					log.info("no record retrieved from ldap. User "+ ldapUserId +" does not exist.");
+					ldapUserinfo = null;
+				}
+					
 			} catch (NamingException ex) {
 				log.warn("NamingException in " + this.getClass().getName() + " (checkauth/2): " + ex.getMessage());
 			}
 
-        }
+		}
 		else
 		{ // can't get context
-			log.warn("LDAP context not available. Wrong authentication !?");
+			log.warn("LDAP context not available. Wrong authentication within JNDI-LDAP-Resource!?");
+			ldapUserinfo = null;
 		}
 
 		return ldapUserinfo;
--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/util/auth/LdapUserinfo.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/util/auth/LdapUserinfo.java
@@ -1,8 +1,12 @@
 package org.bibsonomy.webapp.util.auth;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import javax.naming.NamingException;
 import javax.naming.directory.Attribute;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -18,14 +22,66 @@ public class LdapUserinfo {
 	/**
 	 * Logger
 	 */
-	private final Log log = LogFactory.getLog(Ldap.class);
+	private final Log log = LogFactory.getLog(LdapUserinfo.class);
 
 	private String userId = ""; 
 	private String sureName = ""; 
 	private String firstName = ""; 
 	private String eMail = ""; 
 	private String location = "";
+	private String passwordPicaHash = "";
 
+
+	/**
+	 * generates a pica-"hashed" value from string
+	 * this function will add prefix "{PICA}" to hash  
+	 * @param s string to hash for pica
+	 * @return Picahash
+	 */
+
+	public String generatePicaHash(String s) {
+		return generatePicaHash(s, "{PICA}");
+	}
+	
+	/**
+	 * generates a pica-"hashed" value from string
+	 * this function will add prefix "{PICA}" to hash
+	 * and convert result with base64  
+	 * @param s string to hash for pica
+	 * @return Base64 Picahash
+	 */
+	public String generatePicaHashBase64(String s) {
+		return new String(Base64.encodeBase64(generatePicaHash(s, "{PICA}").getBytes()));
+	}
+
+	/**
+	 * generates a pica-"hashed" value from string
+	 * hash-prefix can be set  
+	 * @param s string to hash for pica
+	 * @return pica hash
+	 */
+	public String generatePicaHash(String s, String prefix) {
+		byte[] b = s.getBytes();
+
+		Integer hash = 0;
+
+		for (int i=0; i < b.length; i++) {
+			hash += b[i]*(i+1);
+		}
+		
+		return prefix + hash.toString();
+	}
+	
+	
+	public boolean checkPasswordPicaHash(String password) {
+		boolean authOk = false;
+		if ( this.getPasswordPicaHash().equals(generatePicaHash(password))) {
+			authOk = true;
+		} else {
+			authOk = false;
+		}
+		return authOk;
+	}
 	
 	
 	/** 
@@ -58,7 +114,7 @@ public class LdapUserinfo {
 		} catch (NamingException ex) {
 			log.error("NamingException in " + this.getClass().getName() + " (setUserId): " + ex.getMessage());
 		}
-		this.userId = userIdS;
+		this.setUserId(userIdS);
 	} 
 
 	/**
@@ -90,7 +146,7 @@ public class LdapUserinfo {
 		} catch (NamingException ex) {
 			log.error("NamingException in " + this.getClass().getName() + " (setSureName): " + ex.getMessage());
 		}
-		this.sureName = sureNameS;
+		this.setSureName(sureNameS);
 	} 
 
 	/**
@@ -122,7 +178,7 @@ public class LdapUserinfo {
 		} catch (NamingException ex) {
 			log.error("NamingException in " + this.getClass().getName() + " (setFirstName): " + ex.getMessage());
 		}
-		this.firstName = firstNameS;
+		this.setFirstName(firstNameS);
 	} 
 
 	/**
@@ -154,7 +210,7 @@ public class LdapUserinfo {
 		} catch (NamingException ex) {
 			log.error("NamingException in " + this.getClass().getName() + " (seteMail): " + ex.getMessage());
 		}
-		this.eMail = eMailS;
+		this.seteMail(eMailS);
 	} 
 	
 	/**
@@ -186,9 +242,63 @@ public class LdapUserinfo {
 		} catch (NamingException ex) {
 			log.error("NamingException in " + this.getClass().getName() + " (setLocation): " + ex.getMessage());
 		}
-		this.location = locationS;
+		this.setLocation(locationS);
 	} 
 
+    public void setPasswordPicaHash (Attribute password) {
+    	byte[] passwordB = null;
 
+    	Object obj; 
+    	try {
+			obj = password.get();
 
+			if (obj instanceof byte[]) {
+				passwordB = ((byte[])obj);
+			}
+			else
+			if (obj instanceof String) {
+				passwordB = (((String)obj).getBytes());
+			}
+
+			// convert byte[] to String
+			String decodedString = new String(passwordB); 
+			
+	    	this.passwordPicaHash = decodedString;
+			
+			
+		} catch (NamingException ex) {
+			log.error("NamingException in " + this.getClass().getName() + " (setPassword): " + ex.getMessage());
+		}
+    	
+    }
+
+    public String getPasswordPicaHash () {
+    	return this.passwordPicaHash;
+    }
+
+    public String getPasswordPicaHashBase64 () {
+    	return new String(Base64.encodeBase64(this.passwordPicaHash.getBytes()));
+    }
+
+    public String getPasswordPicaHashMd5 () {
+    	MessageDigest md5 = null;
+		try {
+			md5 = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException ex) {
+			log.error("NoSuchAlgorithmException in " + this.getClass().getName() + " (getPasswordPicaHashMd5): " + ex.getMessage());
+		}
+        md5.reset();
+        md5.update(this.passwordPicaHash.getBytes());
+        byte[] result = md5.digest();
+        
+        StringBuffer hexString = new StringBuffer();
+        for (int i=0; i<result.length; i++) {
+        	if(result[i] <= 15 && result[i] >= 0){
+        		hexString.append("0");
+        	}
+        	hexString.append(Integer.toHexString(0xFF & result[i]));
+        }
+       
+    	return new String(hexString.toString());
+    }
 }
