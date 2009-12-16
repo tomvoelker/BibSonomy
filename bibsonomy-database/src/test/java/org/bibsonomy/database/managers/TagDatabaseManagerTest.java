712edb61043bb5f14182952099b460bfb3d82195
Clemens Baier <clemens.baier@multi-hosting.de>
2009-12-16 13:52:32 +0100

    Additional test for tags ordered by FREQUENCY

--- bibsonomy-database/src/test/java/org/bibsonomy/database/managers/TagDatabaseManagerTest.java
+++ bibsonomy-database/src/test/java/org/bibsonomy/database/managers/TagDatabaseManagerTest.java
@@ -21,25 +21,28 @@ import org.bibsonomy.database.params.TagParam;
 import org.bibsonomy.model.BibTex;
 import org.bibsonomy.model.Tag;
 import org.bibsonomy.model.User;
+import org.bibsonomy.model.enums.Order;
 import org.junit.Ignore;
 import org.junit.Test;
 
 /**
  * Tests related to tags.
- *
+ * 
  * @author Dominik Benz
  * @author Miranda Grahl
  * @author Jens Illig
  * @author Christian Kramer
  * @author Christian Schenk
- * @version $Id$
+ * @author Clemens Baier
+ * @version $Id: TagDatabaseManagerTest.java,v 1.40 2009-11-11 10:25:59
+ *          nosebrain Exp $
  */
 
 public class TagDatabaseManagerTest extends AbstractDatabaseManagerTest {
-	
+
 	/** logger */
-	private static final Log log = LogFactory.getLog(TagDatabaseManagerTest.class);	
-	
+	private static final Log log = LogFactory.getLog(TagDatabaseManagerTest.class);
+
 	/**
 	 * tests getTagById
 	 */
@@ -58,7 +61,8 @@ public class TagDatabaseManagerTest extends AbstractDatabaseManagerTest {
 	public void getTagByCount() {
 		final List<Tag> tags = this.tagDb.getTagByCount(this.tagParam, this.dbSession);
 		assertEquals(10, tags.size());
-		for (final Tag tag : tags) assertEquals(100, tag.getGlobalcount());
+		for (final Tag tag : tags)
+			assertEquals(100, tag.getGlobalcount());
 	}
 
 	@Test
@@ -73,6 +77,21 @@ public class TagDatabaseManagerTest extends AbstractDatabaseManagerTest {
 	public void getTagsViewable() {
 		final List<Tag> tags = this.tagDb.getTagsViewable(this.tagParam, this.dbSession);
 		assertEquals(10, tags.size());
+	}
+
+	/**
+	 * tests getTagsByUser with additional order by Order.FREQUENCY
+	 */
+	@Test
+	public void getTagsByUserOrderedByFrequency() {
+		this.tagParam.setRequestedUserName("testuser1");
+		this.tagParam.setGroupId(0);
+		this.tagParam.setContentType(ConstantID.BOOKMARK_CONTENT_TYPE);
+		this.tagParam.setOrder(Order.FREQUENCY);
+		final List<Tag> tags = this.tagDb.getTagsByUser(this.tagParam, this.dbSession);
+		assertEquals(tags.get(0).getUsercount(), 4); // "suchmaschine"
+		assertEquals(tags.get(1).getUsercount(), 2); // "google"
+		assertEquals(tags.get(2).getUsercount(), 1); // "testtag"
 	}
 
 	@Test
@@ -130,8 +149,8 @@ public class TagDatabaseManagerTest extends AbstractDatabaseManagerTest {
 		this.tagParam.setRegex("web");
 		this.tagParam.setRequestedUserName("hotho");
 		List<Tag> tags = this.tagDb.getTagsByExpression(this.tagParam, this.dbSession);
-		assertEquals(12,tags.size());
-		this.resetParameters();		
+		assertEquals(12, tags.size());
+		this.resetParameters();
 	}
 
 	@Ignore
@@ -143,10 +162,10 @@ public class TagDatabaseManagerTest extends AbstractDatabaseManagerTest {
 	@Ignore
 	public void getTagDetails() {
 		Tag tag = this.tagDb.getTagDetails(this.tagParam, this.dbSession);
-		assertNotNull(tag);	
-		assertEquals(this.tagParam.getTagIndex().get(0).getTagName(), tag.getName());	
+		assertNotNull(tag);
+		assertEquals(this.tagParam.getTagIndex().get(0).getTagName(), tag.getName());
 		assertNotNull(tag.getGlobalcount());
-		assertNotNull(tag.getUsercount()); 
+		assertNotNull(tag.getUsercount());
 		this.resetParameters();
 	}
 
@@ -173,13 +192,13 @@ public class TagDatabaseManagerTest extends AbstractDatabaseManagerTest {
 	/**
 	 * tests getTagsByAuthor
 	 * 
-	 * we have only a single entry in the 
+	 * we have only a single entry in the
 	 */
 	@Test
 	public void getTagsByAuthor() {
-		/* 
-		 * we have only a single entry in the bibtex search table (authored by "author",
-		 * points to entry with content id 10; has 2 tags assigned  
+		/*
+		 * we have only a single entry in the bibtex search table (authored by
+		 * "author", points to entry with content id 10; has 2 tags assigned
 		 */
 		TagParam param = new TagParam();
 		param.setSearch("author");
@@ -188,28 +207,28 @@ public class TagDatabaseManagerTest extends AbstractDatabaseManagerTest {
 		param.setLimit(1000);
 		param.setOffset(0);
 		param.setContentTypeByClass(BibTex.class);
-	    List<Tag> tags = this.tagDb.getTagsByAuthor(param, this.dbSession);
-	    log.debug("tags found by query getTagsByAuthor for author 'author':");
-	    for (Tag tag : tags) {
-	    	log.debug(tag.getName());
-	    }
-	    assertEquals(2, tags.size());
-	    assertEquals(1, tags.get(0).getGlobalcount());
-	    assertEquals(1, tags.get(1).getGlobalcount());
-	    assertTrue(tags.contains(new Tag("testbibtex")));
-	    assertTrue(tags.contains(new Tag("testtag")));	    
-	    /*
-	     * search for non-existing author -> no results
-	     */
-	    param.setSearch("nonexistingauthor");
-	    tags = this.tagDb.getTagsByAuthor(param, this.dbSession);
-	    assertEquals(0, tags.size());
+		List<Tag> tags = this.tagDb.getTagsByAuthor(param, this.dbSession);
+		log.debug("tags found by query getTagsByAuthor for author 'author':");
+		for (Tag tag : tags) {
+			log.debug(tag.getName());
+		}
+		assertEquals(2, tags.size());
+		assertEquals(1, tags.get(0).getGlobalcount());
+		assertEquals(1, tags.get(1).getGlobalcount());
+		assertTrue(tags.contains(new Tag("testbibtex")));
+		assertTrue(tags.contains(new Tag("testtag")));
+		/*
+		 * search for non-existing author -> no results
+		 */
+		param.setSearch("nonexistingauthor");
+		tags = this.tagDb.getTagsByAuthor(param, this.dbSession);
+		assertEquals(0, tags.size());
 	}
 
 	/**
-	 * this is just a dummy test to check if the function works; please adapt
-	 * it to check it the correct tags are returned when migrating to the new 
-	 * test framwork (dbe)
+	 * this is just a dummy test to check if the function works; please adapt it
+	 * to check it the correct tags are returned when migrating to the new test
+	 * framwork (dbe)
 	 */
 	@Test
 	@Ignore
@@ -217,14 +236,14 @@ public class TagDatabaseManagerTest extends AbstractDatabaseManagerTest {
 		String loginUserName = "hotho";
 		String hash = "palim palim";
 		ArrayList<Integer> visibleGroups = new ArrayList<Integer>();
-		visibleGroups.add(0);		
+		visibleGroups.add(0);
 		List<Tag> tags = this.tagDb.getTagsByBibtexHash(loginUserName, hash, HashID.INTER_HASH, visibleGroups, 0, 20, this.dbSession);
 	}
 
 	/**
-	 * this is just a dummy test to check if the function works; please adapt
-	 * it to check it the correct tags are returned when migrating to the new 
-	 * test framwork (dbe)
+	 * this is just a dummy test to check if the function works; please adapt it
+	 * to check it the correct tags are returned when migrating to the new test
+	 * framwork (dbe)
 	 */
 	@Test
 	@Ignore
@@ -233,14 +252,14 @@ public class TagDatabaseManagerTest extends AbstractDatabaseManagerTest {
 		final String requestedUserName = "hotho";
 		final String hash = "palim palim";
 		ArrayList<Integer> visibleGroups = new ArrayList<Integer>();
-		visibleGroups.add(0);				
-		List<Tag> tags = this.tagDb.getTagsByBibtexHashForUser(loginUserName, requestedUserName, hash, HashID.INTER_HASH, visibleGroups, 0, 20, this.dbSession);	
+		visibleGroups.add(0);
+		List<Tag> tags = this.tagDb.getTagsByBibtexHashForUser(loginUserName, requestedUserName, hash, HashID.INTER_HASH, visibleGroups, 0, 20, this.dbSession);
 	}
 
 	/**
-	 * this is just a dummy test to check if the function works; please adapt
-	 * it to check it the correct tags are returned when migrating to the new 
-	 * test framwork (dbe)
+	 * this is just a dummy test to check if the function works; please adapt it
+	 * to check it the correct tags are returned when migrating to the new test
+	 * framwork (dbe)
 	 */
 	@Test
 	@Ignore
@@ -253,9 +272,9 @@ public class TagDatabaseManagerTest extends AbstractDatabaseManagerTest {
 	}
 
 	/**
-	 * this is just a dummy test to check if the function works; please adapt
-	 * it to check it the correct tags are returned when migrating to the new 
-	 * test framwork (dbe)
+	 * this is just a dummy test to check if the function works; please adapt it
+	 * to check it the correct tags are returned when migrating to the new test
+	 * framwork (dbe)
 	 */
 	@Test
 	@Ignore
@@ -264,7 +283,7 @@ public class TagDatabaseManagerTest extends AbstractDatabaseManagerTest {
 		final String requestedUserName = "hotho";
 		final String hash = "palim palim";
 		final List<Integer> visibleGroups = new ArrayList<Integer>();
-		visibleGroups.add(0);		
+		visibleGroups.add(0);
 		List<Tag> tags = this.tagDb.getTagsByBookmarkHashForUser(loginUserName, requestedUserName, hash, visibleGroups, 0, 20, this.dbSession);
 	}
 
@@ -286,24 +305,23 @@ public class TagDatabaseManagerTest extends AbstractDatabaseManagerTest {
 		param.addTagName("text");
 		ArrayList<Integer> visibleGroupIDs = new ArrayList<Integer>();
 		visibleGroupIDs.add(0);
-		List<Tag> tags = this.tagDb.getRelatedTagsForUser(null,"hotho", param.getTagIndex(), visibleGroupIDs, 0, 10, this.dbSession);
+		List<Tag> tags = this.tagDb.getRelatedTagsForUser(null, "hotho", param.getTagIndex(), visibleGroupIDs, 0, 10, this.dbSession);
 	}
-	
+
 	@Test
 	public void updateTags() {
 		final User user = new User("testuser1");
-		
+
 		final List<Tag> tagsToReplace = new LinkedList<Tag>();
 		tagsToReplace.add(new Tag("suchmaschine"));
-		
+
 		final List<Tag> replacementTags = new LinkedList<Tag>();
 		replacementTags.add(new Tag("search"));
 		replacementTags.add(new Tag("engine"));
-		
-		
+
 		this.tagDb.updateTags(user, tagsToReplace, replacementTags, this.dbSession);
 	}
-	
+
 	/**
 	 * tests getTagsByBibtexkey
 	 */
@@ -318,26 +336,26 @@ public class TagDatabaseManagerTest extends AbstractDatabaseManagerTest {
 		String requestedUserName = null;
 		String loginUserName = null;
 		int offset = 0;
-		int limit = 10;		
+		int limit = 10;
 		List<Tag> tags = this.tagDb.getTagsByBibtexkey("test bibtexKey", visibleGroups, requestedUserName, loginUserName, limit, offset, this.dbSession);
 		assertEquals(3, tags.size());
 		assertTrue(tags.contains(new Tag("spam")));
 		assertTrue(tags.contains(new Tag("testbibtex")));
 		assertTrue(tags.contains(new Tag("testtag")));
-		
+
 		/*
-		 * fetch tags from public bibtex entries of testuser1 with the key "test bibtexkey"
-		 * (should be 2)
+		 * fetch tags from public bibtex entries of testuser1 with the key
+		 * "test bibtexkey" (should be 2)
 		 */
 		requestedUserName = "testuser1";
 		tags = this.tagDb.getTagsByBibtexkey("test bibtexKey", visibleGroups, requestedUserName, loginUserName, limit, offset, this.dbSession);
 		assertEquals(2, tags.size());
 		assertTrue(tags.contains(new Tag("testbibtex")));
 		assertTrue(tags.contains(new Tag("testtag")));
-		
+
 		/*
-		 * fetch tags from private bibtexs of testuser1 with the key "test bibtexkey"
-		 * (should be 1)
+		 * fetch tags from private bibtexs of testuser1 with the key
+		 * "test bibtexkey" (should be 1)
 		 */
 		requestedUserName = null;
 		loginUserName = "testuser1";
@@ -345,7 +363,7 @@ public class TagDatabaseManagerTest extends AbstractDatabaseManagerTest {
 		visibleGroups.add(GroupID.PRIVATE.getId());
 		tags = this.tagDb.getTagsByBibtexkey("test bibtexKey", visibleGroups, requestedUserName, loginUserName, limit, offset, this.dbSession);
 		assertEquals(1, tags.size());
-		assertEquals("privatebibtex", tags.get(0).getName());		
+		assertEquals("privatebibtex", tags.get(0).getName());
 	}
-	
+
 }
