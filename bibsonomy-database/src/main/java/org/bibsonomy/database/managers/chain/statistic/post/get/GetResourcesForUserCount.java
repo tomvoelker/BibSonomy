6d74da64506e26b07fec2efb69d0d87a6d1d4c6a
Michael Velimirov <m.velimirov@googlemail.com>
2010-01-12 01:32:10 +0100

    added classes that were missing

--- bibsonomy-database/src/main/java/org/bibsonomy/database/managers/chain/statistic/post/get/GetResourcesForUserCount.java
+++ bibsonomy-database/src/main/java/org/bibsonomy/database/managers/chain/statistic/post/get/GetResourcesForUserCount.java
@@ -1,8 +1,20 @@
 package org.bibsonomy.database.managers.chain.statistic.post.get;
 
-import src.main.java.org.bibsonomy.database.managers.chain.statistic.StatisticChainElement;
-import src.main.java.org.bibsonomy.database.params.StatisticsParam;
-import src.main.java.org.bibsonomy.database.util.DBSession;
+import static org.bibsonomy.util.ValidationUtils.nullOrEqual;
+import static org.bibsonomy.util.ValidationUtils.present;
+import static org.bibsonomy.util.ValidationUtils.presentValidGroupId;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bibsonomy.common.enums.ConstantID;
+import org.bibsonomy.common.enums.GroupingEntity;
+import org.bibsonomy.database.managers.chain.statistic.StatisticChainElement;
+import org.bibsonomy.database.params.StatisticsParam;
+import org.bibsonomy.database.util.DBSession;
+import org.bibsonomy.model.BibTex;
+import org.bibsonomy.model.Bookmark;
+import org.bibsonomy.model.enums.Order;
 
 /**
  * Gets count of resources of a special user
