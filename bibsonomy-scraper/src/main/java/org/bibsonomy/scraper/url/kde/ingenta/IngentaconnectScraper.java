d1fafca0eca96b201000c5869d28bb4d91054403
Christian Claus <claus@cs.uni-kassel.de>
2010-01-20 15:14:22 +0100

    fixed Ingentaconnect Scraper added Testcase

--- bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/ingenta/IngentaconnectScraper.java
+++ bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/ingenta/IngentaconnectScraper.java
@@ -23,11 +23,7 @@
 
 package org.bibsonomy.scraper.url.kde.ingenta;
 
-import java.io.BufferedInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.StringWriter;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Collections;
@@ -35,11 +31,13 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.bibsonomy.scraper.AbstractUrlScraper;
 import org.bibsonomy.scraper.ScrapingContext;
 import org.bibsonomy.scraper.Tuple;
-import org.bibsonomy.scraper.AbstractUrlScraper;
 import org.bibsonomy.scraper.exceptions.InternalFailureException;
 import org.bibsonomy.scraper.exceptions.ScrapingException;
+import org.bibsonomy.scraper.exceptions.ScrapingFailureException;
+import org.bibsonomy.util.WebUtils;
 
 /** Scraper for ingentaconnect.
  * @author rja
@@ -69,9 +67,11 @@ public class IngentaconnectScraper extends AbstractUrlScraper{
 			/* 
 			 * create query URL
 			 */
-			String URLString = SITE_URL.substring(0, SITE_URL.length()-1);
+			String URLString = SITE_URL.substring(0, SITE_URL.length() - 1);
 			String page = sc.getPageContent();
+			
 			final Matcher m = exportPattern.matcher(page);
+			
 			if (m.find()) {
 				URLString = URLString + m.group(1);
 			} else {
@@ -80,59 +80,63 @@ public class IngentaconnectScraper extends AbstractUrlScraper{
 
 			URL queryURL = new URL(URLString);
 
-			/*
-			 * download BibTex-file
-			 */
-			//String bibResult = sc.getContentAsString(queryURL);
-			String bibResult = "";
+			String cookies      = null;
+			String bibtexResult = null;
+			
 			try {
-				bibResult = getBibTexFromIngenta(queryURL, getCookieFromIngenta());
-			} catch (IOException e) {
-				throw new InternalFailureException(e);
+				cookies = WebUtils.getCookies(queryURL);
+				bibtexResult = WebUtils.getContentAsString(queryURL, cookies);
+			} catch (IOException ex) {
+				throw new InternalFailureException(ex);
 			}
 
 			/*
 			 * fix bibtex
 			 */
-			//System.out.println(bibResult);
-			String[] lines = bibResult.split("\n");
-			lines[0] = lines[0].replaceAll(" ", "");
 			StringBuffer buffer = new StringBuffer();
-			StringBuffer authorBuffer = new StringBuffer("author = \"");
-			boolean firstAuthor = true;
-			for (int i = 0; i < lines.length-1; i++) {
-				//System.out.println(lines[i]);
-				//transform author-lines to ONE author-line
-				if (lines[i].contains("author")){
-					if (firstAuthor){
-						authorBuffer.append(lines[i].substring(lines[i].indexOf("\"")+1, lines[i].lastIndexOf("\"")));
-						firstAuthor = false;
+			
+			if (bibtexResult != null) {
+				String[] lines = bibtexResult.split("\n");
+				lines[0] = lines[0].replaceAll(" ", "");
+				StringBuffer authorBuffer = new StringBuffer("author = \"");
+				
+				boolean firstAuthor = true;
+				
+				for (int i = 0; i < lines.length - 1; i++) {
+					//transform author-lines to ONE author-line
+					if (lines[i].contains("author")){
+						if (firstAuthor){
+							authorBuffer.append(lines[i].substring(lines[i].indexOf("\"") + 1, lines[i].lastIndexOf("\"")));
+							firstAuthor = false;
+						}
+						else{
+							authorBuffer.append(" and " + lines[i].substring(lines[i].indexOf("\"") + 1, lines[i].lastIndexOf("\"")));
+						}
 					}
 					else{
-						authorBuffer.append(" and " + lines[i].substring(lines[i].indexOf("\"")+1, lines[i].lastIndexOf("\"")));
+						lines[i] = removeHTML(lines[i]);
+						//append missing ","
+						if (!lines[i].endsWith(",")){
+							buffer.append(lines[i] + ",\n");
+						}
+						else{
+							buffer.append(lines[i] + "\n");
+						}
 					}
 				}
-				else{
-					lines[i] = removeHTML(lines[i]);
-					//append missing ","
-					if (!lines[i].endsWith(",")){
-						buffer.append(lines[i] + ",");
-					}
-					else{
-						buffer.append(lines[i]);
-					}
-				}
-			}
 
-			//add author-line
-			authorBuffer.append("\"}");
-			buffer.append(authorBuffer);
-			/*
-			 * Job done
-			 */
-			//System.out.println(buffer.toString());
-			sc.setBibtexResult(buffer.toString());
-			return true;
+				//add author-line
+				authorBuffer.append("\"}");
+				buffer.append(authorBuffer);
+			}
+			
+			if (bibtexResult != null && buffer.length() > 0) {
+				sc.setBibtexResult(buffer.toString());
+				return true;
+			} else {
+				throw new ScrapingFailureException("getting bibtex failed");
+			}
+			
 		} catch (MalformedURLException e) {
 			throw new InternalFailureException(e);
 		}
@@ -157,84 +161,13 @@ public class IngentaconnectScraper extends AbstractUrlScraper{
 
 		//remove everything
 		String newline = line.replaceAll("<.*>", "");
-		if (newline.length() < line.length()){
-			System.out.println("DEBUG: irgendwas weggeworfen!");
-			System.out.println("ALT: " + line);
-			System.out.println("NEU: " + newline);
-		}
+//		if (newline.length() < line.length()){
+//			System.out.println("DEBUG: irgendwas weggeworfen!");
+//			System.out.println("ALT: " + line);
+//			System.out.println("NEU: " + newline);
+//		}
 
 		return newline;
-	}
-
-	/** FIXME: refactor
-	 * @param queryURL
-	 * @param cookie
-	 * @return
-	 * @throws IOException
-	 */
-	private String getBibTexFromIngenta(URL queryURL, String cookie) throws IOException {
-		/*
-		 * get BibTex-File from Ingenta
-		 */
-		HttpURLConnection urlConn = (HttpURLConnection) queryURL.openConnection();
-		urlConn.setAllowUserInteraction(false);
-		urlConn.setDoInput(true);
-		urlConn.setDoOutput(false);
-		urlConn.setUseCaches(false);
-		/*
-		 * set user agent (see http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html) since some 
-		 * pages require it to download content.
-		 */
-		urlConn.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)");
-		//insert cookie
-		urlConn.setRequestProperty("Cookie", cookie);
-		urlConn.connect();
-		StringWriter out = new StringWriter();
-		InputStream in = new BufferedInputStream(urlConn.getInputStream());
-		int b;
-		while ((b = in.read()) >= 0) {
-			out.write(b);
-		}
-		urlConn.disconnect();
-		return out.toString();
-	}
-
-	/** FIXME: refactor
-	 * @return
-	 * @throws IOException
-	 */
-	private String getCookieFromIngenta () throws IOException {
-		/*
-		 * receive cookie from springer
-		 */
-		URL mainURL = new URL(SITE_URL);
-		HttpURLConnection urlConn;
-		urlConn = (HttpURLConnection) mainURL.openConnection();
-		urlConn.setAllowUserInteraction(false);
-		urlConn.setDoInput(true);
-		urlConn.setDoOutput(false);
-		urlConn.setUseCaches(false);
-		/*
-		 * set user agent (see http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html) since some 
-		 * pages require it to download content.
-		 */
-		urlConn.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)");
-		urlConn.connect();
-		/*
-		 * extract cookie from connection
-		 */
-		List<String> cookieContent = urlConn.getHeaderFields().get("Set-Cookie");
-		//extract sessionID and store in cookie
-
-		//TODO
-		for (String crumb : cookieContent) {
-			//System.out.println(crumb);
-			if (crumb.contains("JSESSIONID")){
-				return crumb;
-			}
-		}
-		urlConn.disconnect();
-		return null;
 	}
 
 	public String getInfo() {
@@ -246,7 +179,6 @@ public class IngentaconnectScraper extends AbstractUrlScraper{
 	}
 
 	public String getSupportedSiteName() {
-		// TODO Auto-generated method stub
 		return SITE_NAME;
 	}
 
