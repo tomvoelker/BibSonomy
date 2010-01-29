a8d6832d81fe5ba76bba09da2b93ef39d6e064b1
Robert Jäschke <jaeschke@l3s.de>
2010-01-29 12:02:57 +0100

    Test

---
+++ bibsonomy-model/src/test/java/org/bibsonomy/services/URLGeneratorTest.java
@@ -0,0 +1,41 @@
+package org.bibsonomy.services;
+
+import static org.junit.Assert.assertEquals;
+
+import org.bibsonomy.common.enums.HashID;
+import org.bibsonomy.model.BibTex;
+import org.bibsonomy.model.Bookmark;
+import org.bibsonomy.model.Post;
+import org.bibsonomy.model.User;
+import org.bibsonomy.testutil.ModelUtils;
+import org.junit.Test;
+
+/**
+ * @author rja
+ * @version $Id$
+ */
+public class URLGeneratorTest {
+
+	private static URLGenerator ug = new URLGenerator("http://www.bibsonomy.org/");
+	
+	@Test
+	public void testGetPostUrl() {
+		final Post<BibTex> post = ModelUtils.generatePost(BibTex.class);
+		assertEquals("http://www.bibsonomy.org/bibtex/" + HashID.INTRA_HASH.getId() + post.getResource().getIntraHash() + "/" + post.getUser().getName(), ug.getPublicationUrl(post.getResource(), post.getUser()).toString());
+		final Post<Bookmark> bPost = ModelUtils.generatePost(Bookmark.class);
+		assertEquals(bPost.getResource().getUrl(), ug.getPostUrl(bPost).toString());
+
+	}
+
+	@Test
+	public void testGetPublicationUrl() {
+		final Post<BibTex> post = ModelUtils.generatePost(BibTex.class);
+		assertEquals("http://www.bibsonomy.org/bibtex/" + HashID.INTRA_HASH.getId() + post.getResource().getIntraHash() + "/" + post.getUser().getName(), ug.getPublicationUrl(post.getResource(), post.getUser()).toString());
+	}
+
+	@Test
+	public void testGetUserUrl() {
+		assertEquals("http://www.bibsonomy.org/user/jaeschke", ug.getUserUrl(new User("jaeschke")).toString());
+	}
+
+}
