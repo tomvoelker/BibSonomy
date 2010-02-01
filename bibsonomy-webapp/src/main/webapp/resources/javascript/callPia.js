f9b954434c80a47d05fbe5b8da232037bed4dbbd
Michael Velimirov <m.velimirov@googlemail.com>
2010-02-01 17:29:48 +0100

    initial release of a soap handling script

---
+++ bibsonomy-webapp/src/main/webapp/resources/javascript/callPia.js
@@ -0,0 +1,33 @@
+/*****************************************************************************
+ * 
+ * @author mve
+ * 
+ * */
+
+/**
+ * @param method
+ * 			the name of the method to call ('recommendTags' || 'findSimilarUrls')
+ * @param url
+ * 			the url to check
+ */
+
+var PIAObject = null;
+
+function PIARequest(method, url) {
+	this.callback = null;
+	this.container = null;
+	this.count = 10;
+	this.serviceURL = 'http://localhost/piaWebservice/';
+	this.url = url;
+	this.method = method;
+	this.makeRequest = function() {
+			var soapBody = new SOAPObject(method);
+			soapBody.appendChild(new SOAPObject("url").val(this.url));
+			soapBody.appendChild(new SOAPObject("count").val(this.count));
+
+			var sr = new SOAPRequest(this.method, soapBody); //Request is now ready to be sent to a web-service
+			SOAPClient.Proxy = this.serviceURL+this.method+'?url=' + this.url + '&count='+this.count; 
+			SOAPClient.SendRequest(sr, this.callback); 
+	}
+	PIAObject = this;
+}
