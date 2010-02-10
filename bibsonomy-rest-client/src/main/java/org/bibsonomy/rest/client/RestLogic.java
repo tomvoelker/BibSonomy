bf3a03ef1ba417c8341822900b3e4fd5f65c6f00
daill <devnull@localhost>
2010-02-10 23:47:20 +0100

    migrated show(all)/hide(all) functions to new bibsonomy - created new database methods in tagRelationsDBManager - created necessary controller - change concept tag (added forward parameter) and changed it in all files which are using this concept tag

--- bibsonomy-rest-client/src/main/java/org/bibsonomy/rest/client/RestLogic.java
+++ bibsonomy-rest-client/src/main/java/org/bibsonomy/rest/client/RestLogic.java
@@ -33,6 +33,7 @@ import org.apache.commons.logging.LogFactory;
 import org.bibsonomy.common.enums.Classifier;
 import org.bibsonomy.common.enums.ClassifierSettings;
 import org.bibsonomy.common.enums.ConceptStatus;
+import org.bibsonomy.common.enums.ConceptUpdateOperation;
 import org.bibsonomy.common.enums.FilterEntity;
 import org.bibsonomy.common.enums.GroupUpdateOperation;
 import org.bibsonomy.common.enums.GroupingEntity;
@@ -269,7 +270,7 @@ public class RestLogic implements LogicInterface {
 
 	}
 
-	public String updateConcept(Tag concept, GroupingEntity grouping, String groupingName) {
+	public String updateConcept(Tag concept, GroupingEntity grouping, String groupingName, final ConceptUpdateOperation operation) {
 		// TODO Auto-generated method stub
 		return null;
 	}
