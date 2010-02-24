a0e2b4a107860390bfb19ab1706488ddbd40b4a9
Michael Velimirov <m.velimirov@googlemail.com>
2010-02-24 15:59:59 +0100

    add getter/setter methods for user name retrieval

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/ajax/AjaxGetPublicationsByPartialTitleCommand.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/ajax/AjaxGetPublicationsByPartialTitleCommand.java
@@ -1,17 +1,20 @@
 package org.bibsonomy.webapp.command.ajax;
 
+import java.util.List;
+
 import org.bibsonomy.model.BibTex;
 import org.bibsonomy.model.Post;
 import org.bibsonomy.webapp.command.BaseCommand;
-import org.bibsonomy.webapp.command.ListCommand;
 /**
  * @author mve
  * @version $Id$
  */
 public class AjaxGetPublicationsByPartialTitleCommand extends BaseCommand {
-	private ListCommand<Post<BibTex>> posts = null;
-	private String title;
-
+	private List<Post<BibTex>> posts = null;
+	private String title = null;
+	private String userName = null;
+	private String requestedUserName = null;
+	
 	public String getTitle() {
 		return this.title;
 	}
@@ -20,18 +23,34 @@ public class AjaxGetPublicationsByPartialTitleCommand extends BaseCommand {
 		this.title = title;
 	}
 	
+	public String getUserName() {
+		return this.userName;
+	}
+	
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	public String getRequestedUserName() {
+		return this.requestedUserName;
+	}
+	
+	public void setRequestedUserName(String requestedUserName) {
+		this.requestedUserName = requestedUserName;
+	}
+	
 	/**
 	 * 
 	 * @return the titles as JSON output
 	 */
-	public ListCommand<Post<BibTex>> getPosts() {
+	public List<Post<BibTex>> getPosts() {
 		return this.posts;
 	}
 	/**
 	 * 
 	 * @param posts Set Posts to posts
 	 */
-	public void setPosts(ListCommand<Post<BibTex>> posts) {
+	public void setPosts(List<Post<BibTex>> posts) {
 		this.posts = posts;
 	}
 }
