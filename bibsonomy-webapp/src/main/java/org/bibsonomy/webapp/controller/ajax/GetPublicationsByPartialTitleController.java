e424fdf7249742bb1ed1736e80907e26709b9d21
Michael Velimirov <m.velimirov@googlemail.com>
2010-02-24 16:08:02 +0100

    replace the way we retrieve posts with the appropriate lucene implementation

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/ajax/GetPublicationsByPartialTitleController.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/ajax/GetPublicationsByPartialTitleController.java
@@ -1,14 +1,19 @@
 package org.bibsonomy.webapp.controller.ajax;
 
-import java.util.ArrayList;
-import java.util.List;
 
-import org.bibsonomy.common.enums.GroupingEntity;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+import org.bibsonomy.common.enums.GroupID;
+import org.bibsonomy.database.managers.BibTexDatabaseManager;
+import org.bibsonomy.database.util.DBSession;
+import org.bibsonomy.database.util.DBSessionFactory;
+import org.bibsonomy.database.util.IbatisDBSessionFactory;
 import org.bibsonomy.model.BibTex;
 import org.bibsonomy.model.Post;
 import org.bibsonomy.model.Resource;
 import org.bibsonomy.model.logic.LogicInterface;
-import org.bibsonomy.webapp.command.ListCommand;
 import org.bibsonomy.webapp.command.ajax.AjaxGetPublicationsByPartialTitleCommand;
 import org.bibsonomy.webapp.util.MinimalisticController;
 import org.bibsonomy.webapp.util.View;
@@ -16,7 +21,8 @@ import org.bibsonomy.webapp.view.Views;
 
 public class GetPublicationsByPartialTitleController<R extends Resource> implements MinimalisticController <AjaxGetPublicationsByPartialTitleCommand> {
 	protected LogicInterface logic;
-	
+	protected final BibTexDatabaseManager db = BibTexDatabaseManager.getInstance();
+
 	public void setLogic ( LogicInterface logic ) {
 		this.logic = logic;
 	}
@@ -27,15 +33,14 @@ public class GetPublicationsByPartialTitleController<R extends Resource> impleme
 	
 	public View workOn ( AjaxGetPublicationsByPartialTitleCommand command ) {
 		if(command.getTitle() != null) {
-			List <String>tags = new ArrayList<String>();
-			tags.add(new String("sys:title:"+command.getTitle()+"%"));
-			
-			ListCommand<Post<BibTex>> postListCommand = new ListCommand<Post<BibTex>>(command);
-			postListCommand.setList(this.logic.getPosts(BibTex.class, GroupingEntity.ALL, null,
-					tags, null, null, null, 0, 10, null));
-			command.setPosts(postListCommand);
+			DBSessionFactory dbSessionFactory = new IbatisDBSessionFactory();
+			DBSession dbSession = dbSessionFactory.getDatabaseSession();
+			Set<String> allowedGroups = new TreeSet<String>();
+			allowedGroups.add(GroupID.PUBLIC.name());
+			//List<Post<BibTex>> getPostsByTitleLucene(final String search, final int groupId, final String requestedUserName, final String userName, final Set<String> requestedGroupName, final int limit, final int offset, final DBSession session);
+			List<Post<BibTex>> titles = this.db.getPostsByTitleLucene(command.getTitle()+"%", 0, null, command.getUserName(), allowedGroups, 10, 0, dbSession);
+			dbSession.close();
 		}
-
 		
 		
 		return Views.AJAX_GET_PUBLICATION_TITLES;
