9cd062ba06d3dfc1a38877583354e81ffc0b4a22
Robert Jäschke <jaeschke@l3s.de>
2010-03-08 18:23:53 +0100

    small cleanups

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/BasketPageController.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/BasketPageController.java
@@ -1,15 +1,14 @@
 package org.bibsonomy.webapp.controller;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.bibsonomy.common.enums.GroupingEntity;
 import org.bibsonomy.model.Post;
 import org.bibsonomy.model.Resource;
 import org.bibsonomy.webapp.command.BibtexResourceViewCommand;
+import org.bibsonomy.webapp.util.ErrorAware;
 import org.bibsonomy.webapp.util.MinimalisticController;
 import org.bibsonomy.webapp.util.View;
-import org.bibsonomy.webapp.view.ExtendedRedirectView;
 import org.bibsonomy.webapp.view.Views;
+import org.springframework.validation.Errors;
 
 /**
  * Controller for the basket page
@@ -17,65 +16,70 @@ import org.bibsonomy.webapp.view.Views;
  * @author Dominik Benz, benz@cs.uni-kassel.de
  * @version $Id$
  */
-public class BasketPageController extends SingleResourceListController implements MinimalisticController<BibtexResourceViewCommand>{
+public class BasketPageController extends SingleResourceListController implements MinimalisticController<BibtexResourceViewCommand>, ErrorAware {
 
-	private static final Log log = LogFactory.getLog(BasketPageController.class);
-	
+	private Errors errors;
+
 	public View workOn(BibtexResourceViewCommand command) {
-		
-		log.debug(this.getClass().getSigners());
+
 		this.startTiming(this.getClass(), command.getFormat());
-						
+
 		// if user is not logged in, redirect him to login page
 		if (command.getContext().isUserLoggedIn() == false) {
-			log.info("Trying to access basket without being logged in");
-			return new ExtendedRedirectView("/login");
+			errors.reject("error.general.login");
+			return Views.ERROR;
 		}				
-		
+
 		// set login user name + grouping entity = BASKET
 		final String loginUserName = command.getContext().getLoginUser().getName();
 		final GroupingEntity groupingEntity = GroupingEntity.BASKET;
-		
+
 		// determine which lists to initalize depending on the output format 
 		// and the requested resourcetype
 		this.chooseListsToInitialize(command.getFormat(), command.getResourcetype());
-		
+
 		// retrieve and set the requested resource lists
 		for (final Class<? extends Resource> resourceType : listsToInitialise) {			
 			final int entriesPerPage = command.getListCommand(resourceType).getEntriesPerPage();
-			
+
 			this.setList(command, resourceType, groupingEntity, loginUserName, null, null, null, null, null, entriesPerPage);
 			this.postProcessAndSortList(command, resourceType);
-			
+
 			/*
 			 * set all posts from basket page to "picked" such that their "pick"
 			 * link changes to "unpick"
 			 */
 			for (final Post<? extends Resource> post : command.getListCommand(resourceType).getList()){
-					post.setPicked(true);
+				post.setPicked(true);
 			}
 
 			// the number of items in this user's basket has already been fetched
 			command.getListCommand(resourceType).setTotalCount(command.getContext().getLoginUser().getBasket().getNumPosts());
 		}	
-		
-				
-		if (command.getFormat().equals("html")) {
-			// TODO i18n
+
+		this.endTiming();			
+		if ("html".equals(command.getFormat())) {
 			command.setPageTitle(" :: basket" );
-			this.endTiming();			
 			return Views.BASKETPAGE;	
 		}
-		
-		this.endTiming();
-		
+
 		// export - return the appropriate view
 		return Views.getViewByFormat(command.getFormat());
-		
+
 	}
-	
+
 	public BibtexResourceViewCommand instantiateCommand() {
 		return new BibtexResourceViewCommand();
+	}
+
+	@Override
+	public Errors getErrors() {
+		return errors;
+	}
+
+	@Override
+	public void setErrors(Errors errors) {
+		this.errors = errors;
 	}
 
 }
--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/InboxPageController.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/InboxPageController.java
@@ -1,72 +1,76 @@
 package org.bibsonomy.webapp.controller;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.bibsonomy.common.enums.GroupingEntity;
 import org.bibsonomy.model.Post;
 import org.bibsonomy.model.Resource;
 import org.bibsonomy.webapp.command.UserResourceViewCommand;
+import org.bibsonomy.webapp.util.ErrorAware;
 import org.bibsonomy.webapp.util.MinimalisticController;
 import org.bibsonomy.webapp.util.View;
-import org.bibsonomy.webapp.view.ExtendedRedirectView;
 import org.bibsonomy.webapp.view.Views;
 import org.springframework.validation.Errors;
 
 /**
  * Controller for the InboxPage (shows all posts in your inbox)
+ * 
  * @author sdo
  * @version $Id$
  */
-public class InboxPageController extends SingleResourceListController implements MinimalisticController<UserResourceViewCommand> {
-	private static final Log log = LogFactory.getLog(PopularPageController.class);
+public class InboxPageController extends SingleResourceListController implements MinimalisticController<UserResourceViewCommand>, ErrorAware {
 	private Errors errors;
 
-	
 	public View workOn(final UserResourceViewCommand command) {
 		/*
 		 * FIXME: implement filter=no parameter
 		 */
-		log.debug(this.getClass().getSimpleName());
+
 		// user has to be logged in
 		if (!command.getContext().isUserLoggedIn()){
-			log.info("Trying to access inbox without being logged in");
-			return new ExtendedRedirectView("/login");
+			errors.reject("error.general.login");
+			return Views.ERROR;
 		}
 		this.startTiming(this.getClass(), command.getFormat());
-		
+
 		// determine which lists to initialize depending on the output format 
 		// and the requested resource type
 		this.chooseListsToInitialize(command.getFormat(), command.getResourcetype());		
-		String loginUserName = command.getContext().getLoginUser().getName();
+		final String loginUserName = command.getContext().getLoginUser().getName();
 		// retrieve and set the requested resource lists
 		for (final Class<? extends Resource> resourceType : listsToInitialise) {
 			// disable manual setting of start value for homepage
 			command.getListCommand(resourceType).setStart(0);
 			setList(command, resourceType, GroupingEntity.INBOX, loginUserName, null, null, null, null, null, 20);
 			postProcessAndSortList(command, resourceType);
-			for (Object p : command.getListCommand(resourceType).getList()){
-				if (p instanceof Post<?>){
-					Post<?> postItem = (Post<?>)p;
-					postItem.setInboxPost(true);
-				}
+
+			/*
+			 * mark all posts to be inbox posts (such that the "remove" link appears 
+			 */
+			for (final Post<? extends Resource> post: command.getListCommand(resourceType).getList()){
+				post.setInboxPost(true);
 			}
 		}
-												
+		this.endTiming();
+
 		// html format - retrieve tags and return HTML view
-		if (command.getFormat().equals("html")) {
+		if ("html".equals(command.getFormat())) {
 			command.setPageTitle("inbox");
-			//setTags(command, Resource.class, GroupingEntity.USER, command.getRequestedUser(), null, null, null, null, 0, Integer.MAX_VALUE, null);
-			this.endTiming();
 			return Views.INBOX;		
 		}
-		
-		this.endTiming();
+
 		// export - return the appropriate view
 		return Views.getViewByFormat(command.getFormat());	
 	}
-		
+
 	public UserResourceViewCommand instantiateCommand() {
 		return new UserResourceViewCommand();
+	}
+
+	public Errors getErrors() {
+		return this.errors;
+	}
+
+	public void setErrors(Errors errors) {
+		this.errors = errors;
 	}
 
 }
