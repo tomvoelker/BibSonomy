9f6d53bd5356ae3971bc2cdb09cdce778f42d959
Sven Stefani <sst@cs.uni-kassel.de>
2010-03-12 15:33:54 +0100

    resourcelistcontroller wurde verändert.

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/actions/EditPostController.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/actions/EditPostController.java
@@ -260,7 +260,11 @@ public abstract class EditPostController<RESOURCE extends Resource,COMMAND exten
 		 * get the tag cloud of the user (this must be done before any error
 		 * checking, because the user must have this) 
 		 */
-		this.setTags(command, Resource.class, GroupingEntity.USER, loginUser.getName(), null, null, null, null, 0, 20000, null);
+		//this.setTags(command, Resource.class, GroupingEntity.USER, loginUser.getName(), null, null, null, null, 0, 20000, null);
+		// nach Aenderungen im ResourceListController hier die Funktion angepasst 2010-03-12/sst
+		//this.setTags(command, Resource.class, GroupingEntity.USER, loginUser.getName(), null, null, null, null, 0, 20000, null);
+		this.setTags(command, Resource.class, GroupingEntity.USER, loginUser.getName(), null, null, null, 20000, null);
+
 		/*
 		 * get the relations of the user
 		 */
