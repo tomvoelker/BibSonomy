a01b32c682e45eaf5010b5aee84aa31e8b165d32
Robert Jäschke <jaeschke@l3s.de>
2010-03-22 18:00:55 +0100

    added call to Lucene to get related tags for author using Lucene (UNTESTED)

--- bibsonomy-database/src/main/java/org/bibsonomy/database/managers/chain/tag/get/GetRelatedTagsByAuthorAndTags.java
+++ bibsonomy-database/src/main/java/org/bibsonomy/database/managers/chain/tag/get/GetRelatedTagsByAuthorAndTags.java
@@ -1,16 +1,17 @@
 package org.bibsonomy.database.managers.chain.tag.get;
 
+import static org.bibsonomy.util.ValidationUtils.nullOrEqual;
+import static org.bibsonomy.util.ValidationUtils.present;
+
 import java.util.List;
 
+import org.bibsonomy.common.enums.GroupID;
 import org.bibsonomy.common.enums.SearchEntity;
 import org.bibsonomy.database.managers.chain.tag.TagChainElement;
 import org.bibsonomy.database.params.TagParam;
 import org.bibsonomy.database.util.DBSession;
 import org.bibsonomy.model.Tag;
 import org.bibsonomy.model.enums.Order;
-
-import static org.bibsonomy.util.ValidationUtils.nullOrEqual;
-import static org.bibsonomy.util.ValidationUtils.present;
 /**
  * @author mwa
  * @version $Id$
@@ -19,6 +20,12 @@ public class GetRelatedTagsByAuthorAndTags extends TagChainElement{
 	
 	@Override
 	protected List<Tag> handle(TagParam param, DBSession session) {
+
+		if (this.db.isDoLuceneSearch()) {
+			// FIXME: which parameters do we actually need?
+			return this.db.getTagsByAuthorLucene(param.getRawSearch(), GroupID.PUBLIC.getId(), param.getRequestedUserName(), param.getRequestedGroupName(), null, null, null, param.getSimHash(), null, session);
+		}
+		
 		return this.db.getRelatedTagsByAuthorAndTag(param, session);
 	}
 
