53d186752be124803cb2f337079d78dbd84f05af
Sven Stefani <sst@cs.uni-kassel.de>
2010-03-22 15:51:35 +0100

    added border to sidebar for all pages with sidebar added _static_ help-box to all pages with sidebar

--- bibsonomy-webapp/src/main/webapp/resources_puma/javascript/tools.js
+++ bibsonomy-webapp/src/main/webapp/resources_puma/javascript/tools.js
@@ -1,7 +1,7 @@
 /*
 	replaces content of first text child node of given element 
  */
-function replaceElementsTextnode(elname, t) {
+function replaceElementsTextnode(elname, t, color) {
 	
 	// get Element
 	e = document.getElementById(elname);
@@ -9,19 +9,38 @@ function replaceElementsTextnode(elname, t) {
 	if (e) {
 		// replace nodevalue of first child
 		e.firstChild.nodeValue = t;
+		if (color && (color.length==7 || color.length==4)) {
+			e.setAttribute("style", "color:"+color+";");
+		}
+		else
+		{
+			e.removeAttribute("style");
+		}
 	}
 	
 }
 
-
-$(document).ready(function(){
-  
-	$("#nice_tnav").prepend('<div id="navitogglehelp" class="togglehelp">?</div>');
+function pumainit(){
 	
-	$(".togglehelp").click(function () {
-    $("#helpbox").toggle("slow");
-  });    
+	// add surrounding div .sidebarboxinner to #sidebar
+	$("#sidebar").wrap('<div class="sidebarBoxInner" id="sidebarboxinnerAroundSidebar" />');
 
-})
+	// add surrounding div with class="sidebarBoxOuter" id="sidebox" to #sidebarboxinnerAroundSidebar
+	$("#sidebarboxinnerAroundSidebar").wrap('<div class="sidebarBoxOuter" id="sidebox" />');
+	
+	// add help box as sidebox to sidebar
+	$("#sidebarroundcorner").prepend('<div class="sidebarBoxOuter" id="helpbox"><div class="sidebarBoxInner"><div id="helpboxcontent" class="boxcontent"><div id="togglehelp" class="togglehelp closeX">x</div><div class="bc-head">Quicklinks</div><a href="/login">Loggen Sie sich jetzt mit Ihren Bibliotheksausweis ein.</a><br />CV anzeigen: /cv/user/&lt;username&gt;" zur URL hinzufügen<br /><br/><div class="bc-head">Hilfe</div>Weitere Informationen erhalten sie auf den <a href="http://www.ub.uni-kassel.de/puma.html">Projektseiten</a>, in der Hilfe und den beiden Blogs <a href="http://puma-projekt.blogspot.com/">Puma-Projekt</a> und <a href="http://bibsonomy.blogspot.com/">Bibsonomy</a>.<br />Video-Tutorials<br /><div class="bc-highlight">Diese PUMA-Version ist noch in einem frühen Entwicklungsstadium. Daher funktionieren einige Funktionen noch nicht (korrekt).</div></div></div></div>');
+	
+	
+	$(".sidebarBoxInner").corner("round 8px").parent().css('padding', '3px').corner("round 8px");
+	
+	// help side box toggle
+	$("#nice_tnav").prepend('<div id="navitogglehelp" class="togglehelp">?</div>');
+	$(".togglehelp").click(function () {
+		$("#helpbox").toggle("slow");
+	})
+}
+	
+$(document).ready(pumainit);    
 
 
