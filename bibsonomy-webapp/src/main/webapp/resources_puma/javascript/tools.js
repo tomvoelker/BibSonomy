93f4564c596d2035ebfef2d53662ce2d870d1162
Sven Stefani <sst@cs.uni-kassel.de>
2010-03-26 15:05:56 +0100

    added two divs into sidebarroundcorner around sidebar to have possibility for more than one sideboxes (in puma)

--- bibsonomy-webapp/src/main/webapp/resources_puma/javascript/tools.js
+++ bibsonomy-webapp/src/main/webapp/resources_puma/javascript/tools.js
@@ -20,17 +20,17 @@ function replaceElementsTextnode(elname, t, color) {
 	
 }
 
+
 function pumainit(){
 	
-	// add surrounding div .sidebarboxinner to #sidebar
-	$("#sidebar").wrap('<div class="sidebarBoxInner" id="sidebarboxinnerAroundSidebar" />');
+	// add surrounding div .sidebarboxinner to #sidebarroundcorner's content
+//	$("#sidebarroundcorner").wrapInner('<div class="sidebarBoxInner" />');
 
-	// add surrounding div with class="sidebarBoxOuter" id="sidebox" to #sidebarboxinnerAroundSidebar
-	$("#sidebarboxinnerAroundSidebar").wrap('<div class="sidebarBoxOuter" id="sidebox" />');
+	// add surrounding div with class="sidebarBoxOuter" id="sidebox" to #sidebarroundcorner's content
+//	$("#sidebarroundcorner").wrapInner('<div class="sidebarBoxOuter" id="sidebox" />');
 	
-	// add help box as sidebox to sidebar
+	// add help box as sidebox in front of #sidebarroundcorner's content
 	$("#sidebarroundcorner").prepend('<div class="sidebarBoxOuter" id="helpbox"><div class="sidebarBoxInner"><div id="helpboxcontent" class="boxcontent"><div id="togglehelp" class="togglehelp closeX">x</div><div class="bc-head">Quicklinks</div><a href="/login">Loggen Sie sich jetzt mit Ihren Bibliotheksausweis ein.</a><br />CV anzeigen: /cv/user/&lt;username&gt;" zur URL hinzufügen<br /><br/><div class="bc-head">Hilfe</div>Weitere Informationen erhalten sie auf den <a href="http://www.ub.uni-kassel.de/puma.html">Projektseiten</a>, in der Hilfe und den beiden Blogs <a href="http://puma-projekt.blogspot.com/">Puma-Projekt</a> und <a href="http://bibsonomy.blogspot.com/">Bibsonomy</a>.<br />Video-Tutorials<br /><div class="bc-highlight">Diese PUMA-Version ist noch in einem frühen Entwicklungsstadium. Daher funktionieren einige Funktionen noch nicht (korrekt).</div></div></div></div>');
-	
 	
 	$(".sidebarBoxInner").corner("round 8px").parent().css('padding', '3px').corner("round 8px");
 	
@@ -39,6 +39,8 @@ function pumainit(){
 	$(".togglehelp").click(function () {
 		$("#helpbox").toggle("slow");
 	})
+
+
 }
 	
 $(document).ready(pumainit);    
