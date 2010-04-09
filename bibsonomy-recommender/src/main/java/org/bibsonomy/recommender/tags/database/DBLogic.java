cc50e384883ac68b0578c3957c8702d422d61f0f
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2010-04-09 09:57:35 +0200

    removed unnecessary @ Suppresswarnings("unchecked") annotations
    updated javadoc

--- bibsonomy-recommender/src/main/java/org/bibsonomy/recommender/tags/database/DBLogic.java
+++ bibsonomy-recommender/src/main/java/org/bibsonomy/recommender/tags/database/DBLogic.java
@@ -10,7 +10,6 @@ import org.bibsonomy.model.Post;
 import org.bibsonomy.model.RecommendedTag;
 import org.bibsonomy.model.Resource;
 import org.bibsonomy.recommender.tags.database.params.Pair;
-import org.bibsonomy.recommender.tags.database.params.RecAdminOverview;
 import org.bibsonomy.recommender.tags.database.params.RecQueryParam;
 import org.bibsonomy.recommender.tags.database.params.RecSettingParam;
 import org.bibsonomy.recommender.tags.database.params.SelectorSettingParam;
@@ -31,24 +30,25 @@ public interface DBLogic {
 	 * @param userName user who submitted post
 	 * @param date querie's timestamp
 	 * @param post user's post
+	 * @param postID TODO
 	 * @param timeout querie's timeout value
 	 * @return unique query id
 	 * 
 	 * @throws SQLException 
 	 */
-	@SuppressWarnings("unchecked")
-	public abstract Long addQuery(String userName, Date date, Post<? extends Resource> post, int postID, int  timeout) throws SQLException;
+	public Long addQuery(String userName, Date date, Post<? extends Resource> post, int postID, int timeout) throws SQLException;
 
 	/**
 	 * Add recommender to given query.
 	 * 
 	 * @param queryId
 	 * @param recId
+	 * @param recDescr 
 	 * @param recMeta
 	 * @return unique identifier for given recommender settings
 	 * @throws SQLException 
 	 */
-	public abstract Long addRecommender(Long queryId, String recId, String recDescr, byte[] recMeta) throws SQLException;
+	public Long addRecommender(Long queryId, String recId, String recDescr, byte[] recMeta) throws SQLException;
 
 	/**
 	 * adds given recommender (identified by it's id) to given query
@@ -58,26 +58,29 @@ public interface DBLogic {
 	 * @throws SQLException
 	 */
 	public void addRecommenderToQuery(Long qid, Long sid ) throws SQLException;
-
 	
 	/**
 	 * insert recommender setting into table - if given setting already exists,
 	 * return its id. This should always be embedded into a transaction.
+	 * @param recId 
+	 * @param recDescr 
+	 * @param recMeta 
 	 * 
 	 * @return unique identifier for given settings
+	 * @throws SQLException
 	 */
 	public Long insertRecommenderSetting(String recId, String recDescr, byte[] recMeta) throws SQLException;
-
 	
 	/**
 	 * Add result selector to given query.
 	 * @param qid query id
-	 * @param resultSelector
+	 * @param selectorInfo 
+	 * @param selectorMeta 
 	 * 
-	 * @return
+	 * @return TODO
 	 * @throws SQLException 
 	 */
-	public abstract Long addResultSelector(Long qid, String selectorInfo, byte[] selectorMeta) throws SQLException;
+	public Long addResultSelector(Long qid, String selectorInfo, byte[] selectorMeta) throws SQLException;
 
 	/**
 	 * set selection strategy which was applied in given query
@@ -91,6 +94,8 @@ public interface DBLogic {
    /**
 	 * insert selector setting into table - if given setting already exists,
 	 * return its id. This should always be embedded into a transaction.
+	 * @param selectorInfo 
+	 * @param selectorMeta 
 	 * 
 	 * @return unique identifier for given settings
 	 * @throws SQLException 
@@ -104,7 +109,7 @@ public interface DBLogic {
 	 * @param sid recommender's setting id
 	 * @throws SQLException 
 	 */
-	public abstract void addSelectedRecommender(Long qid, Long sid) throws SQLException;
+	public void addSelectedRecommender(Long qid, Long sid) throws SQLException;
 
 	/**
 	 * Add recommender's recommended tags.
@@ -116,17 +121,17 @@ public interface DBLogic {
 	 * @return number of recommendations added
 	 * @throws SQLException
 	 */
-	public abstract int addRecommendation(Long queryId, Long settingsId, SortedSet<RecommendedTag> tags, long latency) throws SQLException;
+	public int addRecommendation(Long queryId, Long settingsId, SortedSet<RecommendedTag> tags, long latency) throws SQLException;
 
 	/**
 	 * Connect postID with recommendation.
-	 *    For each post process an unique id is generated. This is used for mapping 
-	 *    posts to recommendations and vice verca.  
+	 *  For each post process an unique id is generated. This is used for mapping 
+	 *  posts to recommendations and vice verca. 
 	 * @param post post as stored in bibsonomy
-	 * @param post's random id as generated in PostBookmarkController
+	 * @param postID post's random id as generated in PostBookmarkController
 	 * @throws SQLException 
 	 */
-	public abstract void connectWithPost(Post<? extends Resource> post, int postID) throws SQLException;
+	public void connectWithPost(Post<? extends Resource> post, int postID) throws SQLException;
 
 	/**
 	 * Get sorted list of tags recommended in a given query by a given recommender. 
@@ -136,18 +141,18 @@ public interface DBLogic {
 	 * @return tags recommended in query identified by qid and recommender identified by sid
 	 * @throws SQLException
 	 */
-	public abstract SortedSet<RecommendedTag> getRecommendations(Long qid, Long sid) throws SQLException;
+	public SortedSet<RecommendedTag> getRecommendations(Long qid, Long sid) throws SQLException;
 
 	/**
 	 * Append tags which were recommended in a given query by a given recommender to a given collection. 
 	 * 
 	 * @param qid
 	 * @param sid
-	 * @return tags recommended in query identified by qid and recommender identified by sid
+	 * @param recommendedTags 
+	 * 
 	 * @throws SQLException
 	 */
-	@SuppressWarnings("unchecked")
-	public abstract void getRecommendations(Long qid, Long sid, Collection<RecommendedTag> recommendedTags) throws SQLException;
+	public void getRecommendations(Long qid, Long sid, Collection<RecommendedTag> recommendedTags) throws SQLException;
 
 	/**
 	 * Get sorted list of tags recommended in a given query. 
@@ -156,7 +161,7 @@ public interface DBLogic {
 	 * @return tags recommended in query identified by qid and all recommenders 
 	 * @throws SQLException
 	 */
-	public abstract SortedSet<RecommendedTag> getRecommendations(Long qid) throws SQLException;
+	public SortedSet<RecommendedTag> getRecommendations(Long qid) throws SQLException;
 
 	/**
 	 * Append tags which are recommended in a given query to given collection 
@@ -165,8 +170,7 @@ public interface DBLogic {
 	 * @param recommendedTags collection where recommended tags should be appended
 	 * @throws SQLException
 	 */
-	@SuppressWarnings("unchecked")
-	public abstract void getRecommendations(Long qid, Collection<RecommendedTag> recommendedTags) throws SQLException;
+	public void getRecommendations(Long qid, Collection<RecommendedTag> recommendedTags) throws SQLException;
 
 	/**
 	 * Get (unsorted) list of selected tags for a given query. 
@@ -175,8 +179,7 @@ public interface DBLogic {
 	 * @return tags recommended in query identified by qid and all recommenders 
 	 * @throws SQLException
 	 */
-	@SuppressWarnings("unchecked")
-	public abstract List<RecommendedTag> getSelectedTags(Long qid) throws SQLException;
+	public List<RecommendedTag> getSelectedTags(Long qid) throws SQLException;
 
 	/**
 	 * Get list of recommender settings which where selected for given query.
@@ -185,8 +188,7 @@ public interface DBLogic {
 	 * @return list of recommender settings 
 	 * @throws SQLException 
 	 */
-	@SuppressWarnings("unchecked")
-	public abstract List<Long> getSelectedRecommenderIDs(Long qid) throws SQLException;
+	public List<Long> getSelectedRecommenderIDs(Long qid) throws SQLException;
 
 	/**
 	 * Get list of newest tas entries
@@ -195,8 +197,7 @@ public interface DBLogic {
 	 * @return list of range number of new entries, starting by offset 
 	 * @throws SQLException 
 	 */
-	@SuppressWarnings("unchecked")
-	public abstract List<TasEntry> getNewestEntries(Integer offset, Integer range) throws SQLException;
+	public List<TasEntry> getNewestEntries(Integer offset, Integer range) throws SQLException;
 
 	/**
 	 * Get user's most popular tag names with corresponding tag frequencies 
@@ -207,8 +208,7 @@ public interface DBLogic {
 	 * @return list of pairs [tagname,frequency]
 	 * @throws SQLException 
 	 */
-	@SuppressWarnings("unchecked")
-	public abstract List<Pair<String, Integer>> getMostPopularTagsForUser(final String username, final int range) throws SQLException;
+	public List<Pair<String, Integer>> getMostPopularTagsForUser(final String username, final int range) throws SQLException;
 
 	/**
 	 * Gets the most popular tags of the given resource.
@@ -220,8 +220,7 @@ public interface DBLogic {
 	 * @return The most popular tags of the given resource.
 	 * @throws SQLException
 	 */
-	@SuppressWarnings("unchecked")
-	public abstract <T extends Resource> List<Pair<String, Integer>> getMostPopularTagsForResource(final Class<T> resourceType, final String intraHash, final int range) throws SQLException;
+	public <T extends Resource> List<Pair<String, Integer>> getMostPopularTagsForResource(final Class<T> resourceType, final String intraHash, final int range) throws SQLException;
 
 	/**
 	 * Get number of tags used by given user. 
@@ -229,8 +228,7 @@ public interface DBLogic {
 	 * @return number of tags used by given user
 	 * @throws SQLException 
 	 */
-	@SuppressWarnings("unchecked")
-	public abstract Integer getNumberOfTagsForUser(String username) throws SQLException;
+	public Integer getNumberOfTagsForUser(String username) throws SQLException;
 
 	/**
 	 * Get number of TAS of the given user. 
@@ -238,8 +236,7 @@ public interface DBLogic {
 	 * @return number of TAS of given user
 	 * @throws SQLException 
 	 */
-	@SuppressWarnings("unchecked")
-	public abstract Integer getNumberOfTasForUser(String username) throws SQLException;
+	public Integer getNumberOfTasForUser(String username) throws SQLException;
 
 	/**
 	 * Get number of tags attached to a given resource.. 
@@ -250,8 +247,7 @@ public interface DBLogic {
 	 * @return The number of tags attached to the resource.
 	 * @throws SQLException 
 	 */
-	@SuppressWarnings("unchecked")
-	public abstract <T extends Resource> Integer getNumberOfTagsForResource(final Class<T> resourceType, final String intraHash) throws SQLException;
+	public <T extends Resource> Integer getNumberOfTagsForResource(final Class<T> resourceType, final String intraHash) throws SQLException;
 
 	/**
 	 * Get number of TAS for a given resource.. 
@@ -262,8 +258,7 @@ public interface DBLogic {
 	 * @return The number of TAS of the resource.
 	 * @throws SQLException 
 	 */
-	@SuppressWarnings("unchecked")
-	public abstract <T extends Resource> Integer getNumberOfTasForResource(final Class<T> resourceType, final String intraHash) throws SQLException;
+	public <T extends Resource> Integer getNumberOfTasForResource(final Class<T> resourceType, final String intraHash) throws SQLException;
 	
 	/**
 	 * Maps BibSonomy's user name to corresponding user id
@@ -271,7 +266,7 @@ public interface DBLogic {
 	 * @param userName user's name
 	 * @return user's id, null if user name doesn't exist
 	 */
-	public abstract Integer getUserIDByName(String userName);	
+	public Integer getUserIDByName(String userName);	
 
 	/**
 	 * Maps BibSonomy's user id to corresponding user name
@@ -279,7 +274,7 @@ public interface DBLogic {
 	 * @param userID user's id
 	 * @return user's name, null if user id doesn't exist
 	 */
-	public abstract String getUserNameByID(int userID);
+	public String getUserNameByID(int userID);
 
 	/**
 	 * Get list of all tags from given recommender and query
@@ -288,8 +283,7 @@ public interface DBLogic {
 	 * @return list of all tags from given recommender and query
 	 * @throws SQLException 
 	 */
-	@SuppressWarnings("unchecked")
-	public abstract List<String> getTagNamesForRecQuery(Long sid, Long qid) throws SQLException;
+	public List<String> getTagNamesForRecQuery(Long sid, Long qid) throws SQLException;
 
 	/**
 	 * Get list of all tags chosen by user for given post
@@ -297,8 +291,7 @@ public interface DBLogic {
 	 * @return list of all tags chosen by user for given post
 	 * @throws SQLException 
 	 */
-	@SuppressWarnings("unchecked")
-	public abstract List<String> getTagNamesForPost(Integer cid) throws SQLException;
+	public List<String> getTagNamesForPost(Integer cid) throws SQLException;
 
 	/**
 	 * Returns details for given recommender.
@@ -306,7 +299,7 @@ public interface DBLogic {
 	 * @return Details for given recommender if found -- null otherwise
 	 * @throws SQLException 
 	 */
-	public abstract RecSettingParam getRecommender(Long sid) throws SQLException;
+	public RecSettingParam getRecommender(Long sid) throws SQLException;
 
 	/**
 	 * Get list of all recommenders (id) which delivered tags in given query.
@@ -314,8 +307,7 @@ public interface DBLogic {
 	 * @return list of ids
 	 * @throws SQLException 
 	 */
-	@SuppressWarnings("unchecked")
-	public abstract List<Long> getActiveRecommenderIDs(Long qid) throws SQLException;
+	public List<Long> getActiveRecommenderIDs(Long qid) throws SQLException;
 
 	/**
 	 * Get list of all recommenders (id) which where queried.
@@ -323,8 +315,7 @@ public interface DBLogic {
 	 * @return list of ids
 	 * @throws SQLException 
 	 */
-	@SuppressWarnings("unchecked")
-	public abstract List<Long> getAllRecommenderIDs(Long qid) throws SQLException;
+	public List<Long> getAllRecommenderIDs(Long qid) throws SQLException;
 
 	/**
 	 * Get list of all recommenders (id) which where queried.
@@ -332,8 +323,7 @@ public interface DBLogic {
 	 * @return list of ids
 	 * @throws SQLException 
 	 */
-	@SuppressWarnings("unchecked")
-	public abstract List<Pair<Long, Long>> getRecommenderSelectionCount(Long qid) throws SQLException;
+	public List<Pair<Long, Long>> getRecommenderSelectionCount(Long qid) throws SQLException;
 
 	/**
 	 * Get list of all recommenders (id) which where queried and not selected previously 
@@ -344,8 +334,7 @@ public interface DBLogic {
 	 * @throws SQLException 
 	 * @throws SQLException 
 	 */
-	@SuppressWarnings("unchecked")
-	public abstract List<Long> getAllNotSelectedRecommenderIDs(Long qid) throws SQLException;
+	public List<Long> getAllNotSelectedRecommenderIDs(Long qid) throws SQLException;
 
 	/**
 	 * Returns details for given selector.
@@ -354,7 +343,7 @@ public interface DBLogic {
 	 * @throws SQLException 
 	 * @throws SQLException 
 	 */
-	public abstract SelectorSettingParam getSelector(Long sid) throws SQLException;
+	public SelectorSettingParam getSelector(Long sid) throws SQLException;
 
 	/**
 	 * Return query information for given query id
@@ -362,7 +351,7 @@ public interface DBLogic {
 	 * @return RecQueryParam on success, null otherwise
 	 * @throws SQLException 
 	 */
-	public abstract RecQueryParam getQuery(Long qid) throws SQLException;
+	public RecQueryParam getQuery(Long qid) throws SQLException;
 
 	/**
 	 * Return list of all queries for given recommender 
@@ -370,8 +359,7 @@ public interface DBLogic {
 	 * @return list of all queries for given recommender
 	 * @throws SQLException 
 	 */
-	@SuppressWarnings("unchecked")
-	public abstract List<RecQueryParam> getQueriesForRecommender(Long sid) throws SQLException;
+	public List<RecQueryParam> getQueriesForRecommender(Long sid) throws SQLException;
 
 	/**
 	 * Tries to guess query_id from given content id.
@@ -380,8 +368,7 @@ public interface DBLogic {
 	 * @return nearest query_id, if guess is possible -- otherwise null
 	 * @throws SQLException
 	 */
-	@SuppressWarnings("unchecked")
-	public abstract Long guessQueryFromPost(Integer content_id) throws SQLException;
+	public Long guessQueryFromPost(Integer content_id) throws SQLException;
 
 	/**
 	 * Guess content_id for given query_id.
@@ -390,28 +377,36 @@ public interface DBLogic {
 	 * @return nearest content_id if found -- otherwise null
 	 * @throws SQLException 
 	 */
-	@SuppressWarnings("unchecked")
-	public abstract Integer guessPostFromQuery(Long query_id) throws SQLException;
+	public Integer guessPostFromQuery(Long query_id) throws SQLException;
 
 	/**
 	 * Get queryID for given postID, user_name and date
+	 * @param user_name 
+	 * @param date 
 	 * @param postID
-	 * @return
+	 * @return TODO
 	 * @throws SQLException 
 	 */
-	public abstract Long getQueryForPost(String user_name, Date date, Integer postID) throws SQLException;
+	public Long getQueryForPost(String user_name, Date date, Integer postID) throws SQLException;
 
 	/**
 	 * Get contentID for given queryID
+	 * @param queryID 
+	 * 
+	 * @return TODO
 	 * @throws SQLException 
 	 */
-	public abstract Integer getContentIDForQuery(Long queryID) throws SQLException;
+	public Integer getContentIDForQuery(Long queryID) throws SQLException;
 
 	/**
 	 * Get contentID for given query data
-	 * @throws SQLException 
+	 * @param userName 
+	 * @param date 
+	 * @param postID 
+	 * 
+	 * @return TODO
 	 */
-	public abstract Integer getContentIDForQuery(String userName, Date date, Integer postID);
+	public Integer getContentIDForQuery(String userName, Date date, Integer postID);
 
 	/**
 	 * Store selected recommended tags.
@@ -419,9 +414,10 @@ public interface DBLogic {
 	 * @param qid query id
 	 * @param rid result selector id
 	 * @param result set of recommended tags
+	 * @return TODO
 	 * @throws SQLException 
 	 */
-	public abstract int storeRecommendation(Long qid, Long rid, Collection<RecommendedTag> result) throws SQLException;
+	public int storeRecommendation(Long qid, Long rid, Collection<RecommendedTag> result) throws SQLException;
 
 	//------------------------------------------------------------------------
 	// logging interface implementation
@@ -436,6 +432,6 @@ public interface DBLogic {
 	 * @return true on success, false otherwise
 	 * @throws SQLException 
 	 */
-	public abstract boolean logRecommendation(Long qid, Long sid, long latency, SortedSet<RecommendedTag> tags, SortedSet<RecommendedTag> preset) throws SQLException;
+	public boolean logRecommendation(Long qid, Long sid, long latency, SortedSet<RecommendedTag> tags, SortedSet<RecommendedTag> preset) throws SQLException;
 
 }
