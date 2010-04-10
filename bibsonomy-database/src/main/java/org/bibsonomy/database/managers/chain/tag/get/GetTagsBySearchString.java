f217692f81e6da7c66971381c13701696a35b137
Folke Mitzlaff <mitzlaff@cs.uni-kassel.de>
2010-04-11 01:59:43 +0200

    fixed chain element for correctly handling new searchentity parameter attribute

--- bibsonomy-database/src/main/java/org/bibsonomy/database/managers/chain/tag/get/GetTagsBySearchString.java
+++ bibsonomy-database/src/main/java/org/bibsonomy/database/managers/chain/tag/get/GetTagsBySearchString.java
@@ -1,10 +1,12 @@
 package org.bibsonomy.database.managers.chain.tag.get;
 
+import static org.bibsonomy.util.ValidationUtils.nullOrEqual;
 import static org.bibsonomy.util.ValidationUtils.present;
 
 import java.util.List;
 
 import org.bibsonomy.common.enums.GroupingEntity;
+import org.bibsonomy.common.enums.SearchEntity;
 import org.bibsonomy.database.managers.chain.tag.TagChainElement;
 import org.bibsonomy.database.params.TagParam;
 import org.bibsonomy.database.util.DBSession;
@@ -26,13 +28,13 @@ public class GetTagsBySearchString extends TagChainElement {
 
 	@Override
 	protected boolean canHandle(final TagParam param) {
-		return ( present(param.getGrouping())              &&
-				 param.getGrouping() == GroupingEntity.ALL &&
-				 present(param.getSearch())                &&
-				!present(param.getSearchEntity())          &&
-				!present(param.getRegex())                 &&
-				!present(param.getTagIndex())              &&
-				!present(param.getHash())                  &&
+		return ( present(param.getGrouping())                      &&
+				 param.getGrouping() == GroupingEntity.ALL         &&
+			     nullOrEqual(param.getSearchEntity(), SearchEntity.ALL) &&
+				 present(param.getSearch())                        &&
+				!present(param.getRegex())                         &&
+				!present(param.getTagIndex())                      &&
+				!present(param.getHash())                          &&
 		//  	!present(param.getTagRelationType())       &&
 				!present(param.getBibtexKey()) );
 	}
