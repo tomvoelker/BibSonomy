9ca60db4273d62e01737a75ce5b5aa9c0134e830
Dominik Benz <benz@cs.uni-kassel.de>
2010-04-26 00:19:38 +0200

    quick (but not so efficient!!) implementation of backend for group reporting

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/reporting/GroupReportingPageController.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/reporting/GroupReportingPageController.java
@@ -1,11 +1,20 @@
 package org.bibsonomy.webapp.controller.reporting;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.bibsonomy.common.enums.GroupingEntity;
+import org.bibsonomy.model.BibTex;
+import org.bibsonomy.model.Post;
 import org.bibsonomy.model.logic.LogicInterface;
+import org.bibsonomy.model.util.BibTexUtils;
+import org.bibsonomy.util.SortUtils;
 import org.bibsonomy.webapp.command.reporting.GroupReportingCommand;
+import org.bibsonomy.webapp.exceptions.MalformedURLSchemeException;
 import org.bibsonomy.webapp.util.MinimalisticController;
 import org.bibsonomy.webapp.util.View;
 import org.bibsonomy.webapp.view.Views;
@@ -31,29 +40,99 @@ public class GroupReportingPageController implements MinimalisticController<Grou
 	@Override
 	public View workOn(GroupReportingCommand command) {
 		
+		// if no group given -> error FIXME: check errormsg
+		if (command.getRequestedGroup() == null) {
+			log.error("Invalid query /group without roup name");
+			throw new MalformedURLSchemeException("error.group_page_without_groupname");
+		}
+		
+		// if no tags given return FIXME: check errormsg
+		if (command.getRequestedTags() == null || command.getRequestedTags().length() == 0) {
+			log.error("Invalid query /tag without tag");
+			throw new MalformedURLSchemeException("error.tag_page_without_tag");
+		}		
+		
+		/*
+		 * assemble taglist
+		 */
+		List<String> tags = new ArrayList<String>();
+		tags.add(command.getRequestedTags());
+		
+		/*
+		 * fetch all bibtex & remove duplicates
+		 */
+		List<Post<BibTex>> groupBibtexEntries = logic.getPosts(BibTex.class, GroupingEntity.GROUP, command.getRequestedGroup(), tags, null, null, null, 0, 10000, null);
+		BibTexUtils.removeDuplicates(groupBibtexEntries);
+		
+		
+		/*
+		 * sort entries in descending order by year
+		 */
+		BibTexUtils.sortBibTexList(groupBibtexEntries, SortUtils.parseSortKeys("year"), SortUtils.parseSortOrders("desc"));
+		
+		
+		/*
+		 * init entrytypes 
+		 */
+		for (String type : Bibtex.entrytypes) { command.getPublicationCounts().getColumnHeaders().add(type); }
+		/*
+		 * loop over entries, accumulate and fill command
+		 */
+		BibTex bib;
+		int lastYear = Integer.MIN_VALUE;
+		HashMap<String,Integer> row = null;
+		for (Post<BibTex> post : groupBibtexEntries) {
+			bib = post.getResource();
+			try {
+				int curYear = Integer.valueOf(bib.getYear());
+				if ( curYear != lastYear) {
+					if (lastYear != -1) {
+						// write last row into command, if there is one
+						command.getPublicationCounts().getValues().put(lastYear, row);
+						command.getPublicationCounts().getRowHeaders().add(lastYear);
+					}
+					// init a new row with zero values
+					row = new HashMap<String,Integer>();
+					for (String type : Bibtex.entrytypes) {
+						row.put(type, 0);
+					}
+				}
+				// increment counter of type TYPE in current year
+				this.increment(row, bib.getType());
+				lastYear = curYear;
+				
+			} catch (NumberFormatException ex) {
+				// ignore silently
+			}
+			
+		}
+		// add the last year
+		command.getPublicationCounts().getValues().put(lastYear, row);
+		command.getPublicationCounts().getRowHeaders().add(lastYear);		
+		
 		/*
 		 *  create some dummy data for testing 
 		 *  FIXME: replace this by fetching the data from the logic instead!
 		 */		
-		final int[] dummyYears = {2009,2008,2007,2006,2005};
-		final String[] dummyTypes = Bibtex.entrytypes;
-		int dummyValue = 23;
-		
-		// initialize column + row headings
-		for (String type : dummyTypes) { command.getPublicationCounts().getColumnHeaders().add(type); }
-		for (Integer year : dummyYears) { command.getPublicationCounts().getRowHeaders().add(year); }		
-		
-		// init values
-		HashMap<String,Integer> row;
-		for (int year : dummyYears) {
-			row = new HashMap<String,Integer>();			
-			// write row values
-			for (String type : dummyTypes) {
-				row.put(type, dummyValue++);
-			}			
-			// store row in command
-			command.getPublicationCounts().getValues().put(year, row);
-		}
+//		final int[] dummyYears = {2009,2008,2007,2006,2005};
+//		final String[] dummyTypes = Bibtex.entrytypes;
+//		int dummyValue = 23;
+//		
+//		// initialize column + row headings
+//		for (String type : dummyTypes) { command.getPublicationCounts().getColumnHeaders().add(type); }
+//		for (Integer year : dummyYears) { command.getPublicationCounts().getRowHeaders().add(year); }		
+//		
+//		// init values
+//		HashMap<String,Integer> row2;
+//		for (int year : dummyYears) {
+//			row2 = new HashMap<String,Integer>();			
+//			// write row values
+//			for (String type : dummyTypes) {
+//				row2.put(type, dummyValue++);
+//			}			
+//			// store row in command
+//			command.getPublicationCounts().getValues().put(year, row);
+//		}
 						
 		
 		// TODO: add totals: sum up values for each years 
@@ -73,6 +152,17 @@ public class GroupReportingPageController implements MinimalisticController<Grou
 	@Override
 	public GroupReportingCommand instantiateCommand() {
 		return new GroupReportingCommand();
+	}
+	
+	/*
+	 * increment map at position 'key'
+	 */
+	private void increment(Map<String, Integer> map, String key) {
+		if (key == null || !map.containsKey(key.toLowerCase())) {
+			return;
+		}
+		int lastVal = map.get(key);
+		map.put(key, lastVal + 1);
 	}
 
 }
