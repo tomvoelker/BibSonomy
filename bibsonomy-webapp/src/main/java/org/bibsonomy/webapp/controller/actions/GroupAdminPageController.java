d228872bab1e7cfdf9aaa56a775656c9ea82963f
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2010-04-26 16:33:36 +0200

    - sysout + todo cleanup

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/actions/GroupAdminPageController.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/actions/GroupAdminPageController.java
@@ -1,5 +1,7 @@
 package org.bibsonomy.webapp.controller.actions;
 
+import static org.bibsonomy.util.ValidationUtils.present;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.bibsonomy.webapp.command.actions.GroupAdminCommand;
@@ -16,26 +18,27 @@ import org.bibsonomy.webapp.view.Views;
 import org.springframework.validation.Errors;
 
 /**
+ * TODO: check methods (auto generated)
+ * 
  * @author mwa
  * @version $Id$
  */
 public class GroupAdminPageController extends SingleResourceListController implements MinimalisticController<GroupAdminCommand>, ErrorAware, ValidationAwareController<GroupAdminCommand>, RequestAware {
-	
 	private static final Log log = LogFactory.getLog(GroupAdminPageController.class);
 	
-	public View workOn(GroupAdminCommand command) {
-		log.debug("--> GroupAdminPageController: workOn() called");
+	public View workOn(final GroupAdminCommand command) {
+		log.debug("GroupAdminPageController: workOn() called");
+		
+		final String groupName = command.getRequestedGroup();
 		
 		// no group given -> error
-		if (command.getRequestedGroup() == null) {
+		if (!present(groupName)) {
 			log.error("Invalid query /group without groupname");
 			throw new MalformedURLSchemeException("error.group_page_without_groupname");
-		}	
-		
-		String groupName = command.getRequestedGroup();
+		}
 		
 		command.setGroup(this.logic.getGroupDetails(groupName));
-		System.out.println("tagsets: "+command.getGroup().getTagSets().size());
+		log.debug("tagsets: " + command.getGroup().getTagSets().size());
 		return Views.ADMIN_GROUP;
 	}
 	
@@ -65,7 +68,6 @@ public class GroupAdminPageController extends SingleResourceListController imple
 	
 	public void setRequestLogic(RequestLogic requestLogic) {
 		// TODO Auto-generated method stub
-		
 	}
 
 }
