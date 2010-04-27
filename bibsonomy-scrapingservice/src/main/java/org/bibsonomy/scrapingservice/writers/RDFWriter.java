a96a5707f5b631d550ce6b0e4df5c8690e70a996
BibSonomy <webmaster@bibsonomy.org>
2010-04-27 12:56:57 +0200

    updated license headers

--- bibsonomy-scrapingservice/src/main/java/org/bibsonomy/scrapingservice/writers/RDFWriter.java
+++ bibsonomy-scrapingservice/src/main/java/org/bibsonomy/scrapingservice/writers/RDFWriter.java
@@ -1,22 +1,22 @@
 /**
- *  
+ *
  *  BibSonomy-Scrapingservice - Web application to test the BibSonomy web page scrapers (see
  * 		bibsonomy-scraper)
- *   
- *  Copyright (C) 2006 - 2009 Knowledge & Data Engineering Group, 
+ *
+ *  Copyright (C) 2006 - 2010 Knowledge & Data Engineering Group,
  *                            University of Kassel, Germany
  *                            http://www.kde.cs.uni-kassel.de/
- *  
+ *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU General Public License
  *  as published by the Free Software Foundation; either version 2
  *  of the License, or (at your option) any later version.
- * 
+ *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- *  
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -48,7 +48,7 @@ public class RDFWriter {
 	 * Mapping of BibTeX entry types to SWRC entry types
 	 * FIXME: copied from Functions.java tag library.
 	 */
-	private static String[] bibtexEntryTypes = {"article","book","booklet","inbook","incollection","inproceedings","manual","masterthesis","misc","phdthesis","proceedings","techreport",     "unpublished"}; 
+	private static String[] bibtexEntryTypes = {"article","book","booklet","inbook","incollection","inproceedings","manual","mastersthesis","misc","phdthesis","proceedings","techreport",     "unpublished"}; 
 	private static String[] swrcEntryTypes   = {"Article","Book","Booklet","InBook","InCollection","InProceedings","Manual","MasterThesis","Misc","PhDThesis","Proceedings","TechnicalReport","Unpublished"}; 
 	private static Map<String,String> entryTypeMap = new HashMap<String, String>();
 	static {
