98ee2452813376a7f7b8450a3e71f97cd5901fb1
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2010-04-28 14:34:18 +0200

    + errormessage and comment

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/CvPageController.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/CvPageController.java
@@ -1,41 +1,40 @@
 package org.bibsonomy.webapp.controller;
 
+import static org.bibsonomy.util.ValidationUtils.present;
+
 import java.util.Collections;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.bibsonomy.common.enums.GroupingEntity;
 import org.bibsonomy.model.Resource;
 import org.bibsonomy.model.enums.Order;
-import org.bibsonomy.util.ValidationUtils;
 import org.bibsonomy.webapp.command.UserResourceViewCommand;
+import org.bibsonomy.webapp.exceptions.MalformedURLSchemeException;
 import org.bibsonomy.webapp.util.MinimalisticController;
 import org.bibsonomy.webapp.util.View;
 import org.bibsonomy.webapp.view.Views;
 
 /**
+ * Controller for the cv page:
+ * - /cv/user/USERNAME
+ * 
+ * 
  * @author Philipp Beau
  * @version $Id$
  */
 public class CvPageController extends ResourceListController implements MinimalisticController<UserResourceViewCommand> {
 	private static final String TAG_MYOWN = "myown";
-	private static final Log log = LogFactory.getLog(CvPageController.class);
-
-
+	
 	/**
 	 * implementation of {@link MinimalisticController} interface
 	 */
-	public View workOn(UserResourceViewCommand command) {
+	@Override
+	public View workOn(final UserResourceViewCommand command) {
 		command.setPageTitle("Curriculum vitae");
-
-
+		
 		final String requUser = command.getRequestedUser();
 
-		if (!ValidationUtils.present(requUser)) {
-			/*
-			 * FIXME: a valuable error message would be very helpful!
-			 */
-			return Views.ERROR;
+		if (!present(requUser)) {
+			throw new MalformedURLSchemeException("error.cvpage_without_username");
 		}
 
 		command.setUser(this.logic.getUserDetails(requUser));
@@ -45,21 +44,21 @@ public class CvPageController extends ResourceListController implements Minimali
 		this.setTags(command, Resource.class, groupingEntity, requUser, null, command.getRequestedTagsList(), null, 1000, null);
 
 		/*
-		 * retrieve and set the requested bibtex(s) / bookmark(s) with the "myown" tag
+		 * retrieve and set the requested publication(s) / bookmark(s) with the "myown" tag
 		 */
-		for (final Class<? extends Resource> resourceType : listsToInitialise) {
+		for (final Class<? extends Resource> resourceType : this.listsToInitialise) {
 			final int entriesPerPage = command.getListCommand(resourceType).getEntriesPerPage();		
 			this.setList(command, resourceType, groupingEntity, requUser, Collections.singletonList(TAG_MYOWN), null, Order.ADDED, null, null, entriesPerPage);
 		}
+		
 		return Views.CVPAGE;
 	}
 
 	/**
 	 * implementation of {@link MinimalisticController} interface
 	 */
+	@Override
 	public UserResourceViewCommand instantiateCommand() {
 		return new UserResourceViewCommand();
 	}	
 }
-
-
