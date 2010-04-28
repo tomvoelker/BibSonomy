4b54da364f34eed0ad652581865249f3002fbe84
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2010-04-28 17:04:32 +0200

    + javadoc

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/BibtexResourceViewCommand.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/BibtexResourceViewCommand.java
@@ -4,7 +4,7 @@ package org.bibsonomy.webapp.command;
  * @author mwa
  * @version $Id$
  */
-public class BibtexResourceViewCommand extends TagResourceViewCommand{
+public class BibtexResourceViewCommand extends TagResourceViewCommand {
 	
 	/** the intrahash of a publication **/
 	private String requBibtex = "";
@@ -22,24 +22,38 @@ public class BibtexResourceViewCommand extends TagResourceViewCommand{
 		return this.requBibtex;
 	}
 	
+	/**
+	 * @param requBibtex the requBibtex to set
+	 */
 	public void setRequBibtex(String requBibtex) {
 		this.requBibtex = requBibtex;
 	}
 
+	/**
+	 * @return the title
+	 */
 	public String getTitle() {
 		return this.title;
 	}
 
+	/**
+	 * @param title the title to set
+	 */
 	public void setTitle(String title) {
 		this.title = title;
 	}
 
+	/**
+	 * @return the concepts
+	 */
 	public ConceptsCommand getConcepts() {
 		return this.concepts;
 	}
 
+	/**
+	 * @param concepts the concepts to set
+	 */
 	public void setConcepts(ConceptsCommand concepts) {
 		this.concepts = concepts;
 	}
-
 }
