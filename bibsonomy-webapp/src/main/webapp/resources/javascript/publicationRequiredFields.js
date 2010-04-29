0e1a66073c9323f10168d43dfd66f4489e759e18
Michael Velimirov <m.velimirov@googlemail.com>
2010-04-29 13:00:56 +0200

    a bug has been fixed where the database would return titles that don't match also the look and feel of the suggestion box have been addressed.

--- bibsonomy-webapp/src/main/webapp/resources/javascript/publicationRequiredFields.js
+++ bibsonomy-webapp/src/main/webapp/resources/javascript/publicationRequiredFields.js
@@ -1,5 +1,7 @@
 var form_name = '#post\\.resource\\.title';
 var suggestionBox = $("#suggestionBox");
+var bg_color_over = "#006699";
+var text_color = '#006699';
 
 /**
  * queries the titles and further details of publications by a partial title
@@ -23,11 +25,12 @@ function getSuggestions(partialTitle) {
 	if(partialTitle.length > 1) {
 		var query = $.ajax({
 			type: "GET",
-			url: "/json/tag/sys:title:"+partialTitle+"*?items=10",
+			url: "http://www.bibsonomy.org/json/tag/sys:title:"+encodeURIComponent(partialTitle)+"*?items=10",
 			dataType: "jsonp",
 			success: function(json){
 			processResponse(json);
 		}});
+
 		return true;
 	}
 	suggestionBox.hide();
@@ -51,76 +54,89 @@ function processResponse(data) {
 		suggestionBox.hide();
 		return;
 	}
-	// style="background-color: #006699; color: #FFFFFF; padding:3px;"
+
 	var p = $('<div class="suggBox" style="background-color: #006699; color: #FFFFFF; padding:3px;">'
 			+title
 			+'</div>');
-	suggestionBox.html(p);
-	
-	$.each(data.items, function(i, item) {
-		var editors = "";
-		var author = "";
-		var year = "";
-		var formatted_text = formatLabel(item.label);
-		if(item.editor != 'undefined') {
-			editors = concatArray(item.editor, 20);
-		}
 
-		if(item.author != 'undefined') {
-			author = concatArray(item.author, 27);
-		}
-
-		if(item.year != 'undefined') {
-			year = '('+item.year+')';
-		}
-		var intraHash = item.intraHash;
-		var tags = concatArray(item.tags, null, '+');
-		var k = m;
-		var element = 
-			$('<div class="suggestion_entry" style="cursor:pointer; color:#006699;background-color:'
-					+(((m++)%2 == 0)?'#FFFFFF':'#EEEEEE') // change the background-color every step
-					//+'">'+((formatted_text.length > 50)?formatted_text.substr(0, 50)+" ... ":formatted_text)
-					+'">'+formatted_text
-					+'<br><span style="font-size:10px;">'
-					+author
-					+year
-					+'</span></div>');
-
-		element.attr('url', '/editPublication?hash='+intraHash+'&user='+item.user+'&copytag='+tags);
-		element.click(
-				// get title specific data
-				// and set the forms accordingly
-				function () {
-					window.location.href = '/editPublication?hash='+intraHash+'&user='+item.user+'&copytag='+tags;
-					suggestionBox.hide();
-				}
-		);
-		/*
-		element.mouseover(
-				function () {
-					$(form_name).val(formatted_text);
-				}
-		)
-		
-		element.mouseout(
-				function () {
-					$(form_name).val(partialTitle_copy);
-				}
-		)*/
-
-		suggestionBox.append(element);
-	})
 	var pos = $(form_name).offset();
 	var width = $(form_name).width();
 	var top = parseInt(pos.top+$(form_name).height());
+	suggestionBox.html(p);
+	var has_elements = false;
+	$.each(data.items, function(i, item) {
+		if(item.label.toUpperCase().indexOf($(form_name).val().toUpperCase()) >= 0) {
+				has_elements = true;
+				var editors = "";
+				var author = "";
+				var year = "";
+		
+				if(item.editor != 'undefined') {
+					editors = concatArray(item.editor, 20);
+				}
+		
+				if(item.author != 'undefined') {
+					author = concatArray(item.author, 27);
+				}
+		
+				if(item.year != 'undefined') {
+					year = '('+item.year+')';
+				}
+		
+				var intraHash = item.intraHash;
+				var tags = concatArray(item.tags, null, '+');
+				var k = m;
+				var bg_color = (((m++)%2 == 0)?'#FFFFFF':'#EEEEEE');
+				var formatted_text = formatLabel(item.label);
+				var element = 
+					$('<div class="suggestion_entry" style="cursor:pointer; color:'+text_color+';background-color:'
+							+bg_color // change the background-color every step
+							+'"><span>'
+						+formatted_text
+						+'</span><br><span style="font-size:10px;">'
+						+author
+						+year
+						+'</span></div>');
+		
+				element.attr('url', '/editPublication?hash='+intraHash+'&user='+item.user+'&copytag='+tags);
+				element.attr('title', item.label);
+				element.click(
+						// get title specific data
+						// and set the forms accordingly
+						function () {
+							window.location.href = '/editPublication?hash='+intraHash+'&user='+item.user+'&copytag='+tags;
+							suggestionBox.hide();
+						}
+				);
+				
+				element.mouseover(
+						function () {
+							element.css("background-color", bg_color_over);
+							element.css("color", "#FFFFFF");
+							(element.children('span:first')).html(element.attr('title'));
+						}
+				)
+				
+				element.mouseout(
+						function () {
+							element.css("background-color", bg_color);
+							element.css("color", text_color);
+							(element.children('span:first')).html(formatted_text);
+							suggestionBox.children(".suggestion_entry_selected").css({"background-color":bg_color_over, "color":"#FFFFFF"});
+						}
+				)
+		
+				suggestionBox.append(element);
+		}
+	})
 
-	if(suggestionBox.width() < (width+2)){
+	if(suggestionBox.width() < (width)){
 		suggestionBox.width(width+2);
 	}
 
 	suggestionBox.css(
 			{
-				"left":(pos.left+1)+"px",
+				"left":(pos.left)+"px",
 				"top":top+"px",
 				"background-color":"#FFFFFF",
 				"z-index":"999",
@@ -128,7 +144,10 @@ function processResponse(data) {
 				"border-left":"3px solid #006699"
 			}
 	);
-	suggestionBox.show();
+	if(!has_elements)
+		suggestionBox.hide();
+	else 
+		suggestionBox.show();
 }
 
 /**
@@ -166,29 +185,56 @@ function concatArray(data, max_len, delim) {
 function getKey(e) {
 	var parent = 
 		document.getElementById('suggestionBox');
-
 	if(e.keyCode == 38){
 		var el = null;
 		var selected_field = null;
-
 		if((selected_field = DOMTraverseFlatByClass(parent.childNodes[0], 'suggestion_entry_selected')) == null) {
-			getPreviousByClass(parent.lastChild, 'suggestion_entry').className = 'suggestion_entry_selected'; 
-
+			if((el = getPreviousByClass(parent.lastChild, 'suggestion_entry')) != null) {
+				el.className = 'suggestion_entry_selected';
+				if(el.style != 'undefined') {
+					el.setAttribute("bg", el.style.backgroundColor);
+				}
+				el.style.backgroundColor = bg_color_over;
+				el.style.color = "#FFFFFF";
+			}
 		} else {
 			selected_field.className = 'suggestion_entry';
+			selected_field.style.backgroundColor = selected_field.getAttribute("bg");
+			selected_field.removeAttribute("bg");
+			selected_field.style.color = text_color;
 			if((el = getPreviousByClass(selected_field.previousSibling, 'suggestion_entry')) != null) {
 				el.className = 'suggestion_entry_selected';
+				if(el.style != 'undefined') {
+					el.setAttribute("bg", el.style.backgroundColor);
+				}
+				el.style.backgroundColor = bg_color_over;
+				el.style.color = "#FFFFFF";
 			}
 		}
+		
 		return false;
 	} else if(e.keyCode == 40){
 		if((selected_field = DOMTraverseFlatByClass(parent.childNodes[0], 'suggestion_entry_selected')) == null) {
-			if((el = getNextByClass(parent.firstChild, 'suggestion_entry')) != null)
+			if((el = getNextByClass(parent.firstChild, 'suggestion_entry')) != null) {
 				el.className = 'suggestion_entry_selected';
+				if(el.style != 'undefined') {
+					el.setAttribute("bg", el.style.backgroundColor);
+				}
+				el.style.backgroundColor = bg_color_over;
+				el.style.color = "#FFFFFF";
+			}
 		} else {
 			selected_field.className = 'suggestion_entry';
+			selected_field.style.backgroundColor = selected_field.getAttribute("bg");
+			selected_field.removeAttribute("bg");
+			selected_field.style.color = text_color;
 			if((el = getNextByClass(selected_field.nextSibling, 'suggestion_entry')) != null) {
 				el.className = 'suggestion_entry_selected';
+				if(el.style.backgroundColor != 'undefined') {
+					el.setAttribute("bg", el.style.backgroundColor);
+				}
+				el.style.backgroundColor = bg_color_over;
+				el.style.color = "#FFFFFF";
 			}
 		}
 		return false;
@@ -201,6 +247,7 @@ function getKey(e) {
 			|| e.keyCode == 39) {
 		return false;
 	}
+	
 	return getSuggestions($(form_name).val());
 }
 
@@ -251,12 +298,36 @@ function getPreviousByClass(match_el, className) {
  * @return a formatted string
  */
 function formatLabel (label) {
-	var pos = label.toUpperCase().indexOf($(form_name).val().toUpperCase());
+	max_len = 50;
+	form_length = $(form_name).val().length;
+	pos = label.toUpperCase().indexOf($(form_name).val().toUpperCase());
+	prepend = "";
+	append = "";
+	if(pos > 0) {
+		prepend = label.substr(0, pos);
+	}
+	if(label.length > pos+form_length) {
+		append = label.substr(pos + $(form_name).val().length);
+	}
+	
+	max_len -= form_length;
+	
+	if(prepend != "") {
+		l = prepend.length-Math.min(max_len, prepend.length);
+		prepend = ((max_len < prepend.length)?"... ":"")+prepend.substr(l, prepend.length-l);
+		max_len -= Math.min(max_len, prepend.length);
+	} 
 
-	return label.substr(0, pos)
-	+'<b>'
-	+label.substr(pos, $(form_name).val().length)
-	+'</b>'
-	+label.substr(pos + $(form_name).val().length);
+	if(append != "" && max_len > 0) {
+		append = append.substr(0, max_len)+((max_len < append.length)?" ...":"");
+	}
+
+	var title =
+		prepend
+		+'<b>'
+		+label.substr(pos, $(form_name).val().length)
+		+'</b>'
+		+append;
+
+	return title;
 }
-
