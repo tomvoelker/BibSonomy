0d29cb4f86c91b5eec3a78cb012e627be92fbbae
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2010-05-03 15:12:01 +0200

    test for the resource factory

---
+++ bibsonomy-model/src/test/java/org/bibsonomy/model/factories/ResourceFactoryTest.java
@@ -0,0 +1,63 @@
+package org.bibsonomy.model.factories;
+
+import static org.junit.Assert.assertEquals;
+
+import org.bibsonomy.common.exceptions.UnsupportedResourceTypeException;
+import org.bibsonomy.model.BibTex;
+import org.bibsonomy.model.Bookmark;
+import org.bibsonomy.model.GoldStandardPublication;
+import org.bibsonomy.model.Resource;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author nosebrain
+ * @version $Id$
+ */
+public class ResourceFactoryTest {
+	
+	private static ResourceFactory factory;
+	
+	/**
+	 * inits the factory
+	 */
+	@BeforeClass
+	public static void initFactory() {
+		factory = new ResourceFactory();
+	}
+	
+	/**
+	 * creates a new bookmark
+	 */
+	@Test
+	public void createBookmark() {
+		final Resource createResource = factory.createResource(Bookmark.class);
+		assertEquals(Bookmark.class, createResource.getClass());
+	}
+	
+	/**
+	 * creates a new publication
+	 */
+	@Test
+	public void createPublication() {
+		final Resource createResource = factory.createResource(BibTex.class);
+		assertEquals(BibTex.class, createResource.getClass());
+	}
+	
+	/**
+	 * creates a new goldstandard publication
+	 */
+	@Test
+	public void createGoldStandardPublication() {
+		final Resource createResource = factory.createResource(GoldStandardPublication.class);
+		assertEquals(GoldStandardPublication.class, createResource.getClass());
+	}
+	
+	/**
+	 * create a new resource (should throw an exception)
+	 */
+	@Test(expected = UnsupportedResourceTypeException.class)
+	public void createResource() {
+		factory.createResource(Resource.class);
+	}
+}
