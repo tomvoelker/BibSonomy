072263e2ce1b5e7d11a915eaa06c8b51fb0803a1
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2010-05-10 15:39:58 +0200

    added goldstandard references controller to add / remove references

---
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/ajax/EditGoldstandardReferencesController.java
@@ -0,0 +1,52 @@
+package org.bibsonomy.webapp.controller.ajax;
+
+import static org.bibsonomy.util.ValidationUtils.present;
+
+import java.util.Set;
+
+import org.bibsonomy.common.enums.Role;
+import org.bibsonomy.common.exceptions.AccessDeniedException;
+import org.bibsonomy.webapp.command.ajax.EditGoldstandardReferencesCommand;
+import org.bibsonomy.webapp.util.MinimalisticController;
+import org.bibsonomy.webapp.util.RequestWrapperContext;
+import org.bibsonomy.webapp.util.View;
+import org.bibsonomy.webapp.view.Views;
+
+/**
+ * @author dzo
+ * @version $Id$
+ */
+public class EditGoldstandardReferencesController extends AjaxController implements MinimalisticController<EditGoldstandardReferencesCommand> {
+	
+	@Override
+	public EditGoldstandardReferencesCommand instantiateCommand() {
+		return new EditGoldstandardReferencesCommand();
+	}
+
+	@Override
+	public View workOn(final EditGoldstandardReferencesCommand command) {
+		final RequestWrapperContext context = command.getContext();
+		if (!context.isUserLoggedIn() || Role.ADMIN.equals(context.getLoginUser().getRole())) {
+			throw new AccessDeniedException("You are not allowed to edit references of a goldstandard");
+		}
+		
+		final String hash = command.getHash();
+		final Set<String> references = command.getReferences();
+		
+		if (!present(hash) || !present(references)) {
+			return Views.AJAX;
+		}
+		
+		final String action = command.getAction();
+		if ("add".equals(action)) {
+			this.logic.createReferences(hash, references);
+		} else if ("remove".equals(action)) {
+			this.logic.deleteReferences(hash, references);
+		} else {
+			command.setResponseString("error");
+		}
+		
+		return Views.AJAX;
+	}
+
+}
