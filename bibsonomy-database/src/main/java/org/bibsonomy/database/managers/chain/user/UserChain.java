a94ef0e3504011cefea97ae7c12bb689d7cda51f
Clemens Baier <clemens.baier@multi-hosting.de>
2010-05-10 17:41:17 +0200

    added chainelements for pending users

--- bibsonomy-database/src/main/java/org/bibsonomy/database/managers/chain/user/UserChain.java
+++ bibsonomy-database/src/main/java/org/bibsonomy/database/managers/chain/user/UserChain.java
@@ -5,6 +5,9 @@ import org.bibsonomy.database.managers.chain.FirstChainElement;
 import org.bibsonomy.database.managers.chain.user.get.GetAllUsers;
 import org.bibsonomy.database.managers.chain.user.get.GetFollowersOfUser;
 import org.bibsonomy.database.managers.chain.user.get.GetFriendsOfUser;
+import org.bibsonomy.database.managers.chain.user.get.GetPendingUserByActivationCode;
+import org.bibsonomy.database.managers.chain.user.get.GetPendingUserByUsername;
+import org.bibsonomy.database.managers.chain.user.get.GetPendingUsers;
 import org.bibsonomy.database.managers.chain.user.get.GetRelatedUsersByTags;
 import org.bibsonomy.database.managers.chain.user.get.GetRelatedUsersByUser;
 import org.bibsonomy.database.managers.chain.user.get.GetUserFollowers;
@@ -29,21 +32,27 @@ public class UserChain implements FirstChainElement<User, UserParam> {
 	private final ChainElement<User, UserParam> getUsersByGroup;
 	private final ChainElement<User, UserParam> getFollowersOfUser;
 	private final ChainElement<User, UserParam> getUserFollowers;
+    private final ChainElement<User, UserParam> getPendingUsers;
+    private final ChainElement<User, UserParam> getPendingUserByUsername;
+    private final ChainElement<User, UserParam> getPendingUserByActivationCode;
 
 	/**
 	 * Constructs the chain
 	 */
 	public UserChain() {
 		// intialize chain elements
-		this.getAllUsers 				= new GetAllUsers();
-		this.getFriendsOfUser			= new GetFriendsOfUser();
-		this.getRelatedUsersByTags 		= new GetRelatedUsersByTags();
-		this.getRelatedUsersByUser 		= new GetRelatedUsersByUser();
-		this.getUserFriends				= new GetUserFriends();
-		this.getUsersByGroup			= new GetUsersByGroup();
-		this.getFollowersOfUser			= new GetFollowersOfUser();
-		this.getUserFollowers			= new GetUserFollowers();
-	
+		this.getAllUsers 				     = new GetAllUsers();
+		this.getFriendsOfUser			     = new GetFriendsOfUser();
+		this.getRelatedUsersByTags 		     = new GetRelatedUsersByTags();
+		this.getRelatedUsersByUser 		     = new GetRelatedUsersByUser();
+		this.getUserFriends				     = new GetUserFriends();
+		this.getUsersByGroup			     = new GetUsersByGroup();
+		this.getFollowersOfUser			     = new GetFollowersOfUser();
+		this.getUserFollowers			     = new GetUserFollowers();
+        this.getPendingUsers                 = new GetPendingUsers();
+        this.getPendingUserByUsername  = new GetPendingUserByUsername();
+        this.getPendingUserByActivationCode  = new GetPendingUserByActivationCode();
+        
 		// set order of chain elements
 		this.getUsersByGroup.setNext(this.getRelatedUsersByUser);
 		this.getRelatedUsersByUser.setNext(this.getRelatedUsersByTags);
@@ -52,6 +61,9 @@ public class UserChain implements FirstChainElement<User, UserParam> {
 		this.getUserFriends.setNext(this.getFollowersOfUser);
 		this.getFollowersOfUser.setNext(this.getUserFollowers);
 		this.getUserFollowers.setNext(this.getAllUsers);
+        this.getAllUsers.setNext(this.getPendingUsers);
+        this.getPendingUsers.setNext(this.getPendingUserByUsername);
+        this.getPendingUserByUsername.setNext(this.getPendingUserByActivationCode);
 	}
 
 	public ChainElement<User, UserParam> getFirstElement() {
