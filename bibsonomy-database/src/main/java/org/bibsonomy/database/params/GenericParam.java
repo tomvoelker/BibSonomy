e3a935fbd8351c7d79c9e418d86694c4b9b906c4
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2010-05-10 17:13:25 +0200

    + javadoc

--- bibsonomy-database/src/main/java/org/bibsonomy/database/params/GenericParam.java
+++ bibsonomy-database/src/main/java/org/bibsonomy/database/params/GenericParam.java
@@ -749,12 +749,18 @@ public abstract class GenericParam {
 		this.title = title;
 	}
 
-	public void setAuthor(String author) {
-		this.author = author;
+	/**
+	 * @return the author
+	 */
+	public String getAuthor() {
+		return this.author;
 	}
 
-	public String getAuthor() {
-		return author;
+	/**
+	 * @param author the author to set
+	 */
+	public void setAuthor(String author) {
+		this.author = author;
 	}
 
 	/**
