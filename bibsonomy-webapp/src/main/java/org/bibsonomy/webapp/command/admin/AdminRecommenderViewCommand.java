29f1a4bf7e1ee4292f334309f016bd6fd1bef3db
Bastian Schäfermeier <bsc@uni-kassel.de>
2010-05-11 16:21:47 +0200

    + javadoc

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/admin/AdminRecommenderViewCommand.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/admin/AdminRecommenderViewCommand.java
@@ -25,6 +25,10 @@ public class AdminRecommenderViewCommand extends BaseCommand {
 	private List<Long> activeRecs;
 	private List<Long> disabledRecs;
 	private final Map<Integer, String> tabdescriptor;
+	/**
+	 * @author bsc
+	 *
+	 */
 	public enum Tab{ STATUS, ACTIVATE, ADD };
 	private Tab tab;
 	private Map<Long, String> activeRecommenders;
@@ -34,6 +38,8 @@ public class AdminRecommenderViewCommand extends BaseCommand {
 	private String newrecurl;
 	
 	
+	/**
+	 */
 	public AdminRecommenderViewCommand(){
 		this.queriesPerLatency = (long)1000;
 		this.action = null;
@@ -45,101 +51,188 @@ public class AdminRecommenderViewCommand extends BaseCommand {
 		this.tab = Tab.STATUS;
 	}
 	
+	/**
+	 * @param activeRecommenders map {setting-id} -> {recommender-id}
+	 */
 	public void setActiveRecommenders(Map<Long, String> activeRecommenders){
 		this.activeRecommenders = activeRecommenders;
 	}
+	
+	/**
+	 * @param disabledRecommenders map {setting-id} -> {recommender-id}
+	 */
 	public void setDisabledRecommenders(Map<Long, String> disabledRecommenders){
 		this.disabledRecommenders = disabledRecommenders;
 	}
+	
+	/**
+	 * @return Entryset of currently activated recommenders 
+	 */
 	public Set<Entry<Long, String>> getActiveRecommenders(){
 		if (activeRecommenders == null) return null;
-		else return activeRecommenders.entrySet();
+		return activeRecommenders.entrySet();
 	}
+	
+	/**
+	 * @return Entryset of currently deactivated recommenders 
+	 */
 	public Set<Entry<Long, String>> getDisabledRecommenders(){
 		if (disabledRecommenders == null) return null;
 		return disabledRecommenders.entrySet();
 	}
 	
+	/**
+	 * @param t ordinal number of tab to be activated
+	 */
 	public void setTab(Integer t){
 		if(t>=0 && t<Tab.values().length)
 		  this.tab = Tab.values()[t];
 	}
+	/**
+	 * @param t Tab to be activated
+	 */
 	public void setTab(Tab t){
 		this.tab = t;
 	}
+	/**
+	 * @return ordinal number of active tab
+	 */
 	public Integer getTab(){
 		return this.tab.ordinal();
 	}
+	/**
+	 * @return name/description of currently activated tab
+	 */
 	public String getTabDescription(){
 		return tabdescriptor.get(this.tab.ordinal());
 	}
+	/**
+	 * @param t tab to get description for
+	 * @return Description of this tab
+	 */
 	public String getTabDescription(Tab t){
 		return tabdescriptor.get(t.ordinal());
 	}
+
+	/**
+	 * @return Entryset containing Tab-id and their descriptions
+	 */
 	public Set<Entry<Integer, String>> getTabs(){
 		return tabdescriptor.entrySet();
 	}
 	
+	/**
+	 * @param recOverview List of recommmenders contained in multiplexer
+	 */
 	public void setRecOverview(List<RecAdminOverview> recOverview){
 		this.recOverview = recOverview;
 	}
+	/**
+	 * @return List of recommmenders contained in multiplexer
+	 */
 	public List<RecAdminOverview> getRecOverview(){
 		return this.recOverview;
 	}
-	
+	/**
+	 * @param mp multiplexer
+	 */
 	public void setMultiplexingTagRecommender(MultiplexingTagRecommender mp){
 		this.mp = mp;
 	}
+	/**
+	 * @return multiplexer
+	 */
 	public MultiplexingTagRecommender getMultiplexingTagRecommender(){
 		return this.mp;
 	}
-	
+	/**
+	 * @param action the action which will be executed by the controller and set to null again
+	 */
 	public void setAction(String action){
 		this.action = action;
 	}
+	/**
+	 * @return the action which will be executed by the controller and set to null again
+	 */
 	public String getAction(){
 		return this.action;
 	}
-	
+	/**
+	 * @param queriesPerLatency number of values which will be fetched from the database to calculate average recommender-latencies
+	 */
 	public void setQueriesPerLatency(Long queriesPerLatency){
 		//only accept positive values
 		if(queriesPerLatency > 0)
 		   this.queriesPerLatency = queriesPerLatency;
 	}
+	/**
+	 * @return number of values which will be fetched from the database to calculate average recommender-latencies
+	 */
 	public Long getQueriesPerLatency(){
 		return this.queriesPerLatency;
 	}
 	
+	/**
+	 * @param adminResponse response-message to the last action executed (e.g. failure, success etc.) set by the controller
+	 */
 	public void setAdminResponse(String adminResponse){
 		this.adminResponse = adminResponse;
 	}
+	/**
+	 * @return response-message to the last action executed (e.g. failure, success etc.) set by the controller
+	 */
 	public String getAdminResponse(){
 		return this.adminResponse;
 	}
 	
+	/**
+	 * @param activeRecs updated list of active setting-ids.
+	 * This property can be set in the view by administrators and will be managed and set back to null by the controller. 
+	 */
 	public void setActiveRecs(List<Long> activeRecs){
 		this.activeRecs = activeRecs;
 	}
+	/**
+	 * @return updated active recommenders
+	 */
 	public List<Long> getActiveRecs(){
 		return this.activeRecs;
 	}
 	
+	/**
+	 * @param disabledRecs updated list of inactive setting-ids
+	 */
 	public void setDisabledRecs(List<Long> disabledRecs){
 		this.disabledRecs = disabledRecs;
 	}
+	/**
+	 * @return updated list of inactive setting-ids
+	 */
 	public List<Long> getDisabledRecs(){
 		return this.disabledRecs;
 	}
 	
+	/**
+	 * @param sid setting-id of recommender to be deleted
+	 */
 	public void setDeletesid(long sid){
 		this.deletesid = sid;
 	}
+	/**
+	 * @return setting-id of recommender to be deleted
+	 */
 	public long getDeletesid(){
 		return this.deletesid;
 	}
+	/**
+	 * @param recurl url of new recommender to be added
+	 */
 	public void setNewrecurl(String recurl){
 		this.newrecurl = recurl;
 	}
+	/**
+	 * @return url of new recommender to be added
+	 */
 	public String getNewrecurl(){
 		return this.newrecurl;
 	}
