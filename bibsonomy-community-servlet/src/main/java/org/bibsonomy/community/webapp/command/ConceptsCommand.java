fd7fa69f10d4969e495130be851c1909509dae4f
Folke Mitzlaff <mitzlaff@cs.uni-kassel.de>
2010-05-13 21:50:17 +0200

    added html output

---
+++ bibsonomy-community-servlet/src/main/java/org/bibsonomy/community/webapp/command/ConceptsCommand.java
@@ -0,0 +1,53 @@
+package org.bibsonomy.community.webapp.command;
+
+import java.util.List;
+
+import org.bibsonomy.model.Tag;
+
+/**
+ * Bean for tag relations
+ * 
+ * @author Dominik Benz
+ * @version $Id$
+ */
+public class ConceptsCommand extends ListCommand<Tag> {
+		
+
+	/**
+	 * constructor used by the UserRelationCommand
+	 * @param parentCommand
+	 */
+	public ConceptsCommand(final ContextCommand parentCommand) {
+		super(parentCommand);
+	}
+	
+	/**
+	 * default constructor
+	 */
+	public ConceptsCommand() {
+		super(new BaseCommand());
+	}
+
+	/**
+	 * a list of concepts
+	 */
+	private int numConcepts = 0;
+	
+	public int getNumConcepts() {
+		return this.numConcepts;
+	}
+
+	public void setNumConcepts(final int numConcepts) {
+		this.numConcepts = numConcepts;
+	}
+
+
+	public List<Tag> getConceptList() {
+		return this.getList();
+	}
+
+	public void setConceptList(final List<Tag> concepts) {
+		this.setList(concepts);
+	}
+	
+}
