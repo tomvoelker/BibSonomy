5cd79206c5241a41db5039238bf6fc400e3d4eb2
Folke Mitzlaff <mitzlaff@cs.uni-kassel.de>
2010-05-17 00:00:59 +0200

    extended clustering servlet

---
+++ bibsonomy-community-servlet/src/main/java/helpers/constants.java
@@ -0,0 +1,48 @@
+package helpers;
+
+public final class constants {
+	
+	/* ***********************************************************************
+	 * SQL constants
+	 * ***********************************************************************/
+
+	/* constant group ids */
+	public static final int SQL_CONST_GROUP_PUBLIC  = 0;
+	public static final int SQL_CONST_GROUP_PRIVATE = 1;
+	public static final int SQL_CONST_GROUP_FRIENDS = 2;
+	
+	/* privacy levels for groups */
+	public static final int SQL_CONST_PRIVLEVEL_PUBLIC  = 0; /* member list public */
+	public static final int SQL_CONST_PRIVLEVEL_HIDDEN  = 1; /* member list hidden */
+	public static final int SQL_CONST_PRIVLEVEL_MEMBERS = 2; /* members can list members */
+	
+	/* names for ids table */
+	public static final int SQL_IDS_CONTENT_ID = 0;
+	public static final int SQL_IDS_TAS_ID = 1;
+	
+	public static final int SQL_IDS_TAGREL_ID = 2;
+	public static final int SQL_IDS_QUESTION_ID = 3;
+	public static final int SQL_IDS_CYCLE_ID = 4;
+	public static final int SQL_IDS_EXTENDED_FIELDS = 5;
+	public static final int SQL_IDS_SCRAPER_METADATA = 7;
+	
+	/*spammer ids*/
+	public static final int SQL_CONST_SPAMMER_TRUE   = 1;
+	public static final int SQL_CONST_SPAMMER_FALSE  = 0;
+	public static final int SQL_CONST_TO_CLASSIFY_FALSE = 0;
+	
+	/* ***********************************************************************
+	 * HTTP constants
+	 * ***********************************************************************/
+	
+	public static final String HTTP_COOKIE_SPAMMER_KEY = "_lPost";
+	public static final char HTTP_COOKIE_SPAMMER_CONTAINS = '3';
+
+	/* ***********************************************************************
+	 * DBLP constants
+	 * ***********************************************************************/
+
+	public static final String dblpUser = "dblp";
+	
+	
+}
---
+++ bibsonomy-community-servlet/src/main/java/helpers/mail.java
@@ -0,0 +1,41 @@
+package helpers;
+
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.util.*;
+
+public class mail {
+	public static void sendMail( String recipients[ ], String subject, String message , String from) throws MessagingException {
+		boolean debug = false;
+		
+		//Set the host smtp address
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "localhost");
+		
+		// create some properties and get the default Session
+		Session session = Session.getDefaultInstance(props, null);
+		session.setDebug(debug);
+		
+		// create a message
+		Message msg = new MimeMessage(session);
+		
+		// set the from and to address
+		InternetAddress addressFrom = new InternetAddress(from);
+		msg.setFrom(addressFrom);
+		
+		InternetAddress[] addressTo = new InternetAddress[recipients.length]; 
+		for (int i = 0; i < recipients.length; i++)
+		{
+			addressTo[i] = new InternetAddress(recipients[i]);
+		}
+		msg.setRecipients(Message.RecipientType.TO, addressTo);
+		
+		// Optional : You can also set your custom headers in the Email if you Want
+		//msg.addHeader("X-Sent-By", "Bibsonomy-Bot");
+		
+		// Setting the Subject and Content Type
+		msg.setSubject(subject);
+		msg.setText(message);
+		Transport.send(msg);
+	}
+}
