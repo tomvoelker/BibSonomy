c1431c47be580b71191e31b0e1f1f2748197ebcb
BibSonomy <webmaster@bibsonomy.org>
2010-05-18 16:55:22 +0200

    adaptation for joe

--- bibsonomy-batch/src/build/batch_scripts/batch_contexttag_masterslave.pl
+++ bibsonomy-batch/src/build/batch_scripts/batch_contexttag_masterslave.pl
@@ -43,7 +43,7 @@ my $database = shift @ARGV;     # same db name on all hosts
 my $user     = "batch";         # same user name on all databases
 my $password = $ENV{'DB_PASS'}; # same password on all databases
 # fit to slave
-my $slave    = "DBI:mysql:database=$database;host=localhost:3306;mysql_socket=/var/mysql/run/mysqld.sock";
+my $slave    = "DBI:mysql:database=$database;host=localhost:3306;mysql_socket=/var/run/mysqld/mysqld.sock";
 #my $slave    = "DBI:mysql:database=$database;host=127.0.0.1:3306";
 # fit to master
 my $master   = "DBI:mysql:database=$database;host=gandalf:6033";
@@ -60,7 +60,7 @@ my @rel_tags_of_t1=();
 # SLAVE
 #######################################################
 # connect
-my $dbh = DBI->connect($slave, $user, $password, {RaiseError => 1, AutoCommit => 0, "mysql_enable_utf8" => 1, "transaction-isolation" => "READ-UNCOMMITTED"});
+my $dbh = DBI->connect($slave, $user, $password, {RaiseError => 1, AutoCommit => 0, "mysql_enable_utf8" => 1});#, "transaction-isolation" => "READ-UNCOMMITTED"});
 # prepare statements
 # get all public tag_names ordered by post
 my $stm_select_tagtag =$dbh->prepare("SELECT t1 collate utf8_bin , t2 collate utf8_bin , ctr_public  FROM tagtag force index (t1_ctr_public_idx) where ctr_public>0 order by t1 ");
