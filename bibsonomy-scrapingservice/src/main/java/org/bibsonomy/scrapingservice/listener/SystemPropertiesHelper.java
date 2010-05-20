bbf8268cda68dd06501f2351728615aa4fca48e8
Robert Jäschke <jaeschke@l3s.de>
2010-05-20 11:40:17 +0200

    removed logging and fixed parameter name striping

--- bibsonomy-scrapingservice/src/main/java/org/bibsonomy/scrapingservice/listener/SystemPropertiesHelper.java
+++ bibsonomy-scrapingservice/src/main/java/org/bibsonomy/scrapingservice/listener/SystemPropertiesHelper.java
@@ -6,29 +6,20 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 public class SystemPropertiesHelper implements ServletContextListener {
 
-	private static final Log log = LogFactory.getLog(SystemPropertiesHelper.class);
-	
 	private static final String CUSTOM_PREFIX = "systemProperty.";
 
 	@SuppressWarnings("unchecked")
 	public void contextInitialized(final ServletContextEvent event) {
-		
-		log.info("initializing system properties");
-		
 		final ServletContext context = event.getServletContext();
 		final Enumeration<String> params = context.getInitParameterNames();
 		
 		while (params.hasMoreElements()) {
 			final String name = params.nextElement();
 			final String value = context.getInitParameter(name);
-			log.info("handling context param " + name + " = " + value);
 			if (name.startsWith(CUSTOM_PREFIX)) {
-				System.setProperty(name, value);
+				System.setProperty(name.substring(CUSTOM_PREFIX.length()), value);
 			}
 		}
 	}
