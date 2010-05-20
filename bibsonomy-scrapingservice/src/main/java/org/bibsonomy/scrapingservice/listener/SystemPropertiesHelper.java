314516b8098ff29c9492b26d25a9772d5b3919b4
Robert Jäschke <jaeschke@l3s.de>
2010-05-20 11:32:29 +0200

    +logging

--- bibsonomy-scrapingservice/src/main/java/org/bibsonomy/scrapingservice/listener/SystemPropertiesHelper.java
+++ bibsonomy-scrapingservice/src/main/java/org/bibsonomy/scrapingservice/listener/SystemPropertiesHelper.java
@@ -6,20 +6,26 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 public class SystemPropertiesHelper implements ServletContextListener {
 
+	private static final Log log = LogFactory.getLog(SystemPropertiesHelper.class);
+	
 	private static final String CUSTOM_PREFIX = "systemProperty.";
 
 	@SuppressWarnings("unchecked")
 	public void contextInitialized(final ServletContextEvent event) {
 		final ServletContext context = event.getServletContext();
 		final Enumeration<String> params = context.getInitParameterNames();
-
+		
 		while (params.hasMoreElements()) {
-			final String param = params.nextElement();
-			final String value = context.getInitParameter(param);
-			if (param.startsWith(CUSTOM_PREFIX)) {
-				System.setProperty(param, value);
+			final String name = params.nextElement();
+			final String value = context.getInitParameter(name);
+			log.info("handling context param " + name + " = " + value);
+			if (name.startsWith(CUSTOM_PREFIX)) {
+				System.setProperty(name, value);
 			}
 		}
 	}
