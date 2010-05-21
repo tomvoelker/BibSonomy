0768c719c2504ad54b36e824dffd7ad08454a4ba
Folke Mitzlaff <mitzlaff@cs.uni-kassel.de>
2010-05-21 08:49:37 +0200

    initial commit for community features

---
+++ bibsonomy-community/src/main/java/org/bibsonomy/community/util/CommunityConfig.java
@@ -0,0 +1,13 @@
+package org.bibsonomy.community.util;
+
+public class CommunityConfig {
+	private String communityDBName;
+
+	public void setCommunityDBName(String communityDBName) {
+		this.communityDBName = communityDBName;
+	}
+
+	public String getCommunityDBName() {
+		return communityDBName;
+	}
+}
