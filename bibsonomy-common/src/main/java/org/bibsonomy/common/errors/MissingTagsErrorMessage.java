4c6a7008bbe3e54fe4c3a125e68d44ae7b52753d
Stephan Doerfel <doerfel@cs.uni-kassel.de>
2010-05-25 18:49:26 +0200

    *** empty log message ***

---
+++ bibsonomy-common/src/main/java/org/bibsonomy/common/errors/MissingTagsErrorMessage.java
@@ -0,0 +1,17 @@
+package org.bibsonomy.common.errors;
+
+/**
+ * @author sdo
+ * @version $Id$
+ */
+public class MissingTagsErrorMessage extends ErrorMessage {
+
+	/**
+	 * public constructor
+	 */
+	public MissingTagsErrorMessage() {
+		this.setDefaultMessage("The post has no tags. A post must have at least one tag.");
+		this.setErrorCode("database.exception.missingTag");
+	}
+
+}
