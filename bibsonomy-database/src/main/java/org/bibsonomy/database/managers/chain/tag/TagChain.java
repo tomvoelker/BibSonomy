7a2f55da2e1943eed9db4a804b477a91f62b383a
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2010-06-02 13:37:55 +0200

    removed unused tag chain elements

--- bibsonomy-database/src/main/java/org/bibsonomy/database/managers/chain/tag/TagChain.java
+++ bibsonomy-database/src/main/java/org/bibsonomy/database/managers/chain/tag/TagChain.java
@@ -5,10 +5,8 @@ import org.bibsonomy.database.managers.chain.FirstChainElement;
 import org.bibsonomy.database.managers.chain.tag.get.GetAllTags;
 import org.bibsonomy.database.managers.chain.tag.get.GetPopularTags;
 import org.bibsonomy.database.managers.chain.tag.get.GetRelatedTags;
-import org.bibsonomy.database.managers.chain.tag.get.GetRelatedTagsByAuthorAndTags;
 import org.bibsonomy.database.managers.chain.tag.get.GetRelatedTagsForGroup;
 import org.bibsonomy.database.managers.chain.tag.get.GetSimilarTags;
-import org.bibsonomy.database.managers.chain.tag.get.GetTagsByAuthor;
 import org.bibsonomy.database.managers.chain.tag.get.GetTagsByBibtexkey;
 import org.bibsonomy.database.managers.chain.tag.get.GetTagsByExpression;
 import org.bibsonomy.database.managers.chain.tag.get.GetTagsByFriendOfUser;
@@ -16,7 +14,6 @@ import org.bibsonomy.database.managers.chain.tag.get.GetTagsByGroup;
 import org.bibsonomy.database.managers.chain.tag.get.GetTagsByHash;
 import org.bibsonomy.database.managers.chain.tag.get.GetTagsByHashForUser;
 import org.bibsonomy.database.managers.chain.tag.get.GetTagsByResourceSearch;
-import org.bibsonomy.database.managers.chain.tag.get.GetTagsBySearchString;
 import org.bibsonomy.database.managers.chain.tag.get.GetTagsByUser;
 import org.bibsonomy.database.managers.chain.tag.get.GetTagsViewable;
 import org.bibsonomy.database.params.TagParam;
@@ -34,9 +31,7 @@ public class TagChain implements FirstChainElement<Tag, TagParam> {
 	private final ChainElement<Tag, TagParam> getTagsViewable;
 	private final ChainElement<Tag, TagParam> getTagsByRegularExpression;
 	private final ChainElement<Tag, TagParam> getAllTags;
-	private final ChainElement<Tag, TagParam> getTagsByResourceSearch;
-	private final ChainElement<Tag, TagParam> getTagsByAuthor;	
-	private final ChainElement<Tag, TagParam> getTagsBySearchString;	
+	private final ChainElement<Tag, TagParam> getTagsByResourceSearch;	
 	private final ChainElement<Tag, TagParam> getRelatedTagsForGroup;
 	private final ChainElement<Tag, TagParam> getRelatedTags;
 	private final ChainElement<Tag, TagParam> getSimilarTags;
@@ -45,9 +40,6 @@ public class TagChain implements FirstChainElement<Tag, TagParam> {
 	private final ChainElement<Tag, TagParam> getPopularTags;
 	private final ChainElement<Tag, TagParam> getTagsByFriendOfUser;
 	private final ChainElement<Tag, TagParam> getTagsByBibtexkey;
-	private final ChainElement<Tag, TagParam> getRelatedTagsByAuthorAndTag;
-	
-
 	
 	/**
 	 * Constructs the chain
@@ -59,8 +51,6 @@ public class TagChain implements FirstChainElement<Tag, TagParam> {
 		this.getAllTags = new GetAllTags();
 		this.getTagsByRegularExpression = new GetTagsByExpression();
 		this.getTagsByResourceSearch = new GetTagsByResourceSearch();
-		this.getTagsByAuthor=new GetTagsByAuthor();
-		this.getTagsBySearchString = new GetTagsBySearchString();
 		this.getRelatedTagsForGroup = new GetRelatedTagsForGroup();
 		this.getRelatedTags = new GetRelatedTags();
 		this.getTagsByHash = new GetTagsByHash();
@@ -69,19 +59,14 @@ public class TagChain implements FirstChainElement<Tag, TagParam> {
 		this.getPopularTags = new GetPopularTags();
 		this.getTagsByFriendOfUser = new GetTagsByFriendOfUser();
 		this.getTagsByBibtexkey = new GetTagsByBibtexkey();
-		this.getRelatedTagsByAuthorAndTag = new GetRelatedTagsByAuthorAndTags();
-
 		
 		this.getTagsByUser.setNext(this.getTagsByGroup);
 		this.getTagsByGroup.setNext(this.getTagsByFriendOfUser);
 		this.getTagsByFriendOfUser.setNext(this.getAllTags);
 		this.getAllTags.setNext(this.getSimilarTags);
 		this.getSimilarTags.setNext(this.getRelatedTags);
-		this.getRelatedTags.setNext(this.getTagsBySearchString);
-		this.getTagsBySearchString.setNext(this.getTagsByResourceSearch);
-		this.getTagsByResourceSearch.setNext(this.getTagsByAuthor);
-		this.getTagsByAuthor.setNext(this.getRelatedTagsByAuthorAndTag);
-		this.getRelatedTagsByAuthorAndTag.setNext(getTagsViewable);
+		this.getRelatedTags.setNext(this.getTagsByResourceSearch);
+		this.getTagsByResourceSearch.setNext(this.getTagsViewable);
 		this.getTagsViewable.setNext(this.getTagsByRegularExpression);
 		this.getTagsByRegularExpression.setNext(this.getRelatedTagsForGroup);
 		this.getRelatedTagsForGroup.setNext(this.getTagsByHash);
