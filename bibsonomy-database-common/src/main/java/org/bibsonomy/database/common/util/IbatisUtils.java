6084332e8e0ca94eb9733ec40f87dfd6b2f7b1e7
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2010-06-11 11:52:46 +0200

    added a method to easy load a ibatis config file

---
+++ bibsonomy-database-common/src/main/java/org/bibsonomy/database/common/util/IbatisUtils.java
@@ -0,0 +1,31 @@
+package org.bibsonomy.database.common.util;
+
+import java.io.Reader;
+
+import com.ibatis.common.resources.Resources;
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.ibatis.sqlmap.client.SqlMapClientBuilder;
+
+/**
+ * @author dzo
+ * @version $Id$
+ */
+public final class IbatisUtils {
+
+	/**
+	 * loads the specified iBatis config
+	 * 
+	 * @param filename
+	 * @return the ibatis sql map
+	 */
+	public static SqlMapClient loadSqlMap(final String filename) {
+		try {
+			// initialize database client for lucene queries
+			final Reader reader = Resources.getResourceAsReader(filename);
+			return SqlMapClientBuilder.buildSqlMapClient(reader);
+		} catch (final Exception e) {
+			throw new RuntimeException("Error loading " + filename + " sqlmap.", e);
+		}
+	}
+
+}
