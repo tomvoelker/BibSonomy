910cf5da9071a2e9a23b8533001be4bfae82d8b0
Clemens Baier <clemens.baier@multi-hosting.de>
2010-06-15 16:40:36 +0200

    *** empty log message ***

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/ResourceViewCommand.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/ResourceViewCommand.java
@@ -21,6 +21,7 @@ public class ResourceViewCommand extends BaseCommand {
 	private String tagstype; // for queries for specific kinds of tags
 	private String format = "html"; 
 	private String layout; // if format="layout", here the requested layout is stored
+	private boolean quicknav; // if format="layout", here u can choose to show quicknav
 	private boolean formatEmbedded; // 
 	private String sortPage = "none";
 	private String sortPageOrder = "asc";
@@ -299,5 +300,13 @@ public class ResourceViewCommand extends BaseCommand {
 	 */
 	public void setReferer(String referer) {
 		this.referer = referer;
+	}
+
+	public void setQuicknav(boolean quicknav) {
+		this.quicknav = quicknav;
+	}
+
+	public boolean getQuicknav() {
+		return quicknav;
 	}
 }
