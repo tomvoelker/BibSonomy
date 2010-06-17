2b1ce257204fde8c8eae34759dea70b33ac2f86e
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2010-06-17 14:39:54 +0200

    fixed resource search

--- bibsonomy-database/src/main/java/org/bibsonomy/database/managers/chain/bibtex/get/GetBibtexByResourceSearch.java
+++ bibsonomy-database/src/main/java/org/bibsonomy/database/managers/chain/bibtex/get/GetBibtexByResourceSearch.java
@@ -2,21 +2,58 @@ package org.bibsonomy.database.managers.chain.bibtex.get;
 
 import static org.bibsonomy.util.ValidationUtils.present;
 
-import org.bibsonomy.database.managers.chain.resource.get.GetResourcesByResourceSearch;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.bibsonomy.database.common.DBSession;
+import org.bibsonomy.database.common.params.beans.TagIndex;
+import org.bibsonomy.database.managers.chain.bibtex.BibTexChainElement;
 import org.bibsonomy.database.params.BibTexParam;
 import org.bibsonomy.model.BibTex;
+import org.bibsonomy.model.Post;
 
 /**
  * Returns a list of BibTex's for a given search.
  * 
- * @author claus
+ * @author fei
  * @version $Id$
  */
-public class GetBibtexByResourceSearch extends GetResourcesByResourceSearch<BibTex, BibTexParam> {
+public class GetBibtexByResourceSearch extends BibTexChainElement {
+
+	@Override
+	protected List<Post<BibTex>> handle(final BibTexParam param, DBSession session) {
+		// convert tag index to tag list
+		List<String> tagIndex = null;
+		if(present(param.getTagIndex())) {
+			tagIndex = extractTagNames(param.getTagIndex());
+		}
+		
+		// query the resource searcher
+		return this.db.getPostsByResourceSearch(param.getUserName(), param.getRequestedUserName(), param.getRequestedGroupName(),  param.getGroupNames(), param.getRawSearch(), param.getTitle(), param.getAuthor(), tagIndex, param.getYear(), param.getFirstYear(), param.getLastYear(),  param.getLimit(), param.getOffset());
+	}
 
 	@Override
 	protected boolean canHandle(final BibTexParam param) {
 		return (!present(param.getBibtexKey()) &&
-				(present(param.getSearch()) || present(param.getAuthor()) || present(param.getTitle()))); 
+				(present(param.getSearch()) || present(param.getAuthor()) || present(param.getTitle()))
+				); 
+	}
+	
+	/**
+	 * extracts list of tag names from given list of TagIndex instances
+	 * 
+	 * TODO: could we fill and use Generic.tags instead? 
+	 * 
+	 * @param tagIndex
+	 * @return
+	 */
+	private List<String> extractTagNames(final List<TagIndex> tagIndex) {
+		List<String> retVal = new LinkedList<String>();
+		
+		for( TagIndex tagIdx : tagIndex ) {
+			retVal.add(tagIdx.getTagName());
+		}
+		
+		return retVal;
 	}	
 }
--- bibsonomy-database/src/main/java/org/bibsonomy/database/managers/chain/bookmark/get/GetBookmarksByResourceSearch.java
+++ bibsonomy-database/src/main/java/org/bibsonomy/database/managers/chain/bookmark/get/GetBookmarksByResourceSearch.java
@@ -2,9 +2,15 @@ package org.bibsonomy.database.managers.chain.bookmark.get;
 
 import static org.bibsonomy.util.ValidationUtils.present;
 
-import org.bibsonomy.database.managers.chain.resource.get.GetResourcesByResourceSearch;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.bibsonomy.database.common.DBSession;
+import org.bibsonomy.database.common.params.beans.TagIndex;
+import org.bibsonomy.database.managers.chain.bookmark.BookmarkChainElement;
 import org.bibsonomy.database.params.BookmarkParam;
 import org.bibsonomy.model.Bookmark;
+import org.bibsonomy.model.Post;
 
 /**
  * Returns a list of BibTex's for a given search.
@@ -12,10 +18,44 @@ import org.bibsonomy.model.Bookmark;
  * @author claus
  * @version $Id$
  */
-public class GetBookmarksByResourceSearch extends GetResourcesByResourceSearch<Bookmark, BookmarkParam> {
+public class GetBookmarksByResourceSearch extends BookmarkChainElement {
+
+	@Override
+	protected List<Post<Bookmark>> handle(final BookmarkParam param, DBSession session) {
+		// convert tag index to tag list
+		List<String> tagIndex = null;
+		if(present(param.getTagIndex())) {
+			tagIndex = extractTagNames(param.getTagIndex());
+		}
+		
+		// query the resource searcher
+		return this.db.getPostsByResourceSearch(
+				param.getUserName(), param.getRequestedUserName(), param.getRequestedGroupName(), 
+				param.getGroupNames(), param.getRawSearch(), param.getTitle(), param.getAuthor(), tagIndex, 
+				null, null, null, 
+				param.getLimit(), param.getOffset());
+	}
 
 	@Override
 	protected boolean canHandle(final BookmarkParam param) {
-		return (present(param.getSearch()) || present(param.getTitle())); 
+		return ( present(param.getSearch()) || present(param.getTitle()) ); 
+	}
+	
+	/**
+	 * extracts list of tag names from given list of TagIndex instances
+	 * 
+	 * TODO: could we fill and use Generic.tags instead? 
+	 * 
+	 * @param tagIndex
+	 * @return
+	 */
+	private List<String> extractTagNames(final List<TagIndex> tagIndex) {
+		List<String> retVal = new LinkedList<String>();
+		
+		for( TagIndex tagIdx : tagIndex ) {
+			retVal.add(tagIdx.getTagName());
+		}
+		
+		return retVal;
 	}	
 }
