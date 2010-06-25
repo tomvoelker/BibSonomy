08f5c0e394da1894cc9a3f827fe6803856d87860
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2010-06-25 10:56:27 +0200

    cleanup

--- bibsonomy-rest-server/src/test/java/org/bibsonomy/rest/strategy/groups/GetUserListOfGroupStrategyTest.java
+++ bibsonomy-rest-server/src/test/java/org/bibsonomy/rest/strategy/groups/GetUserListOfGroupStrategyTest.java
@@ -5,12 +5,8 @@ import static org.junit.Assert.assertEquals;
 import java.io.ByteArrayOutputStream;
 import java.util.HashMap;
 
-import org.bibsonomy.common.exceptions.InternServerException;
-import org.bibsonomy.common.exceptions.ResourceMovedException;
-import org.bibsonomy.common.exceptions.ResourceNotFoundException;
 import org.bibsonomy.rest.RestProperties;
 import org.bibsonomy.rest.enums.HttpMethod;
-import org.bibsonomy.rest.exceptions.NoSuchResourceException;
 import org.bibsonomy.rest.strategy.AbstractContextTest;
 import org.bibsonomy.rest.strategy.Context;
 import org.junit.Test;
@@ -22,14 +18,10 @@ import org.junit.Test;
 public class GetUserListOfGroupStrategyTest extends AbstractContextTest {
 
 	/**
-	 * @throws ResourceMovedException 
-	 * @throws ResourceNotFoundException 
-	 * @throws NoSuchResourceException 
-	 * @throws InternServerException 
-	 * 
+	 * @throws Exception 
 	 */
 	@Test
-	public void testGetUserListOfGroupStrategy() throws InternServerException, NoSuchResourceException, ResourceNotFoundException, ResourceMovedException {
+	public void testGetUserListOfGroupStrategy() throws Exception {
 		final Context c = new Context(this.is, this.db, HttpMethod.GET, "/groups/public/users", new HashMap<String, String>(), null, null);
 		final ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		c.perform(baos);
