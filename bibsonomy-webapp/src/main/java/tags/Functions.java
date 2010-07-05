c7183bbea89df305b1af2f6f3b7b7465be5c92d8
Clemens Baier <clemens.baier@multi-hosting.de>
2010-07-05 14:29:38 +0200

    removed quicknav

--- bibsonomy-webapp/src/main/java/tags/Functions.java
+++ bibsonomy-webapp/src/main/java/tags/Functions.java
@@ -603,7 +603,7 @@ public class Functions  {
 			if (! ".html".equals(layout.getExtension())) {
 				return "The requested layout is not valid; only HTML layouts are allowed. Requested extension is: " + layout.getExtension();
 			}
-			return layoutRenderer.renderLayout(layout, posts, true, false).toString();
+			return layoutRenderer.renderLayout(layout, posts, true).toString();
 		} catch (final LayoutRenderingException ex) {
 			return ex.getMessage();			
 		} catch (final UnsupportedEncodingException ex) {
