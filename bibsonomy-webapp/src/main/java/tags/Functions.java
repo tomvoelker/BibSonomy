84be7ed5a4352436cb228b45b22c906d966cd216
Robert Jäschke <jaeschke@l3s.de>
2010-07-20 11:18:35 +0200

    proper JSON quoting

--- bibsonomy-webapp/src/main/java/tags/Functions.java
+++ bibsonomy-webapp/src/main/java/tags/Functions.java
@@ -377,16 +377,62 @@ public class Functions  {
 	 * @return The quoted String.
 	 */
 	public static String quoteJSON(final String value) {
-		if (value != null) {
-			return value
-			.replaceAll("\\\\", "\\\\\\\\") // back-slashes
-			.replaceAll("\n", "\\\\n")   // linebreaks 
-			.replaceAll("\t", "\\\\t")   // tabs
-			.replaceAll("\r", "")        // windows linebreaks
-			.replaceAll("\"", "\\\\\"")  // quotation marks
-			;  
-		}
-		return value;
+		if(value == null)
+			return null;
+        final StringBuffer sb = new StringBuffer();
+        escapeJSON(value, sb);
+        return sb.toString();
+	}
+
+	/**
+	 * Taken from http://code.google.com/p/json-simple/
+	 * 
+     * @param s - Must not be null.
+     * @param sb
+     */
+    private static void escapeJSON(final String s, final StringBuffer sb) {
+		for(int i=0;i<s.length();i++){
+			char ch=s.charAt(i);
+			switch(ch){
+			case '"':
+				sb.append("\\\"");
+				break;
+			case '\\':
+				sb.append("\\\\");
+				break;
+			case '\b':
+				sb.append("\\b");
+				break;
+			case '\f':
+				sb.append("\\f");
+				break;
+			case '\n':
+				sb.append("\\n");
+				break;
+			case '\r':
+				sb.append("\\r");
+				break;
+			case '\t':
+				sb.append("\\t");
+				break;
+			case '/':
+				sb.append("\\/");
+				break;
+			default:
+                //Reference: http://www.unicode.org/versions/Unicode5.1.0/
+				if((ch>='\u0000' && ch<='\u001F') || (ch>='\u007F' && ch<='\u009F') || (ch>='\u2000' && ch<='\u20FF')){
+					String ss=Integer.toHexString(ch);
+					sb.append("\\u");
+					for(int k=0;k<4-ss.length();k++){
+						sb.append('0');
+					}
+					sb.append(ss.toUpperCase());
+				}
+				else{
+					sb.append(ch);
+				}
+			}
+		}//for
 	}
 
 	/** First, replaces certain BibTex characters, 
