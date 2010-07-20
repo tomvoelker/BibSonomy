437db188609b70c0267b138580e4e0844fa7f83b
Robert Jäschke <jaeschke@l3s.de>
2010-07-20 11:55:24 +0200

    removed escaping of '/'

--- bibsonomy-webapp/src/main/java/tags/Functions.java
+++ bibsonomy-webapp/src/main/java/tags/Functions.java
@@ -415,9 +415,6 @@ public class Functions  {
 			case '\t':
 				sb.append("\\t");
 				break;
-			case '/':
-				sb.append("\\/");
-				break;
 			default:
                 //Reference: http://www.unicode.org/versions/Unicode5.1.0/
 				if((ch>='\u0000' && ch<='\u001F') || (ch>='\u007F' && ch<='\u009F') || (ch>='\u2000' && ch<='\u20FF')){
