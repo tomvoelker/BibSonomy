2546b3a24a034d18990b369a008751969500cefa
Robert Jäschke <jaeschke@l3s.de>
2010-07-29 11:31:34 +0200

    moved code to discover first name and last name of person into PersonNameUtils

--- bibsonomy-model/src/main/java/org/bibsonomy/model/PersonName.java
+++ bibsonomy-model/src/main/java/org/bibsonomy/model/PersonName.java
@@ -23,25 +23,37 @@
 
 package org.bibsonomy.model;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.StringTokenizer;
-import java.util.regex.Pattern;
+import org.bibsonomy.model.util.PersonNameUtils;
+
 
 /**
- * Name of a person
+ * The name of a person.
  * 
  * @author Jens Illig
  * @version $Id$
  */
 public class PersonName {
-	/** pattern that matches only on positive numbers */
-	private static final Pattern numbersPattern = Pattern.compile("[0-9]+");
 
 	private String name;
 	private String firstName;
 	private String lastName;
 
+	/**
+	 * Default constructor
+	 */
+	public PersonName() {
+		// nothing to do
+	}
+	
+	/**
+	 * Sets name and extracts first and last name.
+	 * 
+	 * @param name
+	 */
+	public PersonName(final String name) {
+		this.setName(name);
+	}
+	
 	/**
 	 * @return the firstname(s) of the person
 	 */
@@ -69,53 +81,22 @@ public class PersonName {
 	 */
 	public void setName(String name) {
 		this.name = name;
-		this.discoverFirstAndLastName();
+		PersonNameUtils.discoverFirstAndLastName(name, this);
 	}
 
 	/**
-	 * Tries to detect the firstname and lastname of each author or editor.
-	 * Firstnames must be abbreviated with a '.' to be identified as firstnames.
+	 * @param firstName
 	 */
-	private void discoverFirstAndLastName() {
-		if (this.name != null) {
-			/*
-			 * extract all parts of a name
-			 */
-			List<String> nameList = new LinkedList<String>();
-			StringTokenizer token = new StringTokenizer(this.name);
-			while (token.hasMoreTokens()) {
-				/*
-				 * ignore numbers (from DBLP author names) 
-				 */
-				final String part = token.nextToken();
-				if (!numbersPattern.matcher(part).matches()) {
-					nameList.add(part);
-				}
-			}
-
-			/*
-			 * detect firstname and lastname
-			 */
-			final StringBuilder firstNameBuilder = new StringBuilder();
-			int i = 0;
-			while (i < nameList.size() - 1) { // iterate up to the last but one part
-				final String part = nameList.get(i++);
-				firstNameBuilder.append(part + " ");
-				/*
-				 * stop, if this is the last abbreviated forename 
-				 */
-				if (part.contains(".") && !nameList.get(i).contains(".")) {
-					break;
-				}
-			}
-
-			final StringBuilder lastNameBuilder = new StringBuilder();
-			while (i < nameList.size()) {
-				lastNameBuilder.append(nameList.get(i++) + " ");
-			}
-
-			this.firstName = firstNameBuilder.toString().trim();
-			this.lastName = lastNameBuilder.toString().trim();
-		}	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
 	}
+
+	/**
+	 * @param lastName
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
 }
