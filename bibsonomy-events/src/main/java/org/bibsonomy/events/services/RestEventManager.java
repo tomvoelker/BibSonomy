4f01b5517b17d08d9ea28029ad55f57388a6b59b
Robert Jäschke <jaeschke@l3s.de>
2010-08-04 12:07:19 +0200

    +exception handlin

--- bibsonomy-events/src/main/java/org/bibsonomy/events/services/RestEventManager.java
+++ bibsonomy-events/src/main/java/org/bibsonomy/events/services/RestEventManager.java
@@ -4,6 +4,7 @@ import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.NameValuePair;
 import org.apache.commons.httpclient.methods.PostMethod;
+import org.bibsonomy.common.exceptions.InvalidModelException;
 import org.bibsonomy.events.model.Event;
 import org.bibsonomy.model.User;
 import org.springframework.beans.factory.annotation.Required;
@@ -11,7 +12,7 @@ import org.springframework.beans.factory.annotation.Required;
 public class RestEventManager implements EventManager {
 
 	private String serviceUrl;
-	
+
 	@Override
 	public Event getEvent(String name) {
 		throw new UnsupportedOperationException();
@@ -20,38 +21,46 @@ public class RestEventManager implements EventManager {
 	@Override
 	public void registerUser(final User user, final Event event, final String subEvent, final String address) {
 		final NameValuePair[] data = {
-			new NameValuePair("event", event.getName()),
-			new NameValuePair("name", user.getName()),
-			new NameValuePair("subEvent", subEvent),
-			new NameValuePair("address", address)
+				new NameValuePair("event", event.getName()),
+				new NameValuePair("name", user.getName()),
+				new NameValuePair("subEvent", subEvent),
+				new NameValuePair("address", address)
 		};
 		doRequest("register", data);
 	}
 
 	private void doRequest(final String method, final NameValuePair[] data) {
 		final PostMethod post = new PostMethod(serviceUrl + "/" + method);
+		int result = -1;
 		try {
 			final HttpClient client = new HttpClient();
-			
-	        post.setRequestBody(data);
-
-			final int result = client.executeMethod(post);
-			
-			if (HttpStatus.SC_OK == result) {
-				/*
-				 * everything is OK from the protocol side - check server answer
-				 */
-				final String response = post.getResponseBodyAsString();
-			}
-			
+			post.setRequestBody(data);
+			result = client.executeMethod(post);
 		} catch (final Exception e) {
-			// TODO
+			// re-throw
+			throw new RuntimeException(e.getMessage());
 		} finally {
 			post.releaseConnection();
 		}
-		
+		/*
+		 * check response
+		 */
+		switch (result) {
+		case HttpStatus.SC_CREATED: 
+			/*
+			 * user has been successfully registered
+			 */
+			return;
+		case HttpStatus.SC_FORBIDDEN:
+			/*
+			 * user is already registered -> throw appropriate exception
+			 */
+			throw new InvalidModelException("already registered");
+		default:
+			throw new RuntimeException("got unknown http status code " + result);
+		}
 	}
-	
+
 	public String getServiceUrl() {
 		return serviceUrl;
 	}
