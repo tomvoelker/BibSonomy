5eede9d332edd52f3eb912e5baf1e23364c34dba
mat <devnull@localhost>
2010-08-27 22:08:22 +0200

    - implemented REST/JSON connection - added ParticipantDetails for model - removed obsolete database stuff

--- bibsonomy-events/src/main/java/org/bibsonomy/events/services/RestEventManager.java
+++ bibsonomy-events/src/main/java/org/bibsonomy/events/services/RestEventManager.java
@@ -1,72 +1,113 @@
 package org.bibsonomy.events.services;
 
+import java.io.IOException;
+import java.io.StringWriter;
+
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpStatus;
-import org.apache.commons.httpclient.NameValuePair;
 import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.methods.StringRequestEntity;
 import org.bibsonomy.common.exceptions.InvalidModelException;
 import org.bibsonomy.events.model.Event;
+import org.bibsonomy.events.model.ParticipantDetails;
 import org.bibsonomy.model.User;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Required;
+
+/**
+ * 
+ * @author rja
+ * @author mat
+ * 
+ */
 
 public class RestEventManager implements EventManager {
 
-	private String serviceUrl;
+    private String serviceUrl;
 
-	@Override
-	public Event getEvent(String id) {
-		throw new UnsupportedOperationException();
-	}
+    public RestEventManager() {
+	super();
+    }
 
-	@Override
-	public void registerUser(final User user, final Event event, final String subEvent, final String address) {
-		final NameValuePair[] data = {
-				new NameValuePair("event", event.getId()),
-				new NameValuePair("name", user.getName()),
-				new NameValuePair("subEvent", subEvent),
-				new NameValuePair("address", address)
-		};
-		doRequest("register", data);
-	}
+    @Override
+    public Event getEvent(String id) {
+	throw new UnsupportedOperationException();
+    }
 
-	private void doRequest(final String method, final NameValuePair[] data) {
-		final PostMethod post = new PostMethod(serviceUrl + "/" + method);
-		int result = -1;
-		try {
-			final HttpClient client = new HttpClient();
-			post.setRequestBody(data);
-			result = client.executeMethod(post);
-		} catch (final Exception e) {
-			// re-throw
-			throw new RuntimeException(e.getMessage());
-		} finally {
-			post.releaseConnection();
-		}
-		/*
-		 * check response
-		 */
-		switch (result) {
-		case HttpStatus.SC_CREATED: 
-			/*
-			 * user has been successfully registered
-			 */
-			return;
-		case HttpStatus.SC_FORBIDDEN:
-			/*
-			 * user is already registered -> throw appropriate exception
-			 */
-			throw new InvalidModelException("already registered");
-		default:
-			throw new RuntimeException("got unknown http status code " + result);
-		}
-	}
+    @Override
+    public void registerUser(final User user, final Event event,
+	    final ParticipantDetails participantDetails) {
+	JSONObject json = new JSONObject();
+	json.put("name", user.getName());
+	json.put("badgeName", participantDetails.getBadgename());
+	json.put("badgeInstitutionName", participantDetails
+		.getBadgeInstitutionName());
+	json.put("subEvent", participantDetails.getSubEvent());
+	json.put("address", participantDetails.getAddress());
+	json.put("hasPoster", participantDetails.getHasPoster());
+	json.put("isPresenter", participantDetails.getIsPresenter());
+	json.put("icq", participantDetails.getIcq());
+	json.put("jabber", participantDetails.getJabber());
+	json.put("msn", participantDetails.getMsn());
+	json.put("skype", participantDetails.getSkype());
+	json.put("facebook", participantDetails.getFacebook());
+	json.put("flickr", participantDetails.getFlickr());
+	json.put("linkedIn", participantDetails.getLinkedIn());
+	json.put("researchGate", participantDetails.getResearchGate());
+	json.put("twitter", participantDetails.getTwitter());
+	json.put("xing", participantDetails.getXing());
 
-	public String getServiceUrl() {
-		return serviceUrl;
+	StringWriter out = new StringWriter();
+	try {
+	    json.writeJSONString(out);
+	} catch (IOException e) {
+	    e.printStackTrace();
 	}
-	@Required
-	public void setServiceUrl(String serviceUrl) {
-		this.serviceUrl = serviceUrl;
+	String jsonText = out.toString();
+	doRequest("register", jsonText);
+
+    }
+
+    private void doRequest(final String method, String body) {
+	final PostMethod post = new PostMethod(serviceUrl + "/" + method);
+	int result = -1;
+	try {
+	    final HttpClient client = new HttpClient();
+	    // FIXME - request entity
+	    post.setRequestEntity(new StringRequestEntity(body, null, null));
+	    result = client.executeMethod(post);
+	} catch (final Exception e) {
+	    // re-throw
+	    throw new RuntimeException(e.getMessage());
+	} finally {
+	    post.releaseConnection();
 	}
+	/*
+	 * check response
+	 */
+	switch (result) {
+	case HttpStatus.SC_CREATED:
+	    /*
+	     * user has been successfully registered
+	     */
+	    return;
+	case HttpStatus.SC_FORBIDDEN:
+	    /*
+	     * user is already registered -> throw appropriate exception
+	     */
+	    throw new InvalidModelException("already registered");
+	default:
+	    throw new RuntimeException("got unknown http status code " + result);
+	}
+    }
+
+    public String getServiceUrl() {
+	return serviceUrl;
+    }
+
+    @Required
+    public void setServiceUrl(String serviceUrl) {
+	this.serviceUrl = serviceUrl;
+    }
 
 }
