ce5f51e31a05a2825af71415e22b15dd9762b1c9
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2010-09-21 15:22:40 +0200

    extracted and moved getPreferredTypes method to rest common

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/util/HeaderUtils.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/util/HeaderUtils.java
@@ -3,12 +3,9 @@ package org.bibsonomy.webapp.util;
 import static org.bibsonomy.util.ValidationUtils.present;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map.Entry;
-import java.util.Scanner;
 import java.util.SortedMap;
-import java.util.TreeMap;
 import java.util.Vector;
 
 /**
@@ -35,8 +32,7 @@ public class HeaderUtils {
 		{"xml", 			"xml",		"layout/dblp"},
 		{"rdf",				null,		"burst"		},
 		{"bibtex",			"bookbib",	"bib"		}
-	};	
-	
+	};
 	
 	/**
 	 * Gets the preferred response format which is supported in 
@@ -65,39 +61,7 @@ public class HeaderUtils {
 		// if no acceptHeader is set, return default (= 0);
 		if (!present(acceptHeader)) return FORMAT_URLS[0][contentType];
 
-		// maps the q-value to output format (reverse order)
-		final SortedMap<Double,Vector<String>> preferredTypes = new TreeMap<Double,Vector<String>>(new Comparator<Double>() {
-			@Override
-			public int compare(Double o1, Double o2) {
-				if (o1.doubleValue() > o2.doubleValue())
-					return -1;
-				else if (o1.doubleValue() < o2.doubleValue())
-					return 1;
-				else
-					return o1.hashCode() - o2.hashCode();
-			}				
-		});		
-
-		// fill map with q-values and formats
-		final Scanner scanner = new Scanner(acceptHeader.toLowerCase());
-		scanner.useDelimiter(",");
-
-		while(scanner.hasNext()) {
-			final String[] types = scanner.next().split(";");
-			final String type = types[0];
-			double qValue = 1;
-
-			if (types.length != 1) 
-				qValue = Double.parseDouble(types[1].split("=")[1]);
-
-			if (!preferredTypes.containsKey(qValue)) {
-				final Vector<String> v = new Vector<String>();
-				v.add(type);				
-				preferredTypes.put(qValue, v);
-			} else {
-				preferredTypes.get(qValue).add(type);					
-			}
-		}
+		final SortedMap<Double, Vector<String>> preferredTypes = org.bibsonomy.rest.utils.HeaderUtils.getPreferredTypes(acceptHeader);
 
 		final List<String> formatOrder = new ArrayList<String>();			
 		for (final Entry<Double, Vector<String>> entry: preferredTypes.entrySet()) {								
