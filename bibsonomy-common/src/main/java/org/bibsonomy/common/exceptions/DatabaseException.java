2a5402a84a25d85ee3dfe42ee28abe4c11b230b9
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2010-09-22 14:38:58 +0200

    moved DatabaseException to exceptions package

--- bibsonomy-common/src/main/java/org/bibsonomy/common/exceptions/DatabaseException.java
+++ bibsonomy-common/src/main/java/org/bibsonomy/common/exceptions/DatabaseException.java
@@ -21,7 +21,7 @@
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-package org.bibsonomy.common.exceptions.database;
+package org.bibsonomy.common.exceptions;
 
 import static org.bibsonomy.util.ValidationUtils.present;
 
