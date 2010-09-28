46b50276d577c179541616056061234ee724b967
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2010-09-28 14:01:08 +0200

    cleanup test

--- bibsonomy-web-common/src/main/java/org/bibsonomy/util/XmlUtils.java
+++ bibsonomy-web-common/src/main/java/org/bibsonomy/util/XmlUtils.java
@@ -1,26 +1,3 @@
-/**
- *  
- *  BibSonomy-Common - Common things (e.g., exceptions, enums, utils, etc.)
- *   
- *  Copyright (C) 2006 - 2010 Knowledge & Data Engineering Group, 
- *                            University of Kassel, Germany
- *                            http://www.kde.cs.uni-kassel.de/
- *  
- *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public License
- *  as published by the Free Software Foundation; either version 2
- *  of the License, or (at your option) any later version.
- * 
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
- *  
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
-
 package org.bibsonomy.util;
 
 import it.unimi.dsi.fastutil.chars.CharOpenHashSet;
@@ -73,7 +50,7 @@ public class XmlUtils {
      * @param substitute 
      * @return a string with control characters removed
      */
-    public static String removeXmlControlCharacters(String string, final Boolean substitute) {
+    public static String removeXmlControlCharacters(String string, final boolean substitute) {
     	if (string == null) return string;
     	char[] ch = string.toCharArray();
     	StringBuilder sb = new StringBuilder(ch.length);    	
@@ -107,7 +84,7 @@ public class XmlUtils {
      * @param substitute 
      * @return a char array with control characters removed
      */
-    public static char[] removeXmlControlCharacters(char[] ch, final Boolean substitute) {
+    public static char[] removeXmlControlCharacters(char[] ch, final boolean substitute) {
     	StringBuilder sb = new StringBuilder(ch.length);
         for (int i = 0; i < ch.length; i++) {
             if (XmlUtils.illegalChars.contains(ch[i])) {
@@ -138,7 +115,7 @@ public class XmlUtils {
      * @param substitute 
      * @return a char with control characters removed
      */
-    public static char removeXmlControlCharacter(char c, final Boolean substitute) {
+    public static char removeXmlControlCharacter(char c, final boolean substitute) {
     	if (XmlUtils.illegalChars.contains(c)) {
     		return substitute ? XmlUtils.ILLEGAL_CHAR_SUBSTITUTE : ' ' ;
     	}
