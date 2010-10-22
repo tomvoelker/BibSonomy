1f7950329d7cabdf7fc96f0c0e0e8c2f826930b2
Robert Jäschke <jaeschke@l3s.de>
2010-10-22 14:09:10 +0200

    moved open access code to PUMA javascript, added messages

--- bibsonomy-webapp/src/main/webapp/resources/javascript/editPublication.js
+++ bibsonomy-webapp/src/main/webapp/resources/javascript/editPublication.js
@@ -107,48 +107,5 @@ function in_array(array, element) {
  	return false;
 }  
 
-/* open access check */
-/* TODO: add error handling, check apicontrol and outcome in response. */
-function checkOpenAccess () {
-	var container = $("#openAccess");	
-	var url = "/ajax/checkOpenAccess";
-
-	container.hide(); // TODO: add progress animation	
-	
-	if ($("#post\\.resource\\.entrytype").val() == "article")
-		url += "?jTitle=" + $("#post\\.resource\\.journal").val();
-	else
-		url += "?publisher=" + $("#post\\.resource\\.publisher").val();
-	
-	$.ajax({
-		url: url,
-		dataType: 'json',
-		success: function(data) {
-			/*
-			 * build list with publishers
-			 */
-			var ul = document.createElement("ul");
-			ul.className = "oa-publishers";
-			$.each(data.publishers, function(index, publisher) {
-				var li = document.createElement("li");
-				li.className = "oa-" + publisher.colour;
-				li.appendChild(document.createTextNode(publisher.name));
-				var ulCond = document.createElement("ul");
-				$.each(publisher.conditions, function(index, condition) {
-					var liCond = document.createElement("li");
-					liCond.appendChild(document.createTextNode(condition));
-					ulCond.appendChild(liCond);
-				});
-				li.appendChild(ulCond);
-				ul.appendChild(li);
-			});
-			container.append(ul);
-			container.fadeIn();
-		},
-		error: function(req, status, e) {
-			alert("check open access: " + status);
-		}
-	});
-}
 // FIXME: use jQuery, otherwise this overwrites other methods/is easily overwritten
 window.onload = changeView;
---
+++ bibsonomy-webapp/src/main/webapp/resources_puma/javascript/openaccess.js
@@ -0,0 +1,44 @@
+var oaBaseUrl = "/ajax/checkOpenAccess";
+
+/* open access check */
+/* TODO: add error handling, check apicontrol and outcome in response. */
+function checkOpenAccess () {
+	var container = $("#openAccess");	
+	container.hide(); // TODO: add progress animation	
+
+	var url;
+	if ($("#post\\.resource\\.entrytype").val() == "article")
+		url = oaBaseUrl + "?jTitle=" + $("#post\\.resource\\.journal").val();
+	else
+		url = oaBaseUrl + "?publisher=" + $("#post\\.resource\\.publisher").val();
+	
+	$.ajax({
+		url: url,
+		dataType: 'json',
+		success: function(data) {
+			/*
+			 * build list with publishers
+			 */
+			var ul = document.createElement("ul");
+			ul.className = "oa-publishers";
+			$.each(data.publishers, function(index, publisher) {
+				var li = document.createElement("li");
+				li.className = "oa-" + publisher.colour;
+				li.appendChild(document.createTextNode(publisher.name));
+				var ulCond = document.createElement("ul");
+				$.each(publisher.conditions, function(index, condition) {
+					var liCond = document.createElement("li");
+					liCond.appendChild(document.createTextNode(condition));
+					ulCond.appendChild(liCond);
+				});
+				li.appendChild(ulCond);
+				ul.appendChild(li);
+			});
+			container.append(ul);
+			container.fadeIn();
+		},
+		error: function(req, status, e) {
+			alert("check open access: " + status);
+		}
+	});
+}
