4535a4eb2dede81cb0ffa3457ae395cf3f127bae
Michael Velimirov <m.velimirov@googlemail.com>
2010-11-03 16:14:14 +0100

    if the sidebar and the sibling to the left occupy a larger space than the width of the parent allows, the sidebar would be positioned beneath the element to the left - this behaviour has been fixed now.

--- bibsonomy-webapp/src/main/webapp/resources/javascript/jquery.sidebarresizer.js
+++ bibsonomy-webapp/src/main/webapp/resources/javascript/jquery.sidebarresizer.js
@@ -11,13 +11,13 @@
 		}
 		sidebar = $(this), originalWidth = parseInt($(sidebar).width());
 		$(sidebar).parent().css('overflow','hidden');
-		$(sidebarGrip).bind("mousedown",{element: this}, startDrag);      
+		$(sidebarGrip).bind("mousedown",{element: this}, startDrag);
 	};
 	
 	function startDrag(e) {
 		sidebar = $(e.data.element);
 		sidebar.css('opacity', 0.7).parent().css('cursor', cursor);
-		
+		$(window).bind("resize",{element: sidebar}, resetSize);
 		lastMousePos = mousePosition(e).x;
 		$(document).mousemove(performDrag).mouseup(endDrag);
 		return false;
@@ -26,23 +26,26 @@
 	function performDrag(e) {
 		var thisMousePos = mousePosition(e).x;
 		var gap = thisMousePos - lastMousePos;
-		var maxOffset = parseInt($(sidebar).position().left+sidebar.width());
-		var minOffset = parseInt($(sidebar).position().left);
-		
-		if(((parseInt(sidebar.width())-gap > 20 && gap > 0) || 
-				(((parseInt(sidebar.width())+gap) < originalWidth) && gap < 0))) 
-					sidebar.width((parseInt(sidebar.width())-gap));
-		if(parseInt(sidebar.width())>parseInt(originalWidth))
-			sidebar.width(parseInt(originalWidth)+'px');
-		lastMousePos = thisMousePos;
+		lastMousePos = thisMousePos;		
+		if((parseInt(sidebar.width())-gap > 20 && gap > 0) ||
+					(((parseInt(sidebar.width())+Math.abs(gap)) < parseInt(originalWidth)) && gap < 0)) 
+						sidebar.width((parseInt(sidebar.width())-gap));
 		return false;
+	}
+	
+	function resetSize(e) {
+		$(e.data.element).width('');
+		$(window).unbind("resize", resetSize);
+		console.log(originalWidth);
+		originalWidth = parseInt($(e.data.element).width());
+		console.log(originalWidth);
 	}
 
 	function endDrag(e) {
 		$(document).unbind('mousemove', performDrag).unbind('mouseup', endDrag);
-		sidebar.css('opacity', 1).parent().css('cursor', 'default');
-		sidebar = null, 
 		lastMousePos = 0;
+		if(sidebar != null)
+			sidebar.css('opacity', 1).parent().css('cursor', 'default'); 
 		return false;
 	}
 
