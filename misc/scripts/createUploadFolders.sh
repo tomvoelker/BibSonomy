15e954107a57f1b39bf104d2c545f9bb11e02185
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2010-11-11 12:07:50 +0100

    fixed createUploadFolders script

--- misc/scripts/createUploadFolders.sh
+++ misc/scripts/createUploadFolders.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-PROJECT_NAME="bibsonomy"
+# check line arguments
+if [[ $# == 0 ]]; then
+  echo "usage: createUploadFolders.sh project_name"
+  exit
+fi
 # create temp dir
-mkdir "$PROJECT_NAME;_temp"
+mkdir "$1_temp"
 # create doc and pictures folder
-for folder_suffix in _doc _pictures
+for folder_suffix in _docs _pictures
 do
   # create root folder
-  folder="$PROJECT_NAME$folder_suffix"
+  folder="$1$folder_suffix"
   mkdir $folder
   # create subfolders 
   for i in 0 1 2 3 4 5 6 7 8 9 a b c d e f
@@ -18,4 +22,4 @@ do
   done
 done
 
-echo "done"
+echo "[INFO] done"
