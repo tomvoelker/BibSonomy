dfc953d09d077f7ec9752e6abc666ff23eb4f29e
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2010-11-24 14:30:54 +0100

    fixed ServiceUnavailableException

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/actions/UserLoginController.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/actions/UserLoginController.java
@@ -7,13 +7,16 @@ import org.bibsonomy.webapp.util.RequestLogic;
 import org.bibsonomy.webapp.util.ValidationAwareController;
 import org.bibsonomy.webapp.util.Validator;
 import org.bibsonomy.webapp.util.View;
+import org.bibsonomy.webapp.util.spring.security.ServiceUnavailableException;
 import org.bibsonomy.webapp.validation.UserLoginValidator;
 import org.bibsonomy.webapp.view.Views;
 import org.springframework.security.web.WebAttributes;
 
 /**
- * TODO
+ * this controller is only responsible for the view
+ * 	- /login
  * 
+ * the login is handled by Spring Security
  * @author dzo
  * @version $Id$
  */
@@ -28,12 +31,19 @@ public class UserLoginController implements ValidationAwareController<UserLoginC
 
 	@Override
 	public View workOn(UserLoginCommand command) {
+		/*
+		 * get last exception from spring security and clean the attribute
+		 */
 		final Exception lastException = (Exception) this.requestLogic.getSessionAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+		this.requestLogic.setSessionAttribute(WebAttributes.AUTHENTICATION_EXCEPTION, null);
 		
 		/*
 		 * get last exception from spring security
 		 */
 		if (present(lastException)) {
+			if (lastException instanceof ServiceUnavailableException) {
+				throw (ServiceUnavailableException) lastException;
+			}
 			final String messageKey = lastException.getClass().getSimpleName().toLowerCase();
 			command.setMessage(messageKey);
 		}
