8421f0251856bb78344a1761fbacbff6e3ef2a7a
Robert Jäschke <jaeschke@l3s.de>
2010-12-20 09:30:03 +0100

    - moved classification code into de.unikassel.puma package - removed dependency to bibsonomy-layout - removed code-duplication from JAXBRenderer - simplified XMLSchema

---
+++ bibsonomy-openaccess/src/main/java/de/unikassel/puma/openaccess/classification/PublicationClassification.java
@@ -0,0 +1,35 @@
+package de.unikassel.puma.openaccess.classification;
+
+/**
+ * @author philipp
+ * @version $Id$
+ */
+public class PublicationClassification {
+
+	final String id;
+	
+	final String description;
+
+	/**
+	 * @param s
+	 * @param description2
+	 */
+	public PublicationClassification(String s, String description2) {
+		this.id = s;
+		this.description = description2;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return this.id;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return this.description;
+	}
+}
