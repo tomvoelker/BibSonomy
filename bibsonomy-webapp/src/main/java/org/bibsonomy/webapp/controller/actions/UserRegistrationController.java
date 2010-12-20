ab060adb7b020429efae4cf8d8bc4a188ec73e69
Robert Jäschke <jaeschke@l3s.de>
2010-12-20 08:27:54 +0100

    re-added method such that we can check during registration if the user has cookies enabled

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/actions/UserRegistrationController.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/actions/UserRegistrationController.java
@@ -117,6 +117,15 @@ public class UserRegistrationController implements ErrorAware, ValidationAwareCo
 		if (context.isUserLoggedIn() && !Role.ADMIN.equals(loginUser.getRole())) {
 			throw new AccessDeniedException("error.method_not_allowed");
 		}
+		
+		
+		/* Check cookies
+		 * 
+		 * Check, if user has cookies enabled (there should be at least a "JSESSIONID" cookie)
+		 */
+		if (!cookieLogic.containsCookies()) {
+			errors.reject("error.cookies_required");
+		}
 
 		/*
 		 * User which wants to register (form data)
