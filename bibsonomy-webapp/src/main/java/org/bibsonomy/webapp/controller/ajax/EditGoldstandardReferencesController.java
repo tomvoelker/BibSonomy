a33bd283b3cdc9f8040760bb9d179248937f52f4
Robert Jäschke <jaeschke@l3s.de>
2011-01-27 10:10:04 +0100

    +ckey check

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/ajax/EditGoldstandardReferencesController.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/ajax/EditGoldstandardReferencesController.java
@@ -10,27 +10,38 @@ import org.bibsonomy.common.enums.Role;
 import org.bibsonomy.common.exceptions.AccessDeniedException;
 import org.bibsonomy.rest.enums.HttpMethod;
 import org.bibsonomy.webapp.command.ajax.EditGoldstandardReferencesCommand;
+import org.bibsonomy.webapp.util.ErrorAware;
 import org.bibsonomy.webapp.util.MinimalisticController;
 import org.bibsonomy.webapp.util.RequestWrapperContext;
 import org.bibsonomy.webapp.util.View;
 import org.bibsonomy.webapp.view.Views;
+import org.springframework.validation.Errors;
 
 /**
  * @author dzo
  * @version $Id$
  */
-public class EditGoldstandardReferencesController extends AjaxController implements MinimalisticController<EditGoldstandardReferencesCommand> {
+public class EditGoldstandardReferencesController extends AjaxController implements MinimalisticController<EditGoldstandardReferencesCommand>, ErrorAware {
 	
 	@Override
 	public EditGoldstandardReferencesCommand instantiateCommand() {
 		return new EditGoldstandardReferencesCommand();
 	}
 
+	private Errors errors;
+	
 	@Override
 	public View workOn(final EditGoldstandardReferencesCommand command) {
+
 		final RequestWrapperContext context = command.getContext();
 		if (!context.isUserLoggedIn() || !Role.ADMIN.equals(context.getLoginUser().getRole())) {
 			throw new AccessDeniedException("You are not allowed to edit references of a goldstandard");
+		}
+
+		//check if ckey is valid
+		if (!context.isValidCkey()) {
+			errors.reject("error.field.valid.ckey");
+			return Views.ERROR;
 		}
 		
 		final String hash = command.getHash();
@@ -55,6 +66,16 @@ public class EditGoldstandardReferencesController extends AjaxController impleme
 		}
 		
 		return Views.AJAX_TEXT;
+	}
+
+	@Override
+	public Errors getErrors() {
+		return errors;
+	}
+
+	@Override
+	public void setErrors(Errors errors) {
+		this.errors = errors;
 	}
 
 }
