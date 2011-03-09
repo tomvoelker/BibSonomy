f67ccf1fb942beec67c75f4ccc239dae7ac36545
Folke Mitzlaff <mitzlaff@cs.uni-kassel.de>
2011-03-09 20:50:33 +0100

    deactivated open social until build stabilizes

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/opensocial/ContainerPageController.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/opensocial/ContainerPageController.java
@@ -1,6 +1,8 @@
 package org.bibsonomy.webapp.controller.opensocial;
 
-import org.bibsonomy.webapp.command.BaseCommand;
+import org.bibsonomy.model.User;
+import org.bibsonomy.util.spring.security.AuthenticationUtils;
+import org.bibsonomy.webapp.command.opensocial.OpenSocialCommand;
 import org.bibsonomy.webapp.controller.SingleResourceListControllerWithTags;
 import org.bibsonomy.webapp.util.MinimalisticController;
 import org.bibsonomy.webapp.util.View;
@@ -10,16 +12,25 @@ import org.bibsonomy.webapp.view.Views;
  * Initial gadget container page for testing the open social interface
  * @author fei
  */
-public class ContainerPageController extends SingleResourceListControllerWithTags implements MinimalisticController<BaseCommand> {
+public class ContainerPageController extends SingleResourceListControllerWithTags implements MinimalisticController<OpenSocialCommand> {
 	
+	@SuppressWarnings("deprecation")
 	@Override
-	public View workOn(BaseCommand command) {
+	public View workOn(OpenSocialCommand command) {
+		User loginUser = AuthenticationUtils.getUser();
+		/*
+		try {
+			String token = SecurityTokenUtil.getSecurityToken(loginUser, "http://www.google.com/ig/modules/horoscope.xml");
+			command.setSecurityToken(token);
+		} catch (Exception ex) {
+		}
+		*/
 		return Views.GADGETCONTAINER;
 	}
 	
 	@Override
-	public BaseCommand instantiateCommand() {
-		return new BaseCommand();
+	public OpenSocialCommand instantiateCommand() {
+		return new OpenSocialCommand();
 	}
 
 }
