d88e064667d6f5e215f401902bd37fb7cc07dc9b
Dominik Benz <benz@cs.uni-kassel.de>
2011-03-21 11:49:57 +0100

    + forgotten param - handling of exceptions (is done in RestServlet)

--- bibsonomy-rest-server/src/main/java/org/bibsonomy/rest/strategy/users/PostRelatedusersForUserStrategy.java
+++ bibsonomy-rest-server/src/main/java/org/bibsonomy/rest/strategy/users/PostRelatedusersForUserStrategy.java
@@ -3,7 +3,6 @@ package org.bibsonomy.rest.strategy.users;
 import java.io.Writer;
 
 import org.bibsonomy.common.enums.UserRelation;
-import org.bibsonomy.common.exceptions.DatabaseException;
 import org.bibsonomy.model.User;
 import org.bibsonomy.rest.strategy.AbstractCreateStrategy;
 import org.bibsonomy.rest.strategy.Context;
@@ -22,7 +21,13 @@ public class PostRelatedusersForUserStrategy extends AbstractCreateStrategy {
 	private String tag;
 	private UserRelation relationship;
 	
-	public PostRelatedusersForUserStrategy(Context context, final String userName, final String relationship, String tag) {
+	/**
+	 * @param context
+	 * @param userName
+	 * @param relationship
+	 * @param tag
+	 */
+	public PostRelatedusersForUserStrategy(final Context context, final String userName, final String relationship, final String tag) {
 		super(context);
 		this.userName = userName;
 		this.tag = tag;
@@ -38,12 +43,10 @@ public class PostRelatedusersForUserStrategy extends AbstractCreateStrategy {
 	@Override
 	protected String create() {
 		User targetUser = parseUser();
-		try {
-			this.getLogic().createUserRelationship(this.userName, targetUser.getName(), relationship, null);
-		}
-		catch (DatabaseException de) {
-			//FIXME: handle exceptions
-		}
+		/*
+		 * perform DB query. Exceptions are handled within the RestServlet.
+		 */
+		this.getLogic().createUserRelationship(this.userName, targetUser.getName(), relationship, tag);
 		/*
 		 * we return the userName as "resource id" to be sent back in the
 		 * response in case of success 
