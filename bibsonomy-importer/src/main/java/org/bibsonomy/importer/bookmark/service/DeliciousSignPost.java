6f8916c5bd484b23da086ff45852e86dceea2306
Haile Misgna Haile <haile.misgna@mu.edu.et>
2011-03-22 13:18:56 +0100

    Improved exception handling

--- bibsonomy-importer/src/main/java/org/bibsonomy/importer/bookmark/service/DeliciousSignPost.java
+++ bibsonomy-importer/src/main/java/org/bibsonomy/importer/bookmark/service/DeliciousSignPost.java
@@ -1,6 +1,5 @@
 package org.bibsonomy.importer.bookmark.service;
 
-import java.io.IOException;
 import java.io.Serializable;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -9,10 +8,6 @@ import oauth.signpost.OAuthConsumer;
 import oauth.signpost.OAuthProvider;
 import oauth.signpost.basic.DefaultOAuthConsumer;
 import oauth.signpost.basic.DefaultOAuthProvider;
-import oauth.signpost.exception.OAuthCommunicationException;
-import oauth.signpost.exception.OAuthExpectationFailedException;
-import oauth.signpost.exception.OAuthMessageSignerException;
-import oauth.signpost.exception.OAuthNotAuthorizedException;
 
 public class DeliciousSignPost implements Serializable {
 	/**
@@ -38,66 +33,28 @@ public class DeliciousSignPost implements Serializable {
 			authorizationWebsiteUrl);
 	}
 
-	public String getRequestToken(String callbackUrl) {
+	public String getRequestToken(String callbackUrl) throws Exception {
 		String authUrl;
-		try {
-			authUrl = provider.retrieveRequestToken(consumer, callbackUrl);
-		} catch (OAuthMessageSignerException e) {
-			e.printStackTrace();
-			return null;
-		} catch (OAuthNotAuthorizedException e) {
-			e.printStackTrace();
-			return null;
-		} catch (OAuthExpectationFailedException e) {
-			e.printStackTrace();
-			return null;
-		} catch (OAuthCommunicationException e) {
-			e.printStackTrace();
-			return null;
-		}
-	
+		authUrl = provider.retrieveRequestToken(consumer, callbackUrl);
 		return authUrl;
 	}
 
-	public void getAccessToken(String pin) {
-		try {
-			provider.retrieveAccessToken(consumer, pin);
-		} catch (OAuthMessageSignerException e) {
-			e.printStackTrace();
-		} catch (OAuthNotAuthorizedException e) {
-			e.printStackTrace();
-		} catch (OAuthExpectationFailedException e) {
-			e.printStackTrace();
-		} catch (OAuthCommunicationException e) {
-			e.printStackTrace();
-		}
+	public void getAccessToken(String pin) throws Exception {
+		provider.retrieveAccessToken(consumer, pin);
 	}
 //	
 //	public void setTokenWithSecret(String arg0, String arg1) {
 //	    consumer.setTokenWithSecret(arg0, arg1);
 //	}
 	
-	public HttpURLConnection sign(URL url) {
+	public HttpURLConnection sign(URL url) throws Exception {
 		HttpURLConnection request;
-		try {
-			request = (HttpURLConnection) url.openConnection();
-			return sign(request);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return null;
+		request = (HttpURLConnection) url.openConnection();
+		return sign(request);
 	}
-	public HttpURLConnection sign(HttpURLConnection request) {
-		try {
-			consumer.sign(request);
-			return request;
-		} catch (OAuthMessageSignerException e) {
-			e.printStackTrace();
-		} catch (OAuthExpectationFailedException e) {
-			e.printStackTrace();
-		} catch (OAuthCommunicationException e) {
-			e.printStackTrace();
-		}
-		return null;
+	
+	public HttpURLConnection sign(HttpURLConnection request) throws Exception {
+		consumer.sign(request);
+		return request;
 	}
 }
