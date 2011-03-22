7b858dc67bc4ca415831850300a6f305b5a04316
Haile Misgna Haile <haile.misgna@mu.edu.et>
2011-03-22 15:56:03 +0100

    Fixed relations import

--- bibsonomy-database/src/main/java/org/bibsonomy/database/DBLogic.java
+++ bibsonomy-database/src/main/java/org/bibsonomy/database/DBLogic.java
@@ -1504,7 +1504,7 @@ public class DBLogic implements LogicInterface, SynchLogicInterface {
     public String createConcept(final Tag concept, final GroupingEntity grouping, final String groupingName) {
 	if (GroupingEntity.USER.equals(grouping)) {
 	    this.permissionDBManager.ensureIsAdminOrSelf(loginUser, groupingName);
-	    return this.storeConcept(concept, grouping, groupingName, false);
+	    return this.storeConcept(concept, grouping, groupingName, true);
 	}
 	throw new UnsupportedOperationException("Currently, tag relations can only be created for users.");
     }
