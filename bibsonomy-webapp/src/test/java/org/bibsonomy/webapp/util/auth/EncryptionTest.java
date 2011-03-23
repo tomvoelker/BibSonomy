d19cbf503395ff7b29991abd135a144183cc4fe9
Dominik Benz <benz@cs.uni-kassel.de>
2011-03-23 15:40:17 +0100

    fixed failing test

--- bibsonomy-webapp/src/test/java/org/bibsonomy/webapp/util/auth/EncryptionTest.java
+++ bibsonomy-webapp/src/test/java/org/bibsonomy/webapp/util/auth/EncryptionTest.java
@@ -1,33 +1,57 @@
 package org.bibsonomy.webapp.util.auth;
 
+import static org.junit.Assert.fail;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jasypt.exceptions.EncryptionOperationNotPossibleException;
 import org.jasypt.util.text.BasicTextEncryptor;
 import org.junit.Test;
 
 /**
+ * Simple test for encryption library.
+ * 
  * @author dbenz
  * @version $Id$
  */
 public class EncryptionTest {
-	
+
+	private static final Log LOGGER = LogFactory.getLog(EncryptionTest.class);
+
+	private static final String TEST_TEXT = "This is to be encrypted...";
+	private static final String TEST_PASSWORD = "hallo";
+	private static final String WRONG_PASSWORD = "halloooo";
+
+	/**
+	 * test encryption
+	 */
 	@Test
-	public void testEncryption() {
-		final String text = "This is to be encrypted...";
+	public void testBasicTextEncryptor() {
+		/*
+		 * encrypt
+		 */
+		LOGGER.info("Starting encryption test; trying to encrypt '" + TEST_TEXT + "'");
+		final String text = TEST_TEXT;
 		BasicTextEncryptor enc = new BasicTextEncryptor();
-		enc.setPassword("hallo");
-		System.out.println(text);
+		enc.setPassword(TEST_PASSWORD);
 		final String crypted = enc.encrypt(text);
-		System.out.println(crypted);
-		System.out.println(enc.decrypt(crypted));
+		LOGGER.info("Encrypted text: " + crypted);
+		/*
+		 * decrypt
+		 */
+		LOGGER.info("Decrypt again. Result: " + enc.decrypt(crypted));
+		/*
+		 * decrypt with wrong password
+		 */
+		LOGGER.info("Trying to decrypt with wrong password...");
 		enc = new BasicTextEncryptor();
-		enc.setPassword("hallo");
-		System.out.println(enc.decrypt(crypted));
-		enc = new BasicTextEncryptor();
-		enc.setPassword("halloo");
-		
-		final String test1 = "HwWtDV5lSRLELKJd/vkmfWU3e/bpCqIKgqJiugRa0CP7QGKl4NGBPw==";
-		final String test2 = "s7ZQxev49d412w4+j3jNjnbUQCt7yudUkc6Lv//Slbz5boVosq8+XQ==";
-		System.out.println(enc.decrypt(test1));
-		System.out.println(enc.decrypt(test2));
+		enc.setPassword(WRONG_PASSWORD);
+		try {
+			enc.decrypt(crypted);
+			fail("Encryption with wrong password should not be possible!");
+		} catch (EncryptionOperationNotPossibleException ex) {
+			// this should be the case
+		}
 	}
-	
+
 }
