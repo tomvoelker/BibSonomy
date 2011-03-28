ed3f98951571992384318c23e2eacba66127caa4
Robert Jäschke <jaeschke@l3s.de>
2011-03-28 08:37:21 +0200

    LDAP registration can now use the LDAP ID as suggested user name.

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/actions/UserLDAPRegistrationController.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/actions/UserLDAPRegistrationController.java
@@ -4,7 +4,6 @@ import org.bibsonomy.model.User;
 import org.bibsonomy.util.StringUtils;
 import org.bibsonomy.webapp.command.actions.UserIDRegistrationCommand;
 import org.bibsonomy.webapp.util.Validator;
-import org.bibsonomy.webapp.util.spring.security.handler.FailureHandler;
 import org.bibsonomy.webapp.validation.UserLDAPRegistrationValidator;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -17,6 +16,11 @@ import org.springframework.security.core.Authentication;
  * @version $Id$
  */
 public class UserLDAPRegistrationController extends AbstractUserIDRegistrationController {
+	
+	/**
+	 * Shall the LDAP ID be suggested as user name?
+	 */
+	private boolean ldapIdIsUsername = false;
 	
 	@Override
 	protected String getLoginNotice() {
@@ -36,20 +40,38 @@ public class UserLDAPRegistrationController extends AbstractUserIDRegistrationCo
 
 	@Override
 	protected Authentication getAuthentication(User user) {
-
-		// FIXME: PUMA/BadCredentialAtRegister
-		Object a = this.requestLogic.getSessionAttribute(FailureHandler.USER_AUTHENTICATION);
-		Authentication userSessionAuthentication = null;
-		if ((null != a) && (a instanceof Authentication)) {
-			userSessionAuthentication = (Authentication) a;
-		}
-
-		return new UsernamePasswordAuthenticationToken(user.getLdapId(), userSessionAuthentication.getCredentials());
-//		return new UsernamePasswordAuthenticationToken(user.getLdapId(), user.getPassword());
+		return new UsernamePasswordAuthenticationToken(user.getLdapId(), user.getPassword());
 	}
 
 	@Override
 	public Validator<UserIDRegistrationCommand> getValidator() {
 		return new UserLDAPRegistrationValidator();
+	}
+	
+	@Override
+	protected String generateUserName(User user) {
+		if (ldapIdIsUsername) {
+			return user.getLdapId();
+		}
+		return super.generateUserName(user);
+	}
+
+	/**
+	 * @return <code>true</code>, if the LDAP ID shall be suggested as user name 
+	 * during registration.
+	 */
+	public boolean isLdapIdIsUsername() {
+		return this.ldapIdIsUsername;
+	}
+
+	/**
+	 * If the LDAP ID shall be suggested as user name during registration, set 
+	 * this to <code>true</code>. Otherwise, a user name is generated using the 
+	 * real name. 
+	 *  
+	 * @param ldapIdIsUsername
+	 */
+	public void setLdapIdIsUsername(boolean ldapIdIsUsername) {
+		this.ldapIdIsUsername = ldapIdIsUsername;
 	}
 }
