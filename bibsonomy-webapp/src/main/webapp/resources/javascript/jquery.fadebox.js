8c9c678e72e3544713628dff954f65834901a315
Michael Velimirov <m.velimirov@googlemail.com>
2011-04-04 13:25:53 +0200

    fixed order, makes more sense now

--- bibsonomy-webapp/src/main/webapp/resources/javascript/jquery.fadebox.js
+++ bibsonomy-webapp/src/main/webapp/resources/javascript/jquery.fadebox.js
@@ -6,19 +6,19 @@
 				function () {
 					var hiddenBox = ((options.element && typeof options.element != 'undefined')?$(options.element).addClass(options.className):$('<div style="position:absolute" class="'+options.className+'"></div>'));
 					var hideTimeout = 0;
+					var content = ((typeof options.contentCallback === "string")?options.contentCallback:options.contentCallback(this));
 					var callback = function() 
 						{
 							hideTimeout = setTimeout (function(){hiddenBox.fadeOut('slow');}, options.timeout );
 						};
 					$("body").append(hiddenBox);
+					hiddenBox.html(content);
 					$(this).mouseover(
 							function() {
-								var content = ((typeof options.contentCallback === "string")?options.contentCallback:options.contentCallback(this));
-								hiddenBox.
-								html(content);
-								var left = ((options.leftOffset && options.leftOffset != 'undefined')?options.leftOffset($(this)):(($(this).offset().left + $(this).width()/2-hiddenBox.width()/2 > 0)?
-										(($(document).width() > ($(this).offset().left + $(this).width()/2)-hiddenBox.width()/2)?
-												$(this).offset().left + $(this).width()/2-hiddenBox.width()/2:$(document).width()-hiddenBox.width()):0));
+								var left = ((options.leftOffset && options.leftOffset != 'undefined')?options.leftOffset($(this)):
+											(($(this).offset().left + $(this).width()/2-hiddenBox.width()/2 > 0)?
+											(($(document).width() > ($(this).offset().left + $(this).width()/2)-hiddenBox.width()/2)?
+													$(this).offset().left + $(this).width()/2-hiddenBox.width()/2:$(document).width()-hiddenBox.width()):0));
 								var top = ((options.topOffset && typeof options.topOffset != 'number')?options.topOffset($(this)):$(this).offset().top-options.topOffset);
 								hiddenBox.
 								css( 
@@ -43,4 +43,3 @@
 			);
 	};
 })(jQuery);
-
