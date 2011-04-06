cc5ff0b48404aa893d7f12de69dd4277706db163
BibSonomy <webmaster@bibsonomy.org>
2011-04-06 17:27:52 +0200

    - added melkor (Würzburg) and joe (slave) as backup targets - added bibsonomy_pics directory

--- bibsonomy-batch/src/build/batch_scripts/backup_documents.sh
+++ bibsonomy-batch/src/build/batch_scripts/backup_documents.sh
@@ -1,13 +1,44 @@
 #!/bin/sh
 
-# location of directories
-HOME_DIR=/home/bibsonomy
-DOC_DIR=$HOME_DIR/bibsonomy_docs
-# rsync options
+BASE_DIR=/home/bibsonomy
+# these directories will be backuped
+DIRS="bibsonomy_docs bibsonomy_pics"
+
+#### daffy
 RHOST=daffy.cs.uni-kassel.de
 RUSER=bibbackup
 RDIR=backup
 ROPTIONS="-av --rsh=ssh"
 
-# backup document directory
-rsync $ROPTIONS $DOC_DIR $RUSER@$RHOST:$RDIR
+# backup directories
+for DIR in $DIRS; do
+  echo "rsync $ROPTIONS $BASE_DIR/$DIR $RUSER@$RHOST:$RDIR"
+        rsync $ROPTIONS $BASE_DIR/$DIR $RUSER@$RHOST:$RDIR
+done
+
+#### joe (Slave)
+RHOST=joe.cs.uni-kassel.de
+RUSER=bibsonomy
+RDIR=/home/bibsonomy
+ROPTIONS="-av --rsh=ssh"
+
+# backup directories
+for DIR in $DIRS; do
+  echo "rsync $ROPTIONS $BASE_DIR/$DIR $RUSER@$RHOST:$RDIR"
+        rsync $ROPTIONS $BASE_DIR/$DIR $RUSER@$RHOST:$RDIR
+done
+
+#### joe (Slave)
+
+#### melkor (Würzburg)
+RHOST=melkor.informatik.uni-wuerzburg.de
+RUSER=bibbackup
+RDIR=backup
+ROPTIONS="-av --rsh=ssh"
+
+# backup directories
+for DIR in $DIRS; do
+  echo "rsync $ROPTIONS $BASE_DIR/$DIR $RUSER@$RHOST:$RDIR"
+        rsync $ROPTIONS $BASE_DIR/$DIR $RUSER@$RHOST:$RDIR
+done
+
