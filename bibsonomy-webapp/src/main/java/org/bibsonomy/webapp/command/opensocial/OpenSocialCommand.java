03f7eb20e512135b4ee14101e13cbd5f5d4a0281
Folke Mitzlaff <mitzlaff@cs.uni-kassel.de>
2011-04-06 11:31:28 +0200

    implemented oauth endpoints

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/opensocial/OpenSocialCommand.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/opensocial/OpenSocialCommand.java
@@ -9,6 +9,9 @@ import org.bibsonomy.webapp.command.BaseCommand;
 public class OpenSocialCommand extends BaseCommand {
 	/** Security token used for authentication */
 	private String securityToken;
+	
+	/** URL of the gadget to render */
+	private String gadgetUrl;
 
 	public void setSecurityToken(String securityToken) {
 		this.securityToken = securityToken;
@@ -17,4 +20,14 @@ public class OpenSocialCommand extends BaseCommand {
 	public String getSecurityToken() {
 		return securityToken;
 	}
+
+	public void setGadgetUrl(String gadgetUrl) {
+		this.gadgetUrl = gadgetUrl;
+	}
+
+	public String getGadgetUrl() {
+		return gadgetUrl;
+	}
+
+	
 }
