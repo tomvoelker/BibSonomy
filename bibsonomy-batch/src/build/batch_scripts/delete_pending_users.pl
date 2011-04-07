8b1e512e8f0cf71d3a648c0d61a86cc15b4d5e02
BibSonomy <webmaster@bibsonomy.org>
2011-04-07 19:02:25 +0200

    added

--- bibsonomy-batch/src/build/batch_scripts/delete_pending_users.pl
+++ bibsonomy-batch/src/build/batch_scripts/delete_pending_users.pl
@@ -23,11 +23,37 @@ if (am_i_running($ENV{'TMP'}."/$PROGRAM_NAME.pid")) {
 my $user     = "bibsonomy";   
 my $password = $ENV{'DB_PASS'};
 my $database = "bibsonomy";     
-my $host     = "DBI:mysql:database=$database;host=gandalf:6033";
+my $host     = "DBI:mysql:database=$database;host=localhost:6033;mysql_socket=/home/bibsonomy/mysql-var/run/mysql.sock";
 
 # connect to database
 my $dbh = DBI->connect($host, $user, $password, {RaiseError => 1, "mysql_auto_reconnect" => 1, "mysql_enable_utf8" => 1});
 my $stm_delete_pending_users = $dbh->prepare ("DELETE FROM `pendingUser` WHERE (NOW() > DATE_ADD(reg_date, INTERVAL 24 HOUR))");
 $stm_delete_pending_users->execute();
-$dbh->commit();
 $dbh->disconnect();
+
+# INPUT: location of lockfile
+# OUTPUT: 1, if a lockfile exists and a program with the pid inside 
+#            the lockfile is running
+#         0, if no lockfile exists or the program with the pid inside 
+#            the lockfile is NOT running; resets the pid in the pid 
+#            to the current pid
+sub am_i_running {
+    my $LOCKFILE = shift;
+    my $PID;
+
+    if (open (FILE, "<$LOCKFILE")) {
+	while (<FILE>) {
+	    $PID = $_;
+	}
+	close (FILE);
+	chomp($PID);
+
+	if (kill(0,$PID)) {
+	    return 1;
+	}
+    }
+    open (FILE, ">$LOCKFILE");
+    print FILE $$;
+    close (FILE);
+    return 0;
+}
