e197c6f3ef33b68a738338d4b9bbdaa46be26e57
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2011-04-18 13:10:48 +0200

    for discussion

---
+++ bibsonomy-model/src/main/java/org/bibsonomy/model/logic/ReviewLogic.java
@@ -0,0 +1,44 @@
+package org.bibsonomy.model.logic;
+
+import org.bibsonomy.model.Review;
+
+/**
+ * @author dzo
+ * @version $Id$
+ */
+public interface ReviewLogic {
+	
+	/**
+	 * 
+	 * @param username
+	 * @param interHash
+	 * @param review
+	 */
+	public void createReview(String username, String interHash, Review review);
+	
+	/**
+	 * 
+	 * @param username
+	 * @param interHash
+	 * @param review
+	 */
+	public void updateReview(String username, String interHash, Review review);
+	
+	/**
+	 * 
+	 * @param username
+	 * @param interHash
+	 */
+	public void deleteReview(String username, String interHash);
+	
+	/**
+	 * 
+	 * @param username
+	 * @param reviewUsername
+	 * @param interHash
+	 * @param helpful
+	 */
+	public void markReview(String username, String reviewUsername, String interHash, boolean helpful);
+	
+	
+}
