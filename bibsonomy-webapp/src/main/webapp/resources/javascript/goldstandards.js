75656306b66832233afb04c2b22a08d69da0ef72
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2011-04-18 15:36:59 +0200

    added reviews view to goldstandard

--- bibsonomy-webapp/src/main/webapp/resources/javascript/goldstandards.js
+++ bibsonomy-webapp/src/main/webapp/resources/javascript/goldstandards.js
@@ -1,3 +1,99 @@
 $(function() {
 	$("#gold_exports").tabs();
 });
+
+function editReferences() {
+	if ($('.reference_menu').length > 0) {
+		// remove edit menu
+		removeReferenceMenu();
+		// TODO replace menu text
+	} else {
+		// TODO replace menu text
+		addReferenceMenu();
+	}
+}
+
+function addReferenceMenu() {
+	$("li.reference").each(function() {
+		var deleteLink = $('<span class="reference_menu"> <a href="#">delete reference</a></span>'); // TODO: i18n
+		deleteLink.hide();
+		$(this).append(deleteLink);
+		deleteLink.fadeIn(500);
+		
+		deleteLink.click(function() {
+			var listElement = $(this).parent('li')
+			var hash = $('#gold_title').attr("data-interhash");
+			var reference = listElement.attr("data-interhash");
+			var ckey = $('#gold_menu').attr("data-ckey");
+			var deleteUrl = '/ajax/goldstandards/references?ckey=' + ckey + '&hash=' + hash + "&references="+ reference;
+			if (confirm("delete ?")) {
+				alert(deleteUrl);
+				$.ajax({
+					type: 'DELETE',
+					url: deleteUrl,
+					success: function(transport) {
+								listElement.fadeOut(500, function() {
+									listElement.remove();
+								});
+							}
+				});
+			}
+			
+			return false;
+		});
+	});
+	
+	var addForm = $('<form class="reference_menu"></form>');
+	var input = $('<input type="text" />');
+	addForm.append($('<label>Add Reference: </label>')); // TODO: i18n
+	addForm.append(input);
+	input.autocomplete({
+		source: function(request, response) {
+			$.ajax({
+				url: "/json/search/" + request.term,
+				data: {items: 10, resourcetype: 'goldStandardPublication', duplicates: 'no'},
+				dataType: "jsonp",
+				success: function( data ) {
+					response($.map(data.items, function(item) {
+						return {
+							label: item.label + ' (' + item.year + ')',
+							value: item.interHash,
+							author: (concatArray(item.author, 40, ' ' + getString('and') + ' '))
+						}
+					}));
+				}
+			});
+		},
+		minLength: 3,
+		select: function(event, ui) {
+			var reference = ui.item.value;
+			var hash = getHash();
+			alert(reference);
+			return false;
+		},
+		focus: function(event, ui) {
+			return false;
+		}
+	})
+	.data('autocomplete')._renderItem = function(ul, item) {
+		if (item.value == getHash()) {
+			return ul;
+		}
+		return $('<li></li>').data('item.autocomplete', item).append($('<a></a>').html(item.label + '<br><span class="ui-autocomplete-subtext">' + item.author + '</span>')).appendTo(ul);
+	};
+	
+	
+	addForm.hide();
+	$("#gold_references").append(addForm);
+	addForm.fadeIn(500);
+}
+
+function getHash() {
+	return $('#gold_title').attr("data-interhash");
+}
+
+function removeReferenceMenu() {
+	$('.reference_menu').fadeOut(500, function() {
+		$(this).remove();
+	});
+}
---
+++ bibsonomy-webapp/src/main/webapp/resources/javascript/reviews.js
@@ -0,0 +1,147 @@
+var REVIEWS_URL = "/ajax/reviews";
+var MARK_REVIEWS_URL = REVIEWS_URL + "/mark";
+
+$(function() {
+	// init all selectable stars
+	$('.reviewrating').stars({
+		split: 2
+	});
+	
+	// create review form
+	$('#createReviewForm').submit(function() {
+		var reviewRating = getRating("#createReviewRating");
+		if (reviewRating === false) {
+			return false;
+		}
+		
+		$('#createSpinner').toggle();
+		
+		var reviewText = $('#createReviewText').val();
+		var starWidth = getStarsWidth(reviewRating);
+		
+		// call service
+		var reviewData = $(this).serialize();
+		$.ajax({
+			url:		REVIEWS_URL,
+			type:		"POST",
+			data:		reviewData,
+			success:	function(msg) {
+							// remove review create form
+			     			$('#postReview').fadeOut(1000, function() {
+			     				$(this).remove();
+			     			});
+			     			
+			     			// update update form
+			     			$('#updateReviewText').text(reviewText);
+			     			$('#updateReviewRating').stars("select", reviewRating);
+			     			
+			     			// display values
+			     			$('#newReview .reviewText').text(reviewText);
+			     			$('#newReview .reviewinfo .stars-on').css('width', starWidth);
+			     			$('#newReview').fadeIn(1000);
+			     			
+			     			// TODO: update overall counter and rating
+						}
+		});
+		return false;
+	});
+	
+	$('#updateReviewForm').submit(function() {
+		var reviewRating = getRating("#updateReviewRating");
+		if (reviewRating === false) {
+			return false;
+		}
+		
+		$('#updateSpinner').toggle();
+		
+		var reviewText = $('#updateReviewText').val();
+		var starWidth = getStarsWidth(reviewRating);
+		
+		// call service
+		var updRevUrl = REVIEWS_URL + "?" + $(this).serialize();
+		alert(updRevUrl);
+		$.ajax({
+			url:		updRevUrl,
+			type:		"PUT",
+			success:	function(msg) {
+							$('#editReviewForm').toggle('slow');
+							$('#updateSpinner').toggle();
+			     			
+							// update values
+			     			$('#ownReview .reviewText').text(reviewText);
+			     			$('#ownReview .reviewinfo .stars-on').css('width', starWidth);
+			     			
+			     			// TODO: update overall counter and rating
+						}
+		});
+		return false;
+	});
+	
+	// delete link for own review
+	$('a#reviewDelete').click(function() {
+		// TODO: show spinner
+		// TODO: confirm?
+		var hash = $('#reviews').data('interHash');
+		var username = $(this).parents('.reviewinfo:last').data('username');
+		var cKey = $('#reviews').data('ckey');
+		var review = $(this).parents('li:last');
+		// TODO: encode user name?
+		var revDelUrl = REVIEWS_URL + "?hash=" + hash + "&username=" + username + "&ckey=" + ckey;
+		$.ajax({
+			url:		revDelUrl,
+			type:		"DELETE",
+			success:	function(msg) {
+							review.fadeOut(1000, function() {
+			     				$(this).remove();
+							});
+						}
+		});
+		return false;
+	});
+	
+	// show edit form for own review
+	$('a#reviewEdit').click(function() {
+		$('#editReviewForm').toggle('slow');
+	});
+	
+	$('.helpful').submit(function() {
+		var helpfulData = $(this).serialize();
+		var container = $(this).parents('.helpfulContainer');
+		var helpful = $(this).children("input[name=helpful]").val() === "true";
+		alert(helpful);
+		// call services
+		$.ajax({
+			url:		MARK_REVIEWS_URL,
+			type:		"POST",
+			data:		helpfulData,
+			success:	function(msg) {
+							// TODO: update feedback counter
+							container.text(getString("post.resource.review.mark.thankyou"));
+						},
+			statusCode: {
+		    400:  		function() {
+		    				container.text(getString("post.resource.review.mark.thankyou"));
+		    			}
+		    },
+		});
+		return false;
+	});
+	
+});
+
+
+function getRating(starsWrapperId) {
+	var stars = $(starsWrapperId).data("stars");
+	var reviewRating = stars.options.value;
+	if (reviewRating == 0) {
+		if (!confirm(getString("post.resource.review.rating0"))) {
+			return false;
+		}
+	}
+	
+	return reviewRating;
+}
+
+function getStarsWidth(rating) {
+	return 16 * rating;
+}
