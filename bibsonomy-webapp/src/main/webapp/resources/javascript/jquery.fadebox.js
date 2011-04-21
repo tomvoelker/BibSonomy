01d1e86128289b2351ac39d2440eb47e5966adef
Michael Velimirov <m.velimirov@googlemail.com>
2011-04-21 22:03:33 +0200

    an addition has been made so that users browsing the site with a mobile device can experience the fadebox too. if you click on the representing icon the box fades in and is visible as long as 3 times the time as by hovering over the icon

--- bibsonomy-webapp/src/main/webapp/resources/javascript/jquery.fadebox.js
+++ bibsonomy-webapp/src/main/webapp/resources/javascript/jquery.fadebox.js
@@ -7,30 +7,37 @@
 					var hiddenBox = ((options.element && typeof options.element != 'undefined')?$(options.element).addClass(options.className):$('<div style="position:absolute" class="'+options.className+'"></div>'));
 					var hideTimeout = 0;
 					var content = ((typeof options.contentCallback === "string")?options.contentCallback:options.contentCallback(this));
+					var self = this;
 					var callback = function() 
 						{
 							hideTimeout = setTimeout (function(){hiddenBox.fadeOut('slow');}, options.timeout );
 						};
+					var drawCallback = function() {
+						var left = ((options.leftOffset && options.leftOffset != 'undefined')?options.leftOffset($(self)):
+							(($(self).offset().left + $(self).width()/2-hiddenBox.width()/2 > 0)?
+								(($(document).width() > ($(self).offset().left + $(self).width()/2)-hiddenBox.width()/2)?
+									$(self).offset().left + $(self).width()/2-hiddenBox.width()/2:$(document).width()-hiddenBox.width()):0));
+						var top = ((options.topOffset && typeof options.topOffset != 'number')?options.topOffset($(self)):$(self).offset().top-options.topOffset);
+						hiddenBox.
+						css( 
+								{ "left": left + "px", 
+								"top": top + "px" } 
+						);
+						clearTimeout(hideTimeout);
+						hiddenBox.show().fadeIn("slow");
+					};
 					$("body").append(hiddenBox);
 					hiddenBox.html(content);
-					$(this).mouseover(
-							function() {
-								var left = ((options.leftOffset && options.leftOffset != 'undefined')?options.leftOffset($(this)):
-											(($(this).offset().left + $(this).width()/2-hiddenBox.width()/2 > 0)?
-											(($(document).width() > ($(this).offset().left + $(this).width()/2)-hiddenBox.width()/2)?
-													$(this).offset().left + $(this).width()/2-hiddenBox.width()/2:$(document).width()-hiddenBox.width()):0));
-								var top = ((options.topOffset && typeof options.topOffset != 'number')?options.topOffset($(this)):$(this).offset().top-options.topOffset);
-								hiddenBox.
-								css( 
-										{ "left": left + "px", 
-										"top": top + "px" } 
-								);
-								clearTimeout(hideTimeout);
-								hiddenBox.show().fadeIn("slow");
+					$(self).mouseover(drawCallback)
+					.mouseout(callback)
+					.css("cursor","pointer")
+					.click(
+							function(){
+								options.timeout*=3;
+								drawCallback();
+								callback();
 							}
-					).mouseout(
-							callback
-					).css("cursor","pointer");
+					);
 					
 					hiddenBox.mouseover(
 							function() {
