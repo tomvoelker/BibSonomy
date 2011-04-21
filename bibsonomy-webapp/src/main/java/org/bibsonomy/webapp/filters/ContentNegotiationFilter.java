acdde81266e310ac3ec810d6341f2f310cf8121b
Robert Jäschke <jaeschke@l3s.de>
2011-04-21 17:08:09 +0200

    new filter for content negotiation using the Accept header

---
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/filters/ContentNegotiationFilter.java
@@ -0,0 +1,168 @@
+package org.bibsonomy.webapp.filters;
+
+import static org.bibsonomy.util.ValidationUtils.present;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.Vector;
+import java.util.regex.Pattern;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.bibsonomy.rest.exceptions.BadRequestOrResponseException;
+import org.bibsonomy.util.UrlUtils;
+import org.bibsonomy.webapp.util.HeaderUtils;
+import org.bibsonomy.webapp.view.Views;
+
+/**
+ * Implements content negotiation for integration of BibSonomy into the 
+ * Open Linked Data Cloud. See http://www4.wiwiss.fu-berlin.de/bizer/pub/LinkedDataTutorial/
+ * <br/>
+ * 
+ * Configure the URLs the filter should handle using the <code>excludePattern</code>
+ * init parameter.
+ * <br/>
+ * 
+ * This filter checks the "accept" header of the request to redirect to the
+ * corresponding format. Only the format with highest priority is used. If the 
+ * first part of the path is one of our export formats, no action is done.   
+ *
+ * TODO: should we add /uri/ to the excludePatterns list?
+ * 
+ * @author rja
+ * @version $Id$
+ */
+public class ContentNegotiationFilter implements Filter {
+
+	private static final Log log = LogFactory.getLog(ContentNegotiationFilter.class);
+	
+	/**
+	 * overwritten by init parameter "excludePatterns"
+	 */
+	private Pattern excludePattern = Pattern.compile("^/(api|resources)/.*");
+	
+	/**
+	 * Mapping of MIME types to the supported export formats. 
+	 * Used for (strict) content negotiation using the "Accept" header on regular pages.
+	 */
+	private static Map<String,String> FORMAT_MAPPING = new HashMap<String, String>();
+	static {
+		FORMAT_MAPPING.put("application/rdf+xml", Views.FORMAT_STRING_BURST);
+		FORMAT_MAPPING.put("application/json", Views.FORMAT_STRING_JSON);
+		FORMAT_MAPPING.put("text/csv", Views.FORMAT_STRING_CSV);
+		FORMAT_MAPPING.put("text/x-bibtex", Views.FORMAT_STRING_BIB);
+	}
+	
+	
+	@Override
+	public void destroy() {
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		final HttpServletRequest httpRequest = (HttpServletRequest)request;
+		/*
+		 * proceed to next filter in chain for certain patterns
+		 */
+		final String requestURI = httpRequest.getRequestURI();
+		if (excludePattern.matcher(requestURI).matches()) {
+			log.debug("skipping " + ContentNegotiationFilter.class.getName() + " for " + requestURI + " (cause: match in exclude pattern)");
+			chain.doFilter(request, response);
+			return;
+		}
+		final String firstPathElement = UrlUtils.getFirstPathElement(requestURI);
+		/*
+		 * We skip working on the request when a specific format is already 
+		 * selected using the URL path or the URL's "format" parameter.
+		 *
+		 * This is also crucial to avoid redirect loops!
+		 */
+		try {
+			Views.getViewByFormat(firstPathElement);
+			chain.doFilter(request, response);
+			log.debug("skipping " + ContentNegotiationFilter.class.getName() + " for " + requestURI  + " (cause: format selected by URL path)");
+			return;
+		} catch (final BadRequestOrResponseException e) {
+			// no known format selected - continue
+		}
+		try {
+			Views.getViewByFormat(httpRequest.getParameter("format"));
+			chain.doFilter(request, response);
+			log.debug("skipping " + ContentNegotiationFilter.class.getName() + " for " + requestURI  + " (cause: format selected by URL parameter)");
+			return;
+		} catch (final BadRequestOrResponseException e) {
+			// no known format selected - continue
+		}
+		
+		final String accept = httpRequest.getHeader("Accept");
+		log.debug("working on accept header '" + accept + "'");
+		log.debug("request URI: " + requestURI);
+		final String responseFormat = getResponseFormat(accept);
+		log.debug("format would be: " + responseFormat);
+		
+		if (present(responseFormat)) {
+			/*
+			 * send redirect
+			 */
+			final HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+			httpServletResponse.setStatus(HttpServletResponse.SC_SEE_OTHER);
+			httpServletResponse.sendRedirect("/" + responseFormat + HeaderUtils.getPathAndQuery(httpRequest));
+			/*
+			 * stop processing
+			 */
+			return;
+		}
+		chain.doFilter(httpRequest, response);
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+        final String initParameterExcludePatterns = filterConfig.getInitParameter("excludePattern");
+        if (present(initParameterExcludePatterns)) {
+        	this.excludePattern = Pattern.compile(initParameterExcludePatterns);
+        }
+	}
+	
+	/**
+	 * Supports content negotiation using the HTTP accept header.
+	 *  
+	 * Extracts the preferred response format from the accept header and returns
+	 * the corresponding URL path for the webapp. Only the format with the 
+	 * highest priority is regarded. Thus, this method is much stricter than
+	 * {@link HeaderUtils#getResponseFormat(String, int)}. 
+	 * <br/> 
+	 * If no matching URL path could be found, <code>null</code> is returned.
+	 *   
+	 * 
+	 * @param acceptHeader - the "Accept" header of the HTTP request.
+	 * @return The 
+	 */
+	private String getResponseFormat(final String acceptHeader) {
+		if (!present(acceptHeader)) return null;
+		/*
+		 * extract ordered list of preferred types
+		 */
+		final SortedMap<Double, Vector<String>> preferredTypes = org.bibsonomy.rest.utils.HeaderUtils.getPreferredTypes(acceptHeader);
+		/*
+		 * extract highest ranked formats
+		 */
+		final Vector<String> firstFormats = preferredTypes.get(preferredTypes.firstKey());
+		/*
+		 * return best match
+		 */
+		return FORMAT_MAPPING.get(firstFormats.firstElement());
+	}
+
+
+}
--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/filters/SetCharacterEncodingFilter.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/filters/SetCharacterEncodingFilter.java
@@ -13,7 +13,7 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-package filters;
+package org.bibsonomy.webapp.filters;
 
 import java.io.IOException;
 
