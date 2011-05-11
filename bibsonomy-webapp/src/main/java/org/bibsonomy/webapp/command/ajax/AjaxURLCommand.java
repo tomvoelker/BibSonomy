5c78aee4e2109716bce8b13ed640b6e32d798d61
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2011-05-11 15:21:03 +0200

    - dupl field

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/ajax/AjaxURLCommand.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/ajax/AjaxURLCommand.java
@@ -13,7 +13,6 @@ public class AjaxURLCommand extends AjaxCommand {
 	private String text;
 	private String ckey;
 	private String hash;
-	private String action;
 	
 	/**
 	 * @return the url
@@ -21,61 +20,53 @@ public class AjaxURLCommand extends AjaxCommand {
 	public String getUrl() {
 		return this.url;
 	}
+	
 	/**
 	 * @param url the url to set
 	 */
 	public void setUrl(String url) {
 		this.url = url;
 	}
+	
 	/**
 	 * @return the text
 	 */
 	public String getText() {
 		return this.text;
 	}
+	
 	/**
 	 * @param text the text to set
 	 */
 	public void setText(String text) {
 		this.text = text;
 	}
+	
 	/**
 	 * @return the ckey
 	 */
 	public String getCkey() {
 		return this.ckey;
 	}
+	
 	/**
 	 * @param ckey the ckey to set
 	 */
 	public void setCkey(String ckey) {
 		this.ckey = ckey;
 	}
+	
 	/**
 	 * @return the hash
 	 */
 	public String getHash() {
 		return this.hash;
 	}
+	
 	/**
 	 * @param hash the hash to set
 	 */
 	public void setHash(String hash) {
 		this.hash = hash;
 	}
-	/**
-	 * @return the action
-	 */
-	@Override
-	public String getAction() {
-		return this.action;
-	}
-	/**
-	 * @param action the action to set
-	 */
-	@Override
-	public void setAction(String action) {
-		this.action = action;
-	}
-
 }
