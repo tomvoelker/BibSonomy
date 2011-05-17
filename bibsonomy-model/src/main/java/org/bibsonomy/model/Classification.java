f75cccd28fef12427548bad6c431d74e2d5989ee
econ11 <devnull@localhost>
2011-05-17 17:32:21 +0200

    added classification url and help button

---
+++ bibsonomy-model/src/main/java/org/bibsonomy/model/Classification.java
@@ -0,0 +1,40 @@
+package org.bibsonomy.model;
+
+/**
+ * @author philipp
+ * @version $Id$
+ */
+public class Classification {
+
+	private String name;
+	
+	private String url = "";
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return this.name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the url
+	 */
+	public String getUrl() {
+		return this.url;
+	}
+
+	/**
+	 * @param url the url to set
+	 */
+	public void setUrl(String url) {
+		this.url = url;
+	}
+}
