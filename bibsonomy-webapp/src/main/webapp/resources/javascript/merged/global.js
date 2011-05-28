197f62ac316192f400ad9137433463826bc091ef
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2011-05-28 14:02:05 +0200

    added jquery to the merged global file

---
+++ bibsonomy-webapp/src/main/webapp/resources/javascript/merged/global.js
@@ -0,0 +1,22 @@
+/*
+ * Scripts that are available on /every/ page of BibSonomy (i.e., part of layout.tagx).
+ * Please add scripts only, if you are 100% sure that this makes sense (discuss
+ * with senior developer).
+ * 
+ * Note: this file is overwritten in the WAR file - you must add your script 
+ * also to the pom.xml into the correct "aggregation" section of the 
+ * yuicompressor-maven-plugin
+ * 
+ */
+function includeJavaScript(jsFile) {
+	// separated end script tag to prevent IE bug
+	document.write('<script type="text/javascript" src="' + jsFile + '"></scr' + 'ipt>');
+}
+
+includeJavaScript("/resources/jquery/jquery.js");
+includeJavaScript("/resources/javascript/functions.js");
+includeJavaScript("/resources/javascript/style.js");
+includeJavaScript("/resources/javascript/chrome.js");
+includeJavaScript("/resources/jquery/plugins/textarearesizer/jquery.textarearesizer.js");
+includeJavaScript("/resources/jquery/plugins/corner/jquery.corner.js");
+includeJavaScript("/resources/javascript/jquery.sidebarresizer.js");
