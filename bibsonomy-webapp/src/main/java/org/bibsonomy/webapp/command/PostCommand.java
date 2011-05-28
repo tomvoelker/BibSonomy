d836efa23d4300e0da1c49335160ec92abde1d55
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2011-05-28 16:17:26 +0200

    - dupl field

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/PostCommand.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/PostCommand.java
@@ -5,18 +5,7 @@ package org.bibsonomy.webapp.command;
  * @version $Id$
  */
 public class PostCommand extends ResourceViewCommand {
-	private TagCloudCommand tagcloud = new TagCloudCommand();
 	private ConceptsCommand concepts = new ConceptsCommand();
-
-	@Override
-	public TagCloudCommand getTagcloud() {
-		return this.tagcloud;
-	}
-
-	@Override
-	public void setTagcloud(final TagCloudCommand tagcloud) {
-		this.tagcloud = tagcloud;
-	}
 
 	/**
 	 * @return the concepts
