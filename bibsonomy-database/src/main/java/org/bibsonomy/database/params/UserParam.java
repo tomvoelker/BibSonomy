94a833ea4ab3ff43cfe04b67772ba9790b8b7f49
Folke Mitzlaff <mitzlaff@cs.uni-kassel.de>
2011-06-02 00:00:14 +0200

    further integration of the tagged user relationship

--- bibsonomy-database/src/main/java/org/bibsonomy/database/params/UserParam.java
+++ bibsonomy-database/src/main/java/org/bibsonomy/database/params/UserParam.java
@@ -1,5 +1,7 @@
 package org.bibsonomy.database.params;
 
+import static org.bibsonomy.util.ValidationUtils.present;
+
 import org.bibsonomy.common.enums.UserRelation;
 import org.bibsonomy.model.User;
 
@@ -17,11 +19,30 @@ public class UserParam extends GenericParam {
 	 */
 	private User user;
 	
+
 	/**
 	 * a user relation
 	 */
 	private UserRelation userRelation;
 
+	
+	/**
+	 * Returns the first friend of this user.<br/>
+	 * 
+	 * XXX: iBatis should support this: "friends[0].name", which should return
+	 * the name of the first friend - but this doesn't seem to work so we need
+	 * this extra method.
+	 * 
+	 * 2011/06/01, fei: moved this code fragment from the user model - this
+	 *                  is used only in the query 'isFriendOf'
+	 * 
+	 * @return friend
+	 */
+	public User getFriend() {
+		if (!present(this.user) || this.user.getFriends().size() < 1) return null;
+		return this.user.getFriends().get(0);
+	}
+	
 	/**
 	 * @return user
 	 */
