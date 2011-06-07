42c84191757c8a66dee31d06fea90db0808cfa6f
Robert Jäschke <jaeschke@l3s.de>
2011-06-07 14:09:48 +0200

    moved constant ID to database-common

--- bibsonomy-database/src/main/java/org/bibsonomy/database/managers/InboxDatabaseManager.java
+++ bibsonomy-database/src/main/java/org/bibsonomy/database/managers/InboxDatabaseManager.java
@@ -2,10 +2,10 @@ package org.bibsonomy.database.managers;
 
 import java.util.List;
 
-import org.bibsonomy.common.enums.ConstantID;
 import org.bibsonomy.common.exceptions.UnsupportedResourceTypeException;
 import org.bibsonomy.database.common.AbstractDatabaseManager;
 import org.bibsonomy.database.common.DBSession;
+import org.bibsonomy.database.common.enums.ConstantID;
 import org.bibsonomy.database.params.InboxParam;
 import org.bibsonomy.model.BibTex;
 import org.bibsonomy.model.Bookmark;
