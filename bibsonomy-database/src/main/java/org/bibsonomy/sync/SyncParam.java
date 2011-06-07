da43b7936d302a9f2688908afb5f966105f84fd7
Robert Jäschke <jaeschke@l3s.de>
2011-06-07 15:45:52 +0200

    moved constant ID to database-common

--- bibsonomy-database/src/main/java/org/bibsonomy/sync/SyncParam.java
+++ bibsonomy-database/src/main/java/org/bibsonomy/sync/SyncParam.java
@@ -1,7 +1,5 @@
 package org.bibsonomy.sync;
 
-import static org.bibsonomy.util.ValidationUtils.present;
-
 import java.net.URI;
 import java.util.Date;
 import java.util.Properties;
@@ -15,65 +13,61 @@ import org.bibsonomy.model.Resource;
  */
 public class SyncParam {
 
-    private final String userName;
-    private final URI service;
-    private final int contentType;
-    private final Date lastSyncDate;
-    private final String status;
-    private final Properties credentials;
+	private final String userName;
+	private final URI service;
+	private final int contentType;
+	private final Date lastSyncDate;
+	private final String status;
+	private final Properties credentials;
 
-    public SyncParam(String userName, URI service, Class<? extends Resource> resourceType, Date lastSyncDate, String status, Properties credentials) {
+	public SyncParam(String userName, final URI service, Class<? extends Resource> resourceType, Date lastSyncDate, String status, Properties credentials) {
 		this.userName = userName;
 		this.service = service;
+		this.contentType = ConstantID.getContentTypeByClass(resourceType).getId();
 		this.lastSyncDate = lastSyncDate;
 		this.status = status;
 		this.credentials = credentials;
-		
-		if (present(resourceType)) {
-			this.contentType = ConstantID.getContentTypeByClass(resourceType).getId();
-		} else 
-			this.contentType = 0;
-    }
+	}
 
-    /**
-     * @return the userName
-     */
-    public String getUserName() {
-	return userName;
-    }
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
 
-    /**
-	 * @return the service
+	/**
+	 * @return the serviceId
 	 */
 	public URI getService() {
 		return service;
 	}
 
 	/**
-     * @return the contentType
-     */
-    public int getContentType() {
-	return contentType;
-    }
+	 * @return the contentType
+	 */
+	public int getContentType() {
+		return contentType;
+	}
 
-    /**
-     * @return the lastSyncDate
-     */
-    public Date getLastSyncDate() {
-	return lastSyncDate;
-    }
+	/**
+	 * @return the lastSyncDate
+	 */
+	public Date getLastSyncDate() {
+		return lastSyncDate;
+	}
 
-    /**
-     * @return the status
-     */
-    public String getStatus() {
-	return status;
-    }
+	/**
+	 * @return the status
+	 */
+	public String getStatus() {
+		return status;
+	}
 
-    /**
-     * @return the credentials
-     */
-    public Properties getCredentials() {
-	return credentials;
-    }
+	/**
+	 * @return the credentials
+	 */
+	public Properties getCredentials() {
+		return credentials;
+	}
 }
