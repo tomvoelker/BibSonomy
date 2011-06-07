5236a0c4ff6bc8023ec6cd6e8c5a3a307a66d48e
Robert Jäschke <jaeschke@l3s.de>
2011-06-07 13:40:55 +0200

    moved constant ID to database-common

--- bibsonomy-database-common/src/main/java/org/bibsonomy/database/common/typehandler/ResourceTypeHandlerCallback.java
+++ bibsonomy-database-common/src/main/java/org/bibsonomy/database/common/typehandler/ResourceTypeHandlerCallback.java
@@ -2,25 +2,19 @@ package org.bibsonomy.database.common.typehandler;
 
 import java.sql.SQLException;
 
-import org.bibsonomy.common.enums.ConstantID;
-import org.bibsonomy.common.enums.Privlevel;
+import org.bibsonomy.database.common.enums.ConstantID;
 import org.bibsonomy.model.BibTex;
 import org.bibsonomy.model.Bookmark;
 
 import com.ibatis.sqlmap.client.extensions.ParameterSetter;
 
 /**
- * An iBATIS type handler callback for {@link Privlevel}es that are mapped to
- * Strings in the database. If the {@link Privlevel} cannot be constructed based
- * on the String, then the Privlevel will be set to <code>MEMBERS</code>.<br/>
+ * An iBATIS type handler callback for Resources that are mapped to
+ * an int in the database. If the Resource cannot be constructed based
+ * on the int, then an exception is thrown.
  * 
- * Almost copied from <a
- * href="http://opensource.atlassian.com/confluence/oss/display/IBATIS/Type+Handler+Callbacks">Atlassian -
- * Type Handler Callbacks</a>
  * 
- * @author Ken Weiner
- * @author Christian Schenk
- * @author Robert Jaeschke
+ * @author Robert Jäschke
  * @version $Id$
  */
 public class ResourceTypeHandlerCallback extends AbstractTypeHandlerCallback {
