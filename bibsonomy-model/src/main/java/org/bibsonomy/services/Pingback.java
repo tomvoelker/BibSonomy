1f892653e90f1b098a7fd218f850bcc7fc135ff3
Robert Jäschke <jaeschke@l3s.de>
2011-06-10 12:57:20 +0200

    +pingback interface

---
+++ bibsonomy-model/src/main/java/org/bibsonomy/services/Pingback.java
@@ -0,0 +1,24 @@
+package org.bibsonomy.services;
+
+import org.bibsonomy.model.Post;
+import org.bibsonomy.model.Resource;
+
+/**
+ * 
+ * 
+ * @author rja
+ * @version $Id$
+ */
+public interface Pingback {
+
+	/**
+	 * Sends a pingback for the provided post. Implementations must ensure that
+	 * <ul>
+	 * <li>pingbacks are send only for publicly visible posts,</li>
+	 * <li>the method does not block, i.e., the pingback is sent asynchronously</li>
+	 * </ul>
+	 * 
+	 * @param post
+	 */
+	public void sendPingback(final Post<? extends Resource> post);
+}
