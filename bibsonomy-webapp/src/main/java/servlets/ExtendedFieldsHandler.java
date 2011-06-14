8d3c8635329aa5e487cf2a3cd77c1cbcc80aaebf
Robert Jäschke <jaeschke@l3s.de>
2011-06-14 13:50:54 +0200

    -extra URL handling

--- bibsonomy-webapp/src/main/java/servlets/ExtendedFieldsHandler.java
+++ bibsonomy-webapp/src/main/java/servlets/ExtendedFieldsHandler.java
@@ -1,11 +1,8 @@
 package servlets;
 
-import helpers.database.DBBibtexURLManager;
 import helpers.database.DBPrivnoteManager;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.net.URLEncoder;
 
 import javax.servlet.ServletConfig;
@@ -16,11 +13,9 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.bibsonomy.common.enums.HashID;
 import org.bibsonomy.model.User;
-import org.bibsonomy.model.extra.BibTexExtra;
 import org.bibsonomy.util.spring.security.AuthenticationUtils;
 
 import filters.ActionValidationFilter;
-import filters.InitUserFilter;
 
 @Deprecated
 public class ExtendedFieldsHandler extends HttpServlet{
@@ -31,47 +26,6 @@ public class ExtendedFieldsHandler extends HttpServlet{
 	public void init(ServletConfig config) throws ServletException{	
 		super.init(config); 
 	}
-
-	@Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		final User user = AuthenticationUtils.getUser();
-		final String currUser = user.getName(); 
-		if (currUser == null) {
-			response.sendRedirect("/login?referer=/basket");
-			return;
-		}
-
-		/*
-		 * FIXME: added to allow handling of some stuff at /bibtex/HASH/USER
-		 */
-		final String action = request.getParameter("action");
-		final boolean validCkey = ActionValidationFilter.isValidCkey(request);
-		if (validCkey && ("addURL".equals(action) || "deleteURL".equals(action))) {
-			final String hash = request.getParameter("hash");
-			
-			/*
-			 * add / delete extra URL
-			 */
-			final String urlString = request.getParameter("url");
-			final URL url2;
-			try {
-				url2 = new URL(urlString);
-			} catch (final MalformedURLException ex) {
-				request.setAttribute("error", "The URL you entered is invalid (" + ex.getMessage() + ").");
-				getServletConfig().getServletContext().getRequestDispatcher("/errors/error.jsp").forward(request, response);
-				return;
-			}
-			if ("addURL".equals(action)) {
-				DBBibtexURLManager.createURL (new BibTexExtra(url2, request.getParameter("text"), null), hash, currUser, validCkey);
-			} else if ("deleteURL".equals(action)) {
-				DBBibtexURLManager.deleteURL (new BibTexExtra(url2, null, null), hash, currUser, validCkey);
-			}
-			
-			response.sendRedirect("/bibtex/" + HashID.INTRA_HASH.getId() + URLEncoder.encode(hash, "UTF-8") + "/" + URLEncoder.encode(currUser, "UTF-8"));
-			return;
-		}
-	}
-
 
 	@Override
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
