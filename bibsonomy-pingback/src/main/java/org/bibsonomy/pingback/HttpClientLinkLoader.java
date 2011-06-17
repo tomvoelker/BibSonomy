46d7877f996bcd469829c82fcff75e9d987a4c19
Robert Jäschke <jaeschke@l3s.de>
2011-06-17 10:45:52 +0200

    added probing for trackback header

--- bibsonomy-pingback/src/main/java/org/bibsonomy/pingback/HttpClientLinkLoader.java
+++ bibsonomy-pingback/src/main/java/org/bibsonomy/pingback/HttpClientLinkLoader.java
@@ -32,22 +32,47 @@ import com.malethan.pingback.LinkLoader;
 public class HttpClientLinkLoader implements LinkLoader {
 
 	private static final Log log = LogFactory.getLog(HttpClientLinkLoader.class);
-	
+
 	private static final String HTTP_HEADER_XPINGBACK = "X-Pingback";
-	private static final Pattern pingbackUrlPattern = Pattern.compile("<link rel=\"pingback\" href=\"([^\"]+)\" ?/?>", Pattern.CASE_INSENSITIVE);
-	private static final Pattern endOfHeadSectionPattern = Pattern.compile("<body[^>]+>|</head>", Pattern.CASE_INSENSITIVE);
+	private static final Pattern PINGBACK_URL_PATTERN = Pattern.compile("<link rel=\"pingback\" href=\"([^\"]+)\" ?/?>", Pattern.CASE_INSENSITIVE);
+	private static final Pattern TRACKBACK_RDF_PATTERN = Pattern.compile("(<rdf:RDF.*?</rdf:RDF>)", Pattern.DOTALL);
+	private static final Pattern TRACKBACK_LINK_URL_PATTERN = Pattern.compile("dc:identifier=\"([^\"]+)\"");
+	private static final Pattern TRACKBACK_PING_URL_PATTERN = Pattern.compile("trackback:ping=\"([^\"]+)\"");
+	private static final Pattern END_OF_HTML_HEAD_PATTERN = Pattern.compile("<body[^>]+>|</head>", Pattern.CASE_INSENSITIVE);
+
+	/**
+	 * The maximal number of characters to read from the body of a HTML page.
+	 * Approx. 500kb (The BibSonomy blog page is approx. 120kb big).
+	 */
+	private static final int MAX_HTML_BODY_CHARS = 500000;
+
 
 	final HttpClient httpClient;
 
 	public HttpClientLinkLoader() {
+		/*
+		 * HTTP client 4.1.1
+		 */
 		final SchemeRegistry schemeRegistry = new SchemeRegistry();
 		schemeRegistry.register(new Scheme("http", 80, PlainSocketFactory.getSocketFactory()));
 		final ThreadSafeClientConnManager conman = new ThreadSafeClientConnManager(schemeRegistry);
 		//		conman.setDefaultMaxPerRoute(10); // allow more than 10 connections to the same host
 		this.httpClient = new DefaultHttpClient(conman);
-
+		/*
+		 * HTTP client 4.0.1
+		 */
+		/*
+		final SchemeRegistry schemeRegistry = new SchemeRegistry();
+		schemeRegistry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
+		final HttpParams httpParams = new BasicHttpParams(); // FIXME: not thread-safe :-(
+		//        httpParams.setIntParameter(ConnManagerPNames.MAX_TOTAL_CONNECTIONS, 25);
+		//        httpParams.setParameter(ConnManagerPNames.MAX_CONNECTIONS_PER_ROUTE, 5);
+		final ThreadSafeClientConnManager conman = new ThreadSafeClientConnManager(httpParams, schemeRegistry);
+		//		conman.setDefaultMaxPerRoute(10); // allow more than 10 connections to the same host
+		final HttpParams httpParams2 = new BasicHttpParams(); // FIXME: probably not thread-safe :-(
+		this.httpClient = new DefaultHttpClient(conman, httpParams2);
+		*/
 	}
-
 
 
 	@Override
@@ -66,13 +91,11 @@ public class HttpClientLinkLoader implements LinkLoader {
 					return new Link(null, linkUrl, header.getValue(), true);
 				}
 				/*
-				 * TODO: probe for trackback header
-				 */
-				/*
 				 * probe content
 				 */
 				final HttpEntity entity = response.getEntity();
-				if (present(entity)) {
+				final Header contentType = entity.getContentType();
+				if (present(entity) && present(contentType) && contentType.getValue().contains("html")) {
 					final BufferedReader reader = new BufferedReader(new InputStreamReader(entity.getContent()));
 					try {
 						final String headContent = readHeadSectionOfPage(reader);
@@ -85,8 +108,13 @@ public class HttpClientLinkLoader implements LinkLoader {
 							return new Link(null, linkUrl, pingbackUrl, true);
 						}
 						/*
-						 * TODO: probe trackback content
+						 * probe trackback content
 						 */
+						final String trackbackUrl = getTrackbackUrl(linkUrl, headContent, reader);
+						if (present(trackbackUrl)) {
+							log.debug("found trackback URL");
+							return new TrackbackLink(null, linkUrl, trackbackUrl, true);
+						}
 					} finally {
 						reader.close();
 					}
@@ -96,23 +124,58 @@ public class HttpClientLinkLoader implements LinkLoader {
 				httpGet.abort();
 			}
 		} catch (final Exception e) {
+			log.debug("got exception: ", e);
 			// ignore
 		}
 		log.debug("no link found");
 		return new Link(null, linkUrl, null, false);
 	}
 
+
+	/**
+	 * Checks first the header for a trackback URL and if no could be found, 
+	 * the body is checked.
+	 * 
+	 * @param linkUrl
+	 * @param headContent
+	 * @param reader
+	 * @return
+	 * @throws IOException
+	 */
+	protected String getTrackbackUrl(final String linkUrl, final String headContent, final BufferedReader reader) throws IOException {
+		/*
+		 * first: check head section
+		 */
+		final String trackbackUrl = getTrackbackUrlFromHtml(linkUrl, headContent);
+		if (present(trackbackUrl)) {
+			return trackbackUrl;
+		}
+		/*
+		 * check HTML body
+		 */
+		return getTrackbackUrlFromHtml(linkUrl, readRemainingPage(reader));
+	}
+
 	@Override
-	public String loadPageContents(String linkUrl) {
+	public String loadPageContents(final String linkUrl) {
 		throw new UnsupportedOperationException();
 	}
 	@Override
-	public boolean containsLink(String htmlText, String link) {
+	public boolean containsLink(final String htmlText, final String link) {
 		throw new UnsupportedOperationException();
 	}
 	@Override
-	public List<String> findLinkAddresses(String textileText) {
+	public List<String> findLinkAddresses(final String textileText) {
 		throw new UnsupportedOperationException();
+	}
+
+	protected String readRemainingPage(final BufferedReader reader) throws IOException {
+		final StringBuilder content = new StringBuilder();
+		String line;
+		while ((line = reader.readLine()) != null && content.length() < MAX_HTML_BODY_CHARS) {
+			content.append(line);
+		}
+		return content.toString();
 	}
 
 	protected String readHeadSectionOfPage(final BufferedReader reader) throws IOException {
@@ -126,14 +189,55 @@ public class HttpClientLinkLoader implements LinkLoader {
 	}
 
 	protected boolean reachedEndOfHeadSection(final String line) {
-		return endOfHeadSectionPattern.matcher(line).find();
+		return END_OF_HTML_HEAD_PATTERN.matcher(line).matches();
 	}
 
-	protected String getPingbackUrlFromHtml(String html) {
-		final Matcher matcher = pingbackUrlPattern.matcher(html);
+	protected String getPingbackUrlFromHtml(final String html) {
+		final Matcher matcher = PINGBACK_URL_PATTERN.matcher(html);
 		if (matcher.find()) {
 			return matcher.group(1);
 		}
+		return null;
+	}
+
+	/**
+	 * Tries to find the trackback ping URL for <code>linkUrl</code> in the 
+	 * provided HTML string. 
+	 * 
+	 * @param linkUrl
+	 * @param html
+	 * @return The trackback ping URL for <code>linkUrl</code> if found, otherwise <code>null</code>.
+	 */
+	protected String getTrackbackUrlFromHtml(final String linkUrl, final String html) {
+		/*
+		 * find all RDF snippets in the page
+		 */
+		final Matcher matcher = TRACKBACK_RDF_PATTERN.matcher(html);
+		while (matcher.find()) {
+			/*
+			 * check if the RDF block is the correct one for our linkUrl 
+			 */
+			final String match = matcher.group(1);
+			final Matcher matcher2 = TRACKBACK_LINK_URL_PATTERN.matcher(match);
+			if (matcher2.find()) {
+				final String linkUrl2 = matcher2.group(1);
+				/*
+				 * check for linkUrl (also with local part # removed)
+				 */
+				if (linkUrl.equals(linkUrl2) || linkUrl.replaceAll("\\#.*$", "").equals(linkUrl2)) {
+					/*
+					 * correct RDF block for linKUrl found 
+					 */
+					final Matcher matcher3 = TRACKBACK_PING_URL_PATTERN.matcher(html);
+					if (matcher3.find()) {
+						return matcher3.group(1);
+					}
+				}
+			}
+		}
+		/*
+		 * nothing found
+		 */
 		return null;
 	}
 
--- bibsonomy-pingback/src/test/java/org/bibsonomy/pingback/HttpClientLinkLoaderTest.java
+++ bibsonomy-pingback/src/test/java/org/bibsonomy/pingback/HttpClientLinkLoaderTest.java
@@ -2,6 +2,7 @@ package org.bibsonomy.pingback;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.BufferedReader;
@@ -11,6 +12,7 @@ import java.io.InputStreamReader;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.mortbay.jetty.testing.ServletTester;
 
@@ -21,22 +23,41 @@ import com.malethan.pingback.Link;
  * @version $Id$
  */
 public class HttpClientLinkLoaderTest {
-    private static final String TOP_OF_HTML_PAGE = "" +
-    "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n" +
-    "<html xmlns=\"http://www.w3.org/1999/xhtml\" dir=\"ltr\" lang=\"en-US\">\n" +
-    "\n" +
-    "    <head profile=\"http://gmpg.org/xfn/11\">\n" +
-    "    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />\n" +
-    "\n" +
-    "    <title>Something blah blah &laquo;  Ping tester</title>\n" +
-    "\n" +
-    "    <link rel=\"pingback\" href=\"http://localhost/~emalethan/wordpress/xmlrpc.php\" />\n" +
-    "\n";
+	private static final String TOP_OF_HTML_PAGE = "" +
+	"<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n" +
+	"<html xmlns=\"http://www.w3.org/1999/xhtml\" dir=\"ltr\" lang=\"en-US\">\n" +
+	"\n" +
+	"    <head profile=\"http://gmpg.org/xfn/11\">\n" +
+	"    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />\n" +
+	"\n" +
+	"    <title>Something blah blah &laquo;  Ping tester</title>\n" +
+	"\n" +
+	"    <link rel=\"pingback\" href=\"http://localhost/~emalethan/wordpress/xmlrpc.php\" />\n" +
+	"\n";
+
+	private static final String TRACKBACK_RDF1 = "<rdf:RDF xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"\n" +
+	"xmlns:dc=\"http://purl.org/dc/elements/1.1/\"\n" +
+	"xmlns:trackback=\"http://madskills.com/public/xml/rss/module/trackback/\">\n" + 
+	"<rdf:Description\n" +
+	"rdf:about=\"http://www.foo.com/archive.html#foo\"\n" +
+	"dc:identifier=\"http://www.foo.com/archive.html#foo\"\n" +
+	"dc:title=\"Foo Bar\"\n" +
+	"trackback:ping=\"http://www.foo.com/tb.cgi/5\" />\n" +
+	"</rdf:RDF>\n";
+	private static final String TRACKBACK_RDF2 = "<rdf:RDF xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"\n" +
+	"xmlns:dc=\"http://purl.org/dc/elements/1.1/\"\n" +
+	"xmlns:trackback=\"http://madskills.com/public/xml/rss/module/trackback/\">\n" + 
+	"<rdf:Description\n" +
+	"rdf:about=\"http://www.foo.com/book.html\"\n" +
+	"dc:identifier=\"http://www.foo.com/book.html\"\n" +
+	"dc:title=\"Foo Bar\"\n" +
+	"trackback:ping=\"http://www.foo.com/tb.cgi/5\" />\n" +
+	"</rdf:RDF>\n";
 
 	private ServletTester tester;
 	private String baseUrl;
 	private HttpClientLinkLoader linkLoader;
-	
+
 	@Before
 	public void setUp() throws Exception {
 		this.tester = new ServletTester();
@@ -44,86 +65,168 @@ public class HttpClientLinkLoaderTest {
 		this.tester.addServlet(TestServlet.class, "/*");
 		this.baseUrl = tester.createSocketConnector(true);
 		this.tester.start();
-		
+
 		this.linkLoader = new HttpClientLinkLoader();
-		
+
 	}
-	
+
 	@After
 	public void shutDown() throws Exception {
 		this.tester.stop();
 	}
-	
+
 	@Test
 	public void testLoadLink() {
 
 		assertFalse(linkLoader.loadLink(baseUrl + "/pingback").isPingbackEnabled());
-//		assertTrue(linkLoader.loadLink(baseUrl + "/pingback?header=true").isPingbackEnabled()); // FIXME: Why infinite loop?
-		
+		//		assertTrue(linkLoader.loadLink(baseUrl + "/pingback?header=true").isPingbackEnabled()); // FIXME: Why infinite loop?
+
 		final Link link1 = linkLoader.loadLink(baseUrl + "/pingback?body=true");
 		assertTrue(link1.isPingbackEnabled());
 		assertEquals(baseUrl + "/pingback/xmlrpc", link1.getPingbackUrl());
 		assertEquals(baseUrl + "/pingback?body=true", link1.getUrl());
-		
+
 		final Link link2 = linkLoader.loadLink(baseUrl + "/pingback?body=true&header=true");
 		assertTrue(link2.isPingbackEnabled());
 		assertEquals(baseUrl + "/pingback/xmlrpc", link2.getPingbackUrl());
 		assertEquals(baseUrl + "/pingback?body=true&header=true", link2.getUrl());
 	}
 
-    @Test
-    public void testStopsReadingAPageAtTheEndOfHead() throws IOException {
-        String htmlPage = "" +
-                TOP_OF_HTML_PAGE +
-                "   <!-- hello -->\n" +
-                "</head>\n" +
-                "<body>\n" +
-                "    <div id=\"page\">\n" +
-                "\n" +
-                "\n" +
-                "    <div id=\"header\">";
-
-        String captured = linkLoader.readHeadSectionOfPage(getReaderForString(htmlPage));
-        System.out.println(captured);
-        assertTrue("Have got this '<!-- hello -->' in", captured.indexOf("<!-- hello -->") > -1);
-        assertFalse("should not have got as far as the end-head tag", captured.indexOf("</head>") > -1);
-        assertFalse("should not have got as far as the body tag", captured.indexOf("<body>") > -1);
-    }
-
-    public void testStopsReadingAPageAtTheStartOfBody() throws IOException {
-        String htmlPage = "" +
-                TOP_OF_HTML_PAGE +
-                "   <!-- hello -->\n" +
-                "<body>\n" +
-                "    <div id=\"page\">\n" +
-                "\n" +
-                "\n" +
-                "    <div id=\"header\">";
-
-        String captured = linkLoader.readHeadSectionOfPage(getReaderForString(htmlPage));
-        System.out.println(captured);
-        assertTrue("Have got this '<!-- hello -->' in", captured.indexOf("<!-- hello -->") > -1);
-        assertFalse("should not have got as far as the body tag", captured.indexOf("<body>") > -1);
-    }
-
-    public void testStopsReadingAPageAtTheStartOfBodyWithAttributes() throws IOException {
-        String htmlPage = "" +
-                TOP_OF_HTML_PAGE +
-                "   <!-- hello -->\n" +
-                "<body class=\"wide\">\n" +
-                "    <div id=\"page\">\n" +
-                "\n" +
-                "\n" +
-                "    <div id=\"header\">";
-
-        String captured = linkLoader.readHeadSectionOfPage(getReaderForString(htmlPage));
-        System.out.println(captured);
-        assertTrue("Have got this '<!-- hello -->' in", captured.indexOf("<!-- hello -->") > -1);
-        assertFalse("should not have got as far as the body tag", captured.indexOf("<body>") > -1);
-    }
 	
-    private BufferedReader getReaderForString(String htmlPage) {
-        return new BufferedReader(new InputStreamReader(new ByteArrayInputStream(htmlPage.getBytes())));
-    }
-    
+	@Test 
+	public void testHttpErrors() {
+		assertFalse(linkLoader.loadLink(baseUrl + "/errors").isPingbackEnabled());
+		assertFalse(linkLoader.loadLink(baseUrl + "/stream").isPingbackEnabled());
+	}
+	
+	
+	@Test
+	public void testStopsReadingAPageAtTheEndOfHead() throws IOException {
+		String htmlPage = "" +
+		TOP_OF_HTML_PAGE +
+		"   <!-- hello -->\n" +
+		"</head>\n" +
+		"<body>\n" +
+		"    <div id=\"page\">\n" +
+		"\n" +
+		"\n" +
+		"    <div id=\"header\">";
+
+		String captured = linkLoader.readHeadSectionOfPage(getReaderForString(htmlPage));
+		System.out.println(captured);
+		assertTrue("Have got this '<!-- hello -->' in", captured.indexOf("<!-- hello -->") > -1);
+		assertFalse("should not have got as far as the end-head tag", captured.indexOf("</head>") > -1);
+		assertFalse("should not have got as far as the body tag", captured.indexOf("<body>") > -1);
+	}
+	
+	@Test
+	@Ignore
+	public void testStopsReadingAPageAtTheStartOfBody() throws IOException {
+		final String htmlPage = TOP_OF_HTML_PAGE +
+		"   <!-- hello -->\n" +
+		"<body>\n" +
+		"    <div id=\"page\">\n" +
+		"\n" +
+		"\n" +
+		"    <div id=\"header\">";
+
+		String captured = linkLoader.readHeadSectionOfPage(getReaderForString(htmlPage));
+		System.out.println(captured);
+		assertTrue("Have got this '<!-- hello -->' in", captured.indexOf("<!-- hello -->") > -1);
+		assertFalse("should not have got as far as the body tag", captured.indexOf("<body>") > -1);
+	}
+
+	@Test
+	public void testStopsReadingAPageAtTheStartOfBodyWithAttributes() throws IOException {
+		final String htmlPage = TOP_OF_HTML_PAGE +
+		"   <!-- hello -->\n" +
+		"<body class=\"wide\">\n" +
+		"    <div id=\"page\">\n" +
+		"\n" +
+		"\n" +
+		"    <div id=\"header\">";
+
+		final String captured = linkLoader.readHeadSectionOfPage(getReaderForString(htmlPage));
+		System.out.println(captured);
+		assertTrue("Have got this '<!-- hello -->' in", captured.indexOf("<!-- hello -->") > -1);
+		assertFalse("should not have got as far as the body tag", captured.indexOf("<body>") > -1);
+	}
+
+	@Test
+	public void testReadRemainingPage() throws IOException {
+		/*
+		 * create a buffer larger than 500kb
+		 */
+		final StringBuilder buf = new StringBuilder();
+		for (int i=0; i < 60000; i++) {
+			buf.append("0123456789\n");
+		}
+		final String result = linkLoader.readRemainingPage(getReaderForString(buf.toString()));
+		/*
+		 * ensure that not much more than 500kb are read
+		 */
+		assertTrue(result.length() < 500100);
+		assertTrue(result.length() > 499000);
+	}
+	
+	@Test
+	public void testGetTrackbackUrlFromHtml() {
+		System.out.println("weg = "  + "http://www.foo.com/archive.html".replaceAll("\\#.*$", ""));
+		
+		assertEquals("http://www.foo.com/tb.cgi/5", linkLoader.getTrackbackUrlFromHtml("http://www.foo.com/archive.html#foo", TRACKBACK_RDF1));
+		assertNull(linkLoader.getTrackbackUrlFromHtml("http://www.foo.com/archive.html", TRACKBACK_RDF1));
+		
+		assertEquals("http://www.foo.com/tb.cgi/5", linkLoader.getTrackbackUrlFromHtml("http://www.foo.com/book.html#foo", TRACKBACK_RDF2));
+		assertEquals("http://www.foo.com/tb.cgi/5", linkLoader.getTrackbackUrlFromHtml("http://www.foo.com/book.html", TRACKBACK_RDF2));
+
+		/*
+		 * trackback URL in HTML head section
+		 */
+		final String html = TOP_OF_HTML_PAGE + "<body>" + TRACKBACK_RDF1 + "\n\n<b>Cool!</b>\n" + TRACKBACK_RDF2;
+		
+		assertEquals("http://www.foo.com/tb.cgi/5", linkLoader.getTrackbackUrlFromHtml("http://www.foo.com/book.html#foo", html));
+		assertEquals("http://www.foo.com/tb.cgi/5", linkLoader.getTrackbackUrlFromHtml("http://www.foo.com/book.html", html));
+		assertEquals("http://www.foo.com/tb.cgi/5", linkLoader.getTrackbackUrlFromHtml("http://www.foo.com/archive.html#foo", html));
+		assertNull(linkLoader.getTrackbackUrlFromHtml("http://www.foo.com/archive.html", html));
+		
+		
+	}
+
+	@Test 
+	public void testGetTrackbackUrl() throws IOException {
+
+		/*
+		 * trackback URL in HTML head section
+		 */
+		final String html = TOP_OF_HTML_PAGE + TRACKBACK_RDF1 + TRACKBACK_RDF2 + "<body>" + "\n\n<b>Cool!</b>\n";
+		final BufferedReader reader = getReaderForString(html);
+		final String headSection = linkLoader.readHeadSectionOfPage(reader);
+		
+		assertEquals("http://www.foo.com/tb.cgi/5", linkLoader.getTrackbackUrl("http://www.foo.com/book.html#foo", headSection, reader));
+		assertEquals("http://www.foo.com/tb.cgi/5", linkLoader.getTrackbackUrl("http://www.foo.com/book.html", headSection, reader));
+		assertEquals("http://www.foo.com/tb.cgi/5", linkLoader.getTrackbackUrl("http://www.foo.com/archive.html#foo", headSection, reader));
+		assertNull(linkLoader.getTrackbackUrl("http://www.foo.com/archive.html", headSection, reader));
+		
+		/*
+		 * trackback URL in HTML body section
+		 */
+		final String html2 = TOP_OF_HTML_PAGE + "<body>" + TRACKBACK_RDF1 + "\n\n<b>Cool!</b>\n" + TRACKBACK_RDF2;
+		
+		final BufferedReader reader2 = getReaderForString(html2);
+		final String headSection2 = linkLoader.readHeadSectionOfPage(reader2);
+		
+		assertEquals("http://www.foo.com/tb.cgi/5", linkLoader.getTrackbackUrl("http://www.foo.com/book.html#foo", headSection2, reader2));
+		assertEquals("http://www.foo.com/tb.cgi/5", linkLoader.getTrackbackUrl("http://www.foo.com/book.html", headSection2, reader2));
+		assertEquals("http://www.foo.com/tb.cgi/5", linkLoader.getTrackbackUrl("http://www.foo.com/archive.html#foo", headSection2, reader2));
+		assertNull(linkLoader.getTrackbackUrl("http://www.foo.com/archive.html", headSection2, reader2));
+		
+		
+		
+	}
+	
+
+	private BufferedReader getReaderForString(String htmlPage) {
+		return new BufferedReader(new InputStreamReader(new ByteArrayInputStream(htmlPage.getBytes())));
+	}
+
 }
