ce65ebcd7eea2255c232811ea7da6be1de09dcca
Robert Jäschke <jaeschke@l3s.de>
2011-06-17 12:07:57 +0200

    moved instanciation of HTTPClient into separate class (that could be put into some commons-project)

--- bibsonomy-pingback/src/main/java/org/bibsonomy/pingback/HttpClientLinkLoader.java
+++ bibsonomy-pingback/src/main/java/org/bibsonomy/pingback/HttpClientLinkLoader.java
@@ -16,11 +16,6 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.conn.scheme.PlainSocketFactory;
-import org.apache.http.conn.scheme.Scheme;
-import org.apache.http.conn.scheme.SchemeRegistry;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
 
 import com.malethan.pingback.Link;
 import com.malethan.pingback.LinkLoader;
@@ -46,32 +41,10 @@ public class HttpClientLinkLoader implements LinkLoader {
 	 */
 	private static final int MAX_HTML_BODY_CHARS = 500000;
 
-
-	final HttpClient httpClient;
+	private final HttpClient httpClient;
 
 	public HttpClientLinkLoader() {
-		/*
-		 * HTTP client 4.1.1
-		 */
-		final SchemeRegistry schemeRegistry = new SchemeRegistry();
-		schemeRegistry.register(new Scheme("http", 80, PlainSocketFactory.getSocketFactory()));
-		final ThreadSafeClientConnManager conman = new ThreadSafeClientConnManager(schemeRegistry);
-		//		conman.setDefaultMaxPerRoute(10); // allow more than 10 connections to the same host
-		this.httpClient = new DefaultHttpClient(conman);
-		/*
-		 * HTTP client 4.0.1
-		 */
-		/*
-		final SchemeRegistry schemeRegistry = new SchemeRegistry();
-		schemeRegistry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
-		final HttpParams httpParams = new BasicHttpParams(); // FIXME: not thread-safe :-(
-		//        httpParams.setIntParameter(ConnManagerPNames.MAX_TOTAL_CONNECTIONS, 25);
-		//        httpParams.setParameter(ConnManagerPNames.MAX_CONNECTIONS_PER_ROUTE, 5);
-		final ThreadSafeClientConnManager conman = new ThreadSafeClientConnManager(httpParams, schemeRegistry);
-		//		conman.setDefaultMaxPerRoute(10); // allow more than 10 connections to the same host
-		final HttpParams httpParams2 = new BasicHttpParams(); // FIXME: probably not thread-safe :-(
-		this.httpClient = new DefaultHttpClient(conman, httpParams2);
-		*/
+		this.httpClient = HttpClientHolder.getInstance().getHttpClient();
 	}
 
 
