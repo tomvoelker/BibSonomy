0fabfcda5d3844149701537bc3ee7c9fa302f962
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2011-06-21 15:54:25 +0200

    made text length more flexible

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/ajax/CommentAjaxController.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/ajax/CommentAjaxController.java
@@ -1,6 +1,9 @@
 package org.bibsonomy.webapp.controller.ajax;
 
 import org.bibsonomy.model.Comment;
+import org.bibsonomy.webapp.command.ajax.DiscussionItemAjaxCommand;
+import org.bibsonomy.webapp.util.Validator;
+import org.bibsonomy.webapp.validation.CommentValidator;
 
 /**
  * - ajax/comments
@@ -13,6 +16,11 @@ public class CommentAjaxController extends DiscussionItemAjaxController<Comment>
 	@Override
 	protected Comment initDiscussionItem() {
 		return new Comment();
+	}
+
+	@Override
+	public Validator<DiscussionItemAjaxCommand<Comment>> getValidator() {
+		return new CommentValidator();
 	}
 
 }
--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/ajax/ReviewAjaxController.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/ajax/ReviewAjaxController.java
@@ -1,6 +1,9 @@
 package org.bibsonomy.webapp.controller.ajax;
 
 import org.bibsonomy.model.Review;
+import org.bibsonomy.webapp.command.ajax.DiscussionItemAjaxCommand;
+import org.bibsonomy.webapp.util.Validator;
+import org.bibsonomy.webapp.validation.ReviewValidator;
 
 /**
  * - ajax/reviews
@@ -13,6 +16,11 @@ public class ReviewAjaxController extends DiscussionItemAjaxController<Review> {
 	@Override
 	protected Review initDiscussionItem() {
 		return new Review();
+	}
+
+	@Override
+	public Validator<DiscussionItemAjaxCommand<Review>> getValidator() {
+		return new ReviewValidator();
 	}
 	
 }
