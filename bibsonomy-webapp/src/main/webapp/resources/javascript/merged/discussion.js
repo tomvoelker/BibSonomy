b943ec2a4800df78b7346aa1d7f100f5459f116c
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2011-06-27 17:27:34 +0200

    renamed review.css => discussion.css added a merged discussion.js file

---
+++ bibsonomy-webapp/src/main/webapp/resources/javascript/merged/discussion.js
@@ -0,0 +1,14 @@
+/*
+ * Scripts that are available on every discussion page
+ */
+function includeJavaScript(jsFile) {
+	// separated end script tag to prevent IE bug
+	document.write('<script type="text/javascript" src="' + jsFile + '"></scr' + 'ipt>');
+}
+
+includeJavaScript("/resources/jquery/plugins/ui/plugins/stars/jquery.ui.stars.js");
+includeJavaScript("/resources/jquery/plugins/flot/jquery.flot.js");
+includeJavaScript("/resources/jquery/plugins/flot/jquery.flot.stack.js")
+includeJavaScript("/resources/javascript/discussion/discussion.js");
+includeJavaScript("/resources/javascript/discussion/comments.js");
+includeJavaScript("/resources/javascript/discussion/reviews.js");
