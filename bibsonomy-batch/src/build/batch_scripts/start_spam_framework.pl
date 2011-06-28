8857b095b8729eff1108c36cfc2e189b2c0f9726
BibSonomy <webmaster@bibsonomy.org>
2011-06-28 14:41:06 +0200

    cleaned up

--- bibsonomy-batch/src/build/batch_scripts/start_spam_framework.pl
+++ bibsonomy-batch/src/build/batch_scripts/start_spam_framework.pl
@@ -3,11 +3,9 @@
 # start spam framework
 #
 use strict; 
+use Common qw(check_running);
 
-if (am_i_running($ENV{'TMP'}."/start_spam_framework.pid")) {
-  #print "another instance of start_spam_framework.pl is running on $ENV{'hostname'}. Aborting this job.\n";
-  exit;
-}
+check_running();
 
 # export spam framework directory
 #system ("export SPAM_HOME=/home/kde/bibbackup/bibsonomy-spam-framework");
@@ -15,32 +13,3 @@ $ENV{"SPAM_HOME"} = "/home/kde/bibbackup/bibsonomy-spamframework-client-api-2.0.
 
 # start spam framework
 system ("/usr/lib/jvm/java-6-sun-1.6.0.13/bin/java -Xmx6G -jar /home/kde/bibbackup/bibsonomy-spamframework-client-api-2.0.2-dist.dir/bibsonomy-spamframework-2.0.2.jar > $ENV{'SPAM_HOME'}/log_info");
-
-#################################
-# subroutines
-#################################
-# INPUT: location of lockfile
-# OUTPUT: 1, if a lockfile exists and a program with the pid inside 
-#            the lockfile is running
-#         0, if no lockfile exists or the program with the pid inside 
-#            the lockfile is NOT running; resets the pid in the pid 
-#            to the current pid
-sub am_i_running {
-  my $LOCKFILE = shift;
-  my $PID ="";
-  if (open (FILE, "<$LOCKFILE")) {
-    while (<FILE>) {
-      $PID = $_;
-    }
-    close (FILE);
-    chomp($PID);
-
-    if (kill(0,$PID)) {
-      return 1;
-    }
-  }
-  open (FILE, ">$LOCKFILE");
-  print FILE $$;
-  close (FILE);
-  return 0;
-}
