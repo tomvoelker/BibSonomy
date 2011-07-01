2d17a53b01477178f3c9a6c297f87a58ca45c25b
Waldemar Lautenschlager <wla@cs.uni-kassel.de>
2011-07-01 16:41:05 +0200

    added error handling for synchronization

---
+++ bibsonomy-common/src/main/java/org/bibsonomy/common/exceptions/SynchronizationRunningException.java
@@ -0,0 +1,11 @@
+package org.bibsonomy.common.exceptions;
+
+/**
+ * @author wla
+ * @version $Id$
+ */
+public class SynchronizationRunningException extends RuntimeException{
+	
+	private static final long serialVersionUID = -5071629773592273849L;
+
+}
