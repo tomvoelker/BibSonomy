26e71dcd8852932d242ad60244d1d6c81b954422
Robert Jäschke <jaeschke@l3s.de>
2011-07-18 15:03:31 +0200

    moved status to enum

---
+++ bibsonomy-database-common/src/main/java/org/bibsonomy/database/common/typehandler/SyncStatusTypeHandlerCallback.java
@@ -0,0 +1,27 @@
+package org.bibsonomy.database.common.typehandler;
+
+import java.sql.SQLException;
+
+import org.bibsonomy.model.sync.SynchronizationStatus;
+
+import com.ibatis.sqlmap.client.extensions.ParameterSetter;
+
+/**
+ * @version $Id$
+ */
+public class SyncStatusTypeHandlerCallback extends AbstractTypeHandlerCallback {
+
+	@Override
+	public void setParameter(final ParameterSetter setter, final Object parameter) throws SQLException {
+		if (parameter == null) {
+			setter.setString(SynchronizationStatus.UNKNOWN.getSynchronizationStatus());
+		} else {
+			setter.setString(((SynchronizationStatus) parameter).getSynchronizationStatus());
+		}
+	}
+
+	@Override
+	public Object valueOf(final String str) {
+		return SynchronizationStatus.valueOf(str);
+	}
+}
