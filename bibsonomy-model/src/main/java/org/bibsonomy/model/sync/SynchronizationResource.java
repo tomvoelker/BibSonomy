10af3d1e27a950c00e2e5109faae31c4b140ef96
Robert Jäschke <jaeschke@l3s.de>
2011-07-18 09:55:14 +0200

    fix indentation

--- bibsonomy-model/src/main/java/org/bibsonomy/model/sync/SynchronizationResource.java
+++ bibsonomy-model/src/main/java/org/bibsonomy/model/sync/SynchronizationResource.java
@@ -7,63 +7,63 @@ import java.util.Date;
  * @version $Id$
  */
 public abstract class SynchronizationResource {
-    
-    /**
-     * Create date of this resource.
-     */
-    private Date createDate;
-    
-    private Date changeDate;
-    
-    
-    private SynchronizationStates state;
-    
-    /**
-     * @param resource
-     * @return true if resources are same
-     */
-    public abstract boolean isSame(SynchronizationResource resource);
-    
-    /**
-     * @param createDate the create date and time of this resource to set
-     */
-    public void setCreateDate(Date createDate) {
-	this.createDate = createDate;
-    }
 
-    /**
-     * @return the create date of this resource to set
-     */
-    public Date getCreateDate() {
-	return createDate;
-    }
+	/**
+	 * Create date of this resource.
+	 */
+	private Date createDate;
 
-    /**
-     * @param changeDate the date and time of the last change of this resource to set
-     */
-    public void setChangeDate(Date changeDate) {
-	this.changeDate = changeDate;
-    }
+	private Date changeDate;
 
-    /**
-     * @return date and time of the last change of this resource
-     */
-    public Date getChangeDate() {
-	return changeDate;
-    }
 
-    /**
-     * @param state the synchronization state to set
-     */
-    public void setState(SynchronizationStates state) {
-	this.state = state;
-    }
+	private SynchronizationStates state;
 
-    /**
-     * @return the state 
-     */
-    public SynchronizationStates getState() {
-	return state;
-    }
-    
+	/**
+	 * @param resource
+	 * @return true if resources are same
+	 */
+	public abstract boolean isSame(SynchronizationResource resource);
+
+	/**
+	 * @param createDate the create date and time of this resource to set
+	 */
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	/**
+	 * @return the create date of this resource to set
+	 */
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	/**
+	 * @param changeDate the date and time of the last change of this resource to set
+	 */
+	public void setChangeDate(Date changeDate) {
+		this.changeDate = changeDate;
+	}
+
+	/**
+	 * @return date and time of the last change of this resource
+	 */
+	public Date getChangeDate() {
+		return changeDate;
+	}
+
+	/**
+	 * @param state the synchronization state to set
+	 */
+	public void setState(SynchronizationStates state) {
+		this.state = state;
+	}
+
+	/**
+	 * @return the state 
+	 */
+	public SynchronizationStates getState() {
+		return state;
+	}
+
 }
