5bf4311cfe76a2c6d39ca0bb2695ec2e867e5010
Waldemar Lautenschlager <wla@cs.uni-kassel.de>
2011-07-25 10:55:13 +0200

    enum for synchronization  direction

---
+++ bibsonomy-model/src/main/java/org/bibsonomy/model/sync/SynchronizationDirection.java
@@ -0,0 +1,39 @@
+package org.bibsonomy.model.sync;
+
+/**
+ * @author wla
+ * @version $Id$
+ */
+public enum SynchronizationDirection {
+	/*
+	 * NOTE: column is a varchar(4), so please use short names
+	 */
+	/**
+	 * both directions 
+	 */
+	BOTH("both"),
+	
+	/**
+	 * only server changes will be applied to client 
+	 */
+	SERVER_TO_CLIENT("stoc"),
+	
+	/**
+	 * only client changes will be applied to server
+	 */
+	CLIENT_TO_SERVER("ctos");
+	
+	
+	private String direction;
+
+	private SynchronizationDirection(final String direction) {
+		this.direction = direction;
+	}
+	
+	/**
+	 * @return The string representation for the synchronization status.
+	 */
+	public String getSynchronizationDirection() {
+		return direction;
+	}
+}
