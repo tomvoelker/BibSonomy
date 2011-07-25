866e0eab5f1d2c20cf8af87539859afa0bb9c2fd
Waldemar Lautenschlager <wla@cs.uni-kassel.de>
2011-07-25 11:49:45 +0200

    Callback for sync direction

---
+++ bibsonomy-database-common/src/main/java/org/bibsonomy/database/common/typehandler/SyncDirectionTypeHandlerCallback.java
@@ -0,0 +1,24 @@
+package org.bibsonomy.database.common.typehandler;
+
+import java.sql.SQLException;
+
+import org.bibsonomy.model.sync.SynchronizationDirection;
+import org.bibsonomy.model.sync.SynchronizationStatus;
+
+import com.ibatis.sqlmap.client.extensions.ParameterSetter;
+
+public class SyncDirectionTypeHandlerCallback extends AbstractTypeHandlerCallback {
+	@Override
+	public void setParameter(final ParameterSetter setter, final Object parameter) throws SQLException {
+		if (parameter == null) {
+			setter.setString(SynchronizationDirection.BOTH.getSynchronizationDirection());
+		} else {
+			setter.setString(((SynchronizationDirection) parameter).getSynchronizationDirection());
+		}
+	}
+
+	@Override
+	public Object valueOf(final String str) {
+		return SynchronizationDirection.getSynchronizationDirectionByString(str);
+	}
+}
