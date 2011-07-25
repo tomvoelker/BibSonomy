dc0ff93ee28ab1555f8a82b59a4252b8d50865e0
Robert Jäschke <jaeschke@l3s.de>
2011-07-25 11:07:55 +0200

    fixed and augmented test

--- bibsonomy-database-common/src/test/java/org/bibsonomy/database/common/enums/ConstantIDTest.java
+++ bibsonomy-database-common/src/test/java/org/bibsonomy/database/common/enums/ConstantIDTest.java
@@ -7,6 +7,7 @@ import org.bibsonomy.common.exceptions.UnsupportedResourceTypeException;
 import org.bibsonomy.model.BibTex;
 import org.bibsonomy.model.Bookmark;
 import org.bibsonomy.model.GoldStandardPublication;
+import org.bibsonomy.model.Resource;
 import org.junit.Test;
 
 /**
@@ -20,16 +21,19 @@ public class ConstantIDTest {
 		assertEquals(ConstantID.BOOKMARK_CONTENT_TYPE, ConstantID.getContentTypeByClass(Bookmark.class));
 		assertEquals(ConstantID.BIBTEX_CONTENT_TYPE, ConstantID.getContentTypeByClass(BibTex.class));
 		assertEquals(ConstantID.BIBTEX_CONTENT_TYPE, ConstantID.getContentTypeByClass(GoldStandardPublication.class));
+		assertEquals(ConstantID.ALL_CONTENT_TYPE, ConstantID.getContentTypeByClass(Resource.class));
 	}
 
 	@Test
 	public void testGetClassByContentType() {
 		assertEquals(Bookmark.class, ConstantID.getClassByContentType(ConstantID.BOOKMARK_CONTENT_TYPE));
 		assertEquals(BibTex.class, ConstantID.getClassByContentType(ConstantID.BIBTEX_CONTENT_TYPE));
+		assertEquals(Resource.class, ConstantID.getClassByContentType(ConstantID.ALL_CONTENT_TYPE));
 	}
 
 	@Test
 	public void testGetClassByContentTypeInt() throws Exception {
+		assertEquals(Resource.class, ConstantID.getClassByContentType(0));
 		assertEquals(Bookmark.class, ConstantID.getClassByContentType(1));
 		assertEquals(BibTex.class, ConstantID.getClassByContentType(2));
 		try {
@@ -39,7 +43,7 @@ public class ConstantIDTest {
 			
 		}
 		try {
-			ConstantID.getClassByContentType(0);
+			ConstantID.getClassByContentType(-1);
 			fail("expected " + UnsupportedResourceTypeException.class.getName());
 		} catch (final UnsupportedResourceTypeException e) {
 			
