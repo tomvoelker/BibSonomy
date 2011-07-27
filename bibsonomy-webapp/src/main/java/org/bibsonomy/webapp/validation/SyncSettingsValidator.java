ac3720299f9b63f5269d3d81f31c0b621074292a
Robert Jäschke <jaeschke@l3s.de>
2011-07-27 14:44:51 +0200

    - sync settings handling now also works when several sync servers or clients are available - adopted validation accordingly

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/validation/SyncSettingsValidator.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/validation/SyncSettingsValidator.java
@@ -1,5 +1,7 @@
 package org.bibsonomy.webapp.validation;
 
+import static org.bibsonomy.util.ValidationUtils.present;
+
 import java.util.List;
 
 import org.bibsonomy.model.sync.SyncService;
@@ -29,20 +31,48 @@ public class SyncSettingsValidator implements Validator<SettingsViewCommand> {
 		Assert.notNull(obj);
 		if (obj instanceof SettingsViewCommand) {
 			final SettingsViewCommand command = (SettingsViewCommand) obj;
-			final List<SyncService> syncServer = command.getSyncServer();
-			Assert.notNull(syncServer);
-			Assert.notEmpty(syncServer);
-			for (int i = 0; i < syncServer.size(); i++) {
-				errors.pushNestedPath("syncServer[" + i + "]");
-				ValidationUtils.rejectIfEmptyOrWhitespace(errors, "service", "error.field.required");
-				ValidationUtils.rejectIfEmptyOrWhitespace(errors, "serverUser['userName']", "error.field.required");
-				ValidationUtils.rejectIfEmptyOrWhitespace(errors, "serverUser['apiKey']", "error.field.required");
-//				ValidationUtils.rejectIfEmptyOrWhitespace(errors, "direction", "error.field.required");
-//				ValidationUtils.rejectIfEmptyOrWhitespace(errors, "resourceType", "error.field.required");
+
+			if (present(command.getNewSyncServer())) {
+				/*
+				 * user wants to create a new sync server -> validate this one
+				 */
+				errors.pushNestedPath("newSyncServer");
+				validateSyncServer(errors);
 				errors.popNestedPath();
+			} else {
+				/* 
+				 * user wants to delete or update an existing server
+				 */
+				ValidationUtils.rejectIfEmpty(errors, "syncServer", "error.field.required");
+				final List<SyncService> syncServer = command.getSyncServer();
+				Assert.notEmpty(syncServer);
+
+				for (int i = 0; i < syncServer.size(); i++) {
+					/*
+					 * FIXME: Spring always creates an object for all list
+					 * elements, even if no form fields for that list index
+					 * have been submitted. Thus, we don't check 
+					 * <code>present(syncServer.get(i))</code> but:
+					 */
+					if (present(syncServer.get(i).getService())) {
+						/*
+						 * this service was specified --> check it 
+						 */
+						errors.pushNestedPath("syncServer[" + i + "]");
+						validateSyncServer(errors);
+						errors.popNestedPath();
+					}
+				}
 			}
-			
 		}
 	}
 
+	private void validateSyncServer(final Errors errors) {
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "service", "error.field.required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "serverUser['userName']", "error.field.required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "serverUser['apiKey']", "error.field.required");
+//		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "direction", "error.field.required");
+//		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "resourceType", "error.field.required");
+	}
+	
 }
