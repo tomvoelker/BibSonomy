e1ec15f11bd0f11ac60b36267239b4f3ee31c813
Waldemar Lautenschlager <wla@cs.uni-kassel.de>
2011-07-27 17:36:57 +0200

    added ConflictResolutionStrategy handling for synchronization

---
+++ bibsonomy-database-common/src/main/java/org/bibsonomy/database/common/typehandler/ConflictResolutionStrategyTypeHandlerCallback.java
@@ -0,0 +1,28 @@
+package org.bibsonomy.database.common.typehandler;
+
+import java.sql.SQLException;
+
+import org.bibsonomy.model.sync.ConflictResolutionStrategy;
+
+import com.ibatis.sqlmap.client.extensions.ParameterSetter;
+
+public class ConflictResolutionStrategyTypeHandlerCallback extends AbstractTypeHandlerCallback {
+	@Override
+	public void setParameter(final ParameterSetter setter, final Object parameter) throws SQLException {
+		if (parameter == null) {
+			throw new IllegalArgumentException("given conflict resolution strategy is null");
+		} else {
+			if (parameter instanceof ConflictResolutionStrategy) {
+				setter.setString(((ConflictResolutionStrategy)parameter).getConflictResolutionStrategy());
+			} else {
+				throw new IllegalArgumentException("given object isn't a instance of ConflictResolutionStartegy");
+			}
+		}
+	}
+
+	@Override
+	public Object valueOf(final String str) {
+		return ConflictResolutionStrategy.getConflictResolutionStrategyByString(str);
+	}
+
+}
