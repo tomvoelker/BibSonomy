21fac09a08aa2222847b6112fc03988d0ead98c1
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2011-07-28 16:44:56 +0200

    cleanup

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/config/Parameters.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/config/Parameters.java
@@ -8,15 +8,23 @@ package org.bibsonomy.webapp.config;
  */
 public class Parameters {
 	
-	public static final Integer NUM_RELATED_USERS = 20;
+	/**
+	 * TODO: improve docu
+	 */
+	public static final int NUM_RELATED_USERS = 20;
 	
-	public static final Integer NUM_RELATED_TAGS = 20;
+	/**
+	 * TODO: improve docu
+	 */
+	public static final int NUM_RELATED_TAGS = 20;
 	
-	public static final Integer NUM_BOOKMARKS_ON_HOMEPAGE = 20;
-	
-	public static final Integer NUM_PUBLICATIONS_ON_HOMEPAGE = 20;
-	
-	public static final Integer NUM_RESOURCES_FOR_PERSONALIZED_RANKING = 1000;
-	
+	/**
+	 * TODO: improve docu
+	 */
+	public static final int NUM_RESOURCES_FOR_PERSONALIZED_RANKING = 1000;
 
+	/**
+	 * the tag threshold of an user
+	 */
+	public static final int TAG_THRESHOLD = 20000;
 }
