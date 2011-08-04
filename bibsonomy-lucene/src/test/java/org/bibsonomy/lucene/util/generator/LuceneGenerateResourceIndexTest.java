655e6508aee32cb6a805f7afe0cda5b60d34e6ff
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2011-08-04 14:46:19 +0200

    refactored lucene module
    removed singletons
    removed unused param objects
    removed unused queries
    removed dependency to mockejb to test scope

---
+++ bibsonomy-lucene/src/test/java/org/bibsonomy/lucene/util/generator/LuceneGenerateResourceIndexTest.java
@@ -0,0 +1,54 @@
+package org.bibsonomy.lucene.util.generator;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.bibsonomy.database.testutil.JNDIBinder;
+import org.bibsonomy.lucene.index.LuceneResourceIndex;
+import org.bibsonomy.lucene.index.manager.LuceneGoldStandardManager;
+import org.bibsonomy.lucene.util.LuceneSpringContextWrapper;
+import org.bibsonomy.model.GoldStandardPublication;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author dzo
+ * @version $Id$
+ */
+public class LuceneGenerateResourceIndexTest {
+    
+    private static LuceneGoldStandardManager<GoldStandardPublication> manager;
+
+    @SuppressWarnings("unchecked")
+	@BeforeClass
+    public static void initLucene() throws Exception {
+    	JNDIBinder.bind();
+		manager = (LuceneGoldStandardManager<GoldStandardPublication>) LuceneSpringContextWrapper.getBeanFactory().getBean("luceneGoldStandardPublicationManager");
+		
+		// delete old indices
+		final List<LuceneResourceIndex<GoldStandardPublication>> resourceIndices = manager.getResourceIndeces();
+		for (final LuceneResourceIndex<GoldStandardPublication> index : resourceIndices) {
+			index.deleteIndex();
+		}
+    }
+    
+    /**
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void generateIndex() throws Exception {
+    	manager.generateIndex(false);
+
+		assertEquals(2, manager.getStatistics().getNumDocs());
+    }
+    
+    @AfterClass
+    public static void resetIndex() {
+    	for (final LuceneResourceIndex<GoldStandardPublication> index : manager.getResourceIndeces()) {
+    		index.reset();
+		}
+    }
+}
