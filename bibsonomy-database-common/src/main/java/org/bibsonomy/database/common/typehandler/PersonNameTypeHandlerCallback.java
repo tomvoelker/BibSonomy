fb5704a6feaf7367ccb140a9e1da9b992d2ea812
Robert Jäschke <jaeschke@l3s.de>
2011-08-18 14:26:00 +0200

    (de)serialization of person name lists

---
+++ bibsonomy-database-common/src/main/java/org/bibsonomy/database/common/typehandler/PersonNameTypeHandlerCallback.java
@@ -0,0 +1,30 @@
+package org.bibsonomy.database.common.typehandler;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.bibsonomy.model.PersonName;
+import org.bibsonomy.model.util.PersonNameUtils;
+
+import com.ibatis.sqlmap.client.extensions.ParameterSetter;
+
+/**
+ * @version $Id$
+ */
+public class PersonNameTypeHandlerCallback extends AbstractTypeHandlerCallback {
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void setParameter(final ParameterSetter setter, final Object parameter) throws SQLException {
+		if (parameter == null) {
+			setter.setString(null);
+		} else {
+			setter.setString(PersonNameUtils.serializePersonNames(((List<PersonName>) parameter), true));
+		}
+	}
+
+	@Override
+	public Object valueOf(final String str) {
+		return PersonNameUtils.discoverPersonNames(str);
+	}
+}
