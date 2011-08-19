d885c6578e553dd938399f657f0ae6457dab312c
berndt <devnull@localhost>
2011-08-19 13:55:09 +0200

    Added an Edit-Textfield on the CV Page

--- bibsonomy-wiki/src/main/java/org/bibsonomy/wiki/tags/PublicationListTag.java
+++ bibsonomy-wiki/src/main/java/org/bibsonomy/wiki/tags/PublicationListTag.java
@@ -18,7 +18,9 @@ import org.bibsonomy.model.Post;
 import org.bibsonomy.model.Resource;
 
 /**
+ * 
  * @author philipp
+ * @author Bernd
  * @version $Id$
  */
 public class PublicationListTag extends AbstractTag {
@@ -27,7 +29,7 @@ public class PublicationListTag extends AbstractTag {
 	private static final String LAYOUT = "layout";
 	
 	
-	public static final String TAG_NAME = "publicationlist";
+	public static final String TAG_NAME = "publications";
 
 	final static public HashSet<String> ALLOWED_ATTRIBUTES_SET = new HashSet<String>(Arrays.asList(NAME, LAYOUT));
 
@@ -57,56 +59,23 @@ public class PublicationListTag extends AbstractTag {
         final String requestedUserName = this.requestedUser.getName();
         
         final List<? extends Post<? extends Resource>> posts = this.logic.getPosts(BibTex.class, GroupingEntity.USER, requestedUserName, Collections.singletonList(tags), null, null, null, 0, Integer.MAX_VALUE, null);
+        Layout layout;
         
-        Layout layout22;
 		try {
-			layout22 = this.layoutRenderer.getLayout(tagAtttributes.get(LAYOUT), requestedUserName);
-			renderedHTML.append(this.layoutRenderer.renderLayout(layout22, posts, false));
+			if(null != tagAtttributes.get(LAYOUT)) {
+				layout = this.layoutRenderer.getLayout(tagAtttributes.get(LAYOUT), requestedUserName);
+			} else {
+				layout = this.layoutRenderer.getLayout("simplehtml", requestedUserName);
+			}
+			renderedHTML.append(this.layoutRenderer.renderLayout(layout, posts, false));
 		} catch (final LayoutRenderingException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (final IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-        
-        
-        /*
-        
-        renderedHTML.append("<div class='align'>");
-        renderedHTML.append("<ul id='liste' class='bookmarkList'>");
-        
-        for (final Post<BibTex> post : posts) {
-        	final BibTex publication = post.getResource();
-        	
-        	renderedHTML.append("<div style='margin:1.2em;' class='entry'><li><span class='entry_title'>");
-        	
-        	// TODO: use an URLGenerator for generating the publication link
-        	renderedHTML.append("<a href='/bibtex/2");
-			renderedHTML.append(publication.getIntraHash());
-        	renderedHTML.append("/");
-			renderedHTML.append(requestedUserName);
-        	renderedHTML.append("' rel='nofollow'>");
-        	renderedHTML.append(publication.getTitle());
-        	renderedHTML.append("</a>");
-        	renderedHTML.append("</span>");
-        	
-        	final String description = post.getDescription();
-			if (present(description)) {
-        		renderedHTML.append("<a class='hand'> [show details] </a>"); // TODO i18n [show details]
-        		renderedHTML.append("<p class='details'>");
-        		renderedHTML.append(description);
-        		renderedHTML.append("</p>");
-        	}
-        	
-        	renderedHTML.append("</li></div>");
-        }
-        
-        renderedHTML.append("</ul>");
-        renderedHTML.append("</div >");
-        */
-        return renderedHTML;
+		return renderedHTML;
+		
 	}
 
 	@Override
