322f283bfe4da3f5e7088d3df0d2c5a4541105e0
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2011-08-25 15:22:20 +0200

    oauth logic config via spring

--- bibsonomy-opensocial/src/main/java/org/bibsonomy/database/ShindigDBLogicUserInterfaceFactory.java
+++ bibsonomy-opensocial/src/main/java/org/bibsonomy/database/ShindigDBLogicUserInterfaceFactory.java
@@ -1,42 +1,21 @@
 package org.bibsonomy.database;
 
-import org.apache.shindig.auth.SecurityToken;
-import org.bibsonomy.database.DBLogic;
-import org.bibsonomy.database.common.DBSessionFactory;
-import org.bibsonomy.model.User;
-import org.bibsonomy.model.logic.LogicInterface;
-import org.bibsonomy.model.logic.LogicInterfaceFactory;
 import static org.bibsonomy.util.ValidationUtils.present;
 
-public class ShindigDBLogicUserInterfaceFactory  implements ShindigLogicInterfaceFactory {
-	private DBSessionFactory dbSessionFactory;
-	
-	private LogicInterfaceFactory noAuthLogicFactory;
+import org.apache.shindig.auth.SecurityToken;
+import org.bibsonomy.model.logic.LogicInterface;
 
-	public LogicInterface getLogicAccess(SecurityToken st) {
-		if (present(st)&&!st.isAnonymous()) {
-			return new DBLogic(new User(st.getViewerId()), this.dbSessionFactory);
-		} else {
-			return this.noAuthLogicFactory.getLogicAccess(null, null);
+/**
+ * @author fmi
+ * @version $Id$
+ */
+public class ShindigDBLogicUserInterfaceFactory extends DBLogicNoAuthInterfaceFactory implements ShindigLogicInterfaceFactory {
+	
+	public LogicInterface getLogicAccess(final SecurityToken st) {
+		if (present(st) && !st.isAnonymous()) {
+			return this.getLogicAccess(st.getViewerId(), null);
 		}
-	}
-	
-	//------------------------------------------------------------------------
-	// getter/setter
-	//------------------------------------------------------------------------
-	public void setNoAuthLogicFactory(LogicInterfaceFactory noAuthLogicFactory) {
-		this.noAuthLogicFactory = noAuthLogicFactory;
-	}
-
-	public LogicInterfaceFactory getNoAuthLogicFactory() {
-		return noAuthLogicFactory;
-	}
-
-	public void setDbSessionFactory(DBSessionFactory dbSessionFactory) {
-		this.dbSessionFactory = dbSessionFactory;
-	}
-
-	public DBSessionFactory getDbSessionFactory() {
-		return dbSessionFactory;
+		
+		return this.getLogicAccess(null, null);
 	}
 }
