5c82866d8c95233b41747b51d1ff818bb876c4ed
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2011-09-02 14:08:29 +0200

    + todo

--- bibsonomy-webapp/src/main/java/tags/ajax/Autofill.java
+++ bibsonomy-webapp/src/main/java/tags/ajax/Autofill.java
@@ -6,21 +6,26 @@ import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.TagSupport;
 
 /**
- * Encodes the given value with URLEncoder in UTF-8 encoding.
+ * TODO: remove as soon as possible
  *
  */
+@Deprecated // useless!
 public class Autofill extends TagSupport {
 	private static final long serialVersionUID = 868854563296030689L;
 	private String var;
 
-	public void setVar(String var) {
+	/**
+	 * @param var
+	 */
+	public void setVar(final String var) {
 	    this.var = var;
 	}
 	
+   @Override
    public int doStartTag() throws JspException {
       try {
          pageContext.getOut().print("<span class=\"autofill\" id=\"" + var + "\"/>");
-      } catch (IOException ioe) {
+      } catch (final IOException ioe) {
          throw new JspException("Error: IOException while writing to client" + ioe.getMessage());
       }
       return SKIP_BODY;
