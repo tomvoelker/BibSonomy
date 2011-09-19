ad47298d02631cc86af557308098de5aeb0c5c44
berndt <devnull@localhost>
2011-09-19 18:06:03 +0200

    Fixed the CV look.

--- bibsonomy-wiki/src/main/java/org/bibsonomy/wiki/enums/UserLayout.java
+++ bibsonomy-wiki/src/main/java/org/bibsonomy/wiki/enums/UserLayout.java
@@ -1,8 +1,10 @@
 package org.bibsonomy.wiki.enums;
 
 public enum UserLayout {
-	LAYOUT_ROBERT("cv.layout.robert"),
-	LAYOUT_TABLE("cv.layout.table"),
+	LAYOUT_DEFAULT_II_EN("cv.layout.robert.en"),
+	LAYOUT_DEFAULT_II_GER("cv.layout.robert.ger"),
+	LAYOUT_DEFAULT_I_EN("cv.layout.table.en"),
+	LAYOUT_DEFAULT_I_GER("cv.layout.table.ger"),
 	LAYOUT_CURRENT("");
 	
 	private final String ref;
--- bibsonomy-wiki/src/main/java/org/bibsonomy/wiki/tags/user/RegDateTag.java
+++ bibsonomy-wiki/src/main/java/org/bibsonomy/wiki/tags/user/RegDateTag.java
@@ -1,21 +1,20 @@
 package org.bibsonomy.wiki.tags.user;
 
 import static org.bibsonomy.util.ValidationUtils.present;
-import info.bliki.htmlcleaner.Utils;
+
+import java.util.Date;
 
 import org.bibsonomy.wiki.tags.UserTag;
 
 /**
- * This is a simple registered-date-tag
- * Usage: <regdate />
+ * This is a simple registered-date-tag Usage: <regdate />
+ * 
  * @author Bernd Terbrack
- *
  */
 public class RegDateTag extends UserTag {
-	
+
 	/*
-	 * TODO Unify date handling for this tag and for the BirthdayTag (same options)
-	 * probably useful would be an AbstractDate Tag
+	 * TODO Unify date handling for this tag and for the BirthdayTag (same options) probably useful would be an AbstractDate Tag
 	 */
 	private static final String TAG_NAME = "regdate";
 
@@ -29,7 +28,7 @@ public class RegDateTag extends UserTag {
 	@Override
 	protected String renderUserTag() {
 		final StringBuilder renderedHTML = new StringBuilder();
-		final String regDate = Utils.escapeXmlChars(this.requestedUser.getRegistrationDate().toString());
+		final Date regDate = this.requestedUser.getRegistrationDate();
 		if (present(regDate)) {
 			renderedHTML.append(regDate);
 		}
