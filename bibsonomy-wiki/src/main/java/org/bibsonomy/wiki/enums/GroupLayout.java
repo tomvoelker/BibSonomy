35719c4b006c380638a17892321a0b22e6dd83ad
berndt <devnull@localhost>
2011-09-22 00:54:03 +0200

    öffentliche vorschau now written out Settings-Page link has been added /cv/user|group/name fixed group/user specific layout menu added group def layouts added fixed some messages

--- bibsonomy-wiki/src/main/java/org/bibsonomy/wiki/enums/GroupLayout.java
+++ bibsonomy-wiki/src/main/java/org/bibsonomy/wiki/enums/GroupLayout.java
@@ -1,5 +1,19 @@
 package org.bibsonomy.wiki.enums;
 
 public enum GroupLayout {
+	LAYOUT_G_DEFAULT_I_EN("cv.layout.group.def1en"),
+	LAYOUT_G_DEFAULT_I_GER("cv.layout.group.def1ger"),
+	LAYOUT_CURRENT("");
+	
+	private final String ref;
+	private GroupLayout(final String ref) {
+		this.ref = ref;
+	}
 
+	/**
+	 * @return the ref
+	 */
+	public String getRef() {
+		return this.ref;
+	}
 }
