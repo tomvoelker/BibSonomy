d81709c9d6050c044fe0081185dac80d217e06fc
Robert Jäschke <jaeschke@l3s.de>
2011-09-30 14:03:09 +0200

    +Statistics

---
+++ bibsonomy-model/src/main/java/org/bibsonomy/model/statistics/Statistics.java
@@ -0,0 +1,49 @@
+package org.bibsonomy.model.statistics;
+
+/**
+ * Statistics about certain elements of the model, e.g., posts of a user.
+ * 
+ * @author rja
+ * @version $Id$
+ */
+public class Statistics {
+
+	private int count;
+	private int min;
+	private int max;
+	private double average;
+	
+
+	public int getCount() {
+		return this.count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public int getMin() {
+		return this.min;
+	}
+
+	public void setMin(int min) {
+		this.min = min;
+	}
+
+	public int getMax() {
+		return this.max;
+	}
+
+	public void setMax(int max) {
+		this.max = max;
+	}
+
+	public double getAverage() {
+		return this.average;
+	}
+
+	public void setAverage(double average) {
+		this.average = average;
+	}
+	
+}
