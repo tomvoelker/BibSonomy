1110801c74ee9fd88b861bcbd069003410bae00c
Robert Jäschke <jaeschke@l3s.de>
2011-09-30 14:35:48 +0200

    s/contentType/resourceType/

--- bibsonomy-database/src/test/java/org/bibsonomy/database/managers/discussion/CommentDatabaseManagerTest.java
+++ bibsonomy-database/src/test/java/org/bibsonomy/database/managers/discussion/CommentDatabaseManagerTest.java
@@ -8,11 +8,11 @@ import java.util.Collections;
 
 import org.bibsonomy.common.exceptions.ValidationException;
 import org.bibsonomy.database.managers.AbstractDatabaseManagerTest;
-import org.bibsonomy.database.managers.discussion.CommentDatabaseManager;
-import org.bibsonomy.database.managers.discussion.DiscussionItemDatabaseManager;
+import org.bibsonomy.model.BibTex;
 import org.bibsonomy.model.Comment;
 import org.bibsonomy.model.DiscussionItem;
 import org.bibsonomy.model.Group;
+import org.bibsonomy.model.Resource;
 import org.bibsonomy.model.User;
 import org.bibsonomy.model.util.GroupUtils;
 import org.junit.BeforeClass;
@@ -38,7 +38,7 @@ public class CommentDatabaseManagerTest extends AbstractDatabaseManagerTest {
 		final String userNameComment = ReviewDatabaseManagerTest.USERNAME_2;
 		final String userNameSubComment = ReviewDatabaseManagerTest.USERNAME_1;
 		final String commentText = "Good review!";
-		final String parentHash = this.insertComment(userNameComment, ReviewDatabaseManagerTest.HASH, commentText, null);
+		final String parentHash = this.insertComment(userNameComment, BibTex.class, ReviewDatabaseManagerTest.HASH, commentText, null);
 		
 		final Comment comment = commentDatabaseManager.getDiscussionItemByHashForResource(interHash, userNameComment, parentHash, this.dbSession);
 		assertNotNull(comment);
@@ -48,7 +48,7 @@ public class CommentDatabaseManagerTest extends AbstractDatabaseManagerTest {
 		/*
 		 * test thread structure
 		 */
-		final String subCommentHash = this.insertComment(ReviewDatabaseManagerTest.USERNAME_1, ReviewDatabaseManagerTest.HASH, "correct", parentHash);
+		final String subCommentHash = this.insertComment(ReviewDatabaseManagerTest.USERNAME_1, BibTex.class, ReviewDatabaseManagerTest.HASH, "correct", parentHash);
 		final DiscussionItem subComment = commentDatabaseManager.getDiscussionItemByHashForResource(interHash, userNameSubComment, subCommentHash, this.dbSession);
 		assertNotNull(subComment);
 		assertNotNull(comment.getDate());
@@ -70,13 +70,14 @@ public class CommentDatabaseManagerTest extends AbstractDatabaseManagerTest {
 	
 	@Test(expected = ValidationException.class)
 	public void testNoParentCommentFound() {
-		this.insertComment("test", "hah", "jjdfs", "thisisastrangehash");
+		this.insertComment("test", BibTex.class, "hah", "jjdfs", "thisisastrangehash");
 	}
 	
-	private String insertComment(final String username, final String hash, final String text, final String parentHash) {
+	private String insertComment(final String username, final Class<? extends Resource> resourceType, final String hash, final String text, final String parentHash) {
 		final Comment comment = new Comment();
 		fillComment(comment, username, text);
 		comment.setParentHash(parentHash);
+		comment.setResourceType(resourceType);
 		comment.setText(text);
 
 		commentDatabaseManager.createDiscussionItemForResource(hash, comment, this.dbSession);
