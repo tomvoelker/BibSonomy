f0895fa17e605f3b696d4a358678a35ee6d56a5e
Sven Stefani <sst@cs.uni-kassel.de>
2011-10-06 14:00:16 +0200

    added ratings statistics of  discussed posts. return value of getPostStatistics() changed from int to Statistics

--- bibsonomy-database/src/main/java/org/bibsonomy/database/managers/chain/statistic/StatisticChainElement.java
+++ bibsonomy-database/src/main/java/org/bibsonomy/database/managers/chain/statistic/StatisticChainElement.java
@@ -3,12 +3,13 @@ package org.bibsonomy.database.managers.chain.statistic;
 import org.bibsonomy.database.managers.StatisticsDatabaseManager;
 import org.bibsonomy.database.managers.chain.ChainElement;
 import org.bibsonomy.database.params.StatisticsParam;
+import org.bibsonomy.model.statistics.Statistics;
 
 /**
  * @author Stefan Stützer
  * @version $Id$
  */
-public abstract class StatisticChainElement extends ChainElement<Integer, StatisticsParam> {
+public abstract class StatisticChainElement extends ChainElement<Statistics, StatisticsParam> {
 
 	protected final StatisticsDatabaseManager db;
 
--- bibsonomy-database/src/main/java/org/bibsonomy/database/managers/chain/statistic/tag/get/GetRelationCountByUser.java
+++ bibsonomy-database/src/main/java/org/bibsonomy/database/managers/chain/statistic/tag/get/GetRelationCountByUser.java
@@ -3,6 +3,7 @@ package org.bibsonomy.database.managers.chain.statistic.tag.get;
 import org.bibsonomy.database.common.DBSession;
 import org.bibsonomy.database.managers.chain.statistic.StatisticChainElement;
 import org.bibsonomy.database.params.StatisticsParam;
+import org.bibsonomy.model.statistics.Statistics;
 
 /**
  * Returns the number of relations for the current user.
@@ -18,8 +19,8 @@ public class GetRelationCountByUser extends StatisticChainElement {
 	}
 
 	@Override
-	protected Integer handle(StatisticsParam param, DBSession session) {
-		return this.db.getNumberOfRelationsForUser(param, session);
+	protected Statistics handle(StatisticsParam param, DBSession session) {
+		return new Statistics(this.db.getNumberOfRelationsForUser(param, session));
 	}
 
 }
