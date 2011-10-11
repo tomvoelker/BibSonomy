a778529cbd404a76fc9b00ee061d118549c19074
Sven Stefani <sst@cs.uni-kassel.de>
2011-10-11 13:14:55 +0200

    merged properties from RatingStatValues to Statistics renamed UserDiscussionsRatingStatistic to StatisticsValues and use Statistics instead of RatingStatValues

---
+++ bibsonomy-model/src/main/java/org/bibsonomy/model/statistics/StatisticsValues.java
@@ -0,0 +1,55 @@
+/**
+ *
+ *  BibSonomy-Model - Java- and JAXB-Model.
+ *
+ *  Copyright (C) 2006 - 2011 Knowledge & Data Engineering Group,
+ *                            University of Kassel, Germany
+ *                            http://www.kde.cs.uni-kassel.de/
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package org.bibsonomy.model.statistics;
+
+import java.util.List;
+
+
+/**
+ * @author sven
+ * @version $Id$
+ */
+
+public class StatisticsValues extends Statistics{
+
+	
+	private List<Statistics> values;
+
+
+	/**
+	 * @return the list of values
+	 */
+	public List<Statistics> getValues() {
+		return this.values;
+	}
+
+	/**
+	 * @param values the list of values to set
+	 */
+	public void setValues(List<Statistics> values) {
+		this.values = values;
+	}
+
+
+}
