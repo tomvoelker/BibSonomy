82a0539165e5a7eb1edaccae707f918d3c6a544f
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2011-11-01 11:50:16 +0100

    - sys out and err

--- bibsonomy-rest-server/src/main/java/org/bibsonomy/rest/strategy/SynchronizationHandler.java
+++ bibsonomy-rest-server/src/main/java/org/bibsonomy/rest/strategy/SynchronizationHandler.java
@@ -20,7 +20,6 @@ import org.bibsonomy.rest.strategy.sync.PutSyncStatusStrategy;
  * @version $Id$
  */
 public class SynchronizationHandler implements ContextHandler {
-
 	private static final Log log = LogFactory.getLog(SynchronizationHandler.class);
 	
 	private enum SyncMethod {
@@ -31,23 +30,18 @@ public class SynchronizationHandler implements ContextHandler {
 	
 	@Override
 	public Strategy createStrategy(final Context context, final StringTokenizer urlTokens, final HttpMethod httpMethod) {
-		int numTokensLeft = urlTokens.countTokens();
-		if(numTokensLeft != 2) {
+		final int numTokensLeft = urlTokens.countTokens();
+		if (numTokensLeft != 2) {
 			log.error("wrong url tokens count");
-			while (numTokensLeft != 0) {
-				System.err.println(urlTokens.nextToken());
-				numTokensLeft = urlTokens.countTokens();
-			}
 			throw new NoSuchResourceException("cannot process url (no strategy available) - please check url syntax ");
 		}
 		URI serviceURI = null;
 		try {
 			serviceURI = new URI(urlTokens.nextToken());
 		} catch (final URISyntaxException ex) {
-			
-			ex.printStackTrace();
 			throw new NoSuchResourceException("cannot process url (no strategy available) - please check url syntax ");
 		}
+		
 		final SyncMethod actionToken = Enum.valueOf(SyncMethod.class, urlTokens.nextToken().toUpperCase());
 		switch (actionToken) {
 		case PLAN:
