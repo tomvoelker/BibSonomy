ecd25a4b5f34f6b15ca22e3a223e7325c749c23b
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2011-11-07 10:03:41 +0100

    + parsing misc field of the publication

--- bibsonomy-database/src/main/java/org/bibsonomy/database/managers/GoldStandardPublicationDatabaseManager.java
+++ bibsonomy-database/src/main/java/org/bibsonomy/database/managers/GoldStandardPublicationDatabaseManager.java
@@ -1,9 +1,14 @@
 package org.bibsonomy.database.managers;
 
+import static org.bibsonomy.util.ValidationUtils.present;
+
+import java.util.List;
+
 import org.bibsonomy.database.common.DBSession;
 import org.bibsonomy.database.params.BibTexParam;
 import org.bibsonomy.model.BibTex;
 import org.bibsonomy.model.GoldStandardPublication;
+import org.bibsonomy.model.Post;
 
 /**
  * Used to create, read, update and delete gold standard publications from the database.
@@ -22,6 +27,20 @@ public final class GoldStandardPublicationDatabaseManager extends GoldStandardDa
 	}
 	
 	private GoldStandardPublicationDatabaseManager() {}
+	
+	@Override
+	public Post<GoldStandardPublication> getPostDetails(final String loginUserName, final String resourceHash, final String userName, final List<Integer> visibleGroupIDs, final DBSession session) {
+		final Post<GoldStandardPublication> post = super.getPostDetails(loginUserName, resourceHash, userName, visibleGroupIDs, session);
+		
+		if (present(post)) {
+			/*
+			 * before the resource leaves the logic parse the misc field
+			 */
+			post.getResource().parseMiscField();
+		}
+		
+		return post;
+	}
 	
 	@Override
 	protected void onGoldStandardReferenceDelete(final String userName, final String interHash, final String interHashRef, final DBSession session) {
