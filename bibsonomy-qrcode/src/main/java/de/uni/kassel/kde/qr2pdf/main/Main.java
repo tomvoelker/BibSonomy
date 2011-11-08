057e43417197d43cd45d7c7fddc40718d83c5293
derbeukatt <devnull@localhost>
2011-11-08 16:48:22 +0100

    First implementation of correct logging of pdf reading.

--- bibsonomy-qrcode/src/main/java/de/uni/kassel/kde/qr2pdf/main/Main.java
+++ bibsonomy-qrcode/src/main/java/de/uni/kassel/kde/qr2pdf/main/Main.java
@@ -1,4 +1,5 @@
 package de.uni.kassel.kde.qr2pdf.main;
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -13,37 +14,41 @@ public class Main {
 	
 
 	
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
 	{
 		
 		MyLogger logger = null;
 		
-		try
+
+		logger = new MyLogger();
+		
+		Logger.getLogger("org.apache.pdfbox").setLevel(Level.OFF);
+		
+		//PDFFile file = PDFRendererParser.parse("/home/philipp/Dokumente/pdftest/in/1.pdf");
+		//PDFRendererParser.manipulatePDF(file);
+		
+		for(int i = 1; i <= 10; i++)
 		{
-			logger = new MyLogger();
-			
-			Logger.getLogger("org.apache.pdfbox").setLevel(Level.OFF);
-			
-			//PDFFile file = PDFRendererParser.parse("/home/philipp/Dokumente/pdftest/in/1.pdf");
-			//PDFRendererParser.manipulatePDF(file);
-			
-			for(int i = 1; i < 10; i++)
+			try
 			{
 				PDFBoxParser parser = new PDFBoxParser();
-				PDDocument doc = parser.parse("/home/philipp/Dokumente/pdftest/in/2.pdf", logger);
+				PDDocument doc = parser.parse("/home/philipp/Dokumente/pdftest/in/" + String.valueOf(i) + ".pdf", logger);
 				parser.manipulatePDF(doc, i, logger);
 				
-				ITextParser.manipulatePDF("/home/philipp/Dokumente/pdftest/in/2.pdf", i, logger);
+				ITextParser.manipulatePDF("/home/philipp/Dokumente/pdftest/in/" + String.valueOf(i) + ".pdf", i, logger);
+			}	
+			catch(Exception e)
+			{
+				logger.getOut().println("-----------------------------------------");
+				e.printStackTrace(logger.getOut());
+				
+				logger.getOut().println("-----------------------------------------");
+				logger.getOut().println();
+				logger.getOut().println();
 			}
-			
-			logger.getOut().close();
 		}
-		catch(Exception e)
-		{
-			logger.getOut().println("-----------------------------------------");
-			logger.getOut().println(e.getMessage());
-			
-			logger.getOut().println("-----------------------------------------");
-		}
+		
+		logger.getOut().close();		
+		
 	}
 }
