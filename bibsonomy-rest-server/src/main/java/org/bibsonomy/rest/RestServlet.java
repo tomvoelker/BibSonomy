af988af8e171b7e9c8f4e5dc9e6131575c60b0ea
Sven Stefani <sst@cs.uni-kassel.de>
2011-11-21 09:50:03 +0100

    removed shortly ago added debug infos

--- bibsonomy-rest-server/src/main/java/org/bibsonomy/rest/RestServlet.java
+++ bibsonomy-rest-server/src/main/java/org/bibsonomy/rest/RestServlet.java
@@ -366,11 +366,6 @@ public final class RestServlet extends HttpServlet {
 		final String verifyHeader = request.getHeader(SSL_VERIFY_HEADER);
 
 		if (!SUCCESS.equals(verifyHeader)) {
-			if (null != verifyHeader) {
-				log.debug("ssl_verify_header: " + verifyHeader );
-			} else {
-				log.debug("ssl_verify_header: NULL" );
-			}
 			log.debug("ssl_verify_header not found or not '" + SUCCESS + "'");
 			return;
 		}
