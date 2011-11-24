ab29e37ec86492a5231582644af36f398cc39f00
Robert Jäschke <jaeschke@l3s.de>
2011-11-24 23:34:15 +0100

    +batch script for preview images

---
+++ bibsonomy-batch/src/build/batch_scripts/build_preview_images.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#
+# builds for each uploaded file (if possible) a preview image
+#
+# parameters:
+# 1) folder with the documents
+# 2) task \in {"force", "delete"}
+#
+# Changes:
+# 2011-11-24 (rja)
+# - initial version
+#
+
+if [ $# -lt 2 ]; then
+    echo "usage:"
+    echo "  $0 document_dir task"
+    echo "  where task is either force, delete"
+    exit 1;
+fi
+
+DOCS=$1
+TASK=$2
+
+for doc in $(find $1 -type f -name "[0-9a-f]*[0-9a-f]"); do
+    type=$(file -ib $doc)
+    case "$type" in 
+	"application/postscript" | "application/pdf")
+	    # when no small preview there, generate a new one
+	    small=${doc}_SMALL
+	    if [ ! -f $small -o $TASK == "force" ]; then
+		echo "converting $doc ($type)"
+		# get a temporary file
+		temp=$(tempfile)
+		# convert to PNG
+		gs -q -dSAFER -sDEVICE=png16m -dLastPage=1 -r300 -o$temp $doc
+		# make small JPEG previews
+		convert -thumbnail '100x100>' $temp ${doc}_SMALL
+		convert -thumbnail '200x200>' $temp ${doc}_MEDIUM
+		convert -thumbnail '400x400>' $temp ${doc}_LARGE
+	    fi
+	    ;;
+	*)
+	    echo "$type can not be handled"
+	    ;;
+    esac
+	
+done
