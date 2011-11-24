3f02cd759f290d88d721548b01dbcd71d5871cf6
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2011-11-24 15:48:05 +0100

    switched to new resource page controller

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/BibtexkeyCommand.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/BibtexkeyCommand.java
@@ -1,5 +1,7 @@
 package org.bibsonomy.webapp.command;
 
+import org.bibsonomy.webapp.command.resource.PublicationPageCommand;
+
 /**
  * 
  * Bean for providing the bibtexkey
@@ -7,7 +9,7 @@ package org.bibsonomy.webapp.command;
  * @author Flori
  * @version $Id$
  */
-public class BibtexkeyCommand extends BibtexResourceViewCommand{
+public class BibtexkeyCommand extends PublicationPageCommand {
 	
 	/** String to search for */
 	private String requestedKey = "";
@@ -22,7 +24,7 @@ public class BibtexkeyCommand extends BibtexResourceViewCommand{
 	/**
 	 * @param requKey set the bibtexKey 
 	 */
-	public void setRequestedKey(String requKey) {
+	public void setRequestedKey(final String requKey) {
 		this.requestedKey = requKey;
 	}
 }
