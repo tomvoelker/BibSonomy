7a98416af3a3be914b02d905eb95bb24434a7724
berndt <devnull@localhost>
2011-12-07 10:46:09 +0100

    CV: - Minor bugfixes - Changed the look

--- bibsonomy-wiki/src/main/java/org/bibsonomy/wiki/enums/UserLayout.java
+++ bibsonomy-wiki/src/main/java/org/bibsonomy/wiki/enums/UserLayout.java
@@ -1,10 +1,10 @@
 package org.bibsonomy.wiki.enums;
 
 public enum UserLayout {
-	LAYOUT_DEFAULT_II_EN("cv.layout.robert.en"),
-	LAYOUT_DEFAULT_II_GER("cv.layout.robert.ger"),
-	LAYOUT_DEFAULT_I_EN("cv.layout.table.en"),
-	LAYOUT_DEFAULT_I_GER("cv.layout.table.ger"),
+	LAYOUT_DEFAULT_II_EN("cv.layout.user.def2en"),
+	LAYOUT_DEFAULT_II_GER("cv.layout.user.def2ger"),
+	LAYOUT_DEFAULT_I_EN("cv.layout.user.def1en"),
+	LAYOUT_DEFAULT_I_GER("cv.layout.user.def1ger"),
 	LAYOUT_CURRENT("");
 	
 	private final String ref;
