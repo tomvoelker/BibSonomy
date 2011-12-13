be8cc50aa13cabaab6c53b759c8d496d9543ae39
Haile Misgna Haile <haile.misgna@mu.edu.et>
2011-12-13 10:56:22 +0100

    added formatter for publisher

---
+++ bibsonomy-layout/src/main/java/net/sf/jabref/export/layout/format/DIN1505T2PublisherNamesFormatter.java
@@ -0,0 +1,24 @@
+package net.sf.jabref.export.layout.format;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import net.sf.jabref.export.layout.LayoutFormatter;
+
+public class DIN1505T2PublisherNamesFormatter implements LayoutFormatter {
+	
+	public static void main(String[] args) {
+		System.out.println(new DIN1505T2PublisherNamesFormatter().format("Amnesty International ; Barendregt, Wolmet ; Bekker, Mathilde M. ; Speerstra, Mathilde"));
+	}
+
+	@Override
+	public String format(String arg0) {
+		arg0 = arg0.trim();
+		Matcher m = Pattern.compile("(\\A|;\\s)(\\w+)(,|\\z)").matcher(arg0);
+		while (m.find()) {
+			arg0 = arg0.replaceFirst(m.group(), m.group(1) + "<span style=\"font-variant: small-caps\">" + m.group(2) + "</span>" + m.group(3));
+		}
+		return arg0;
+	}
+
+}
