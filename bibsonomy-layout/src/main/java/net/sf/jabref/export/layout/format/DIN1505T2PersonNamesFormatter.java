7a9064a269caf995e28ab8f16dfa389b9dfee83c
Haile Misgna Haile <haile.misgna@mu.edu.et>
2011-12-13 10:52:31 +0100

    changed strategy

--- bibsonomy-layout/src/main/java/net/sf/jabref/export/layout/format/DIN1505T2PersonNamesFormatter.java
+++ bibsonomy-layout/src/main/java/net/sf/jabref/export/layout/format/DIN1505T2PersonNamesFormatter.java
@@ -5,17 +5,16 @@ import java.util.regex.Pattern;
 
 import net.sf.jabref.export.layout.LayoutFormatter;
 
-public class DIN1505T2NameFormatter implements LayoutFormatter {
+public class DIN1505T2PersonNamesFormatter implements LayoutFormatter {
 	
 	public static void main(String[] args) {
-		System.out.println(new DIN1505T2NameFormatter().format("Barendregt, Wolmet ; Bekker, Mathilde M. ; Speerstra, Mathilde"));
-		System.out.println(new DIN1505T2NameFormatter().format("Amnesty International ; Bekker, Mathilde M. ; Speerstra, Mathilde"));
+		System.out.println(new DIN1505T2PersonNamesFormatter().format("Barendregt, Wolmet ; Bekker, Mathilde M. ; Speerstra, Mathilde"));
 	}
 
 	@Override
 	public String format(String arg0) {
 		arg0 = arg0.trim();
-		Matcher m = Pattern.compile("(\\A|;\\s)([\\w\\s]+)(,|\\z|\\s)").matcher(arg0);
+		Matcher m = Pattern.compile("(\\A|;\\s)(\\w+)(,|\\z)").matcher(arg0);
 		while (m.find()) {
 			arg0 = arg0.replaceFirst(m.group(), m.group(1) + "<span style=\"font-variant: small-caps\">" + m.group(2) + "</span>" + m.group(3));
 		}
