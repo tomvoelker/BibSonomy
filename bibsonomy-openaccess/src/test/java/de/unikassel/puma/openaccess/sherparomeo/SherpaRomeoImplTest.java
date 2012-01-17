be89420d78c027f5ad0f7d119d5319fdbca3e07e
Robert Jäschke <jaeschke@l3s.de>
2012-01-17 15:59:49 +0100

    tests for Sherpa Romeo

---
+++ bibsonomy-openaccess/src/test/java/de/unikassel/puma/openaccess/sherparomeo/SherpaRomeoImplTest.java
@@ -0,0 +1,22 @@
+package de.unikassel.puma.openaccess.sherparomeo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+
+/**
+ * @author rja
+ * @version $Id$
+ */
+public class SherpaRomeoImplTest {
+
+	@Test
+	public void testGetPolicyForJournal() {
+		final String start = "{\"publishers\":[{\"name\":\"Association for the Advancement of Artificial Intelligence\"";
+		final SherpaRomeoImpl sr = new SherpaRomeoImpl();
+		final String policy = sr.getPolicyForJournal("AI Magazine", null);
+		
+		assertEquals(start, policy.substring(0, start.length()));
+	}
+}
