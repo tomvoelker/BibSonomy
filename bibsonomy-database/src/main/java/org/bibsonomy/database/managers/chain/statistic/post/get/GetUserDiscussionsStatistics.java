c75f27530c18c808b4f5fa93146ce27e3ecc9f11
Stephan Doerfel <doerfel@cs.uni-kassel.de>
2012-01-17 20:39:23 +0100

    new Discussed - groups page (database)

--- bibsonomy-database/src/main/java/org/bibsonomy/database/managers/chain/statistic/post/get/GetUserDiscussionsStatistics.java
+++ bibsonomy-database/src/main/java/org/bibsonomy/database/managers/chain/statistic/post/get/GetUserDiscussionsStatistics.java
@@ -1,10 +1,15 @@
 package org.bibsonomy.database.managers.chain.statistic.post.get;
 
+import static org.bibsonomy.util.ValidationUtils.present;
+
 import org.bibsonomy.common.enums.FilterEntity;
+import org.bibsonomy.common.enums.GroupID;
+import org.bibsonomy.common.enums.GroupingEntity;
 import org.bibsonomy.database.common.DBSession;
 import org.bibsonomy.database.common.enums.ConstantID;
 import org.bibsonomy.database.managers.chain.statistic.StatisticChainElement;
 import org.bibsonomy.database.params.StatisticsParam;
+import org.bibsonomy.model.Group;
 import org.bibsonomy.model.statistics.Statistics;
 
 /**
@@ -15,6 +20,16 @@ public class GetUserDiscussionsStatistics extends StatisticChainElement {
 
 	@Override
 	protected Statistics handle(StatisticsParam param, DBSession session) {
+		if (GroupingEntity.GROUP.equals(param.getGrouping())) {
+			final Group group = this.groupDb.getGroupByName(param.getRequestedGroupName(), session);
+			if (!present(group) || group.getGroupId() == GroupID.INVALID.getId() || GroupID.isSpecialGroupId(group.getGroupId())) {
+				log.debug("group '" + param.getRequestedGroupName() + "' not found or special group");
+				return new Statistics(0);			
+			}
+			param.setGroupId(group.getGroupId());
+			return this.db.getUserDiscussionsStatisticsForGroup(param, session);
+		}
+		// all other (USER and ALL)
 		return this.db.getUserDiscussionsStatistics(param, session);
 	}
 
