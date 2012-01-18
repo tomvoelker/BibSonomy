1ed0ea117a8d89dd7a32f38caf84f3c4e0ba55da
Robert Jäschke <jaeschke@l3s.de>
2012-01-18 10:19:04 +0100

    return intrahash

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/ajax/DocumentsController.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/ajax/DocumentsController.java
@@ -30,22 +30,22 @@ import org.springframework.context.MessageSource;
  */
 public class DocumentsController extends AjaxController implements MinimalisticController<AjaxDocumentCommand> {
 	private static final Log log = LogFactory.getLog(DocumentsController.class);
-	
+
 	private static final String ALLOWED_EXTENSIONS = StringUtils.implodeStringArray(FileUploadInterface.fileUploadExt, ", ");
-	
+
 	/**
 	 * Path to the documents folder
 	 */
 	private String docPath;
 	private String tempPath;
 	private MessageSource messageSource;
-	
+
 	/**
 	 * max file size, currently 50mb
 	 */
 	private final int maxFileSizeMB = 50;
 	private final long maxFileSize = maxFileSizeMB * 1024 * 1024;
-	
+
 	@Override
 	public AjaxDocumentCommand instantiateCommand() {
 		return new AjaxDocumentCommand();
@@ -63,7 +63,7 @@ public class DocumentsController extends AjaxController implements MinimalisticC
 			command.setResponseString(getXmlError("error.general.login", null, command.getFileID(), null, locale));
 			return Views.AJAX_XML;
 		}
-		
+
 		/*
 		 * check ckey
 		 */
@@ -71,7 +71,7 @@ public class DocumentsController extends AjaxController implements MinimalisticC
 			command.setResponseString(getXmlError("error.field.valid.ckey", null, command.getFileID(), null, locale));
 			return Views.AJAX_XML;
 		}
-		
+
 		/*
 		 * check request method, GET is delete file request, POST is upload File request
 		 */
@@ -97,7 +97,7 @@ public class DocumentsController extends AjaxController implements MinimalisticC
 		command.setResponseString(response);
 		return Views.AJAX_XML;
 	}
-	
+
 	/**
 	 * This method removes temporary file
 	 * @param command
@@ -110,7 +110,7 @@ public class DocumentsController extends AjaxController implements MinimalisticC
 		new File(tempPath + command.getFileHash().substring(32)).delete();
 		return "<root><status>ok</status><fileid>" + command.getFileID() + "</fileid></root>";
 	}
-	
+
 	/**
 	 * This method deletes an existing  file from filesystem and database 
 	 * @param command
@@ -133,12 +133,12 @@ public class DocumentsController extends AjaxController implements MinimalisticC
 		if (!documentOwner.equals(userName)) {
 			return getXmlError("post.bibtex.wrongUser", null, command.getFileID(), null, locale); 
 		}
-		
+
 		/*
 		 * delete entry in database
 		 */
 		logic.deleteDocument(document, intraHash);
-		
+
 		/*
 		 * delete file on disk
 		 */
@@ -146,13 +146,13 @@ public class DocumentsController extends AjaxController implements MinimalisticC
 		final String response = messageSource.getMessage("bibtex.actions.filedeleted", new Object[] {fileName}, locale); 
 		return "<root><status>deleted</status><response>" + response + "</response></root>";
 	}
-	
+
 	/**
 	 * This method handles file upload to a temporary directory 
 	 * @param command
 	 * @return
 	 */
-	private String uploadFile(AjaxDocumentCommand command, Locale locale) {
+	private String uploadFile(final AjaxDocumentCommand command, final Locale locale) {
 		log.debug("Start uploading file");
 
 		/*
@@ -166,23 +166,23 @@ public class DocumentsController extends AjaxController implements MinimalisticC
 		if (!StringUtils.matchExtension(fileItem.getName(), FileUploadInterface.fileUploadExt)) {
 			return getXmlError("error.upload.failed.filetype", new Object[] {ALLOWED_EXTENSIONS}, fileID, fileItem.getName(), locale);	
 		}
-		
+
 		/*
 		 * wrong file size
 		 */
 		final long size = fileItem.getSize();
-		if (size >= maxFileSize) {
+		if (size >= this.maxFileSize) {
 			return getXmlError("error.upload.failed.size", new Object[] {maxFileSizeMB}, fileID, fileItem.getName(), locale);
 		} else if (size == 0) {
 			return getXmlError("error.upload.failed.size0", null, fileID, fileItem.getName(), locale);
 		}
-		
+
 		final File uploadFile;
 		String fileHash = FileUtil.getRandomFileHash(fileItem.getName());
 		if (command.isTemp()) { // /editPublication
-			uploadFile = new File(tempPath + fileHash);
+			uploadFile = new File(this.tempPath + fileHash);
 		} else { // /bibtex/....
-			uploadFile = new File(FileUtil.getFilePath(docPath, fileHash));
+			uploadFile = new File(FileUtil.getFilePath(this.docPath, fileHash));
 		}
 		final String md5Hash = HashUtils.getMD5Hash(fileItem.get());
 		try {
@@ -191,26 +191,34 @@ public class DocumentsController extends AjaxController implements MinimalisticC
 			log.error("Could not write uploaded file.", ex);
 			return getXmlError("error.500", null, fileID, fileItem.getName(), locale);
 		}
-		
+
+		final String intraHash = command.getIntraHash();
 		if (!command.isTemp()) {
 			final Document document = new Document();
 			document.setFileName(fileItem.getName());
 			document.setFileHash(fileHash);
 			document.setMd5hash(md5Hash);
 			document.setUserName(command.getContext().getLoginUser().getName());
-			
+
 			/*
 			 * add document to the data base
 			 */
-			logic.createDocument(document, command.getIntraHash());
-			
+			this.logic.createDocument(document, intraHash);
+
 			/*
 			 * clear fileHash (randomFileName), so only md5Hash over the file content will be sent back
 			 */
-			fileHash ="";
+			fileHash = "";
 		}
-		
-		return "<root><status>ok</status><fileid>" + fileID + "</fileid><filehash>" + md5Hash + fileHash + "</filehash><filename>" + fileItem.getName() + "</filename></root>";
+
+		return "<root>" +
+		"<status>ok</status>" +
+		"<fileid>" + fileID + "</fileid>" +
+		"<filehash>" + md5Hash + fileHash + "</filehash>" +
+		"<filename>" + fileItem.getName() + "</filename>" +
+		"<filename>" + fileItem.getName() + "</filename>" +
+		"<intrahash>" + intraHash + "</intrahash>" +
+		"</root>";
 	}
 
 	/**
@@ -223,7 +231,7 @@ public class DocumentsController extends AjaxController implements MinimalisticC
 		final String errorMsg = messageSource.getMessage("error.upload.failed", new Object[] {reason}, locale);
 		return "<root><status>error</status><reason>" + errorMsg + "</reason><fileid>"+ fileID + "</fileid><filename>" + fileName + "</filename></root>";
 	}
-	
+
 	/**
 	 * @param docPath
 	 *            path to the documents folder to set
