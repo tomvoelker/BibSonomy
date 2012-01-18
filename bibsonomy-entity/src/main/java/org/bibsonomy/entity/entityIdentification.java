50d18a155896bd66ebed5a07a240686c7228ec0d
rkr <devnull@localhost>
2012-01-18 18:24:58 +0100

    coauthors added

--- bibsonomy-entity/src/main/java/org/bibsonomy/entity/entityIdentification.java
+++ bibsonomy-entity/src/main/java/org/bibsonomy/entity/entityIdentification.java
@@ -1,5 +1,9 @@
 package org.bibsonomy.entity;
 
+import java.util.jar.Attributes.Name;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import java.io.IOException;
 import java.io.Reader;
 import java.text.Normalizer;
@@ -18,14 +22,18 @@ import org.apache.ibatis.datasource.jndi.JndiDataSourceFactory;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.mapping.Environment;
 import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.apache.ibatis.transaction.TransactionFactory;
 import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
+import org.apache.ibatis.annotations.Param;
+
 import org.bibsonomy.model.PersonName;
 import org.bibsonomy.model.util.PersonNameParser.PersonListParserException;
 import org.bibsonomy.model.util.PersonNameUtils;
+import org.bibsonomy.util.StringUtils;
 
 public class entityIdentification {
 
@@ -43,7 +51,7 @@ public class entityIdentification {
 			
 			SqlSession session = sqlMapper.openSession();
 			try {
-			authorList = session.selectList("org.mybatis.example.Entity-Identification.selectBibtex");
+			authorList = session.selectList("org.mybatis.example.Entity-Identification.selectBibtex", 1);
 			//author = (String)session.selectOne(
 			//"org.mybatis.example.BlogMapper.selectBibtex");
 			//System.out.println(authorList.get(3));
@@ -65,39 +73,34 @@ public class entityIdentification {
 			
 			SqlSession sessionRkr = sqlMapper.openSession();
 
-		    List<String> test = null;
-		    test = sessionRkr.selectList("org.mybatis.example.Entity-Identification.countAuthorsByName");
-		    //System.out.println(authorCount.get(0));
-
 		List<List<PersonName>> coAuthorList = new ArrayList<List<PersonName>>(0);
 		
 		List<String> allPersons = new ArrayList<String>(0);
 		int n=0;
 		
 		for (String authors: authorList) { //authorList for each publication
-			final List<PersonName> personNames = PersonNameUtils.discoverPersonNames(authors); //.replaceAll("[_[^\\w\\däüöÄÜÖ\\+\\- ]]", ""));
-			coAuthorList.add(personNames);
+			final List<PersonName> allAuthorNamesOfOnePublication = PersonNameUtils.discoverPersonNames(authors); //.replaceAll("[_[^\\w\\däüöÄÜÖ\\+\\- ]]", ""));
 						
-			for (PersonName person: personNames) { //each author in the list of authors
+			for (PersonName author: allAuthorNamesOfOnePublication) { //each author in the list of authors
 			     HashMap<String, String> authorName = new HashMap<String, String>();
-			     authorName.put("firstName", person.getFirstName());
-			     authorName.put("lastName", person.getLastName());
+			     authorName.put("firstName", author.getFirstName());
+			     authorName.put("lastName", author.getLastName());
+			     authorName.put("normalizedName", author.getFirstName() + author.getLastName());
 
-			     sessionRkr.insert("org.mybatis.example.Entity-Identification.insertAuthor", authorName);
-				//check if person already exists in database
-				
-				if (true) {} //select >= 1
-				for (String personA: allPersons) { //compare each author in the list with each author already computed
-					if (personA.equals(person.toString())) { //this person name already exists
-						n++;
-					}
-				}
-				//System.out.println(person);
-				//System.out.println(StringUtil.foldToASCII(person.toString())); //only ASCII
-				//System.out.println(StringUtil.foldToASCII(person.getLastName()));
-				allPersons.add(person.toString());
+				 List<Integer> authorWithThisNameCount = null;
+				 authorWithThisNameCount = sessionRkr.selectList("org.mybatis.example.Entity-Identification.countAuthorsByName", authorName);
+				 
+				 if (authorWithThisNameCount.get(0) < 2) {
+					 sessionRkr.insert("org.mybatis.example.Entity-Identification.insertAuthor", authorName);
+				 }
+				 
+				 for (PersonName coAuthor: allAuthorNamesOfOnePublication) { //add the coauthors to a table
+					 if (!coAuthor.equals(author)) {
+						 sessionRkr.insert("org.mybatis.example.Entity-Identification.insertCoAuthors", normalizePersonName(coAuthor));
+					 }
+				 }
 			}
-			//System.out.println("----------------------------------------");
+
 		}
 
 		System.out.println(n);
@@ -123,13 +126,43 @@ public class entityIdentification {
 		//reduce the
 		String newFirstName = personName.getFirstName().substring(0,1);
 			
-		//check if firstName is shortened
+		/*check if firstName is shortened
 		if (personName.getFirstName().length() == 2 && personName.getFirstName().substring(1,2).equals(".")) {
 		
 		}
+		*/
 		
-		return newFirstName + personName.getLastName();
-		
+		String normalizedName = newFirstName + personName.getLastName();
+		return normalizedName.toLowerCase();		
+	}
+	
+	/**
+	 * Extracts from the last name the last part and cleans it. I.e., from 
+	 * "van de Gruyter" we get "gruyter"
+	 * 
+	 * @param last
+	 * @return
+	 */
+	private static String getLast(final String last) {
+		/*
+		 * A name enclosed in brackets {Like this One} is detected as a single 
+		 * last name. We here re-parse such names to extract the "real" name.
+		 */
+		final String trimmedLast = last.trim();
+		if (trimmedLast.startsWith("{") && trimmedLast.endsWith("}")) {
+			final List<PersonName> name = PersonNameUtils.discoverPersonNamesIgnoreExceptions(trimmedLast.substring(1, trimmedLast.length() - 1));
+			//return name.get(0);
+			return "test";
+		} 
+		/*
+		 * We remove all unusual characters.
+		 */
+		final String cleanedLast = StringUtils.removeNonNumbersOrLettersOrDotsOrCommaOrSpace(trimmedLast).toLowerCase().trim();
+		/*
+		 * If we find a space character, we take the last part of the name
+		 */
+		final int pos = cleanedLast.lastIndexOf(' ');
+		return pos > 0 ? cleanedLast.substring(pos + 1) : cleanedLast;
 	}
 	
 }
