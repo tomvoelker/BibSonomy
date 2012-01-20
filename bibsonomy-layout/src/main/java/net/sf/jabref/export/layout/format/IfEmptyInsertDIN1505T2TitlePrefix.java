e5292c435fd03a1f802d2eb4eb31558aebd9164e
Haile Misgna Haile <haile.misgna@mu.edu.et>
2012-01-20 15:27:22 +0100

    another workaround

---
+++ bibsonomy-layout/src/main/java/net/sf/jabref/export/layout/format/IfEmptyInsertDIN1505T2TitlePrefix.java
@@ -0,0 +1,15 @@
+package net.sf.jabref.export.layout.format;
+
+import static org.bibsonomy.util.ValidationUtils.present;
+
+import net.sf.jabref.export.layout.LayoutFormatter;
+
+public class IfEmptyInsertDIN1505T2TitlePrefix implements LayoutFormatter {
+
+	@Override
+	public String format(String arg0) {
+		if (!present(arg0)) return ": ";
+		return "";
+	}
+
+}
