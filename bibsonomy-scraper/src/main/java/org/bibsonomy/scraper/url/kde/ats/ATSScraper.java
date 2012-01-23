eedf21d561e38d9ce0b0d1ad40c8ba0e13c5f97d
Clemens Baier <clemens.baier@multi-hosting.de>
2012-01-23 09:57:35 +0100

    Scraper for ATS (PATS, AJRCCM, AJRCMB)

---
+++ bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/ats/ATSScraper.java
@@ -0,0 +1,44 @@
+package org.bibsonomy.scraper.url.kde.ats;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import org.bibsonomy.scraper.Tuple;
+import org.bibsonomy.scraper.generic.CitationManagerScraper;
+
+/**
+ * @author clemens
+ * @version $Id$
+ */
+
+public class ATSScraper extends CitationManagerScraper {
+	private static final Pattern DOWNLOAD_LINK_PATTERN = Pattern.compile("<a href=\\\"([^\\\"]*)\\\">Download to citation manager</a>");
+	private static final String SITE_NAME = "American Thoracic Society Journals";
+	private static final String SITE_URL = "http://www.atsjournals.org/";
+	private static final String INFO = "This scraper parses a publication page from the " + href(SITE_URL, SITE_NAME);
+	private static final List<Tuple<Pattern, Pattern>> URL_PATTERNS = Collections.singletonList(new Tuple<Pattern, Pattern>(Pattern.compile(".*" + "atsjournals.org"), Pattern.compile("/content" + ".*")));
+
+	public String getSupportedSiteName() {
+		return SITE_NAME;
+	}
+
+	public String getSupportedSiteURL() {
+		return SITE_URL;
+	}
+
+	public String getInfo() {
+		return INFO;
+	}
+
+	@Override
+	public Pattern getDownloadLinkPattern() {
+		return DOWNLOAD_LINK_PATTERN;
+	}
+
+	@Override
+	public List<Tuple<Pattern, Pattern>> getUrlPatterns() {
+		return URL_PATTERNS;
+	}
+}
+
---
+++ bibsonomy-scraper/src/test/java/org/bibsonomy/scraper/url/kde/ats/ATSScraperTest.java
@@ -0,0 +1,42 @@
+package org.bibsonomy.scraper.url.kde.ats;
+
+import static org.junit.Assert.assertTrue;
+
+import org.bibsonomy.scraper.UnitTestRunner;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * @author clemens
+ * @version $Id$
+ */
+public class ATSScraperTest {
+
+	/**
+	 * starts URL test with id url_212
+	 */
+	@Test
+	//@Ignore
+	public void urlTest1Run() {
+		UnitTestRunner runner = new UnitTestRunner();
+		assertTrue(runner.runSingleTest("url_212"));
+	}
+	/**
+	 * starts URL test with id url_213
+	 */
+	@Test
+	//@Ignore
+	public void urlTest2Run() {
+		UnitTestRunner runner = new UnitTestRunner();
+		assertTrue(runner.runSingleTest("url_213"));
+	}
+	/**
+	 * starts URL test with id url_214
+	 */
+	@Test
+	//@Ignore
+	public void urlTest3Run() {
+		UnitTestRunner runner = new UnitTestRunner();
+		assertTrue(runner.runSingleTest("url_214"));
+	}
+}
