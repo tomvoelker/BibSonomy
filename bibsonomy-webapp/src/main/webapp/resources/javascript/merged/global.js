525c1564e3741197fc79ad75c6f9d9c23c676bdc
Robert Jäschke <jaeschke@l3s.de>
2012-02-01 09:11:25 +0100

    - moved admin JavaScript into separate directory - removed unused JavaScript files

--- bibsonomy-webapp/src/main/webapp/resources/javascript/merged/global.js
+++ bibsonomy-webapp/src/main/webapp/resources/javascript/merged/global.js
@@ -21,7 +21,6 @@ includeJavaScript("/resources/javascript/style.js");
 includeJavaScript("/resources/jquery/plugins/textarearesizer/jquery.textarearesizer.js");
 includeJavaScript("/resources/jquery/plugins/corner/jquery.corner.js");
 includeJavaScript("/resources/jquery/plugins/ui/jquery-ui-1.8.16.custom.min.js");
-includeJavaScript("/resources/javascript/jquery.sidebarresizer.js");
 includeJavaScript("/resources/javascript/jquery.fadebox.js");
 includeJavaScript("/resources/javascript/userRelation.js");
 includeJavaScript("/resources/javascript/addToSpheres.js");
