180ff497a6d200a82a5684928df8b57601b2b204
Stephan Doerfel <doerfel@cs.uni-kassel.de>
2012-02-06 12:11:53 +0100

    added my

--- bibsonomy-batch/src/build/batch_scripts/repair_review_ratings_cache.pl
+++ bibsonomy-batch/src/build/batch_scripts/repair_review_ratings_cache.pl
@@ -22,10 +22,10 @@ my $master = get_master();
 #$master->prepare("LOCK TABLES discussion WRITE");
 
 # delete old data
-$stm_delete_old_ratings = $master->prepare("DELETE FROM review_ratings_cache");
+my $stm_delete_old_ratings = $master->prepare("DELETE FROM review_ratings_cache");
 
 # recalc the avg 
-$stm_insert_new_ratings = $master->prepare("INSERT INTO review_ratings_cache (interhash, rating_arithmetic_mean, number_of_ratings) SELECT interHash, avg(rating) as rating_arithmetic_mean, count(DISTINCT(user_name)) as number_of_ratings FROM discussion where type=1 AND `group` >= 0 GROUP BY interHash");
+my $stm_insert_new_ratings = $master->prepare("INSERT INTO review_ratings_cache (interhash, rating_arithmetic_mean, number_of_ratings) SELECT interHash, avg(rating) as rating_arithmetic_mean, count(DISTINCT(user_name)) as number_of_ratings FROM discussion where type=1 AND `group` >= 0 GROUP BY interHash");
 
 # execute
 $stm_delete_old_ratings->execute();
