c7ed9f2e8442217b558e6c75c262c219ca4782d3
Dominik Benz <benz@cs.uni-kassel.de>
2012-02-07 07:56:39 +0100

    extended range of parameter: ?duplicates=merged deduplicates a list of bibtex posts and merges all tags of duplicate posts within a single one

---
+++ bibsonomy-common/src/main/java/org/bibsonomy/common/enums/Duplicates.java
@@ -0,0 +1,16 @@
+package org.bibsonomy.common.enums;
+
+/**
+ * Different options to display duplicate posts.
+ * 
+ * @author dbe
+ * @version $Id$
+ */
+public enum Duplicates {	
+	/** display duplicates */
+	YES,
+	/** do not display duplicates */
+	NO,
+	/** display merged duplicates */
+	MERGED;
+}
