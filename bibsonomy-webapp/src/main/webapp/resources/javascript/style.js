989ac4105df89c44ce6fdc07729884e8118dacdd
Folke Mitzlaff <mitzlaff@cs.uni-kassel.de>
2012-02-07 11:22:16 +0100

    added infobox to layout

--- bibsonomy-webapp/src/main/webapp/resources/javascript/style.js
+++ bibsonomy-webapp/src/main/webapp/resources/javascript/style.js
@@ -345,7 +345,6 @@ function initBookmarksPublicationsListsLast() {
 		// set heights to maximum_heights
 		$("#bookmarks_0 ul.posts").height(maxheight);
 		$("#publications_0 ul.posts").height(maxheight);
-		$("#sidebar").height(maxheight);
 	}
 }
 
