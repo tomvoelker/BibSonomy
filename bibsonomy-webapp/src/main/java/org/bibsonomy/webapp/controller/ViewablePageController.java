1eaef16a7240b12d6b92139fa57f874e212b5fbb
Robert Jäschke <jaeschke@l3s.de>
2012-02-08 17:19:15 +0100

    important fix

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/ViewablePageController.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/ViewablePageController.java
@@ -58,8 +58,8 @@ public class ViewablePageController extends SingleResourceListControllerWithTags
 		// html format - retrieve tags and return HTML view
 		if ("html".equals(format)) {
 			// only fetch tags if they were not already fetched by handleTagsOnly
-			if (command.getTagstype() != null) {
-				this.setTags(command, Resource.class, groupingEntity, groupingName, null, null, null, 1000, null);
+			if (command.getTagstype() == null) {
+				this.setTags(command, Resource.class, groupingEntity, groupingName, null, null, null, Integer.MAX_VALUE, null);
 			}
 			this.setGroupDetails(command, groupingName);
 			
