134e86bda0386501741e1ab48f445d368e132ef9
Sven Stefani <sst@cs.uni-kassel.de>
2012-03-01 14:08:11 +0100

    more puma design changes

--- bibsonomy-webapp/src/main/java/de/unikassel/puma/webapp/controller/HomepageController.java
+++ bibsonomy-webapp/src/main/java/de/unikassel/puma/webapp/controller/HomepageController.java
@@ -5,9 +5,11 @@ import java.util.Arrays;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.bibsonomy.common.enums.GroupingEntity;
+import org.bibsonomy.common.enums.Role;
 import org.bibsonomy.model.Bookmark;
 import org.bibsonomy.model.Resource;
 import org.bibsonomy.webapp.command.HomepageCommand;
+import org.bibsonomy.webapp.command.ListCommand;
 import org.bibsonomy.webapp.controller.SingleResourceListController;
 import org.bibsonomy.webapp.util.MinimalisticController;
 import org.bibsonomy.webapp.util.View;
@@ -21,40 +23,59 @@ import org.bibsonomy.webapp.view.Views;
  * @version $Id$
  */
 public class HomepageController extends SingleResourceListController implements MinimalisticController<HomepageCommand> {
-	private static final int MAX_TAGS = 50;
+	private static final int POSTS_PER_RESOURCETYPE_LOGGED_IN = 20;
+
+	private static final int POSTS_PER_RESOURCETYPE = 5;
+
 	private static final Log log = LogFactory.getLog(HomepageController.class);
+
+	/*
+	 * on the homepage, only 50 tags are shown in the tag cloud
+	 */
+	private static final int MAX_TAGS = 50;
 
 	@Override
 	public View workOn(final HomepageCommand command) {
-		/*
-		 * FIXME: implement filter=no parameter
-		 */
 		log.debug(this.getClass().getSimpleName());
-		log.info("PUMA:" + this.getClass().getSimpleName());
 		final String format = command.getFormat();
 		this.startTiming(this.getClass(), format);
 		
 		// handle the case when only tags are requested
-		this.handleTagsOnly(command, GroupingEntity.ALL, null, null, null, null, MAX_TAGS, null);		
-		
+		this.handleTagsOnly(command, GroupingEntity.ALL, null, null, null, null, MAX_TAGS, null);
+				
 		// retrieve and set the requested resource lists
 		for (final Class<? extends Resource> resourceType : this.getListsToInitialize(format, command.getResourcetype())) {
-			// disable manual setting of start value for home page
-			command.getListCommand(resourceType).setStart(0);
-			setList(command, resourceType, GroupingEntity.ALL, null, null, null, null, null, null, command.getStartDate(), command.getEndDate(), 5);
+			// disable manual setting of start value for homepage
+			final ListCommand<?> listCommand = command.getListCommand(resourceType);
+			listCommand.setStart(0);
+			/*
+			 * logged in users see 20 posts, other users 5
+			 * admins may see as many posts as they like 
+			 */
+			final int entriesPerPage;
+			if (command.getContext().isUserLoggedIn()) {
+				if (Role.ADMIN.equals(command.getContext().getLoginUser().getRole())) {
+					entriesPerPage = listCommand.getEntriesPerPage();
+				} else {
+					entriesPerPage = POSTS_PER_RESOURCETYPE_LOGGED_IN;	
+				}
+			} else {
+				entriesPerPage = POSTS_PER_RESOURCETYPE;
+			}			
+			setList(command, resourceType, GroupingEntity.ALL, null, null, null, null, command.getFilter(), null, command.getStartDate(), command.getEndDate(), entriesPerPage);
 			postProcessAndSortList(command, resourceType);
 		}
 												
 		// html format - retrieve tags and return HTML view
 		if ("html".equals(format)) {
-			command.setPageTitle("home");
+			command.setPageTitle("home"); // TODO: i18n
 			setTags(command, Resource.class, GroupingEntity.ALL, null, null, null, null, MAX_TAGS, null);
-
+			
 			/*
 			 * add news posts (= latest blog posts) FIXME: make configurable
 			 */
 			command.setNews(this.logic.getPosts(Bookmark.class, GroupingEntity.GROUP, "puma", Arrays.asList("pumanews"), null, null, null, null, null, null, 0, 3));
-			
+		
 			this.endTiming();
 			return Views.PUMAHOMEPAGE;		
 		}
