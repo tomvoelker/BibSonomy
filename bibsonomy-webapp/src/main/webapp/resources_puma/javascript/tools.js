18c0e7bbfa5c5a5a20a2a2d23c49359c4596ad68
Sven Stefani <sst@cs.uni-kassel.de>
2012-03-06 18:34:19 +0100

    removed puma init (old sidebar help box)

--- bibsonomy-webapp/src/main/webapp/resources_puma/javascript/tools.js
+++ bibsonomy-webapp/src/main/webapp/resources_puma/javascript/tools.js
@@ -23,51 +23,6 @@ function replaceElementsTextnode(elname, t, color) {
 
 function pumainit(){
 	
-	// if no sidebar available, add sidebar into dom
-	
-	if ($("#sidebarroundcorner").attr("id") == null)
-	{
-		// add after id=outer
-		$("#outer").after('<div id="sidebarroundcorner" style="display: none;"></div>');
-	
-		// adjust width of outer, threrefore remove attribute style from element, only if sidebar is visible
-		$("#outer").attr("oldstyle", $("#outer").attr("style"));
-	}
-	
-	
-	// add help box as sidebox in front of #sidebarroundcorner's content
-	//$("#sidebarroundcorner").prepend('<div class="sidebarBoxOuter" id="helpbox"><div class="sidebarBoxInner"><div id="helpboxcontent" class="boxcontent"><div id="togglehelp" class="togglehelp closeX">x</div><div class="bc-head">Quicklinks</div><a href="/login">Loggen Sie sich jetzt mit Ihren Bibliotheksausweis ein.</a><br />CV anzeigen: Wählen Sie unter meinPUMA meinLebenslauf aus.<br /><div class="bc-head">Hilfe</div>Weitere Informationen erhalten sie auf den <a href="http://www.ub.uni-kassel.de/puma.html">Projektseiten</a>, in der Hilfe und den beiden Blogs <a href="http://puma-projekt.blogspot.com/">Puma-Projekt</a> und <a href="http://bibsonomy.blogspot.com/">Bibsonomy</a>.</div></div></div>');
-	$("#sidebarroundcorner").prepend('<div class="sidebarBoxOuter" id="helpbox"><div class="sidebarBoxInner"><div id="helpboxcontent" class="boxcontent"><div id="togglehelp" class="togglehelp closeX">x</div><div class="bc-head">Hilfe</div>Ausführliche Informationen erhalten sie in der <a href="http://puma.uni-kassel.de/help_de">Hilfe</a> und im <a href="http://blog.puma.uni-kassel.de/">PUMA-Blog</a>..</div></div></div>');
-	$('img#sidebarGrip').remove();
-	$('div#helpbox').before('<img id="sidebarGrip" border="0" src="/resources/image/grip.png" alt="a gripper"/>');
-	$("#sidebarroundcorner").SideBarResizer('img#sidebarGrip');
-	$(".sidebarBoxInner").corner("round 8px").parent().css('padding', '3px').corner("round 8px");
-	
-	$("#helpbox").toggle();
-
-	// help side box toggle
-	$("#nice_tnav").prepend('<div id="navitogglehelp" class="togglehelp">?</div>');
-	$(".togglehelp").click(function () {
-
-		if (($("#outer").attr("oldstyle") != null) || ($("#sidebarroundcorner").css("display")=="none")) {
-			if (($("#outer").attr("style") != null) || ($("#sidebarroundcorner").css("display")=="none")){
-				$("#outer").removeAttr("style");
-				$("#sidebarroundcorner").css("display", "block");
-				$("#helpbox").show();
-			} else {
-				$("#helpbox").hide();
-				$("#sidebarroundcorner").css("display", "none");
-				$("#outer").attr("style", $("#outer").attr("oldstyle"));
-			}
-		}
-		else
-		{
-			$("#helpbox").toggle();
-		}
-	})
-
-	
-	
 }
 	
 $(pumainit);    
