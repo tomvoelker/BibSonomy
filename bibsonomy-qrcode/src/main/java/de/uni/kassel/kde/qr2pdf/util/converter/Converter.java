7ef70531c217ec1d202627ba0b063c8939685620
derbeukatt <devnull@localhost>
2012-03-18 12:49:01 +0100

    Deleted old files from project.

--- bibsonomy-qrcode/src/main/java/de/uni/kassel/kde/qr2pdf/util/converter/Converter.java
+++
@@ -1,9 +0,0 @@
-package de.uni.kassel.kde.qr2pdf.util.converter;
-
-import java.awt.image.BufferedImage;
-
-public abstract class Converter {
-
-	public abstract BufferedImage convertToImage(String fileName) throws Exception;
-	
-}
--- bibsonomy-qrcode/src/main/java/de/uni/kassel/kde/qr2pdf/util/parser/Parser.java
+++
@@ -1,7 +0,0 @@
-package de.uni.kassel.kde.qr2pdf.util.parser;
-
-
-public abstract class Parser {
-
-	public abstract void parse(String inFile, String outFile) throws Exception;
-}
