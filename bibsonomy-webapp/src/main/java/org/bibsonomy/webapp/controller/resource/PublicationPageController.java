cd949f9ebd7d1a8b4c2a62d588c25aa842f2e985
Waldemar Lautenschlager <wla@cs.uni-kassel.de>
2012-03-21 13:50:22 +0100

    Added preprint entry type for publications and discussion handling for preprints

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/resource/PublicationPageController.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/resource/PublicationPageController.java
@@ -1,12 +1,17 @@
 package org.bibsonomy.webapp.controller.resource;
 
+import static org.bibsonomy.model.util.BibTexUtils.PREPRINT;
+import static org.bibsonomy.util.ValidationUtils.present;
+
 import java.util.List;
 import java.util.Map;
 
 import org.bibsonomy.common.enums.GroupingEntity;
 import org.bibsonomy.model.BibTex;
+import org.bibsonomy.model.DiscussionItem;
 import org.bibsonomy.model.GoldStandardPublication;
 import org.bibsonomy.model.Post;
+import org.bibsonomy.model.User;
 import org.bibsonomy.webapp.command.resource.PublicationPageCommand;
 import org.bibsonomy.webapp.command.resource.ResourcePageCommand;
 import org.bibsonomy.webapp.util.View;
@@ -55,5 +60,31 @@ public class PublicationPageController extends AbstractResourcePageController<Bi
 	@Override
 	protected Class<GoldStandardPublication> getGoldStandardClass() {
 		return GoldStandardPublication.class;
+	}
+
+	/**
+	 * Handles discussionItems for preprint entry type.
+	 * Remove this method and here and in parent class, if you want remove preprint entry type
+	 * @param goldStandard
+	 * @param loginUser
+	 */
+	@Override
+	protected void handleDiskussionItems(Post<GoldStandardPublication> goldStandard, User loginUser) {
+		
+		//if creating first discussion item on normal post
+		if (!present(goldStandard)) {
+			return;
+		}
+		
+		//remove all discussion if user self has not discussed
+		if(goldStandard.getResource().getEntrytype().equals(PREPRINT)) {
+			List<DiscussionItem> discussionItems = goldStandard.getResource().getDiscussionItems();
+			for (DiscussionItem item : discussionItems) {
+				if(item.getUser().equals(loginUser)) {
+					return;
+				}
+			}
+			discussionItems.clear();
+		}
 	}
 }
