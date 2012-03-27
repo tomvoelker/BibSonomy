9bdb7f0844011caed6ea048bd407a1623a6ee4da
derbeukatt <devnull@localhost>
2012-03-27 18:49:36 +0200

    Refactored code and added comments

--- bibsonomy-qrcode/src/main/java/org/bibsonomy/util/Point.java
+++ bibsonomy-qrcode/src/main/java/org/bibsonomy/util/Point.java
@@ -1,54 +1,92 @@
 package org.bibsonomy.util;
 
 /**
- * class to represent found free square.
+ * class to represent found free square point.
  * contains additional size of found free square.
  * 
  * @author pbu
- *
+ * @version $Id$
  */
-public class Point implements Comparable<Point>{
+public class Point implements Comparable<Point> {
 
+	/**
+	 * x coordinate of found free square
+	 */
 	private int x;
+	
+	/**
+	 * y coordinate of found free square
+	 */
 	private int y;
+	
+	/**
+	 * size of found free square
+	 */
 	private int size;
 
-	public Point(int x, int y, int size)
-	{
-		this.x = x;
-		this.y = y;
-		this.size = size;
+	/**
+	 * @param x X coordinate
+	 * @param y Y coordinate
+	 * @param size size of found free square
+	 */
+	public Point(int x, int y, int size) {
+		this.setX(x);
+		this.setY(y);
+		this.setSize(size);
 	}
 
+	/**
+	 * 
+	 * @return x coordinate of found free square
+	 */
 	public int getX() {
 		return x;
 	}
 
+	/**
+	 * 
+	 * @param x
+	 */
 	public void setX(int x) {
 		this.x = x;
 	}
 
+	/**
+	 * 
+	 * @return y coordinate of found free square
+	 */
 	public int getY() {
 		return y;
 	}
 
+	/**
+	 * 
+	 * @param y
+	 */
 	public void setY(int y) {
 		this.y = y;
 	}
 
+	/**
+	 * 
+	 * @return size of found free square
+	 */
 	public int getSize() {
 		return size;
 	}
 
+	/**
+	 * 
+	 * @param size
+	 */
 	public void setSize(int size) {
 		this.size = size;
 	}
-	
-	public String toString()
-	{
-		return "This point: " + this.getX() + "/" + this.getY() + "\tSize: " + this.getSize();
-	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see java.lang.Comparable#compareTo(java.lang.Object)
+	 */
 	@Override
 	public int compareTo(Point p) {
 		
@@ -66,3 +104,4 @@ public class Point implements Comparable<Point>{
 	}
 	
 }
+
--- bibsonomy-qrcode/src/main/java/org/bibsonomy/util/QRCodeCreator.java
+++ bibsonomy-qrcode/src/main/java/org/bibsonomy/util/QRCodeCreator.java
@@ -7,10 +7,30 @@ import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 
+/**
+ * class uses zxing qr code library to generate a qr code 
+ * based on the encodee. a preferred size is of the resulting image
+ * is also set.
+ * 
+ * @author pbu
+ * @version $Id$
+ */
 public class QRCodeCreator {
 	
+	/**
+	 * method takes encodee and preferred size and generates
+	 * the corresponding qr code
+	 * 
+	 * @param encodee the URL to encode
+	 * @param size preferred size of the resulting image
+	 * @return an image representation of the generated qr code
+	 * @throws WriterException if qr code could not be generated
+	 */
 	public static BufferedImage createQRCode(String encodee, int size) throws WriterException {
 		
+		/*
+		 * get write and encode encodee to bit matrix
+		 */
 		QRCodeWriter writer = new QRCodeWriter();
 		BitMatrix matrix = writer.encode(encodee, BarcodeFormat.QR_CODE, size, size);
 		
@@ -19,10 +39,13 @@ public class QRCodeCreator {
 		
 		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
 		
+		/*
+		 * convert bit matrix to buffered image
+		 */
 		for (int x = 0; x < width; x++) {
-		  for (int y = 0; y < height; y++) {
-		    image.setRGB(x, y, matrix.get(x, y) ? SquareFinder.BLACK : SquareFinder.WHITE);
-		  }
+			for (int y = 0; y < height; y++) {
+				image.setRGB(x, y, matrix.get(x, y) ? SquareFinder.BLACK : SquareFinder.WHITE);
+			}
 		}
 		
 		return image;
