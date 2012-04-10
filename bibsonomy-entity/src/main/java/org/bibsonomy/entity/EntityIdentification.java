96056961e4f21d64e8659c5d39f0595e8e259106
rkr <devnull@localhost>
2012-04-10 17:36:02 +0200

    *** empty log message ***

--- bibsonomy-entity/src/main/java/org/bibsonomy/entity/EntityIdentification.java
+++ bibsonomy-entity/src/main/java/org/bibsonomy/entity/EntityIdentification.java
@@ -81,54 +81,115 @@ public class EntityIdentification {
 			e.printStackTrace();
 		}
 				
+		//count the first and last names and define the name of the user as the most counted first and last name
 		String userName = null;
 		int userNameCount = 0;
 		HashMap<String,Integer> saveFirstNameCount = new HashMap<String,Integer>();
 		HashMap<String,Integer> saveLastNameCount = new HashMap<String,Integer>();
+		List<String> firstNamesAlreadyCounted = new ArrayList<String>();
+		List<String> lastNamesAlreadyCounted = new ArrayList<String>();
+		List<String> publicationsForThisUsername = new ArrayList<String>();
 		for(int k=0; k < myOwnAuthorList.size(); k++) {
-			//TODO count  the name only one time for each publication e.g. Alexander X. and Alexaner Y.
+			publicationsForThisUsername.add(myOwnAuthorList.get(k).get("author"));
 			List<PersonName> authors = PersonNameUtils.discoverPersonNames(myOwnAuthorList.get(k).get("author"));
+			//count the name only one time for each publication e.g. Alexander X. and Alexander Y. shouldnt be the same person
+			firstNamesAlreadyCounted.clear();
+			lastNamesAlreadyCounted.clear();
 			for(PersonName author: authors) {
 				int countFirstName = 0, countLastName = 0;
 				if (saveFirstNameCount.containsKey(author.getFirstName())) {
-					countFirstName = saveFirstNameCount.get(author.getFirstName());
-					saveFirstNameCount.put(author.getFirstName(),++countFirstName);
+					if (!firstNamesAlreadyCounted.contains(author.getFirstName())) {
+						countFirstName = saveFirstNameCount.get(author.getFirstName());
+						saveFirstNameCount.put(author.getFirstName(),++countFirstName);
+						firstNamesAlreadyCounted.add(author.getFirstName());
+					}
 				}
-				else saveFirstNameCount.put(author.getFirstName(),1);
+				else {
+					saveFirstNameCount.put(author.getFirstName(),1);
+					firstNamesAlreadyCounted.add(author.getFirstName());
+				}
 				
 				if (saveLastNameCount.containsKey(author.getLastName())) {
-					countLastName = saveLastNameCount.get(author.getLastName());
-					saveLastNameCount.put(author.getLastName(),++countLastName);
+					if (!lastNamesAlreadyCounted.contains(author.getLastName())) {
+						countLastName = saveLastNameCount.get(author.getLastName());
+						saveLastNameCount.put(author.getLastName(),++countLastName);
+						lastNamesAlreadyCounted.add(author.getLastName());
+					}
 				}
-				else saveLastNameCount.put(author.getLastName(),1);				
+				else {
+					saveLastNameCount.put(author.getLastName(),1);
+					lastNamesAlreadyCounted.add(author.getLastName());
+				}
 			}
 			
-			if (userName == null) userName =  myOwnAuthorList.get(k).get("user_name");
-			else if(!myOwnAuthorList.get(k).get("user_name").equals(userName)) {
+			if (userName == null) {
+				System.out.println("user_name: " + myOwnAuthorList.get(k).get("user_name"));
+				userName = myOwnAuthorList.get(k).get("user_name");
+			}
+			else if(!myOwnAuthorList.get(k).get("user_name").equals(userName)) { //here starts a new userName
 				if (userNameCount > 1) {
-					System.out.println("user_name: " + myOwnAuthorList.get(k).get("user_name"));
 					System.out.println("---firstName:---");
-					String nameWithMostCounts = null;
-					int nameCount = 0;
+					String firstNameWithMostCounts = null, lastNameWithMostCounts = null;
+					int firstNameCount = 0, lastNameCount = 0;
 					for( Map.Entry<String, Integer> e : saveFirstNameCount.entrySet()) {
-						if (e.getValue() > nameCount) {
-							nameWithMostCounts = e.getKey();
-							nameCount = e.getValue();
+						if (e.getValue() > firstNameCount) {
+							firstNameWithMostCounts = e.getKey();
+							firstNameCount = e.getValue();
 						}
 						//System.out.println(e.getKey() + ": " + e.getValue());
 					}
-					System.out.println("winner is: " + nameWithMostCounts + " with " + nameCount);
+					System.out.println("winner is: " + firstNameWithMostCounts + " with " + firstNameCount);
 					System.out.println("---lastName:---");
-					nameWithMostCounts = null;
-					nameCount = 0;
+					lastNameWithMostCounts = null;
+					lastNameCount = 0;
 					for( Map.Entry<String, Integer> e : saveLastNameCount.entrySet()) {
-						if (e.getValue() > nameCount) {
-							nameWithMostCounts = e.getKey();
-							nameCount = e.getValue();
+						if (e.getValue() > lastNameCount) {
+							lastNameWithMostCounts = e.getKey();
+							lastNameCount = e.getValue();
 						}
 						//System.out.println(e.getKey() + ": " + e.getValue());
 					}
-					System.out.println("winner is: " + nameWithMostCounts + " with " + nameCount);
+					System.out.println("winner is: " + lastNameWithMostCounts + " with " + lastNameCount);
+					
+					//now we think we know the real name and we can check all the names the person is listed in the database with
+					String realName = null;
+					boolean firstNameTest = true;
+					if (lastNameCount > firstNameCount) {
+						realName = lastNameWithMostCounts;
+						firstNameTest = false;
+					}
+					else realName = firstNameWithMostCounts;
+					
+					HashMap<String,Integer> otherNamesOfThisAuthor = new HashMap<String,Integer>();
+					List<PersonName> tempAuthorNames = null;
+					publicationsForThisUsername.remove(publicationsForThisUsername.size()-1); //remove the last element
+					for (String tempAuthorsList: publicationsForThisUsername) { //go through the publications for this userName
+						tempAuthorNames = PersonNameUtils.discoverPersonNames(tempAuthorsList);
+					}
+					for (PersonName authorName: tempAuthorNames) { //check if there is an author with the same first/last name in the publication
+						if(firstNameTest) { //save other lastNames
+							if (authorName.getFirstName() != null) {
+								if(authorName.getFirstName().equals(realName)) {
+									int count = 1;
+									if (otherNamesOfThisAuthor.containsKey(authorName.getLastName())) count = otherNamesOfThisAuthor.get(authorName.getLastName());
+									otherNamesOfThisAuthor.put(authorName.getLastName(), count);
+								}
+							}
+						}
+						else { //save other firstNames
+							if (authorName.getLastName() != null) {
+								if(authorName.getLastName().equals(realName)) {
+									int count = 1;
+									if(otherNamesOfThisAuthor.containsKey(authorName.getFirstName())) count = otherNamesOfThisAuthor.get(authorName.getFirstName());
+									otherNamesOfThisAuthor.put(authorName.getFirstName(), count);
+								}
+							}
+						}
+					}
+					System.out.println("other names of this person are:");
+					for( Map.Entry<String, Integer> e : otherNamesOfThisAuthor.entrySet()) {
+						System.out.println(e.getKey() + " count: " + e.getValue());
+					}
 				}
 					
 				//delete the name counts
@@ -136,10 +197,10 @@ public class EntityIdentification {
 				userName = null;
 				saveFirstNameCount.clear();
 				saveLastNameCount.clear();
+				publicationsForThisUsername.clear();
 			}
 			else userNameCount++;
 		}
-		System.out.println("ende");
 		System.exit(1);
 		
 		
