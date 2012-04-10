183214ef58696c3b2e44377b40531392cccfe90f
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2012-04-10 10:31:49 +0200

    moved pair to commom module
    to replace the tuple class and all other pair classes in the project :)

---
+++ bibsonomy-common/src/main/java/org/bibsonomy/common/Pair.java
@@ -0,0 +1,105 @@
+package org.bibsonomy.common;
+
+/**
+ *  
+ * @author ?
+ * @version $Id$
+ * 
+ * @param <T>
+ * @param <U>
+ */
+public class Pair <T, U> {
+	private T first;
+	private U second;
+
+	/**
+	 * default constructor
+	 */
+	public Pair() {
+	}
+
+	/**
+	 * build constructor
+	 * 
+	 * @param first
+	 * @param second
+	 */
+	public Pair( final T first, final U second ) {
+		this.first = first;
+		this.second = second;
+	}
+
+	/**
+	 * @return the first
+	 */
+	public T getFirst() {
+		return first;
+	}
+
+	/**
+	 * @param first the first to set
+	 */
+	public void setFirst(final T first) {
+		this.first = first;
+	}
+
+	/**
+	 * @return the second
+	 */
+	public U getSecond() {
+		return second;
+	}
+
+	/**
+	 * @param second the second to set
+	 */
+	public void setSecond(final U second) {
+		this.second = second;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((first == null) ? 0 : first.hashCode());
+		result = prime * result + ((second == null) ? 0 : second.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof Pair<?,?>)) {
+			return false;
+		}
+		final Pair<?,?> other = (Pair<?,?>) obj;
+		if (first == null) {
+			if (other.first != null) {
+				return false;
+			}
+		} else if (!first.equals(other.first)) {
+			return false;
+		}
+		if (second == null) {
+			if (other.second != null) {
+				return false;
+			}
+		} else if (!second.equals(other.second)) {
+			return false;
+		}
+		return true;
+	}
+
+
+} 
