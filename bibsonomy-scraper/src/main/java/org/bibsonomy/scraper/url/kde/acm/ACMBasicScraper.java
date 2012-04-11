b98ad5eec8fa2c7c574cd07517753d90de176c3d
BibSonomy <webmaster@bibsonomy.org>
2012-04-11 15:54:08 +0200

    Tuple heißt jetzt Pair

--- bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/acm/ACMBasicScraper.java
+++ bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/acm/ACMBasicScraper.java
@@ -36,10 +36,10 @@ import java.util.regex.Pattern;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.bibsonomy.common.Pair;
 import org.bibsonomy.model.util.BibTexUtils;
 import org.bibsonomy.scraper.AbstractUrlScraper;
 import org.bibsonomy.scraper.ScrapingContext;
-import org.bibsonomy.scraper.Tuple;
 import org.bibsonomy.scraper.exceptions.InternalFailureException;
 import org.bibsonomy.scraper.exceptions.ScrapingException;
 import org.bibsonomy.scraper.exceptions.ScrapingFailureException;
@@ -62,7 +62,7 @@ public class ACMBasicScraper extends AbstractUrlScraper {
 	private static final String SITE_URL  = "http://portal.acm.org/";
 	private static final String info      = "This scraper parses a publication page from the " + href(SITE_URL, SITE_NAME);
 
-	private static final List<Tuple<Pattern, Pattern>> patterns = Collections.singletonList(new Tuple<Pattern, Pattern>(
+	private static final List<Pair<Pattern, Pattern>> patterns = Collections.singletonList(new Pair<Pattern, Pattern>(
 			Pattern.compile(".*" + "[(portal)(dl)].acm.org"), 
 			Pattern.compile("(/beta)?/citation.cfm.*")
 	));
@@ -211,7 +211,7 @@ public class ACMBasicScraper extends AbstractUrlScraper {
 	}
 
 	@Override
-	public List<Tuple<Pattern, Pattern>> getUrlPatterns() {
+	public List<Pair<Pattern, Pattern>> getUrlPatterns() {
 		return patterns; 
 	}
 
