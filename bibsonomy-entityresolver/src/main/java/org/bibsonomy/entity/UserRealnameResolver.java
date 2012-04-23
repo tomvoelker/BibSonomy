10381b9f22c8c56496fd61fb8c9be54d7b4d0890
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2012-04-23 12:50:16 +0200

    added catch for general exception while creating the index for disabling the index

--- bibsonomy-entityresolver/src/main/java/org/bibsonomy/entity/UserRealnameResolver.java
+++ bibsonomy-entityresolver/src/main/java/org/bibsonomy/entity/UserRealnameResolver.java
@@ -1,18 +1,21 @@
 package org.bibsonomy.entity;
 
+import static org.bibsonomy.util.ValidationUtils.present;
+
 import java.io.IOException;
 import java.nio.channels.OverlappingFileLockException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.SortedSet;
 
 import no.priv.garshol.duke.ConfigLoader;
 import no.priv.garshol.duke.Configuration;
+import no.priv.garshol.duke.DataSource;
 import no.priv.garshol.duke.Database;
 import no.priv.garshol.duke.Processor;
-import no.priv.garshol.duke.DataSource;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -27,6 +30,7 @@ import org.xml.sax.SAXException;
  * class for matching user objects based on their real names
  * 
  * @author fei
+ * @version $Id$
  */
 public class UserRealnameResolver {
 	private final Log log = LogFactory.getLog(UserRealnameResolver.class);
@@ -54,12 +58,14 @@ public class UserRealnameResolver {
 			config.setPath(this.indexPath);
 			this.userIndex = config.createDatabase(false);
 			this.userIndex.openSearchers(); 
-		} catch (OverlappingFileLockException e) {
+		} catch (final OverlappingFileLockException e) {
 			log.error("Error opening user name index for the Facebook importer: " + e.getMessage());
-		} catch (IOException e) {
-			log.error("Error accessing config file '+CONFIG_PATH+'", e);
-		} catch (SAXException e) {
-			log.error("Error parsing config file '+CONFIG_PATH+'", e);
+		} catch (final IOException e) {
+			log.error("Error accessing config file '" + CONFIG_PATH + "'", e);
+		} catch (final SAXException e) {
+			log.error("Error parsing config file '" + CONFIG_PATH + "'", e);
+		} catch (final Exception e) {
+			log.warn("error while creating database, disabling index", e);
 		}
 	}
 	
@@ -67,31 +73,41 @@ public class UserRealnameResolver {
 	 * build a new user resolution index
 	 */
 	public void buildIndex() {
+		if (this.isDisabled()) {
+			return;
+		}
 		try {
 			final Processor proc = new Processor(config, this.userIndex);
 			proc.close();
 			proc.index(config.getDataSources(), INDEX_BLOCK_SIZE);
-		} catch (CorruptIndexException e) {
+		} catch (final CorruptIndexException e) {
 			log.error("Corrupt duke index.", e);
-		} catch (IOException e) {
+		} catch (final IOException e) {
 			log.error("Error accessing duke index.", e);
 		}
 		
 	}
 	
+	private boolean isDisabled() {
+		return userIndex == null;
+	}
+
 	/**
 	 * resolve given users against the entity linkage index
 	 * 
 	 * @param others
 	 * @return
 	 */
-	public Map<String, Collection<User>> resolveUsers(Collection<User> others)  {
-		/**
+	public Map<String, Collection<User>> resolveUsers(final Collection<User> others)  {
+		if (!present(this.userIndex)) {
+			return Collections.emptyMap();
+		}
+		/*
 		 * matcher for resolving identities  
 		 */
-		UserEntityMatcher matcher = new UserEntityMatcher();
+		final UserEntityMatcher matcher = new UserEntityMatcher();
 
-		/** 
+		/* 
 		 * main record linking engine
 		 */
 		Processor proc;
@@ -102,25 +118,25 @@ public class UserRealnameResolver {
 		try {
 			proc = new Processor(config, this.userIndex);
 			proc.addMatchListener(matcher);
-			UserDataSource newEntries = new UserDataSource(others);
-			Collection<DataSource> linkGroup = new ArrayList<DataSource>();
+			final UserDataSource newEntries = new UserDataSource(others);
+			final Collection<DataSource> linkGroup = new ArrayList<DataSource>();
 			linkGroup.add(newEntries);
 			proc.linkRecords(linkGroup);
-		} catch (CorruptIndexException e) {
+		} catch (final CorruptIndexException e) {
 			log.error("Corrupt duke index.", e);
-		} catch (IOException e) {
+		} catch (final IOException e) {
 			log.error("Error accessing duke index.", e);
 		}
 
 		//
 		// build output data structure
 		//
-		Map<String, Collection<User>> retVal = new HashMap<String, Collection<User>>();
-		for (Map.Entry<String,SortedSet<UserMatch>> match : matcher.getMatching().entrySet()) {
-			String extId = match.getKey();
-			Collection<User> matches = new ArrayList<User>();
-			for (UserMatch other : match.getValue()) {
-				User importUser = new User();
+		final Map<String, Collection<User>> retVal = new HashMap<String, Collection<User>>();
+		for (final Map.Entry<String,SortedSet<UserMatch>> match : matcher.getMatching().entrySet()) {
+			final String extId = match.getKey();
+			final Collection<User> matches = new ArrayList<User>();
+			for (final UserMatch other : match.getValue()) {
+				final User importUser = new User();
 				importUser.setName(other.getProperty("user_name"));
 				importUser.setRealname(other.getId());
 				matches.add(importUser);
@@ -131,11 +147,10 @@ public class UserRealnameResolver {
 		return retVal;
 	}
 
-	public void setIndexPath(String indexPath) {
+	/**
+	 * @param indexPath the indexPath to set
+	 */
+	public void setIndexPath(final String indexPath) {
 		this.indexPath = indexPath;
-	}
-
-	public String getIndexPath() {
-		return indexPath;
 	}
 }
