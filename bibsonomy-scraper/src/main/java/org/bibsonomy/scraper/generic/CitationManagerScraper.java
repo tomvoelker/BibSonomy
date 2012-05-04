fd549ea3cdd54022a04d3480eafe44bc3cbce162
Haile Misgna Haile <haile.misgna@mu.edu.et>
2012-05-04 09:18:52 +0200

    reverted

--- bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/generic/CitationManagerScraper.java
+++ bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/generic/CitationManagerScraper.java
@@ -28,15 +28,12 @@ import java.net.URL;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.bibsonomy.model.util.BibTexUtils;
 import org.bibsonomy.scraper.AbstractUrlScraper;
 import org.bibsonomy.scraper.ScrapingContext;
 import org.bibsonomy.scraper.exceptions.InternalFailureException;
 import org.bibsonomy.scraper.exceptions.ScrapingException;
 import org.bibsonomy.scraper.exceptions.ScrapingFailureException;
 import org.bibsonomy.util.WebUtils;
-
-import bibtex.parser.ParseException;
 
 /**
  * Superclass for scraping pages, using the same system like PNAS, RSOC or ScienceMag.
@@ -55,19 +52,26 @@ public abstract class CitationManagerScraper extends AbstractUrlScraper {
 	protected boolean scrapeInternal(ScrapingContext sc) throws ScrapingException {
 		sc.setScraper(this);
 		try {
-			String downloadLink = buildDownloadLink(sc.getUrl(), WebUtils.getContentAsString(sc.getUrl()));
-			
+			final String content = WebUtils.getContentAsString(sc.getUrl());
+
+			// get link to download page
+			final Matcher downloadLinkMatcher = getDownloadLinkPattern().matcher(content);
+			final String downloadLink;
+			if(downloadLinkMatcher.find()) // add type=bibtex to the end of the link
+				downloadLink = "http://" + sc.getUrl().getHost() + downloadLinkMatcher.group(1) + "&type=bibtex";
+			else
+				throw new ScrapingFailureException("Download link is not available");
+
 			// download bibtex directly
-			final String bibtexMessage = WebUtils.getContentAsString(new URL(downloadLink));
-			if (bibtexMessage != null) {
-				try {
-					String bibtex = BibTexUtils.addBibtexKeyIfNotPresent(bibtexMessage);
-					bibtex = BibTexUtils.addFieldIfNotContained(bibtex, "url", sc.getUrl().toExternalForm());
-					sc.setBibtexResult(bibtex);
-					return true;
-				} catch (ParseException ex) {
-					throw new ScrapingException(ex);
-				}
+			final String bibtex = WebUtils.getContentAsString(new URL(downloadLink));
+			if (bibtex != null) {
+				// clean up (whitespaces in bibtex key)
+				final int indexOfComma = bibtex.indexOf(",");
+				
+				final String key = bibtex.substring(0, indexOfComma).replaceAll("\\s", "");
+				final String rest = bibtex.substring(indexOfComma);				
+				sc.setBibtexResult(key + rest);
+				return true;
 			}
 
 		} catch (IOException ex) {
@@ -75,18 +79,6 @@ public abstract class CitationManagerScraper extends AbstractUrlScraper {
 		}
 
 		return false;
-	}
-	
-	protected String buildDownloadLink(URL url, String content) throws ScrapingFailureException {
-
-		// get link to download page
-		final Matcher downloadLinkMatcher = getDownloadLinkPattern().matcher(content);
-		final String downloadLink;
-		if(downloadLinkMatcher.find()) // add type=bibtex to the end of the link
-			downloadLink = "http://" + url.getHost() + downloadLinkMatcher.group(1) + "&type=bibtex";
-		else
-			throw new ScrapingFailureException("Download link is not available");
-		return downloadLink;
 	}
 
 }
