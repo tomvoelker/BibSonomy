e6b8edd68eef58d89758849afab4b6d35ed6833f
Waldemar Lautenschlager <wla@cs.uni-kassel.de>
2012-05-07 15:25:35 +0200

    CSL engine for discussion links

--- bibsonomy-layout/src/main/java/org/bibsonomy/layout/csl/CslModelConverter.java
+++ bibsonomy-layout/src/main/java/org/bibsonomy/layout/csl/CslModelConverter.java
@@ -23,10 +23,13 @@
 
 package org.bibsonomy.layout.csl;
 
+import static org.bibsonomy.util.ValidationUtils.present;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.bibsonomy.layout.csl.model.Date;
 import org.bibsonomy.layout.csl.model.Person;
 import org.bibsonomy.layout.csl.model.Record;
 import org.bibsonomy.model.BibTex;
@@ -41,103 +44,111 @@ import org.bibsonomy.model.Resource;
  * @author Dominik Benz, benz@cs.uni-kassel.de
  * @version $Id$
  */
-public class CslModelConverter  {
-    
-    // bibtex -> csl
-    private static Map<String, String> typemap;
-    
-    static {
-	// see http://xbiblio-devel.2463403.n2.nabble.com/Citeproc-json-data-input-specs-td5135372.html
-	// FIXME: this is incomplete!
-	typemap = new HashMap<String, String>();
-	typemap.put("article", "article-journal");
-	typemap.put("book", "book");
-	typemap.put("booklet", "pamphlet");
-	typemap.put("inbook", "chapter");
-	typemap.put("incollection", "chapter");
-	typemap.put("phdthesis", "thesis");
-	typemap.put("mastersthesis", "thesis");
-	typemap.put("report", "techreport");
-	typemap.put("inproceedings", "chapter");
-    }
-    
+public class CslModelConverter {
 
-    public static List<Post<? extends Resource>> convertEntries(List<Record> entries) {
-	// todo
-	return null;
-    }
-    
-    public static Post<? extends Resource> convertEntry(Record entry) {
-	// todo
-	return null;
-    }
-    
-    public static List<Record> convertPosts(List<Post<? extends Resource>> posts) {
-	//todo
-	return null;
-    }
-    
-    /**
-     * Convert a bibtex post into a CSL record.
-     * 
-     * @param post - the bibtex post
-     * @return the corresponding CSL model
-     */
-    public static Record convertPost(Post<? extends Resource> post) {
-	Record rec = new Record();
-	BibTex bib = (BibTex) post.getResource();
-	//id
-	rec.setId(createId(post));
-	//type
-	rec.setType(mapToCslType(bib.getType()));
-	// authors, editors
-	for (PersonName author : bib.getAuthor()) {
-	    Person a = new Person();
-	    a.setGiven(author.getFirstName());
-	    a.setFamily(author.getLastName());
-	    rec.getAuthor().add(a);
-	}	
-	for (PersonName author : bib.getEditor()) {
-	    Person a = new Person();
-	    a.setGiven(author.getFirstName());
-	    a.setFamily(author.getLastName());
-	    rec.getEditor().add(a);
+	// bibtex -> csl
+	private static Map<String, String> typemap;
+
+	static {
+		// see http://xbiblio-devel.2463403.n2.nabble.com/Citeproc-json-data-input-specs-td5135372.html
+		// FIXME: this is incomplete!
+		typemap = new HashMap<String, String>();
+		typemap.put("article", "article-journal");
+		typemap.put("book", "book");
+		typemap.put("booklet", "pamphlet");
+		typemap.put("inbook", "chapter");
+		typemap.put("incollection", "chapter");
+		typemap.put("phdthesis", "thesis");
+		typemap.put("mastersthesis", "thesis");
+		typemap.put("report", "techreport");
+		typemap.put("inproceedings", "chapter");
 	}
-	
-	// fields a-z (FIXME: this is a rather tentative mapping!)
-	rec.setAbstractt(bib.getAbstract());
-	rec.setCitation_label(bib.getBibtexKey());
-	rec.setCollection_title(bib.getBooktitle());
-	rec.setContainer_title(bib.getBooktitle());
-	rec.setDOI(bib.getMiscField("doi"));
-	rec.setEvent_place(bib.getAddress());
-	rec.setISBN(bib.getMiscField("isbn"));
-	rec.setNote(bib.getNote());
-	rec.setPage(bib.getPages());
-	rec.setPublisher(bib.getPublisher());
-	rec.setTitle(bib.getTitle());
-	rec.setURL(bib.getUrl());
-	rec.setVolume(bib.getVolume());	
-	return rec;
-    }
-    
-    /**
-     * create ID for a post
-     * @param post - the post
-     * @return the ID
-     */
-    private static final String createId(final Post<? extends Resource> post) {
-	return ((BibTex) post.getResource()).getIntraHash() + post.getUser().getName();
-    }
-    
-    
-    /**
-     * get the mapping of bibtex types to CSL types.
-     * @param bibtexType - the bibtex entrytype
-     * @return the corresponding csl type
-     */
-    private static final String mapToCslType(final String bibtexType) {
-	return typemap.get(bibtexType);
-    }
-    
+
+	public static List<Post<? extends Resource>> convertEntries(final List<Record> entries) {
+		// todo
+		return null;
+	}
+
+	public static Post<? extends Resource> convertEntry(final Record entry) {
+		// todo
+		return null;
+	}
+
+	public static List<Record> convertPosts(final List<Post<? extends Resource>> posts) {
+		// todo
+		return null;
+	}
+
+	/**
+	 * Convert a bibtex post into a CSL record.
+	 * 
+	 * @param post - the bibtex post
+	 * @return the corresponding CSL model
+	 */
+	public static Record convertPost(final Post<? extends Resource> post) {
+		final Record rec = new Record();
+		final BibTex bib = (BibTex) post.getResource();
+		// id
+		rec.setId(createId(post));
+		// type
+		rec.setType(mapToCslType(bib.getType()));
+
+		// authors, editors
+		if (present(bib.getAuthor())) {
+			for (final PersonName author : bib.getAuthor()) {
+				final Person a = new Person();
+				a.setGiven(author.getFirstName());
+				a.setFamily(author.getLastName());
+				rec.getAuthor().add(a);
+			}
+		}
+		if (present(bib.getEditor())) {
+			for (final PersonName author : bib.getEditor()) {
+				final Person a = new Person();
+				a.setGiven(author.getFirstName());
+				a.setFamily(author.getLastName());
+				rec.getEditor().add(a);
+			}
+		}
+
+		// fields a-z (FIXME: this is a rather tentative mapping!)
+		rec.setAbstractt(bib.getAbstract());
+		rec.setCitation_label(bib.getBibtexKey());
+		rec.setCollection_title(bib.getBooktitle());
+		rec.setContainer_title(bib.getBooktitle());
+		rec.setDOI(bib.getMiscField("doi"));
+		rec.setEvent_place(bib.getAddress());
+		rec.setISBN(bib.getMiscField("isbn"));
+		rec.setNote(bib.getNote());
+		rec.setPage(bib.getPages());
+		rec.setPublisher(bib.getPublisher());
+		rec.setTitle(bib.getTitle());
+		rec.setURL(bib.getUrl());
+		rec.setVolume(bib.getVolume());
+
+		final Date date = new Date();
+		date.setLiteral(bib.getYear());
+		rec.setIssued(date);
+
+		return rec;
+	}
+
+	/**
+	 * create ID for a post
+	 * @param post - the post
+	 * @return the ID
+	 */
+	private static final String createId(final Post<? extends Resource> post) {
+		return ((BibTex) post.getResource()).getIntraHash() + post.getUser().getName();
+	}
+
+	/**
+	 * get the mapping of bibtex types to CSL types.
+	 * @param bibtexType - the bibtex entrytype
+	 * @return the corresponding csl type
+	 */
+	private static final String mapToCslType(final String bibtexType) {
+		return typemap.get(bibtexType);
+	}
+
 }
