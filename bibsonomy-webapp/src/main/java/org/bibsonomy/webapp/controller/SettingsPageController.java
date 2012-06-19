ab3b12dd63dccf2e721fef2df95797dfb3e20e73
janus <devnull@localhost>
2012-06-19 15:17:03 +0200

    - moved the admin panel for the CV wiki to the settings page - removed the admin panel from the public cv wiki page

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/SettingsPageController.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/SettingsPageController.java
@@ -16,10 +16,12 @@ import org.bibsonomy.model.Document;
 import org.bibsonomy.model.Group;
 import org.bibsonomy.model.User;
 import org.bibsonomy.model.UserSettings;
+import org.bibsonomy.model.Wiki;
 import org.bibsonomy.model.logic.LogicInterface;
 import org.bibsonomy.model.sync.SyncService;
 import org.bibsonomy.model.util.UserUtils;
 import org.bibsonomy.webapp.command.SettingsViewCommand;
+import org.bibsonomy.webapp.exceptions.MalformedURLSchemeException;
 import org.bibsonomy.webapp.util.ErrorAware;
 import org.bibsonomy.webapp.util.MinimalisticController;
 import org.bibsonomy.webapp.util.RequestAware;
@@ -27,6 +29,8 @@ import org.bibsonomy.webapp.util.RequestLogic;
 import org.bibsonomy.webapp.util.View;
 import org.bibsonomy.webapp.util.spring.security.exceptions.AccessDeniedNoticeException;
 import org.bibsonomy.webapp.view.Views;
+import org.bibsonomy.wiki.CVWikiModel;
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.validation.Errors;
 
 /**
@@ -44,12 +48,15 @@ public class SettingsPageController implements MinimalisticController<SettingsVi
 	protected LogicInterface logic;
 	protected RequestLogic requestLogic;
 
+	private CVWikiModel wikiRenderer;
+
 	/**
 	 * @param command
 	 * @return the view
 	 */
 	@Override
 	public View workOn(final SettingsViewCommand command) {
+		
 		if (!command.getContext().isUserLoggedIn()) {
 			throw new AccessDeniedNoticeException("please log in", "error.general.login");
 		}
@@ -93,6 +100,9 @@ public class SettingsPageController implements MinimalisticController<SettingsVi
 		case 4:	// sync tab
 			workOnSyncSettingsTab(command);
 			break;
+		case 5: // cv tab
+			workOnCVTab(command);
+			break;
 		default:
 			errors.reject("error.settings.tab");
 			break;
@@ -100,7 +110,6 @@ public class SettingsPageController implements MinimalisticController<SettingsVi
 
 		return Views.SETTINGSPAGE;
 	}
-
 
 	/**
 	 * checks whether the user has already uploaded jabref layout definitions
@@ -229,6 +238,110 @@ public class SettingsPageController implements MinimalisticController<SettingsVi
 	@Override
 	public void setRequestLogic(final RequestLogic requestLogic) {
 		this.requestLogic = requestLogic;
+	}
+
+
+
+	private void workOnCVTab(final SettingsViewCommand command) {
+		log.debug("cvPageController accessed.");
+
+		try {
+			final String requestedUser = command.getContext().getLoginUser().getName();
+			final Group requestedGroup = this.logic.getGroupDetails(requestedUser);
+			/* Check if the group is present. If it should be a user. If its no
+			   user the we will catch the exception and return an error message
+			   to the user. */
+			if (present(requestedGroup)) {
+				handleGroupCV(this.logic.getGroupDetails(requestedUser), command);
+			} else {
+				handleUserCV(this.logic.getUserDetails(requestedUser), command);
+			}
+		} catch (RuntimeException e) {
+			//If the name does not fit to anything a runtime exception is thrown while attempting to get the requestedUser
+			throw new MalformedURLSchemeException("Something went wrong! You are most likely looking for a non existant user/group.");
+		} catch (Exception e) {
+			throw new MalformedURLSchemeException("Something went wrong while working on your request. Please try again.");
+		}
+	}
+
+
+	 /** Handles the group cv page request
+	 * 
+	 * @param reqGroup
+	 * @param command
+	 * @return The group-cv-page view
+	 */
+	private void handleGroupCV(final Group requestedGroup, final SettingsViewCommand command) {
+		final String groupName = requestedGroup.getName();
+		command.setIsGroup(true);
+
+		final List<User> groupUsers = this.logic.getUsers(null, GroupingEntity.GROUP, groupName, null, null, null, null, null, 0, 1000);
+		requestedGroup.setUsers(groupUsers);
+
+		//this.setTags(command, Resource.class, GroupingEntity.GROUP, requestedGroup.getName(), null,  null, null, 1000, null);
+
+		final Wiki wiki = this.logic.getWiki(groupName, null);
+		final String wikiText;
+
+		if (present(wiki)) {
+			wikiText = wiki.getWikiText();
+		} else {
+			wikiText = "";
+		}
+
+		/*
+		 * set the group to render
+		 */
+		this.wikiRenderer.setRequestedGroup(requestedGroup); //FIXME: not thread-safe!
+		command.setRenderedWikiText(this.wikiRenderer.render(wikiText));
+		command.setWikiText(wikiText);
+
+		//return Views.WIKICVPAGE;
+	}
+
+	/**
+	 * Handles the user cv page request
+	 * 
+	 * @param reqUser
+	 * @param command
+	 * @return The user-cv-page view
+	 */
+	private void handleUserCV(final User requestedUser, final SettingsViewCommand command) {
+		command.setUser(requestedUser);
+		final String userName = requestedUser.getName();
+		//this.setTags(command, Resource.class, GroupingEntity.USER, userName, null, new LinkedList<String>(), null, 1000, null);
+
+		/*
+		 * convert the wiki syntax
+		 */
+		final Wiki wiki = this.logic.getWiki(userName, null);
+		final String wikiText;
+
+		if (present(wiki) && (requestedUser.equals(command.getContext().getLoginUser())
+				|| !requestedUser.isSpammer() && requestedUser.getToClassify() != null && requestedUser.getToClassify() != 1)) {
+			wikiText = wiki.getWikiText();
+		} else {
+			wikiText = "";
+		}
+
+		/*
+		 * set the user to render
+		 */
+		this.wikiRenderer.setRequestedUser(requestedUser); // FME: not thread-safe!
+		command.setRenderedWikiText(this.wikiRenderer.render(wikiText));
+		command.setWikiText(wikiText);
+
+		//return Views.WIKICVPAGE;
+
+	}
+	
+	/**
+	 * @param wikiRenderer
+	 *            the wikiRenderer to set
+	 */
+	@Required
+	public void setWikiRenderer(CVWikiModel wikiRenderer) {
+		this.wikiRenderer = wikiRenderer;
 	}
 
 }
