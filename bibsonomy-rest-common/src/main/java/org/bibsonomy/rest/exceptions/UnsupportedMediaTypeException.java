9c8f3bf01da7d0fad87a3b1edbf1ebbcee661910
Waldemar Lautenschlager <wla@cs.uni-kassel.de>
2012-06-29 14:55:06 +0200

    Added error messages for CSL format

---
+++ bibsonomy-rest-common/src/main/java/org/bibsonomy/rest/exceptions/UnsupportedMediaTypeException.java
@@ -0,0 +1,13 @@
+package org.bibsonomy.rest.exceptions;
+
+/**
+ * @author wla
+ * @version $Id$
+ */
+public class UnsupportedMediaTypeException extends RuntimeException {
+
+	public UnsupportedMediaTypeException(final String message) {
+		super(message);
+	}
+
+}
