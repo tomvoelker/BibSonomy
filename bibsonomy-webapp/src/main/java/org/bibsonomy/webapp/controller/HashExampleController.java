8da710cedd2af9f7450edfa6f63db99096f66aa4
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2012-06-29 11:29:03 +0200

    cleanup (unused imports) and added todo

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/HashExampleController.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/HashExampleController.java
@@ -3,7 +3,6 @@ package org.bibsonomy.webapp.controller;
 import org.bibsonomy.model.BibTex;
 import org.bibsonomy.model.Post;
 import org.bibsonomy.webapp.command.HashExampleCommand;
-import org.bibsonomy.webapp.command.ContextCommand;
 import org.bibsonomy.webapp.util.ErrorAware;
 import org.bibsonomy.webapp.util.MinimalisticController;
 import org.bibsonomy.webapp.util.View;
@@ -11,8 +10,13 @@ import org.bibsonomy.webapp.view.Views;
 import org.springframework.validation.Errors;
 
 /**
- *
+ * controller for
+ * 		- /hashexample
+ * 
+ * TODO: adapt or delete http://www.bibsonomy.org/help/doc/inside.html 
+ * 
  * @author janus
+ * @version $Id$
  */
 public class HashExampleController implements MinimalisticController<HashExampleCommand>, ErrorAware {
     
@@ -20,30 +24,26 @@ public class HashExampleController implements MinimalisticController<HashExample
 
     @Override
     public HashExampleCommand instantiateCommand() {
-        
         final HashExampleCommand command = new HashExampleCommand();
-        Post<BibTex> post = new Post<BibTex>();
+        final Post<BibTex> post = new Post<BibTex>();
         post.setResource(new BibTex());
         command.setPost(post);
-        
         return command;
     }
 
     @Override
-    public View workOn(HashExampleCommand command) {
+    public View workOn(final HashExampleCommand command) {
         command.getPost().getResource().recalculateHashes();
-        
         return Views.HASHEXAMPLE;
     }
 
     @Override
     public Errors getErrors() {
-        return errors;
+        return this.errors;
     }
 
     @Override
-    public void setErrors(Errors errors) {
+    public void setErrors(final Errors errors) {
         this.errors = errors;
     }
-    
 }
