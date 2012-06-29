7c46401ff31f05020434a66409462f8bf510de06
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2012-06-29 11:08:03 +0200

    logged in user via command.context

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/opensocial/ContainerPageController.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/opensocial/ContainerPageController.java
@@ -3,7 +3,6 @@ package org.bibsonomy.webapp.controller.opensocial;
 import org.apache.shindig.config.ContainerConfig;
 import org.bibsonomy.model.User;
 import org.bibsonomy.opensocial.security.SecurityTokenUtil;
-import org.bibsonomy.util.spring.security.AuthenticationUtils;
 import org.bibsonomy.webapp.command.opensocial.OpenSocialCommand;
 import org.bibsonomy.webapp.controller.SingleResourceListControllerWithTags;
 import org.bibsonomy.webapp.util.MinimalisticController;
@@ -14,21 +13,23 @@ import com.google.inject.Inject;
 
 /**
  * Initial gadget container page for testing the open social interface
+ * 
  * @author fei
+ * @version $Id$
  */
 public class ContainerPageController extends SingleResourceListControllerWithTags implements MinimalisticController<OpenSocialCommand> {
-	@Inject
+	
+	@Inject // FIXME: inject via spring!
 	ContainerConfig config;
 	
-	@SuppressWarnings("deprecation")
 	@Override
-	public View workOn(OpenSocialCommand command) {
-		User loginUser = AuthenticationUtils.getUser();
-		//String st;
+	public View workOn(final OpenSocialCommand command) {
+		final User loginUser = command.getContext().getLoginUser();
+		// TODO: handle user not logged in?
 		try {
-			String token = SecurityTokenUtil.getSecurityToken(loginUser, command.getGadgetUrl());
+			final String token = SecurityTokenUtil.getSecurityToken(loginUser, command.getGadgetUrl());
 			command.setSecurityToken(token);
-		} catch (Exception ex) {
+		} catch (final Exception ex) {
 		}
 		return Views.GADGETCONTAINER;
 	}
@@ -37,5 +38,4 @@ public class ContainerPageController extends SingleResourceListControllerWithTag
 	public OpenSocialCommand instantiateCommand() {
 		return new OpenSocialCommand();
 	}
-
 }
