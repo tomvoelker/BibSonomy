cb8dc0c4dfea1a5e16fcd2c8e72aef075162663f
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2012-06-29 15:06:05 +0200

    cleanup and fixmes and todos

--- bibsonomy-logging/src/main/java/org/bibsonomy/logging/LogMe.java
+++ bibsonomy-logging/src/main/java/org/bibsonomy/logging/LogMe.java
@@ -12,174 +12,135 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
  * @author sst
- *
+ * @version $Id$
  */
-
 public class LogMe extends HttpServlet {
-	
+	private static final long serialVersionUID = 7035035642527692979L;
 
-        private static final long serialVersionUID = 7035035642527692979L;
-        
 	private static final Log log = LogFactory.getLog(LogMe.class);
 
-	public static List<String> getMatches(Pattern pattern, String text, int splitAtSpace) {
-		List<String> matches = new ArrayList<String>();
-		Matcher m = pattern.matcher(text);
-		while(m.find()) {
-			
-			if (m.group(1).contains(" ") && splitAtSpace==1 ) {
-				String[] tempMatchesM = m.group(1).split(" ");
-				for (String s : tempMatchesM) {
+	public static List<String> getMatches(final Pattern pattern, final String text, final int splitAtSpace) {
+		final List<String> matches = new ArrayList<String>();
+		final Matcher m = pattern.matcher(text);
+		while (m.find()) {
+			if (m.group(1).contains(" ") && (splitAtSpace == 1)) {
+				final String[] tempMatchesM = m.group(1).split(" ");
+				for (final String s : tempMatchesM) {
 					matches.add(s);
 				}
-			}
-			else {
+			} else {
 				matches.add(m.group(1));
 			}
-			 
 		}
 		return matches;
-	} 
+	}
 
-	public static List<String> getMatches(Pattern pattern, String text) {
+	public static List<String> getMatches(final Pattern pattern, final String text) {
 		return getMatches(pattern, text, 0);
-	} 
+	}
 
-	
 	@Override
-	public void doGet(HttpServletRequest req, HttpServletResponse response)
-			throws ServletException, IOException {
-
-
-		// Antwort am Anfang senden, damit Browser nicht warten muss
+	public void doGet(final HttpServletRequest req, final HttpServletResponse response) throws ServletException, IOException {
+		/*
+		 * Antwort am Anfang senden, damit Browser nicht warten muss
+		 */
 		response.setContentType("text/html");
-		PrintWriter pw = new PrintWriter (response.getOutputStream());
-		
-/*
-		pw.println("");
-		pw.println("<head><title>Sven TEST</title></head>");
-		pw.println("");
-		pw.println("<body>");
-		pw.println("");
-		pw.println("Hallo!");
-		pw.println("");
-		pw.println("</body>");
-		pw.println("</html>");
-*/
+		final PrintWriter pw = new PrintWriter(response.getOutputStream());
+		pw.close();
 
-//		pw.println("");
-	    pw.close();
-
-	    
-		
-		
 		int dompath_length = 0;
-		
+
 		try {
 			dompath_length = req.getParameter("dompath").length();
-		} catch (NullPointerException e) {
-			// kein dompath angegeben 
+		} catch (final NullPointerException e) {
+			// kein dompath angegeben
 		}
-		
+
 		log.debug("dompath_lenght = " + dompath_length);
 
-		// if dompath is empty 
-		if (dompath_length>0)
-		{
-
-	//		String userAgent =  req.getHeader("user-agent");
-	//		System.out.println("Hallo " + userAgent);
-	
+		// if dompath is empty
+		if (dompath_length > 0) {
 			// schreibe Cookies in Cookie-Array cookie
-			Cookie[] cookies = req.getCookies();
-	//		System.out.println (req.getParameter("dompath"));
-	//		System.out.println (req.getHeader("remote_addr"));
-			
+			final Cookie[] cookies = req.getCookies();
+
 			@SuppressWarnings("unchecked")
-			Enumeration<String> headerNames = req.getHeaderNames();
+			final Enumeration<String> headerNames = req.getHeaderNames();
 			String cookieUsername = "";
-	    	String cookieSessionId = "";
-	    	String completeHeader ="";
-	    	String logType ="";
-	    	int separatorIndex = 0;
-	        
+			String cookieSessionId = "";
+			String completeHeader = "";
+			String logType = "";
+			int separatorIndex = 0;
+
 			while (headerNames.hasMoreElements()) {
-			  String element = headerNames.nextElement();
-	          completeHeader += (element + ": " + req.getHeader(element) + "\n");
-	        }
-	
-			if (cookies != null) {
-		        for (int i = 0; i < cookies.length; i++) {
-	//	        	System.out.println ("Cookie " + i + ": " + cookies[i].getName() + " = " + cookies[i].getValue());
-		        	if (cookies[i].getName().equals("_currUser")){
-		        		separatorIndex = cookies[i].getValue().indexOf("%20");
-		        		cookieUsername = cookies[i].getValue().substring(0, separatorIndex);
-		        	}
-		        
-		        	if (cookies[i].getName().equals("JSESSIONID")){
-		        		cookieSessionId = cookies[i].getValue();
-		        	}
-		        }
+				final String element = headerNames.nextElement();
+				completeHeader += (element + ": " + req.getHeader(element) + "\n");
 			}
-			
-			
-			// build an array for used ids with all char strings, beginning with # and ending with / or . 
+
+			if (cookies != null) {
+				for (final Cookie cookie : cookies) {
+					// FIXME: cookie name changed!! and content has changed too
+					if (cookie.getName().equals("_currUser")) {
+						separatorIndex = cookie.getValue().indexOf("%20");
+						cookieUsername = cookie.getValue().substring(0, separatorIndex);
+					}
+
+					if (cookie.getName().equals("JSESSIONID")) {
+						cookieSessionId = cookie.getValue();
+					}
+				}
+			}
+
+			// build an array for used ids with all char strings, beginning with
+			// # and ending with / or .
 			// (ending with non a-z, A-Z, 0-9 or -)
 			// regular expression: /#([A-Za-z0-9\-]+)/
+			Pattern p = Pattern.compile("#([a-zA-Z0-9-_]+)");
+			String text = req.getParameter("dompath2");
+			// TODO: id array unused
+			final List<String> idArray = getMatches(p, text);
 			
-			 Pattern p = Pattern.compile("#([a-zA-Z0-9-_]+)");
-			 String text = req.getParameter("dompath2");
-			 @SuppressWarnings("unused")
-			List<String> idArray = getMatches(p, text);
-			
-//			 System.out.println (idArray.toString());
-			 
-			
-			
-			// then build another array for used classes with all char strings, beginning with . and ending with / or .
+			// then build another array for used classes with all char strings,
+			// beginning with . and ending with / or .
 			// if class contains spaces, split it to multiple classes
 
 			// regular expression: /\.[A-Za-z0-9\- ]+/
-			
-			 p = Pattern.compile("\\.([a-zA-Z0-9- _]+)");
-			 text = req.getParameter("dompath2");
-			 List<String> classArray = getMatches(p, text,1);
 
-//			 System.out.println (classArray.toString());
-			
-	
+			p = Pattern.compile("\\.([a-zA-Z0-9- _]+)");
+			text = req.getParameter("dompath2");
+			final List<String> classArray = getMatches(p, text, 1);
+
+			// System.out.println (classArray.toString());
+
 			// logType is the type of logging information
-			// where in page has user clicked? Bookmark area,...  
+			// where in page has user clicked? Bookmark area,...
 			logType = req.getParameter("dompath").replaceFirst("^[^#]+#", "");
 			logType = logType.replaceFirst("/.*$", "");
-			
-			
+
 			// if class tagcloud exists, add to type with blank inbetween
 			// if class bmown set bmown-value to 1 otherwise to 0
 
 			String abmown = "0";
-			// if classArray contains class bmown, then link is users own bookmark
+			// if classArray contains class bmown, then link is users own
+			// bookmark
 			if (classArray.contains("bmown")) {
 				abmown = "1";
-			}
-			else {
+			} else {
 				abmown = "0";
 			}
-			
-			
-			org.bibsonomy.logging.Log LogData = new org.bibsonomy.logging.Log();
-			
+
+			final org.bibsonomy.logging.Log LogData = new org.bibsonomy.logging.Log();
+
 			LogData.setAhref(req.getParameter("ahref"));
 			LogData.setAcontent(req.getParameter("acontent"));
 			LogData.setAnumberofposts(req.getParameter("numberofposts"));
@@ -188,15 +149,13 @@ public class LogMe extends HttpServlet {
 			LogData.setType(logType);
 			LogData.setPageurl(req.getParameter("pageurl"));
 			LogData.setUseragent(req.getHeader("user-agent"));
-			
+
 			if (req.getParameter("username").isEmpty()) {
 				LogData.setUsername(cookieUsername);
-			}
-			else
-			{
+			} else {
 				LogData.setUsername(req.getParameter("username"));
 			}
-			
+
 			LogData.setSessionid(cookieSessionId);
 			LogData.setHost(req.getHeader("host"));
 			LogData.setCompleteheader(completeHeader);
@@ -207,29 +166,23 @@ public class LogMe extends HttpServlet {
 			LogData.setMousedocumentpos(req.getParameter("mousedocumentpos"));
 			LogData.setAbmown(abmown);
 			LogData.setReferer(req.getParameter("referer"));
-			
+
 			log.debug("LogData to insert:\n" + LogData.toString());
 			log.info("Clicked at anchor with shown text: " + LogData.getAcontent());
-			
+
 			try {
 				QueryDB.getInstance().insertLogdata(LogData);
 				log.info("Database access: insertLogdata ok");
-				
-			} catch (SQLException e) {
+
+			} catch (final SQLException e) {
 				log.error("Database error: insertLogdata", e);
 			}
-			
 		}
 	}
 
 	@Override
-	public void doPost(HttpServletRequest req, HttpServletResponse response)
-	throws ServletException, IOException {
+	public void doPost(final HttpServletRequest req, final HttpServletResponse response) throws ServletException, IOException {
 		log.debug("POST-Request");
-		doGet (req, response);
+		this.doGet(req, response);
 	}
-
-
-
-
 }
