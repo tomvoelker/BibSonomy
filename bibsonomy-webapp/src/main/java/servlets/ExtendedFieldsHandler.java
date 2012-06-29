2c97b7394d3352dac1eefb2eacfc79471f2f7e68
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2012-06-29 12:08:48 +0200

    moved db private note manager code to extended fields handler

--- bibsonomy-webapp/src/main/java/servlets/ExtendedFieldsHandler.java
+++ bibsonomy-webapp/src/main/java/servlets/ExtendedFieldsHandler.java
@@ -1,16 +1,17 @@
 package servlets;
 
-import helpers.database.DBPrivnoteManager;
-
 import java.io.IOException;
 import java.net.URLEncoder;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.bibsonomy.common.enums.HashID;
 import org.bibsonomy.model.User;
 import org.bibsonomy.util.spring.security.AuthenticationUtils;
@@ -18,17 +19,13 @@ import org.bibsonomy.util.spring.security.AuthenticationUtils;
 import filters.ActionValidationFilter;
 
 @Deprecated
-public class ExtendedFieldsHandler extends HttpServlet{
-
+public class ExtendedFieldsHandler extends AbstractServlet {
+	private static final Log log = LogFactory.getLog(ExtendedFieldsHandler.class);
+	
 	private static final long serialVersionUID = 4051324539558769200L;
 
 	@Override
-	public void init(ServletConfig config) throws ServletException{	
-		super.init(config); 
-	}
-
-	@Override
-	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public void doPost(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
 		final User user = AuthenticationUtils.getUser();
 		final String currUser = user.getName(); 
 		if (currUser == null) {
@@ -41,7 +38,7 @@ public class ExtendedFieldsHandler extends HttpServlet{
 		 */
 		final String action = request.getParameter("action");
 		final boolean validCkey = ActionValidationFilter.isValidCkey(request);
-		if (action != null && validCkey) {
+		if ((action != null) && validCkey) {
 			if ("updatePrivateNote".equals(action)) {		
 				final String hash = request.getParameter("hash");
 				
@@ -50,11 +47,24 @@ public class ExtendedFieldsHandler extends HttpServlet{
 				 */
 				final String privnote    = request.getParameter("privnote");
 				final String oldprivnote = request.getParameter("oldprivnote");
-				if (((privnote == null && oldprivnote != null) || (privnote != null && (oldprivnote == null || !privnote.equals(oldprivnote))))) {
+				if ((((privnote == null) && (oldprivnote != null)) || ((privnote != null) && ((oldprivnote == null) || !privnote.equals(oldprivnote))))) {
 					/*
 					 * something has changed --> write it to DB
 					 */
-					DBPrivnoteManager.setPrivnoteForUser(privnote, currUser, hash);
+					Connection connection = null;
+					PreparedStatement statement = null;
+					try {
+						connection = this.getConnection();
+						statement = connection.prepareStatement("UPDATE bibtex SET privnote = ? WHERE user_name = ? AND simhash2 = ?");
+						statement.setString(1, privnote);
+						statement.setString(2, currUser);
+						statement.setString(3, hash);
+						statement.executeUpdate();
+					} catch (final SQLException e) {
+						log.error("error updating privatenote for " + currUser + " (hash=" + hash + ")");
+					} finally {
+						this.closeAll(connection, statement);
+					}
 				}
 				
 				response.sendRedirect("/bibtex/" + HashID.INTRA_HASH.getId() + URLEncoder.encode(hash, "UTF-8") + "/" + URLEncoder.encode(currUser, "UTF-8"));
@@ -62,5 +72,4 @@ public class ExtendedFieldsHandler extends HttpServlet{
 			}
 		}
 	}
-
 }
