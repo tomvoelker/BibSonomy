ea59c0c6aea04451f12024ae847cb6a1dcaa9dc8
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2012-07-02 12:06:22 +0200

    switch to interface

--- bibsonomy-database/src/test/java/org/bibsonomy/testutil/DummyResourceSearch.java
+++ bibsonomy-database/src/test/java/org/bibsonomy/testutil/DummyResourceSearch.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.bibsonomy.model.Post;
 import org.bibsonomy.model.Resource;
-import org.bibsonomy.model.ResultList;
 import org.bibsonomy.model.Tag;
 import org.bibsonomy.services.searcher.ResourceSearch;
 
@@ -16,7 +15,7 @@ import org.bibsonomy.services.searcher.ResourceSearch;
 public class DummyResourceSearch implements ResourceSearch<Resource> {
 	
 	@Override
-	public ResultList<Post<Resource>> getPosts(final String userName, final String requestedUserName, final String requestedGroupName, final Collection<String> allowedGroups, final String searchTerms, final String titleSearchTerms, final String authorSearchTerms, final Collection<String> tagIndex, final String year, final String firstYear, final String lastYear, final int limit, final int offset) {
+	public List<Post<Resource>> getPosts(final String userName, final String requestedUserName, final String requestedGroupName, final Collection<String> allowedGroups, final String searchTerms, final String titleSearchTerms, final String authorSearchTerms, final Collection<String> tagIndex, final String year, final String firstYear, final String lastYear, final int limit, final int offset) {
 		return null;
 	}
 	
