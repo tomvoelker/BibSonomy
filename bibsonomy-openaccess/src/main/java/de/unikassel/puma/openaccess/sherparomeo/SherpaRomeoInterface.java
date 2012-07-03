2d3584741b8fb5ecd2cd7bc27475b7f34a0c9ee3
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2012-07-03 16:09:14 +0200

    cleanup replaced ex.printStackTrace with logging

--- bibsonomy-openaccess/src/main/java/de/unikassel/puma/openaccess/sherparomeo/SherpaRomeoInterface.java
+++ bibsonomy-openaccess/src/main/java/de/unikassel/puma/openaccess/sherparomeo/SherpaRomeoInterface.java
@@ -1,7 +1,10 @@
 package de.unikassel.puma.openaccess.sherparomeo;
 
+/**
+ * @author rja
+ * @version $Id$
+ */
 public interface SherpaRomeoInterface {
-    
     
     /**
      * Performs a search for the publisher. 
@@ -13,10 +16,9 @@ public interface SherpaRomeoInterface {
      *                  - any   Publishers’ names must include one or more of the publisher strings
      *                  - exact The publisher string must appear as an intact ‘phrase’ somewhere in the
      *                          pblisher’s name
-     * @return
+     * @return the policy
      */
     public String getPolicyForPublisher(String publisher, String qtype);
-    
     
     /**
      * Performs a search for the journal title. Qtype can be exact or contains
@@ -27,7 +29,7 @@ public interface SherpaRomeoInterface {
      *                              publisher’s name
      *                  - contains  The jtitle string may appear anywhere in the journal title
      *                  - exact     Full title (excluding any added place of publication) must equal jtitle
-     * @return
+     * @return the policy
      */
     public String getPolicyForJournal(String jtitle, String qtype);
 }
