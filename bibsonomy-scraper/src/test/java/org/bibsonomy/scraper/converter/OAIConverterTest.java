bcf5fa88f7d3263474ecb35654c01f1376c1796b
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2012-07-04 10:29:09 +0200

    removed duplicated code

--- bibsonomy-scraper/src/test/java/org/bibsonomy/scraper/converter/OAIConverterTest.java
+++ bibsonomy-scraper/src/test/java/org/bibsonomy/scraper/converter/OAIConverterTest.java
@@ -2,11 +2,10 @@ package org.bibsonomy.scraper.converter;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 import org.bibsonomy.scraper.exceptions.ScrapingException;
+import org.bibsonomy.testutil.TestUtils;
 import org.junit.Test;
 
 /**
@@ -19,31 +18,19 @@ public class OAIConverterTest {
 	
 	@Test
 	public void testConvert1() throws Exception {
-		testFile("arxiv1");
+		this.testFile("arxiv1");
 	}
 
 	@Test
 	public void testConvert2() throws Exception {
-		testFile("arxiv2");
+		this.testFile("arxiv2");
 	}
 	
 
 	private void testFile(final String fileName) throws IOException, ScrapingException {
-		final String xml = this.readEntryFromFile(fileName + ".xml");
-		final String bib = this.readEntryFromFile(fileName + ".bib");
+		final String xml = TestUtils.readEntryFromFile(OAIConverterTest.PATH_TO_FILES + fileName + ".xml");
+		final String bib = TestUtils.readEntryFromFile(OAIConverterTest.PATH_TO_FILES + fileName + ".bib");
 		
 		assertEquals(bib.trim(), OAIConverter.convert(xml).trim());
 	}
-	
-	private String readEntryFromFile(final String fileName) throws IOException {
-		final StringBuffer resultString = new StringBuffer();
-		final BufferedReader in = new BufferedReader(new InputStreamReader(OAIConverterTest.class.getClassLoader().getResourceAsStream(PATH_TO_FILES + fileName), "UTF-8"));
-		String line = null;
-		while ((line = in.readLine()) != null) {
-			resultString.append(line + "\n");
-		}
-		return resultString.toString();
-	}
-	
-	
 }
--- bibsonomy-scraper/src/test/java/org/bibsonomy/scraper/converter/RisToBibtexConverterTest.java
+++ bibsonomy-scraper/src/test/java/org/bibsonomy/scraper/converter/RisToBibtexConverterTest.java
@@ -2,15 +2,9 @@ package org.bibsonomy.scraper.converter;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
+import org.bibsonomy.testutil.TestUtils;
 import org.junit.Test;
-
-import bibtex.parser.ParseException;
 
 /**
  * @author rja
@@ -22,35 +16,20 @@ public class RisToBibtexConverterTest {
 	
 	/**
 	 * http://www.agu.org/pubs/crossref/2008/2008JD010287.shtml
-	 * @throws ParseException 
+	 * @throws Exception 
 	 */
 	@Test
-	public void testRisToBibtex1() throws ParseException {
-		try {
-			String ris = this.readEntryFromFile("2008JD010287.ris");
+	public void testRisToBibtex1() throws Exception {
+		final String ris = TestUtils.readEntryFromFile(PATH_TO_FILES + "2008JD010287.ris");
 
-			// test the canHandle heuristic
-			assertTrue(RisToBibtexConverter.canHandle(ris));
+		// test the canHandle heuristic
+		assertTrue(RisToBibtexConverter.canHandle(ris));
 
-			// test the conversion
-			final String expectedBibTeX = this.readEntryFromFile("2008JD010287.bib");
-			final RisToBibtexConverter ris2bConverter = new RisToBibtexConverter();
-			final String bibTeX = ris2bConverter.risToBibtex(ris);
+		// test the conversion
+		final String expectedBibTeX = TestUtils.readEntryFromFile(PATH_TO_FILES + "2008JD010287.bib");
+		final RisToBibtexConverter ris2bConverter = new RisToBibtexConverter();
+		final String bibTeX = ris2bConverter.risToBibtex(ris);
 
-			assertEquals (expectedBibTeX, bibTeX);
-		} catch (IOException ex) {
-			fail(ex.getMessage());
-		}
+		assertEquals (expectedBibTeX, bibTeX);
 	}
-	
-	private String readEntryFromFile(final String fileName) throws IOException {
-		final StringBuffer resultString = new StringBuffer();
-		final BufferedReader in = new BufferedReader(new InputStreamReader(ToBibtexConverterTest.class.getClassLoader().getResourceAsStream(PATH_TO_FILES + fileName), "UTF-8"));
-		String line = null;
-		while ((line = in.readLine()) != null) {
-			resultString.append(line + "\n");
-		}
-		return resultString.toString();
-	}
-
 }
