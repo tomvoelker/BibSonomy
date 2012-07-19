f15a6f7d634b8ec8152e4c146606cf5bd265abc4
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2012-07-19 11:21:37 +0200

    expanded logging info

--- bibsonomy-synchronization/src/main/java/org/bibsonomy/synchronization/AutoSync.java
+++ bibsonomy-synchronization/src/main/java/org/bibsonomy/synchronization/AutoSync.java
@@ -37,25 +37,25 @@ public class AutoSync {
 	public void performAutoSync() {
 		log.info("start automatic synchronization");
 		
-		if(!present(syncClient)) {
+		if (!present(this.syncClient)) {
 			log.info("snyc client not available");
 			return;
 		}
 		
-		List<SyncService> syncServices = adminLogic.getSyncService(null, null, true);
-		for (SyncService syncService : syncServices) {
-			//skip, if autosync not selected or direction is both
-			if(!syncService.isAutosync() || syncService.getDirection() == SynchronizationDirection.BOTH) {
+		final List<SyncService> syncServices = this.adminLogic.getSyncService(null, null, true);
+		for (final SyncService syncService : syncServices) {
+			// skip, if autosync not selected or direction is both
+			if (!syncService.isAutosync() || syncService.getDirection() == SynchronizationDirection.BOTH) {
 				continue;
 			}
 			
-			
-			User clientUser = adminLogic.getUserDetails(syncService.getUserName());
-			log.info("Autosync for user:" + clientUser.getName() + " and service: " + syncService.getService().toString() + " api: " + syncService.getSecureAPI());
+			final User clientUser = this.adminLogic.getUserDetails(syncService.getUserName());
+			final String userNameToSync = clientUser.getName();
+			log.info("Autosync for user:" + userNameToSync + " and service: " + syncService.getService().toString() + " api: " + syncService.getSecureAPI());
 			try {
-				LogicInterface clientLogic = userLogicFactory.getLogicAccess(clientUser.getName(), clientUser.getApiKey());
+				final LogicInterface clientLogic = this.userLogicFactory.getLogicAccess(userNameToSync, clientUser.getApiKey());
 				
-				Map<Class<? extends Resource>, List<SynchronizationPost>> syncPlan = syncClient.getSyncPlan(clientLogic, syncService);
+				final Map<Class<? extends Resource>, List<SynchronizationPost>> syncPlan = this.syncClient.getSyncPlan(clientLogic, syncService);
 				if (!present(syncPlan)) {
 					log.info("no sync plan received");
 					continue;
@@ -66,60 +66,39 @@ public class AutoSync {
 				 * run sync plan
 				 */
 				try {
-					Map<Class<? extends Resource>, SynchronizationData> syncData = syncClient.synchronize(clientLogic, syncService, syncPlan);
-					for (Entry<Class<? extends Resource>, SynchronizationData> data : syncData.entrySet()) {
+					final Map<Class<? extends Resource>, SynchronizationData> syncData = this.syncClient.synchronize(clientLogic, syncService, syncPlan);
+					for (final Entry<Class<? extends Resource>, SynchronizationData> data : syncData.entrySet()) {
 						log.info(data.getValue().getInfo());
 					}
 				} catch (final SynchronizationRunningException e) {
-					//FIXME handle this, i think it is nothing to do in this case
+					// FIXME handle this, i think it is nothing to do in this case
 					log.debug(e.getMessage());
 				}
-			} catch (BadRequestOrResponseException ex) {
-				log.error(ex.getMessage());
+			} catch (final BadRequestOrResponseException ex) {
+				log.error("error while autosyncing " + userNameToSync, ex);
 				continue;
 			}
 		}
 	}
 
 	/**
-	 * @return the adminLogic
-	 */
-	public LogicInterface getAdminLogic() {
-		return adminLogic;
-	}
-
-	/**
 	 * @param adminLogic the adminLogic to set
 	 */
-	public void setAdminLogic(LogicInterface adminLogic) {
+	public void setAdminLogic(final LogicInterface adminLogic) {
 		this.adminLogic = adminLogic;
 	}
-
-	/**
-	 * @return the syncClient
-	 */
-	public TwoStepSynchronizationClient getSyncClient() {
-		return syncClient;
-	}
-
+	
 	/**
 	 * @param synclient the syncClient to set
 	 */
-	public void setSyncClient(TwoStepSynchronizationClient synclient) {
+	public void setSyncClient(final TwoStepSynchronizationClient synclient) {
 		this.syncClient = synclient;
-	}
-
-	/**
-	 * @return the userLogicFactory
-	 */
-	public LogicInterfaceFactory getUserLogicFactory() {
-		return this.userLogicFactory;
 	}
 
 	/**
 	 * @param userLogicFactory the userLogicFactory to set
 	 */
-	public void setUserLogicFactory(LogicInterfaceFactory userLogicFactory) {
+	public void setUserLogicFactory(final LogicInterfaceFactory userLogicFactory) {
 		this.userLogicFactory = userLogicFactory;
 	}
 
