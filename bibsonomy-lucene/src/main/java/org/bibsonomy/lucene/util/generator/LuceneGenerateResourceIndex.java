289761826ba65d85e66faa5e93b3a39c91f9e497
telekoma <devnull@localhost>
2012-08-17 13:54:44 +0200

    Switched back to single threaded index generation

--- bibsonomy-lucene/src/main/java/org/bibsonomy/lucene/util/generator/LuceneGenerateResourceIndex.java
+++ bibsonomy-lucene/src/main/java/org/bibsonomy/lucene/util/generator/LuceneGenerateResourceIndex.java
@@ -38,62 +38,66 @@ import org.bibsonomy.model.Resource;
  * 
  * @author sst
  * @author fei
- * @version $Id$
+ * @version $Id: LuceneGenerateResourceIndex.java,v 1.20 2012-08-17 11:10:56
+ *          telekoma Exp $
  * 
- * @param <R> the resource of the index to generate
+ * @param <R>
+ *            the resource of the index to generate
  */
 public class LuceneGenerateResourceIndex<R extends Resource> implements Runnable {
-	
+
 	public static final String TMP_INDEX_SUFFIX = ".tmp";
-	
+
 	protected static final Log log = LogFactory.getLog(LuceneGenerateResourceIndex.class);
 
 	/** TODO: improve documentation */
 	private static final int SQL_BLOCKSIZE = 25000;
-	
+
 	/** database logic */
 	private LuceneDBInterface<R> dbLogic;
-		
+
 	/** writes the resource index */
 	private IndexWriter indexWriter;
-	
+
 	private int numberOfThreads = 1;
-	
+
 	/** converts post model objects to lucene documents */
 	private LuceneResourceConverter<R> resourceConverter;
-	
+
 	private LuceneResourceIndex<R> resourceIndex;
-	
+
 	private GenerateIndexCallback<R> callback = null;
-	
+
 	/** set to true if the generator is currently generating an index */
 	private boolean isRunning;
 
 	private int numberOfPosts;
 	private int numberOfPostsImported;
-	
+
 	// FolkRank caches
 	private String lastHash = "";
 	private Map<Character, String> tagFieldValues = new HashMap<Character, String>();
 	private Map<Character, String> userFieldValues = new HashMap<Character, String>();
-	
+
 	/**
 	 * frees allocated resources and closes all files
-	 * @throws IOException 
-	 * @throws CorruptIndexException 
+	 * 
+	 * @throws IOException
+	 * @throws CorruptIndexException
 	 */
 	public void shutdown() throws CorruptIndexException, IOException {
 		this.indexWriter.close();
-		
+
 		if (this.callback != null) {
-		    this.callback.generatedIndex(this.resourceIndex);
+			this.callback.generatedIndex(this.resourceIndex);
 		}
 	}
-	
+
 	/**
-	 * Read in data from database and build index. 
+	 * Read in data from database and build index.
 	 * 
-	 * Database as well as index files are configured in the lucene.properties file.
+	 * Database as well as index files are configured in the lucene.properties
+	 * file.
 	 * 
 	 * @throws CorruptIndexException
 	 * @throws IOException
@@ -105,27 +109,27 @@ public class LuceneGenerateResourceIndex<R extends Resource> implements Runnable
 		if (this.isRunning) {
 			return;
 		}
-		
+
 		this.isRunning = true;
-		
+
 		// create a new, yet temporally index
 		this.createEmptyIndex();
-		
+
 		// generate index
 		this.createIndexFromDatabase();
-		
+
 		// delete the old index, if exists
-//    	this.resourceIndex.deleteIndex();
-		
+		// this.resourceIndex.deleteIndex();
+
 		// Hope that works
 		this.replaceIndex();
-		
+
 		// activate the index
 		this.resourceIndex.reset();
-		
+
 		this.isRunning = false;
 	}
-	
+
 	/**
 	 * deletes the old index and replaces it with the new one
 	 */
@@ -133,20 +137,20 @@ public class LuceneGenerateResourceIndex<R extends Resource> implements Runnable
 		try {
 			resourceIndex.close();
 			final File indexPath = new File(resourceIndex.getIndexPath());
-			final File tmpIndexPath = new File(resourceIndex.getIndexPath()+TMP_INDEX_SUFFIX);
+			final File tmpIndexPath = new File(resourceIndex.getIndexPath() + TMP_INDEX_SUFFIX);
 			final Directory indexDirectory = FSDirectory.open(indexPath);
 			final Directory tmpIndexDirectory = FSDirectory.open(tmpIndexPath);
-			
+
 			log.info("Deleting index " + indexPath.getAbsolutePath() + "...");
-			for (final String filename: indexDirectory.listAll()) {
-			    indexDirectory.deleteFile(filename);
-			    log.debug("Deleted " + filename);
+			for (final String filename : indexDirectory.listAll()) {
+				indexDirectory.deleteFile(filename);
+				log.debug("Deleted " + filename);
 			}
 			log.info("Success.");
 			log.info("Coping new index files from " + tmpIndexPath.getName());
 			boolean movedIndexSuccessful = true;
-			for (final String filename: tmpIndexDirectory.listAll()) {
-				File file = new File(tmpIndexPath.getAbsolutePath()+"/"+filename);
+			for (final String filename : tmpIndexDirectory.listAll()) {
+				File file = new File(tmpIndexPath.getAbsolutePath() + "/" + filename);
 				// Move file to new directory
 				log.debug("Move " + filename + " to " + indexPath.getName());
 				File file2 = new File(indexPath, file.getName());
@@ -156,12 +160,13 @@ public class LuceneGenerateResourceIndex<R extends Resource> implements Runnable
 					log.error(filename + " was not successfully moved");
 				}
 			}
-			/* If the new regenerated index files were successfully moved to
-			 * the index directory remove the temporary index directory */
+			/*
+			 * If the new regenerated index files were successfully moved to the
+			 * index directory remove the temporary index directory
+			 */
 			if (movedIndexSuccessful) {
 				boolean success = (new File(tmpIndexPath.getAbsolutePath())).delete();
-				if (success) 
-					log.info("Temporary index directory successfully deleted.");
+				if (success) log.info("Temporary index directory successfully deleted.");
 			}
 		} catch (final NoSuchDirectoryException e) {
 			log.warn("Tried to delete the lucene-index-directory but it could not be found.", e);
@@ -170,10 +175,10 @@ public class LuceneGenerateResourceIndex<R extends Resource> implements Runnable
 			log.error("Could not delete directory-content before index-generation or index-copy.", e);
 		}
 	}
-	
+
 	/**
 	 * Create empty index. Attributes must already be configured (via init()).
-	 *  
+	 * 
 	 * @throws CorruptIndexException
 	 * @throws LockObtainFailedException
 	 * @throws IOException
@@ -181,10 +186,10 @@ public class LuceneGenerateResourceIndex<R extends Resource> implements Runnable
 	protected void createEmptyIndex() throws CorruptIndexException, LockObtainFailedException, IOException {
 		// create index, possibly overwriting existing index files
 		log.info("Creating empty lucene index...");
-		final Directory indexDirectory = FSDirectory.open(new File(this.resourceIndex.getIndexPath()+TMP_INDEX_SUFFIX));
-		this.indexWriter = new IndexWriter(indexDirectory, this.resourceIndex.getAnalyzer(), true, this.resourceIndex.getMaxFieldLength()); 
+		final Directory indexDirectory = FSDirectory.open(new File(this.resourceIndex.getIndexPath() + TMP_INDEX_SUFFIX));
+		this.indexWriter = new IndexWriter(indexDirectory, this.resourceIndex.getAnalyzer(), true, this.resourceIndex.getMaxFieldLength());
 	}
-	
+
 	/**
 	 * creates index of resource entries
 	 * 
@@ -193,24 +198,24 @@ public class LuceneGenerateResourceIndex<R extends Resource> implements Runnable
 	 */
 	protected void createIndexFromDatabase() throws CorruptIndexException, IOException {
 		log.info("Filling index with database post entries.");
-		
+
 		final ExecutorService executor = Executors.newFixedThreadPool(this.numberOfThreads);
-		
+
 		// number of post entries to calculate progress
 		// FIXME: the number of posts is wrong
 		this.numberOfPosts = this.dbLogic.getNumberOfPosts();
-		log.info("Number of post entries: "+  this.numberOfPosts);
-		
+		log.info("Number of post entries: " + this.numberOfPosts);
+
 		// initialize variables
 		final Integer lastTasId = this.dbLogic.getLastTasId();
-		Date lastLogDate  = this.dbLogic.getLastLogDate();
-		
+		Date lastLogDate = this.dbLogic.getLastLogDate();
+
 		if (lastLogDate == null) {
-		    lastLogDate = new Date(System.currentTimeMillis() - 1000);
+			lastLogDate = new Date(System.currentTimeMillis() - 1000);
 		}
 
 		log.info("Start writing data to lucene index (with duplicate detection)");
-		
+
 		// read block wise all posts
 		List<LucenePost<R>> postList = null;
 		int skip = 0;
@@ -226,48 +231,45 @@ public class LuceneGenerateResourceIndex<R extends Resource> implements Runnable
 			for (final LucenePost<R> post : postList) {
 				post.setLastLogDate(lastLogDate);
 				post.setLastTasId(lastTasId);
-				executor.execute(new Runnable() {
-					
-					@Override
-					public void run() {
-						if (LuceneGenerateResourceIndex.this.isNotSpammer(post)) {  
-							// create index document from post model
-							final Document doc = LuceneGenerateResourceIndex.this.resourceConverter.readPost(post);
-							try {
-								LuceneGenerateResourceIndex.this.indexWriter.addDocument(doc);
-								LuceneGenerateResourceIndex.this.importedPost(post);
-							} catch (final IOException e) {
-								log.error("error while inserting post " + post.getUser().getName() + "/" + post.getResource().getIntraHash(), e);
-							}
-						}
+
+				if (LuceneGenerateResourceIndex.this.isNotSpammer(post)) {
+					// create index document from post model
+					final Document doc = LuceneGenerateResourceIndex.this.resourceConverter.readPost(post);
+					try {
+						LuceneGenerateResourceIndex.this.indexWriter.addDocument(doc);
+						LuceneGenerateResourceIndex.this.importedPost(post);
+					} catch (final IOException e) {
+						log.error("error while inserting post " + post.getUser().getName() + "/" + post.getResource().getIntraHash(), e);
 					}
-				});
+				}
+
 			}
-			
+
 			if (postListSize > 0) {
 				lastContenId = postList.get(postListSize - 1).getContentId();
 			}
 		} while (postListSize == SQL_BLOCKSIZE);
-		
+
 		try {
 			executor.shutdown();
 			executor.awaitTermination(18, TimeUnit.HOURS);
-			
+
 			// optimize index
 			log.info("optimizing index " + this.resourceIndex);
 			this.indexWriter.optimize();
-			
+
 			// close resource indexWriter
 			log.info("closing index " + this.resourceIndex);
 			this.indexWriter.close();
-			
+
 			// all done
-			// log.info("(" + i + " indexed entries, " + is + " not indexed spam entries)");
+			// log.info("(" + i + " indexed entries, " + is +
+			// " not indexed spam entries)");
 		} catch (final InterruptedException e) {
 			log.error("lucene finished not in 18 hours", e);
 		}
 	}
-	
+
 	/**
 	 * creates index of resource entries and adds FolkRanks to posts
 	 * (experimental purpose)
@@ -277,32 +279,32 @@ public class LuceneGenerateResourceIndex<R extends Resource> implements Runnable
 	 */
 	protected void createIndexFromDatabaseWithFolkRanks() throws CorruptIndexException, IOException {
 		log.info("Filling index with database post entries.");
-		
+
 		final ExecutorService executor = Executors.newFixedThreadPool(this.numberOfThreads);
-		
+
 		// number of post entries to calculate progress
 		// FIXME: the number of posts is wrong
 		this.numberOfPosts = this.dbLogic.getNumberOfPosts();
-		log.info("Number of post entries: "+  this.numberOfPosts);
-		
+		log.info("Number of post entries: " + this.numberOfPosts);
+
 		// initialize variables
 		final Integer lastTasId = this.dbLogic.getLastTasId();
-		Date lastLogDate  = this.dbLogic.getLastLogDate();
-		
+		Date lastLogDate = this.dbLogic.getLastLogDate();
+
 		if (lastLogDate == null) {
-		    lastLogDate = new Date(System.currentTimeMillis() - 1000);
+			lastLogDate = new Date(System.currentTimeMillis() - 1000);
 		}
 
 		log.info("Start writing data to lucene index (with duplicate detection)");
-		
+
 		// TODO configure FolkRank file location (Spring?)
 		String folkRankFile = this.resourceIndex.getResourceClass().getSimpleName();
 		FolkRankDao folkRankDao = new FileFolkRankDao(folkRankFile);
-		
+
 		// read block wise all posts
 		List<LucenePost<R>> postList = null;
 		int skip = 0;
-		//int lastContenId = -1;
+		// int lastContenId = -1;
 		int lastOffset = 0;
 		int postListSize = 0;
 		do {
@@ -315,54 +317,56 @@ public class LuceneGenerateResourceIndex<R extends Resource> implements Runnable
 			for (final LucenePost<R> post : postList) {
 				post.setLastLogDate(lastLogDate);
 				post.setLastTasId(lastTasId);
-				//executor.execute(new Runnable() {
-					
-				// FIXME had to remove Thread creation because reading FolkRank values is not thread safe.
-					//@Override
-					//public void run() {
-						if (LuceneGenerateResourceIndex.this.isNotSpammer(post)) {  
-							// create index document from post model
-							final Document doc = LuceneGenerateResourceIndex.this.resourceConverter.readPost(post);
-							
-							// FolkRanks
-							String hash = post.getResource().getInterHash();
-							addFolkRanksAsFullTextFields(doc, hash, folkRankDao);
-							try {
-								LuceneGenerateResourceIndex.this.indexWriter.addDocument(doc);
-								LuceneGenerateResourceIndex.this.importedPost(post);
-							} catch (final IOException e) {
-								log.error("error while inserting post " + post.getUser().getName() + "/" + post.getResource().getIntraHash(), e);
-							}
-						}
-					//}
-				//});
+				// executor.execute(new Runnable() {
+
+				// FIXME had to remove Thread creation because reading FolkRank
+				// values is not thread safe.
+				// @Override
+				// public void run() {
+				if (LuceneGenerateResourceIndex.this.isNotSpammer(post)) {
+					// create index document from post model
+					final Document doc = LuceneGenerateResourceIndex.this.resourceConverter.readPost(post);
+
+					// FolkRanks
+					String hash = post.getResource().getInterHash();
+					addFolkRanksAsFullTextFields(doc, hash, folkRankDao);
+					try {
+						LuceneGenerateResourceIndex.this.indexWriter.addDocument(doc);
+						LuceneGenerateResourceIndex.this.importedPost(post);
+					} catch (final IOException e) {
+						log.error("error while inserting post " + post.getUser().getName() + "/" + post.getResource().getIntraHash(), e);
+					}
+				}
+				// }
+				// });
 			}
-			
+
 			if (postListSize > 0) {
-				//lastContenId = postList.get(postListSize - 1).getContentId();
+				// lastContenId = postList.get(postListSize - 1).getContentId();
 				lastOffset += postListSize;
 			}
 		} while (postListSize == SQL_BLOCKSIZE);
-		
+
 		try {
 			executor.shutdown();
 			executor.awaitTermination(18, TimeUnit.HOURS);
-			
+
 			// optimize index
 			log.info("optimizing index " + this.resourceIndex);
 			this.indexWriter.optimize();
-			
+
 			// close resource indexWriter
 			log.info("closing index " + this.resourceIndex);
 			this.indexWriter.close();
-			
+
 			// all done
-			// log.info("(" + i + " indexed entries, " + is + " not indexed spam entries)");
+			// log.info("(" + i + " indexed entries, " + is +
+			// " not indexed spam entries)");
 		} catch (final InterruptedException e) {
 			log.error("lucene finished not in 18 hours", e);
 		}
 	}
-	
+
 	/**
 	 * 
 	 * Adds available tag/user FolkRanks to given post.
@@ -373,30 +377,30 @@ public class LuceneGenerateResourceIndex<R extends Resource> implements Runnable
 	 * @return
 	 */
 	protected int addFolkRanksAsFullTextFields(Document doc, String hash, FolkRankDao folkRankDao) {
-		
+
 		List<FolkRankInfo> folkRanks = folkRankDao.getTagUserFolkRanks(hash);
-		
+
 		if (folkRanks.size() == 0) {
 			return 0; // no FolkRanks for this item
 		}
-		
+
 		if (!hash.equals(lastHash)) {
-			
+
 			lastHash = hash;
-			
+
 			tagFieldValues.clear();
 			userFieldValues.clear();
-			
+
 			for (FolkRankInfo folkRank : folkRanks) {
-				
+
 				String item = folkRank.getItem().toLowerCase();
 				String dim = Integer.toString(folkRank.getDim());
 				String weight = Float.toString(folkRank.getWeight());
-				
+
 				if (item.length() == 0 || item.length() > 50 || item.contains(" ")) {
 					continue;
 				}
-				
+
 				Map<Character, String> usedHashMap;
 				if (dim.equals("0")) {
 					usedHashMap = tagFieldValues;
@@ -405,35 +409,37 @@ public class LuceneGenerateResourceIndex<R extends Resource> implements Runnable
 				} else {
 					continue;
 				}
-				
+
 				char firstCharacter = item.charAt(0);
-				
+
 				if (!Character.isLetter(firstCharacter)) {
 					firstCharacter = '#';
 				}
-				
+
 				if (!usedHashMap.containsKey(firstCharacter)) {
 					usedHashMap.put(firstCharacter, "");
 				}
-				
-				usedHashMap.put(firstCharacter, usedHashMap.get(firstCharacter) + item + " " + weight + " ");			
+
+				usedHashMap.put(firstCharacter, usedHashMap.get(firstCharacter) + item + " " + weight + " ");
 			}
-			
-		} 
-		
+
+		}
+
 		for (char key : tagFieldValues.keySet()) {
-			//addStoreField(doc, "frtag" + key, tagFieldValues.get(key).trim());
+			// addStoreField(doc, "frtag" + key,
+			// tagFieldValues.get(key).trim());
 			doc.add(new Field("frtag" + key, tagFieldValues.get(key).trim(), Field.Store.YES, Field.Index.NO));
 		}
-		
+
 		for (char key : userFieldValues.keySet()) {
-			//addStoreField(doc, "fruser" + key, userFieldValues.get(key).trim());
+			// addStoreField(doc, "fruser" + key,
+			// userFieldValues.get(key).trim());
 			doc.add(new Field("fruser" + key, userFieldValues.get(key).trim(), Field.Store.YES, Field.Index.NO));
 		}
-		
+
 		return folkRanks.size();
 	}
-	
+
 	protected synchronized void importedPost(final LucenePost<R> post) {
 		// update counter
 		this.numberOfPostsImported++;
@@ -456,19 +462,20 @@ public class LuceneGenerateResourceIndex<R extends Resource> implements Runnable
 		}
 		return true;
 	}
-	
+
 	/**
 	 * Get the progress-percentage
+	 * 
 	 * @return the progressPercentage
 	 */
 	public int getProgressPercentage() {
 		return (int) Math.round(100 * ((double) this.numberOfPostsImported / this.numberOfPosts));
 	}
-	
+
 	/** Run the index-generation in a thread. */
 	@Override
 	public void run() {
-        try {
+		try {
 			this.generateIndex();
 		} catch (final Exception e) {
 			log.error("Failed to generate " + this.resourceIndex + "-index!", e);
@@ -482,35 +489,40 @@ public class LuceneGenerateResourceIndex<R extends Resource> implements Runnable
 	}
 
 	/**
-	 * @param dbLogic the dbLogic to set
+	 * @param dbLogic
+	 *            the dbLogic to set
 	 */
 	public void setLogic(final LuceneDBInterface<R> dbLogic) {
 		this.dbLogic = dbLogic;
 	}
 
 	/**
-	 * @param resourceConverter the resourceConverter to set
+	 * @param resourceConverter
+	 *            the resourceConverter to set
 	 */
 	public void setResourceConverter(final LuceneResourceConverter<R> resourceConverter) {
 		this.resourceConverter = resourceConverter;
 	}
 
 	/**
-	 * @param callback the callback to set
+	 * @param callback
+	 *            the callback to set
 	 */
 	public void setCallback(final GenerateIndexCallback<R> callback) {
 		this.callback = callback;
 	}
-	
+
 	/**
-	 * @param resourceIndex the resourceIndex to set
+	 * @param resourceIndex
+	 *            the resourceIndex to set
 	 */
 	public void setResourceIndex(final LuceneResourceIndex<R> resourceIndex) {
 		this.resourceIndex = resourceIndex;
 	}
 
 	/**
-	 * @param numberOfThreads the numberOfThreads to set
+	 * @param numberOfThreads
+	 *            the numberOfThreads to set
 	 */
 	public void setNumberOfThreads(final int numberOfThreads) {
 		this.numberOfThreads = numberOfThreads;
