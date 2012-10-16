f398ee90a0f3d978e55041d335c02edac0bc3afc
markusjordan88 <devnull@localhost>
2012-10-16 09:46:21 +0200

    Expire CronJob

---
+++ bibsonomy-database/src/main/java/org/bibsonomy/database/systemstags/ScheduledTagsManager.java
@@ -0,0 +1,13 @@
+package org.bibsonomy.database.systemstags;
+
+/**
+ * @author markus
+ * @version $Id$
+ */
+public class ScheduledTagsManager {
+
+	
+	public void scheduleTagsExpire() {
+		System.out.println("Hello World");
+	}
+}
