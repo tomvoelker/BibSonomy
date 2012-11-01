1b1220e8450fc90045e2073789d35273c199a6b1
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2012-11-01 11:20:31 +0100

    replaced UserDetailsService with an AuthenticationUserDetailsService
    the openid auth provider used in 3.0.5 the user details service directly in 3.1 an UserDetailsByNameServiceWrapper wrapps the user details service as a result the user name and not the openid is used to authenticate the user

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/util/spring/security/userdetails/OpenIdAuthenticationUserDetailsService.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/util/spring/security/userdetails/OpenIdAuthenticationUserDetailsService.java
@@ -1,21 +1,25 @@
-package org.bibsonomy.webapp.util.spring.security.userdetailsservice;
+package org.bibsonomy.webapp.util.spring.security.userdetails;
 
 import org.bibsonomy.model.User;
 import org.bibsonomy.util.spring.security.UserAdapter;
 import org.bibsonomy.webapp.util.spring.security.exceptions.OpenIdUsernameNotFoundException;
-import org.springframework.dao.DataAccessException;
+import org.bibsonomy.webapp.util.spring.security.userdetailsservice.DatabaseUserDetailsService;
 import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.core.userdetails.AuthenticationUserDetailsService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.openid.OpenIDAuthenticationToken;
 
 /**
  * @author dzo
  * @version $Id$
  */
-public class OpenIDDatabaseUserDetailsService extends DatabaseUserDetailsService {
-	
+public class OpenIdAuthenticationUserDetailsService extends DatabaseUserDetailsService implements AuthenticationUserDetailsService<OpenIDAuthenticationToken> {
+
 	@Override
-	public UserDetails loadUserByUsername(final String openID) throws UsernameNotFoundException, DataAccessException {
+	public UserDetails loadUserDetails(final OpenIDAuthenticationToken token) throws UsernameNotFoundException {
+		final String openID = token.getIdentityUrl();
+		
 		final String username = this.adminLogic.getOpenIDUser(openID == null ? null : openID.trim());
 		
 		if (username == null) {
@@ -31,4 +35,5 @@ public class OpenIDDatabaseUserDetailsService extends DatabaseUserDetailsService
 		
 		return userAdapter;
 	}
+
 }
