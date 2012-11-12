e6e76f4ffc6d6afa6c42bfc6f14ea367fac36157
Haile Misgna Haile <haile.misgna@mu.edu.et>
2012-11-12 14:20:08 +0100

    added test class for the JNeurosciScraper

---
+++ bibsonomy-scraper/src/test/java/org/bibsonomy/scraper/url/kde/jneurosci/JNeurosciScraperTest.java
@@ -0,0 +1,25 @@
+package org.bibsonomy.scraper.url.kde.jneurosci;
+
+import static org.junit.Assert.*;
+
+import org.bibsonomy.scraper.UnitTestRunner;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * @author hagen
+ * @version $Id$
+ */
+public class JNeurosciScraperTest {
+
+	/**
+	 * starts URL test with id url_232
+	 */
+	@Test
+	@Ignore
+	public void url1TestRun() {
+		UnitTestRunner runner = new UnitTestRunner();
+		assertTrue(runner.runSingleTest("url_232"));
+	}
+
+}
