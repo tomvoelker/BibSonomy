14a317c73fb1c738e7fa4591944ad0a6d33a7c45
Haile Misgna Haile <haile.misgna@mu.edu.et>
2012-11-15 10:01:03 +0100

    Test for Biologists Scraper

---
+++ bibsonomy-scraper/src/test/java/org/bibsonomy/scraper/url/kde/biologists/BiologistsScraperTest.java
@@ -0,0 +1,25 @@
+package org.bibsonomy.scraper.url.kde.biologists;
+
+import static org.junit.Assert.*;
+
+import org.bibsonomy.scraper.UnitTestRunner;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * @author hagen
+ * @version $Id$
+ */
+public class BiologistsScraperTest {
+
+	/**
+	 * starts URL test with id url_234
+	 */
+	@Test
+	@Ignore
+	public void url1TestRun() {
+		UnitTestRunner runner = new UnitTestRunner();
+		assertTrue(runner.runSingleTest("url_234"));
+	}
+
+}
