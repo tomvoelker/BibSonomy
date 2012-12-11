0c8f50be6d32577e8f04eee3063e0af08a81966f
derbeukatt <devnull@localhost>
2012-12-11 13:20:12 +0100

    - Updated language files - added javascript to disable tag areas - migrated batchedit javascript to jquery

--- bibsonomy-webapp/src/main/webapp/resources/javascript/batchEdit.js
+++ bibsonomy-webapp/src/main/webapp/resources/javascript/batchEdit.js
@@ -1,30 +1,48 @@
 function markAll(input) {
-	if(input) {	 
-		var markAllChecked = input.checked;
-		var inp = document.getElementById("batchedit").getElementsByTagName("input");
-		for (var i = 0; i<inp.length; i++) {
-			if (inp[i].type.toLowerCase() == "checkbox" && inp[i].name.toLowerCase().indexOf("posts") == 0) {
-				inp[i].checked = markAllChecked;
-			}
+	if(input) {	
+		if($(input).is(':checkbox')) {
+			var markAllChecked = $(input).is(':checked');
+			/*
+			 * mega haxxor jquery selector to select input checkboxes with name beginning
+			 * with posts.
+			 */
+			$('input[name^=posts]:checkbox').each(function() {
+				$(this).attr('checked', markAllChecked);
+			});
 		}
 	}
 }
 
 function checkMarkAll(input) {
 	if(input) {
-		if (input.type.toLowerCase() == "checkbox") {
+		if($(input).is(':checkbox')) {
 			var oneNotChecked = false;
-			var inp = document.getElementById("batchedit").getElementsByTagName("input");
-			for (var i= 0; i<inp.length; i++) {
-				if (inp[i].type.toLowerCase() == "checkbox" && inp[i].name.toLowerCase().indexOf("posts") == 0) {
-					if (!inp[i].checked) {
-						oneNotChecked = true;
-					}
-				}
-			}
-			document.getElementById("all").checked = !oneNotChecked;
+			/*
+			 * mega haxxor jquery selector to select input checkboxes with name beginning
+			 * with posts that are not checked.
+			 */
+			$('input[name^=posts]:checkbox:not(:checked)').each(function() {
+				oneNotChecked = true;
+				return;
+			});
+			$('#all').attr('checked', !oneNotChecked);
 		}
 	}
+}
+
+function askForMarked(input) {
+	var confirmed = confirm(input);
+	manipulateTagArea(confirmed);
+}
+
+function manipulateTagArea(disabled) {
+	$('input[name^=posts]:checkbox:checked').each(function() {
+		/*
+		 * remove possible special characters from selector string
+		 */
+		var attr = $(this).attr('name').replace('posts','newTags').replace(/([;&,\.\+\*\~':"\!\^#$%@\[\]\(\)=>\|])/g, '\\$1');
+		$('input[name=' + attr + ']:text').attr('disabled', disabled);
+	});
 }
 		           
 maximizeById("general");
