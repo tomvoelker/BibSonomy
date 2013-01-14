246256fc79861536fd236a46e114c2bb0f263e81
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2013-01-14 10:31:25 +0100

    fixed json deserialization of bibtextype
    JAXB and Jersey JSON has some problems with inheritance (GoldStandardPublication -> BibtexType)
    
    introduced an "abstract" publicationType as a fix
    
    updated Jersey JSON to version 1.16 (from 1.3)

--- bibsonomy-model/src/main/java/org/bibsonomy/model/util/ModelValidationUtils.java
+++ bibsonomy-model/src/main/java/org/bibsonomy/model/util/ModelValidationUtils.java
@@ -31,6 +31,7 @@ import org.bibsonomy.model.Bookmark;
 import org.bibsonomy.model.Group;
 import org.bibsonomy.model.Tag;
 import org.bibsonomy.model.User;
+import org.bibsonomy.rest.renderer.xml.AbstractPublicationType;
 import org.bibsonomy.rest.renderer.xml.BibtexType;
 import org.bibsonomy.rest.renderer.xml.BookmarkType;
 import org.bibsonomy.rest.renderer.xml.GroupType;
@@ -41,8 +42,10 @@ import org.bibsonomy.rest.renderer.xml.UserType;
 /**
  * Sanity checks for the model.
  * 
+ * FIXME: implement a validation layer that can be used by the REST service and the webapp
  * 
- * XXX: InvalidModelException was: 
+ * TODO: remove?
+ * InvalidModelException was: 
  * 
  * public InvalidXMLException(final String message) {
  *   super("The body part of the received XML document is not valid: " + message);
@@ -58,7 +61,10 @@ import org.bibsonomy.rest.renderer.xml.UserType;
  */
 public class ModelValidationUtils {
 
-	private static final String XML_IS_INVALID_MSG = "The body part of the received XML document is not valid: ";
+	/**
+	 * the error message rendered
+	 */
+	public static final String DOCUMENT_NOT_VALID_ERROR_MESSAGE = "The body part of the received document is not valid: ";
 
 	/**
 	 * @param tag the object to run sanity checks on
@@ -132,11 +138,11 @@ public class ModelValidationUtils {
 	public static void checkTag(final TagType xmlTag) throws InvalidModelException {
 		final String tagName = xmlTag.getName();
 		if (!present(tagName)) {
-			throw new InvalidModelException(XML_IS_INVALID_MSG + "tag name is missing in element 'tag'");
+			throw new InvalidModelException(DOCUMENT_NOT_VALID_ERROR_MESSAGE + "tag name is missing in element 'tag'");
 		}
 		
 		if (tagName.contains(" ")) {
-			throw new InvalidModelException(XML_IS_INVALID_MSG + "tag name contains space character. To assign several tags to a post, please use one tag element for each tag.");
+			throw new InvalidModelException(DOCUMENT_NOT_VALID_ERROR_MESSAGE + "tag name contains space character. To assign several tags to a post, please use one tag element for each tag.");
 		}
 	}
 
@@ -146,7 +152,7 @@ public class ModelValidationUtils {
 	 */
 	public static void checkUser(final UserType xmlUser) throws InvalidModelException {
 		if (!present(xmlUser.getName())) {
-			throw new InvalidModelException(XML_IS_INVALID_MSG + "username is missing in element 'user'");
+			throw new InvalidModelException(DOCUMENT_NOT_VALID_ERROR_MESSAGE + "username is missing in element 'user'");
 		}
 	}
 
@@ -156,7 +162,7 @@ public class ModelValidationUtils {
 	 */
 	public static void checkGroup(final GroupType xmlGroup) throws InvalidModelException {
 		if (!present(xmlGroup.getName())) {
-			throw new InvalidModelException(XML_IS_INVALID_MSG + "groupname is missing in element 'group'");
+			throw new InvalidModelException(DOCUMENT_NOT_VALID_ERROR_MESSAGE + "groupname is missing in element 'group'");
 		}
 	}
 
@@ -165,20 +171,20 @@ public class ModelValidationUtils {
 	 * @throws InvalidModelException if there is a lack of sanity
 	 */
 	public static void checkPost(final PostType xmlPost) throws InvalidModelException {
-		if (xmlPost.getTag() == null) throw new InvalidModelException(XML_IS_INVALID_MSG + "list of tags is missing");
+		if (xmlPost.getTag() == null) throw new InvalidModelException(DOCUMENT_NOT_VALID_ERROR_MESSAGE + "list of tags is missing");
 		
 		// 2011/10/05, fei: deactivated test, as system tags are hidden and thus posts without tags are valid
 		// if (xmlPost.getTag().size() == 0) throw new InvalidModelException(XML_IS_INVALID_MSG + "no tags specified");
 		
-		if (xmlPost.getUser() == null) throw new InvalidModelException(XML_IS_INVALID_MSG + "user is missing");
+		if (xmlPost.getUser() == null) throw new InvalidModelException(DOCUMENT_NOT_VALID_ERROR_MESSAGE + "user is missing");
 
 		final BibtexType xmlPublication = xmlPost.getBibtex();
 		final BookmarkType xmlBookmark = xmlPost.getBookmark();
 		
 		if (xmlPublication == null && xmlBookmark == null) {
-			throw new InvalidModelException(XML_IS_INVALID_MSG + "resource is missing inside element 'post'");
+			throw new InvalidModelException(DOCUMENT_NOT_VALID_ERROR_MESSAGE + "resource is missing inside element 'post'");
 		} else if (xmlPublication != null && xmlBookmark != null) {
-			throw new InvalidModelException(XML_IS_INVALID_MSG + "only one resource type is allowed inside element 'post'");
+			throw new InvalidModelException(DOCUMENT_NOT_VALID_ERROR_MESSAGE + "only one resource type is allowed inside element 'post'");
 		} else {
 			// just fine (bibtex xor bookmark):
 			// ( xmlBibtex == null && xmlBookmark != null ) || ( xmlBibtex != null || xmlBookmark == null )
@@ -191,11 +197,11 @@ public class ModelValidationUtils {
 	 */
 	public static void checkStandardPost(final PostType xmlPost) throws InvalidModelException {
 		// user
-		if (xmlPost.getUser() == null) throw new InvalidModelException(XML_IS_INVALID_MSG + "user is missing");
+		if (xmlPost.getUser() == null) throw new InvalidModelException(DOCUMENT_NOT_VALID_ERROR_MESSAGE + "user is missing");
 		
 		// resource
 		if (xmlPost.getGoldStandardPublication() == null) {
-			throw new InvalidModelException(XML_IS_INVALID_MSG + "resource is missing inside element 'post'");
+			throw new InvalidModelException(DOCUMENT_NOT_VALID_ERROR_MESSAGE + "resource is missing inside element 'post'");
 		}
 	}
 
@@ -204,19 +210,19 @@ public class ModelValidationUtils {
 	 * @throws InvalidModelException if there is a lack of sanity
 	 */
 	public static void checkBookmark(final BookmarkType xmlBookmark) throws InvalidModelException {
-		if (!present(xmlBookmark.getUrl())) throw new InvalidModelException(XML_IS_INVALID_MSG + "url is missing in element 'bookmark'");
-		if (!present(xmlBookmark.getTitle())) throw new InvalidModelException(XML_IS_INVALID_MSG + "title is missing in element 'bookmark'");
+		if (!present(xmlBookmark.getUrl())) throw new InvalidModelException(DOCUMENT_NOT_VALID_ERROR_MESSAGE + "url is missing in element 'bookmark'");
+		if (!present(xmlBookmark.getTitle())) throw new InvalidModelException(DOCUMENT_NOT_VALID_ERROR_MESSAGE + "title is missing in element 'bookmark'");
 	}
 
 	/**
 	 * @param xmlPublication the object to run sanity checks on
 	 * @throws InvalidModelException if there is a lack of sanity
 	 */
-	public static void checkPublication(final BibtexType xmlPublication) throws InvalidModelException {
-		if (!present(xmlPublication.getTitle())) throw new InvalidModelException(XML_IS_INVALID_MSG + "title is missing in element 'bibtex'");
-		if (!present(xmlPublication.getYear())) throw new InvalidModelException(XML_IS_INVALID_MSG + "year is missing in element 'bibtex'");
-		if (!present(xmlPublication.getBibtexKey())) throw new InvalidModelException(XML_IS_INVALID_MSG + "bibtex key is missing in element 'bibtex'");
-		if (!present(xmlPublication.getEntrytype())) throw new InvalidModelException(XML_IS_INVALID_MSG + "bibtex entry type is missing in element 'bibtex'");
-		if (!present(xmlPublication.getAuthor()) && !present(xmlPublication.getEditor())) throw new InvalidModelException(XML_IS_INVALID_MSG + "editor(s) and author(s) are missing (one of the two is required) in element 'bibtex'");
+	public static void checkPublication(final AbstractPublicationType xmlPublication) throws InvalidModelException {
+		if (!present(xmlPublication.getTitle())) throw new InvalidModelException(DOCUMENT_NOT_VALID_ERROR_MESSAGE + "title is missing in element 'bibtex'");
+		if (!present(xmlPublication.getYear())) throw new InvalidModelException(DOCUMENT_NOT_VALID_ERROR_MESSAGE + "year is missing in element 'bibtex'");
+		if (!present(xmlPublication.getBibtexKey())) throw new InvalidModelException(DOCUMENT_NOT_VALID_ERROR_MESSAGE + "bibtex key is missing in element 'bibtex'");
+		if (!present(xmlPublication.getEntrytype())) throw new InvalidModelException(DOCUMENT_NOT_VALID_ERROR_MESSAGE + "bibtex entry type is missing in element 'bibtex'");
+		if (!present(xmlPublication.getAuthor()) && !present(xmlPublication.getEditor())) throw new InvalidModelException(DOCUMENT_NOT_VALID_ERROR_MESSAGE + "editor(s) and author(s) are missing (one of the two is required) in element 'bibtex'");
 	}
 }
