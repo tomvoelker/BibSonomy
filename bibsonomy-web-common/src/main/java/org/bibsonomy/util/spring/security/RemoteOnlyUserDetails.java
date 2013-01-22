84f88ea2264a9c9ad9f7e992e338c79b064d6cca
Jens Illig <jil@cs.uni-kassel.de>
2013-01-22 20:14:10 +0100

    +RemoteOnlyUserDetails (for SAML)

---
+++ bibsonomy-web-common/src/main/java/org/bibsonomy/util/spring/security/RemoteOnlyUserDetails.java
@@ -0,0 +1,68 @@
+package org.bibsonomy.util.spring.security;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+/**
+ * Wrapper around our user object to make it available as UserDetails.
+ * 
+ * @author jensi
+ * @version $Id$
+ */
+public class RemoteOnlyUserDetails implements UserDetails {
+	private static final long serialVersionUID = -7121668540134905067L;
+	
+	private final Object creds;
+
+	/**
+	 * @param user the user to adapt
+	 */
+	public RemoteOnlyUserDetails(Object creds) {
+		this.creds = creds;
+	}
+	
+	@Override
+	public Collection<GrantedAuthority> getAuthorities() {
+		return Collections.emptyList();
+	}
+
+	@Override
+	public String getPassword() {
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+	/**
+	 * @return the creds
+	 */
+	public Object getCreds() {
+		return creds;
+	}
+}
