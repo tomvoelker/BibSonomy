7c6e8040cad255160ef8e185d1cdc3f6d9bc9652
Haile Misgna Haile <haile.misgna@mu.edu.et>
2013-01-30 17:15:45 +0100

    This commit adds EconstorScraperTest

---
+++ bibsonomy-scraper/src/test/java/org/bibsonomy/scraper/url/kde/econstor/EconstorScraperTest.java
@@ -0,0 +1,25 @@
+package org.bibsonomy.scraper.url.kde.econstor;
+
+import static org.junit.Assert.assertTrue;
+
+import org.bibsonomy.scraper.UnitTestRunner;
+import org.junit.Ignore;
+import org.junit.Test;
+
+
+/**
+ * @author L3S
+ * @version $Id$
+ */
+public class EconstorScraperTest {
+	/**
+	 * 
+	 */
+	@Test
+	@Ignore
+	public void urlTestRun() {
+		UnitTestRunner runner = new UnitTestRunner();
+		assertTrue(runner.runSingleTest("url_231"));
+	}
+
+}
