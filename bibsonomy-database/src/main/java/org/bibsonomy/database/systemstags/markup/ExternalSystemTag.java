2e5e0847cc2891ab9704dd30790a3608f08b990d
Stephan Doerfel <doerfel@cs.uni-kassel.de>
2013-02-14 10:16:12 +0100

    external system tag for vufind

---
+++ bibsonomy-database/src/main/java/org/bibsonomy/database/systemstags/markup/ExternalSystemTag.java
@@ -0,0 +1,37 @@
+package org.bibsonomy.database.systemstags.markup;
+
+import org.bibsonomy.database.systemstags.AbstractSystemTagImpl;
+import org.bibsonomy.database.systemstags.SystemTagsUtil;
+
+/**
+ * The External System Tag marks a post relevant for some external system. 
+ * For example the tag sys:external:vufind would marks posts that are created through the system VuFind.
+ * @author sdo
+ * @version $Id$
+ */
+public class ExternalSystemTag extends AbstractSystemTagImpl implements MarkUpSystemTag {
+
+	public static final String NAME = "external";
+
+	@Override
+	public String getName() {
+		return NAME;
+	}
+
+	@Override
+	public boolean isToHide() {
+		return true;
+	}
+
+
+	@Override
+	public ExternalSystemTag newInstance() {
+		return new ExternalSystemTag();
+	}
+
+	@Override
+	public boolean isInstance(final String tagName) {
+		return SystemTagsUtil.hasPrefixTypeAndArgument(tagName) && NAME.equals(SystemTagsUtil.extractType(tagName));
+	}
+
+}
