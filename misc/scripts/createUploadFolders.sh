f38ef0dc8a1cef0cf821a61571bef3d1af181c73
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2013-03-05 19:54:59 +0100

    cleanup

--- misc/scripts/createUploadFolders.sh
+++ misc/scripts/createUploadFolders.sh
@@ -2,15 +2,18 @@
 # check line arguments
 if [[ $# == 0 ]]; then
   echo "usage: createUploadFolders.sh project_name"
-  exit
+  exit -1
 fi
+
+prefix=$1
+
 # create temp dir
-mkdir "$1_temp"
+mkdir ${prefix}_temp
 # create doc and pictures folder
 for folder_suffix in _docs _pictures
 do
   # create root folder
-  folder="$1$folder_suffix"
+  folder=$prefix$folder_suffix
   mkdir $folder
   # create subfolders 
   for i in 0 1 2 3 4 5 6 7 8 9 a b c d e f
