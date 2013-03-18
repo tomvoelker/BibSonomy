ac1000357e2e1aae328821128034a0f0650d06a0
Haile Misgna Haile <haile.misgna@mu.edu.et>
2013-03-18 02:28:50 +0100

    using apache commons

--- bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/apa/APAScraper.java
+++ bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/apa/APAScraper.java
@@ -2,7 +2,7 @@
  *
  *  BibSonomy-Scraper - Web page scrapers returning BibTeX for BibSonomy.
  *
- *  Copyright (C) 2006 - 2011 Knowledge & Data Engineering Group,
+ *  Copyright (C) 2006 - 2013 Knowledge & Data Engineering Group,
  *                            University of Kassel, Germany
  *                            http://www.kde.cs.uni-kassel.de/
  *
@@ -26,18 +26,14 @@ package org.bibsonomy.scraper.url.kde.apa;
 import static org.bibsonomy.util.ValidationUtils.present;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.CookieManager;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map.Entry;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpURL;
+import org.apache.commons.httpclient.URIException;
 import org.bibsonomy.common.Pair;
 import org.bibsonomy.scraper.AbstractUrlScraper;
 import org.bibsonomy.scraper.ScrapingContext;
@@ -92,7 +88,7 @@ public class APAScraper extends AbstractUrlScraper {
 		scrapingContext.setScraper(this);
 		
 		//We have to proof the visit of several locations
-		CookieManager cookieMan = new CookieManager();
+		HttpClient client = WebUtils.getHttpClient();
 		
 		//This id is needed to build RIS download link
 		String lstUIDs = null;
@@ -107,26 +103,12 @@ public class APAScraper extends AbstractUrlScraper {
 		} else {
 			
 			//If scraping request is not during buy action, the id is contained in the page requested to scrape
-			HttpURLConnection c = null;
-			InputStream in = null;
 			String page;
 			try {
-				c = openPageConnection(scrapingContext.getUrl(), cookieMan);
-				if (!present(c)) throw new ScrapingException("Could not establish connection to requestet URL");
-				in = c.getInputStream();
-				page = WebUtils.inputStreamToStringBuilder(in, WebUtils.extractCharset(c.getHeaderField("Content-Type"))).toString();
+				page = WebUtils.getContentAsString(client, scrapingContext.getUrl().toExternalForm());
 			} catch (IOException ex) {
 				throw new ScrapingException(ex);
-			} catch (URISyntaxException ex) {
-				throw new ScrapingException(ex);
-			} finally {
-				try {
-					if (in != null) in.close();
-				} catch (IOException e) {
-				}
-				if (c != null) c.disconnect();
 			}
-			
 			//Is the page present?
 			if (!present(page)) throw new ScrapingException("Could not get the page requested to scrape");
 			
@@ -141,33 +123,20 @@ public class APAScraper extends AbstractUrlScraper {
 		if (!present(lstUIDs)) throw new ScrapingException("could not find lstUIDs");
 		
 		//Build link to RIS download
-		URL risURL;
+		HttpURL risURL;
 		try {
-			risURL = new URL("http://psycnet.apa.org/index.cfm?fa=search.export&id=&lstUids=" + lstUIDs);
-		} catch (MalformedURLException ex) {
-			throw new ScrapingException(ex);
+			risURL = new HttpURL("http://psycnet.apa.org/index.cfm?fa=search.export&id=&lstUids=" + lstUIDs);
+		} catch (URIException ex1) {
+			throw new ScrapingException(ex1);
 		}
 		
 		//download RIS exactly two times, because the first request will finally be redirected to a login page
 		String ris = null;
 		for (int i = 0; i < 2; i++) {
-			HttpURLConnection c = null;
-			InputStream in = null;
 			try {
-				c = openPageConnection(risURL, cookieMan);
-				if (!present(c)) throw new ScrapingException("Could not establish connection to download URL");
-				in = c.getInputStream();
-				ris = WebUtils.inputStreamToStringBuilder(in, WebUtils.extractCharset(c.getHeaderField("Content-Type"))).toString();
+				ris = WebUtils.getContentAsString(client, risURL);
 			} catch (IOException ex) {
 				throw new ScrapingException(ex);
-			} catch (URISyntaxException ex) {
-				throw new ScrapingException(ex);
-			} finally {
-				try {
-					if (in != null) in.close();
-				} catch (IOException e) {
-				}
-				if (c != null) c.disconnect();
 			}
 			if (ris.contains("Provider: American Psychological Association")) break;
 		}
@@ -181,40 +150,6 @@ public class APAScraper extends AbstractUrlScraper {
 		
 		//success
 		return true;
-	}
-	/**
-	 * Neither apache commons HttpClient nor java.net can do this appropriately in a shorthand manner.
-	 * 
-	 * @param url the location to be connected to originally
-	 * @param cookieMan the cookie manager
-	 * @return the final, already connected connection that returned response code 200
-	 * @throws IOException
-	 * @throws URISyntaxException
-	 */
-	private static HttpURLConnection openPageConnection(URL url, CookieManager cookieMan) throws IOException, URISyntaxException {
-		boolean success = false;
-		for (int i = 0; i < 7; i++) {
-			HttpURLConnection c = null;
-			try {
-				c = (HttpURLConnection) url.openConnection();
-				c.setInstanceFollowRedirects(false);
-				for (Entry<String, List<String>> entry : cookieMan.get(url.toURI(), c.getRequestProperties()).entrySet()) {
-					c.addRequestProperty(entry.getKey(), WebUtils.buildCookieString(entry.getValue()));
-				}
-				c.connect();
-				cookieMan.put(url.toURI(), c.getHeaderFields());
-				if (c.getResponseCode() == HttpURLConnection.HTTP_OK) {
-					success = true;
-					return c;
-				}
-			} finally {
-				if (success == false && c != null) c.disconnect();
-			}
-			String location = c.getHeaderField("Location");
-			if (!present(location)) return null;
-			url = new URL(url, location);
-		}
-		return null;
 	}
 
 }
