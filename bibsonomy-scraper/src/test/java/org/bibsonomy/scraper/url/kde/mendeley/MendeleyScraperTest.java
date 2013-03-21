1ed79ef7301b63c0118bc579073d4e342cdd286a
Haile Misgna Haile <haile.misgna@mu.edu.et>
2013-03-21 15:03:44 +0100

    This commit adds JUnit test for MendeleyScraper

---
+++ bibsonomy-scraper/src/test/java/org/bibsonomy/scraper/url/kde/mendeley/MendeleyScraperTest.java
@@ -0,0 +1,24 @@
+package org.bibsonomy.scraper.url.kde.mendeley;
+
+import static org.junit.Assert.assertTrue;
+
+import org.bibsonomy.scraper.UnitTestRunner;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * @author Haile
+ * @version $Id$
+ */
+public class MendeleyScraperTest {
+	/**
+	 * 
+	 */
+	@Test
+	@Ignore
+	public void urlTestRun() {
+		UnitTestRunner runner = new UnitTestRunner();
+		assertTrue(runner.runSingleTest("url_253"));
+	}
+
+}
