826d89e02dd1bddac29700729e646c52e4683d9a
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2013-04-30 18:27:52 +0200

    extracted pumarenderer renamed pumarenderer to metsrenderer

---
+++ bibsonomy-openaccess/src/main/java/de/unikassel/puma/openaccess/sword/METSRenderer.java
@@ -0,0 +1,244 @@
+package de.unikassel.puma.openaccess.sword;
+
+import static org.bibsonomy.util.ValidationUtils.present;
+
+import java.io.Writer;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map.Entry;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.namespace.QName;
+
+import org.bibsonomy.common.exceptions.InternServerException;
+import org.bibsonomy.database.systemstags.SystemTagsUtil;
+import org.bibsonomy.model.BibTex;
+import org.bibsonomy.model.PersonName;
+import org.bibsonomy.model.Resource;
+import org.bibsonomy.model.User;
+import org.bibsonomy.model.util.PersonNameUtils;
+import org.bibsonomy.rest.exceptions.BadRequestOrResponseException;
+import org.bibsonomy.rest.renderer.UrlRenderer;
+import org.bibsonomy.rest.renderer.impl.JAXBRenderer;
+import org.bibsonomy.rest.renderer.xml.BibtexType;
+import org.bibsonomy.rest.renderer.xml.TagType;
+import org.xml.sax.SAXParseException;
+
+import com.sun.xml.bind.marshaller.NamespacePrefixMapper;
+
+import de.unikassel.puma.openaccess.sword.renderer.xml.Mets;
+import de.unikassel.puma.openaccess.sword.renderer.xml.PumaPost;
+import de.unikassel.puma.openaccess.sword.renderer.xml.PumaUserType;
+
+/**
+ * 
+ * @author: sven
+ * @version $Id$
+ */
+public class METSRenderer extends JAXBRenderer {
+
+	/**
+	 * default constructor with urlrenderer
+	 * @param urlRenderer
+	 */
+	public METSRenderer(final UrlRenderer urlRenderer) {
+		super(urlRenderer);
+	}
+
+	protected PumaPost createPumaPost(final PumaData<? extends Resource> pumaData, final User userData)	throws InternServerException {
+		final PumaPost myPost = new PumaPost();
+		this.fillXmlPost(myPost, pumaData.getPost());
+
+		/*
+		 * delete unwanted data from post
+		 * - remove all system tags. they should not be sent to repository
+		 */
+		final Iterator<TagType> tagIterator = myPost.getTag().iterator();
+		while (tagIterator.hasNext()) {
+			final TagType tag = tagIterator.next();
+
+			if (SystemTagsUtil.isSystemTag(tag.getName())) {
+				tagIterator.remove();
+			}
+		}
+		
+		/*
+		 * remove url. there is no need for this url to be present in repository
+		 */
+		final BibtexType bibtex = myPost.getBibtex();
+		bibtex.setUrl(null);
+		myPost.setBibtex(bibtex);
+
+		/*
+		 * add more user informations
+		 */
+		if (null != userData) {
+			if (null == myPost.getUser()) {
+				myPost.setUser(new PumaUserType());
+			}
+			myPost.getUser().setName(userData.getName());
+			myPost.getUser().setRealname(userData.getRealname());
+			myPost.getUser().setEmail(userData.getEmail());
+			myPost.getUser().setId(userData.getLdapId());
+		}
+
+		/*
+		 * add additional metadata
+		 */
+		final Resource resource = pumaData.getPost().getResource();
+		if (resource instanceof BibTex) {
+			final BibTex bibtexResource = (BibTex) resource;
+			bibtexResource.parseMiscField();
+			if (null != myPost.getBibtex()) {
+				if (null != bibtexResource.getMiscField("isbn")) {
+					myPost.setISBN(bibtexResource.getMiscField("isbn"));
+				} 
+				if (null != bibtexResource.getMiscField("issn")) {
+					myPost.setISSN(bibtexResource.getMiscField("issn"));
+				} 
+				if (null != bibtexResource.getMiscField("doi")) {
+					myPost.setDOI(bibtexResource.getMiscField("doi"));
+				} 
+				if (null != bibtexResource.getMiscField("location")) {
+					myPost.setLocation(bibtexResource.getMiscField("location"));
+				} 
+				if (null != bibtexResource.getMiscField("dcc")) {
+					myPost.setDCC(bibtexResource.getMiscField("dcc"));
+				} 
+			}
+
+			if (present(pumaData.getAuthor())) {
+				for (final PersonName personName : pumaData.getAuthor()) {
+					myPost.getAuthor().add(PersonNameUtils.serializePersonName(personName));
+				}
+			}
+
+			if (null != pumaData.getExaminstitution()) {
+				myPost.setExaminstitution(pumaData.getExaminstitution());
+			}
+
+			if (null != pumaData.getExamreferee()) {
+				for (final String item : pumaData.getExamreferee()) {
+					myPost.getExamreferee().add(item);
+				}
+			}
+
+			if (null != pumaData.getPhdoralexam()) {
+				myPost.setPhdoralexam(pumaData.getPhdoralexam());
+			}
+
+			if (null != pumaData.getSponsors()) {
+				for (final String item : pumaData.getSponsors()) {
+					myPost.getSponsors().add(item);
+				}
+			}
+
+			if (null != pumaData.getAdditionaltitle()) {
+				for (final String item : pumaData.getAdditionaltitle()) {
+					myPost.getAdditionaltitle().add(item);
+				}
+			}
+
+			if (null != pumaData.getClassification()) {					
+				for (final Entry<String, List<String>> entry : pumaData.getClassification().entrySet()) {
+					for (final String listValue : entry.getValue() ) {
+						final PumaPost.Classification pptClassification = new PumaPost.Classification();
+						pptClassification.setName(entry.getKey().toLowerCase(Locale.getDefault()).replaceAll("/ /",""));
+						pptClassification.setValue(listValue);
+						myPost.getClassification().add(pptClassification);
+					}
+				}
+			}
+
+			/*
+			 * add publisher info / romeo sherpa 
+			 */
+			// TODO: get info from romeo/sherpa
+			//myPost.setPublisherinfo("");
+		}
+		return myPost;
+	}
+
+	@Override
+	protected JAXBContext getJAXBContext() throws JAXBException {
+		return JAXBContext.newInstance("org.bibsonomy.rest.renderer.xml:de.unikassel.puma.openaccess.sword.renderer.xml", this.getClass().getClassLoader());
+	}
+
+	/**
+	 * Initializes java xml bindings, builds the document and then marshalls
+	 * it to the writer.
+	 * @param writer 
+	 * @param mets 
+	 * 
+	 * @throws InternServerException
+	 *             if the document can't be marshalled
+	 */
+	public void serializeMets(final Writer writer, final Mets mets) throws InternServerException {
+		try {
+			// initialize context for java xml bindings
+			final JAXBContext jc = this.getJAXBContext();
+
+			// buildup document model
+			final JAXBElement<Mets> webserviceElement = new JAXBElement<Mets>(new QName("http://www.loc.gov/METS/", "mets"), Mets.class, null, mets);
+
+			// create a marshaller
+			final Marshaller marshaller = jc.createMarshaller();
+			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+			marshaller.setProperty(Marshaller.JAXB_SCHEMA_LOCATION, "http://www.loc.gov/METS/ http://www.loc.gov/standards/mets/mets.xsd");
+			
+			/*
+			 * configure namespace
+			 */
+			final NamespacePrefixMapper npmapper = new NamespacePrefixMapper() {
+
+				private final String[] namespace_decls = new String[] {
+						"mets", "http://www.loc.gov/METS/",
+						"bib", "http://www.bibsonomy.org/2010/11/BibSonomy",
+						"puma", "http://puma.uni-kassel.de/2010/11/PUMA-SWORD",
+						"xsi", "http://www.w3.org/2001/XMLSchema-instance",
+						"xlink", "http://www.w3.org/1999/xlink"
+				};
+
+				@Override
+				public String getPreferredPrefix(final String arg0, final String arg1, final boolean arg2) {
+					return null;
+				}
+
+				@Override
+				public String[] getContextualNamespaceDecls() {
+					return this.namespace_decls;
+				}
+
+				@Override
+				public String[] getPreDeclaredNamespaceUris2() {
+					return this.namespace_decls;
+				}
+
+			};
+			marshaller.setProperty("com.sun.xml.bind.namespacePrefixMapper", npmapper);
+
+			if (this.validateXMLOutput) {
+				// validate the XML produced by the marshaller
+				marshaller.setSchema(schema);
+			}
+
+			// marshal to the writer
+			marshaller.marshal(webserviceElement, writer);
+		} catch (final JAXBException e) {
+			final Throwable linkedException = e.getLinkedException();
+			if (present(linkedException) && (linkedException.getClass() == SAXParseException.class)) {
+				final SAXParseException ex = (SAXParseException) linkedException;
+				throw new BadRequestOrResponseException(
+						"Error while parsing XML (Line " + ex.getLineNumber() + ", Column "
+						+ ex.getColumnNumber() + ": " + ex.getMessage()
+				);				
+			}						
+			throw new InternServerException(e.toString());
+		}
+	}
+
+}
--- bibsonomy-openaccess/src/main/java/de/unikassel/puma/openaccess/sword/MetsBibTexMLGenerator.java
+++ bibsonomy-openaccess/src/main/java/de/unikassel/puma/openaccess/sword/MetsBibTexMLGenerator.java
@@ -1,44 +1,21 @@
 package de.unikassel.puma.openaccess.sword;
 
-import static org.bibsonomy.util.ValidationUtils.present;
-
 import java.io.StringWriter;
-import java.io.Writer;
 import java.util.ArrayList;
 import java.util.GregorianCalendar;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Locale;
-import java.util.Map.Entry;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBElement;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
 import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.XMLGregorianCalendar;
-import javax.xml.namespace.QName;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.bibsonomy.common.exceptions.InternServerException;
-import org.bibsonomy.database.systemstags.SystemTagsUtil;
 import org.bibsonomy.model.BibTex;
 import org.bibsonomy.model.Document;
-import org.bibsonomy.model.PersonName;
-import org.bibsonomy.model.Resource;
 import org.bibsonomy.model.User;
-import org.bibsonomy.model.util.PersonNameUtils;
-import org.bibsonomy.rest.exceptions.BadRequestOrResponseException;
 import org.bibsonomy.rest.renderer.UrlRenderer;
-import org.bibsonomy.rest.renderer.impl.JAXBRenderer;
-import org.bibsonomy.rest.renderer.xml.BibtexType;
-import org.bibsonomy.rest.renderer.xml.TagType;
 import org.bibsonomy.util.XmlUtils;
-import org.xml.sax.SAXParseException;
-
-import com.sun.xml.bind.marshaller.NamespacePrefixMapper;
 
 import de.unikassel.puma.openaccess.sword.renderer.xml.DivType;
 import de.unikassel.puma.openaccess.sword.renderer.xml.DivType.Fptr;
@@ -54,7 +31,6 @@ import de.unikassel.puma.openaccess.sword.renderer.xml.MetsType.MetsHdr;
 import de.unikassel.puma.openaccess.sword.renderer.xml.MetsType.MetsHdr.Agent;
 import de.unikassel.puma.openaccess.sword.renderer.xml.ObjectFactory;
 import de.unikassel.puma.openaccess.sword.renderer.xml.PumaPost;
-import de.unikassel.puma.openaccess.sword.renderer.xml.PumaUserType;
 import de.unikassel.puma.openaccess.sword.renderer.xml.StructMapType;
 
 
@@ -79,7 +55,7 @@ public class MetsBibTexMLGenerator {
 	private List<String> filenameList = null; 
 	private User user = null; 
 
-	private final PumaRenderer xmlRenderer;
+	private final METSRenderer xmlRenderer;
 
 	/**
 	 * @return the _user
@@ -96,7 +72,7 @@ public class MetsBibTexMLGenerator {
 	}
 
 	public MetsBibTexMLGenerator(final UrlRenderer urlRenderer) {
-		this.xmlRenderer = new PumaRenderer(urlRenderer);
+		this.xmlRenderer = new METSRenderer(urlRenderer);
 		this.post = new PumaData<BibTex>();
 	}
 
@@ -139,207 +115,6 @@ public class MetsBibTexMLGenerator {
 		this.post.setPhdoralexam(pumaData.getPhdoralexam());
 		this.post.setSponsors(pumaData.getSponsors());
 		this.post.setAdditionaltitle(pumaData.getAdditionaltitle());
-	}
-
-
-	private class PumaRenderer extends JAXBRenderer {
-
-		public PumaRenderer(final UrlRenderer urlRenderer) {
-			super(urlRenderer);
-		}
-
-		protected PumaPost createPumaPost(final PumaData<? extends Resource> pumaData, final User userData)	throws InternServerException {
-			final PumaPost myPost = new PumaPost();
-			this.fillXmlPost(myPost, pumaData.getPost());
-
-			/*
-			 * delete unwanted data from post
-			 * - remove all system tags. they should not be sent to repository
-			 */
-			final Iterator<TagType> tagIterator = myPost.getTag().iterator();
-			while (tagIterator.hasNext()) {
-				final TagType tag = tagIterator.next();
-
-				if (SystemTagsUtil.isSystemTag(tag.getName())) {
-					tagIterator.remove();
-				}
-			}
-			
-			/*
-			 * remove url. there is no need for this url to be present in repository
-			 */
-			final BibtexType bibtex = myPost.getBibtex();
-			bibtex.setUrl(null);
-			myPost.setBibtex(bibtex);
-
-			/*
-			 * add more user informations
-			 */
-			if (null != userData) {
-				if (null == myPost.getUser()) {
-					myPost.setUser(new PumaUserType());
-				}
-				myPost.getUser().setName(userData.getName());
-				myPost.getUser().setRealname(userData.getRealname());
-				myPost.getUser().setEmail(userData.getEmail());
-				myPost.getUser().setId(userData.getLdapId());
-			}
-
-			/*
-			 * add additional metadata
-			 */
-			final Resource resource = pumaData.getPost().getResource();
-			if (resource instanceof BibTex) {
-				final BibTex bibtexResource = (BibTex) resource;
-				bibtexResource.parseMiscField();
-				if (null != myPost.getBibtex()) {
-					if (null != bibtexResource.getMiscField("isbn")) {
-						myPost.setISBN(bibtexResource.getMiscField("isbn"));
-					} 
-					if (null != bibtexResource.getMiscField("issn")) {
-						myPost.setISSN(bibtexResource.getMiscField("issn"));
-					} 
-					if (null != bibtexResource.getMiscField("doi")) {
-						myPost.setDOI(bibtexResource.getMiscField("doi"));
-					} 
-					if (null != bibtexResource.getMiscField("location")) {
-						myPost.setLocation(bibtexResource.getMiscField("location"));
-					} 
-					if (null != bibtexResource.getMiscField("dcc")) {
-						myPost.setDCC(bibtexResource.getMiscField("dcc"));
-					} 
-				}
-
-				if (present(pumaData.getAuthor())) {
-					for (final PersonName personName : pumaData.getAuthor()) {
-						myPost.getAuthor().add(PersonNameUtils.serializePersonName(personName));
-					}
-				}
-
-				if (null != pumaData.getExaminstitution()) {
-					myPost.setExaminstitution(pumaData.getExaminstitution());
-				}
-
-				if (null != pumaData.getExamreferee()) {
-					for (final String item : pumaData.getExamreferee()) {
-						myPost.getExamreferee().add(item);
-					}
-				}
-
-				if (null != pumaData.getPhdoralexam()) {
-					myPost.setPhdoralexam(pumaData.getPhdoralexam());
-				}
-
-				if (null != pumaData.getSponsors()) {
-					for (final String item : pumaData.getSponsors()) {
-						myPost.getSponsors().add(item);
-					}
-				}
-
-				if (null != pumaData.getAdditionaltitle()) {
-					for (final String item : pumaData.getAdditionaltitle()) {
-						myPost.getAdditionaltitle().add(item);
-					}
-				}
-
-				if (null != pumaData.getClassification()) {					
-					for (final Entry<String, List<String>> entry : pumaData.getClassification().entrySet()) {
-						for (final String listValue : entry.getValue() ) {
-							final PumaPost.Classification pptClassification = new PumaPost.Classification();
-							pptClassification.setName(entry.getKey().toLowerCase(Locale.getDefault()).replaceAll("/ /",""));
-							pptClassification.setValue(listValue);
-							myPost.getClassification().add(pptClassification);
-						}
-					}
-				}
-
-				/*
-				 * add publisher info / romeo sherpa 
-				 */
-				// TODO: get info from romeo/sherpa
-				//myPost.setPublisherinfo("");
-			}
-			return myPost;
-		}
-
-		@Override
-		protected JAXBContext getJAXBContext() throws JAXBException {
-			return JAXBContext.newInstance("org.bibsonomy.rest.renderer.xml:de.unikassel.puma.openaccess.sword.renderer.xml", this.getClass().getClassLoader());
-		}
-
-		/**
-		 * Initializes java xml bindings, builds the document and then marshalls
-		 * it to the writer.
-		 * @param writer 
-		 * @param mets 
-		 * 
-		 * @throws InternServerException
-		 *             if the document can't be marshalled
-		 */
-		private void serializeMets(final Writer writer, final Mets mets) throws InternServerException {
-			try {
-				// initialize context for java xml bindings
-				final JAXBContext jc = this.getJAXBContext();
-
-				// buildup document model
-				final JAXBElement<Mets> webserviceElement = new JAXBElement<Mets>(new QName("http://www.loc.gov/METS/", "mets"), Mets.class, null, mets);
-
-				// create a marshaller
-				final Marshaller marshaller = jc.createMarshaller();
-				marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-				marshaller.setProperty(Marshaller.JAXB_SCHEMA_LOCATION, "http://www.loc.gov/METS/ http://www.loc.gov/standards/mets/mets.xsd");
-				
-				/*
-				 * configure namespace
-				 */
-				final NamespacePrefixMapper npmapper = new NamespacePrefixMapper() {
-
-					private final String[] namespace_decls = new String[] {
-							"mets", "http://www.loc.gov/METS/",
-							"bib", "http://www.bibsonomy.org/2010/11/BibSonomy",
-							"puma", "http://puma.uni-kassel.de/2010/11/PUMA-SWORD",
-							"xsi", "http://www.w3.org/2001/XMLSchema-instance",
-							"xlink", "http://www.w3.org/1999/xlink"
-					};
-
-					@Override
-					public String getPreferredPrefix(final String arg0, final String arg1, final boolean arg2) {
-						return null;
-					}
-
-					@Override
-					public String[] getContextualNamespaceDecls() {
-						return this.namespace_decls;
-					}
-
-					@Override
-					public String[] getPreDeclaredNamespaceUris2() {
-						return this.namespace_decls;
-					}
-
-				};
-				marshaller.setProperty("com.sun.xml.bind.namespacePrefixMapper", npmapper);
-
-				if (this.validateXMLOutput) {
-					// validate the XML produced by the marshaller
-					marshaller.setSchema(schema);
-				}
-
-				// marshal to the writer
-				marshaller.marshal(webserviceElement, writer);
-			} catch (final JAXBException e) {
-				final Throwable linkedException = e.getLinkedException();
-				if (present(linkedException) && (linkedException.getClass() == SAXParseException.class)) {
-					final SAXParseException ex = (SAXParseException) linkedException;
-					throw new BadRequestOrResponseException(
-							"Error while parsing XML (Line " + ex.getLineNumber() + ", Column "
-							+ ex.getColumnNumber() + ": " + ex.getMessage()
-					);				
-				}						
-				throw new InternServerException(e.toString());
-			}
-		}
-
 	}
 
 	public String generateMets() {
