897e3ded49a6392504c84ae068441f2fff940720
Jens Illig <jil@cs.uni-kassel.de>
2013-04-30 17:37:01 +0200

    +httpspretendingfilter for saml

---
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/filters/HttpsPretendingFilter.java
@@ -0,0 +1,79 @@
+package org.bibsonomy.webapp.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+/**
+ * Wraps the request and overrides getScheme to always return https if enabled.
+ * 
+ * @author Jens Illig
+ * @version $Id$
+ */
+public class HttpsPretendingFilter implements Filter {
+	
+	private boolean enabled;
+	
+	private String pathPrefix;
+	
+	@Override
+	public void destroy() {
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		if ((enabled == false) || (pathPrefix != null) && (((HttpServletRequest) request).getServletPath().startsWith(pathPrefix) == false)) {
+			chain.doFilter(request, response);
+			return;
+		}
+		
+		HttpServletRequestWrapper reqProxy = new HttpServletRequestWrapper((HttpServletRequest)request) {
+			@Override
+			public String getScheme() {
+				return "https";
+			}
+		};
+		chain.doFilter(reqProxy, response);
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+	
+	/**
+	 * @return the enabled
+	 */
+	public boolean isEnabled() {
+		return this.enabled;
+	}
+
+	/**
+	 * @param enabled the enabled to set
+	 */
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	/**
+	 * @return the pathPrefix
+	 */
+	public String getPathPrefix() {
+		return this.pathPrefix;
+	}
+
+	/**
+	 * @param pathPrefix the pathPrefix to set
+	 */
+	public void setPathPrefix(String pathPrefix) {
+		this.pathPrefix = pathPrefix;
+	}
+
+
+}
