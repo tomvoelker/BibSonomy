3c2529a265d2bfdb69d9ed5aaa9e4c9a90bffa60
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2013-04-30 18:46:32 +0200

    removed unused code

--- bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/jstor/JStorScraper.java
+++ bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/jstor/JStorScraper.java
@@ -25,17 +25,9 @@ package org.bibsonomy.scraper.url.kde.jstor;
 
 import static org.bibsonomy.util.ValidationUtils.present;
 
-import java.io.BufferedInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.StringWriter;
-import java.net.CookieManager;
-import java.net.HttpURLConnection;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -85,17 +77,17 @@ public class JStorScraper extends AbstractUrlScraper {
 		patterns.add(new Pair<Pattern, Pattern>(hostPattern, Pattern.compile(JSTOR_DISCOVER_PATH + ".*")));
 	}
 	
+	@Override
 	public String getInfo() {
 		return info;
 	}
 
-	protected boolean scrapeInternal(ScrapingContext sc) throws ScrapingException {
-
+	@Override
+	protected boolean scrapeInternal(final ScrapingContext sc) throws ScrapingException {
 		sc.setScraper(this);
 		
 		//also one of the cookies we need is named "tenacious"
-		HttpClient client = WebUtils.getHttpClient();
-		
+		final HttpClient client = WebUtils.getHttpClient();
 		String bibtexResult = null;
 		
 		try {
@@ -105,9 +97,11 @@ public class JStorScraper extends AbstractUrlScraper {
 			
 			//get the page content or at least get the cookies
 			GetMethod getMethod = new GetMethod(sc.getUrl().toExternalForm());
-			String page = WebUtils.getContentAsString(client, getMethod);
+			final String page = WebUtils.getContentAsString(client, getMethod);
 			
-			if(page == null) throw new ScrapingException("Cannot access requested location");
+			if (page == null) {
+				throw new ScrapingException("Cannot access requested location");
+			}
 
 			//first assume we are on export page and search for submit action
 			Matcher m = SUBMIT_ACTION_PATTERN.matcher(page);
@@ -133,12 +127,16 @@ public class JStorScraper extends AbstractUrlScraper {
 					} else {
 						//we havn't found the doi, so let's search for export link
 						m = EXPORT_LINK_PATTERN.matcher(page);
-						if (!m.find()) throw new ScrapingException("Cannot continue. JStor Scraper must get updated");
+						if (!m.find()) {
+							throw new ScrapingException("Cannot continue. JStor Scraper must get updated");
+						}
 						exportPageLink = m.group(1).replace("&amp;", "&");				}
 				}
 				
 				//is the export page link present?
-				if (!present(exportPageLink)) throw new ScrapingException("Cannot continue, finally not having submit action");
+				if (!present(exportPageLink)) {
+					throw new ScrapingException("Cannot continue, finally not having submit action");
+				}
 				
 				//download export page
 				getMethod = new GetMethod(exportPageLink);
@@ -148,16 +146,20 @@ public class JStorScraper extends AbstractUrlScraper {
 				m = SUBMIT_ACTION_PATTERN.matcher(downloadPage);
 				
 				//did we find it?
-				if (!m.find()) throw new ScrapingException("Downloaded export page but didn't find submit action");
+				if (!m.find()) {
+					throw new ScrapingException("Downloaded export page but didn't find submit action");
+				}
 				
 				submitAction = m.group(1);
 			}
 			
 			//get some data from the page and switch submit action
-			Matcher noDoiMatcher = SUBMIT_ACTION_NODOI_PATTERN.matcher(downloadPage);
-			Matcher doiMatcher = SUBMIT_ACTION_DOI_PATTERN.matcher(downloadPage);
-			if (!noDoiMatcher.find() || !doiMatcher.find()) throw new ScrapingException("Couldn't get required data for export form");
-			String noDoi = noDoiMatcher.group(1);
+			final Matcher noDoiMatcher = SUBMIT_ACTION_NODOI_PATTERN.matcher(downloadPage);
+			final Matcher doiMatcher = SUBMIT_ACTION_DOI_PATTERN.matcher(downloadPage);
+			if (!noDoiMatcher.find() || !doiMatcher.find()) {
+				throw new ScrapingException("Couldn't get required data for export form");
+			}
+			final String noDoi = noDoiMatcher.group(1);
 			URI actionURL;
 			if ("noDoi".equalsIgnoreCase(noDoi)) {
 				actionURL = getMethod.getURI();
@@ -166,117 +168,47 @@ public class JStorScraper extends AbstractUrlScraper {
 			}
 			
 			//post export form
-			PostMethod postMethod = new PostMethod();
+			final PostMethod postMethod = new PostMethod();
 			postMethod.setURI(actionURL);
 			postMethod.addParameter("redirectUri", getMethod.getPath());
 			postMethod.addParameter("noDoi", noDoi);
 			postMethod.addParameter("doi", doiMatcher.group(1));
 			bibtexResult = WebUtils.getPostContentAsString(client, postMethod);
 
-		} catch (IOException ex) {
+		} catch (final IOException ex) {
 			throw new ScrapingException(ex);
 		}
 		
-		//check the result
-		if (!present(bibtexResult)) throw new ScrapingException("Could not submit export form");
-		Matcher numberOfCitMatcher = NUMBER_CITS_EXPORTED_PATTERN.matcher(bibtexResult);
-		if (!numberOfCitMatcher.find()) throw new ScrapingException("no citations received");
-		int numberOfCit = Integer.parseInt(numberOfCitMatcher.group(1));
-		if (numberOfCit < 1) throw new ScrapingException("received " + numberOfCit + " citations");
+		// check the result
+		if (!present(bibtexResult)) {
+			throw new ScrapingException("Could not submit export form");
+		}
+		
+		final Matcher numberOfCitMatcher = NUMBER_CITS_EXPORTED_PATTERN.matcher(bibtexResult);
+		if (!numberOfCitMatcher.find()) {
+			throw new ScrapingException("no citations received");
+		}
+		
+		final int numberOfCit = Integer.parseInt(numberOfCitMatcher.group(1));
+		if (numberOfCit < 1) {
+			throw new ScrapingException("received " + numberOfCit + " citations");
+		}
+		
 		sc.setBibtexResult(bibtexResult);
 		return true;
-
 	}
 
-	/** FIXME: refactor
-	 * @param queryURL
-	 * @param cookie
-	 * @return
-	 * @throws IOException
-	 */
-	@SuppressWarnings("unused")
-	private String getContent(URL queryURL, String cookie) throws IOException {
-
-		HttpURLConnection urlConn = (HttpURLConnection) queryURL.openConnection();
-		urlConn.setAllowUserInteraction(false);
-		urlConn.setDoInput(true);
-		urlConn.setDoOutput(false);
-		urlConn.setUseCaches(false);
-		/*
-		 * set user agent (see http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html) since some 
-		 * pages require it to download content.
-		 */
-		urlConn.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)");
-
-		//insert cookie
-		urlConn.setRequestProperty("Cookie", cookie);
-
-		urlConn.connect();
-
-		StringWriter out = new StringWriter();
-		InputStream in = new BufferedInputStream(urlConn.getInputStream());
-		int b;
-		while ((b = in.read()) >= 0) {
-			out.write(b);
-		}
-		urlConn.disconnect();
-
-		return out.toString();
-	}
-
-	/** FIXME: refactor
-	 * @param queryURL
-	 * @return
-	 * @throws IOException
-	 */
-	@SuppressWarnings("unused")
-	private String getCookies(URL queryURL) throws IOException {
-		HttpURLConnection urlConn = null;
-
-		urlConn = (HttpURLConnection) queryURL.openConnection();
-
-		urlConn.setAllowUserInteraction(false);
-		urlConn.setDoInput(true);
-		urlConn.setDoOutput(false);
-		urlConn.setUseCaches(false);
-
-		/*
-		 * set user agent (see http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html) since some 
-		 * pages require it to download content.
-		 */
-		urlConn.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)");
-
-		//seems that the application needs this cookie even before getting the other cookies, strange :-\
-		urlConn.setRequestProperty("Cookie", "I2KBRCK=1");
-
-		urlConn.connect();
-		/*
-		 * extract cookie from connection
-		 */
-		List<String> cookies = urlConn.getHeaderFields().get("Set-Cookie");
-
-		StringBuffer cookieString = new StringBuffer();
-
-		for(String cookie : cookies) {
-			cookieString.append(cookie.substring(0, cookie.indexOf(";") + 1) + " ");
-		}
-
-		//This is neccessary, otherwise we don't get the Bibtex file.
-		cookieString.append("I2KBRCK=1");
-
-		urlConn.disconnect();
-
-		return cookieString.toString();
-	}
-
+	@Override
 	public List<Pair<Pattern, Pattern>> getUrlPatterns() {
 		return patterns;
 	}
 
+	@Override
 	public String getSupportedSiteName() {
 		return "JSTOR";
 	}
 
+	@Override
 	public String getSupportedSiteURL() {
 		return JSTOR_HOST_NAME;
 	}
