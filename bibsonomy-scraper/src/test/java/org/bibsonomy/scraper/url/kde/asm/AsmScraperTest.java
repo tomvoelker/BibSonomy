f284f98aad48f7c83eae718d67359947a5c1c98c
Haile Misgna Haile <haile.misgna@mu.edu.et>
2013-05-02 17:59:48 +0200

    This commit adds AsmScraperTest

---
+++ bibsonomy-scraper/src/test/java/org/bibsonomy/scraper/url/kde/asm/AsmScraperTest.java
@@ -0,0 +1,28 @@
+package org.bibsonomy.scraper.url.kde.asm;
+
+import static org.junit.Assert.assertTrue;
+
+import org.bibsonomy.scraper.UnitTestRunner;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * @author Haile
+ * @version $Id$
+ */
+public class AsmScraperTest {
+
+	/**
+	 * @param args
+	 */
+	/**
+	 * starts URL test with id url_259
+	 */
+	@Test
+	@Ignore
+	public void urlTestRun() {
+		UnitTestRunner runner = new UnitTestRunner();
+		assertTrue(runner.runSingleTest("url_259"));
+	}
+
+}
