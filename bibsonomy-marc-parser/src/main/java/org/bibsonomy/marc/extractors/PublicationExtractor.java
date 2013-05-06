c5dad66f68145aa5669a698f0c7cd59d5c765490
Lukas Hanke <lhanke@cs.uni-kassel.de>
2013-05-06 10:28:04 +0200

    + MARC PublicationExtractor

---
+++ bibsonomy-marc-parser/src/main/java/org/bibsonomy/marc/extractors/PublicationExtractor.java
@@ -0,0 +1,19 @@
+package org.bibsonomy.marc.extractors;
+
+import org.bibsonomy.marc.AttributeExtractor;
+import org.bibsonomy.marc.ExtendedMarcRecord;
+import org.bibsonomy.model.BibTex;
+
+/**
+ * @author Lukas
+ * @version $Id$
+ */
+public class PublicationExtractor implements AttributeExtractor {
+
+	@Override
+	public void extraxtAndSetAttribute(BibTex target, ExtendedMarcRecord src) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
