8f27e5ddc89498828ebfff58b15a0bc2b03bd541
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2013-05-08 14:21:16 +0200

    fixed url cleanup
    only working for the "how to cite this article" pages, please fix!

--- bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/hindawi/HindawiScraper.java
+++ bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/hindawi/HindawiScraper.java
@@ -2,8 +2,6 @@ package org.bibsonomy.scraper.url.kde.hindawi;
 
 import static org.bibsonomy.util.ValidationUtils.present;
 
-
-import java.io.IOException;
 import java.net.URL;
 import java.util.Collections;
 import java.util.List;
@@ -26,20 +24,27 @@ import org.bibsonomy.util.WebUtils;
  * @version $Id$
  */
 public class HindawiScraper extends AbstractUrlScraper{
-	private final Log log = LogFactory.getLog(HindawiScraper.class);
+	private static final Log log = LogFactory.getLog(HindawiScraper.class);
+	
 	private static final String SITE_NAME = "Hindawi Publishing Corporation";
-	private static final String SITE_URL = "hindawi.com";
-	private static final String INFO = "This scraper parses a publication page from the " + href(SITE_URL, SITE_NAME);	
+	private static final String SITE_URL = "http://hindawi.com";
+	private static final String INFO = "This scraper parses a publication page from the " + href(SITE_URL, SITE_NAME);
+	
 	private static final List<Pair<Pattern, Pattern>> PATTERNS = Collections.singletonList(new Pair<Pattern, Pattern>(Pattern.compile(".*" + "hindawi.com"), AbstractUrlScraper.EMPTY_PATTERN));
 
 	private static final Pattern DOWNLOADLINK = Pattern.compile("<a href=\"([^\"]*+)\">Download citation as EndNote</a>");
-	private static final int ID_GROUP = 1;
+
 	@Override
-	protected boolean scrapeInternal(ScrapingContext scrapingContext) throws ScrapingException {
+	protected boolean scrapeInternal(final ScrapingContext scrapingContext) throws ScrapingException {
 		scrapingContext.setScraper(this);
 		
 		try {
-			final String endNoteLink = extractURL(WebUtils.getContentAsString(scrapingContext.getUrl()));
+			// FIXME: this only works on urls ending with .../cta
+			/*
+			 * TODO: we could use the ids in the urls to build the download link!
+			 * Are their any guidelines what to do in this situation?
+			 */
+			final String endNoteLink = this.extractURL(WebUtils.getContentAsString(scrapingContext.getUrl()));
 
 			if (endNoteLink == null) {
 				log.error("can't parse publication URL");
@@ -47,29 +52,29 @@ public class HindawiScraper extends AbstractUrlScraper{
 			}
 			
 			final String endNote = WebUtils.getContentAsString(new URL(endNoteLink));
-			EndnoteToBibtexConverter converter = new EndnoteToBibtexConverter();
+			final EndnoteToBibtexConverter converter = new EndnoteToBibtexConverter();
 			
 			final String bibTex = converter.endnoteToBibtex(endNote);
 			
 			if (present(bibTex)) {
 				scrapingContext.setBibtexResult(bibTex);
 				return true;
-			} else {
-				throw new ScrapingFailureException("getting bibtex failed");
 			}
-
+			
+			throw new ScrapingFailureException("getting bibtex failed");
 		} catch (final Exception e) {
 			throw new InternalFailureException(e);
 		}
 	}
 	
-	private String extractURL(String content){
+	private String extractURL(final String content){
 		final Matcher matcher = DOWNLOADLINK.matcher(content);
 		if (matcher.find()) {
-			return matcher.group(ID_GROUP);
+			return matcher.group(1);
 		}
 		return null;
 	}
+	
 	@Override
 	public String getSupportedSiteName() {
 		return SITE_NAME;
