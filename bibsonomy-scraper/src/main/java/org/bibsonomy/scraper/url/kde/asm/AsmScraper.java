2952deea46148b64c8fbb701a5a55bc4ae0b1ee4
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2013-05-08 13:36:08 +0200

    fixed site url cleanup

--- bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/asm/AsmScraper.java
+++ bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/asm/AsmScraper.java
@@ -1,25 +1,34 @@
+/**
+ *
+ *  BibSonomy-Scraper - Web page scrapers returning BibTeX for BibSonomy.
+ *
+ *  Copyright (C) 2006 - 2013 Knowledge & Data Engineering Group,
+ *                            University of Kassel, Germany
+ *                            http://www.kde.cs.uni-kassel.de/
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 package org.bibsonomy.scraper.url.kde.asm;
 
-import static org.bibsonomy.util.ValidationUtils.present;
-
-import java.net.URL;
 import java.util.Collections;
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.bibsonomy.common.Pair;
-import org.bibsonomy.scraper.AbstractUrlScraper;
-import org.bibsonomy.scraper.ScrapingContext;
-import org.bibsonomy.scraper.converter.EndnoteToBibtexConverter;
-import org.bibsonomy.scraper.exceptions.InternalFailureException;
-import org.bibsonomy.scraper.exceptions.ScrapingException;
-import org.bibsonomy.scraper.exceptions.ScrapingFailureException;
 import org.bibsonomy.scraper.generic.CitationManagerScraper;
-import org.bibsonomy.scraper.url.kde.hindawi.HindawiScraper;
-import org.bibsonomy.util.WebUtils;
 
 /**
  * @author Haile
@@ -29,21 +38,24 @@ public class AsmScraper extends CitationManagerScraper {
 	
 	private static final Pattern DOWNLOAD_LINK_PATTERN = Pattern.compile("<a href=\"(.+?)\">Download to citation manager</a>");
 	private static final String SITE_NAME = "American Society for microbiology";
-	private static final String SITE_URL = "aem.asm.org";
+	private static final String SITE_URL = "http://asm.org";
 	private static final String INFO = "This scraper parses a publication page from the " + href(SITE_URL, SITE_NAME);
 	private static final List<Pair<Pattern, Pattern>> URL_PATTERNS = Collections.singletonList(new Pair<Pattern, Pattern>(
 			Pattern.compile(".*" + "asm.org"), 
 			Pattern.compile("/content" + ".*")
 		));
 
+	@Override
 	public String getSupportedSiteName() {
 		return SITE_NAME;
 	}
 
+	@Override
 	public String getSupportedSiteURL() {
 		return SITE_URL;
 	}
 
+	@Override
 	public String getInfo() {
 		return INFO;
 	}
