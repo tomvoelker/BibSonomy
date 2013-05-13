55cae80a5ea2a4ce2aebdaf21e1054152f86b12f
Haile Misgna Haile <haile.misgna@mu.edu.et>
2013-05-13 19:02:29 +0200

    http://asm.org is replaced by http://journals.asm.org/

--- bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/asm/AsmScraper.java
+++ bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/asm/AsmScraper.java
@@ -38,7 +38,7 @@ public class AsmScraper extends CitationManagerScraper {
 	
 	private static final Pattern DOWNLOAD_LINK_PATTERN = Pattern.compile("<a href=\"(.+?)\">Download to citation manager</a>");
 	private static final String SITE_NAME = "American Society for Microbiology";
-	private static final String SITE_URL = "http://asm.org";
+	private static final String SITE_URL = "http://journals.asm.org/";
 	private static final String INFO = "This scraper parses a publication page from the " + href(SITE_URL, SITE_NAME);
 	private static final List<Pair<Pattern, Pattern>> URL_PATTERNS = Collections.singletonList(new Pair<Pattern, Pattern>(
 			Pattern.compile(".*" + "asm.org"), 
