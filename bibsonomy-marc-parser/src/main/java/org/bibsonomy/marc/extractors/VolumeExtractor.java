8c7667cecba899ba007637e295cfcd70fce4116f
Lukas Hanke <lhanke@cs.uni-kassel.de>
2013-05-14 14:00:08 +0200

    + VolumeExtractor MARC parser

---
+++ bibsonomy-marc-parser/src/main/java/org/bibsonomy/marc/extractors/VolumeExtractor.java
@@ -0,0 +1,20 @@
+package org.bibsonomy.marc.extractors;
+
+import org.bibsonomy.marc.AttributeExtractor;
+import org.bibsonomy.marc.ExtendedMarcRecord;
+import org.bibsonomy.model.BibTex;
+
+/**
+ * @author Lukas
+ * @version $Id$
+ */
+public class VolumeExtractor implements AttributeExtractor {
+
+	@Override
+	public void extraxtAndSetAttribute(BibTex target, ExtendedMarcRecord src) {
+		/*
+		 * extract volume
+		 */
+	}
+
+}
