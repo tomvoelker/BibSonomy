7988fbffd164189b41299aaa1b839407b507a2ee
Jens Illig <jil@cs.uni-kassel.de>
2013-05-15 21:30:42 +0200

    +format comment

--- bibsonomy-marc-parser/src/main/java/org/bibsonomy/marc/extractors/TitleExtractor.java
+++ bibsonomy-marc-parser/src/main/java/org/bibsonomy/marc/extractors/TitleExtractor.java
@@ -52,6 +52,51 @@ public class TitleExtractor implements AttributeExtractor {
  * Typ: Pica: 13H 0k k->Konferenz -> 090? f->festschrift Pica: 002@ 2.char b ->
  * marc? 006?
  * 
+ * 002@ 2.Position c oder d -> mehrbändiges werk (mvbook)
+ * 	http://www.hebis.de/de/1publikationen/arbeitsmaterialien/hebis-handbuch/kategorien/kategorien_detail.php?we_editObject_ID=2253
+ * 002@ 2.Position a -> einbändiges werk (book)
+ * 002@ 2.Position a  -> teil eines mehrbändigen W. (book)
+ * 	f -> gesamtname (036c $a) + ": " + teilname=normalname (021a $a)
+ *  F -> nur teiltitel (021a $a)
+ * 
+ * mvbook für gesammte sammlungen
+ * book für einzelne bücher aus der sammlung
+ * 
+ * 002@
+ * 	b (Zeitschrift) -> periodical
+ * 		021a als titel
+ * 		029A oder F
+ * 		033A
+ * 		031@  $a (marc 362 a) (wichtig)
+ *	o (Aufsatz) ->?
+ *	
+ * series extractor:
+ * 
  * 
  * TODO: titel bei mehrbändern (knuth)
+ * 
+ * 
+ * PICA Format
+ * lok: <...> <ILN (library number)>
+ * 
+ * exp: (exemplare)?
+ *
+ * 
+ * 013H $0 u -> phdthesis (oder ggf book wenn publisher 33A $n vorhanden ist? - zu heikel)
+ *   037C $a ins note feld)
+ *   
+ *   
+ * 013H/0X = k-> proceedings
+ *   dann (nein immer) marc 111 oder 110 als organization und nicht als autor
+ * 
+ * organisation
+ * 029A/0X
+ * 029A/0X
+ * 029A/0X
+ * 
+ * 
+ * kein autor sondern organisation:
+ * 	pica 029A $a <$c>    sonst 029F $8 <$g>          sonst 029E $8 <$g> / $b    marc 710 genauso 
+ * 
+ * 002@ 1.char == 'O' (online resource) -> 009Q $u als url
  */
