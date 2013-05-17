6c1ffb8b1f2015c84a22305c59d2a765bebdd8f9
Haile Misgna Haile <haile.misgna@mu.edu.et>
2013-05-17 11:53:53 +0200

    Modified in order to support all journals from sage journal.

--- bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/sage/SageJournalScraper.java
+++ bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/sage/SageJournalScraper.java
@@ -23,88 +23,27 @@
 
 package org.bibsonomy.scraper.url.kde.sage;
 
-import static org.bibsonomy.util.ValidationUtils.present;
-
-import java.net.URL;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.bibsonomy.common.Pair;
-import org.bibsonomy.scraper.AbstractUrlScraper;
-import org.bibsonomy.scraper.ScrapingContext;
-import org.bibsonomy.scraper.exceptions.InternalFailureException;
-import org.bibsonomy.scraper.exceptions.ScrapingException;
-import org.bibsonomy.scraper.exceptions.ScrapingFailureException;
-import org.bibsonomy.util.WebUtils;
+import org.bibsonomy.scraper.generic.CitationManagerScraper;
 
 /**
  * @author wla
  * @version $Id$
  */
-public class SageJournalScraper extends AbstractUrlScraper {
+public class SageJournalScraper extends CitationManagerScraper {
 
-	private final Log log = LogFactory.getLog(SageJournalScraper.class);
-
-	private static final String SITE_NAME = "Journal of Information Science";
-	private static final String SITE_URL = "http://jis.sagepub.com/";
-	private static final String info = "This scraper parses a publication page from the " + href(SITE_URL, SITE_NAME);
-
-	private static final List<Pair<Pattern, Pattern>> patterns = new ArrayList<Pair<Pattern,Pattern>>();
-
-	static {
-		patterns.add(new Pair<Pattern, Pattern>(Pattern.compile(".*" + "jis.sagepub.com"), AbstractUrlScraper.EMPTY_PATTERN));
-	}
-	
-	private static final Pattern URL_ID_PATTERN = Pattern.compile("\\d+/\\d+/\\d+");
-
-	private static final String BIBTEX_URL = "http://jis.sagepub.com/citmgr?type=bibtex&gca=spjis;";
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.bibsonomy.scraper.AbstractUrlScraper#scrapeInternal(org.bibsonomy.scraper.ScrapingContext)
-	 */
-	@Override
-	protected boolean scrapeInternal(final ScrapingContext scrapingContext) throws ScrapingException {
-		final URL scrapingUrl = scrapingContext.getUrl();
-		try {
-			scrapingContext.setScraper(this);
-			final String id = extractId(scrapingUrl);
-			if (!present(id)) {
-				log.error("can't parse publication id");
-				throw new ScrapingException("URL pattern not supported yet");
-			}
-			String bibTex = WebUtils.getContentAsString(BIBTEX_URL + id);
-
-			// some abstracts has multiple spaces
-			bibTex = bibTex.replaceAll(" +", " ");
-
-			if (present(bibTex)) {
-				scrapingContext.setBibtexResult(bibTex);
-				return true;
-			} else {
-				throw new ScrapingFailureException("getting bibtex failed");
-			}
-		} catch (final Exception e) {
-			throw new InternalFailureException(e);
-		}
-	}
-
-	/**
-	 * 
-	 * @param url
-	 * @return paper id recovered form url
-	 */
-	private String extractId(final URL url) {
-		final Matcher matcher = URL_ID_PATTERN.matcher(url.toString());
-		if (matcher.find()) {
-			return matcher.group();
-		}
-		return null;
-	}
+	private static final Pattern DOWNLOAD_LINK_PATTERN = Pattern.compile("<a href=\"(.+?)\">Download to citation manager</a>");
+	private static final String SITE_NAME = "Sage Journal";
+	private static final String SITE_URL = "http://online.sagepub.com/";
+	private static final String INFO = "This scraper parses a publication page from the " + href(SITE_URL, SITE_NAME);
+	private static final List<Pair<Pattern, Pattern>> URL_PATTERNS = Collections.singletonList(new Pair<Pattern, Pattern>(
+			Pattern.compile(".*" + "sagepub.com"), 
+			Pattern.compile("/content" + ".*")
+		));
 
 	@Override
 	public String getSupportedSiteName() {
@@ -118,12 +57,18 @@ public class SageJournalScraper extends AbstractUrlScraper {
 
 	@Override
 	public String getInfo() {
-		return info;
+		return INFO;
+	}
+
+	@Override
+	public Pattern getDownloadLinkPattern() {
+		return DOWNLOAD_LINK_PATTERN;
 	}
 
 	@Override
 	public List<Pair<Pattern, Pattern>> getUrlPatterns() {
-		return patterns;
+		return URL_PATTERNS;
 	}
+
 
 }
