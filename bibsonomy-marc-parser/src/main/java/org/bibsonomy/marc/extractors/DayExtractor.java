db819e65d4dc00122eaa8133c5147c2726af7bc9
Nils Raabe <nraabe@cs.uni-kassel.de>
2013-05-21 13:52:49 +0200

    marc to bibtex parser: + work on

---
+++ bibsonomy-marc-parser/src/main/java/org/bibsonomy/marc/extractors/DayExtractor.java
@@ -0,0 +1,19 @@
+package org.bibsonomy.marc.extractors;
+
+import org.bibsonomy.marc.AttributeExtractor;
+import org.bibsonomy.marc.ExtendedMarcRecord;
+import org.bibsonomy.model.BibTex;
+
+/**
+ * @author nilsraabe
+ * @version $Id$
+ */
+public class DayExtractor implements AttributeExtractor {
+
+	@Override
+	public void extraxtAndSetAttribute(BibTex target, ExtendedMarcRecord src) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
