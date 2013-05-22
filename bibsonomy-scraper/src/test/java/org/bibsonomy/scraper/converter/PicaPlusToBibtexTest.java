e72d364f78e7ff96d0dc694fa0160f6d038aeee3
BibSonomy <webmaster@bibsonomy.org>
2013-05-22 14:47:02 +0200

    + assert equals

--- bibsonomy-scraper/src/test/java/org/bibsonomy/scraper/converter/PicaPlusToBibtexTest.java
+++ bibsonomy-scraper/src/test/java/org/bibsonomy/scraper/converter/PicaPlusToBibtexTest.java
@@ -23,6 +23,8 @@
 
 package org.bibsonomy.scraper.converter;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Collection;
@@ -36,20 +38,22 @@ import org.junit.Test;
  * @version $Id$
  */
 public class PicaPlusToBibtexTest {
+	
 	/**
 	 * more like a main
 	 */
 	@Test
 	public void readPicaPlus() {
 		try {
-			PicaPlusReader reader = new PicaPlusReader();
-			BufferedReader br = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("dwl20100116-01p.txt"), "UTF-8"));
-			Collection<PicaRecord> picas = reader.parseRawPicaPlus(br);
+			final PicaPlusReader reader = new PicaPlusReader();
+			final BufferedReader br = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("dwl20100116-01p.txt"), "UTF-8"));
+			final Collection<PicaRecord> picas = reader.parseRawPicaPlus(br);
 			br.close();
-			for (PicaRecord p : picas) {
+			assertEquals(133, picas.size());
+			for (final PicaRecord p : picas) {
 				System.out.println(PicaParser.getBibRes(p, "bla"));
 			}
-		} catch (Exception ex) {
+		} catch (final Exception ex) {
 			throw new RuntimeException(ex);
 		}
 	}
