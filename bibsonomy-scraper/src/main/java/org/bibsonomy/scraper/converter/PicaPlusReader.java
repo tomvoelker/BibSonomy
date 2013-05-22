0f7a1f2cd1e19fa270aaf142928633e75d42b290
BibSonomy <webmaster@bibsonomy.org>
2013-05-22 14:50:20 +0200

    cleanup + TODO

--- bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/converter/PicaPlusReader.java
+++ bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/converter/PicaPlusReader.java
@@ -42,7 +42,10 @@ import org.bibsonomy.scraper.converter.picatobibtex.Row;
  * @version $Id$
  */
 public class PicaPlusReader {
+	private static final String PPN = "PPN:";
+
 	private static final Log log = LogFactory.getLog(PicaPlusReader.class);
+	
 	private static final Pattern CATEGORY_PATTERN = Pattern.compile("^\\d{3}+.*");
 	private static final String DEFAULT_SUBFIELD_SEPARATOR = "\u0192";
 
@@ -51,30 +54,31 @@ public class PicaPlusReader {
 	 * @return list of PicaRecords in the data
 	 * @throws IOException if some io stuff in the reader goes wrong
 	 */
-	public Collection<PicaRecord> parseRawPicaPlus(BufferedReader r) throws IOException {
-		List<PicaRecord> rVal = new ArrayList<PicaRecord>();
+	public Collection<PicaRecord> parseRawPicaPlus(final BufferedReader r) throws IOException {
+		final List<PicaRecord> picaRecords = new ArrayList<PicaRecord>();
 		PicaRecord openPicaRecord = null;
 		String s;
-		Collection<Row> rowsBeforeFirstPPN = new ArrayList<Row>();
+		final Collection<Row> rowsBeforeFirstPPN = new ArrayList<Row>();
 		while ((s = r.readLine()) != null) {
 			if (s.length() == 0) {
 				continue;
 			}
-			if (s.contains("PPN:")) {
+			if (s.contains(PPN)) {
 				if (rowsBeforeFirstPPN.size() > 0) {
 					log.warn("rows before first ppn row: " + rowsBeforeFirstPPN);
 					rowsBeforeFirstPPN.clear();
 				}
 				//a new record begins
 				if (openPicaRecord != null) {
-					rVal.add(openPicaRecord);
+					picaRecords.add(openPicaRecord);
 				}
-				String ppn = s.substring(s.indexOf("PPN:") + 5, s.indexOf("PPN:") + 5 + 9);
-				Row ppnRow = readRow("003@ \u01920"+ppn);
+				final int ppnIndex = s.indexOf(PPN) + 5; // TODO: comment why + 5
+				final String ppn = s.substring(ppnIndex, ppnIndex + 9); // TODO: comment why + 9
+				final Row ppnRow = readRow("003@ \u01920" + ppn);
 				openPicaRecord = new PicaRecord();
 				openPicaRecord.addRow(ppnRow);
 			} else if (CATEGORY_PATTERN.matcher(s).matches()) {
-				Row row = readRow(s);
+				final Row row = readRow(s);
 				if ((openPicaRecord != null) && (row != null)) {
 					openPicaRecord.addRow(row);
 				} else {
@@ -85,21 +89,22 @@ public class PicaPlusReader {
 		if (rowsBeforeFirstPPN.size() > 0) {
 			// hebis does not use PPN delimiters and describes only a single entity
 			openPicaRecord = new PicaRecord();
-			for (Row row : rowsBeforeFirstPPN) {
+			for (final Row row : rowsBeforeFirstPPN) {
 				openPicaRecord.addRow(row);
 			}
-			rVal.add(openPicaRecord);
+			picaRecords.add(openPicaRecord);
 		}
-		return rVal;
+		return picaRecords;
 	}
 	
 	private Row readRow(String line) {
 		// get Field
-		int pos = line.indexOf(" ");
-		String[] data = line.substring(0, pos).split("/");
-		String tag = data[0];
-		Row rVal = new Row(tag);
+		final int pos = line.indexOf(" ");
+		final String[] data = line.substring(0, pos).split("/");
+		final String tag = data[0];
+		final Row rVal = new Row(tag);
 		if (data.length > 1) {
+			// TODO: remove?!
 			//rVal.setOccurrence(data[1]);
 		}
 	
@@ -107,8 +112,8 @@ public class PicaPlusReader {
 		line = line.replace(" $", DEFAULT_SUBFIELD_SEPARATOR);
 
 		// get Subfields
-		String[] subfields = line.substring(pos + 1).split(DEFAULT_SUBFIELD_SEPARATOR);
-		for (String sub : subfields) {
+		final String[] subfields = line.substring(pos + 1).split(DEFAULT_SUBFIELD_SEPARATOR);
+		for (final String sub : subfields) {
 			if (sub.length() > 1) {				
 				rVal.addSubField("$" + sub.substring(0, 1), sub.substring(1));
 			}
