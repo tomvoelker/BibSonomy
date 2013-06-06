375ea8812ad647b32aade03234d15c3c10895215
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2013-06-06 16:27:12 +0200

    renamed styles.js to cslstyles.js

--- bibsonomy-webapp/src/main/webapp/resources/javascript/merged/discussion.js
+++ bibsonomy-webapp/src/main/webapp/resources/javascript/merged/discussion.js
@@ -19,4 +19,4 @@ includeJavaScript("/resources/javascript/citeproc/csllocales.js");
 includeJavaScript("/resources/javascript/citeproc/xmldom.js");
 //includeJavaScript("/resources/javascript/citeproc/xmle4x.js");
 includeJavaScript("/resources/javascript/citeproc/citeproc.js");
-includeJavaScript("/resources/javascript/citeproc/styles.js");
+includeJavaScript("/resources/javascript/citeproc/cslstyles.js");
