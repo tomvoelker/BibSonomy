a1ef39853d575e669259012cf78cfebbbb283b61
Jens Illig <jil@cs.uni-kassel.de>
2013-07-22 11:55:35 +0200

    ->extended test

--- bibsonomy-webapp/src/additional/sh/check_puma.sh
+++ bibsonomy-webapp/src/additional/sh/check_puma.sh
@@ -1,5 +1,21 @@
-#!/bin/bash
+#!/bin/bash 
 PUMA_HOST=$1
-PUMA_URL="http://$PUMA_HOST/postBookmark"
-wget -t 5 --timeout 30 -O /dev/null -o /dev/null --no-cache "$PUMA_URL"
-echo $?
+FILE_NAME=$2
+#PUMA_URL="http://$PUMA_HOST/postBookmark"
+PUMA_URL="http://$PUMA_HOST/swrc/"
+#TIMES=$3
+#SLEEP=`expr 60 / $TIMES`
+SLEEP=30
+for I in `seq 1 2`; do
+	LINES=`wget -t 5 --timeout 15 -O /dev/stdout -o /dev/null --no-cache "$PUMA_URL" | grep 'Ontology' | wc -l`
+	if [ $LINES -gt 0 ]; then
+		touch "$FILE_NAME"
+	else
+		if [ -e "$FILE_NAME" ]; then
+			rm "$FILE_NAME"
+		fi
+	fi
+	if [ $I -lt 2 ]; then
+		sleep $SLEEP
+	fi
+done
