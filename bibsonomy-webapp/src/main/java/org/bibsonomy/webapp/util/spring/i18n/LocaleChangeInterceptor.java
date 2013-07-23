6b97ed4e85b4fc195f0cde7f1f416fa5c564effa
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2013-07-23 10:40:04 +0200

    added own LocaleChangeInterceptor to fix ?lang=xyz?abc exceptions

---
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/util/spring/i18n/LocaleChangeInterceptor.java
@@ -0,0 +1,29 @@
+package org.bibsonomy.webapp.util.spring.i18n;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * To fix feature request SPR-9456
+ * @see "https://jira.springsource.org/browse/SPR-9456"
+ * 
+ * @author dzo
+ * @version $Id$
+ */
+public class LocaleChangeInterceptor extends org.springframework.web.servlet.i18n.LocaleChangeInterceptor {
+	private static final Log log = LogFactory.getLog(LocaleChangeInterceptor.class);
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException {
+		try {
+			return super.preHandle(request, response, handler);
+		} catch (final IllegalArgumentException e) {
+			log.info("parameter for lang was not correct.", e);
+		}
+		return true;
+	}
+}
