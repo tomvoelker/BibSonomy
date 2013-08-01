f947d30ac2dde280b948bf326190358f2d86fc17
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2013-08-01 19:50:34 +0200

    disabled xml escaper

--- bibsonomy-layout/src/main/java/net/sf/jabref/export/layout/format/XMLEscaper.java
+++ bibsonomy-layout/src/main/java/net/sf/jabref/export/layout/format/XMLEscaper.java
@@ -23,15 +23,13 @@
 
 package net.sf.jabref.export.layout.format;
 
-import org.apache.commons.lang.StringEscapeUtils;
-
 import net.sf.jabref.export.layout.LayoutFormatter;
 
 public class XMLEscaper implements LayoutFormatter{
 
 	@Override
 	public String format(String arg0) {
-		return StringEscapeUtils.escapeXml(arg0);
+		return arg0; // TODO: StringEscapeUtils.escapeXml(arg0);
 	}
 
 }
