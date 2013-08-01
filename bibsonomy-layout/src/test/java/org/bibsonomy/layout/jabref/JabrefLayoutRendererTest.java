b18233424e6fd3957aa7597b6170928098cf7d7f
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2013-08-01 14:46:02 +0200

    added tests for jabref renderer

--- bibsonomy-layout/src/test/java/org/bibsonomy/layout/jabref/JabrefLayoutRendererTest.java
+++ bibsonomy-layout/src/test/java/org/bibsonomy/layout/jabref/JabrefLayoutRendererTest.java
@@ -26,21 +26,24 @@ package org.bibsonomy.layout.jabref;
 import static org.junit.Assert.assertEquals;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FilenameFilter;
+import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
-import net.sf.jabref.BibtexEntry;
-import net.sf.jabref.BibtexEntryType;
-import net.sf.jabref.bst.VM;
-
+import org.apache.commons.io.FilenameUtils;
 import org.bibsonomy.model.BibTex;
 import org.bibsonomy.model.Post;
 import org.bibsonomy.model.User;
-import org.bibsonomy.model.util.PersonNameUtils;
 import org.bibsonomy.model.util.PersonNameParser.PersonListParserException;
+import org.bibsonomy.model.util.PersonNameUtils;
 import org.bibsonomy.services.URLGenerator;
-import org.junit.Ignore;
+import org.bibsonomy.testutil.TestUtils;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 /**
  * 
@@ -48,108 +51,93 @@ import org.junit.Test;
  * @version: $Id$
  * $Author$
  */
+@RunWith(Parameterized.class)
 public class JabrefLayoutRendererTest {
-
-    @Test
-    public void testInit() {
-	getRenderer();
-    }
-    
-    @Test
-    @Ignore
-    public void testRenderInternal() throws Exception {
-	    final JabrefLayoutRenderer renderer = getRenderer();
-	    renderer.setUrlGenerator(new URLGenerator("http://www.bibsonomy.org"));
-	    final JabrefLayout layout = renderer.getLayout("dblp", "foo");
-	    final StringBuffer renderedLayout = renderer.renderLayout(layout, getPosts(), false);
-	    final String cleanedLayout = renderedLayout.toString().replaceAll(" +", " ");//.replaceAll("\n", "\\\\n").replaceAll("\"", "\\\\\"");
-	    assertEquals("<?xml version=\"1.0\"?>\n<!DOCTYPE dblp SYSTEM \"http://www.informatik.uni-trier.de/~ley/db/about/dblp.dtd\">\n<!-- This file was exported from BibSonomy, http://www.bibsonomy.org -->\n\n<dblp><inproceedings mdate=\"2009\" key=\"benz2009managing\">\n <author>Dominik Benz</author>\n<author>Folke Eisterlehner</author>\n<author>Andreas Hotho</author>\n<author>Robert J&#228;schke</author>\n<author>Beate Krause</author>\n<author>Gerd Stumme</author> \n <editor>Ciro Cattuto and Giancarlo Ruffo and Filippo Menczer</editor>\n <address>New York&#44; NY&#44; USA</address>\n\n <title>Managing publications and bookmarks with BibSonomy</title>\n <booktitle>HT &#39;09: Proceedings of the 20th ACM Conference on Hypertext and Hypermedia</booktitle>\n <pages>323&#x2013;324</pages>\n <year>2009</year>\n\n\n\n <month>June</month>\n <url>http://portal.acm.org/citation.cfm&#63;doid=1557914.1557969&#35;</url>\n\n <publisher>ACM</publisher>\n\n\n\n\n <isbn>978&#45;1&#45;60558&#45;486&#45;7</isbn>\n\n\n</inproceedings>\n \n\n\n\n<book mdate=\"\" key=\"knuth\">\n <author>Donald E. Knuth</author> \n\n\n\n <title>The Art of Computer Programming</title>\n\n\n\n\n\n\n\n\n\n <publisher>Addison Wesley</publisher>\n\n\n\n\n\n\n\n</book>\n \n\n\n\n</dblp>\n", cleanedLayout);
-    }
-
-
-    private JabrefLayoutRenderer getRenderer() {
-	final JabrefLayoutRenderer renderer = new JabrefLayoutRenderer();
-	renderer.setDefaultLayoutFilePath("org/bibsonomy/layout/jabref");
-	return renderer;
-    }
-
-
-    private List<Post<BibTex>> getPosts() throws PersonListParserException {
-	final List<Post<BibTex>> posts = new LinkedList<Post<BibTex>>();
-
-	final BibTex bibtex = new BibTex();
-	bibtex.setTitle("The Art of Computer Programming");
-	bibtex.setAuthor(PersonNameUtils.discoverPersonNames("Donald E. Knuth"));
-	bibtex.setPublisher("Addison Wesley");
-	bibtex.setEntrytype("book");
-	bibtex.setBibtexKey("knuth");
-
-	final Post<BibTex> post = new Post<BibTex>();
-	post.setResource(bibtex);
-
-	final User u = new User();
-	u.setName("Wiglaf Droste");
-	post.setUser(u);
-
-	posts.add(post);
-
-	/*
-	 * next one
-	 */
-	final BibTex b2 = new BibTex(); 
-	b2.setEntrytype("inproceedings");
-	b2.setBibtexKey("benz2009managing"); 
-	b2.setAddress("New York, NY, USA");
-	b2.setAuthor(PersonNameUtils.discoverPersonNames("Dominik Benz and Folke Eisterlehner and Andreas Hotho and Robert Jäschke and Beate Krause and Gerd Stumme"));
-	b2.setBooktitle("HT '09: Proceedings of the 20th ACM Conference on Hypertext and Hypermedia");
-	b2.setEditor(PersonNameUtils.discoverPersonNames("Ciro Cattuto and Giancarlo Ruffo and Filippo Menczer"));
-	b2.setPages("323--324");
-	b2.setPublisher("ACM");
-	b2.setTitle("Managing publications and bookmarks with BibSonomy");
-	b2.setUrl("http://portal.acm.org/citation.cfm?doid=1557914.1557969#");
-	b2.setYear("2009");
-	b2.setMisc("isbn = {978-1-60558-486-7},\ndoi = {10.1145/1557914.1557969}");
-	b2.setMonth("jun");
-	b2.setPrivnote("This is a test note"); 
-	b2.setAbstract("In this demo we present BibSonomy, a social bookmark and publication sharing system.");
-
-	final Post<BibTex> p2 = new Post<BibTex>();
-	p2.setResource(b2);
-	p2.setUser(u);
-	p2.setDescription("Our demo at HT 2009");
+	private static final String LAYOUT_ENTRYTYPE_SPLIT = "#";
 	
-	posts.add(p2);
-
-	return posts;
-    }
-
-    @Test
-    @Ignore
-    public void testBstVM() {
-	try {
-	    final File bst = new File("/home/rja/paper/papers/2007/issi/lni.bst");
-	    final VM vm = new VM(bst);
-
-	    final List<BibtexEntry> bibtexs = new LinkedList<BibtexEntry>();
-	    final BibtexEntry entry = new BibtexEntry();
-
-	    entry.setField("title", "Als ich ein kleiner Junge war");
-	    entry.setField("author", "Erich Kästner");
-	    entry.setField("year", "2007");
-	    entry.setType(BibtexEntryType.BOOK);
-	    bibtexs.add(entry);
-
-
-	    final String result = vm.run(bibtexs);
-
-	    System.out.println("-------------------------------------------------------------------");
-	    System.out.println(result);
-	    System.out.println("-------------------------------------------------------------------");
-
-	} catch (final Exception ex) {
-	    System.out.println(ex);
+	@Parameters
+	public static Collection<Object[]> data() {
+		final File folder = new File("src/test/resources/jabref-layout-tests");
+		final File[] listOfFiles = folder.listFiles(new FilenameFilter() {
+			
+			@Override
+			public boolean accept(File dir, String name) {
+				return name.endsWith(".layoutResult");
+			}
+		});
+		
+		final Collection<Object[]> files = new LinkedList<Object[]>();
+		for (File file : listOfFiles) {
+			files.add(new Object[] { file });
+		}
+		return files;
 	}
+	
+	private static JabrefLayoutRenderer getRenderer() {
+		final JabrefLayoutRenderer renderer = new JabrefLayoutRenderer();
+		renderer.setDefaultLayoutFilePath("org/bibsonomy/layout/jabref");
+		renderer.setUrlGenerator(new URLGenerator("http://www.bibsonomy.org"));
+		return renderer;
+	}
+	
+	private final File layoutTest;
+	
+	public JabrefLayoutRendererTest(File layoutTest) {
+		this.layoutTest = layoutTest;
+	}
+	
+    @Test
+    public void testRender() throws Exception {
+	    final JabrefLayoutRenderer renderer = getRenderer();
+	    String fileName = FilenameUtils.removeExtension(this.layoutTest.getName());
+	    String layoutName = fileName;
+	    String entryType = "article";
+	    
+		if (fileName.contains(LAYOUT_ENTRYTYPE_SPLIT)) {
+	    	String[] parts = fileName.split(LAYOUT_ENTRYTYPE_SPLIT);
+	    	layoutName = parts[0];
+	    	entryType = parts[1];
+	    }
+	    final JabrefLayout layout = renderer.getLayout(layoutName, "foo");
+	    final StringBuffer renderedLayout = renderer.renderLayout(layout, getPosts(entryType), false);
+	    
+	    assertEquals("layout: " + layoutName + ", entrytype: " + entryType, TestUtils.toString(new FileInputStream(this.layoutTest)).trim(), renderedLayout.toString().replaceAll("\\r", "").trim());
     }
 
+    private List<Post<BibTex>> getPosts(String entryType) throws PersonListParserException {
+    	final List<Post<BibTex>> posts = new LinkedList<Post<BibTex>>();
+
+    	final User u = new User();
+    	u.setName("Wiglaf Droste");
+
+		/*
+		 * next one
+		 */
+		final BibTex b2 = new BibTex(); 
+		b2.setEntrytype(entryType);
+		b2.setBibtexKey("benz2009managing"); 
+		b2.setAddress("New York, NY, USA");
+		b2.setAuthor(PersonNameUtils.discoverPersonNames("Dominik Benz and Folke Eisterlehner and Andreas Hotho and Robert Jäschke and Beate Krause and Gerd Stumme"));
+		b2.setBooktitle("HT '09: Proceedings of the 20th ACM Conference on Hypertext and Hypermedia");
+		b2.setEditor(PersonNameUtils.discoverPersonNames("Ciro Cattuto and Giancarlo Ruffo and Filippo Menczer"));
+		b2.setPages("323--324");
+		b2.setPublisher("ACM");
+		b2.setTitle("Managing publications and bookmarks with BibSonomy");
+		b2.setUrl("http://portal.acm.org/citation.cfm?doid=1557914.1557969#");
+		b2.setYear("2009");
+		b2.setMisc("isbn = {978-1-60558-486-7},\ndoi = {10.1145/1557914.1557969}");
+		b2.setMonth("jun");
+		b2.setPrivnote("This is a test note"); 
+		b2.setAbstract("In this demo we present BibSonomy, a social bookmark and publication sharing system.");
+	
+		final Post<BibTex> p2 = new Post<BibTex>();
+		p2.setResource(b2);
+		p2.setUser(u);
+		p2.setDescription("Our demo at HT 2009");
+		
+		posts.add(p2);
+	
+		return posts;
+    }
 }
 
