fc82b453607819fcfacf7cdd1a8f8adf6310f38d
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2013-08-06 13:28:24 +0200

    + javadoc

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/HashExampleCommand.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/HashExampleCommand.java
@@ -4,19 +4,26 @@ import org.bibsonomy.model.BibTex;
 import org.bibsonomy.model.Post;
 
 /**
- * TODO: Beschreibung.
+ * command for the hash example
+ * just holding a reference to a publication post
+ * 
  * @author janus
  */
 public class HashExampleCommand extends BaseCommand {
-    
-    private Post<BibTex> post;
 
-    public Post<BibTex> getPost() {
-        return post;
-    }
+	private Post<BibTex> post;
 
-    public void setPost(Post<BibTex> post) {
-        this.post = post;
-    }
-    
+	/**
+	 * @return the post
+	 */
+	public Post<BibTex> getPost() {
+		return this.post;
+	}
+
+	/**
+	 * @param post the post to set
+	 */
+	public void setPost(Post<BibTex> post) {
+		this.post = post;
+	}
 }
