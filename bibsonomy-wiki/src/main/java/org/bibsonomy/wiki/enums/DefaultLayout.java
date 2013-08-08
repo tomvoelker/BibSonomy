edb75947de3549d0c0f0fe336ac5b801e23c3453
janus <devnull@localhost>
2013-08-08 14:37:40 +0200

    inserted the correct identifiers for the layout added a quite basic default layout.

--- bibsonomy-wiki/src/main/java/org/bibsonomy/wiki/enums/DefaultLayout.java
+++ bibsonomy-wiki/src/main/java/org/bibsonomy/wiki/enums/DefaultLayout.java
@@ -2,34 +2,35 @@ package org.bibsonomy.wiki.enums;
 
 /**
  * Enum to summarize the default layouts (i.e. wiki texts) available for the CV
- * wiki. These map to message keys defined in messages*.properties (there, the
- * wiki texts are stored)
+ * wiki. These map to files in the wiki submodule. the encoded strings represent
+ * the file prefix for a layout.
  * 
  * @author Dominik Benz, benz@cs.uni-kassel.de
  * @author Bernd Terbrack
+ * @author Thomas Niebler
  */
 public enum DefaultLayout {
 
 	/** default user layout 2 (english) */
-	LAYOUT_DEFAULT_II_EN("cv.layout.user.def2en"),
+	LAYOUT_DEFAULT_II_EN("user2en"),
 
 	/** default user layout 2 (german) */
-	LAYOUT_DEFAULT_II_GER("cv.layout.user.def2ger"),
+	LAYOUT_DEFAULT_II_GER("user2de"),
 
 	/** default user layout 1 (english) */
-	LAYOUT_DEFAULT_I_EN("cv.layout.user.def1en"),
+	LAYOUT_DEFAULT_I_EN("user1en"),
 
 	/** default user layout 2 (german) */
-	LAYOUT_DEFAULT_I_GER("cv.layout.user.def1ger"),
+	LAYOUT_DEFAULT_I_GER("user1de"),
 
 	/** current user layout (i.e. the one ) */
 	LAYOUT_CURRENT(""),
 
 	/** default group layout 1 (english) */
-	LAYOUT_G_DEFAULT_I_EN("cv.layout.group.def1en"),
+	LAYOUT_G_DEFAULT_I_EN("group1en"),
 
 	/** default group layout 1 (german) */
-	LAYOUT_G_DEFAULT_I_GER("cv.layout.group.def1ger");
+	LAYOUT_G_DEFAULT_I_GER("group1de");
 
 	/** the message key which holds the default cv wiki text */
 	private final String ref;
