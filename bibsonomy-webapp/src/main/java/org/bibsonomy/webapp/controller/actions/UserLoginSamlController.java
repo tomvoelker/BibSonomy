b3e4592f5ed59cc516531f9509bb0224effdc8d4
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2013-08-08 16:10:29 +0200

    + todo - logging

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/actions/UserLoginSamlController.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/actions/UserLoginSamlController.java
@@ -1,32 +1,29 @@
 package org.bibsonomy.webapp.controller.actions;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.bibsonomy.webapp.command.BaseCommand;
 import org.bibsonomy.webapp.util.MinimalisticController;
 import org.bibsonomy.webapp.util.View;
+import org.bibsonomy.webapp.util.spring.security.filter.SamlLoginFilter;
 import org.bibsonomy.webapp.view.ExtendedRedirectView;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 /**
+ * This controller is neccessary to process the success of a saml login
+ * TODO: document why we are not using the {@link AuthenticationSuccessHandler} provided
+ * by spring security @see {@link SamlLoginFilter}
+ * 
  * @author nilsraabe
  * @version $Id$
  */
 public class UserLoginSamlController implements MinimalisticController<BaseCommand> {
 
-	private static final Log log = LogFactory.getLog(UserLoginSamlController.class);
-	
 	@Override
 	public BaseCommand instantiateCommand() {
-
-		log.info("UserLoginSamlCommand in UserLoginSamlController initialized");
 		return new BaseCommand();
 	}
 
 	@Override
 	public View workOn(final BaseCommand command) {
-		
-		log.info("UserLoginSamlController starts WorkOn");
-		
 		return new ExtendedRedirectView("/");
 	}
 
