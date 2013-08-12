008acb91365ee1659a3877454b078038dc086b01
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2013-08-12 17:23:56 +0200

    fixed perl path fixed utf8 bibtex -> publication rename

--- bibsonomy-batch/src/build/batch_scripts/batch_suggestion_title.pl
+++ bibsonomy-batch/src/build/batch_scripts/batch_suggestion_title.pl
@@ -1,8 +1,8 @@
-#!/Users/nilsraabe/perl5/perlbrew/perls/perl-5.16.0/bin/perl
+#!/usr/bin/perl
 
 use DBI();
 use English;
-use Common qw(debug get_slave get_master check_running);
+use Common qw(debug get_slave check_running);
 use DBI qw(:utils);
 use Encode;
 
@@ -12,9 +12,6 @@ use Encode;
 
 # connect to database as slave
 my $slave = get_slave();
-
-binmode FILE, ":utf8";
-binmode STDOUT, ":utf8";			
 
 ################ GET BOOKMARK TITLE ################
 print "Reading bookmark title ...\n";
@@ -28,26 +25,27 @@ $stm_select_bookmark_names->execute();
 $slave->commit;
 
 # go over all public title + ratings and store it in a text file
-open (FILE, ">bookmark_title.txt") or die $!;
-	while (my @bookmark = $stm_select_bookmark_names->fetchrow_array ) {
-		if ($bookmark[0] ne "") {
+open (FILE, "> " . $ENV{'SUGGESTION_PATH'} . "/bookmark_title.txt") or die $!;
+binmode FILE, ":utf8";
+while (my @bookmark = $stm_select_bookmark_names->fetchrow_array ) {
+	if ($bookmark[0] ne "") {
+		
+		$title = $bookmark[0];
+		$title =~ s/\r\n/ /;
+		$title =~ s/\s{1}\s+/ /;
+		$title =~ s/\R//g;
 			
-			$title = decode_utf8($bookmark[0]);
-			$title =~ s/\r\n/ /;
-			$title =~ s/\s{1}\s+/ /;
-			$title =~ s/\R//g;
-			
-			$rating = decode_utf8($bookmark[1]);
+		$rating = $bookmark[1];
 
-			print FILE "$title\n$rating\n";
-		}
+		print FILE "$title\n$rating\n";
 	}
+}
 close (FILE);
-$slave->commit;
+
 print "Bookmark complete !\n";
 
 ################ GET BIBTEX TITLE ################
-print "Reading bibtex title ...\n";
+print "Reading publication title ...\n";
 
 # prepare statements
 # get all public bibtex title + rating from the bibtex table
@@ -58,21 +56,21 @@ $stm_select_bibtex_names->execute();
 $slave->commit;
 
 # go over all public title + ratings and store it in a text file
-open (FILE, ">bibtex_title.txt") or die $!;
-	while (my @bibtex = $stm_select_bibtex_names->fetchrow_array ) {
-		if ($bibtex[0] ne "") {
-			
-			my $title = decode_utf8($bibtex[0]);
-			$title =~ s/\r\n/ /;
-			$title =~ s/\s{1}\s+/ /;
-			$title =~ s/\R//g;
-			
-			$rating = decode_utf8($bibtex[1]);
+open (FILE, "> " . $ENV{'SUGGESTION_PATH'} . "/publication_title.txt") or die $!;
+binmode FILE, ":utf8";
+while (my @bibtex = $stm_select_bibtex_names->fetchrow_array ) {
+	if ($bibtex[0] ne "") {
+		my $title = $bibtex[0];
+		$title =~ s/\r\n/ /;
+		$title =~ s/\s{1}\s+/ /;
+		$title =~ s/\R//g;
+		
+		$rating = $bibtex[1];
 
-			print FILE "$title\n$rating\n";
-		}
+		print FILE "$title\n$rating\n";
 	}
+}
 close (FILE);
 
-print "Bibtex complete !\n";
-$slave->commit;
+print "publication complete !\n";
+$slave->disconnect();
