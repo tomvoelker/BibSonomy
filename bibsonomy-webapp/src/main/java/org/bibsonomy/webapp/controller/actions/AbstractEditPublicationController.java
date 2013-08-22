d849d0bfc93a68e8cc8dd4829036f6eae4f1782e
BibSonomy <webmaster@bibsonomy.org>
2013-08-22 17:26:12 +0200

    removed url from message key

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/actions/AbstractEditPublicationController.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/actions/AbstractEditPublicationController.java
@@ -266,7 +266,7 @@ public abstract class AbstractEditPublicationController<COMMAND extends EditPubl
 
 	@Override
 	protected void setDuplicateErrorMessage(final Post<BibTex> post, final Errors errors) {
-		errors.rejectValue("post.resource.title", "error.field.valid.url.alreadyStoredPublication", "You already have this publication in your collection. ");
+		errors.rejectValue("post.resource.title", "error.field.valid.alreadyStoredPublication", "You already have this publication in your collection. ");
 	}
 
 	/**
