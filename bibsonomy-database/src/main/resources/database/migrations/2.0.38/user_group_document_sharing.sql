8938133899a4e20c29c756714cb675f28405c33c
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2013-08-23 14:34:13 +0200

    added status quo update script

--- bibsonomy-database/src/main/resources/database/migrations/2.0.38/user_group_document_sharing.sql
+++ bibsonomy-database/src/main/resources/database/migrations/2.0.38/user_group_document_sharing.sql
@@ -3,3 +3,6 @@ ALTER TABLE groups ADD COLUMN `userSharedDocuments` tinyint(1) default '0';
 
 -- remove the on update condition on start_date
 ALTER TABLE groups CHANGE `start_date` `start_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- restore the status quo for groups sharing documents
+UPDATE groups JOIN groupids USING (`group`) SET groups.userSharedDocuments = TRUE WHERE groupids.sharedDocuments = TRUE;
