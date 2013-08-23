d002f1131746c82e564728cb52f54a22ee2ccf20
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2013-08-23 14:25:20 +0200

    added change query to remove on update trigger f?r start_date

--- bibsonomy-database/src/main/resources/database/migrations/2.0.38/user_group_document_sharing.sql
+++ bibsonomy-database/src/main/resources/database/migrations/2.0.38/user_group_document_sharing.sql
@@ -1,1 +1,5 @@
+-- add userSharedDocuments column to membership table
 ALTER TABLE groups ADD COLUMN `userSharedDocuments` tinyint(1) default '0';
+
+-- remove the on update condition on start_date
+ALTER TABLE groups CHANGE `start_date` `start_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
