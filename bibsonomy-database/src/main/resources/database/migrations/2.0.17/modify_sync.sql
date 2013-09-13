85d947745bda407cf7afba64743bcdbe4e1a4ee1
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2013-09-13 13:55:30 +0200

    fixing strategy

--- bibsonomy-database/src/main/resources/database/migrations/2.0.17/modify_sync.sql
+++ bibsonomy-database/src/main/resources/database/migrations/2.0.17/modify_sync.sql
@@ -4,3 +4,5 @@ ALTER TABLE sync_data CHANGE COLUMN `status` `status` varchar(8) NOT NULL;
 ALTER TABLE sync_data ADD COLUMN `info` varchar(255) default NULL;
 ALTER TABLE sync ADD COLUMN `content_type` tinyint(1) unsigned default 0;
 ALTER TABLE sync ADD COLUMN `direction` varchar(4) default 'both';
+
+ALTER TABLE sync ADD COLUMN `strategy` varchar(2) default 'lw';
