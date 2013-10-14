37fa46eb8be69e6bb1d332087ba3c870e22b542f
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2013-10-14 17:13:55 +0200

    config of ReportingMailInformationService

---
+++ bibsonomy-database/src/test/java/org/bibsonomy/testutil/DummyInformationService.java
@@ -0,0 +1,18 @@
+package org.bibsonomy.testutil;
+
+import org.bibsonomy.model.Post;
+import org.bibsonomy.model.Resource;
+import org.bibsonomy.services.information.InformationService;
+
+/**
+ * @author dzo
+ * @version $Id$
+ */
+public class DummyInformationService implements InformationService {
+
+	@Override
+	public void createdPost(String username, Post<? extends Resource> post) {
+		// ignore
+	}
+
+}
