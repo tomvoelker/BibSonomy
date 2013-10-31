5d2fd2594e526d6c5dae8469a8d8ee0eb411a056
Matthias Gerecht <mge@cs.uni-kassel.de>
2013-10-31 14:07:25 +0100

    Added chain for post history

---
+++ bibsonomy-database/src/main/java/org/bibsonomy/database/managers/chain/resource/get/GetResourcesByParent.java
@@ -0,0 +1,30 @@
+package org.bibsonomy.database.managers.chain.resource.get;
+
+import java.util.List;
+
+import org.bibsonomy.common.enums.FilterEntity;
+import org.bibsonomy.database.common.DBSession;
+import org.bibsonomy.database.managers.chain.resource.ResourceChainElement;
+import org.bibsonomy.database.params.ResourceParam;
+import org.bibsonomy.model.Post;
+import org.bibsonomy.model.Resource;
+
+/**
+ * @author matthias gerecht
+ * @version $Id$
+ */
+public class GetResourcesByParent<R extends Resource, P extends ResourceParam<R>> extends ResourceChainElement<R, P>  {
+
+    //@Override
+	@Override
+	protected List<Post<R>> handle(final P param, final DBSession session) {
+		return this.databaseManager.getPostsWithHistory(param.getHash(),param.getRequestedUserName(), param.getUserName(), param.getFilter(), param.getLimit(), 
+				param.getOffset(), param.getSystemTags(), session);
+	}
+
+	@Override
+	protected boolean canHandle(final P param) {
+		return (param.getFilter() == FilterEntity.POSTS_HISTORY_BIBTEX) || (param.getFilter() == FilterEntity.POSTS_HISTORY_GOLD);
+	}
+
+}
