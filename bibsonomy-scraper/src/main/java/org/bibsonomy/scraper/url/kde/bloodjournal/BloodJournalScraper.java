34ac6b2dae2dd94f9f9f2906d05a1e5db783cf85
Haile Misgna Haile <haile.misgna@mu.edu.et>
2013-11-04 15:50:59 +0100

    BloodJournalScraper - Scrapes citation from http://bloodjournal.hematologylibrary.org.

---
+++ bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/bloodjournal/BloodJournalScraper.java
@@ -0,0 +1,50 @@
+package org.bibsonomy.scraper.url.kde.bloodjournal;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import org.bibsonomy.common.Pair;
+import org.bibsonomy.scraper.generic.CitationManagerScraper;
+
+/**
+ * @author Haile
+ * @version $Id$
+ */
+public class BloodJournalScraper extends CitationManagerScraper {
+	
+	private static final Pattern DOWNLOAD_LINK_PATTERN = Pattern.compile("<a href=\"(.+?)\">Download to citation manager</a>");
+	private static final String SITE_NAME = "Blood - JOURNAL OF THE AMERICAN SOCIETY OF HEMATOLOGY";
+	private static final String SITE_URL = "http://bloodjournal.hematologylibrary.org/";
+	private static final String INFO = "This scraper parses a publication page from the " + href(SITE_URL, SITE_NAME);
+	private static final List<Pair<Pattern, Pattern>> URL_PATTERNS = Collections.singletonList(new Pair<Pattern, Pattern>(
+			Pattern.compile(".*" + "hematologylibrary.org"), 
+			Pattern.compile("/content" + ".*")
+		));
+
+	@Override
+	public String getSupportedSiteName() {
+		return SITE_NAME;
+	}
+
+	@Override
+	public String getSupportedSiteURL() {
+		return SITE_URL;
+	}
+
+	@Override
+	public String getInfo() {
+		return INFO;
+	}
+
+	@Override
+	public Pattern getDownloadLinkPattern() {
+		return DOWNLOAD_LINK_PATTERN;
+	}
+
+	@Override
+	public List<Pair<Pattern, Pattern>> getUrlPatterns() {
+		return URL_PATTERNS;
+	}
+}
+
