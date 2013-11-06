c3610db2fd8d6b4bba41c49492bd817d9c558738
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2013-11-06 10:40:47 +0100

    added remotetest category

--- bibsonomy-scraper/src/test/java/org/bibsonomy/scraper/url/kde/bloodjournal/BloodJournalScraperTest.java
+++ bibsonomy-scraper/src/test/java/org/bibsonomy/scraper/url/kde/bloodjournal/BloodJournalScraperTest.java
@@ -3,12 +3,15 @@ package org.bibsonomy.scraper.url.kde.bloodjournal;
 import static org.junit.Assert.assertTrue;
 
 import org.bibsonomy.scraper.UnitTestRunner;
+import org.bibsonomy.scraper.junit.RemoteTest;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 /**
  * @author Haile
  * @version $Id$
  */
+@Category(RemoteTest.class)
 public class BloodJournalScraperTest {
 	/**
 	 * @param args
