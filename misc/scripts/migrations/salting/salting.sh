0a0c2004b93a7fd646acf406d2617deacdd85f70
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2013-11-10 16:08:27 +0100

    fixed typo
    added forgotten common file
    
    
    disabled backup and migration of database schema

--- misc/scripts/migrations/salting/salting.sh
+++ misc/scripts/migrations/salting/salting.sh
@@ -7,7 +7,9 @@ export MASTER_SOCK=/var/run/mysql.sock
 export MASTER_USER=***
 export MASTER_PASS=***
 
-mysqldump -P $MASTER_PORT -u $MASTER_USER -p$MASTER_PASS $MASTER_DB user pendingUser log_user > usertable_backup.sql
-mysql -u $MASTER_USER -p$MASTER_PASS $MASTER_DB -P $MASTER_PORT -h $MASTER_HOST < add_salt_to_user_tables.sql
+# for backup enable the following line
+# mysqldump -P $MASTER_PORT -u $MASTER_USER -p$MASTER_PASS $MASTER_DB user pendingUser log_user > usertable_backup.sql
+# for schema migration enable the following line
+# mysql -u $MASTER_USER -p$MASTER_PASS $MASTER_DB -P $MASTER_PORT -h $MASTER_HOST < add_salt_to_user_tables.sql
 
-./addSaltToPassoword.pl
+./addSaltToPassword.pl
