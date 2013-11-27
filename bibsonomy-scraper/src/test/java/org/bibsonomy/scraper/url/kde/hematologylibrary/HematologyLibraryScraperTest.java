b6c76feb0bd1fd271d3097bc189cd481f8fb40d9
Haile Misgna Haile <haile.misgna@mu.edu.et>
2013-11-27 17:45:18 +0100

    BloodJournalScraper is renamed as HematologyLibraryScraper because the BloodJournalScraper scrapes from all journals of hematology.org.

---
+++ bibsonomy-scraper/src/test/java/org/bibsonomy/scraper/url/kde/hematologylibrary/HematologyLibraryScraperTest.java
@@ -0,0 +1,23 @@
+package org.bibsonomy.scraper.url.kde.hematologylibrary;
+
+import static org.junit.Assert.assertTrue;
+
+import org.bibsonomy.scraper.UnitTestRunner;
+import org.junit.Test;
+
+/**
+ * @author Haile
+ * @version $Id$
+ */
+public class HematologyLibraryScraperTest {
+	/**
+	 * @param args
+	 */
+	/**
+	 * starts URL test with id url_264
+	 */
+	@Test
+	public void urlTestRun() {
+		assertTrue(UnitTestRunner.runSingleTest("url_264"));
+	}
+}
