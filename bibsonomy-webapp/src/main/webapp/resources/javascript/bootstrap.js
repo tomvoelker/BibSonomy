4be13beb1b5a85c30e20c9a3a1a8e05b632bc989
Sebastian Boettger <boettger@cs.uni-kassel.de>
2014-04-10 14:29:34 +0200

    postlist bibtex

--- bibsonomy-webapp/src/main/webapp/resources/javascript/bootstrap.js
+++ bibsonomy-webapp/src/main/webapp/resources/javascript/bootstrap.js
@@ -1,19 +1,19 @@
 $(function() {
 	
 	$('.collapse').collapse();
-	
-	$('.media-body .desc').ellipsis();
+
 	$('.toggleAdvanced').click(function(event){
 		event.preventDefault();
 		$(this).parents('li.media').find('.advanced').toggle();
 	});
-
+	
+	/*
+	 * 
 	$('.post-popover, .help-popover').popover({ 
 	    html : true,
 	    trigger: "focus",
 	    container: 'body',
 	    placement: function() {
-	    	console.log($(this));
 			return 'right';	    	
 	    },
 	    title: function() {
@@ -23,4 +23,44 @@ $(function() {
 	      	return $(this).next().next().html();
 	    }
 	});
+	*/
+//	$('.post-popover, .help-popover').popover({ 
+//	    html : true,
+//	    trigger: "focus",
+//	    container: 'body',
+//	    placement: function() {
+//    		return ($(window).width() >= 990) ? 'right' : 'auto';
+//    	},	    	
+//	    title: function() {
+//	      	$(this).next().html();
+//	    },
+//	    content: function() {
+//	      	return $(this).next().next().html();
+//	    },
+//		delay: 0
+//	});
+
+	$('.system-tags-link').popover({
+		animation: false,
+		html: true,
+		trigger: 'manual',
+		placement : 'right',
+		delay: 0,
+		title: function() {
+			return $(this).next().find('.popover-title').html();
+		},
+		content: function() {
+			return $(this).next().find('.popover-content').html();
+		}
+		
+	});
+	
+	$('.system-tags-link').mouseenter(function(event){
+		$(this).popover('show');
+	});
+	
+	$('.popover-close').click(function(event){
+		$(this).parent().parent().prev().popover('hide');
+	});
+	
 });
