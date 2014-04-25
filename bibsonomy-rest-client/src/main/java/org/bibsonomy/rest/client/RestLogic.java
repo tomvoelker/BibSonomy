c122f95ee52a2b64d6226cb1640b14b9aed33a4f
Thomas Niebler <thomas.niebler@uni-wuerzburg.de>
2014-04-25 21:47:40 +0200

    implemented getConceptDetails, fixing #1952

--- bibsonomy-rest-client/src/main/java/org/bibsonomy/rest/client/RestLogic.java
+++ bibsonomy-rest-client/src/main/java/org/bibsonomy/rest/client/RestLogic.java
@@ -82,6 +82,7 @@ import org.bibsonomy.rest.client.queries.delete.DeleteSyncDataQuery;
 import org.bibsonomy.rest.client.queries.delete.DeleteUserQuery;
 import org.bibsonomy.rest.client.queries.delete.RemoveUserFromGroupQuery;
 import org.bibsonomy.rest.client.queries.delete.UnpickClipboardQuery;
+import org.bibsonomy.rest.client.queries.get.GetConceptDetailsQuery;
 import org.bibsonomy.rest.client.queries.get.GetFriendsQuery;
 import org.bibsonomy.rest.client.queries.get.GetGroupDetailsQuery;
 import org.bibsonomy.rest.client.queries.get.GetGroupListQuery;
@@ -419,7 +420,19 @@ public class RestLogic implements LogicInterface {
 
 	@Override
 	public Tag getConceptDetails(final String conceptName, final GroupingEntity grouping, final String groupingName) {
-		throw new UnsupportedOperationException();
+		GetConceptDetailsQuery query = new GetConceptDetailsQuery(conceptName);
+		
+		if (GroupingEntity.GROUP.equals(grouping)) {
+			query.setGroupName(groupingName);
+			return execute(query);
+		} else if (GroupingEntity.FRIEND.equals(grouping)) {
+			query.setUserName(groupingName);
+			return execute(query);
+		} else {
+			log.error("grouping entity " + grouping.name() + " not yet supported in RestLogic implementation.");
+		}
+
+		return null;
 	}
 
 	@Override
