f43b0ac7087518b68a888d0135a0cff260d4fb8f
Michael Velimirov <m.velimirov@googlemail.com>
2014-06-16 16:24:25 +0200

    fixed issue of harvard tab not being activated on load and the all citations tab not working properly

--- bibsonomy-webapp/src/main/webapp/resources/bootstrap/js/publicationdetails.js
+++ bibsonomy-webapp/src/main/webapp/resources/bootstrap/js/publicationdetails.js
@@ -1,61 +1,51 @@
 $(document).ready(function () {
+
 	$('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
 		var targetElement  = $(e.target.getAttribute("href"));
 		if(targetElement.html().length > 0) return; 
 		
 		if(e.target.getAttribute("href")=="#citation_all") {
+				var url = $(e.target).data("formaturl");
 				$.ajax(
 						  {
-							  url: "/exportLayouts", 
-							  dataType: "json", 
+							  url: url, 
+							  dataType: "html", 
 							  success: function(data) {
-
-									var newElement = $("select").addClass("form-control input-sm");
-									targetElement.append(newElement);
-									data.layouts.forEach(function(value) {
-										addLayoutElement($(newElement), value)
-									});
+								  	targetElement
+								  	.html(data)
+									.find("select")
+									.addClass("form-control input-sm");
+									
 							  }
 						  }
 					);
 				return;
-			}
+		}
 			
-			targetElement.html(getString("bibtex.citation_format.loading")); // activated tab
-			var url = $(e.target).data("formaturl");
-			$.ajax(
-				  {
-					  url: url, 
-					  dataType: "html", 
-					  success: function(data) {
-						  $(e.target.getAttribute("href")).html(data);
-					  }
+		targetElement.html(getString("bibtex.citation_format.loading")); // activated tab
+		var url = $(e.target).data("formaturl");
+		$.ajax(
+			  {
+				  url: url, 
+				  dataType: "html", 
+				  success: function(data) {
+					  targetElement.html(data);
 				  }
-			);
+			  }
+		);
 	});
 	
-	var URI_PREFIX = "resources/javascript/pdf.js/web/viewer.html?file=";
+	$($(".firstTab")[0]).tab("show");
+	
+	var URI_PREFIX = "/resources/javascript/pdf.js/web/viewer.html?file=";
 	var PDFJS_FRAME_ID = "#pdfViewer";	
 	
 	$(".pdfFile").each(function(i, l) {
 		
 		$(l).click(function (e) {
-			$(PDFJS_FRAME_ID).attr("src", URI_PREFIX+$(e.target).parent().attr("href"));
-			
 			e.preventDefault();
-			
-	        BootstrapDialog.show({
-	            size: BootstrapDialog.SIZE_LARGE,
-	            message: $(PDFJS_FRAME_ID),
-	            
-	        });
+			$(PDFJS_FRAME_ID).attr("src", URI_PREFIX+$(e.target).parent().attr("href")).parent().modal({});
+			return false;
 		});
 	})
 });
-
-function addLayoutElement(element, value) {
-	var option = document.createElement("option");
-	option.setAttribute("value", value.baseFileName);
-	option.innerHTML = value.displayName;
-	element.append($(option));
-}
