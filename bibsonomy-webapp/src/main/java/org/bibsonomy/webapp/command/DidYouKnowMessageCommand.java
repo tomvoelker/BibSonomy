0aea0c4f966ac507df9d42f41697be98141b87bd
Sebastian Boettger <boettger@cs.uni-kassel.de>
2014-07-02 08:45:04 +0200

    Did you know messages to give users hints for how to using BibSonomy.

---
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/DidYouKnowMessageCommand.java
@@ -0,0 +1,13 @@
+package org.bibsonomy.webapp.command;
+
+import org.bibsonomy.webapp.util.DidYouKnowMessage;
+
+/**
+ * 
+ * @author Sebastian Böttger <boettger@cs.uni-kassel.de>
+ */
+public interface DidYouKnowMessageCommand {
+
+	
+	public DidYouKnowMessage getDidYouKnowMessage();
+}
