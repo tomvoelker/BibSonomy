a5ae0033a400c48fd74808c1bbd624b1ff568f1d
Lena Schwemmmlein <lena.schwemmlein@uni-wuerzburg.de>
2014-07-21 14:03:16 +0200

    New classes for standard layouts -> unify standard and jabref layouts

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/ExportPageCommand.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/ExportPageCommand.java
@@ -1,27 +1,51 @@
 package org.bibsonomy.webapp.command;
 
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeSet;
 
 import org.bibsonomy.layout.jabref.JabrefLayout;
+import org.bibsonomy.layout.standard.StandardLayout;
+import org.bibsonomy.model.Layout;
 
 /**
- * @author daill
+ * @author daill, lsc
  */
 public class ExportPageCommand extends ResourceViewCommand{
 	
-	private Map<String, JabrefLayout> layoutMap;
+	private Set<Layout> layoutSet;
 
 	/**
-	 * @return jabref layout map
+	 * @return layout set
 	 */
-	public Map<String, JabrefLayout> getLayoutMap() {
-		return this.layoutMap;
+	public Set<Layout> getLayoutSet() {
+		return this.layoutSet;
 	}
 
+	/**
+	 * @param layoutSet
+	 */
+	public void setLayoutSet(final Set<Layout> layoutSet) {
+		this.layoutSet = layoutSet;
+	}
+	
 	/**
 	 * @param layoutMap
 	 */
-	public void setLayoutMap(final Map<String, JabrefLayout> layoutMap) {
-		this.layoutMap = layoutMap;
+	public void addJabrefLayoutMap(final Map<String, JabrefLayout> layoutMap) {
+		for(Entry<String, JabrefLayout> layout : layoutMap.entrySet()){
+			this.layoutSet.add(layout.getValue());
+		}
 	}
+	
+	/**
+	 * @param layoutMap
+	 */
+	public void addStandardLayoutMap(final Map<String, StandardLayout> layoutMap) {
+		for(Entry<String, StandardLayout> layout : layoutMap.entrySet()){
+			this.layoutSet.add(layout.getValue());
+		}
+	}
+	
 }
