2ebec4f97a132222680c610e6a004030bfa39e05
Daniel Zoller <daniel.zoller@uni-wuerzburg.de>
2014-08-12 16:53:47 +0200

    removed duplicated javascript files
    removed 3x loading of javascript files (tooltip.js, popover.js)

--- bibsonomy-webapp/src/main/webapp/resources/javascript/merged/bs/global.js
+++ bibsonomy-webapp/src/main/webapp/resources/javascript/merged/bs/global.js
@@ -14,11 +14,8 @@ function includeJavaScript(jsFile) {
 }
 
 includeJavaScript("/resources/javascript/bs/jquery-2.1.0.min.js");
-includeJavaScript("/resources/javascript/bs/bootstrap.js");
+includeJavaScript("/resources/bootstrap/js/bootstrap.js");
 includeJavaScript("/resources/javascript/bs/holder.js");
-includeJavaScript("/resources/javascript/bs/collapse.js");
-includeJavaScript("/resources/javascript/bs/tooltip.js");
-includeJavaScript("/resources/javascript/bs/popover.js");
 includeJavaScript("/resources/javascript/bs/bootstrap-dialog.js");
 includeJavaScript("/resources/javascript/bs/custom.js");
 includeJavaScript("/resources/javascript/bs/bootstrap-datepicker.js");
