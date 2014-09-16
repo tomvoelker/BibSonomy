3f1417ea1b8f879c6a7fa4e749c7301309643c39
Daniel Zoller <daniel.zoller@uni-wuerzburg.de>
2014-09-16 20:21:09 +0200

    added statement to update primary key

--- bibsonomy-database/src/main/resources/database/migrations/2.0.46/add_relation_kind_column.sql
+++ bibsonomy-database/src/main/resources/database/migrations/2.0.46/add_relation_kind_column.sql
@@ -1,2 +1,4 @@
 ALTER TABLE log_gold_standard_publication_references ADD relation_kind TINYINT(1) NOT NULL default '0';
 ALTER TABLE gold_standard_publication_references ADD relation_kind TINYINT(1) NOT NULL default '0';
+
+alter table xx drop primary key, add primary key(k1, k2, k3);
