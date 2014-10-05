64836a7a2dce46947298df37c45a9bde800206eb
Daniel Zoller <daniel.zoller@uni-wuerzburg.de>
2014-10-05 21:59:55 +0200

    fixed null factory bean test (webapp dir is no longer part of the class path)

--- bibsonomy-webapp/src/test/java/org/bibsonomy/webapp/util/spring/factorybeans/NullFactoryBeanTest.java
+++ bibsonomy-webapp/src/test/java/org/bibsonomy/webapp/util/spring/factorybeans/NullFactoryBeanTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertNull;
 
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 
 /**
@@ -14,7 +14,7 @@ public class NullFactoryBeanTest {
 
 	@Test
 	public void testSpringInstantiation() throws Exception {
-		final ApplicationContext factory = new ClassPathXmlApplicationContext("WEB-INF/bibsonomy2-servlet-bibsonomy.xml");
+		final ApplicationContext factory = new FileSystemXmlApplicationContext("src/main/webapp/WEB-INF/bibsonomy2-servlet-bibsonomy.xml");
 		final Object swordService = factory.getBean("swordService_bibsonomy");
 		
 		/*
