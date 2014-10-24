c1e947bd4457315e65736e0e6472bf291a3fc239
Daniel Zoller <daniel.zoller@uni-wuerzburg.de>
2014-10-24 11:38:42 +0200

    removed unneccessary class attribute to get the first citation tab link

--- bibsonomy-webapp/src/main/webapp/resources/javascript/bs/publication.js
+++ bibsonomy-webapp/src/main/webapp/resources/javascript/bs/publication.js
@@ -1,38 +1,31 @@
 $(function() {
-	$('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
+	var tabs = $('.citation-box a[data-toggle="tab"]')
+	tabs.on('show.bs.tab', function (e) {
 		var targetElement  = $(e.target.getAttribute("href"));
-		if(targetElement.html().length > 0) return; 
+		if (targetElement.html().length > 0) return; 
 		
-		if(e.target.getAttribute("href")=="#citation_all") {
+		if (e.target.getAttribute("href")=="#citation_all") {
 				var url = $(e.target).data("formaturl");
-				$.ajax(
-						  {
-							  url: url, 
-							  dataType: "html", 
-							  success: function(data) {
-								  	targetElement
-								  	.html(data)
-									.find("select")
-									.addClass("form-control input-sm");
-									
-							  }
-						  }
-					);
-				return;
+				$.ajax({
+					url: url, 
+					dataType: "html", 
+					success: function(data) {
+								targetElement.html(data).find("select").addClass("form-control input-sm");
+							}
+				});
+			return;
 		}
 			
 		targetElement.html(getString("bibtex.citation_format.loading")); // activated tab
 		var url = $(e.target).data("formaturl");
-		$.ajax(
-			  {
-				  url: url, 
-				  dataType: "html", 
-				  success: function(data) {
-					  targetElement.html(data);
-				  }
-			  }
-		);
+		$.ajax({
+			url: url, 
+			dataType: "html", 
+			success: function(data) {
+						targetElement.html(data);
+			}
+		});
 	});
 	
-	$($(".firstTab")[0]).tab("show");
+	tabs.first().tab("show");
 });
