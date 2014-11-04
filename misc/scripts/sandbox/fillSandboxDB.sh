731eb9b6727308d35be53c4207d6f7dabca0993e
Stephan Doerfel <doerfel@cs.uni-kassel.de>
2014-11-04 16:47:51 +0100

    sandbox_script

--- misc/scripts/sandbox/fillSandboxDB.sh
+++ misc/scripts/sandbox/fillSandboxDB.sh
@@ -1,16 +1,22 @@
 #! /bin/bash
 
-# This Script fills the sandbox with a dump of the system and updates the dates of all posts                                                     
-# It expects the dump to be in the directory $sandboxInitDir together with all documentes and previews                                           
+# This Script fills the sandbox with a dump of the system and updates the dates of all posts
+# It expects the dump to be in the directory $sandboxInitDir together with all documentes and previews
+# Arguments
+# The date of the youngest post in the database in the format "yyyymmdd" (used to update all posts to the current date)
 
-database="sandbox_puma"
+shopt -s expand_aliases
+source ~/.bash_aliases
+
+
+database="sandbox_puma_interim"
 dbUser="sandbox_puma"
-dbPw="INSERT PASSWORD"
-dbHost="p210.puma.bibliothek.uni-kassel.de"
-pumaDataDir=/home/puma/pumadata
-sandboxInitDir=/home/puma/init_sandbox
-
-mysqlConnection="mysql -h $dbHost -u $dbUser -p$dbPw -N --database $database"
+dbPw="sandbox48idfd7qUU823op2r"
+pumaHome=/home/puma-sandbox
+pumaDataDir=${pumaHome}/pumadata
+sandboxInitDir=${pumaHome}/init_sandbox
+diffDateString=$1
+mysqlConnection="mysql --default-character-set=utf8 -u $dbUser -p$dbPw -N --database $database"
 
 
 # RESET Database
@@ -21,7 +27,7 @@ showTables="$mysqlConnection -e \"show tables\""
 IFS=$'\n'
 
 for table in $(eval $showTables); do
-  eval $mysqlConnection -e "\"TRUNCATE ${table}\""
+  eval $mysqlConnection -e "\"DELETE FROM ${table}\""
 done
 
 # Enter all default data into the database
@@ -30,17 +36,18 @@ eval $mysqlConnection < ${sandboxInitDir}/initial_sandbox_data/init_sandbox_dump
 # RESET File directoris (uploaded pictures, documents, etc.)
 # delete current directory, copy default directories
 
-/etc/init.d/tomcat stop
+#/etc/init.d/tomcat stop
 rm -fr ${pumaDataDir}/lucene
 rm -fr ${pumaDataDir}/puma*
 cp -r ${sandboxInitDir}/initial_sandbox_data/pumadata/* ${pumaDataDir}
+
 
 # Modify the dates of the posts such that the posts look "fresh" (recently added)
 # otherwise, the popular page will soon be empty and the system look deserted
 
 # dayDiff is the difference between today and the youngest date in the dump we have loaded into the database 2013-11-09
 
-dayDiff=$(( ($(date +%s) - $(date --date="20140103" +%s) )/(60*60*24) -1))
+dayDiff=$(( ($(date +%s) - $(date --date="$diffDateString" +%s) )/(60*60*24) -1))
 
 # add the dayDiff to all posts
 
@@ -58,6 +65,8 @@ updateDate "tas" "date"
 updateDate "tas" "change_date"
 updateDate "user_wiki" "date"
 
+# rebuild all lucene indexes (with the new dates)
+/usr/bin/java -cp .:./bibsonomy-internal-tools-2.0.48-SNAPSHOT-executable.jar org.bibsonomy.lucene.util.LuceneIndexGenerator 1 publication bookmark goldStandardPublication
 
-/etc/init.d/tomcat start
-## TODO Was ist mit Lucene?
+#/etc/init.d/tomcat start
+
