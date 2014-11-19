b1f3b6a3210c76b261855a01472bd81f1bdce823
Sebastian Boettger <boettger@cs.uni-kassel.de>
2014-11-19 18:04:09 +0100

    extends tagsinput.js for choosing custom delimiter.

--- bibsonomy-webapp/src/main/webapp/resources/javascript/bs/bootstrap-tagsinput.js
+++ bibsonomy-webapp/src/main/webapp/resources/javascript/bs/bootstrap-tagsinput.js
@@ -244,8 +244,8 @@
           val = $.map(self.items(), function(item) {
             return self.options.itemValue(item).toString();
           });
-
-      self.$element.val(val, true).trigger('change');
+      	
+      self.$element.val(val.join(self.options.delimiter), true).trigger('change');
     },
 
     /**
