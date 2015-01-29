c763fb0096820b7ccb54a223d300464c490e9810
Mario Holtmüller <m.holtmueller@hothodata.de>
2015-01-29 17:16:58 +0100

    reworked /grouprequest page

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/GroupRequestController.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/GroupRequestController.java
@@ -26,45 +26,160 @@
  */
 package org.bibsonomy.webapp.controller;
 
+import static org.bibsonomy.util.ValidationUtils.present;
+
+import org.bibsonomy.model.Group;
+import org.bibsonomy.model.User;
+import org.bibsonomy.model.logic.LogicInterface;
 import org.bibsonomy.webapp.command.GroupRequestCommand;
 import org.bibsonomy.webapp.command.SettingsViewCommand;
-import org.bibsonomy.webapp.util.MinimalisticController;
+import org.bibsonomy.webapp.util.ErrorAware;
+import org.bibsonomy.webapp.util.RequestWrapperContext;
+import org.bibsonomy.webapp.util.ValidationAwareController;
+import org.bibsonomy.webapp.util.Validator;
 import org.bibsonomy.webapp.util.View;
 import org.bibsonomy.webapp.util.spring.security.exceptions.AccessDeniedNoticeException;
+import org.bibsonomy.webapp.validation.GroupValidator;
+import org.bibsonomy.webapp.view.ExtendedRedirectView;
 import org.bibsonomy.webapp.view.Views;
+import org.springframework.util.Assert;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
 
 
 /**
  * @author Mario Holtmueller
  */
-public class GroupRequestController implements MinimalisticController<GroupRequestCommand> {
+public class GroupRequestController implements ValidationAwareController<GroupRequestCommand>, ErrorAware {
 
+	
+	private Errors errors = null;
+	private LogicInterface logic;
+	
 	/**
 	 * @param command
 	 * @return the view
 	 */
 	@Override
 	public View workOn(final GroupRequestCommand command) {
+		final RequestWrapperContext context = command.getContext();
+
+		/*
+		 * user has to be logged in to see this page
+		 */
 		if (!command.getContext().isUserLoggedIn()) {
 			throw new AccessDeniedNoticeException("please log in", "error.general.login");
+		}
+		
+		final String action = command.getOperation();
+		
+		if (present(action)) {
+			if (new String("REQUEST").equals(action)) {
+
+				/*
+				 * check the ckey
+				 */
+				if (!context.isValidCkey()) {
+					errors.reject("error.field.valid.ckey");
+				}
+				
+				final User loginUser = context.getLoginUser();
+				final Group requestedGroup = command.getGroup();
+				
+				if (present(requestedGroup)) {
+					
+					if (!present(requestedGroup.getName())) {
+						// TODO: add form error for field (rejectValue)
+						this.errors.reject("settings.group.error.requestGroupFailed");
+					}
+					
+					// TODO: add valid username check here
+					
+					if (!present(requestedGroup.getDescription())) {
+						// TODO: add form error for field
+						this.errors.reject("settings.group.error.requestGroupFailed");
+					}
+					if (!present(requestedGroup.getGroupRequest().getReason())) {
+						// TODO: add form error for field
+						this.errors.reject("settings.group.error.requestGroupFailed");
+					}
+					
+					// TODO: add field for email?
+					
+					// TODO: add spammer check here
+					
+					// TODO: add check if username is already in the system
+					
+					if (!this.errors.hasErrors()) {
+						// set the username and create the request
+						requestedGroup.getGroupRequest().setUserName(loginUser.getName());
+						this.logic.createGroup(requestedGroup);
+					}
+					
+					return new ExtendedRedirectView("/");
+				}
+			}
 		}
 
 		return Views.GROUPREQUEST;
 	}
-
-	@Deprecated
-	private void workOnGroupTab(final SettingsViewCommand command) {}
-
-
+	
 	/**
 	 * @return the current command
 	 */
 	@Override
 	public GroupRequestCommand instantiateCommand() {
-		return new GroupRequestCommand();
+		final GroupRequestCommand command = new GroupRequestCommand();
+		command.setGroup(new Group());
+		return command;
 	}
 
 
+	
+	
+	
+	
+	
+	
+	public void setLogic(LogicInterface logic) {
+		this.logic = logic;
+	}
+	
+	@Override
+	public Errors getErrors() {
+		return this.errors;
+	}
+
+	@Override
+	public void setErrors(Errors errors) {
+		this.errors = errors;
+	}
+	
+	@Override
+	public boolean isValidationRequired(GroupRequestCommand command) {
+		// FIXME: why?
+		return command.getContext().getLoginUser().isSpammer()
+				&& command.getContext().getLoginUser().getToClassify() == 0;
+	}
+
+	@Override
+	public Validator<GroupRequestCommand> getValidator() {
+		return new Validator<GroupRequestCommand>() {
+
+			@Override
+			public boolean supports(Class<?> clazz) {
+				return SettingsViewCommand.class.equals(clazz);
+			}
+
+			@Override
+			public void validate(Object target, Errors errors) {
+				Assert.notNull(target);
+				final SettingsViewCommand command = (SettingsViewCommand) target;
+				
+				ValidationUtils.invokeValidator(new GroupValidator(), command.getGroup(), errors);
+			}
+		};
+	}
 
 
 }
