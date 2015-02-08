3b5bda504744c272c1b44f20323ee4026d132579
Daniel Zoller <daniel.zoller@uni-wuerzburg.de>
2015-02-08 20:11:58 +0100

    cleanup
    converted line encoding ending

--- bibsonomy-database/src/main/resources/database/migrations/3.1.0/create_indexes_for_history.sql
+++
@@ -1,7 +0,0 @@
-CREATE INDEX `current_content_id_user_name` ON `log_bibtex` (`current_content_id`,`user_name`);
-
-CREATE INDEX `current_content_id_user_name` ON `log_bookmark` (`current_content_id`,`user_name`);
-
-CREATE INDEX `current_content_id` ON `log_gold_standard` (`current_content_id`);
-
-CREATE INDEX `content_id_idx` ON `log_tas` (`content_id`);
