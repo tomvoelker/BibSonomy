79ac25c825b9f5414981f4867c921225224038e0
Daniel Zoller <daniel.zoller@uni-wuerzburg.de>
2015-02-08 21:14:58 +0100

    group role privileges do not depend on database id anymore, now the compiler checks it
    if group privlevel is hidden the user now always sees his own group membership (maybe fixes #2385)
    
    todos and cleanup

--- bibsonomy-database/src/main/java/org/bibsonomy/database/DBLogic.java
+++ bibsonomy-database/src/main/java/org/bibsonomy/database/DBLogic.java
@@ -1340,7 +1340,7 @@ public class DBLogic implements LogicInterface {
 			case DECLINE_JOIN_REQUEST:
 				final GroupMembership currentMembership = this.groupDBManager.getPendingMembershipForUserAndGroup(requestedUserName, group.getName(), session);
 
-				if (!present(currentMembership) || !currentMembership.getGroupRole().isPendingRole()) {
+				if (!present(currentMembership) || !GroupRole.PENDING_GROUP_ROLES.contains(currentMembership.getGroupRole())) {
 					throw new AccessDeniedException("You are not allowed to decline this request/invitation");
 				}
 				if (GroupRole.INVITED.equals(currentMembership.getGroupRole())) {
@@ -2430,7 +2430,7 @@ public class DBLogic implements LogicInterface {
 		this.permissionDBManager.ensureAdminAccess(this.loginUser);
 		final DBSession session = this.openSession();
 		try {
-			return this.adminDBManager.getClassifiedUserCount(classifier, status, interval, session);
+			return this.adminDBManager.getClassifiedUserCount(classifier, status, interval, session).intValue();
 		} finally {
 			session.close();
 		}
@@ -2505,11 +2505,8 @@ public class DBLogic implements LogicInterface {
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see
-	 * org.bibsonomy.model.logic.LogicInterface#getUsernameByRemoteUserId(org
-	 * .bibsonomy.model.user.remote.RemoteUserId)
+	 * @see org.bibsonomy.model.logic.LogicInterface#getUsernameByRemoteUserId(org.bibsonomy.model.user.remote.RemoteUserId)
 	 */
-
 	@Override
 	public String getUsernameByRemoteUserId(final RemoteUserId remoteUserId) {
 		final DBSession session = this.openSession();
