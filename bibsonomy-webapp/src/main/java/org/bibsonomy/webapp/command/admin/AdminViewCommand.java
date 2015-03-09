991185f6c37e4c21931a0f8d15d3a00d968d5984
Daniel Zoller <daniel.zoller@uni-wuerzburg.de>
2015-03-09 13:17:44 +0100

    deleted admin index page
    re #2448

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/admin/AdminViewCommand.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/admin/AdminViewCommand.java
@@ -92,7 +92,6 @@ public class AdminViewCommand extends TabsCommand<User> {
 	private final LinkedList<String> infos;
 
 
-
 	public AdminViewCommand() {	
 		addTabs(tabTitles);
 
@@ -101,7 +100,7 @@ public class AdminViewCommand extends TabsCommand<User> {
 
 		// initialise info list
 		infos = new LinkedList<String>();
-	}	
+	}
 
 
 	public AdminSettingsCommand getSettingsCommand() {
