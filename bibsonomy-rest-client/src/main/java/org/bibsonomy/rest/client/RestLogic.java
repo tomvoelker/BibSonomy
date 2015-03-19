804522640a3effb272b52d176a1e4f336c6aad6a
Nasim Nabavi <n.nabavi1385@gmail.com>
2015-03-19 16:40:30 +0100

    rest client updated

--- bibsonomy-rest-client/src/main/java/org/bibsonomy/rest/client/RestLogic.java
+++ bibsonomy-rest-client/src/main/java/org/bibsonomy/rest/client/RestLogic.java
@@ -906,4 +906,12 @@ public class RestLogic implements LogicInterface {
 		return null;
 	}
 
+	/* (non-Javadoc)
+	 * @see org.bibsonomy.model.logic.PersonLogicInterface#getPersonNameById(int)
+	 */
+	@Override
+	public PersonName getPersonNameById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
