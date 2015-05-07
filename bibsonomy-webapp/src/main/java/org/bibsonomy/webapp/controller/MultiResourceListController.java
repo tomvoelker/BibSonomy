e30272cf19fc309b81e68262c9e87332ec3f2141
Daniel Zoller <daniel.zoller@uni-wuerzburg.de>
2015-05-07 21:58:46 +0200

    fixed api url building for non secure sync services

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/MultiResourceListController.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/MultiResourceListController.java
@@ -88,7 +88,7 @@ public abstract class MultiResourceListController extends ResourceListController
 		final ListCommand<Post<T>> listCommand = new ListCommand<Post<T>>(cmd);
 		// retrieve posts		
 		log.debug("getPosts " + resourceType + " " + groupingEntity + " " + groupingName + " " + listCommand.getStart() + " " + itemsPerPage + " " + filter);
-		listCommand.setList(this.logic.getPosts(resourceType, groupingEntity, groupingName, tags, hash, search,SearchType.LOCAL, Sets.<Filter>asSet(filter), order, null, null, listCommand.getStart(), listCommand.getStart() + itemsPerPage) );
+		listCommand.setList(this.logic.getPosts(resourceType, groupingEntity, groupingName, tags, hash, search, SearchType.LOCAL, Sets.<Filter>asSet(filter), order, null, null, listCommand.getStart(), listCommand.getStart() + itemsPerPage) );
 		cmd.getListCommand(resourceType).add(listCommand);
 
 		// list settings
