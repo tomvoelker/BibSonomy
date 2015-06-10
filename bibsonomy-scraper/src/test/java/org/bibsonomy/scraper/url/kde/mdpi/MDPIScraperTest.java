8ad77f5d3763d7069227af7bb8d12b299a002179
mabed <devnull@localhost>
2015-06-10 12:52:06 +0200

    change the format

--- bibsonomy-scraper/src/test/java/org/bibsonomy/scraper/url/kde/mdpi/MDPIScraperTest.java
+++ bibsonomy-scraper/src/test/java/org/bibsonomy/scraper/url/kde/mdpi/MDPIScraperTest.java
@@ -65,7 +65,7 @@ public class MDPIScraperTest {
 		final String cby = sc.getCitedBy();
 		assertNotNull(cby);
 		assertTrue(cby.length() > 100);
-		assertEquals("<em>Citations registered in CrossRef as of 9 May 2015. The citation count might be different from <a hr".trim(), cby.substring(0, 103).trim());
+		assertEquals("<em>Citations registered in CrossRef as of 6 June 2015. The citation count might be different from <a h".trim(), cby.substring(0, 103).trim());
 		assertTrue(cby.contains("Zhang, L."));
 	}
 }
