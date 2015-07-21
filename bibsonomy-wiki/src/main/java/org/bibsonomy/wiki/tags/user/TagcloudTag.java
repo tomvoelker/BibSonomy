2e4802f963baf55d0842c4e0250b68d2cb864077
Jennifer Häfner <jenniferhaefner@web.de>
2015-07-21 17:15:08 +0200

    specify tagcloud in CV

--- bibsonomy-wiki/src/main/java/org/bibsonomy/wiki/tags/user/TagcloudTag.java
+++ bibsonomy-wiki/src/main/java/org/bibsonomy/wiki/tags/user/TagcloudTag.java
@@ -26,29 +26,27 @@
  */
 package org.bibsonomy.wiki.tags.user;
 
-import static org.bibsonomy.util.ValidationUtils.present;
-import info.bliki.htmlcleaner.Utils;
 
-import java.net.URI;
+import java.util.Arrays;
 import java.util.Collections;
-import java.util.Date;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 
 import org.bibsonomy.common.enums.GroupingEntity;
-import org.bibsonomy.common.enums.TagSimilarity;
+import org.bibsonomy.common.enums.SearchType;
+import org.bibsonomy.model.BibTex;
+import org.bibsonomy.model.Bookmark;
+import org.bibsonomy.model.Post;
 import org.bibsonomy.model.Resource;
 import org.bibsonomy.model.Tag;
 import org.bibsonomy.model.enums.Order;
-import org.bibsonomy.model.util.BibTexUtils;
+import org.bibsonomy.model.logic.PostLogicInterface;
 import org.bibsonomy.util.Sets;
-import org.bibsonomy.util.SortUtils;
-import org.bibsonomy.util.UrlUtils;
-import org.bibsonomy.util.ValidationUtils;
-import org.bibsonomy.wiki.tags.SharedTag;
 import org.bibsonomy.wiki.tags.UserTag;
 
 
@@ -59,7 +57,7 @@ import org.bibsonomy.wiki.tags.UserTag;
  */
 public class TagcloudTag extends UserTag {
 	
-	private static final String TAG_NAME = "tags";
+	private static final String TAG_NAME = "tagcloud";
 	
 	private static final String TAGSTYLE = "tagstyle";
 	
@@ -75,9 +73,19 @@ public class TagcloudTag extends UserTag {
 	
 	private static final String MINFREQ = "minfreq";
 	
+	private static final String TYPE = "type";
+	
+	private static final String TYPE_BOTH = "both";
+	
+	private static final String TYPE_BOOKMARKS = "bookmarks";
+	
+	private static final String TYPE_PUBLICATIONS = "publications";
+	
+	private static final String TAGS = "tags";
+	
 	private static final Map<String, String> defaultOrder = new HashMap<String, String>();
 
-	private final static Set<String> ALLOWED_ATTRIBUTES_SET = Sets.asSet(TAGSTYLE, ORDER, MINFREQ);
+	private final static Set<String> ALLOWED_ATTRIBUTES_SET = Sets.asSet(TAGSTYLE, ORDER, MINFREQ, TYPE, TAGS);
 
 	static {
 		defaultOrder.put(TAGSTYLE_TAGCLOUD, ORDER_ALPHA);
@@ -113,63 +121,234 @@ public class TagcloudTag extends UserTag {
 		
 		final StringBuilder renderedHTML = new StringBuilder();
 		final Map<String, String> tagAttributes = this.getAttributes();
+		
+		
+		/*
+		 * no value for key tagstyle --> see user settings
+		 * 0 = cloud, 1 = list
+		 */
+		if (!tagAttributes.containsKey(TAGSTYLE)) {
+			int tagstyleInt = this.requestedUser.getSettings().getTagboxStyle();
+			if (tagstyleInt==0){
+				tagAttributes.put(TAGSTYLE, TAGSTYLE_TAGCLOUD);
+			} else {
+				tagAttributes.put(TAGSTYLE, TAGSTYLE_TAGLIST);
+			}
+		}
+		
+		/*
+		 * no value for key order --> see user settings
+		 * 0 = alph, 1 = freq
+		 */
+		if (!tagAttributes.containsKey(ORDER)) {
+			int tagsortInt = this.requestedUser.getSettings().getTagboxSort();
+			if (tagsortInt==0){
+				tagAttributes.put(ORDER, ORDER_ALPHA);
+			} else {
+				tagAttributes.put(ORDER, ORDER_FREQ);
+			}
+		}
+		
+		/*
+		 * no value for minfreq --> see user settings
+		 */
+		if (!tagAttributes.containsKey(MINFREQ)) {
+			Integer minfreqInt = this.requestedUser.getSettings().getTagboxMinfreq();
+			tagAttributes.put(MINFREQ, minfreqInt.toString());
+		}
+		
+		/*
+		 * no value for type --> both bookmarks and publications
+		 */
+		if (!tagAttributes.containsKey(TYPE)) {
+			tagAttributes.put(TYPE, TYPE_BOTH);
+		}
+		
+		/*
+		 * no value for tags --> empty 
+		 */
+		if (!tagAttributes.containsKey(TAGS)) {
+			tagAttributes.put(TAGS, "");
+		}
+		
 		final String requestedName = this.requestedUser.getName();
 		final List<Tag> tagsFinal = new LinkedList<Tag>();
 		
+
 		/*
-		 * order the tags, alpha or frequency
+		 * order the tags: alpha or frequency / bookmarks or publications or both
 		 */
 		String orderValue = tagAttributes.get(ORDER);
+		String typeValue = tagAttributes.get(TYPE);
 		int tagMax = 20000;
-		final List<Tag> tags;
+		List<Tag> tags = new LinkedList<Tag>();	
 		
-		if (orderValue.equals(null)){
-			orderValue = ORDER_FREQ;
+			
+		if ((orderValue.equals(ORDER_ALPHA)) && (typeValue.equals(TYPE_BOOKMARKS))) {
+				//Alphabet and Bookmarks
+				tags = this.logic.getTags(Bookmark.class, GroupingEntity.USER, requestedName, null, null, null, null, null, Order.ALPH, null, null, 0, tagMax);
 		}
-	
-		if (orderValue.equals(ORDER_ALPHA)){
-			//nach Alphabet sortieren
-			tags = this.logic.getTags(Resource.class, GroupingEntity.USER, requestedName, null, null, null, null, null, Order.ALPH, null, null, 0, tagMax);
-		} else {
-			tags = this.logic.getTags(Resource.class, GroupingEntity.USER, requestedName, null, null, null, null, null, Order.FREQUENCY, null, null, 0, tagMax);
+		else if ((orderValue.equals(ORDER_ALPHA)) && (typeValue.equals(TYPE_PUBLICATIONS))) {
+				//Alphabet and Publications
+				tags = this.logic.getTags(BibTex.class, GroupingEntity.USER, requestedName, null, null, null, null, null, Order.ALPH, null, null, 0, tagMax);
 		}
-		
-		final int tagMinFrequency = getMinFreqFromTaglist(tags);
-		final int tagMaxFrequency = getMaxFreqFromTaglist(tags);
-		
-		/*
-		 * only show tags with a frequency higher than minfreq
-		 */
-		final String minfreqValueString = tagAttributes.get(MINFREQ);
-		final int minfreqValue;
-		
-		if (minfreqValueString.equals(null)){
-			//minfreq aus Useroptionen
-			minfreqValue = this.requestedUser.getSettings().getTagboxMinfreq();
-		}
+		else if ((orderValue.equals(ORDER_ALPHA)) && (typeValue.equals(TYPE_BOTH))) {
+				//Alphabet and both
+				tags = this.logic.getTags(Resource.class, GroupingEntity.USER, requestedName, null, null, null, null, null, Order.ALPH, null, null, 0, tagMax);
+			}
+		else if ((orderValue.equals(ORDER_FREQ)) && (typeValue.equals(TYPE_BOOKMARKS))) {
+				//Frequency and Bookmarks
+				tags = this.logic.getTags(Bookmark.class, GroupingEntity.USER, requestedName, null, null, null, null, null, Order.FREQUENCY, null, null, 0, tagMax);
+			}
+		else if ((orderValue.equals(ORDER_FREQ)) && (typeValue.equals(TYPE_PUBLICATIONS))) {
+				//Frequency and Publications
+				tags = this.logic.getTags(BibTex.class, GroupingEntity.USER, requestedName, null, null, null, null, null, Order.FREQUENCY, null, null, 0, tagMax);
+			}
 		else {
-			minfreqValue = Integer.parseInt(minfreqValueString);
+				//Frequency and both
+				tags = this.logic.getTags(Resource.class, GroupingEntity.USER, requestedName, null, null, null, null, null, Order.FREQUENCY, null, null, 0, tagMax);
 		}
 		
-		for (Tag t:tags){
-			if (t.getUsercount() >= minfreqValue){
-				tagsFinal.add(t);
+		int tagMinFrequency = 0;
+		int tagMaxFrequency = 0;
+		
+		
+		//if value(s) for tags are given:
+		//get the tags that should be displayed
+		List<Tag> tagspost = new LinkedList<Tag>();
+		if (!tagAttributes.get(TAGS).equals("")) {
+			if (typeValue.equals(TYPE_PUBLICATIONS)) {
+				List<Post<BibTex>> posts = this.logic.getPosts(BibTex.class, GroupingEntity.USER, requestedName, Arrays.asList(tagAttributes.get(TAGS).split(" ")), null, null,SearchType.LOCAL, null, null, null, null, 0, PostLogicInterface.MAX_QUERY_SIZE);
+				if (!posts.isEmpty()) {
+					tagspost = addTagsToListBibTex(posts);
+				}
+			}
+			else if (typeValue.equals(TYPE_BOOKMARKS)) {
+				List<Post<Bookmark>> posts = this.logic.getPosts(Bookmark.class, GroupingEntity.USER, requestedName, Arrays.asList(tagAttributes.get(TAGS).split(" ")), null, null,SearchType.LOCAL, null, null, null, null, 0, PostLogicInterface.MAX_QUERY_SIZE);
+				if (!posts.isEmpty()) {
+					tagspost = addTagsToListBookmarks(posts);
+				}
+			} else {
+				List<Post<BibTex>> postsBibTex = this.logic.getPosts(BibTex.class, GroupingEntity.USER, requestedName, Arrays.asList(tagAttributes.get(TAGS).split(" ")), null, null,SearchType.LOCAL, null, null, null, null, 0, PostLogicInterface.MAX_QUERY_SIZE);
+				if (!postsBibTex.isEmpty()) {
+					tagspost = addTagsToListBibTex(postsBibTex);
+				}
+				List<Post<Bookmark>> postsBookmark = this.logic.getPosts(Bookmark.class, GroupingEntity.USER, requestedName, Arrays.asList(tagAttributes.get(TAGS).split(" ")), null, null,SearchType.LOCAL, null, null, null, null, 0, PostLogicInterface.MAX_QUERY_SIZE);
+				if (!postsBookmark.isEmpty()) {
+					List<Tag> tagspost2 = addTagsToListBookmarks(postsBookmark);
+					tagspost.addAll(tagspost2);
+				}	
+			}
+			
+			
+			//delete all tags that should not be displayed (are not in tagspost)
+			List<Tag> tagsNew = new LinkedList<Tag>();
+			
+			if ((!tagspost.isEmpty()) && (!tags.isEmpty())) {
+				for (Tag t: tags) {
+					String tagName = t.getName();
+					for (Tag s: tagspost) {
+						if (s.getName().equals(tagName)) {
+							tagsNew.add(t);
+						}
+					}
+				}
+			}
+			
+			//instead of Usercount: the actual amount of the tag in list tagsNew
+			TreeMap<Tag, Integer> tagMap = new TreeMap<Tag, Integer>();
+			if (!tagsNew.isEmpty()) {
+				for (Tag t: tagsNew){
+					String tagName = t.getName();
+					int counter = 0;
+					for (Tag s: tagsNew){
+						if (s.getName().equals(tagName)){
+							counter++;
+						}
+					}
+					tagMap.put(t, counter);	
+				}
+			}
+			
+			
+			//from map to list again for further work
+			LinkedList<Tag> tagsNewFinal = new LinkedList<Tag>();
+			if (!tagMap.isEmpty()) {
+				for (Map.Entry<Tag, Integer> entry : tagMap.entrySet()) {
+				    Tag key = entry.getKey();
+				    int value = entry.getValue();
+				    key.setUsercount(value);
+				    tagsNewFinal.add(key);
+				}
+			}
+			
+			
+			if (!tagsNewFinal.isEmpty()) {
+				if (orderValue.equals(ORDER_ALPHA)){
+					// sort alphabetically
+					Collections.sort(tagsNewFinal);
+				} else {
+					// sort by usercount
+					Collections.sort(tagsNewFinal, new Comparator<Tag>() {
+
+				        public int compare(Tag t1, Tag t2) {
+				        	Integer t1Count = new Integer(t1.getUsercount());
+				        	Integer t2Count = new Integer(t2.getUsercount());
+				            return t2Count.compareTo(t1Count);
+				        }
+				    });	
+				}
+				    
+				
+				tagMinFrequency = getMinFreqFromTaglist(tagsNewFinal);
+				tagMaxFrequency = getMaxFreqFromTaglist(tagsNewFinal);
+			}
+				
+				/*
+				 * only show tags with a frequency higher than minfreq
+				 */
+				final String minfreqValueString = tagAttributes.get(MINFREQ);
+				final int minfreqValue;
+				
+				minfreqValue = Integer.parseInt(minfreqValueString);
+				
+				if (!tagsNewFinal.isEmpty()) {
+					for (Tag t:tagsNewFinal){
+						if (t.getUsercount() >= minfreqValue){
+							tagsFinal.add(t);
+						}
+					}
+				}
+			
+		} else {
+			
+			if (!tags.isEmpty()) {
+				tagMinFrequency = getMinFreqFromTaglist(tags);
+				tagMaxFrequency = getMaxFreqFromTaglist(tags);
+			}
+			
+			/*
+			 * only show tags with a frequency higher than minfreq
+			 */
+			final String minfreqValueString = tagAttributes.get(MINFREQ);
+			final int minfreqValue;
+			
+			minfreqValue = Integer.parseInt(minfreqValueString);
+			
+			if (!tags.isEmpty()) {
+				for (Tag t:tags){
+					if (t.getUsercount() >= minfreqValue){
+						tagsFinal.add(t);
+					}
+				}
 			}
 		}
+
 		
 		/*
 		 * tagcloud or taglist
-		 * 0 = cloud, 1 = list
 		 */
 		String tagstyle = tagAttributes.get(TAGSTYLE);
-		if (tagstyle.equals(null)){
-			int tagstyleInt = this.requestedUser.getSettings().getTagboxStyle();
-			if (tagstyleInt==0){
-				tagstyle=TAGSTYLE_TAGCLOUD;
-			} else {
-				tagstyle=TAGSTYLE_TAGLIST;
-			}
-		}
 		
 		if (tagstyle.equals(TAGSTYLE_TAGLIST)){
 			//taglist
@@ -177,45 +356,85 @@ public class TagcloudTag extends UserTag {
 			renderedHTML.append("<ul class='list-group'>");
 			renderedHTML.append("<li class='list-group-item'>");
 			
-			for (Tag t: tagsFinal){
-				final String tagName = t.getName();
-				final String link = "http://localhost:8080/user/" + this.requestedUser.getName() + "/" + tagName;
-				final int tagCount = t.getUsercount();
-				int fontSize = computeTagFontsize(tagCount, tagMinFrequency, tagMaxFrequency, "user");
-				renderedHTML.append("<a href='" + link + "' title='" + tagCount + " posts' style='font-size:" + fontSize + "%' >" + tagName + " </a><br>");
+			if (!tagsFinal.isEmpty()) {
+				for (Tag t: tagsFinal){
+					final String tagName = t.getName();
+					final String link = "http://localhost:8080/user/" + this.requestedUser.getName() + "/" + tagName;
+					final int tagCount = t.getUsercount();
+					int fontSize = computeTagFontsize(tagCount, tagMinFrequency, tagMaxFrequency, "user");
+					renderedHTML.append("<a href='" + link + "' title='" + tagCount + " posts' style='font-size:" + fontSize + "%' >" + tagName + " </a><br>");
+				}
+			}  else {
+				renderedHTML.append("you have no tags in this category");
 			}
+			
 			renderedHTML.append("</ul>");
 			renderedHTML.append("</li>");
 			renderedHTML.append("</div>");
 			
 		} else {
 			//tagcloud
-			
 			renderedHTML.append("<div id='tags'>");
-			renderedHTML.append("<ul class='" + tagstyle + " tagbox'>");
-			for (Tag t: tagsFinal){
-				final String tagName = t.getName();
-				final String link = "http://localhost:8080/user/" + this.requestedUser.getName() + "/" + tagName;
-				final int tagCount = t.getUsercount();
-				String tagSize = getTagSize(tagCount, tagMaxFrequency);
-				int fontSize = computeTagFontsize(tagCount, tagMinFrequency, tagMaxFrequency, "user");
-				renderedHTML.append("<li class='" + tagSize + "'>");
-				renderedHTML.append("<a href='" + link + "' title='" + tagCount + " posts' style='font-size:" + fontSize + "%' >" + tagName + " </a>");
-				renderedHTML.append("</li>");
+			renderedHTML.append("<ul class='tagcloud tagbox'>");
+			
+			if (!tagsFinal.isEmpty()) {
+				for (Tag t: tagsFinal){
+					final String tagName = t.getName();
+					final String link = "http://localhost:8080/user/" + this.requestedUser.getName() + "/" + tagName;
+					final int tagCount = t.getUsercount();
+					String tagSize = getTagSize(tagCount, tagMaxFrequency);
+					int fontSize = computeTagFontsize(tagCount, tagMinFrequency, tagMaxFrequency, "user");
+					renderedHTML.append("<li class='" + tagSize + "'>");
+					renderedHTML.append("<a href='" + link + "' title='" + tagCount + " posts' style='font-size:" + fontSize + "%' >" + tagName + " </a>");
+					renderedHTML.append("</li>");
+				}
+			} else {
+				renderedHTML.append("you have no tags in this category");
 			}
+			
 			renderedHTML.append("</ul>");
 			renderedHTML.append("</div>");
 		}
-		
-
-		
-	
-		
-		
-		//return ValidationUtils.present(tagsToString) ? "<div id='tags'>" + tagsToString + "</div>" : "";
 			return renderedHTML.toString();
 	}
 	
+	
+	/**
+	 * saves the tags of a list of BibTex posts into a list of tags
+	 * @param posts - the list of BibTex posts
+	 * @return tagspost - a list of tags
+	 */
+	private List<Tag> addTagsToListBibTex(List<Post<BibTex>> posts) {
+		List<Tag> tagspost = new LinkedList<Tag>();
+		if (!posts.isEmpty()) {
+			for (Post<BibTex> p: posts) {
+				Set<Tag> list = p.getTags();
+				for (Tag t: list) {
+					tagspost.add(t);
+				}
+			}
+		}
+		return tagspost;
+	}
+	
+	/**
+	 * saves the tags of a list of BibTex posts into a list of tags
+	 * @param posts - the list of BibTex posts
+	 * @return tagspost - a list of tags
+	 */
+	private List<Tag> addTagsToListBookmarks(List<Post<Bookmark>> posts) {
+		List<Tag> tagspost = new LinkedList<Tag>();
+		if (!posts.isEmpty()) {
+			for (Post<Bookmark> p: posts) {
+				Set<Tag> list = p.getTags();
+				for (Tag t: list) {
+					tagspost.add(t);
+				}
+			}
+		}	
+		return tagspost;
+	}
+
 	/**
 	 * returns the css Class for a given tag
 	 * 
