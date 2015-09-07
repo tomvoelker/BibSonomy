9f89248edd021d6f06f1ceee616d9e5d8bee291a
Daniel Zoller <daniel.zoller@uni-wuerzburg.de>
2015-09-08 00:26:24 +0200

    just a little cleanup

--- bibsonomy-opensocial/src/main/resources/database/migrations/3.3.0/oauth_token_callbackURL_fix.sql
+++ bibsonomy-opensocial/src/main/resources/database/migrations/3.3.0/oauth_token_callbackURL_fix.sql
@@ -1,3 +1,3 @@
 /* issue #2522, alter column `callback_url` datatype to TEXT - was VARCHAR(255) */
 ALTER TABLE `oauth_provider_tokens` 
-CHANGE COLUMN `callback_url` `callback_url` TEXT NULL DEFAULT NULL ;
+CHANGE COLUMN `callback_url` `callback_url` TEXT NULL DEFAULT NULL;
