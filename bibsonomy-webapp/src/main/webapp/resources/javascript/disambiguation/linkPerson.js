e1664898d73998a02c09c2dcffd1118ca621e2e5
Daniel Zoller <daniel.zoller@uni-wuerzburg.de>
2015-09-10 11:43:03 +0200

    removed empty js file

--- bibsonomy-webapp/src/main/webapp/resources/javascript/disambiguation/linkPerson.js
+++ bibsonomy-webapp/src/main/webapp/resources/javascript/disambiguation/linkPerson.js
@@ -1,29 +1,7 @@
-
 $(document).ready(function() {
 	$(".btnLinkPerson").on("click", function() {
-		var e = $(this); 	
+		var e = $(this);
 		$("#linkShowPerson").attr('href', e.attr("data-person-url"));
 		$("#fieldLinkPersonId").val( e.attr("data-person-id"));
 	});
-//	
-//	$("#btnLinkPersonSubmit").on("click", function() {
-//		var e = $(this); 
-//		$.post("/person", {
-//			formAction: "addName",
-//			formPersonId: e.attr("data-person-id"),
-//			formFirstName: e.attr("data-firstName"),
-//			formLastName: e.attr("data-lastName")
-//		}).done(function(data) {
-//			$.post("/person",
-//					{ 	formAction: "addRole",
-//						formInterHash: e.attr("data-resource-simhash1"),
-//						formPersonId: e.attr("data-person-id") ,
-//						formPersonRole: "AUTHOR",
-//						formPersonIndex: e.attr("data-author-index")
-//					}
-//			).done(function(data) {
-//				window.location.href = "/person/" + e.attr("data-person-id");
-//			});
-//		});
-//	});
 });
