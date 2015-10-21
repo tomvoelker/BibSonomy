b9d9502875fff916f759e70cc66016c00129a343
Tom Hanika <hanika@cs.uni-kassel.de>
2015-10-21 14:27:54 +0200

    Fixed issue #2540.
    (More or less a MacGyver fix. One may rethink the way simhash1 is used here.)

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/DisambiguationPageController.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/DisambiguationPageController.java
@@ -96,7 +96,15 @@ public class DisambiguationPageController extends SingleResourceListController i
 		}
 		
 		final BibTex res = command.getPost().getResource();
-		final List<PersonName> persons = res.getPersonNamesByRole(command.getRequestedRole());
+		final List<PersonName> personsTmp = res.getPersonNamesByRole(command.getRequestedRole());
+		final List<PersonName> persons;
+		// MacGyver-fix, in case there are multiple similar simhash1 caused by Author == Editor  
+		if (personsTmp == null ){ 
+			final PersonResourceRelationType requestedRole = PersonResourceRelationType.valueOf("EDITOR");
+			persons = res.getPersonNamesByRole(requestedRole);
+		}else{
+			persons = personsTmp;
+		}
 		
 		final PersonName requestedName = persons.get(command.getRequestedIndex());
 		command.setPersonName(requestedName);
