77fd006ad6900a4bbb665f67c340d24d98e3d457
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2015-11-26 00:40:11 +0100

    moved js files from bs directory

--- bibsonomy-webapp/src/main/webapp/resources/javascript/merged/discussion.js
+++ bibsonomy-webapp/src/main/webapp/resources/javascript/merged/discussion.js
@@ -15,13 +15,13 @@ includeJavaScript("/resources/jquery/plugins/flot/excanvas.js"); // IE fix
 includeJavaScript("/resources/jquery/plugins/flot/jquery.flot.js");
 includeJavaScript("/resources/jquery/plugins/flot/jquery.flot.stack.js");
 
-includeJavaScript("/resources/javascript/bs/discussion/discussion.js");
-includeJavaScript("/resources/javascript/bs/discussion/comments.js");
-includeJavaScript("/resources/javascript/bs/discussion/reviews.js");
-includeJavaScript("/resources/javascript/bs/discussion/csllinks.js");
+includeJavaScript("/resources/javascript/discussion/discussion.js");
+includeJavaScript("/resources/javascript/discussion/comments.js");
+includeJavaScript("/resources/javascript/discussion/reviews.js");
+includeJavaScript("/resources/javascript/discussion/csllinks.js");
 
-includeJavaScript("/resources/javascript/bs/citeproc/csllocales.js");
-includeJavaScript("/resources/javascript/bs/citeproc/xmldom.js");
-includeJavaScript("/resources/javascript/bs/citeproc/xmle4x.js");
-includeJavaScript("/resources/javascript/bs/citeproc/citeproc.js");
-includeJavaScript("/resources/javascript/bs/citeproc/cslstyles.js");
+includeJavaScript("/resources/javascript/citeproc/csllocales.js");
+includeJavaScript("/resources/javascript/citeproc/xmldom.js");
+includeJavaScript("/resources/javascript/citeproc/xmle4x.js");
+includeJavaScript("/resources/javascript/citeproc/citeproc.js");
+includeJavaScript("/resources/javascript/citeproc/cslstyles.js");
