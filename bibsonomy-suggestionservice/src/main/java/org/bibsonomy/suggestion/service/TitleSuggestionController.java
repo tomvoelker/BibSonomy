cde07efd998ffd3a4e6cd22d9597d20e0a02d29d
Jenkins <webmaster@bibsonomy.org>
2015-12-02 19:44:07 +0100

    updated/added license header(s)

--- bibsonomy-suggestionservice/src/main/java/org/bibsonomy/suggestion/service/TitleSuggestionController.java
+++ bibsonomy-suggestionservice/src/main/java/org/bibsonomy/suggestion/service/TitleSuggestionController.java
@@ -1,5 +1,5 @@
 /**
- * BibSonomy Suggestion Service - Service for suggesting tags, resources, users
+ * BibSonomy - A blue social bookmark and publication sharing system.
  *
  * Copyright (C) 2006 - 2015 Knowledge & Data Engineering Group,
  *                               University of Kassel, Germany
@@ -12,16 +12,16 @@
  *                               http://www.l3s.de/
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Affero General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.bibsonomy.suggestion.service;
