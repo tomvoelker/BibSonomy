633313e3902f3c98a71395238e51d5bb0b2a103d
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2016-01-10 23:14:02 +0100

    added catch for SearchPhaseExecutionException

---
+++ bibsonomy-search/src/main/java/org/bibsonomy/search/InvalidSearchRequestException.java
@@ -0,0 +1,10 @@
+package org.bibsonomy.search;
+
+/**
+ * exception indicating that the user entered an invalid search query
+ * 
+ * @author dzo
+ */
+public class InvalidSearchRequestException extends RuntimeException {
+	private static final long serialVersionUID = -431648472171377762L;
+}
