ba576a328ebfe6f17d3853a0cf39a7660e011c01
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2016-01-19 22:45:12 +0100

    fixed #2400 (validator validated user and hash; clearAll does not require this info)

---
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/ajax/action/ClipboardAction.java
@@ -0,0 +1,18 @@
+package org.bibsonomy.webapp.command.ajax.action;
+
+/**
+ * action for the clipboard
+ *
+ * @author dzo
+ */
+public enum ClipboardAction {
+	
+	/** unpick all posts */
+	CLEARALL,
+	
+	/** pick post */
+	PICK,
+	
+	/** unpick post */
+	UNPICK;
+}
