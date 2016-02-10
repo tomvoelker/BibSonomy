a4f1666193ce6a9e1bf032e636c7dd93d4d16889
Thomas Niebler <thomas.niebler@uni-wuerzburg.de>
2016-02-10 23:15:51 +0100

    added includeNoscript option

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/util/captcha/ReCaptcha2.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/util/captcha/ReCaptcha2.java
@@ -61,14 +61,39 @@ public class ReCaptcha2 implements Captcha {
 	private String privateKey;
 	private String publicKey;
 	private String recaptchaServer;
+	private boolean includeNoscript;
 	
 	/* (non-Javadoc)
 	 * @see org.bibsonomy.webapp.util.captcha.Captcha#createCaptchaHtml(java.util.Locale)
 	 */
 	@Override
 	public String createCaptchaHtml(final Locale locale) {
-		return "<script src='https://www.google.com/recaptcha/api.js?hl=" + locale.getLanguage() + "'></script>" + 
+		String captchaHtml = "<script src='https://www.google.com/recaptcha/api.js?hl=" + locale.getLanguage() + "'></script>" + 
 				"<div class=\"g-recaptcha\" data-sitekey=\"" + this.publicKey +  "\"></div>";
+		if (this.includeNoscript) {
+			captchaHtml += "<noscript>"
+					+ "<div style=\"width: 302px; height: 422px;\">"
+					+ "<div style=\"width: 302px; height: 422px; position: relative;\">"
+					+ "<div style=\"width: 302px; height: 422px; position: absolute;\">"
+					+ "<iframe src=\"https://www.google.com/recaptcha/api/fallback?hl=" + locale.getLanguage() + "&k=" + this.publicKey + "\""
+					+ "frameborder=\"0\" scrolling=\"no\""
+					+ "style=\"width: 302px; height:422px; border-style: none;\">"
+					+ "</iframe>"
+					+ "</div>"
+					+ "<div style=\"width: 300px; height: 60px; border-style: none;"
+					+ "bottom: 12px; left: 25px; margin: 0px; padding: 0px; right: 25px;"
+					+ "background: #f9f9f9; border: 1px solid #c1c1c1; border-radius: 3px;\">"
+					+ "<textarea id=\"g-recaptcha-response\" name=\"g-recaptcha-response\""
+					+ "class=\"g-recaptcha-response\""
+					+ "style=\"width: 250px; height: 40px; border: 1px solid #c1c1c1;"
+					+ "margin: 10px 25px; padding: 0px; resize: none;\" >"
+					+ "</textarea>"
+					+ "</div>"
+					+ "</div>"
+					+ "</div>"
+					+ "</noscript>";
+		}
+		return captchaHtml;
 	}
 
 	/* (non-Javadoc)
@@ -148,5 +173,19 @@ public class ReCaptcha2 implements Captcha {
 	 */
 	public void setClient(CloseableHttpClient client) {
 		this.client = client;
+	}
+
+	/**
+	 * @return the includeNoscript
+	 */
+	public boolean isIncludeNoscript() {
+		return this.includeNoscript;
+	}
+
+	/**
+	 * @param includeNoscript the includeNoscript to set
+	 */
+	public void setIncludeNoscript(boolean includeNoscript) {
+		this.includeNoscript = includeNoscript;
 	}
 }
