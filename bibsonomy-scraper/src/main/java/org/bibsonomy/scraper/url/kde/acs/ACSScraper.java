117bfc07dac12fa52581bd275e0bfc859d1e19ad
mabed <devnull@localhost>
2016-04-07 19:34:02 +0200

    resolving #2527

--- bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/acs/ACSScraper.java
+++ bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/acs/ACSScraper.java
@@ -34,7 +34,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.bibsonomy.common.Pair;
-import org.bibsonomy.model.util.BibTexUtils;
 import org.bibsonomy.scraper.AbstractUrlScraper;
 import org.bibsonomy.scraper.ScrapingContext;
 import org.bibsonomy.scraper.exceptions.InternalFailureException;
@@ -64,7 +63,8 @@ public class ACSScraper extends AbstractUrlScraper {
 	private static final Pattern pathPatternBibtex = Pattern.compile(ACS_BIBTEX_PATH + ".*");
 	
 	private static final List<Pair<Pattern,Pattern>> patterns = new LinkedList<Pair<Pattern,Pattern>>();
-
+	private final Pattern URL_PATTERN_FOR_URL = Pattern.compile("URL = \\{ \n        (.*)\n    \n\\}");
+	
 	static {
 		final Pattern hostPattern = Pattern.compile(".*" + "pubs.acs.org");
 		patterns.add(new Pair<Pattern, Pattern>(hostPattern, pathPatternBibtex));
@@ -114,7 +114,6 @@ public class ACSScraper extends AbstractUrlScraper {
 		 * clean the bibtex for better format
 		 */
 		if (bibResult != null) {
-			final Pattern URL_PATTERN_FOR_URL = Pattern.compile("URL = \\{ \n        (.*)\n    \n\\}");
 			Matcher m = URL_PATTERN_FOR_URL.matcher(bibResult);
 			if(m.find()) {
 				bibResult = bibResult.replaceAll(URL_PATTERN_FOR_URL.toString(), "URL = {" + m.group(1) + "}");
