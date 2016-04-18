42110da9078d7a9e23beddf9e97633a6d3bf9b03
Jan Pfister <janpfister95@gmail.com>
2016-04-18 18:32:37 +0200

    working on typeahead and <ul> <li>

--- bibsonomy-webapp/src/main/webapp/resources/javascript/settings.js
+++ bibsonomy-webapp/src/main/webapp/resources/javascript/settings.js
@@ -9,4 +9,39 @@ $(function() {
 		$(this).children('.fa').removeClass('fa-times').addClass('fa-check');
 		$(this).children('.button-text').text(getString('groups.documentsharing.shared'));
 	});
+
+
+
+
+//instantiate the bloodhound suggestion engine
+var engine = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('d'),
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    highlight: true,
+    local: [{
+        d: "BibTeX"
+    }, {
+        d: "Endnote"
+    }]
+});
+
+
+
+// initialize the bloodhound suggestion engine
+engine.initialize();
+
+$('.typeahead').typeahead(null, {
+    displayKey: 'd',
+    source: engine.ttAdapter()
+});
+
+
+$('#searchCitationAutocomplete').on('typeahead:select', function (e, datum) {
+	 alert(datum + "chosen");
+	$("#favouriteLayoutsList ul").append('<li class="list-group-item"><input type="hidden" name="user.settings.favouriteLayouts"  id="' + datum + '" value="' + datum + '" /><span class="btn btn-default badge label-danger delete-Style">Delete</span>' + datum + '</li>');
+});
+
+$('.delete-Style').click(function(){
+	$(this).parent().remove();
+});
 });
