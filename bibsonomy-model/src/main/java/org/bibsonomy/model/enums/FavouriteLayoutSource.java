c9c5837a1770e09543b92240ac028bd6a70ef453
Jan Pfister <janpfister95@gmail.com>
2016-04-20 10:48:13 +0200

    implemented converter for spring

---
+++ bibsonomy-model/src/main/java/org/bibsonomy/model/enums/FavouriteLayoutSource.java
@@ -0,0 +1,10 @@
+package org.bibsonomy.model.enums;
+
+/**
+ * TODO: add documentation to this class
+ *
+ * @author jp
+ */
+public enum FavouriteLayoutSource {
+	SIMPLE, JABREF
+}
---
+++ bibsonomy-web-common/src/main/java/org/bibsonomy/web/spring/converter/StringToFavouriteLayoutConverter.java
@@ -0,0 +1,29 @@
+package org.bibsonomy.web.spring.converter;
+
+
+import org.bibsonomy.model.enums.FavouriteLayoutSource;
+import org.bibsonomy.model.user.settings.FavouriteLayout;
+import org.springframework.core.convert.converter.Converter;
+
+/**
+ * TODO: add documentation to this class
+ *
+ * @author pfister
+ */
+public class StringToFavouriteLayoutConverter implements Converter<String, FavouriteLayout>{
+
+	/* (non-Javadoc)
+	 * @see org.springframework.core.convert.converter.Converter#convert(java.lang.Object)
+	 */
+	@Override
+	public FavouriteLayout convert(String sourceandstyle) {		
+		String splitted[] = sourceandstyle.split("/");
+		if(splitted.length != 2){
+			throw new IllegalArgumentException("Foramt has to be source/style");
+		}
+		FavouriteLayoutSource source = FavouriteLayoutSource.valueOf(sourceandstyle);
+		String style = splitted[2];
+		return new FavouriteLayout(source,style);
+	}
+
+}
