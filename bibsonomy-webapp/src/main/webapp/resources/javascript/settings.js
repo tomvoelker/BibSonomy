62224da9b94a1252d5a8f87845cc1ced14f507d9
Jan Pfister <janpfister95@gmail.com>
2016-04-21 14:02:28 +0200

    Settings working :)

--- bibsonomy-webapp/src/main/webapp/resources/javascript/settings.js
+++ bibsonomy-webapp/src/main/webapp/resources/javascript/settings.js
@@ -9,43 +9,65 @@ $(function() {
 		$(this).children('.fa').removeClass('fa-times').addClass('fa-check');
 		$(this).children('.button-text').text(getString('groups.documentsharing.shared'));
 	});
-
-
-map = {};
-styles = [];
      
-var data = '[{"source": "SIMPLE", "style": "BibTeX"},{"source": "SIMPLE", "style": "Endnote"}]';
-var jsonObj = $.parseJSON(data);
+var localdata = '[{"source": "SIMPLE", "displayName": "BibTeX", "name":"BIBTEX"},{"source": "SIMPLE", "displayName": "Endnote", "name":"ENDNOTE"}]';
+var jsonObj = $.parseJSON(localdata);
 
-$.each(jsonObj, function (i, favLay) {
-	//alert(favLay.style);
-	map[favLay.style] = favLay;
-	styles.push(favLay.style);
-	});
+//alert(jsonObj);
+
+//var localdata2 = '{"source": "SIMPLE", "displayName": "TESTETSTEST"}';
+//var jsonObj2 = $.parseJSON(localdata2);
+//jsonObj.push(jsonObj2);
+
+
+$.get("/layoutinfo", function (data) {
+    processResult(data);
+});
+
+function processResult(data) {
+	for (var prop in data.layouts) {
+//		alert("Key:" + prop);
+//		alert("Value:" + data.layouts[prop].displayName);
+		
+		var localdata2 = '{"source": "JABREF", "displayName": "' + data.layouts[prop].displayName + '", "name":"' + data.layouts[prop].name +'"}';
+		var jsonObj2 = $.parseJSON(localdata2);
+		jsonObj.push(jsonObj2);
+	}
+	
+	
+	initializeBloodhound();
+};
+
 //instantiate the bloodhound suggestion engine
+
+function initializeBloodhound() {
 var engine = new Bloodhound({
-    datumTokenizer: function (d) {return Bloodhound.tokenizers.whitespace(d.style);},
+    datumTokenizer: function (d) {return Bloodhound.tokenizers.whitespace(d.displayName);},
     queryTokenizer: Bloodhound.tokenizers.whitespace,
-    highlight: true,
-    local: jsonObj
+    //identify: function(obj) { return obj.name; },
+//    highlight: true,
+    local: jsonObj,
 });
 
 
 // initialize the bloodhound suggestion engine
 engine.initialize();
 
-$('.typeahead').typeahead(null, {
-    displayKey: 'style',
-    highlight: true,
+$('.typeahead').typeahead({
+	
+//	highlight: true,
+},
+{   displayKey: 'displayName',
     source: engine.ttAdapter()
 });
+};
 
 
-//$('#searchCitationAutocomplete').on('typeahead:select', function (e, datum) {
-//	var toBeAppended = '<li class="list-group-item favouriteLayoutsListItem"><input type="hidden" name="user.settings.favouriteLayouts"  id="SIMPLE/' + datum.value.toUpperCase() + '" value="SIMPLE/' + datum.value.toUpperCase() + '"/><span class="btn btn-default badge label-danger delete-Style">Delete</span>' + datum.value + '</li>';
-//	$('#favouriteLayoutsList').append(toBeAppended);
-//	clearFavouriteLayoutsList();
-//});
+$('#searchCitationAutocomplete').on('typeahead:select', function (e, datum) {
+	var toBeAppended = '<li class="list-group-item favouriteLayoutsListItem"><input type="hidden" name="user.settings.favouriteLayouts"  id="'+ datum.source.toUpperCase() +'/' + datum.name.toUpperCase() + '" value="'+datum.source.toUpperCase()+'/' + datum.name.toUpperCase() + '"/><span class="btn btn-default badge label-danger delete-Style">Delete</span>' + datum.displayName + '</li>';
+	$('#favouriteLayoutsList').append(toBeAppended);
+	clearFavouriteLayoutsList();
+});
 
 
 $('#searchCitationAutocomplete').on('keydown', function(event) {
@@ -67,7 +89,7 @@ $('.delete-Style').click(function(){
 function clearFavouriteLayoutsList() { //removing duplicates
 	var seen = {};
 	$('.favouriteLayoutsListItem').each(function() {
-	    var txt = $(this).text();
+	    var txt = $(this).text().toUpperCase();
 	    if (seen[txt])
 	        $(this).remove();
 	    else
