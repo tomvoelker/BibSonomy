fdf3bd0d6fa6f67c22509ad4979e253886fb014d
Jan Pfister <janpfister95@gmail.com>
2016-04-25 09:32:13 +0200

    nothing working

---
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/command/CSLLayoutCommand.java
@@ -0,0 +1,15 @@
+package org.bibsonomy.webapp.command;
+
+import java.util.Collection;
+
+import org.bibsonomy.scraper.Scraper;
+
+/**
+ * TODO: add documentation to this class
+ *
+ * @author jp
+ */
+public class CSLLayoutCommand extends ResourceViewCommand{
+	
+
+}
---
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/CSLLayoutsController.java
@@ -0,0 +1,30 @@
+package org.bibsonomy.webapp.controller;
+
+import org.bibsonomy.webapp.command.CSLLayoutCommand;
+import org.bibsonomy.webapp.util.MinimalisticController;
+import org.bibsonomy.webapp.util.View;
+import org.bibsonomy.webapp.view.Views;
+
+/**
+ * TODO: add documentation to this class
+ *
+ * @author jp
+ */
+public class CSLLayoutsController implements MinimalisticController<CSLLayoutCommand>{
+
+	/* (non-Javadoc)
+	 * @see org.bibsonomy.webapp.util.MinimalisticController#instantiateCommand()
+	 */
+	@Override
+	public CSLLayoutCommand instantiateCommand() {
+		return new CSLLayoutCommand();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.bibsonomy.webapp.util.MinimalisticController#workOn(org.bibsonomy.webapp.command.ContextCommand)
+	 */
+	@Override
+	public View workOn(CSLLayoutCommand command) {
+		return Views.CSL;
+	}
+}
