a0f6d2e09c0f2aaf75f799d5450b83cce98abc0c
Jan Pfister <janpfister95@gmail.com>
2016-04-28 18:47:57 +0200

    implemented /csl-layout/${style}/bibtex/654654

--- bibsonomy-webapp/src/main/webapp/resources/javascript/citeproc-wrapper.js
+++ bibsonomy-webapp/src/main/webapp/resources/javascript/citeproc-wrapper.js
@@ -4,36 +4,43 @@ $(function() {
 	
 	
 	var format = container.data('style');
-	
-	$.get( "/csl-style/" + format, function( data ) {
-		
-		alert( data );
-		});
-	
-		$.ajax({
-			url: url,
-			success: function(data) {
-				var sys = new Sys(data);
-				for (var key in data) {
-					// TODO: use the correct style
-					var citeproc = new CSL.Engine(sys, xml);
-					var citation = {
-						"citationItems" : [ {
-							id : key
-						} ],
-						"properties" : {
-							"noteIndex" : 1
-						}
-					};
-					var renderedCitation = citeproc.appendCitationCluster(citation);
-					var bibliographyEntry = citeproc.makeBibliography();
-					var output = bibliographyEntry[1][0];
-					container.append(output);
+	$.ajax({
+		type: "get",
+	    url: "/csl-style/" + format,
+	    dataType: "xml",
+	    success: function(xmldata) {
+	    	var serializer = new XMLSerializer(); 
+	    	var original = serializer.serializeToString(xmldata);
+	    	
+	    	var xml = $("<div/>").html(original).text();
+	    	xml = $.trim(xml);
+	    	
+	    	//alert(xml);
+	    	 
+	    	$.ajax({
+				url: url,
+				success: function(data) {
+					var sys = new Sys(data);
+					for (var key in data) {
+						var citeproc = new CSL.Engine(sys, xml);
+						var citation = {
+							"citationItems" : [ {
+								id : key
+							} ],
+							"properties" : {
+								"noteIndex" : 1
+							}
+						};
+						var renderedCitation = citeproc.appendCitationCluster(citation);
+						var bibliographyEntry = citeproc.makeBibliography();
+						var output = bibliographyEntry[1][0];
+						container.append(output);
+					}
 				}
-			}
-		});
-	
-	
+			});
+		
+	    }
+	});	
 });
 
 // FIXME: code copy
@@ -54,4 +61,5 @@ function Sys(data) {
 		};
 		return ABBREVS[name];
 	};
+
 };
