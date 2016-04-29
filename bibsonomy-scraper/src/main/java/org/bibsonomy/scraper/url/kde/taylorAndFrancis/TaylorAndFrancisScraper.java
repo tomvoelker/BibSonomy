9bccbd1bbe5cd72e796cfb909c51c6724608efa3
mabed <devnull@localhost>
2016-04-29 11:32:30 +0200

    resolving #2628 closing #2628 enhancement the scraper to be able to scrape from the hosthttp://amstat.tandfonline.com/

--- bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/taylorAndFrancis/TaylorAndFrancisScraper.java
+++ bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/taylorAndFrancis/TaylorAndFrancisScraper.java
@@ -52,18 +52,18 @@ public class TaylorAndFrancisScraper extends AbstractUrlScraper implements Refer
 	private static final String SITE_NAME = "Taylor & Francis Online";
 	private static final String SITE_URL = "http://www.tandfonline.com/";
 	private static final String INFO = "This scraper parses a publication page from " + href(SITE_URL, SITE_NAME)+".";
-
-	private static final String TANDF_HOST_NAME = "tandfonline.com";
 	
+	private static final String TANDF_HOST_NAME = "tandfonline.com";
 	private static final List<Pair<Pattern, Pattern>> PATTERNS = Collections.singletonList(new Pair<Pattern, Pattern>(Pattern.compile(".*" + TANDF_HOST_NAME), AbstractUrlScraper.EMPTY_PATTERN));
 
 	private static final Pattern DOI_PATTERN = Pattern.compile("/10\\.1080/\\d++(\\.\\d++)*");
 	
-	private static final String TANDF_BIBTEX_DOWNLOAD_PATH = "action/downloadCitation";
+	private static final String TANDF_BIBTEX_DOWNLOAD_PATH = "/action/downloadCitation";
 	private static final String DOWNLOADFILENAME = "tandf_rajp2080_124";
 	
 	private final Pattern URL_PATTERN_FOR_URL = Pattern.compile("URL = \\{ \n        (.*)\n    \n\\}");
-	
+	private static final String HTTP = "http://";
+
 	private final static Pattern REF_PATTERN = Pattern.compile("(?s)<ul class=\"references\">(.*)</ul></div></div>");
 	private static PostMethod postContent(PostMethod method, String doi) {
 		method.addParameter("doi", doi);
@@ -102,9 +102,10 @@ public class TaylorAndFrancisScraper extends AbstractUrlScraper implements Refer
 		try {
 			final HttpClient client = WebUtils.getHttpClient();
 			//get the page to start the session
+			
 			WebUtils.getContentAsString(client, scrapingContext.getUrl().toExternalForm());
 			//post to receive the BibTeX file
-			final PostMethod method = new PostMethod(SITE_URL + TANDF_BIBTEX_DOWNLOAD_PATH);
+			final PostMethod method = new PostMethod(HTTP + scrapingContext.getUrl().getHost().toString() + TANDF_BIBTEX_DOWNLOAD_PATH);
 			final String doi = matcher.group().substring(1);
 			String bibtexEntry = WebUtils.getPostContentAsString(client, postContent(method, doi));
 			if (present(bibtexEntry)) {
