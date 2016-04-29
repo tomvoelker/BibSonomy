667127ee3fd48452cd651ed973eb85abc0d0c025
mabed <devnull@localhost>
2016-04-29 21:28:35 +0200

    new scraper for spie digital library

---
+++ bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/spiedigitallibrary/SpieDigitalLibraryScraper.java
@@ -0,0 +1,88 @@
+/**
+ * BibSonomy-Scraper - Web page scrapers returning BibTeX for BibSonomy.
+ *
+ * Copyright (C) 2006 - 2015 Knowledge & Data Engineering Group,
+ *                               University of Kassel, Germany
+ *                               http://www.kde.cs.uni-kassel.de/
+ *                           Data Mining and Information Retrieval Group,
+ *                               University of Würzburg, Germany
+ *                               http://www.is.informatik.uni-wuerzburg.de/en/dmir/
+ *                           L3S Research Center,
+ *                               Leibniz University Hannover, Germany
+ *                               http://www.l3s.de/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.bibsonomy.scraper.url.kde.spiedigitallibrary;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.bibsonomy.common.Pair;
+import org.bibsonomy.scraper.AbstractUrlScraper;
+import org.bibsonomy.scraper.exceptions.ScrapingException;
+import org.bibsonomy.scraper.generic.GenericBibTeXURLScraper;
+
+/**
+ * @author Mohammed abed
+ */
+public class SpieDigitalLibraryScraper extends GenericBibTeXURLScraper{
+
+	private static final String SITE_NAME = "SPIE Digital Library";
+	private static final String SITE_URL = "http://remotesensing.spiedigitallibrary.org/";
+	private static final String INFO = "This scraper parses a publication page of citations from " + href(SITE_URL, SITE_NAME) + ".";
+	private static final List<Pair<Pattern, Pattern>> PATTERNS = new LinkedList<Pair<Pattern, Pattern>>();
+	private static final String SPIE_DIGITAL_LIBRARY_HOST = "remotesensing.spiedigitallibrary.org";
+	private static final String DOWNLOAD_URL = "http://remotesensing.spiedigitallibrary.org/downloadCitation.aspx?format=bib&articleid=";
+	private static final Pattern ID_PATERN_FROM_URL = Pattern.compile(".+?articleid=(.+)$");
+	static {
+		PATTERNS.add(new Pair<Pattern, Pattern>(Pattern.compile(".*" + SPIE_DIGITAL_LIBRARY_HOST), AbstractUrlScraper.EMPTY_PATTERN));
+	}
+
+	@Override
+	protected String getDownloadURL(URL url) throws ScrapingException,IOException {
+
+		Matcher m = ID_PATERN_FROM_URL.matcher(url.toString());
+		if (m.find()) {
+			final String id = m.group(1);
+			return DOWNLOAD_URL + id;
+		}
+		return null;
+	}
+	
+	@Override
+	public String getSupportedSiteName() {
+		return SITE_NAME;
+	}
+
+	@Override
+	public String getSupportedSiteURL() {
+		return SITE_URL;
+	}
+
+	@Override
+	public String getInfo() {
+		return INFO;
+	}
+
+	@Override
+	public List<Pair<Pattern, Pattern>> getUrlPatterns() {
+		return PATTERNS;
+	}
+
+}
