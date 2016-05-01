1a3d8cb0893c8acc5cd18a6bddd676d2e0ce6369
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2016-05-02 00:12:05 +0200

    working delete for comments and reviews

--- bibsonomy-webapp/src/main/webapp/resources/javascript/discussion/discussion.js
+++ bibsonomy-webapp/src/main/webapp/resources/javascript/discussion/discussion.js
@@ -33,17 +33,49 @@ $(function() {
 	});
 	
 	$('.deleteLink').click(function() {
+		var link = $(this);
 		var type = $(this).data("type");
 		if (!confirmDeleteByUser(type)) {
 			return false;
 		}
 		var interhash = $('#discussion').data('interhash');
-		var hash = $(this).parents('.media-body').find('.info').first().data('discussion-item-hash');
+		var hash = $(this).parents('.media-body:first').find('.info').first().data('discussion-item-hash');
+		
 		$.ajax({
-			url: '/ajax/' + type + "s"
+			url: '/ajax/' + type + "s",
+			method: 'POST',
+			data:'hash=' + interhash + "&discussionItem.hash=" + hash + "&_method=delete&ckey=" + ckey,
+			success: function() {
+				var item = link.parents('li.media:first');
+				var replyButton = item.parent().siblings('.actions').find('.toggleReplies');
+				var badge = replyButton.find('.badge');
+				
+				var subCount = parseInt(badge.text());
+				if (subCount == 1) {
+					replyButton.remove();
+				} else {
+					badge.text(subCount - 1);
+				}
+				
+				var subItems = item.find('.subdiscussion:first>li.media').length;
+				if (subItems == 0) {
+					item.remove();
+				} else {
+					var left = item.children('.media-left:first');
+					var imageContainer = left.find('a.thumbnail');
+					imageContainer.find('img').remove();
+					imageContainer.find('span').remove();
+					
+					imageContainer.append($('<i class="fa fa-user fa-3x"></i>'));
+					var content = item.children('.media-body');
+					content.prepend($('<div class="alert alert-info">' + getString('post.resource.discussion.info') + '</div>'));
+					content.find('.actions:first>div.edit-media-buttons:last').remove();
+					content.find('.details:first').remove();
+					content.find('.info:first').text('');
+				}
+			}
 		});
-		alert(interhash)
-		alert(hash)
+		
 		return false;
 	});
 	
@@ -62,10 +94,35 @@ $(function() {
 		$('.createreview').hide();
 	}
 	
+	$('.createreview').submit(function() {
+		var data = $(this).serialize();
+		$.ajax({
+			url: '/ajax/reviews',
+			method: 'POST',
+			data: data,
+			success: function(data) {
+				var reload = (data.reload);
+				if (reload == "true") {
+					window.location.reload();
+					return;
+				}
+				alert("review created");
+			}
+		});
+		return false;
+	});
+	
 	$('.createcomment').submit(function() {
 		var data = $(this).serialize();
+		$.ajax({
+			url: '/ajax/comments',
+			method: 'POST',
+			data: data,
+			success: function() {
+				alert("created");
+			}
+		});
 		
-		alert(data);
 		return false;
 	});
 });
