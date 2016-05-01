241dd216058f1f409b00ec4493e0e23a0bc90a1d
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2016-05-01 20:42:05 +0200

    reverted all header changes

--- bibsonomy-common/src/main/java/org/bibsonomy/common/enums/PreviewSize.java
+++ bibsonomy-common/src/main/java/org/bibsonomy/common/enums/PreviewSize.java
@@ -1,7 +1,7 @@
 /**
  * BibSonomy-Common - Common things (e.g., exceptions, enums, utils, etc.)
  *
- * Copyright (C) 2006 - 2016 Knowledge & Data Engineering Group,
+ * Copyright (C) 2006 - 2015 Knowledge & Data Engineering Group,
  *                               University of Kassel, Germany
  *                               http://www.kde.cs.uni-kassel.de/
  *                           Data Mining and Information Retrieval Group,
