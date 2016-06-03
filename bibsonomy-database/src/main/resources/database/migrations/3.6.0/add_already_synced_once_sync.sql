8e898d90d47f9ff1a3daf261e71f98ad6de909f2
Viktor Hemsen <vhem@cs.uni-kassel.de>
2016-06-03 19:53:14 +0200

    set already_synced_once for users which have synced already

--- bibsonomy-database/src/main/resources/database/migrations/3.6.0/add_already_synced_once_sync.sql
+++ bibsonomy-database/src/main/resources/database/migrations/3.6.0/add_already_synced_once_sync.sql
@@ -1,3 +1,7 @@
 ALTER TABLE `sync` ADD `already_synced_once` TINYINT(1) NULL DEFAULT '0' AFTER `autosync`;
 
-UPDATE sync SET already_synced_once = 1;
+UPDATE sync s INNER JOIN
+	( 	SELECT DISTINCT sd.service_id, sd.user_name
+		FROM sync_data sd WHERE sd.status = "done"
+	) 	AS s2 ON s2.service_id = s.service_id and s2.user_name = s.user_name
+SET s.already_synced_once = 1;
