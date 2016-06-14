b2aacf408b0c0bd47c1d57fa44bced9801dc9cf8
mabed <devnull@localhost>
2016-06-14 15:46:52 +0200

    cleaning and adding all required test cases...

--- bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/springer/SpringerLinkScraper.java
+++ bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/springer/SpringerLinkScraper.java
@@ -26,31 +26,17 @@
  */
 package org.bibsonomy.scraper.url.kde.springer;
 
-import static org.bibsonomy.util.ValidationUtils.present;
-
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.URI;
-import org.apache.commons.httpclient.methods.GetMethod;
 import org.bibsonomy.common.Pair;
-import org.bibsonomy.model.util.BibTexUtils;
 import org.bibsonomy.scraper.AbstractUrlScraper;
-import org.bibsonomy.scraper.ScrapingContext;
-import org.bibsonomy.scraper.exceptions.InternalFailureException;
 import org.bibsonomy.scraper.exceptions.ScrapingException;
-import org.bibsonomy.scraper.exceptions.ScrapingFailureException;
 import org.bibsonomy.scraper.generic.GenericBibTeXURLScraper;
-import org.bibsonomy.scraper.url.kde.worldcat.WorldCatScraper;
-import org.bibsonomy.util.UrlUtils;
-import org.bibsonomy.util.WebUtils;
-import org.bibsonomy.util.id.ISBNUtils;
 
 /**
  * Scraper für SpringerLink.
@@ -62,21 +48,15 @@ public class SpringerLinkScraper extends GenericBibTeXURLScraper {
 	private static final String SITE_NAME = "SpringerLink";
 	private static final String SITE_URL = "http://www.springerlink.com/";
 	private static final String INFO = "This scraper parses a publication page from " + href(SITE_URL, SITE_NAME)+".";
-	private static final Pattern ID_PATTERN = Pattern.compile("(article|chapter|book)/(.+?)(/|$)");
+	private static final Pattern ID_PATTERN = Pattern.compile("(article|chapter)/(.+?)(/|$)");
 		
-	private static final String SPRINGER_CITATION_HOST_COM = "springerlink.com";
-	private static final String SPRINGER_CITATION_HOST_DE = "springerlink.de";
-	private static final String SPRINGER_CITATION_HOST_NEW = "link.springer.com";
-	private static final String SPRINGER_LINK_METAPRESS = "springerlink.metapress.com";
 	private static final String DOWNLOAD_URL = "http://citation-needed.services.springer.com/v2/references/";
 	private static final String DOWNLOAD_TYPE = "?format=bibtex&flavour=citation";
+	private static final String SPRINGER_CITATION_HOST = "link.springer.com";
 	
 	private static final List<Pair<Pattern,Pattern>> patterns = new LinkedList<Pair<Pattern,Pattern>>();
 	static{
-		patterns.add(new Pair<Pattern, Pattern>(Pattern.compile(".*" + SPRINGER_CITATION_HOST_COM), AbstractUrlScraper.EMPTY_PATTERN));
-		patterns.add(new Pair<Pattern, Pattern>(Pattern.compile(".*" + SPRINGER_CITATION_HOST_DE), AbstractUrlScraper.EMPTY_PATTERN));
-		patterns.add(new Pair<Pattern, Pattern>(Pattern.compile(".*" + SPRINGER_CITATION_HOST_NEW), AbstractUrlScraper.EMPTY_PATTERN));
-		patterns.add(new Pair<Pattern, Pattern>(Pattern.compile(".*" + SPRINGER_LINK_METAPRESS), AbstractUrlScraper.EMPTY_PATTERN));
+		patterns.add(new Pair<Pattern, Pattern>(Pattern.compile(".*" + SPRINGER_CITATION_HOST), AbstractUrlScraper.EMPTY_PATTERN));
 	}
 		
 	@Override
