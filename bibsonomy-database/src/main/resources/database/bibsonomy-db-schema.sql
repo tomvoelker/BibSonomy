a20acf4e571b982cdca1d87a5cfac6783d89c2e9
Thomas Niebler <thomas.niebler@uni-wuerzburg.de>
2016-06-27 14:09:42 +0200

    added current_content_id column to log_postchange and adapted SQL statements added update of current_content_id after each change and deletion of a post

--- bibsonomy-database/src/main/resources/database/bibsonomy-db-schema.sql
+++ bibsonomy-database/src/main/resources/database/bibsonomy-db-schema.sql
@@ -1787,10 +1787,10 @@ CREATE TABLE `log_postchange` (
 	`post_editor` VARCHAR(50) NOT NULL,
 	`old_content_id` INT(11) NOT NULL,
 	`new_content_id` INT(11) NOT NULL,
+	`current_content_id` INT(11) NOT NULL,
 	`content_type` TINYINT(4) NOT NULL,
-	`date` DATETIME NOT NULL
+	`date` DATETIME NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
