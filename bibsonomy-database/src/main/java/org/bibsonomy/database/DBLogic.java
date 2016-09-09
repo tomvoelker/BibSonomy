15b2b74babfc8bd844cca66e5300f5824246a523
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2016-09-09 14:17:54 +0200

    logging for exception to get the stack trace

--- bibsonomy-database/src/main/java/org/bibsonomy/database/DBLogic.java
+++ bibsonomy-database/src/main/java/org/bibsonomy/database/DBLogic.java
@@ -1673,6 +1673,7 @@ public class DBLogic implements LogicInterface {
 				} catch (final Exception ex) {
 					// some exception other than those covered in the
 					// DatabaseException was thrown
+					log.error("updating post " + post.getResource().getIntraHash() + "/" + this.loginUser.getName() + " failed", ex);
 					collectedException.addToErrorMessages(post.getResource().getIntraHash(), new UnspecifiedErrorMessage(ex));
 				}
 			}
