16c97b6324a74de0d7dad442b6974cb27844ee7c
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2016-11-23 12:15:46 +0100

    fixing indexing of documents that contain db fields in misc field

---
+++ bibsonomy-search-elasticsearch/src/test/java/org/bibsonomy/search/es/index/PublicationConverterTest.java
@@ -0,0 +1,31 @@
+package org.bibsonomy.search.es.index;
+
+import static org.junit.Assert.assertFalse;
+
+import org.bibsonomy.model.BibTex;
+import org.bibsonomy.model.Post;
+import org.bibsonomy.model.User;
+import org.bibsonomy.search.es.ESConstants;
+import org.bibsonomy.search.index.utils.SimpleFileContentExtractorService;
+import org.bibsonomy.testutil.TestUtils;
+import org.junit.Test;
+
+import java.util.Map;
+
+/**
+ * @author dzo
+ */
+public class PublicationConverterTest {
+	private static final PublicationConverter CONVERTER = new PublicationConverter(TestUtils.createURI("https://www.bibsonomy.org"), new SimpleFileContentExtractorService());
+
+	@Test
+	public void testConvert() {
+		final Post<BibTex> post = new Post<>();
+		post.setUser(new User("testuser"));
+		final BibTex publication = new BibTex();
+		publication.setMisc("editors = { Wade Wilson and Vanessa Geraldine Carlysle }");
+		post.setResource(publication);
+		final Map<String, Object> convertedPost = CONVERTER.convert(post);
+		assertFalse("converted post contains misc field", convertedPost.containsKey(ESConstants.Fields.Publication.EDITORS));
+	}
+}
