9481fa2f79324e19c1f441ec5d6819c686379dd8
Johannes Büttner <joh.buettner@gmail.com>
2017-01-18 12:53:24 +0100

    updated AmazonScraper test files

--- bibsonomy-scraper/src/test/java/org/bibsonomy/scraper/url/kde/amazon/AmazonScraperTest.java
+++ bibsonomy-scraper/src/test/java/org/bibsonomy/scraper/url/kde/amazon/AmazonScraperTest.java
@@ -26,8 +26,11 @@
  */
 package org.bibsonomy.scraper.url.kde.amazon;
 
+import static org.bibsonomy.scraper.junit.RemoteTestAssert.assertScraperResult;
+
 import org.bibsonomy.scraper.UnitTestRunner;
 import org.bibsonomy.scraper.junit.RemoteTest;
+import org.bibsonomy.scraper.url.kde.biorxiv.BioRxivScraper;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
@@ -43,7 +46,9 @@ public class AmazonScraperTest {
 	 */
 	@Test
 	public void url1TestRun(){
-		UnitTestRunner.runSingleTest("url_42");
+		final String url = "http://www.amazon.com/Semantic-Primer-Cooperative-Information-Systems/dp/0262012103/ref=pd_bbs_sr_1?ie=UTF8&amp;s=books&amp;qid=1200485020&amp;sr=1-1";
+		final String resultFile = "AmazonScraperUnitURLTest1.bib";
+		assertScraperResult(url, null, AmazonScraper.class, resultFile);
 	}
 	
 	/**
@@ -51,7 +56,9 @@ public class AmazonScraperTest {
 	 */
 	@Test
 	public void url2TestRun(){
-		UnitTestRunner.runSingleTest("url_46");
+		final String url = "http://www.amazon.de/gp/product//3827415020/ref=br_fq_2";
+		final String resultFile = "AmazonScraperUnitURLTest2.bib";
+		assertScraperResult(url, null, AmazonScraper.class, resultFile);
 	}
 
 	/**
@@ -59,7 +66,9 @@ public class AmazonScraperTest {
 	 */
 	@Test
 	public void url3TestRun(){
-		UnitTestRunner.runSingleTest("url_47");
+		final String url = "http://www.amazon.ca/Digital-Photography-Book-Scott-Kelby/dp/032147404X/ref=sr_1_3?ie=UTF8&s=books&qid=1201615938&sr=1-3";
+		final String resultFile = "AmazonScraperUnitURLTest3.bib";
+		assertScraperResult(url, null, AmazonScraper.class, resultFile);
 	}
 
 	/**
@@ -67,7 +76,9 @@ public class AmazonScraperTest {
 	 */
 	@Test
 	public void url4TestRun(){
-		UnitTestRunner.runSingleTest("url_48");
+		final String url = "http://www.amazon.fr/Mac-programmation-AppleScript-Dashboard-Core-Animation/dp/2100500767/ref=sr_1_5?ie=UTF8&s=books&qid=1201616015&r=1-5";
+		final String resultFile = "AmazonScraperUnitURLTest4.bib";
+		assertScraperResult(url, null, AmazonScraper.class, resultFile);
 	}
 
 	/**
@@ -75,7 +86,9 @@ public class AmazonScraperTest {
 	 */
 	@Test
 	public void url5TestRun(){
-		UnitTestRunner.runSingleTest("url_49");
+		final String url = "http://www.amazon.co.jp/Programming-Collective-Intelligence-Building-Applications/dp/0596529325/ref=sr_1_1/249-7857785-0612358?ie=UTF8&s=gateway&qid=1201616160&sr=8-1";
+		final String resultFile = "AmazonScraperUnitURLTest5.bib";
+		assertScraperResult(url, null, AmazonScraper.class, resultFile);
 	}
 
 	/**
@@ -83,7 +96,9 @@ public class AmazonScraperTest {
 	 */
 	@Test
 	public void url6TestRun(){
-		UnitTestRunner.runSingleTest("url_50");
+		final String url = "http://www.amazon.co.uk/Pro-2008-NET-Platform-Fourth/dp/1590598849/ref=sr_1_1?ie=UTF8&s=books&qid=1201616235&sr=1-1";
+		final String resultFile = "AmazonScraperUnitURLTest6.bib";
+		assertScraperResult(url, null, AmazonScraper.class, resultFile);
 	}
 	
 	/**
@@ -91,7 +106,9 @@ public class AmazonScraperTest {
 	 */
 	@Test
 	public void url7TestRun(){
-		UnitTestRunner.runSingleTest("url_57");
+		final String url = "http://www.amazon.com/Text-Mining-Predictive-Unstructured-Information/dp/0387954333";
+		final String resultFile = "AmazonScraperUnitURLTest7.bib";
+		assertScraperResult(url, null, AmazonScraper.class, resultFile);
 	}
 
 	/**
@@ -99,7 +116,9 @@ public class AmazonScraperTest {
 	 */
 	@Test
 	public void url8TestRun(){
-		UnitTestRunner.runSingleTest("url_105");
+		final String url = "http://www.amazon.de/Web-2-0-Unternehmenspraxis-Grundlagen-Fallstudien/dp/3486585797/ref=pd_bbs_sr_1?ie=UTF8&s=books&qid=1214381841&sr=8-1";
+		final String resultFile = "AmazonScraperUnitURLTest8.bib";
+		assertScraperResult(url, null, AmazonScraper.class, resultFile);
 	}
 	
 	/**
@@ -107,7 +126,9 @@ public class AmazonScraperTest {
 	 */
 	@Test
 	public void url9TestRun(){
-		UnitTestRunner.runSingleTest("url_160");
+		final String url = "http://www.amazon.de/gp/product/3426274914/ref=s9_top_bw_i3/278-8327421-8514538?pf_rd_m=A3JWKAKR8XB7XF&pf_rd_s=center-4&pf_rd_r=0QDEYW5TCNM11354JY22&pf_rd_t=101&pf_rd_p=473590033&pf_rd_i=299956";
+		final String resultFile = "AmazonScraperUnitURLTest9.bib";
+		assertScraperResult(url, null, AmazonScraper.class, resultFile);
 	}
 	
 	/**
@@ -115,13 +136,17 @@ public class AmazonScraperTest {
 	 */
 	@Test
 	public void url10TestRun(){
-		UnitTestRunner.runSingleTest("url_161");
+		final String url = "https://www.amazon.de/Sebastian-Deisler-Zur%C3%BCck-Geschichte-Fu%C3%9Fballspielers/dp/3941378287%3FSubscriptionId%3DAKIAJOMMTFWDOHFJSJXQ%26tag%3Dws%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3D3941378287";
+		final String resultFile = "AmazonScraperUnitURLTest10.bib";
+		assertScraperResult(url, null, AmazonScraper.class, resultFile);
 	}
 	/**
 	 * starts URL test with id url_272
 	 */
 	@Test
 	public void url11TestRun(){
-		UnitTestRunner.runSingleTest("url_272");
+		final String url = "http://www.amazon.de/Computational-Intelligence-Cyber-Security-Models-ebook/dp/B00GXY5FFC/ref=sr_1_1";
+		final String resultFile = "AmazonScraperUnitURLTest11.bib";
+		assertScraperResult(url, null, AmazonScraper.class, resultFile);
 	}
 }
