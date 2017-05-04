3a180609b075e7a81f3e8aed0bc19be112da45db
Mario Holtmüller <m.holtmueller@hothodata.de>
2017-05-04 11:52:28 +0200

    Updated PersonDatabaseManager tests

--- bibsonomy-database/src/test/java/org/bibsonomy/database/managers/PersonDatabaseManagerTest.java
+++ bibsonomy-database/src/test/java/org/bibsonomy/database/managers/PersonDatabaseManagerTest.java
@@ -30,6 +30,8 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Collections;
 
 import org.bibsonomy.model.BibTex;
@@ -39,6 +41,7 @@ import org.bibsonomy.model.Post;
 import org.bibsonomy.model.ResourcePersonRelation;
 import org.bibsonomy.model.User;
 import org.bibsonomy.model.enums.PersonResourceRelationType;
+import org.bibsonomy.testutil.TestUtils;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -150,10 +153,11 @@ public class PersonDatabaseManagerTest extends AbstractDatabaseManagerTest {
 	
 	/**
 	 * tests {@link PersonDatabaseManager#updateHomepage(Person, org.bibsonomy.database.common.DBSession)}
+	 * @throws MalformedURLException 
 	 */
 	@Test
-	public void testUpdateHomepage() {
-		final String homepage = "http://www.bibsonomy.org";
+	public void testUpdateHomepage(){
+		final URL homepage = TestUtils.createURL("http://www.bibsonomy.org");
 		this.testPerson.setHomepage(homepage);
 		PERSON_DATABASE_MANAGER.updateHomepage(this.testPerson, this.dbSession);
 		final Person person = PERSON_DATABASE_MANAGER.getPersonById(this.testPerson.getPersonId(), this.dbSession);
