a72eb823cc7926e9cf4e33027b052411c18da490
Robert Jäschke <jaeschke@l3s.de>
2018-03-12 21:33:41 +0100

    merged two scrapers dealing with the same site

--- bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/inspire/InspireScraper.java
+++ bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/inspire/InspireScraper.java
@@ -43,11 +43,8 @@ import org.bibsonomy.scraper.ScrapingContext;
 import org.bibsonomy.scraper.exceptions.InternalFailureException;
 import org.bibsonomy.scraper.exceptions.ScrapingException;
 import org.bibsonomy.scraper.exceptions.ScrapingFailureException;
+import org.bibsonomy.util.ValidationUtils;
 import org.bibsonomy.util.WebUtils;
-import org.bibsonomy.util.XmlUtils;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
 /**
  * Scraper for INSPIRE. The upgrade of SPIRES.
@@ -56,68 +53,87 @@ import org.w3c.dom.NodeList;
  */
 public class InspireScraper extends AbstractUrlScraper implements ReferencesScraper {
 	private static final Log log = LogFactory.getLog(InspireScraper.class);
-	
+
 	private static final String SITE_NAME = "INSPIRE";
 	private static final String SITE_URL = "http://inspirehep.net/";
+
+	private static final Pattern URL_ID_PATTERN = Pattern.compile("/record/([0-9]+)");
+	private static final Pattern URL_DOWNLOAD_PATTERN = Pattern.compile("/export/hx");
+	private static final String URL_FORMAT_WWWBRIEFBIBTEX = "FORMAT=WWWBRIEFBIBTEX";
+
+	private static final Pattern BIBTEX_DIV_PATTERN = Pattern.compile("<div class=\"pagebodystripemiddle\"><pre>(.+)</pre>", Pattern.DOTALL);
+	private static final Pattern BRIEFBIBTEX_PATTERN = Pattern.compile("<a href=\"?(/spires/find/hep/www\\?.*?\\&FORMAT=WWWBRIEFBIBTEX)\"?>");
+	private static final Pattern BIBTEX_PATTERN = Pattern.compile("<a href=\"(.*?)\".*?>BibTeX</a>");
 	
-	private static final Pattern pattern_id = Pattern.compile("/record/([0-9]+)");
-	private static final Pattern pattern_download = Pattern.compile("/export/hx");
+	private static final String INFO = "Gets publications from " + href(SITE_URL, SITE_NAME)+".";
 
-	private static final String info = "Gets publications from " + href(SITE_URL, SITE_NAME)+".";
-
-	private static final List<Pair<Pattern, Pattern>> patterns = new LinkedList<Pair<Pattern,Pattern>>();
+	private static final List<Pair<Pattern, Pattern>> PATTERNS = new LinkedList<Pair<Pattern,Pattern>>();
 	static {
-		patterns.add(new Pair<Pattern, Pattern>(Pattern.compile(".*" + "inspirehep.net"), AbstractUrlScraper.EMPTY_PATTERN));
+		PATTERNS.add(new Pair<Pattern, Pattern>(Pattern.compile(".*" + "inspirehep.net"), AbstractUrlScraper.EMPTY_PATTERN));
 	}
-	private static final Pattern pattern_abstract = Pattern.compile("(?i).*Abstract(.*)<span>(.*)</span>");
-	private static final Pattern references = Pattern.compile("(?s)<div id=\'referenceinp_link_box\'>(.*)<div id='referenceinp_link_box'>");
-	
+	private static final Pattern HTML_ABSTRACT_PATTERN = Pattern.compile("(?i).*Abstract(.*)<span>(.*)</span>");
+	private static final Pattern HTML_REFERENCES_PATTERN = Pattern.compile("(?s)<div id=\'referenceinp_link_box\'>(.*)<div id='referenceinp_link_box'>");
+
 	@Override
 	protected boolean scrapeInternal(ScrapingContext sc) throws ScrapingException {
 		sc.setScraper(this);
-		
+
 		try {
-			final String url = sc.getUrl().toString();
-			
-			final Matcher idMatcher = pattern_id.matcher(url);
-			
-			if(idMatcher.find()) {
-				URL bibtexUrl = new URL(SITE_URL + "record/" + idMatcher.group(1) + pattern_download);
-				
-				final Document temp = XmlUtils.getDOM(bibtexUrl);
-				
-				//extract the bibtex snippet which is embedded in pre tags
-				String bibtex = null;
-				final NodeList nl = temp.getElementsByTagName("pre"); //get the pre tags (normally one)
-				for (int i = 0; i < nl.getLength(); i++) {
-					Node currNode = nl.item(i);
-					if (currNode.hasChildNodes()){
-						bibtex = currNode.getChildNodes().item(0).getNodeValue();	
+			final URL url = sc.getUrl();
+			final URL bibtexUrl;
+
+			// test the different options to get the BibTeX export URL from URL and HTML content
+			if (ValidationUtils.present(url.getQuery()) && url.getQuery().contains(URL_FORMAT_WWWBRIEFBIBTEX)) { 
+				bibtexUrl = url;
+			} else {
+				final Matcher idMatcher = URL_ID_PATTERN.matcher(url.toExternalForm());
+				if (idMatcher.find()) {
+					bibtexUrl = new URL(SITE_URL + "record/" + idMatcher.group(1) + URL_DOWNLOAD_PATTERN);
+				} else {
+					//we are looking for some pattern in the source of the page
+					final Matcher m = BRIEFBIBTEX_PATTERN.matcher(sc.getPageContent());
+					//if we do not find, we maybe find a link :-)
+					if (!m.find()) {
+						final Matcher m2 = BIBTEX_PATTERN.matcher(sc.getPageContent());
+						if (!m2.find()) throw new ScrapingFailureException("no download link found");
+						bibtexUrl = new URL(url, m2.group(1));
+					} else {
+						bibtexUrl = new URL(url.getProtocol() + "://" + url.getHost() + m.group(1));
 					}
 				}
-				
-				/*
-				 * add URL
-				 */
-				bibtex = BibTexUtils.addFieldIfNotContained(bibtex, "url", url);
-				bibtex = BibTexUtils.addFieldIfNotContained(bibtex, "abstract", abstractParser(sc.getUrl()));
-				
-				//-- bibtex string may not be empty
-				if (bibtex != null && ! "".equals(bibtex)) {
-					sc.setBibtexResult(bibtex);
-					return true;
-				}
+			}
+
+			String bibtex = getBibTeX(bibtexUrl);
+
+			/*
+			 * add URL
+			 */
+			bibtex = BibTexUtils.addFieldIfNotContained(bibtex, "url", url.toExternalForm());
+			bibtex = BibTexUtils.addFieldIfNotContained(bibtex, "abstract", abstractParser(url));
+
+			//-- bibtex string may not be empty
+			if (bibtex != null && ! "".equals(bibtex)) {
+				sc.setBibtexResult(bibtex);
+				return true;
 			}
 			throw new ScrapingFailureException("getting bibtex failed");
-			
+
 		} catch (Exception e) {
 			throw new InternalFailureException(e);
 		}
 	}
-	
+
+	private static String getBibTeX(final URL bibtexUrl) throws IOException {
+		final String html = WebUtils.getContentAsString(bibtexUrl.toExternalForm());
+		final Matcher m = BIBTEX_DIV_PATTERN.matcher(html);
+		if (m.find()) {
+			return m.group(1);
+		}
+		return null;
+	}
 	private static String abstractParser(URL url){
 		try {
-			Matcher m = pattern_abstract.matcher(WebUtils.getContentAsString(url));
+			final Matcher m = HTML_ABSTRACT_PATTERN.matcher(WebUtils.getContentAsString(url));
 			if(m.find())
 				return m.group(2);
 		} catch(Exception e) {
@@ -127,12 +143,12 @@ public class InspireScraper extends AbstractUrlScraper implements ReferencesScra
 	}
 	@Override
 	public String getInfo() {
-		return info;
+		return INFO;
 	}
-	
+
 	@Override
 	public List<Pair<Pattern, Pattern>> getUrlPatterns() {
-		return patterns;
+		return PATTERNS;
 	}
 
 	@Override
@@ -151,7 +167,7 @@ public class InspireScraper extends AbstractUrlScraper implements ReferencesScra
 	@Override
 	public boolean scrapeReferences(ScrapingContext sc) throws ScrapingException {
 		try{
-			final Matcher m = references.matcher(WebUtils.getContentAsString(sc.getUrl().toString() + "/references"));
+			final Matcher m = HTML_REFERENCES_PATTERN.matcher(WebUtils.getContentAsString(sc.getUrl().toString() + "/references"));
 			if(m.find()) {
 				sc.setReferences(m.group(1));
 				return true;
