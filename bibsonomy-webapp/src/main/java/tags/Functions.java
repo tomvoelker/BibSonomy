ba255a0e68052e2a85f1b63152d42969212387b0
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2018-08-09 17:58:33 +0200

    added todos

--- bibsonomy-webapp/src/main/java/tags/Functions.java
+++ bibsonomy-webapp/src/main/java/tags/Functions.java
@@ -1066,6 +1066,7 @@ public class Functions {
 	 * @param orcid
 	 * @return
 	 */
+	@Deprecated // remove; useless; can be done in a normal tagx file
 	public static String beatifyOrcid(String orcid) {
 		if (orcid.length() == 16) {
 			return orcid.substring(0, 4) + "-" + orcid.substring(4, 8) + "-" + orcid.substring(8, 12) + "-" + orcid.substring(12, 16);
@@ -1078,6 +1079,7 @@ public class Functions {
 	 * @param orcid
 	 * @return
 	 */
+	@Deprecated // remove; useless; can be done in a normal tagx file
 	public static String callOrcidURL(String orcid) {
 		if (orcid.length() == 16) {
 			return "https://orcid.org/" + orcid.substring(0, 4) + "-" + orcid.substring(4, 8) + "-" + orcid.substring(8, 12) + "-" + orcid.substring(12, 16);
@@ -1091,6 +1093,7 @@ public class Functions {
 	 * @param researcherid
 	 * @return
 	 */
+	@Deprecated // remove; useless; can be done in a normal tagx file
 	public static String beatifyResearcherid(String researcherid) {
 		if (researcherid.length() == 9) {
 			return researcherid.substring(0, 1) + "-" + researcherid.substring(2, 5) + "-" + researcherid.substring(5, 9);
@@ -1103,6 +1106,7 @@ public class Functions {
 	 * @param researcherid
 	 * @return
 	 */
+	@Deprecated // remove; useless; can be done in a normal tagx file
 	public static String callResearcheridURL(String researcherid) {
 		if (researcherid.length() == 9) {
 			return "https://orcid.org/eid/" + researcherid.substring(0, 1) + "-" + researcherid.substring(2, 5) + "-" + researcherid.substring(5, 9);
