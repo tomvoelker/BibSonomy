43a381867f894d0e109fc74eb7bd188e212890f9
Tobias Koopmann <tobias-koopmann@t-online.de>
2018-10-01 15:47:02 +0200

    changed hidden divs to data attribute for simplePubView

--- bibsonomy-webapp/src/main/webapp/resources/javascript/person/deletePublicationModal.js
+++ bibsonomy-webapp/src/main/webapp/resources/javascript/person/deletePublicationModal.js
@@ -1,20 +1,15 @@
 $(document).ready(function() {
     $('.delete-button').click(function (e) {
-        var parent = $(e.target).parent();
-        var interhash, index, type;
-        // Check if clicked the button or the trash item
-        if (parent.is('span')) {
-            interhash = parent.find('.interhash');
-            index = parent.find('.index');
-            type = parent.find('.type');
-        } else {
-            interhash = parent.parent().find('.interhash');
-            index = parent.parent().find('.index');
-            type = parent.parent().find('.type');
+        var ele = $(e.target);
+        if (ele.is('span')) {
+            ele = ele.parent();
         }
-        $('#interhashToDelete').val(interhash.text());
-        $('#indexToDelete').val(index.text());
-        $('#typeToDelete').val(type.text());
+        var interhash = ele.data('interhash');
+        var index = ele.data('personindex');
+        var type = ele.data('relationtype');
+        $('#interhashToDelete').val(interhash);
+        $('#indexToDelete').val(index);
+        $('#typeToDelete').val(type);
         $("#unlinkPublication").modal("toggle");
     });
 });
