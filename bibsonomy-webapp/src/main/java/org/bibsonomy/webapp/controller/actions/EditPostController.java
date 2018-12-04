105e4f22b1d6695b4437887a1f110dbf4e5c70a3
Tobias Koopmann <tobias-koopmann@t-online.de>
2018-12-04 12:29:40 +0100

    autolink view works, needs checking

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/actions/EditPostController.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/actions/EditPostController.java
@@ -41,7 +41,6 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.bibsonomy.common.JobResult;
 import org.bibsonomy.common.enums.ConceptStatus;
-import org.bibsonomy.common.enums.Filter;
 import org.bibsonomy.common.enums.FilterEntity;
 import org.bibsonomy.common.enums.GroupRole;
 import org.bibsonomy.common.enums.GroupingEntity;
@@ -772,6 +771,7 @@ public abstract class EditPostController<RESOURCE extends Resource, COMMAND exte
 
 	private View handleCreatePost(final COMMAND command, final RequestWrapperContext context, final User loginUser, final Post<RESOURCE> post) {
 		final String loginUserName = loginUser.getName();
+		command.setUser(loginUserName);
 
 		/*
 		 * no intra hash given --> user posts a new entry (which might already
@@ -835,6 +835,11 @@ public abstract class EditPostController<RESOURCE extends Resource, COMMAND exte
 			log.debug("finally: creating a new post in the DB");
 			final List<JobResult> results = this.logic.createPosts(Collections.singletonList(post));
 			command.setJobResults(results);
+			final List<Post<RESOURCE>> posts = new LinkedList<>();
+			for (JobResult j : results) {
+				posts.add(this.getPostDetails(j.getId(), loginUserName));
+			}
+			command.setAbstractPosts(posts);
 			final String createdPost = results.get(0).getId();
 			/*
 			 * store intraHash for some later changes (file upload)
