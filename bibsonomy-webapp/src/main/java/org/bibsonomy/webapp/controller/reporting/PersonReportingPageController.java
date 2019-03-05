e21a9f177f09a63bd89c8cddcb1a9251e4307d1b
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2019-03-05 21:32:28 +0100

    fixed person reporting page

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/reporting/PersonReportingPageController.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/reporting/PersonReportingPageController.java
@@ -1,11 +1,10 @@
 package org.bibsonomy.webapp.controller.reporting;
 
-import static org.bibsonomy.util.ValidationUtils.present;
-
-import org.bibsonomy.model.logic.LogicInterface;
+import org.bibsonomy.model.Group;
+import org.bibsonomy.model.Person;
 import org.bibsonomy.model.logic.query.PersonQuery;
+import org.bibsonomy.webapp.command.ListCommand;
 import org.bibsonomy.webapp.command.reporting.PersonReportingCommand;
-import org.bibsonomy.webapp.util.MinimalisticController;
 import org.bibsonomy.webapp.util.View;
 import org.bibsonomy.webapp.view.Views;
 
@@ -17,34 +16,36 @@ import org.bibsonomy.webapp.view.Views;
  * @author pda
  * @author dzo
  */
-public class PersonReportingPageController implements MinimalisticController<PersonReportingCommand> {
+public class PersonReportingPageController extends AbstractReportingPageController<PersonReportingCommand> {
 
-	private LogicInterface logic;
-
-	/**
-	 * @param logic the logic to set
-	 */
-	public void setLogic(final LogicInterface logic) {
-		this.logic = logic;
-	}
+	private String college;
 
 	@Override
-	public PersonReportingCommand instantiateCommand() {
+	protected PersonReportingCommand instantiateReportingCommand() {
 		return new PersonReportingCommand();
 	}
 
 	@Override
-	public View workOn(PersonReportingCommand command) {
+	protected void workOn(PersonReportingCommand command, Person person, Group organization) {
 		PersonQuery query = new PersonQuery(command.getQuery());
-		query.setOrganization(command.getOrganization());
-		query.setStart(command.getPersons().getStart());
-		query.setEnd(command.getPersons().getStart() + command.getPersons().getEntriesPerPage());
-		command.getPersons().setList(this.logic.getPersons(query));
+		query.setOrganization(organization);
+		final ListCommand<Person> personListCommand = command.getPersons();
+		final int start = personListCommand.getStart();
+		query.setStart(start);
+		query.setCollege(this.college);
+		query.setEnd(start + personListCommand.getEntriesPerPage());
+		personListCommand.setList(this.logic.getPersons(query));
+	}
 
-		if (present(command.getDownloadFormat())) {
-			return Views.REPORTING_DOWNLOAD;
-		}
-
+	@Override
+	protected View reportingView() {
 		return Views.PERSONS_REPORTING;
+	}
+
+	/**
+	 * @param college the college to set
+	 */
+	public void setCollege(String college) {
+		this.college = college;
 	}
 }
