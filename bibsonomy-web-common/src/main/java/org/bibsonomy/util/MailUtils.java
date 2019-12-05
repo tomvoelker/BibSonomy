a7465dc40ca4a165b0fbc3f0204cdff3c63e08f7
Mario Holtmüller <m.holtmueller@hothodata.de>
2019-12-05 08:07:33 +0100

    removed unused import (cleanup)

--- bibsonomy-web-common/src/main/java/org/bibsonomy/util/MailUtils.java
+++ bibsonomy-web-common/src/main/java/org/bibsonomy/util/MailUtils.java
@@ -38,7 +38,6 @@ import javax.mail.Transport;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import javax.print.attribute.standard.PresentationDirection;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
