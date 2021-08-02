f6f78b80c92e6162335884177b30f5b7e910455b
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2021-08-02 11:19:01 +0200

    fixing delete project controller

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/actions/DeleteProjectController.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/actions/DeleteProjectController.java
@@ -2,9 +2,8 @@ package org.bibsonomy.webapp.controller.actions;
 
 import static org.bibsonomy.util.ValidationUtils.present;
 
+import org.bibsonomy.common.enums.Role;
 import org.bibsonomy.common.exceptions.AccessDeniedException;
-import org.bibsonomy.model.User;
-import org.bibsonomy.model.cris.CRISLink;
 import org.bibsonomy.model.cris.Project;
 import org.bibsonomy.model.logic.LogicInterface;
 import org.bibsonomy.services.URLGenerator;
@@ -17,7 +16,9 @@ import org.bibsonomy.webapp.view.ExtendedRedirectView;
 /**
  * the controler deletes projects
  *
- * handles requests form TODO
+ * request paths:
+ *
+ * - /deleteProject
  *
  * @author tko
  */
@@ -35,45 +36,34 @@ public class DeleteProjectController implements MinimalisticController<DeletePro
 		final RequestWrapperContext context = command.getContext();
 		final String requestedProjectId = command.getProjectIdToDelete();
 
-		Project project = this.logic.getProjectDetails(requestedProjectId);
+		final Project project = this.logic.getProjectDetails(requestedProjectId);
 		if (!present(project)) {
 			return new ExtendedRedirectView(this.urlGenerator.getProjectsUrl());
 		}
 
-		// --- Check if allowed ---
-		boolean isMember = false;
-		for (CRISLink link : project.getCrisLinks()) {
-			User user;
-			try {
-				user = (User) link.getTarget();
-			} catch (Exception e) {
-				user = null;
-			}
-			// fixme cris links return person, not user
-			if (present(user) && user.equals(command.getContext().getLoginUser())) {
-				isMember = true;
-			}
-		}
-		if (!(context.getLoginUser().getRole().getRole() == 0) || isMember) {
+		// check if the project can be deleted
+		// FIXME: add check that the user is manager of the project
+		if (!(context.getLoginUser().getRole() == Role.ADMIN)) {
 			throw new AccessDeniedException("not allowed");
 		}
 
-		// --- delete and redirect ---
-		logic.deleteProject(requestedProjectId);
+		// delete the project and redirect
+		this.logic.deleteProject(requestedProjectId);
+		// TODO: add success message
 		return new ExtendedRedirectView(this.urlGenerator.getProjectsUrl());
 	}
 
 	/**
-	 * @param urlGenerator
+	 * @param urlGenerator the urlGenerator to set
 	 */
-	public void setUrlGenerator(URLGenerator urlGenerator) {
+	public void setUrlGenerator(final URLGenerator urlGenerator) {
 		this.urlGenerator = urlGenerator;
 	}
 
 	/**
-	 * @param logic
+	 * @param logic the logic to set
 	 */
-	public void setLogic(LogicInterface logic) {
+	public void setLogic(final LogicInterface logic) {
 		this.logic = logic;
 	}
 }
