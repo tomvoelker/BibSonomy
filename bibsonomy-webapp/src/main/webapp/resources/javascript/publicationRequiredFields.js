8d697f0e2a3119840640a060be7f95e1e82e4022
Tom Völker <tom.voelker@stud-mail.uni-wuerzburg.de>
2021-09-08 12:55:57 +0200

    moved script to publicationRequiredFields.js

--- bibsonomy-webapp/src/main/webapp/resources/javascript/publicationRequiredFields.js
+++ bibsonomy-webapp/src/main/webapp/resources/javascript/publicationRequiredFields.js
@@ -1,5 +1,18 @@
 $(function(){
 	myownTagInit($('#myownChkBox'), $('#inpf_tags'));
+
+	document.getElementById("post.resource.author").onblur = function() {
+		if(isAuthor() && !document.getElementById("myownChkBox").checked){
+			document.getElementById("imAmAuthorWarning").style.display = "block"
+		} else {
+			document.getElementById("imAmAuthorWarning").style.display = "none"
+		}
+	}
+
+	document.getElementById("myownChkBox").onclick = function() {
+		document.getElementById("imAmAuthorWarning").style.display = "none"
+	}
+
 });
 
 function highlightMatches(text, input) {
@@ -47,4 +60,30 @@ function myownTagInit(chkbox, tagbox) {
 					tagbox.val(tagbox.val().replace(expr, ' ').replace(/^[ ]?/, ''));
 				}
 			}).parent().removeClass('hiddenElement');
+}
+
+/**
+ * Determines whether the currently logged in user has entered a form of their own name in the authors field
+ * @returns {boolean}
+ */
+function isAuthor() {
+	let allPossibleNames = [];
+	if(document.getElementById("claimedPersonMainNameID") == null){ //uses the User.realName as a fallback if no Person was claimed by the user
+		let individualUserRealName = document.getElementById("userRealnameID").value
+		allPossibleNames.push(individualUserRealName)
+	} else { //uses all names saved in the person claimed by the current user
+		let userClaimedPersonMainName = document.getElementById("claimedPersonMainNameID").value;
+		allPossibleNames.push(userClaimedPersonMainName)
+		let userClaimedPersonNames = document.getElementById("claimedPersonNamesID").value;
+		allPossibleNames = allPossibleNames.concat(userClaimedPersonNames.split(" and "));
+	}
+
+	let enteredAuthors = document.getElementById("post.resource.author").value;
+	let individualEnteredAuthors = enteredAuthors.split("\n");
+
+	if (enteredAuthors.length !== 0){
+		return individualEnteredAuthors.some(enteredAuthor => allPossibleNames.some(userRealName => enteredAuthor === userRealName));
+	} else{ //Fallback if the input was left empty or the input was deleted
+		return false;
+	}
 }
