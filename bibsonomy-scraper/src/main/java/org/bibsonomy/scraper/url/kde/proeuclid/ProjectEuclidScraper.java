8cae7064a52a2445e69a119339e2b42e4306cdfa
Til Barthel <til.barthel@stud-mail.uni-wuerzburg.de>
2021-10-25 00:11:47 +0200

    fixed ProjectEuclidScraper and tests

--- bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/proeuclid/ProjectEuclidScraper.java
+++ bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/proeuclid/ProjectEuclidScraper.java
@@ -29,21 +29,33 @@
  */
 package org.bibsonomy.scraper.url.kde.proeuclid;
 
+import static org.bibsonomy.util.ValidationUtils.present;
+
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.http.HttpException;
 import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.message.BasicNameValuePair;
 import org.bibsonomy.common.Pair;
 import org.bibsonomy.scraper.AbstractUrlScraper;
 import org.bibsonomy.scraper.ScrapingContext;
 import org.bibsonomy.scraper.exceptions.ScrapingException;
+import org.bibsonomy.scraper.exceptions.ScrapingFailureException;
+import org.bibsonomy.scraper.generic.GenericBibTeXURLScraper;
 import org.bibsonomy.util.UrlUtils;
 import org.bibsonomy.util.WebUtils;
+import org.bibsonomy.util.id.DOIUtils;
 
 /**
  * scraper for the Astronomy and Astrophysics
@@ -56,27 +68,54 @@ public class ProjectEuclidScraper extends AbstractUrlScraper {
 	private static final String SITE_HOST = "projecteuclid.org";
 	private static final String SITE_URL = "https://" + SITE_HOST + "/";
 	private static final String INFO = "Scraper for references from " + href(SITE_URL, SITE_NAME)+".";
-	private static final String DOWNLOAD_URL = SITE_URL + "export_citations";
-	private static final Pattern ID_PATTERN = Pattern.compile("/(.+)$");
 	private static final List<Pair<Pattern, Pattern>> PATTERNS = Collections.singletonList(new Pair<>(Pattern.compile(".*"+ SITE_HOST), AbstractUrlScraper.EMPTY_PATTERN));
-	
+
+	private static final String DOWNLOAD_URL = "https://projecteuclid.org/citation/download";
+
+	private static final Pattern URL_ID_PATTERN = Pattern.compile("/([A-z]*/\\d*)\\.full");
+
+
+
 	@Override
 	protected boolean scrapeInternal(ScrapingContext scrapingContext) throws ScrapingException {
-		final Matcher m = ID_PATTERN.matcher(scrapingContext.getUrl().getPath());
-		if (m.find()) {
-			try {
-				final String id = UrlUtils.safeURIEncode(m.group(1));
-				
-				final List<NameValuePair> postData = new ArrayList<>(2);
-				postData.add(new BasicNameValuePair("h", id));
-				postData.add(new BasicNameValuePair("format", "bibtex"));
-				scrapingContext.setBibtexResult(WebUtils.getContentAsString(DOWNLOAD_URL, null, postData, null));
-			} catch (final IOException e) {
-				throw new ScrapingException(e);
+		scrapingContext.setScraper(this);
+		try {
+
+			URL url = WebUtils.getRedirectUrl(scrapingContext.getUrl());
+			if (!present(url)){
+				url = scrapingContext.getUrl();
 			}
+			url = new URL(url.getProtocol() + "://" + url.getHost() + url.getPath());
+
+
+			String id = DOIUtils.getDoiFromURL(url);
+			if (!present(id)){
+				Matcher m_id = URL_ID_PATTERN.matcher(url.toExternalForm());
+				if (m_id.find())id = m_id.group(1);
+			}else {
+				id = id.replaceAll("\\.full|\\.short", "");
+			}
+
+			HttpClient client = WebUtils.getHttpClient();
+			HttpPost post = new HttpPost(DOWNLOAD_URL);
+			post.setHeader("Content-Type", "application/json; charset=UTF-8");
+			post.setEntity(new StringEntity("{\"contentType\":\"0\",\"formatType\":\"2\",\"referenceType\":\"\",\"urlid\":\"" + id + "\"}"));
+			String urlId = WebUtils.getContentAsString(client, post).replaceAll("\n", "");
+
+			String fullDownloadUrl = DOWNLOAD_URL + "/" + URLEncoder.encode(urlId, "UTF-8");
+
+			String bibtex = WebUtils.getContentAsString(fullDownloadUrl);
+			if (present(bibtex)){
+				scrapingContext.setBibtexResult(bibtex);
+				return true;
+			}
+
+		} catch (HttpException | IOException e) {
+			throw new ScrapingFailureException(e);
 		}
 		return false;
 	}
+
 	
 	@Override
 	public String getSupportedSiteName() {
@@ -97,4 +136,5 @@ public class ProjectEuclidScraper extends AbstractUrlScraper {
 	public List<Pair<Pattern, Pattern>> getUrlPatterns() {
 		return PATTERNS;
 	}
+
 }
