b9f3b68187d2fc6fe45beb6fa0ef5935fbc7b07d
Til Barthel <til.barthel@stud-mail.uni-wuerzburg.de>
2021-10-25 09:19:39 +0200

    fixed formatting

--- bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/oapen/OapenScraper.java
+++ bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/oapen/OapenScraper.java
@@ -1,29 +1,29 @@
 /**
  * BibSonomy-Scraper - Web page scrapers returning BibTeX for BibSonomy.
- * <p>
+ *
  * Copyright (C) 2006 - 2021 Data Science Chair,
- * University of Würzburg, Germany
- * https://www.informatik.uni-wuerzburg.de/datascience/home/
- * Information Processing and Analytics Group,
- * Humboldt-Universität zu Berlin, Germany
- * https://www.ibi.hu-berlin.de/en/research/Information-processing/
- * Knowledge & Data Engineering Group,
- * University of Kassel, Germany
- * https://www.kde.cs.uni-kassel.de/
- * L3S Research Center,
- * Leibniz University Hannover, Germany
- * https://www.l3s.de/
- * <p>
+ *                               University of Würzburg, Germany
+ *                               https://www.informatik.uni-wuerzburg.de/datascience/home/
+ *                           Information Processing and Analytics Group,
+ *                               Humboldt-Universität zu Berlin, Germany
+ *                               https://www.ibi.hu-berlin.de/en/research/Information-processing/
+ *                           Knowledge & Data Engineering Group,
+ *                               University of Kassel, Germany
+ *                               https://www.kde.cs.uni-kassel.de/
+ *                           L3S Research Center,
+ *                               Leibniz University Hannover, Germany
+ *                               https://www.l3s.de/
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * <p>
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * <p>
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -52,7 +52,7 @@ public class OapenScraper extends GenericRISURLScraper {
 	private static final List<Pair<Pattern, Pattern>> PATTERNS = new LinkedList<>();
 
 	static {
-		PATTERNS.add(new Pair<Pattern, Pattern>(Pattern.compile(".*library.oapen.org"), Pattern.compile("/handle.*")));
+		PATTERNS.add(new Pair<Pattern, Pattern>(Pattern.compile(".*library.oapen.org"), Pattern.compile("handle.*")));
 	}
 
 	private static final Pattern EXPORT_CITATION = Pattern.compile("<a class=\"btn btn-default\" href=\"(.*?)\">Export citation</a>");
