c985b70829bfe21546f3641f281498c7bfd2da37
Til Barthel <til.barthel@stud-mail.uni-wuerzburg.de>
2021-11-11 15:38:45 +0100

    fixed AmsScraper and added more tests

--- bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/ams/AmsScraper.java
+++ bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/ams/AmsScraper.java
@@ -29,39 +29,83 @@
  */
 package org.bibsonomy.scraper.url.kde.ams;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
+import static org.bibsonomy.util.ValidationUtils.present;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.http.HttpException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.bibsonomy.common.Pair;
 import org.bibsonomy.scraper.AbstractUrlScraper;
 import org.bibsonomy.scraper.CitedbyScraper;
 import org.bibsonomy.scraper.ReferencesScraper;
 import org.bibsonomy.scraper.ScrapingContext;
 import org.bibsonomy.scraper.exceptions.ScrapingException;
-import org.bibsonomy.scraper.generic.LiteratumScraper;
+import org.bibsonomy.scraper.exceptions.ScrapingFailureException;
 import org.bibsonomy.util.WebUtils;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Scraper for ams.allenpress.com
  * @author tst
  */
-public class AmsScraper extends LiteratumScraper implements CitedbyScraper, ReferencesScraper {
+//TODO: create a Generic Scraper for AmsScraper and AKJournalsScraper
+public class AmsScraper extends AbstractUrlScraper implements CitedbyScraper, ReferencesScraper {
 	private static final Log log = LogFactory.getLog(AmsScraper.class);
 	
 	private static final String SITE_NAME = "American Meteorological Society";
 	private static final String SITE_HOST = "journals.ametsoc.org";
-	private static final String SITE_URL  = "http://" + SITE_HOST + "/";
+	private static final String SITE_URL  = "https://" + SITE_HOST + "/";
 	private static final String SITE_INFO = "For references from the "+href(SITE_URL, SITE_NAME)+".";
 	private static final List<Pair<Pattern, Pattern>> PATTERNS = Collections.singletonList(new Pair<Pattern, Pattern>(Pattern.compile(".*" + SITE_HOST), AbstractUrlScraper.EMPTY_PATTERN));
 	
 
 	private static final Pattern CITEDBY = Pattern.compile("(?s)<a name=\"citedBySection\"></a><h2>Cited by</h2>(.*)<!-- /fulltext content --></div>");
 	private static final Pattern REFERENCERS = Pattern.compile("(?s)<table border=\"0\" class=\"references\">(.*)</table><!-- /fulltext content --></div>");
-	
+
+	private static final String DOWNLOAD_URL = "https://journals.ametsoc.org/rest/citation/export";
+	private static final Pattern JSON_DOCUMENT_URI_PATTERN = Pattern.compile("(/journals.*)");
+
+	@Override
+	protected boolean scrapeInternal(ScrapingContext scrapingContext) throws ScrapingException {
+		scrapingContext.setScraper(this);
+
+		try {
+			URL url = WebUtils.getRedirectUrl(scrapingContext.getUrl());
+			if (!present(url)){
+				url = scrapingContext.getUrl();
+			}
+			// documentUri ist a part of the path of the url and is needed for the json
+			String documentUri;
+			Matcher m_documentUri = JSON_DOCUMENT_URI_PATTERN.matcher(url.getPath());
+			if (m_documentUri.find()){
+				documentUri = m_documentUri.group(1);
+			}else {
+				throw new ScrapingException("can't find documentUri in " + url.getPath());
+			}
+			// creating a post-request with the json as body. post returns the bibtex
+			HttpPost post = new HttpPost(DOWNLOAD_URL);
+			post.setHeader("Content-Type", "application/json");
+			String jsonForPost = "{\"format\":\"bibtex\",\"citationExports\":[{\"documentUri\":\""+ documentUri + "\",\"citationId\":null}]}";
+			post.setEntity(new StringEntity(jsonForPost));
+			String bibtex = WebUtils.getContentAsString(WebUtils.getHttpClient(), post);
+
+			if (!present(bibtex)){
+				throw new ScrapingException("can't get bibtex from " + DOWNLOAD_URL);
+			}
+			scrapingContext.setBibtexResult(bibtex);
+			return true;
+		} catch (final IOException | HttpException e) {
+			throw new ScrapingFailureException(e);
+		}
+	}
+
 	@Override
 	public String getInfo() {
 		return SITE_INFO;
