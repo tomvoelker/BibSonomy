9dbb3f4ceae144bb7ec5d28b3f5c1427279d8b4b
Til Barthel <til.barthel@stud-mail.uni-wuerzburg.de>
2021-11-11 16:41:47 +0100

    fixed BioOneScraper and tests

--- bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/bioone/BioOneScraper.java
+++ bibsonomy-scraper/src/main/java/org/bibsonomy/scraper/url/kde/bioone/BioOneScraper.java
@@ -29,25 +29,40 @@
  */
 package org.bibsonomy.scraper.url.kde.bioone;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.regex.Pattern;
-
+import static org.bibsonomy.util.ValidationUtils.present;
+import org.apache.http.HttpException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.bibsonomy.common.Pair;
 import org.bibsonomy.scraper.AbstractUrlScraper;
-import org.bibsonomy.scraper.generic.LiteratumScraper;
+import org.bibsonomy.scraper.ScrapingContext;
+import org.bibsonomy.scraper.exceptions.ScrapingException;
+import org.bibsonomy.util.WebUtils;
+import org.bibsonomy.util.id.DOIUtils;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author Mohammed Abed
  */
-public class BioOneScraper extends LiteratumScraper {
+//TODO: generic Scraper for ProjectEuclidScraper and BioOne
+public class BioOneScraper extends AbstractUrlScraper {
 
 	private static final String SITE_NAME = "Bio One Research Evolved";
 	private static final String SITE_HOST = "bioone.org";
-	private static final String SITE_URL  = "http://" + SITE_HOST + "/";
+	private static final String SITE_URL  = "https://" + SITE_HOST + "/";
 	private static final String SITE_INFO = "This scraper parses a publication page of citations from " + href(SITE_URL, SITE_NAME) + ".";
 	private static final List<Pair<Pattern, Pattern>> PATTERNS = Collections.singletonList(new Pair<Pattern, Pattern>(Pattern.compile(".*"+ SITE_HOST), AbstractUrlScraper.EMPTY_PATTERN));
 
+	private static final String DOWNLOAD_URL = "https://bioone.org/citation/download";
+
+	private static final Pattern URL_ID_PATTERN = Pattern.compile("/([A-z]*/\\d*)\\.full");
 	@Override
 	public String getSupportedSiteName() {
 		return SITE_NAME;
@@ -66,5 +81,51 @@ public class BioOneScraper extends LiteratumScraper {
 	@Override
 	public List<Pair<Pattern, Pattern>> getUrlPatterns() {
 		return PATTERNS;
+	}
+
+	@Override
+	protected boolean scrapeInternal(ScrapingContext scrapingContext) throws ScrapingException {
+		scrapingContext.setScraper(this);
+
+		try {
+			URL url = WebUtils.getRedirectUrl(scrapingContext.getUrl());
+			if (!present(url)){
+				url = scrapingContext.getUrl();
+			}
+			url = new URL(url.getProtocol() + "://" + url.getHost() + url.getPath());
+
+			String id = DOIUtils.getDoiFromURL(url);
+			if (!present(id)){
+				Matcher m_id = URL_ID_PATTERN.matcher(url.toExternalForm());
+				if (m_id.find())id = m_id.group(1);
+			}else {
+				id = id.replaceAll("\\.full|\\.short", "");
+			}
+			if (!present(id)){
+				throw new ScrapingException("id of " + url + " was not found");
+			}
+
+			HttpPost post = new HttpPost(DOWNLOAD_URL);
+			post.setHeader("Content-Type", "application/json; charset=UTF-8");
+			StringEntity postBody = new StringEntity("{\"contentType\":\"1\",\"formatType\":\"2\",\"referenceType\":\"\",\"urlid\":\"" + id + "\"}");
+			post.setEntity(postBody);
+			String urlId = WebUtils.getContentAsString(WebUtils.getHttpClient(), post);
+			if (!present(urlId)){
+				throw new ScrapingException("Post to " + DOWNLOAD_URL + " with body " + postBody + "did not return urlId");
+			}else {
+				urlId = urlId.replaceAll("\n", "");
+			}
+
+			String fullDownloadUrl = DOWNLOAD_URL + "/" + URLEncoder.encode(urlId, "UTF-8");
+			String bibtex = WebUtils.getContentAsString(fullDownloadUrl);
+			if (!present(bibtex)){
+				throw new ScrapingException("bibtex was not returned from " + fullDownloadUrl);
+			}
+
+			scrapingContext.setBibtexResult(bibtex);
+			return true;
+		} catch (HttpException | IOException e) {
+			throw new ScrapingException(e);
+		}
 	}
 }
