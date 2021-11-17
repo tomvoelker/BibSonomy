f0990b12073607fa57ffeee31bc627d3bdcf4a44
Mario Holtmüller <m.holtmueller@hothodata.de>
2021-11-17 17:52:00 +0100

    included fav layouts in quick cite

--- bibsonomy-webapp/src/main/webapp/resources/javascript/quickcite.js
+++ bibsonomy-webapp/src/main/webapp/resources/javascript/quickcite.js
@@ -32,7 +32,6 @@ function openSelect2(element) {
     }
 }
 
-
 function openModalWithBibTex() {
     $("#sidebar-quick-cite-box-modal .modal-body").html($("#sidebar-quick-cite-box-bibtex").html());
     $("#sidebar-quick-cite-box-modal").modal("show");
@@ -43,59 +42,25 @@ function openModalWithEndnote() {
     $("#sidebar-quick-cite-box-modal").modal("show");
 }
 
-// /layout/apa_html/bibtex/27befbc559cad029f1c2ac2534c0adc09/tester01?formatEmbedded=true&items=1
-
-http://puma.dd/layout/apa_html/bibtex/27befbc559cad029f1c2ac2534c0adc09/tester01?formatEmbedded=true&items=1
-
-/*
-function openModalWithStyle(intrahash, style) {
-    csl_url = "/csl/bibtex/" + csl_style;
-    csl_url = "/layout/" + style + "/bibtex/" + intrahash + "?formatEmbedded=true&items=1";
+function ajaxLoadLayout(url) {
+	url_parts = url.split("/");
     container = $("#sidebar-quick-cite-box-modal .modal-body");
     container.empty();
 
-    $.ajax({
-        url: csl_url,
-        success: function(data) {
-            renderCSL(data, csl_style, container, false);
-            $("#sidebar-quick-cite-box-modal").modal("show");
-        }
-    });
-}
-*/
-
-function openModalWithStyle(url) {
-    container = $("#sidebar-quick-cite-box-modal .modal-body");
-    container.empty();
-
-    $.ajax({
-        url: url,
-        success: function(data) {
-            $("#sidebar-quick-cite-box-modal .modal-body").html(data);
-            $("#sidebar-quick-cite-box-modal").modal("show");
-        }
-    });
-}
-
-
-function ajaxLoadLayout(link) {
-	link_parts = link.split("/");
-
-	switch (link_parts[1]) {
+	switch (url_parts[1]) {
 		case "bib":
 			openModalWithBibTex();
 			break;
 		case "csl":
 		case "layout":
-			if (link_parts[2] == "endnote") {
+			if (url_parts[2] == "endnote") {
 				openModalWithEndnote();
 			} else {
-			alert(link)
 				container = $("#sidebar-quick-cite-box-modal .modal-body");
                 container.empty();
 
                 $.ajax({
-                    url: link,
+                    url: url,
                     success: function(data) {
                         $("#sidebar-quick-cite-box-modal .modal-body").html(data);
                         $("#sidebar-quick-cite-box-modal").modal("show");
@@ -105,8 +70,8 @@ function ajaxLoadLayout(link) {
 			break;
 		case "csl-layout":
 			// load CSL via AJAX
-			csl_style = link_parts[2];
-			csl_url = "/csl/bibtex/" + link_parts[4];
+			csl_style = url_parts[2];
+			csl_url = "/csl/bibtex/" + url_parts[4];
 			container = $("#sidebar-quick-cite-box-modal .modal-body");
 			container.empty();
 
