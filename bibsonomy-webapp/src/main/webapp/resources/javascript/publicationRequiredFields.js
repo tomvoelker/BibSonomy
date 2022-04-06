d46fbcdf59013d939423ad44ef9988c0b2590626
Tom Völker <tom.voelker@stud-mail.uni-wuerzburg.de>
2022-04-06 11:10:48 +0200

    Reimplemented "I am author" Reminder

--- bibsonomy-webapp/src/main/webapp/resources/javascript/publicationRequiredFields.js
+++ bibsonomy-webapp/src/main/webapp/resources/javascript/publicationRequiredFields.js
@@ -1,5 +1,19 @@
 $(function () {
     myownTagInit($('#myownChkBox'), $('#inpf_tags'));
+
+    function authorWarning() {
+        if(isAuthor() && !$("#myownChkBox").is(':checked')){
+            $('#imAmAuthorWarning').attr("style", "display:block");
+        } else {
+            $('#imAmAuthorWarning').attr("style", "display:none");
+        }
+    }
+
+    $("#post\\.resource\\.editor").on('change',authorWarning);
+    $("#post\\.resource\\.author").on('change',authorWarning);
+    $('#myownChkBox').click( function() {
+        $('#imAmAuthorWarning').attr("style", "display:none");
+    })
 });
 
 function highlightMatches(text, input) {
@@ -48,4 +62,31 @@ function myownTagInit(chkbox, tagbox) {
             }
         })
         .parent().removeClass('hiddenElement');
+}
+
+function isAuthor() {
+    var allPossibleNames = [];
+    if(!$("#claimedPersonMainNameID").length){ //uses the User.realName as a fallback if no Person was claimed by the user
+        var individualUserRealName = $('#userRealnameID').val();
+        allPossibleNames.push(individualUserRealName);
+    } else { //uses all names saved in the person claimed by the current user
+        var userClaimedPersonMainName = $('#claimedPersonMainNameID').val();
+        allPossibleNames.push(userClaimedPersonMainName);
+        var userClaimedPersonNames = $('#claimedPersonNamesID').val();
+        allPossibleNames = allPossibleNames.concat(userClaimedPersonNames.split(" and ")).filter(function(n){ return n });
+    }
+
+    var enteredAuthors = $("#post\\.resource\\.author").val();
+    var individualEnteredAuthors = enteredAuthors.split("\n");
+    var enteredEditors = $("#post\\.resource\\.editor").val();
+    individualEnteredAuthors = individualEnteredAuthors.concat(enteredEditors.split("\n")).filter(function(n){ return n });
+
+    if (enteredAuthors.length !== 0 || enteredEditors.length !== 0){
+        var output = individualEnteredAuthors.filter(function (obj) {
+            return allPossibleNames.indexOf(obj) !== -1;
+        });
+        return output.length!==0
+    } else{ //Fallback if the input was left empty or the input was deleted
+        return false;
+    }
 }
