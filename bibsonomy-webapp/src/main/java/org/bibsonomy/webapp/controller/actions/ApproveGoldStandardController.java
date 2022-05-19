dad48230118f6b5ff5547cd3ceb0dc2d7cdca91b
Kevin Choong <choong.kvn@gmail.com>
2022-05-19 17:39:46 +0200

    refactored to action controller

--- bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/actions/ApproveGoldStandardController.java
+++ bibsonomy-webapp/src/main/java/org/bibsonomy/webapp/controller/actions/ApproveGoldStandardController.java
@@ -1,4 +1,33 @@
-package org.bibsonomy.webapp.controller.ajax;
+/**
+ * BibSonomy-Webapp - The web application for BibSonomy.
+ *
+ * Copyright (C) 2006 - 2021 Data Science Chair,
+ *                               University of Würzburg, Germany
+ *                               https://www.informatik.uni-wuerzburg.de/datascience/home/
+ *                           Information Processing and Analytics Group,
+ *                               Humboldt-Universität zu Berlin, Germany
+ *                               https://www.ibi.hu-berlin.de/en/research/Information-processing/
+ *                           Knowledge & Data Engineering Group,
+ *                               University of Kassel, Germany
+ *                               https://www.kde.cs.uni-kassel.de/
+ *                           L3S Research Center,
+ *                               Leibniz University Hannover, Germany
+ *                               https://www.l3s.de/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.bibsonomy.webapp.controller.actions;
 
 import static org.bibsonomy.util.ValidationUtils.present;
 
@@ -16,13 +45,13 @@ import org.bibsonomy.common.exceptions.AccessDeniedException;
 import org.bibsonomy.common.exceptions.DatabaseException;
 import org.bibsonomy.common.exceptions.ObjectMovedException;
 import org.bibsonomy.model.BibTex;
-import org.bibsonomy.model.GoldStandardPublication;
 import org.bibsonomy.model.Post;
 import org.bibsonomy.model.User;
+import org.bibsonomy.model.logic.LogicInterface;
 import org.bibsonomy.model.logic.PostLogicInterface;
 import org.bibsonomy.model.util.BibTexUtils;
 import org.bibsonomy.services.URLGenerator;
-import org.bibsonomy.webapp.command.ajax.ApproveGoldStandardAjaxCommand;
+import org.bibsonomy.webapp.command.actions.ApproveGoldStandardCommand;
 import org.bibsonomy.webapp.util.ErrorAware;
 import org.bibsonomy.webapp.util.MinimalisticController;
 import org.bibsonomy.webapp.util.RequestWrapperContext;
@@ -32,14 +61,16 @@ import org.springframework.validation.Errors;
 
 @Getter
 @Setter
-public class ApproveGoldStandardAjaxController extends AjaxController implements MinimalisticController<ApproveGoldStandardAjaxCommand>, ErrorAware {
+public class ApproveGoldStandardController implements MinimalisticController<ApproveGoldStandardCommand>, ErrorAware {
+
+    private LogicInterface logic;
+    private URLGenerator urlGenerator;
 
     private Errors errors;
-    private URLGenerator urlGenerator;
     private String redirectUrl;
 
     @Override
-    public View workOn(ApproveGoldStandardAjaxCommand command) {
+    public View workOn(ApproveGoldStandardCommand command) {
         final RequestWrapperContext context = command.getContext();
         final User loggedInUser = context.getLoginUser();
 
@@ -82,7 +113,7 @@ public class ApproveGoldStandardAjaxController extends AjaxController implements
         // get details with no username, since we are retrieving an existing gold standard
         Post<BibTex> postToUpdate = this.getPostDetails(interhash, "");
         if (!present(postToUpdate)) {
-            this.errors.reject("error.post.update", "Could not update post.");
+            this.errors.reject("error.post.notfound", "Could not find post.");
             return;
         }
 
@@ -113,7 +144,7 @@ public class ApproveGoldStandardAjaxController extends AjaxController implements
     private void handleCopyAndApprove(final String intrahash, final String username) {
         Post<BibTex> postToCreate = this.getPostDetails(intrahash, username);
         if (!present(postToCreate)) {
-            this.errors.reject("error.post.update", "Could not update post.");
+            this.errors.reject("error.post.notfound", "Could not find post.");
             return;
         }
         postToCreate.setApproved(true);
@@ -150,8 +181,8 @@ public class ApproveGoldStandardAjaxController extends AjaxController implements
     }
 
     @Override
-    public ApproveGoldStandardAjaxCommand instantiateCommand() {
-        return new ApproveGoldStandardAjaxCommand();
+    public ApproveGoldStandardCommand instantiateCommand() {
+        return new ApproveGoldStandardCommand();
     }
 
     @Override
