3627faa7eeee507530ffa7f052238a319667b1ed
Kevin Choong <choong.kvn@gmail.com>
2023-07-12 14:49:08 +0200

    set correct policy after dissemin request, using primarly doi and fall back to metadata with title, authors & year

--- bibsonomy-webapp/src/main/java/de/unikassel/puma/webapp/controller/ajax/DisseminController.java
+++ bibsonomy-webapp/src/main/java/de/unikassel/puma/webapp/controller/ajax/DisseminController.java
@@ -8,15 +8,15 @@ import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.http.HttpResponse;
+import org.apache.http.HttpException;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 import org.bibsonomy.model.BibTex;
 import org.bibsonomy.model.PersonName;
 import org.bibsonomy.model.Post;
+import org.bibsonomy.util.WebUtils;
 import org.bibsonomy.webapp.controller.ajax.AjaxController;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -25,8 +25,11 @@ import org.json.simple.parser.ParseException;
 
 public class DisseminController extends AjaxController {
 
-    public class DisseminConfig {
-        public static final String API_URL = "https://dissem.in/api/query/";
+    public static class DisseminConfig {
+        public static final String API_URL = "https://dissem.in/api/";
+        public static final String QUERY_URL = API_URL + "query/";
+        public static final String ERROR = "error";
+        public static final String PAPER = "paper";
         public static final String POLICY = "policy";
         public static final String POLICY_PREPRINT = "preprint";
         public static final String POLICY_POSTPRINT = "postprint";
@@ -35,6 +38,7 @@ public class DisseminController extends AjaxController {
         public static final String POLICY_CANNOT = "cannot";
         public static final String POLICY_RESTRICTED = "restricted";
         public static final String POLICY_UNKNOWN = "unknown";
+        public static final String RECORDS = "records";
         public static final String STATUS = "classification";
         public static final String STATUS_OA = "oa";
         public static final String STATUS_OK = "ok";
@@ -56,13 +60,76 @@ public class DisseminController extends AjaxController {
 
         // Get policy, if post is present
         if (present(post)) {
-            String postDocument = this.convertPost(post);
-            JSONObject resultJson = this.postRequest(postDocument);
-            if (present(resultJson)) {
+            String doi = post.getResource().getMiscField("doi");
+            if (present(doi)) {
+                this.handleByDoi(doi, policy);
+            } else {
+                this.handleByMetadata(post, policy);
             }
         }
 
         return policy;
+    }
+
+    private void processResponse(final String response, final Map<String, String> policy) {
+        if (present(response)) {
+            try {
+                JSONParser parser = new JSONParser();
+                JSONObject resultJson =  (JSONObject) parser.parse(response);
+                if (present(resultJson) && !resultJson.containsKey(DisseminConfig.ERROR)) {
+                    JSONObject paperJson = (JSONObject) resultJson.getOrDefault(DisseminConfig.PAPER, null);
+                    if (present(paperJson)) {
+                        policy.put(DisseminConfig.STATUS, ((String) paperJson.getOrDefault(DisseminConfig.STATUS, DisseminConfig.STATUS_UNK)).toLowerCase());
+
+                        JSONArray recordsJson = (JSONArray) paperJson.getOrDefault(DisseminConfig.RECORDS, new JSONArray());
+                        for (Object o : recordsJson) {
+                            if (o instanceof JSONObject) {
+                                JSONObject recordJson = (JSONObject) o;
+                                if (recordJson.containsKey(DisseminConfig.POLICY)) {
+                                    JSONObject policyJson = (JSONObject) recordJson.get(DisseminConfig.POLICY);
+                                    policy.put(DisseminConfig.POLICY_PREPRINT, (String) policyJson.getOrDefault(DisseminConfig.POLICY_PREPRINT, DisseminConfig.POLICY_UNKNOWN));
+                                    policy.put(DisseminConfig.POLICY_POSTPRINT, (String) policyJson.getOrDefault(DisseminConfig.POLICY_POSTPRINT, DisseminConfig.POLICY_UNKNOWN));
+                                    policy.put(DisseminConfig.POLICY_PUBLISHED, (String) policyJson.getOrDefault(DisseminConfig.POLICY_PUBLISHED, DisseminConfig.POLICY_UNKNOWN));
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            } catch (ParseException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private void handleByDoi(final String doi, final Map<String, String> policy) {
+        try {
+            final HttpGet httpGet = new HttpGet(DisseminConfig.API_URL + doi);
+            httpGet.setHeader("Accept", "application/json");
+
+            final String response = WebUtils.getContentAsString(WebUtils.getHttpClient(), httpGet);
+            this.processResponse(response, policy);
+        } catch (HttpException | IOException e) {
+            // TODO
+            e.printStackTrace();
+        }
+    }
+
+    private void handleByMetadata(final Post<? extends BibTex> post, final Map<String, String> policy) {
+        try {
+            String postDocument = this.convertPost(post);
+            final HttpPost httpPost = new HttpPost(DisseminConfig.QUERY_URL);
+            final StringEntity entity = new StringEntity(postDocument, ContentType.APPLICATION_JSON);
+            httpPost.setEntity(entity);
+            httpPost.setHeader("Accept", "application/json");
+            httpPost.setHeader("Content-type", "application/json");
+
+            final String response = WebUtils.getContentAsString(WebUtils.getHttpClient(), httpPost);
+            this.processResponse(response, policy);
+        } catch (HttpException | IOException e) {
+            // TODO
+            e.printStackTrace();
+        }
     }
 
     private String convertPost(final Post<? extends BibTex> post) {
@@ -80,32 +147,5 @@ public class DisseminController extends AjaxController {
         postJson.put("authors", authors);
 
         return postJson.toJSONString();
-    }
-
-    private JSONObject postRequest(String postDoc) {
-        try {
-            final HttpPost httpPost = new HttpPost(DisseminConfig.API_URL);
-
-            final StringEntity entity = new StringEntity(postDoc);
-            httpPost.setEntity(entity);
-            httpPost.setHeader("Accept", "application/json");
-            httpPost.setHeader("Content-type", "application/json");
-            final HttpResponse response;
-            try (CloseableHttpClient client = HttpClients.createDefault()) {
-                LOG.info("Check dissemin status for publication:" + postDoc);
-                response = client.execute(httpPost);
-            }
-
-            if (present(response)) {
-                final int statusCode = response.getStatusLine().getStatusCode();
-                final String stringResponse = EntityUtils.toString(response.getEntity());
-                JSONParser parser = new JSONParser();
-                JSONObject resultObj = (JSONObject) parser.parse(stringResponse);
-                return resultObj;
-            }
-        } catch (IOException | ParseException e) {
-            throw new RuntimeException(e);
-        }
-        return new JSONObject();
     }
 }
