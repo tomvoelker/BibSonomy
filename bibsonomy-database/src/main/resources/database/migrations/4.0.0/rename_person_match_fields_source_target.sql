777ed1502382e24ff3175897c6d816765e5a7738
Kevin Choong <choong.kvn@gmail.com>
2023-08-24 15:36:25 +0200

    release version 4.0.58, use change column instead of rename due to not working on some systems

--- bibsonomy-database/src/main/resources/database/migrations/4.0.0/rename_person_match_fields_source_target.sql
+++ bibsonomy-database/src/main/resources/database/migrations/4.0.0/rename_person_match_fields_source_target.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `person_match` RENAME COLUMN `person1_id` TO `target_person_id`;
-ALTER TABLE `person_match` RENAME COLUMN `person2_id` TO `source_person_id`;
+ALTER TABLE `person_match` CHANGE COLUMN `person1_id` `target_person_id` varchar(64) NOT NUll;
+ALTER TABLE `person_match` CHANGE COLUMN `person2_id` `source_person_id` varchar(64) NOT NUll;
