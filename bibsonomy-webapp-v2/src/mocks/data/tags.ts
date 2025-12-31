15476b8728cf194537ea355059b1661081e48990
Tom Völker <voelker@informatik.uni-wuerzburg.de>
2025-12-31 21:25:55 +0100

    Fix mock tags data to match TagSchema (use globalCount)

--- bibsonomy-webapp-v2/src/mocks/data/tags.ts
+++ bibsonomy-webapp-v2/src/mocks/data/tags.ts
@@ -3,16 +3,16 @@
  */
 
 export const mockTags = [
-  { name: 'deep-learning', count: 150, countPublic: 120 },
-  { name: 'machine-learning', count: 200, countPublic: 170 },
-  { name: 'neural-networks', count: 120, countPublic: 95 },
-  { name: 'computer-vision', count: 95, countPublic: 80 },
-  { name: 'nlp', count: 85, countPublic: 70 },
-  { name: 'python', count: 180, countPublic: 150 },
-  { name: 'tensorflow', count: 110, countPublic: 90 },
-  { name: 'pytorch', count: 105, countPublic: 88 },
-  { name: 'keras', count: 90, countPublic: 72 },
-  { name: 'scikit-learn', count: 75, countPublic: 60 },
-  { name: 'pandas', count: 140, countPublic: 115 },
-  { name: 'numpy', count: 130, countPublic: 105 },
+  { name: 'deep-learning', globalCount: 150 },
+  { name: 'machine-learning', globalCount: 200 },
+  { name: 'neural-networks', globalCount: 120 },
+  { name: 'computer-vision', globalCount: 95 },
+  { name: 'nlp', globalCount: 85 },
+  { name: 'python', globalCount: 180 },
+  { name: 'tensorflow', globalCount: 110 },
+  { name: 'pytorch', globalCount: 105 },
+  { name: 'keras', globalCount: 90 },
+  { name: 'scikit-learn', globalCount: 75 },
+  { name: 'pandas', globalCount: 140 },
+  { name: 'numpy', globalCount: 130 },
 ]
