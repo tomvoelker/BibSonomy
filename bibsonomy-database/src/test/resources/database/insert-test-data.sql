b80db69f18c065c9a06fa62adef3bc3655aeb0bc
Tom Völker <voelker@informatik.uni-wuerzburg.de>
2025-12-31 20:34:00 +0100

    Fix duplicate ids table entries in test data
    Use REPLACE INTO for ids table to update values set by init.sql

--- bibsonomy-database/src/test/resources/database/insert-test-data.sql
+++ bibsonomy-database/src/test/resources/database/insert-test-data.sql
@@ -306,11 +306,12 @@ INSERT INTO `group_preset_tags` VALUES
 ('testtag2', 'testgroup1', 3, null),
 ('testtag3', 'testgroup1', 3, 'Tag 3');
 
--- 
+--
 -- Data for table `ids`
--- 
+-- Note: Using REPLACE because init.sql creates these with value 0, but tests need specific values
+--
 
-INSERT INTO `ids` VALUES 
+REPLACE INTO `ids` VALUES
 (0,  1073741827, 'content_id'),
 (1,  1073741830, 'tas id'),
 (2,  21,  'relation id'),
